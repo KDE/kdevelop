dd3271a619571473d85d433dc9df3d39a5c73622
Pascal Krahmer <pascal@beast.de>
1999-03-28 22:15:00 +0000

    1999-03-29 Pascal Krahmer <pascal@beast.de>         * fixed another segfault bug         * since now only one item can be selected at the same time         * "items"-view now selects the item selected in the listview         * properties of the selected item are shown now         * improved "Cursor"-property edit widget (now it is also possible to set a file as cursor).           However up to now the properties are ignored.

--- kdevelop/ckdevelop_kdlginit.cpp
+++ kdevelop/ckdevelop_kdlginit.cpp
@@ -29,7 +29,7 @@ void CKDevelop::initKDlg(){
 
   kdlg_widgets_view= new KDlgWidgets(kdlg_tabctl,"widgets_view");
   kdlg_dialogs_view = new KDlgDialogs(kdlg_tabctl,"dialogs_view");
-  kdlg_items_view = new KDlgItems(kdlg_tabctl,"items_view");
+  kdlg_items_view = new KDlgItems(this,kdlg_tabctl,"items_view");
 
   kdlg_tabctl->addTab(kdlg_widgets_view,i18n("Widgets"));
   kdlg_tabctl->addTab(kdlg_dialogs_view,i18n("Dialogs"));
