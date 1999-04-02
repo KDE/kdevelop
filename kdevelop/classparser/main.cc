4b6a885bec6107d5122ab2d29a16ecfe01755822
Jonas Nordin <jonas.nordin@cenacle.se>
1999-04-02 10:59:12 +0000

    Minor fixes.

--- kdevelop/classparser/main.cc
+++ kdevelop/classparser/main.cc
@@ -1,14 +1,15 @@
-#include <qlist.h>
 #include <iostream.h>
 #include "ClassParser.h"
-#include "ParsedClass.h"
 
 int main( int argc, char *argv[] )
 {
   CClassParser cp;
   
   for( int i=1; i < argc; i++ )
+  {
+    cout << "Parsing: " << argv[ i ] << "\n";
     cp.parse( argv[ i ] );
+  }
 
   cp.out();
 }
