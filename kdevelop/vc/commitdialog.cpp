571e7bdb2d7c20d1571cb8ce1ea56757350f8f30
Bernd Gehrmann <bernd@mail.berlios.de>
1999-07-23 17:20:19 +0000

    Sorry, forgot to commit these

---
+++ kdevelop/vc/commitdialog.cpp
@@ -0,0 +1,45 @@
+#include <qlayout.h>
+#include <qlabel.h>
+#include <kapp.h>
+#include <kbuttonbox.h>
+#include <klocale.h>
+
+#include "commitdialog.h"
+#include "commitdialog.moc"
+
+
+CommitDialog::CommitDialog()
+    : QDialog(0, "", true)
+{
+    setCaption( i18n("CVS Commit") );
+
+    QBoxLayout *layout = new QVBoxLayout(this, 10);
+    
+    QLabel *messagelabel = new QLabel(i18n("Log message:"), this);
+    messagelabel->setMinimumSize(messagelabel->sizeHint());
+    layout->addWidget(messagelabel, 0);
+    
+    edit = new QMultiLineEdit(this);
+    QFontMetrics fm(edit->fontMetrics());
+    edit->setMinimumSize(fm.width("0")*40, fm.lineSpacing()*3);
+    layout->addWidget(edit, 10);
+    
+    KButtonBox *buttonbox = new KButtonBox(this);
+    buttonbox->addStretch();
+    QPushButton *ok = buttonbox->addButton(i18n("Ok"));
+    QPushButton *cancel = buttonbox->addButton(i18n("Cancel"));
+    connect( ok, SIGNAL(clicked()), SLOT(accept()) );
+    connect( cancel, SIGNAL(clicked()), SLOT(reject()) );
+    ok->setDefault(true);
+    buttonbox->layout();
+    layout->addWidget(buttonbox, 0);
+
+    layout->activate();
+    adjustSize();
+}
+
+// Local Variables:
+// c-basic-offset: 4
+// End:
+
+    
