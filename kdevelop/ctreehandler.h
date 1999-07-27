5ff8ffb998e13a20bce8ed9860f4a4d1ffdc6846
Jonas Nordin <jonas.nordin@cenacle.se>
1999-07-27 10:02:46 +0000

    Added comments.

--- kdevelop/ctreehandler.h
+++ kdevelop/ctreehandler.h
@@ -25,7 +25,7 @@
 
 typedef enum
 { 
-  THFOLDER,THFOLDER_OPEN, THCLASS, THPROJECT, THBOOK, THBOOK_OPEN, THDOC,
+  THFOLDER, THFOLDER_OPEN, THCLASS, THPROJECT, THBOOK, THBOOK_OPEN, THDOC,
   THGLOBAL_FUNCTION, THGLOBAL_VARIABLE,
   THPUBLIC_METHOD, THPROTECTED_METHOD, THPRIVATE_METHOD, 
   THPUBLIC_ATTR, THPROTECTED_ATTR, THPRIVATE_ATTR,
@@ -34,7 +34,13 @@ typedef enum
   THSTRUCT, THC_FILE, THINSTALLED_FILE, THDELETE, THEND_POS
 } THType;
 
-/** */
+/** Generic class to handle the troblesome process of adding items to
+ * a tree. 
+ * This class also has methods to check the type of the currently
+ * selected item. The type-checking is based upon the unique id 
+ * every pixmap gets when it is loaded.
+ * @author Jonas Nordin
+ */
 class CTreeHandler
 {
 public: // Constructor & destructor
@@ -44,10 +50,14 @@ public: // Constructor & destructor
 
 public: // Public methods to set attribute values.
 
-  /** Set the tree to draw in. */
+  /** Set the tree to draw in. 
+   * @param aTree The list view to make operations on.
+   */
   void setTree( QListView *aTree );
 
-  /** Set the last added item. */
+  /** Set the last added item. 
+   * @param aItem The last item added to the view.
+   */
   void setLastItem( QListViewItem *aItem );
 
 public: // Public methods
@@ -55,22 +65,37 @@ public: // Public methods
   /** Clear the view and reset internal state. */
   void clear();
 
-  /** Add an item at root level. */
+  /** Add an item at root level. 
+   * @param aName The name to show in the view.
+   * @param iconType What kind of icon should be shown.
+   */
   QListViewItem *addRoot( const char *aName, THType iconType );
 
-  /** Add an item with the selected icon. */
+  /** Add an item with the selected icon. 
+   * @param aName The name to show in the view.
+   * @param iconType What kind of icon should be shown.
+   * @param parent Which parent should this item be added to.
+   */
   QListViewItem *addItem( const char *aName, THType iconType,
                           QListViewItem *parent );
 
 public: // Public queries
 
-  /** Return the selected pixmap. */
+  /** Return the selected pixmap. 
+   * @param anIcon Type for which we want to fetch the icon.
+   * @return Pointer to the wanted icon.
+   */
   QPixmap *getIcon( THType anIcon );
 
-  /** Return the type of the current icon. */
+  /** Return the type of the current icon. 
+   * @return The type of the currently selected item.
+   */
   THType itemType();
 
-  /** Return the type of the item. */
+  /** Return the type of the item. 
+   * @param item Item to check the type on.
+   * @return The type of a supplied item.
+   */
   THType itemType( QListViewItem *item );
 
 protected: // Protected attributes
@@ -83,7 +108,7 @@ private: // Private attributes
   /** The last item added to the tree. */
   QListViewItem *lastItem;
 
-  /** The last rootitem added to the tree. */
+  /** The last root-item added to the tree. */
   QListViewItem *lastRootItem;
 
   /** Array containing all pixmaps used */
@@ -99,5 +124,3 @@ private: // Private methods
 };
 
 #endif
-
-
