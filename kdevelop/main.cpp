9cce49089046c520f65b9352487b598abd011eb5
Sandy Meier <smeier@rz.uni-potsdam.de>
1999-11-13 15:03:21 +0000

    * if cvs is enabled the workspace isn't saved anymore, this reduced merging conflict in *.kdevprj 	* commited patch by Ralf Palsa <rpalsa@eso.org> (fixed kdevelop --setup segfault, if logo is disabled

--- kdevelop/main.cpp
+++ kdevelop/main.cpp
@@ -19,30 +19,15 @@
 #include "ckdevelop.h"
 #include "kstartuplogo.h"
 #include "ckdevinstall.h"
-//#include <kwmmapp.h>
+#include <kwmmapp.h>
 
-#include "./kwrite/kguicommand.h"
-#include "./kwrite/kwview.h"
-
-
-KGuiCmdManager cmdMngr; //manager for user -> gui commands
 
 
 int main(int argc, char* argv[]) {
   
   KStartupLogo* start_logo=0L;
-  KGuiCmdKWMModuleApp a(argc,argv,"kdevelop");//KWMModuleApplication a(argc,argv,"kdevelop");
+  KWMModuleApplication a(argc,argv,"kdevelop");
 	a.connectToKWM();
-
-  // commands
-  KWrite::addCursorCommands(cmdMngr);
-  KWrite::addEditCommands(cmdMngr);
-  KWrite::addFindCommands(cmdMngr);
-  KWrite::addStateCommands(cmdMngr);
-  cmdMngr.makeDefault();
-  cmdMngr.readConfig(a.getConfig());
-
-
   a.getConfig()->setGroup("General Options");
   bool bStartLogo= a.getConfig()->readBoolEntry("Logo",true);
   bool bInstall=a.getConfig()->readBoolEntry("Install",true);
@@ -61,7 +46,11 @@ int main(int argc, char* argv[]) {
   else{
     if(bInstall){
       CKDevInstall* install=new CKDevInstall(0,"install");
-      start_logo->close();
+      if (bStartLogo) {
+	start_logo->close();
+	delete start_logo;
+	bStartLogo = false;
+      }
       install->show();
       delete install;
     }
