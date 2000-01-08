2fd3f7669260faa0b7601d14ce0a4b858b79326f
John Birch <jbb@kdevelop.org>
2000-01-08 00:35:15 +0000

    "i" should be "i" not "Next" or "Step in" in this case.

--- kdevelop/dbgtoolbar.cpp
+++ kdevelop/dbgtoolbar.cpp
@@ -268,13 +268,21 @@ DbgToolbar::DbgToolbar(DbgController* dbgController, CKDevelop* parent) :
   DbgButton*    bNext       = new DbgButton(0, pm, this);
 
   pm.load(KApplication::kde_datadir() + "/kdevelop/toolbar/dbgnext.xpm");
-  DbgButton*    bNexti      = new DbgButton(i18n("Next"), pm, this);
+  // Single character "i" means a machine instruction. We
+  // rely on the icon to give meaning to this button. This button is
+  // partnered with the source code "next" button, and not used in the toolbar
+  // size routine.
+  DbgButton*    bNexti      = new DbgButton(i18n("i"), pm, this);
 
   pm.load(KApplication::kde_datadir() + "/kdevelop/toolbar/dbgstep.xpm");
   DbgButton*    bStep       = new DbgButton(0, pm, this);
 
   pm.load(KApplication::kde_datadir() + "/kdevelop/toolbar/dbgstep.xpm");
-  DbgButton*    bStepi      = new DbgButton(i18n("Step in"), pm, this);
+  // Single character "i" means a machine instruction. We
+  // rely on the icon to give meaning to this button. This button is
+  // partnered with the source code "step" button, and not used in the toolbar
+  // size routine.
+  DbgButton*    bStepi      = new DbgButton(i18n("i"), pm, this);
 
   pm.load(KApplication::kde_datadir() + "/kdevelop/toolbar/dbgstepout.xpm");
   DbgButton*    bFinish     = new DbgButton(i18n("Step out"), pm, this);
