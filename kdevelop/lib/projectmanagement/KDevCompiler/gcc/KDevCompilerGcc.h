5d550abcd995e47b786de4ae138cab20bc25e962
Omid Givi <omid@givi.nl>
2001-02-09 15:14:15 +0000

    gcc compiler entity initialization

---
+++ kdevelop/lib/projectmanagement/KDevCompiler/gcc/KDevCompilerGcc.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+                          GccCompiler.h  -  description
+                             -------------------
+    begin                : Thu Feb 1 2001
+    copyright            : (C) 2001 by Omid Givi
+    email                : omid@givi.nl
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KDEVCOMPILERGCC_H
+#define KDEVCOMPILERGCC_H
+
+#include <KDevCompiler.h>
+
+/**
+  *@author Omid Givi
+  */
+
+#endif
