11f3caf727f55408fc666df031ac0ab8b5ef60c2
Richard Dale <Richard_Dale@tipitina.demon.co.uk>
2001-05-31 17:42:21 +0000

    Added Java plugin template

---
+++ parts/appwizard/kdevpartjava/kdevpartjava_part.java
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (C) 2001 $AUTHOR$ <$EMAIL$>   
+ */
+ 
+import org.kde.qt.*;
+import org.kde.koala.*;
+
+class $APPNAME$Part extends KDevPart
+{
+
+
+  private $APPNAME$Widget m_widget;
+
+public $APPNAME$Part(KDevApi api, QObject parent, String name)
+{
+  super(api, parent, name);
+  m_widget = new $APPNAME$Widget(this);
+  
+  QWhatsThis.add(m_widget, "WHAT DOES THIS PART DO?");
+  
+  core().embedWidget(m_widget, KDevCore.OutputView, "$APPNAME$");
+}
+
+}
+
---
+++ parts/appwizard/kdevpartjava/kdevpartjava_widget.java
@@ -0,0 +1,13 @@
+
+import org.kde.qt.*;
+import org.kde.koala.*;
+
+class $APPNAME$Widget extends QWidget
+{
+
+public $APPNAME$Widget($APPNAME$Part part)
+{
+	super(null, "$APPNAMELC$ widget");
+}
+
+}
