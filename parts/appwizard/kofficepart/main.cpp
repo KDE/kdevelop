4efc3cbe3cf449d1a6054018b81af94ff684d202
Ian Reinhart Geiser <geiseri@kde.org>
2001-07-19 19:45:13 +0000

    Here are the KOffice application templates I have been promiseing for the last year and a half... They are in need of some debugging but they look okay.

---
+++ parts/appwizard/kofficepart/main.cpp
@@ -0,0 +1,32 @@
+/* This template is based off of the KOffice example written by Torben Weis <weis@kde.org
+   It was converted to a KDevelop template by Ian Reinhart Geiser <geiseri@yahoo.com>
+*/
+
+#include <koApplication.h>
+#include <koDocument.h>
+#include <koMainWindow.h>
+#include <kcmdlineargs.h>
+#include <klocale.h>
+#include <dcopclient.h>
+#include "$APPNAMELC$_aboutdata.h"
+
+
+static const KCmdLineOptions options[]=
+{
+	{"+[file]", I18N_NOOP("File To Open"),0},
+	{0,0,0}
+};
+
+int main( int argc, char **argv )
+{
+    KCmdLineArgs::init( argc, argv, newExampleAboutData() );
+    KCmdLineArgs::addCmdLineOptions( options );
+    KoApplication app;
+
+    app.dcopClient()->attach();
+    app.dcopClient()->registerAs( "$APPNAMELC$" );
+
+    if (!app.start()) // parses command line args, create initial docs and shells
+	return 1;
+    return app.exec();
+}
