c4948727395e98e928111e83a7aa30ef6eed74c2
Daniel Haberkorn <dhaberkorn@gmx.de>
2001-08-03 19:08:22 +0000

    only interface definition

--- parts/cppsupport/cppcodecompletionparser.cpp
+++ parts/cppsupport/cppcodecompletionparser.cpp
@@ -1,11 +1,9 @@
 // Here resides the C/C++ code completion parser
 
-CppCodeCompletionParser::CppCodeCompletionParser ( KEditor::EditDocumentIface* pEditIface, int nLine, int nCol );
+CppCodeCompletionParser::CppCodeCompletionParser ( KEditor::EditDocumentIface* pEditIface, ClassStore* pStore );
 {
 	m_pEditIface = pEditIface;
-	m_bIsPointer = false;
-
-	m_nLine = nLine; m_nCol = nCol;
+	m_pStore = pStore;
 }
 
 CppCodeCompletionParser::~CppCodeCompletionParser()
@@ -13,17 +11,67 @@ CppCodeCompletionParser::~CppCodeCompletionParser()
 
 }
 
-QString CppCodeCompletionParser::getTypeOf ( QString& strObject );
+int CppCodeCompletionParser::getNodePos ( int nCol )
+{
+	return 0;
+}
+
+QString CppCodeCompletionParser::getNodeText ( int nNode )
+{
+	return "";
+}
+
+QString CppCodeCompletionParser::getNodeDelimiter ( int nNode )
+{
+	return "";
+}
+
+QString CppCodeCompletionParser::getTypeOfObject ( const QString& strObject, int nLine )
+{
+	return "";
+}
+
+QString CppCodeCompletionParser::getReturnTypeOfMethod ( const QString& strMethod )
+{
+	return "";
+}
+
+QValueList CppCodeCompletionParser::getEntryListForClass ( const QString& strClass )
 {
 	return NULL;
 }
 
-bool CppCodeCompletionParser::isPointer ( QString& strObject );
+QValueList CppCodeCompletionParser::getEntryListForNamespace ( const QString& strNamespace )
 {
-	if ( m_strCurObject == strObject )
-		return m_bIsPointer;
+	return NULL;
+}
 
-	getTypeOf ( strObject );
+QValueList CppCodeCompletionParser::getEntryListForStruct ( const QString& strStruct )
+{
+	return NULL;
+}
 
-	return m_bIsPointer;
+bool CppCodeCompletionParser::isMethod ( const QString& strNodeText )
+{
+	return false;
+}
+
+bool CppCodeCompletionParser::isObject ( const QString& strNodeText )
+{
+	return false;
+}
+
+bool CppCodeCompletionParser::isClass ( const QString& strNodeText )
+{
+	return false;
+}
+
+bool CppCodeCompletionParser::isStruct ( const QString& strNodeText )
+{
+	return false;
+}
+
+bool CppCodeCompletionParser::isNamespace ( const QString& strNodeText )
+{
+	return false;
 }
--- parts/cppsupport/cppcodecompletionparser.h
+++ parts/cppsupport/cppcodecompletionparser.h
@@ -1,28 +1,44 @@
 // Here resides the C/C++ code completion parser
 
-#ifndef __CPPCODECOMPLETION_H__
-#define __CPPCODECOMPLETION_H__
+#ifndef __CPPCODECOMPLETIONPARSER_H__
+#define __CPPCODECOMPLETIONPARSER_H__
 
 #include <qstring.h>
+#include <qvaluelist.h>
 
 #include "keditor/edit_iface.h"
+#include "classstore.h"
+#include "parsedclass.h"
+#include "parsedmethod.h"
 
 class CppCodeCompletionParser
 {
 	public:
-		CppCodeCompletionParser ( KEditor::EditDocumentIface* pEditIface, int nLine, int nCol );
+		CppCodeCompletionParser ( KEditor::EditDocumentIface* pEditIface, ClassStore* pStore );
 		virtual ~CppCodeCompletionParser();
 
 	public:
-		QString getTypeOf ( QString& strObject );
-		bool isPointer ( QString& strObject );
+		int getNodePos ( int nCol );
+		QString getNodeText ( int nNode );
+		QString getNodeDelimiter ( int nNode );
+		QString getTypeOfObject ( const QString& strObject, int nLine );
+		QString getReturnTypeOfMethod ( const QString& strMethod );
+		QValueList getEntryListForClass ( const QString& strClass );
+		QValueList getEntryListForNamespace ( const QString& strNamespace );
+		QValueList getEntryListForStruct ( const QString& strStruct );
+		bool isMethod ( const QString& strNodeText );
+		bool isObject ( const QString& strNodeText );
+		bool isClass ( const QString& strNodeText );
+		bool isStruct ( const QString& strNodeText );
+		bool isNamespace ( const QString& strNodeText );
+
+		void setLine ( const QString& strLine ) { m_strCurLine = strLine };
 
 	private:
 		KEditor::EditDocumentIface* m_pEditIface;
-		QString m_strCurObject;
-		bool m_bIsPointer;
-		int m_nLine;
-		int m_nCol;
+		ClassStore* m_pStore;
+		QString m_strCurLine;
+
 };
 
 #endif
