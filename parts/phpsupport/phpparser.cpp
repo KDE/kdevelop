5141ef26a953b87a1cc37a0b94cb82ed8174f716
Bernd Gehrmann <bernd@mail.berlios.de>
2001-09-27 20:24:01 +0000

    This is not my stuff :-)

--- parts/phpsupport/phpparser.cpp
+++ parts/phpsupport/phpparser.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2001 by Bernd Gehrmann,Sandy Meier                      *
- *   bernd@kdevelop.org,smeier@kdevelop.org                                *
+ *   Copyright (C) 2001 by Sandy Meier                                     *
+ *   smeier@kdevelop.org                                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
--- parts/phpsupport/phpparser.h
+++ parts/phpsupport/phpparser.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2001 by Bernd Gehrmann,Sandy Meier                      *
- *   bernd@kdevelop.org,smeier@kdevelop.org                                *
+ *   Copyright (C) 2001 by Sandy Meier                                     *
+ *   smeier@kdevelop.org                                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -17,7 +17,7 @@
 class KDevCore;
 class ClassStore;
 /**
-  *@author Sandy Meier, Bernd Gehrmann
+  *@author Sandy Meier
   */
 
 class PHPParser {
