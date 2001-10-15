8ffef790bd99bde334345e6bb94720e1e9d19d0a
Harald Fernengel <harry@kdevelop.org>
2001-10-15 13:59:02 +0000

    (hopefully) fixed disappearing-cursor bug

--- kdevelop/kwrite/kwview.cpp
+++ kdevelop/kwrite/kwview.cpp
@@ -947,10 +947,15 @@ void KWriteView::placeCursor(int x, int y, int flags) {
 }
 
 void KWriteView::focusInEvent(QFocusEvent *e) {
+
+/* Removed by HarryF (harry@bnro.de) because the cursor could disappear when 
+   opening and closing Popup Menus. (10/15/01)
+
   // every widget get a focusInEvent when a popup menu is opened!?! -> maybe bug of QT
   if (e && ((e->reason())==QFocusEvent::Popup)) {
     return;
   }
+*/
   if (m_hasFocus)
     return;
   else
