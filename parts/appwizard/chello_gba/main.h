13a3a7a67e4d0c02a6962601058c10f2159c5e23
Sandy Meier <smeier@rz.uni-potsdam.de>
2001-11-29 16:00:15 +0000

    added GBA (Gameboy Advance) "hello world" template. :-)

---
+++ parts/appwizard/chello_gba/main.h
@@ -0,0 +1,11 @@
+
+// gba.h has all the register definitions
+#include "gba.h"     
+
+extern font_Tiles[]; // the letters as 8x8 tiles
+extern Master_Palette[]; // the color palette 
+
+void print(char* text,u16 x,u16 y);
+void initTextMode();
+
+
