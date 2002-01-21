7f6560eb08b136b631b9d2e5387fe566cbafa14b
Eray Ozkural <erayo@cs.bilkent.edu.tr>
2002-01-21 03:54:19 +0000

    * revert Harald's change that didn't work, I need a working replace there, but that's not until  we understand exactly what's wrong there.

--- kdevelop/kwrite/kwdoc.cpp
+++ kdevelop/kwrite/kwdoc.cpp
@@ -1101,7 +1101,8 @@ void KWriteDoc::newLine(KWriteView *view, VConfig &c) {
     c.cursor.y++;
     c.cursor.x = 0;
     if (pos > 0) {
-      recordReplace(c.cursor,0,textLine->getText(),pos);
+      // TODO: need a working record here now
+      //recordReplace(c.cursor,0,textLine->getText(),pos);
       c.cursor.x = pos;
     }
   }
