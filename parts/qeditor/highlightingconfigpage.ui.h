55f45f7223f76d6de8ff15412a62d43fd836c94a
Roberto Raggi <roberto@kdevelop.org>
2002-06-11 21:38:06 +0000

    now you can change the font

--- parts/qeditor/highlightingconfigpage.ui.h
+++ parts/qeditor/highlightingconfigpage.ui.h
@@ -53,7 +53,8 @@ void HighlightingConfigPage::accept()
 void HighlightingConfigPage::slotUpdatePreview()
 {
     kdDebug() << "HighlightingConfigPage::slotUpdatePreview()" << endl;
-    QFont font( comboFontFamily->currentFont(), spinFontSize->value() );
+    
+    QFont font( comboFontFamily->currentText(), spinFontSize->value() );
     font.setBold( checkBold->isChecked() );
     font.setItalic( checkItalic->isChecked() );
     font.setUnderline( checkUnderline->isChecked() );
