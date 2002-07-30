11a3bd9350cd580fed556d896abd905c691fb5c0
Harald Fernengel <harry@kdevelop.org>
2002-07-30 15:00:11 +0000

    forgot to commit these for the diff frontend

---
+++ lib/interfaces/kdevdifffrontend.cpp
@@ -0,0 +1,13 @@
+#include "kdevdifffrontend.h"
+
+
+KDevDiffFrontend::KDevDiffFrontend(QObject *parent, const char *name)
+    : KDevPlugin(parent, name)
+{
+}
+
+KDevDiffFrontend::~KDevDiffFrontend()
+{
+}
+
+#include "kdevdifffrontend.moc"
---
+++ lib/interfaces/kdevdifffrontend.h
@@ -0,0 +1,38 @@
+/**
+ * The interface to a diff frontend
+ */
+
+#ifndef _KDEVDIFFFRONTEND_H_
+#define _KDEVDIFFFRONTEND_H_
+
+#include <kurl.h>
+#include "kdevplugin.h"
+
+
+class KDevDiffFrontend : public KDevPlugin
+{
+    Q_OBJECT
+    
+public:
+    
+    KDevDiffFrontend( QObject *parent=0, const char *name=0 );
+    virtual ~KDevDiffFrontend();
+
+    /**
+     * displays the patch.
+     */
+    virtual void showDiff( const QString& diff ) = 0;
+
+    /**
+     * displays a patch file
+     */
+    virtual void openURL( const KURL &url ) = 0;
+
+    /**
+     * displays the difference between the two files
+     */
+    virtual void showDiff( const KURL &url1, const KURL &url2 ) = 0;
+
+};
+
+#endif
