7f2283cd3160704302ac1fb99618f7fa34be69b5
Mickael Marchand <marchand@kde.org>
2002-08-25 20:23:23 +0000

    adding GPL copyright statements

--- parts/svn/svnoptionswidget.cpp
+++ parts/svn/svnoptionswidget.cpp
@@ -1,3 +1,22 @@
+/* Copyright (C) 2002 
+   Mickael Marchand <marchand@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+   */
+
 #include "domutil.h"
 #include "svnoptionswidget.h"
 #include "svn_part.h"
