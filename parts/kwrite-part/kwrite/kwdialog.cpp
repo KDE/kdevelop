a5e15486461dd21f7b61d811b538178c1520d0e6
Malcolm Hunter <malcolm.hunter@gmx.co.uk>
2002-09-03 00:55:06 +0000

    CVS_SILENT Consistency of words

--- parts/kwrite-part/kwrite/kwdialog.cpp
+++ parts/kwrite-part/kwrite/kwdialog.cpp
@@ -247,9 +247,9 @@ IndentConfigTab::IndentConfigTab(QWidget *parent, KWrite *kWrite)
 
   // What is this? help
   QWhatsThis::add(opt[0], i18n("When <b>Auto indent</b> is on, KWrite will indent new lines to equal the indent on the previous line.<p>If the previous line is blank, the nearest line above with text is used"));
-  QWhatsThis::add(opt[1], i18n("Check this if you want to indent with spaces rather than tabs.<br>A Tab will be converted to <u>Tab-width</u> as set in the <b>edit</b> options"));
-  QWhatsThis::add(opt[2], i18n("This allows the <b>backspace</b> key to be used to indent."));
-  QWhatsThis::add(opt[3], i18n("This allows the <b>tab</b> key to be used to indent."));
+  QWhatsThis::add(opt[1], i18n("Check this if you want to indent with spaces rather than tabs.<br>A Tab will be converted to <u>Tab-width</u> as set in the <b>Edit</b> options"));
+  QWhatsThis::add(opt[2], i18n("This allows the <b>Backspace</b> key to be used to indent."));
+  QWhatsThis::add(opt[3], i18n("This allows the <b>Tab</b> key to be used to indent."));
   QWhatsThis::add(opt[4], i18n("This retains current indentation settings for future documents."));
   QWhatsThis::add(opt[5], i18n("Indentations of more than the selected number of spaces will not be shortened."));
 }
