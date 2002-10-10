5fb324b1ae885137853780c8984f4f6de2c30d57
Caleb Tennis <caleb@gentoo.org>
2002-10-10 16:08:18 +0000

    Move the splashscreen to the center of an active monitor for Xinerama folks

--- src/splashscreen.cpp
+++ src/splashscreen.cpp
@@ -34,10 +34,17 @@ SplashScreen::SplashScreen()
 
   showMessage(i18n("Starting core application"));
 
-  m_splash->resize(m_splash->sizeHint());
-  m_splash->setGeometry(QApplication::desktop()->width()/2-m_splash->width()/2,
-              QApplication::desktop()->height()/2-m_splash->height()/2,
-              m_splash->width(), m_splash->height());
+  // m_splash->resize(m_splash->sizeHint());
+
+  QRect rect = QApplication::desktop()->screenGeometry(
+    QApplication::desktop()->screenNumber(QPoint(0,0)));
+  m_splash->move(rect.x() + (rect.width() - m_splash->sizeHint().width()) / 2,
+     rect.y() + (rect.height() - m_splash->sizeHint().height()) / 2);
+  m_splash->setFixedSize(m_splash->sizeHint());
+
+//  m_splash->setGeometry(QApplication::desktop()->width()/2-m_splash->width()/2,
+//              QApplication::desktop()->height()/2-m_splash->height()/2,
+//              m_splash->width(), m_splash->height());
 
   m_splash->show();
 }
