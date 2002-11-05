bede365a24fa1436857562cd3b924d8482e0ecf1
Yann Hodique <Yann.Hodique@lifl.fr>
2002-11-05 21:56:36 +0000

    global cvs part

---
+++ parts/cvs/globalcvs/globalcvs.cpp
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *   Copyright (C) 2002 by Yann Hodique                                    *
+ *   Yann.Hodique@lifl.fr                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ ***************************************************************************/
+
+#include "globalcvs.h"
+
+#include <kaction.h>
+#include <klocale.h>
+#include <kgenericfactory.h>
+#include <qlabel.h>
+#include <qlineedit.h>
+#include <qcheckbox.h>
+#include "cvsform.h"
+#include "kdevmakefrontend.h"
+
+typedef KGenericFactory<GlobalCvs> GlobalCvsFactory;
+K_EXPORT_COMPONENT_FACTORY( libkdevglobalcvs, GlobalCvsFactory( "kdevglobalcvs" ) );
+
+GlobalCvs::GlobalCvs(QObject *parent, const char *name, const QStringList &)
+        : KDevGlobalVersionControl(parent, name,"Cvs") {
+
+    setInstance(GlobalCvsFactory::instance());
+    setXMLFile("kdevglobalcvs.rc");
+
+    KAction * action = new KAction( i18n("Import Cvs Repository..."),"wizard", 0,
+                                    this, SLOT(slotImportCvs()),
+                                    actionCollection(), "cvs_import" );
+    action->setStatusText( i18n("Imports an existing Cvs repository.") );
+}
+
+
+GlobalCvs::~GlobalCvs() {}
+
+void GlobalCvs::slotImportCvs() {}
+
+QWidget* GlobalCvs::newProjectWidget(QWidget *parent) {
+    form = new CvsForm(parent);
+    form->init_check->setEnabled(false);
+    return form;
+}
+
+void GlobalCvs::createNewProject(QString dir) {
+    if (!form)
+        return;
+    QString command = "cd "+ dir +" && cvs -d " + form->root_edit->text() + " import -m '" + form->message_edit->text() + "' "
+                      + form->repository_edit->text() + " " + form->vendor_edit->text() + " " + form->release_edit->text();
+    makeFrontend()->queueCommand(dir,command);
+}
