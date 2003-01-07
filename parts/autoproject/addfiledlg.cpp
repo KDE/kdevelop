7361cdb10c04c2d86e2a44ecc148dc7bc388052e
Victor Roeder <Victor_Roeder@GMX.de>
2003-01-07 13:28:34 +0000

    * see ChangeLog for details

--- parts/autoproject/addfiledlg.cpp
+++ parts/autoproject/addfiledlg.cpp
@@ -35,22 +35,25 @@
 #include "kdevpartcontroller.h"
 
 AddFileDialog::AddFileDialog(AutoProjectPart *part, AutoProjectWidget *widget,
-                             SubprojectItem *spitem, TargetItem *item,
-                             QWidget *parent, const char *name)
-    : AddFileDlgBase(parent, name, true)
+							SubprojectItem *spitem, TargetItem *item,
+							QWidget *parent, const char *name)
+	: AddFileDlgBase(parent, name, true)
 {
-    connect ( createButton, SIGNAL ( clicked() ), this, SLOT ( accept() ) );
-    connect ( cancelButton, SIGNAL ( clicked() ), this, SLOT ( reject() ) );
+	connect ( createButton, SIGNAL ( clicked() ), this, SLOT ( accept() ) );
+	connect ( cancelButton, SIGNAL ( clicked() ), this, SLOT ( reject() ) );
 
-    directoryLabel->setText ( spitem->path );
-    targetLabel->setText ( item->name );
+	directoryLabel->setText ( spitem->path );
+	if ( item->name.isEmpty() )
+		targetLabel->setText ( i18n ( "%1 in %2" ).arg ( item->primary ).arg ( item->prefix ) );
+	else
+		targetLabel->setText ( item->name );
 
-    setIcon ( SmallIcon ( "filenew.png" ) );
+	setIcon ( SmallIcon ( "filenew.png" ) );
 
-    m_part = part;
-    m_widget = widget;
-    subProject = spitem;
-    target = item;
+	m_part = part;
+	m_widget = widget;
+	subProject = spitem;
+	target = item;
 }
 
 
@@ -60,50 +63,50 @@ AddFileDialog::~AddFileDialog()
 
 void AddFileDialog::accept()
 {
-    QString name = fileEdit->text();
-    if (name.find('/') != -1) {
-        KMessageBox::sorry(this, i18n("Please enter the file name without '/' and so on."));
-        return;
-    }
+	QString name = fileEdit->text();
+	if (name.find('/') != -1) {
+		KMessageBox::sorry(this, i18n("Please enter the file name without '/' and so on."));
+		return;
+	}
 
-    QListViewItem *child = target->firstChild();
-    while (child) {
-        FileItem *item = static_cast<FileItem*>(child);
-        if (name == item->name) {
-            KMessageBox::sorry(this, i18n("This file is already in the target."));
-            return;
-        }
-        child = child->nextSibling();
-    }
-        
-    if (templateCheckBox->isChecked()) {
-        QString srcdir = m_widget->projectDirectory();
-        QString destdir = subProject->subdir;
-        QString destpath = destdir + "/" + name;
-        if (QFileInfo(destpath).exists()) {
-            KMessageBox::sorry(this, i18n("<b>A file with this name already exists!</b><br><br>Please use the \"Add existing file\" dialog!"));
-            return;
-        }
-        FileTemplate::copy(m_part, "cpp", destpath);
-    }
+	QListViewItem *child = target->firstChild();
+	while (child) {
+		FileItem *item = static_cast<FileItem*>(child);
+		if (name == item->name) {
+			KMessageBox::sorry(this, i18n("This file is already in the target."));
+			return;
+		}
+		child = child->nextSibling();
+	}
 
-    FileItem *fitem = m_widget->createFileItem(name);
-    target->sources.append(fitem);
-    target->insertItem(fitem);
-    
-    QString canontargetname = AutoProjectTool::canonicalize(target->name);
-    QString varname = canontargetname + "_SOURCES";
-    subProject->variables[varname] += (" " + name);
-    
-    QMap<QString,QString> replaceMap;
-    replaceMap.insert(varname, subProject->variables[varname]);
-    
-    AutoProjectTool::modifyMakefileam(subProject->path + "/Makefile.am", replaceMap);
+	if (templateCheckBox->isChecked()) {
+		QString srcdir = m_widget->projectDirectory();
+		QString destdir = subProject->subdir;
+		QString destpath = destdir + "/" + name;
+		if (QFileInfo(destpath).exists()) {
+			KMessageBox::sorry(this, i18n("<b>A file with this name already exists!</b><br><br>Please use the \"Add existing file\" dialog!"));
+			return;
+		}
+		FileTemplate::copy(m_part, "cpp", destpath);
+	}
+
+	FileItem *fitem = m_widget->createFileItem(name);
+	target->sources.append(fitem);
+	target->insertItem(fitem);
+
+	QString canontargetname = AutoProjectTool::canonicalize(target->name);
+	QString varname = canontargetname + "_SOURCES";
+	subProject->variables[varname] += (" " + name);
+
+	QMap<QString,QString> replaceMap;
+	replaceMap.insert(varname, subProject->variables[varname]);
+
+	AutoProjectTool::modifyMakefileam(subProject->path + "/Makefile.am", replaceMap);
 
 	m_widget->emitAddedFile( subProject->path.mid ( m_part->project()->projectDirectory().length() + 1 ) + "/" + name );
 	m_part->partController()->editDocument ( KURL ( subProject->path + "/" + name ) );
-    
-    QDialog::accept();
+
+	QDialog::accept();
 }
 
 #include "addfiledlg.moc"
