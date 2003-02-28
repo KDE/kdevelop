de9268d72c8b69caf9f1c8b446c083091a9f6270
Roberto Raggi <roberto@kdevelop.org>
2003-02-28 23:28:34 +0000

    removed the headers

--- parts/appwizard/qmakeapp/qmakeapp.h
+++ parts/appwizard/qmakeapp/qmakeapp.h
@@ -1,13 +1,3 @@
-/****************************************************************************
-** $Id$
-**
-** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
-**
-** This file is part of an example program for Qt.  This example
-** program may be used, distributed and modified without limitation.
-**
-*****************************************************************************/
-
 #ifndef $APPNAMEUC$_H
 #define $APPNAMEUC$_H
 
