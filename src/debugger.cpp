37ba037f97cf74e3335f1bd9eb14aea214998d1e
John Birch <jbb@kdevelop.org>
2003-03-21 07:37:51 +0000

    Finally, after many years, the "sliding" breakpoint problem has been solved thanks to the initial efforts of Andras Mantia (Mantia Andras <amantia@freemail.hu>) See bug 48732.

--- src/debugger.cpp
+++ src/debugger.cpp
@@ -1,3 +1,6 @@
+
+#include "debugger.h"
+
 #include <kdebug.h>
 #include <klocale.h>
 #include <ktexteditor/document.h>
@@ -5,192 +8,188 @@
 #include "editorproxy.h"
 #include "partcontroller.h"
 
-#include "debugger.h"
 
 using namespace KTextEditor;
-
 
 Debugger *Debugger::s_instance = 0;
 
 Debugger::Debugger()
 {
-  connect( PartController::getInstance(), SIGNAL(partAdded(KParts::Part*)),
-           this, SLOT(partAdded(KParts::Part*)) );
+    connect( PartController::getInstance(), SIGNAL(partAdded(KParts::Part*)),
+             this, SLOT(partAdded(KParts::Part*)) );
 }
 
 
 Debugger::~Debugger()
-{
-}
+{}
 
 
 Debugger *Debugger::getInstance()
 {
-  if (!s_instance)
-    s_instance = new Debugger;
+    if (!s_instance)
+        s_instance = new Debugger;
 
-  return s_instance;
+    return s_instance;
 }
 
 
 void Debugger::setBreakpoint(const QString &fileName, int lineNum, int id, bool enabled, bool pending)
 {
-  KParts::Part *part = PartController::getInstance()->partForURL(KURL(fileName));
-  if( !part )
-    return;
-  MarkInterface *iface = dynamic_cast<MarkInterface*>(part);
-  if (!iface)
-    return;
-  // Temporarily disconnect so we don't get confused by receiving extra markChanged signals
-  // This wouldn't be a problem if the debugging interfaces had explicit add/remove methods
-  // rather than just toggle
-//TODO: Remove if there is no problem with breakpoint marks
-/*
-  disconnect( part, SIGNAL(markChanged(KTextEditor::Mark, KTextEditor::MarkInterfaceExtension::MarkChangeAction)),
-              this, SLOT(markChanged(KTextEditor::Mark, KTextEditor::MarkInterfaceExtension::MarkChangeAction)) );
-*/
-  disconnect( part, SIGNAL(marksChanged()),
-              this, SLOT(marksChanged()) );
-  iface->removeMark( lineNum, Breakpoint | ActiveBreakpoint | ReachedBreakpoint | DisabledBreakpoint );
-  if (BPList.contains(lineNum))
-  {
-    QValueList<int>::Iterator it;
-    it = BPList.find(lineNum);
-    BPList.remove(it);
-  }
+    KParts::Part *part = PartController::getInstance()->partForURL(KURL(fileName));
+    if( !part )
+        return;
 
-  if( id != -1 ) {
-    uint markType = Breakpoint;
-    if( !pending )
-      markType |= ActiveBreakpoint;
-    if( !enabled )
-      markType |= DisabledBreakpoint;
-    iface->addMark( lineNum, markType );
-    BPList.append(lineNum);
-  }
-//TODO: Remove if there is no problem with breakpoint marks
-/*
-  connect( part, SIGNAL(markChanged(KTextEditor::Mark, KTextEditor::MarkInterfaceExtension::MarkChangeAction)),
-           this, SLOT(markChanged(KTextEditor::Mark, KTextEditor::MarkInterfaceExtension::MarkChangeAction)) );
-*/
-  connect( part, SIGNAL(marksChanged()),
-           this, SLOT(marksChanged()) );
+    MarkInterface *iface = dynamic_cast<MarkInterface*>(part);
+    if (!iface)
+        return;
+
+    // Temporarily disconnect so we don't get confused by receiving extra markChanged signals
+    // This wouldn't be a problem if the debugging interfaces had explicit add/remove methods
+    // rather than just toggle
+    disconnect( part, SIGNAL(marksChanged()), this, SLOT(marksChanged()) );
+    iface->removeMark( lineNum, Breakpoint | ActiveBreakpoint | ReachedBreakpoint | DisabledBreakpoint );
+
+    BPItem bpItem(fileName, lineNum);
+    QValueList<BPItem>::Iterator it = BPList.find(bpItem);
+    if (it != BPList.end())
+    {
+        kdDebug() << "Removing BP=" << fileName << ":" << lineNum << endl;
+        BPList.remove(it);
+    }
+
+    // An id of -1 means this breakpoint should be hidden from the user.
+    // I believe this functionality is not used presently.
+    if( id != -1 )
+    {
+        uint markType = Breakpoint;
+        if( !pending )
+            markType |= ActiveBreakpoint;
+        if( !enabled )
+            markType |= DisabledBreakpoint;
+        iface->addMark( lineNum, markType );
+        kdDebug() << "Appending BP=" << fileName << ":" << lineNum << endl;
+        BPList.append(BPItem(fileName, lineNum));
+    }
+
+    connect( part, SIGNAL(marksChanged()), this, SLOT(marksChanged()) );
 }
 
 
 void Debugger::clearExecutionPoint()
 {
-  QPtrListIterator<KParts::Part> it(*PartController::getInstance()->parts());
-  for ( ; it.current(); ++it)
-  {
-    MarkInterface *iface = dynamic_cast<MarkInterface*>(it.current());
-    if (!iface)
-      continue;
+    QPtrListIterator<KParts::Part> it(*PartController::getInstance()->parts());
+    for ( ; it.current(); ++it)
+    {
+        MarkInterface *iface = dynamic_cast<MarkInterface*>(it.current());
+        if (!iface)
+            continue;
 
-    QPtrList<Mark> list = iface->marks();
-    QPtrListIterator<Mark> markIt(list);
-    for( ; markIt.current(); ++markIt ) {
-      Mark* mark = markIt.current();
-      if( mark->type & ExecutionPoint )
-        iface->removeMark( mark->line, ExecutionPoint );
+        QPtrList<Mark> list = iface->marks();
+        QPtrListIterator<Mark> markIt(list);
+        for( ; markIt.current(); ++markIt )
+        {
+            Mark* mark = markIt.current();
+            if( mark->type & ExecutionPoint )
+                iface->removeMark( mark->line, ExecutionPoint );
+        }
     }
-  }
 }
 
 
 void Debugger::gotoExecutionPoint(const KURL &url, int lineNum)
 {
-  clearExecutionPoint();
+    clearExecutionPoint();
 
-  PartController::getInstance()->editDocument(url, lineNum);
+    PartController::getInstance()->editDocument(url, lineNum);
 
-  KParts::Part *part = PartController::getInstance()->partForURL(url);
-  if( !part )
-    return;
-  MarkInterface *iface = dynamic_cast<MarkInterface*>(part);
-  if( !iface )
-    return;
+    KParts::Part *part = PartController::getInstance()->partForURL(url);
+    if( !part )
+        return;
+    MarkInterface *iface = dynamic_cast<MarkInterface*>(part);
+    if( !iface )
+        return;
 
-  iface->addMark( lineNum, ExecutionPoint );
+    iface->addMark( lineNum, ExecutionPoint );
 }
 
-
-//TODO: This method should not be needed anymore, as the marksChanged() does the job. (Andras Mantia)
-/*
-void Debugger::markChanged( Mark mark, MarkInterfaceExtension::MarkChangeAction action )
-{
-  if( !sender()->inherits("KTextEditor::Document") )
-    return;
-  KTextEditor::Document* doc = (KTextEditor::Document*) sender();
-  MarkInterfaceExtension* iface = KTextEditor::markInterfaceExtension( doc );
-  if( !iface )
-    return;
-  if( mark.type & Breakpoint ) {
-    if( !PartController::getInstance()->partForURL( doc->url() ) )
-      return; // Probably means the document is being closed.
-    switch( action ) {
-    // Would be better to call distinct methods here rather than toggle...
-    case MarkInterfaceExtension::MarkAdded:
-    case MarkInterfaceExtension::MarkRemoved:
-      emit toggledBreakpoint( doc->url().path(), mark.line );
-      break;
-    }
-  }
-}
-*/
 
 void Debugger::marksChanged()
 {
- if(sender()->inherits("KTextEditor::Document") )
- {
-    KTextEditor::Document* doc = (KTextEditor::Document*) sender();
-    MarkInterface* iface = KTextEditor::markInterface( doc );
-    if (iface)
+    if(sender()->inherits("KTextEditor::Document") )
     {
-      if( !PartController::getInstance()->partForURL( doc->url() ) )
-         return; // Probably means the document is being closed.
-      KTextEditor::Mark *m;
-      KTextEditor::Mark mark;
-      QValueList<int> oldBPList = BPList;
-      for (uint i = 0; i < oldBPList.count(); i++)
-      {
-        emit toggledBreakpoint( doc->url().path(), oldBPList[i] );
-      }
-      BPList.clear();
-      QPtrList<KTextEditor::Mark> newMarks = iface->marks();
-      for (uint i = 0; i < newMarks.count(); i++)
-      {
-        m = newMarks.at(i);
-        if (m->type & Breakpoint)
+        KTextEditor::Document* doc = (KTextEditor::Document*) sender();
+        MarkInterface* iface = KTextEditor::markInterface( doc );
+        if (iface)
         {
-          emit toggledBreakpoint( doc->url().path(), m->line );
+            if( !PartController::getInstance()->partForURL( doc->url() ) )
+                return; // Probably means the document is being closed.
+
+            KTextEditor::Mark *m;
+            QValueList<BPItem> oldBPList = BPList;
+            QPtrList<KTextEditor::Mark> newMarks = iface->marks();
+
+            // Compare the oldBPlist to the new list from the editor.
+            //
+            // If we don't have some of the old breakpoints in the new list
+            // then they have been moved by the user adding or removing source
+            // code. Remove these old breakpoints
+            //
+            // If we _can_ find these old breakpoints in the newlist then
+            // nothing has happened to them. We can just ignore these and to
+            // do that we must remove them from the new list.
+
+            for (uint i = 0; i < oldBPList.count(); i++)
+            {
+                if (oldBPList[i].fileName() != doc->url().path())
+                    continue;
+
+                bool found=false;
+                for (uint newIdx=0; newIdx < newMarks.count(); newIdx++)
+                {
+                    m = newMarks.at(newIdx);
+                    if ((m->type & Breakpoint) &&
+                            m->line == oldBPList[i].lineNum() &&
+                            doc->url().path() == oldBPList[i].fileName())
+                    {
+                        newMarks.remove(newIdx);
+                        found=true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                    emit toggledBreakpoint( doc->url().path(), oldBPList[i].lineNum() );
+            }
+
+            // Any breakpoints left in the new list are the _new_ position of
+            // the moved breakpoints. So add these as new breakpoints via
+            // toggling them.
+            for (uint i = 0; i < newMarks.count(); i++)
+            {
+                m = newMarks.at(i);
+                if (m->type & Breakpoint)
+                    emit toggledBreakpoint( doc->url().path(), m->line );
+            }
         }
-       }
-   }
- }
+    }
 }
 
 
 void Debugger::partAdded( KParts::Part* part )
 {
-  MarkInterfaceExtension *iface = dynamic_cast<MarkInterfaceExtension*>(part);
-  if( !iface )
-    return;
+    MarkInterfaceExtension *iface = dynamic_cast<MarkInterfaceExtension*>(part);
+    if( !iface )
+        return;
 
-  iface->setDescription((MarkInterface::MarkTypes)Breakpoint, i18n("Breakpoint"));
-  iface->setPixmap((MarkInterface::MarkTypes)Breakpoint, *inactiveBreakpointPixmap());
-  iface->setPixmap((MarkInterface::MarkTypes)ActiveBreakpoint, *activeBreakpointPixmap());
-  iface->setPixmap((MarkInterface::MarkTypes)ReachedBreakpoint, *reachedBreakpointPixmap());
-  iface->setPixmap((MarkInterface::MarkTypes)DisabledBreakpoint, *disabledBreakpointPixmap());
-  iface->setPixmap((MarkInterface::MarkTypes)ExecutionPoint, *executionPointPixmap());
-  iface->setMarksUserChangable( Bookmark | Breakpoint );
-//TODO: Remove if there is no problem with breakpoint marks
-/*
-  connect( part, SIGNAL(markChanged(KTextEditor::Mark, KTextEditor::MarkInterfaceExtension::MarkChangeAction)),
-           this, SLOT(markChanged(KTextEditor::Mark, KTextEditor::MarkInterfaceExtension::MarkChangeAction)) );
-*/
-  connect( part, SIGNAL(marksChanged()),
-           this, SLOT(marksChanged()) );
+    iface->setDescription((MarkInterface::MarkTypes)Breakpoint, i18n("Breakpoint"));
+    iface->setPixmap((MarkInterface::MarkTypes)Breakpoint, *inactiveBreakpointPixmap());
+    iface->setPixmap((MarkInterface::MarkTypes)ActiveBreakpoint, *activeBreakpointPixmap());
+    iface->setPixmap((MarkInterface::MarkTypes)ReachedBreakpoint, *reachedBreakpointPixmap());
+    iface->setPixmap((MarkInterface::MarkTypes)DisabledBreakpoint, *disabledBreakpointPixmap());
+    iface->setPixmap((MarkInterface::MarkTypes)ExecutionPoint, *executionPointPixmap());
+    iface->setMarksUserChangable( Bookmark | Breakpoint );
+
+    connect( part, SIGNAL(marksChanged()), this, SLOT(marksChanged()) );
 }
 
 #include "debugger.moc"
