a08d556584fdb7b7639f6950b6557372cf087b63
Alexander Dymo <adymo@kdevelop.org>
2003-03-29 21:56:33 +0000

    CppSupport's own factory introduced.

---
+++ parts/cppsupport/cppsupportfactory.cpp
@@ -0,0 +1,30 @@
+/***************************************************************************
+ *   Copyright (C) 2000-2001 by Bernd Gehrmann                             *
+ *   bernd@kdevelop.org                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <kinstance.h>
+#include <kstandarddirs.h>
+#include "cppsupportfactory.h"
+
+K_EXPORT_COMPONENT_FACTORY( libkdevcppsupport, CppSupportFactory );
+
+CppSupportFactory::CppSupportFactory()
+    : KGenericFactory<CppSupportPart>( "kdevcppsupport" )
+{
+}
+
+KInstance *CppSupportFactory::createInstance()
+{
+    KInstance *instance = KGenericFactory<CppSupportPart>::createInstance();
+    KStandardDirs *dirs = instance->dirs();
+    dirs->addResourceType("newclasstemplates", KStandardDirs::kde_default("data") + "kdevcppsupport/newclass/");
+    return instance;
+}
+
---
+++ parts/cppsupport/cppsupportfactory.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+ *   Copyright (C) 2000-2001 by Bernd Gehrmann                             *
+ *   bernd@kdevelop.org                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _CPPSUPPORTFACTORY_H_
+#define _CPPSUPPORTFACTORY_H_
+
+#include <kgenericfactory.h>
+#include "cppsupportpart.h"
+
+class CppSupportFactory : public KGenericFactory<CppSupportPart>
+{
+public:
+    CppSupportFactory();
+
+protected:
+    virtual KInstance *createInstance();
+};
+
+#endif
