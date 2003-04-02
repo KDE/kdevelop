d883b7e4e1e2d79296d8a51ae3ed3e3ec5d9b991
Caleb Tennis <caleb@gentoo.org>
2003-04-02 20:30:56 +0000

    this example is more c++ like (from kde-devel mailing list suggestion)

--- parts/appwizard/cpphello/main.cpp
+++ parts/appwizard/cpphello/main.cpp
@@ -2,12 +2,12 @@
 #include <config.h>
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-  printf("Hello, world!\n");
+  cout << "Hello, world!" << endl;
 
   return EXIT_SUCCESS;
 }
