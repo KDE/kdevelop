3e74cad37417b700eeb01e70069eef79afb6754f
Amilcar do Carmo Lucas <amilcar@kdevelop.org>
2003-04-12 17:57:53 +0000

    Add Jens Dagerbo <jens.dagerbo (at) swipnet.se> aka teatime  to the credits.

--- src/main.cpp
+++ src/main.cpp
@@ -59,6 +59,7 @@ int main(int argc, char *argv[])
   aboutData.addCredit("Julian Rockey", I18N_NOOP("Filecreate part and other bits and patches"), "linux@jrockey.com");
   aboutData.addCredit("Amilcar do Carmo Lucas", I18N_NOOP("Patches"), "amilcar@ida.ing.tu-bs.de");
   aboutData.addCredit("Ajay Guleria", I18N_NOOP("ClearCase support"), "ajay_guleria@yahoo.com");
+  aboutData.addCredit("Jens Dagerbo", I18N_NOOP("Replace part, patches"), "jens.dagerbo@swipnet.se");
 
   KCmdLineArgs::init(argc, argv, &aboutData);
   KCmdLineArgs::addCmdLineOptions( options );
