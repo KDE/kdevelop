38790e4c034d94afbcb255c75a4b969c1ba8609b
Roberto Raggi <roberto@kdevelop.org>
2003-04-15 14:43:27 +0000

    added TagCreator

---
+++ parts/cppsupport/tag_creator.cpp
@@ -0,0 +1,665 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Roberto Raggi                                   *
+ *   roberto@kdevelop.org                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "tag_creator.h"
+#include "catalog.h"
+#include "ast_utils.h"
+
+#include <kdebug.h>
+#include <qfileinfo.h>
+
+TagCreator::TagCreator( const QString& fileName, Catalog* c )
+    : m_catalog( c ), m_fileName( fileName ), m_anon( 0 )
+{
+}
+
+TagCreator::~TagCreator()
+{
+}
+
+void TagCreator::parseTranslationUnit( TranslationUnitAST* ast )
+{
+    m_currentScope.clear();
+    m_currentAccess = QString::null;
+    m_inSlots = false;
+    m_inSignals = false;
+    m_anon = 0;
+    m_imports.clear();
+    m_inClass = false;
+
+    m_imports << QStringList();
+    TreeParser::parseTranslationUnit( ast );
+    m_imports.pop_back();
+}
+
+void TagCreator::parseDeclaration( DeclarationAST* ast )
+{
+    TreeParser::parseDeclaration( ast );
+}
+
+void TagCreator::parseLinkageSpecification( LinkageSpecificationAST* ast )
+{
+    TreeParser::parseLinkageSpecification( ast );
+}
+
+void TagCreator::parseNamespace( NamespaceAST* ast )
+{
+    QString nsName;
+    if( !ast->namespaceName() ){
+        // anonymous namespace
+    } else {
+	nsName = ast->namespaceName()->text();
+    }
+
+    Tag tag;
+    tag.setKind( Tag::Kind_Namespace );
+    tag.setFileName( m_fileName );
+    tag.setName( nsName );
+    tag.setScope( m_currentScope );
+
+    int line, col;
+    ast->getStartPosition( &line, &col );
+    tag.setStartPosition( line, col );
+
+    ast->getEndPosition( &line, &col );
+    tag.setEndPosition( line, col );
+
+    m_catalog->addItem( tag );
+
+    m_currentScope.push_back( nsName );
+    TreeParser::parseNamespace( ast );
+    m_currentScope.pop_back();
+}
+
+void TagCreator::parseNamespaceAlias( NamespaceAliasAST* ast )
+{
+    TreeParser::parseNamespaceAlias( ast );
+}
+
+void TagCreator::parseUsing( UsingAST* ast )
+{
+    TreeParser::parseUsing( ast );
+}
+
+void TagCreator::parseUsingDirective( UsingDirectiveAST* ast )
+{
+    QString name;
+    if( ast->name() )
+        name = ast->name()->text();
+
+    if( !name.isNull() ){
+        Tag tag;
+        tag.setKind( Tag::Kind_UsingDirective );
+        tag.setFileName( m_fileName );
+        tag.setName( name );
+        tag.setScope( m_currentScope );
+
+        int line, col;
+        ast->getStartPosition( &line, &col );
+        tag.setStartPosition( line, col );
+
+        ast->getEndPosition( &line, &col );
+        tag.setEndPosition( line, col );
+
+        m_catalog->addItem( tag );
+    }
+
+    m_imports.back().push_back( name );
+    TreeParser::parseUsingDirective( ast );
+}
+
+void TagCreator::parseTypedef( TypedefAST* ast )
+{
+    TypeSpecifierAST* typeSpec = ast->typeSpec();
+    InitDeclaratorListAST* declarators = ast->initDeclaratorList();
+
+    if( typeSpec && declarators ){
+        QString typeId;
+
+        if( typeSpec->name() )
+            typeId = typeSpec->name()->text();
+
+        QPtrList<InitDeclaratorAST> l( declarators->initDeclaratorList() );
+        QPtrListIterator<InitDeclaratorAST> it( l );
+
+	InitDeclaratorAST* initDecl = 0;
+	while( 0 != (initDecl = it.current()) ){
+
+	    QString type, id;
+	    if( initDecl->declarator() ){
+	       type = typeOfDeclaration( typeSpec, initDecl->declarator() );
+
+	       DeclaratorAST* d = initDecl->declarator();
+	       while( d->subDeclarator() ){
+	           d = d->subDeclarator();
+	       }
+
+	       if( d->declaratorId() )
+	          id = d->declaratorId()->text();
+	    }
+
+	    Tag tag;
+	    tag.setKind( Tag::Kind_Typedef );
+	    tag.setFileName( m_fileName );
+	    tag.setName( id );
+	    tag.setScope( m_currentScope );
+
+	    int line, col;
+	    initDecl->getStartPosition( &line, &col );
+	    tag.setStartPosition( line, col );
+
+	    initDecl->getEndPosition( &line, &col );
+	    tag.setEndPosition( line, col );
+
+	    m_catalog->addItem( tag );
+
+	    ++it;
+	}
+
+    }
+
+    TreeParser::parseTypedef( ast );
+}
+
+void TagCreator::parseTemplateDeclaration( TemplateDeclarationAST* ast )
+{
+    if( ast->declaration() )
+	parseDeclaration( ast->declaration() );
+
+    TreeParser::parseTemplateDeclaration( ast );
+}
+
+void TagCreator::parseSimpleDeclaration( SimpleDeclarationAST* ast )
+{
+    TypeSpecifierAST* typeSpec = ast->typeSpec();
+    InitDeclaratorListAST* declarators = ast->initDeclaratorList();
+
+    if( typeSpec )
+	parseTypeSpecifier( typeSpec );
+
+    if( declarators ){
+	QPtrList<InitDeclaratorAST> l = declarators->initDeclaratorList();
+
+	QPtrListIterator<InitDeclaratorAST> it( l );
+	while( it.current() ){
+	    parseDeclaration(  ast->functionSpecifier(), ast->storageSpecifier(), typeSpec, it.current() );
+	    ++it;
+	}
+    }
+
+    TreeParser::parseSimpleDeclaration( ast );
+}
+
+void TagCreator::parseFunctionDefinition( FunctionDefinitionAST* ast )
+{
+    TypeSpecifierAST* typeSpec = ast->typeSpec();
+    GroupAST* funSpec = ast->functionSpecifier();
+    GroupAST* storageSpec = ast->storageSpecifier();
+
+    if( !ast->initDeclarator() )
+	return;
+
+    DeclaratorAST* d = ast->initDeclarator()->declarator();
+
+    if( !d->declaratorId() )
+	return;
+
+    bool isFriend = false;
+    bool isVirtual = false;
+    bool isStatic = false;
+    bool isInline = false;
+
+    if( funSpec ){
+	QPtrList<AST> l = funSpec->nodeList();
+	QPtrListIterator<AST> it( l );
+	while( it.current() ){
+	    QString text = it.current()->text();
+	    if( text == "virtual" ) isVirtual = true;
+	    else if( text == "inline" ) isInline = true;
+	    ++it;
+	}
+    }
+
+    if( storageSpec ){
+	QPtrList<AST> l = storageSpec->nodeList();
+	QPtrListIterator<AST> it( l );
+	while( it.current() ){
+	    QString text = it.current()->text();
+	    if( text == "friend" ) isFriend = true;
+	    else if( text == "static" ) isStatic = true;
+	    ++it;
+	}
+    }
+
+
+    QString id = d->declaratorId()->unqualifiedName()->text().stripWhiteSpace();
+    QString scopeStr = scopeOfDeclarator( d );
+
+    Tag tag;
+    tag.setKind( Tag::Kind_Function );
+
+    tag.setFileName( m_fileName );
+    tag.setName( id );
+    tag.setScope( QStringList::split(".", scopeStr) );
+
+    int line, col;
+    ast->getStartPosition( &line, &col );
+    tag.setStartPosition( line, col );
+
+    ast->getEndPosition( &line, &col );
+    tag.setEndPosition( line, col );
+
+    QString type = typeOfDeclaration( typeSpec, d );
+    tag.setAttribute( "type", type );
+
+    parseFunctionArguments( tag, d );
+
+    if( !m_currentAccess.isEmpty() ){
+        tag.setAttribute( "access", m_currentAccess );
+    }
+
+    tag.setAttribute( "isFriend", isFriend );
+    tag.setAttribute( "isVirtual", isVirtual );
+    tag.setAttribute( "isStatic", isStatic );
+    tag.setAttribute( "isInline", isInline );
+    tag.setAttribute( "isPure", false );
+
+    m_catalog->addItem( tag );
+
+    if( !m_currentAccess.isEmpty() ){
+        tag.setKind( Tag::Kind_FunctionDeclaration );
+        m_catalog->addItem( tag );
+    }
+
+    TreeParser::parseFunctionDefinition( ast );
+}
+
+void TagCreator::parseLinkageBody( LinkageBodyAST* ast )
+{
+    TreeParser::parseLinkageBody( ast );
+}
+
+void TagCreator::parseTypeSpecifier( TypeSpecifierAST* ast )
+{
+    TreeParser::parseTypeSpecifier( ast );
+}
+
+void TagCreator::parseClassSpecifier( ClassSpecifierAST* ast )
+{
+    int startLine, startColumn;
+    int endLine, endColumn;
+    ast->getStartPosition( &startLine, &startColumn );
+    ast->getEndPosition( &endLine, &endColumn );
+
+    QString oldAccess = m_currentAccess;
+    bool oldInSlots = m_inSlots;
+    bool oldInSignals = m_inSignals;
+
+    QString kind = ast->classKey()->text();
+    if( kind == "class" )
+	m_currentAccess = "private";
+    else
+	m_currentAccess = "public";
+    m_inSlots = false;
+    m_inSignals = false;
+
+    QString className;
+    if( !ast->name() ){
+	//QFileInfo fileInfo( m_fileName );
+	//QString shortFileName = fileInfo.baseName();
+	//className.sprintf( "(%s_%d)", shortFileName.latin1(), m_anon++ );
+    } else {
+	className = ast->name()->text();
+    }
+
+    Tag tag;
+    if( kind == "class" )
+        tag.setKind( Tag::Kind_Class );
+    else if( kind == "struct" )
+        tag.setKind( Tag::Kind_Struct );
+    else if( kind == "union" )
+        tag.setKind( Tag::Kind_Union );
+
+    tag.setFileName( m_fileName );
+    tag.setName( className );
+    tag.setScope( m_currentScope );
+
+    int line, col;
+    ast->getStartPosition( &line, &col );
+    tag.setStartPosition( line, col );
+
+    ast->getEndPosition( &line, &col );
+    tag.setEndPosition( line, col );
+
+    QString id = m_catalog->addItem( tag );
+    Q_UNUSED( id ); // TODO
+
+    if ( ast->baseClause() )
+        parseBaseClause( className, ast->baseClause() );
+
+    m_currentScope.push_back( className );
+    int oldInClass = m_inClass;
+    m_inClass = true;
+    TreeParser::parseClassSpecifier( ast );
+    m_currentScope.pop_back();
+    m_inClass = oldInClass;
+
+    m_currentAccess = oldAccess;
+    m_inSlots = oldInSlots;
+    m_inSignals = oldInSignals;
+}
+
+void TagCreator::parseEnumSpecifier( EnumSpecifierAST* ast )
+{
+    Tag tag;
+    tag.setKind( Tag::Kind_Enum );
+
+    tag.setFileName( m_fileName );
+    if( ast->name() )
+        tag.setName( ast->name()->text() );
+    tag.setScope( m_currentScope );
+
+    int line, col;
+    ast->getStartPosition( &line, &col );
+    tag.setStartPosition( line, col );
+
+    ast->getEndPosition( &line, &col );
+    tag.setEndPosition( line, col );
+
+    QString id = m_catalog->addItem( tag );
+    Q_UNUSED( id );
+
+    QPtrList<EnumeratorAST> l = ast->enumeratorList();
+    QPtrListIterator<EnumeratorAST> it( l );
+    while( it.current() ){
+	QString name = it.current()->id()->text();
+
+	Tag tag;
+	tag.setKind( Tag::Kind_Enumerator );
+
+	tag.setFileName( m_fileName );
+	tag.setName( name );
+	tag.setScope( m_currentScope );
+
+	int line, col;
+	it.current()->getStartPosition( &line, &col );
+	tag.setStartPosition( line, col );
+
+	it.current()->getEndPosition( &line, &col );
+	tag.setEndPosition( line, col );
+
+	m_catalog->addItem( tag );
+
+	++it;
+    }
+
+    TreeParser::parseEnumSpecifier( ast );
+}
+
+void TagCreator::parseElaboratedTypeSpecifier( ElaboratedTypeSpecifierAST* ast )
+{
+    TreeParser::parseElaboratedTypeSpecifier( ast );
+}
+
+void TagCreator::parseTypeDeclaratation( TypeSpecifierAST* typeSpec )
+{
+    parseTypeSpecifier( typeSpec );
+}
+
+void TagCreator::parseDeclaration( GroupAST* funSpec, GroupAST* storageSpec, TypeSpecifierAST* typeSpec, InitDeclaratorAST* decl )
+{
+    DeclaratorAST* d = decl->declarator();
+
+    if( !d )
+	return;
+
+    if( !d->subDeclarator() && d->parameterDeclarationClause() )
+	return parseFunctionDeclaration( funSpec, storageSpec, typeSpec, decl );
+
+    DeclaratorAST* t = d;
+    while( t && t->subDeclarator() )
+	t = t->subDeclarator();
+
+    QString id;
+    if( t && t->declaratorId() && t->declaratorId()->unqualifiedName() )
+	id = t->declaratorId()->unqualifiedName()->text();
+
+    QString scopeStr = scopeOfDeclarator( d );
+
+    QString type = typeOfDeclaration( typeSpec, d );
+
+
+    bool isFriend = false;
+    //bool isVirtual = false;
+    bool isStatic = false;
+    //bool isInline = false;
+    //bool isInitialized = decl->initializer() != 0;
+
+    if( storageSpec ){
+	QPtrList<AST> l = storageSpec->nodeList();
+	QPtrListIterator<AST> it( l );
+	while( it.current() ){
+	    QString text = it.current()->text();
+	    if( text == "friend" ) isFriend = true;
+	    else if( text == "static" ) isStatic = true;
+	    ++it;
+	}
+    }
+
+    Tag tag;
+    tag.setKind( Tag::Kind_Variable );
+    tag.setFileName( m_fileName );
+    tag.setName( id );
+    tag.setScope( QStringList::split(".",scopeStr) );
+
+    int line, col;
+    decl->getStartPosition( &line, &col );
+    tag.setStartPosition( line, col );
+
+    decl->getEndPosition( &line, &col );
+    tag.setEndPosition( line, col );
+
+    tag.setAttribute( "type", type );
+    tag.setAttribute( "isFriend", isFriend );
+    tag.setAttribute( "isStatic", isStatic );
+
+    QString access = m_currentAccess;
+    if( access.isNull() )
+        access = "public";
+    tag.setAttribute( "access", access );
+
+    m_catalog->addItem( tag );
+}
+
+void TagCreator::parseAccessDeclaration( AccessDeclarationAST * access )
+{
+    QPtrList<AST> l = access->accessList();
+
+    m_currentAccess = l.at( 0 )->text();
+    if( m_currentAccess == "signals" )
+	m_currentAccess = "public"; //!?
+    
+    m_inSlots = l.count() > 1 ? l.at( 1 )->text() == "slots" : false;
+    m_inSignals = l.count() > 1 ? l.at( 0 )->text() == "signals" : false;
+}
+
+void TagCreator::parseFunctionDeclaration(  GroupAST* funSpec, GroupAST* storageSpec,
+					     TypeSpecifierAST * typeSpec, InitDeclaratorAST * decl )
+{
+    bool isFriend = false;
+    bool isVirtual = false;
+    bool isStatic = false;
+    bool isInline = false;
+    bool isPure = decl->initializer() != 0;
+
+    if( funSpec ){
+	QPtrList<AST> l = funSpec->nodeList();
+	QPtrListIterator<AST> it( l );
+	while( it.current() ){
+	    QString text = it.current()->text();
+	    if( text == "virtual" ) isVirtual = true;
+	    else if( text == "inline" ) isInline = true;
+	    ++it;
+	}
+    }
+
+    if( storageSpec ){
+	QPtrList<AST> l = storageSpec->nodeList();
+	QPtrListIterator<AST> it( l );
+	while( it.current() ){
+	    QString text = it.current()->text();
+	    if( text == "friend" ) isFriend = true;
+	    else if( text == "static" ) isStatic = true;
+	    ++it;
+	}
+    }
+
+    int startLine, startColumn;
+    int endLine, endColumn;
+    decl->getStartPosition( &startLine, &startColumn );
+    decl->getEndPosition( &endLine, &endColumn );
+
+    DeclaratorAST* d = decl->declarator();
+    QString id = d->declaratorId()->unqualifiedName()->text();
+
+    QString type = typeOfDeclaration( typeSpec, d );
+
+    Tag tag;
+    tag.setKind( Tag::Kind_FunctionDeclaration );
+    tag.setFileName( m_fileName );
+    tag.setName( id );
+    tag.setScope( m_currentScope );
+
+    int line, col;
+    decl->getStartPosition( &line, &col );
+    tag.setStartPosition( line, col );
+
+    decl->getEndPosition( &line, &col );
+    tag.setEndPosition( line, col );
+
+    tag.setAttribute( "type", type );
+    tag.setAttribute( "isFriend", isFriend );
+    tag.setAttribute( "isVirtual", isVirtual );
+    tag.setAttribute( "isStatic", isStatic );
+    tag.setAttribute( "isInline", isInline );
+    tag.setAttribute( "isPure", isPure );
+
+    parseFunctionArguments( tag, d );
+
+    m_catalog->addItem( tag );
+}
+
+void TagCreator::parseFunctionArguments( Tag& tag, DeclaratorAST* declarator )
+{
+    ParameterDeclarationClauseAST* clause = declarator->parameterDeclarationClause();
+
+    if( clause && clause->parameterDeclarationList() ){
+        ParameterDeclarationListAST* params = clause->parameterDeclarationList();
+	QPtrList<ParameterDeclarationAST> l( params->parameterList() );
+	QPtrListIterator<ParameterDeclarationAST> it( l );
+
+	QStringList types;
+	QStringList args;
+	while( it.current() ){
+	    ParameterDeclarationAST* param = it.current();
+	    ++it;
+
+	    QString name;
+	    if( param->declarator() ){
+		name = declaratorToString(param->declarator(), QString::null, true );
+	    }
+
+	    QString type = typeOfDeclaration( param->typeSpec(), param->declarator() );
+
+	    types << type;
+	    args << name;
+	}
+
+        tag.setAttribute( "arguments", types );
+        tag.setAttribute( "argumentNames", args );
+    }
+}
+
+QString TagCreator::typeOfDeclaration( TypeSpecifierAST* typeSpec, DeclaratorAST* declarator )
+{
+    if( !typeSpec || !declarator )
+        return QString::null;
+
+    QString text;
+
+    text += typeSpec->text();
+    text = text.simplifyWhiteSpace();
+
+    QPtrList<AST> ptrOpList = declarator->ptrOpList();
+    for( QPtrListIterator<AST> it(ptrOpList); it.current(); ++it ){
+	text += it.current()->text();
+    }
+
+    return text;
+}
+
+void TagCreator::parseBaseClause( const QString& className, BaseClauseAST * baseClause )
+{
+    QPtrList<BaseSpecifierAST> l = baseClause->baseSpecifierList();
+    QPtrListIterator<BaseSpecifierAST> it( l );
+    while( it.current() ){
+	BaseSpecifierAST* baseSpecifier = it.current();
+
+	QString access;
+	if( baseSpecifier->access() )
+	    access = baseSpecifier->access()->text();
+	bool isVirtual = baseSpecifier->isVirtual() != 0;
+
+	QString baseName;
+	if( baseSpecifier->name() )
+	    baseName = baseSpecifier->name()->text();
+
+	Tag tag;
+	tag.setKind( Tag::Kind_Base_class );
+	tag.setFileName( m_fileName );
+	tag.setName( className );
+	tag.setScope( m_currentScope );
+
+	tag.setAttribute( "baseClass", baseName );
+	tag.setAttribute( "isVirtual", isVirtual );
+	tag.setAttribute( "access", access );
+
+	m_catalog->addItem( tag );
+
+	int line, col;
+	baseClause->getStartPosition( &line, &col );
+	tag.setStartPosition( line, col );
+
+	baseClause->getEndPosition( &line, &col );
+	tag.setEndPosition( line, col );
+
+	++it;
+    }
+}
+
+QString TagCreator::scopeOfDeclarator( DeclaratorAST* d )
+{
+    QStringList scope = m_currentScope;
+    if( d && d->declaratorId() && d->declaratorId()->classOrNamespaceNameList().count() ){
+        if( d->declaratorId()->isGlobal() )
+	    scope.clear();
+	QPtrList<ClassOrNamespaceNameAST> l = d->declaratorId()->classOrNamespaceNameList();
+	QPtrListIterator<ClassOrNamespaceNameAST> it( l );
+	while( it.current() ){
+	    if( it.current()->name() ){
+	       scope << it.current()->name()->text();
+	    }
+	    ++it;
+	}
+    }
+
+    return scope.join( "." );
+}
---
+++ parts/cppsupport/tag_creator.h
@@ -0,0 +1,77 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Roberto Raggi                                   *
+ *   roberto@kdevelop.org                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef __tag_creator_h
+#define __tag_creator_h
+
+#include "tree_parser.h"
+
+#include <qstringlist.h>
+
+class Catalog;
+class Tag;
+
+class TagCreator: public TreeParser
+{
+public:
+    TagCreator( const QString& fileName, Catalog* c );
+    virtual ~TagCreator();
+
+    // translation-unit
+    virtual void parseTranslationUnit( TranslationUnitAST* );
+
+    // declarations
+    virtual void parseDeclaration( DeclarationAST* );
+    virtual void parseLinkageSpecification( LinkageSpecificationAST* );
+    virtual void parseNamespace( NamespaceAST* );
+    virtual void parseNamespaceAlias( NamespaceAliasAST* );
+    virtual void parseUsing( UsingAST* );
+    virtual void parseUsingDirective( UsingDirectiveAST* );
+    virtual void parseTypedef( TypedefAST* );
+    virtual void parseTemplateDeclaration( TemplateDeclarationAST* );
+    virtual void parseSimpleDeclaration( SimpleDeclarationAST* );
+    virtual void parseFunctionDefinition( FunctionDefinitionAST* );
+    virtual void parseLinkageBody( LinkageBodyAST* );
+    virtual void parseAccessDeclaration( AccessDeclarationAST* );
+
+    // type-specifier
+    virtual void parseTypeSpecifier( TypeSpecifierAST* );
+    virtual void parseClassSpecifier( ClassSpecifierAST* );
+    virtual void parseEnumSpecifier( EnumSpecifierAST* );
+    virtual void parseElaboratedTypeSpecifier( ElaboratedTypeSpecifierAST* );
+
+    virtual void parseTypeDeclaratation( TypeSpecifierAST* typeSpec );
+    virtual void parseDeclaration( GroupAST* funSpec, GroupAST* storageSpec, TypeSpecifierAST* typeSpec, InitDeclaratorAST* decl );
+    virtual void parseFunctionDeclaration( GroupAST* funSpec, GroupAST* storageSpec, TypeSpecifierAST* typeSpec, InitDeclaratorAST* decl );
+    virtual void parseFunctionArguments( Tag& tag, DeclaratorAST* declarator );
+    virtual void parseBaseClause( const QString& className, BaseClauseAST* baseClause );
+
+private:
+    QString scopeOfDeclarator( DeclaratorAST* d );
+    QString typeOfDeclaration( TypeSpecifierAST* typeSpec, DeclaratorAST* declarator );
+
+private:
+    Catalog* m_catalog;
+    QString m_fileName;
+    QStringList m_currentScope;
+    QValueList<QStringList> m_imports;
+    QString m_currentAccess;
+    bool m_inClass;
+    bool m_inSlots;
+    bool m_inSignals;
+    int m_anon;
+
+private:
+    TagCreator( const TagCreator& source );
+    void operator = ( const TagCreator& source );
+};
+
+#endif // __tag_creator_h
