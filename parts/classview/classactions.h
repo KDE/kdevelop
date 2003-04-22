0bf8b23eec0175fb64bc0a924007d8f7c4dfef72
Harald Fernengel <harry@kdevelop.org>
2003-04-22 12:47:12 +0000

    move the stuff around

--- parts/classview/classactions.h
+++ parts/classview/classactions.h
@@ -17,60 +17,11 @@
 #include <kaction.h>
 #include <kdeversion.h>
 
+//only for KDE < 3.1
+#include <kdevwidgetaction.h>
+
 class KDevPlugin;
 class QComboBox;
-
-#if !(KDE_VERSION > 305)
-#include <qguardedptr.h>
-//--------------------begin-of-copy-of-kde-3.1-----------
-/**
- * An action that automatically embeds a widget into a
- * toolbar.
- */
-class KWidgetAction : public KAction
-{
-    Q_OBJECT
-public:
-    /**
-     * Create an action that will embed widget into a toolbar
-     * when plugged. This action may only be plugged into
-     * a toolbar.
-     */
-    KWidgetAction( QWidget* widget, const QString& text,
-                   const KShortcut& cut,
-                   const QObject* receiver, const char* slot,
-                   KActionCollection* parent, const char* name );
-    virtual ~KWidgetAction();
-
-    /**
-     * Returns the widget associated with this action.
-     */
-    QWidget* widget() { return m_widget; }
-
-    void setAutoSized( bool );
-
-    /**
-     * Plug the action. The widget passed to the constructor
-     * will be reparented to w, which must inherit KToolBar.
-     */
-    virtual int plug( QWidget* w, int index = -1 );
-    /**
-     * Unplug the action. Ensures that the action is not
-     * destroyed. It will be hidden and reparented to 0L instead.
-     */
-    virtual void unplug( QWidget *w );
-private:
-    QGuardedPtr<QWidget> m_widget;
-    bool                 m_autoSized;
-protected:
-    virtual void virtual_hook( int id, void* data );
-private:
-    class KWidgetActionPrivate;
-    KWidgetActionPrivate *d;
-};
-
-//--------------------end-of-copy-of-kde-3.1-----------
-#endif // !(KDE_VERSION > 305)
 
 class ClassListAction : public KWidgetAction
 {
