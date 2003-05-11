f18508ec8397d41d87baad746d8430e1692fb264
Jens Dagerbo <jens.dagerbo@swipnet.se>
2003-05-11 18:32:18 +0000

    patch from Marcel Turino, solves Bug 55287 and 51529.

--- parts/doctreeview/doctreeviewpart.cpp
+++ parts/doctreeview/doctreeviewpart.cpp
@@ -191,7 +191,7 @@ void DocTreeViewPart::slotContextFulltextSearch()
     dlg.setSearchTerm(m_popupstr);
     if (dlg.performSearch()) {
         QString indexdir = kapp->dirs()->saveLocation("data", "kdevdoctreeview/helpindex");
-        partController()->showDocument(KURL("file://" + indexdir + "/results.html"));
+        partController()->showDocument(KURL("file://" + indexdir + "/results.html"), m_popupstr );
     }
 }
 
