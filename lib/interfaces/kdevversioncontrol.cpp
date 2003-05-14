17af7ccbfd86b08d516623b1a88631f25ec3bd77
Mario Scalas <mario.scalas@libero.it>
2003-05-14 14:41:58 +0000

    Merged with older KDevGlobalVersionControl

--- lib/interfaces/kdevversioncontrol.cpp
+++ lib/interfaces/kdevversioncontrol.cpp
@@ -1,13 +1,26 @@
+
+#include <kdebug.h>
+
 #include "kdevversioncontrol.h"
 
+static KDevVersionControl::VersionControlMap m_registeredVcs;
 
-KDevVersionControl::KDevVersionControl(const QString& pluginName, const QString& icon, QObject *parent, const char *name)
-    : KDevPlugin(pluginName, icon, parent, name)
+KDevVersionControl::KDevVersionControl( const QString& pluginName, const QString& icon, QObject *parent, const char *name )
+	: KDevPlugin( pluginName, icon, parent, name )
 {
+	kdDebug( 9000 ) << "  ** Registering Version Control System: " << this->pluginName() << endl;
+    m_registeredVcs.insert( this->pluginName(), this );
 }
 
 KDevVersionControl::~KDevVersionControl()
 {
+	m_registeredVcs.remove( pluginName() );
 }
+
+const KDevVersionControl::VersionControlMap &KDevVersionControl::getRegisteredVCS()
+{
+	return m_registeredVcs;
+}
+
 
 #include "kdevversioncontrol.moc"
--- lib/interfaces/kdevversioncontrol.h
+++ lib/interfaces/kdevversioncontrol.h
@@ -6,7 +6,11 @@
 #ifndef _KDEVVERSIONCONTROL_H_
 #define _KDEVVERSIONCONTROL_H_
 
-#include <qstringlist.h>
+#include <qobject.h>
+#include <qstring.h>
+#include <qwidget.h>
+#include <qmap.h>
+
 #include "kdevplugin.h"
 
 
@@ -15,17 +19,45 @@ class KDevVersionControl : public KDevPlugin
     Q_OBJECT
 
 public:
-    enum State { canBeCommited = 1, canBeAdded = 2 };
+	typedef QMap<QString,KDevVersionControl*> VersionControlMap;
 
-    KDevVersionControl( const QString& pluginName, const QString& icon, QObject *parent, const char *name=0 );
-    ~KDevVersionControl();
+    enum State { invalid = -1, canBeCommited = 1, canBeAdded = 2 };
 
+	/**
+	* Builds the object.
+	* 	@p pluginName	this _is_ *** unique *** identintifier between all available version controls.
+	*   @p icon			symbolic name for the icon
+	*	@p parent		the parent QObject for this version control system
+	*	@p name			a name for this object; it may be null since it is usefull for debugging
+	*/
+	KDevVersionControl( const QString& pluginName, const QString& icon, QObject *parent, const char *name=0 );
+	/**
+	* Destructor. Removes current VCS from the list of available ones.
+	*/
+	~KDevVersionControl();
+
+	/**
+	* Return a pointer to the associated configuration widget for this vcs.
+	*/
+	virtual QWidget* newProjectWidget(QWidget */*parent*/) { return 0; };
+	/**
+	* Creates a new project in the passed path @p dir.
+	*/
+	virtual void createNewProject(const QString&/* dir*/) { return; }
     /**
      * Tells whether the given file is registered
      * in the version control system.
+	 * Note: I see no need for it since KDevVersionControl implementations
+	 * currently handle this internally and there is no request for external entities
+	 * to access this. (marioS, 13-may-2003)
      */
      //    This will be replaced by a smarter solution
      //    virtual State registeredState(const QString &fileName) = 0;
+
+	 /**
+	 * Accessor to the collection of registered VCSs.
+	 */
+	static const VersionControlMap &getRegisteredVCS();
 };
 
 #endif
