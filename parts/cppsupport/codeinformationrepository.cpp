eee1e7622fa501ea3c6dd1b3c0d64eba26f61489
Roberto Raggi <roberto@kdevelop.org>
2003-05-15 17:35:01 +0000

    bugfix

--- parts/cppsupport/codeinformationrepository.cpp
+++ parts/cppsupport/codeinformationrepository.cpp
@@ -59,7 +59,7 @@ QValueList<Tag> CodeInformationRepository::query( const QValueList<Catalog :: Qu
     kdDebug(9020) << "CodeInformationRepository::query()" << endl;
 
     QValueList<Tag> tags;
-	
+
     QMap<QString, Catalog*>::Iterator it = m_catalogs.begin();
     while( it != m_catalogs.end() ){
         kdDebug(9020) << "try with the catalog " << it.key() << endl;
@@ -99,20 +99,20 @@ QValueList<Tag> CodeInformationRepository::getTagsInFile( const QString & fileNa
 QValueList<Tag> CodeInformationRepository::getTagsInScope( const QStringList & scope, bool isInstance )
 {
     kdDebug(9020) << "CodeInformationRepository::getTagsInScope()" << endl;
-    
+
     QValueList<Tag> tags;
     QValueList<Catalog::QueryArgument> args;
-    
+
     args.clear();
     args << Catalog::QueryArgument( "kind", Tag::Kind_Namespace )
 	<< Catalog::QueryArgument( "scope", scope );
     tags += query( args );
-    
+
     args.clear();
     args << Catalog::QueryArgument( "kind", Tag::Kind_Class )
 	<< Catalog::QueryArgument( "scope", scope );
     tags += query( args );
-    
+
     args.clear();
     args << Catalog::QueryArgument( "kind", Tag::Kind_FunctionDeclaration )
 	<< Catalog::QueryArgument( "scope", scope );
@@ -129,22 +129,22 @@ QValueList<Tag> CodeInformationRepository::getTagsInScope( const QStringList & s
 	    << Catalog::QueryArgument( "scope", scope );
 	tags += query( args );
     }
-    
+
     return tags;
 }
 
 QValueList<KTextEditor::CompletionEntry> CodeInformationRepository::getEntriesInScope( const QStringList & scope, bool isInstance, bool recompute )
 {
     kdDebug(9020) << "CodeInformationRepository::getEntriesInScope()" << endl;
-    
+
     if( !recompute && !scope.size() && m_globalEntries.size() )
 	return m_globalEntries;
     else if( scope.size() == 0 ){
 	m_globalEntries = my_unique( toEntryList( getTagsInScope(scope, isInstance) ) );
 	return m_globalEntries;
     }
-    
-    return toEntryList( getTagsInScope(scope, isInstance) ); 
+
+    return toEntryList( getTagsInScope(scope, isInstance) );
 }
 
 
@@ -172,20 +172,22 @@ QStringList CodeInformationRepository::getSignatureList( const QStringList & sco
     QStringList list;
     QValueList<Tag>::Iterator it = tags.begin();
     while( it != tags.end() ){
-        const Tag& tag = *it;
+        Tag tag = *it;
         ++it;
+
+	CppFunction<Tag> tagInfo( tag );
 
         QString signature;
         signature += tag.name() + "(";
-	QStringList arguments = tag.attribute( "arguments" ).toStringList();
-	QStringList argumentNames = tag.attribute( "argumentNames" ).toStringList();
-	
+	QStringList arguments = tagInfo.arguments();
+	QStringList argumentNames = tagInfo.argumentNames();
+
 	for( uint i=0; i<arguments.size(); ++i ){
 	    signature += arguments[ i ];
 	    QString argName = argumentNames[ i ];
 	    if( !argName.isEmpty() )
 		signature += QString::fromLatin1( " " ) + argName;
-	    
+
 	    if( i != (arguments.size()-1) ){
 		signature += ", ";
 	    }
@@ -202,100 +204,100 @@ QValueList<Tag> CodeInformationRepository::getClassOrNamespaceList( const QStrin
 {
     kdDebug(9020) << "CodeInformationRepository::getClassOrNamespaceList()" << endl;
 
-    QValueList<Tag> tags;    
+    QValueList<Tag> tags;
     QValueList<Catalog::QueryArgument> args;
- 
+
     args.clear();
     args << Catalog::QueryArgument( "kind", Tag::Kind_Namespace )
 	<< Catalog::QueryArgument( "scope", scope );
     tags += query( args );
-    
+
     args << Catalog::QueryArgument( "kind", Tag::Kind_Class )
     	<< Catalog::QueryArgument( "scope", scope );
-    
+
     tags += query( args );
- 
+
     return tags;
 }
 
 QValueList<Tag> CodeInformationRepository::getTagsInScope( const QString & name, const QStringList & scope )
 {
-    QValueList<Tag> tags;    
+    QValueList<Tag> tags;
     QValueList<Catalog::QueryArgument> args;
 
     args.clear();
     args << Catalog::QueryArgument( "scope", scope )
 	<< Catalog::QueryArgument( "name", name );
-    
+
     tags += query( args );
-    
+
     return tags;
 }
 
 QValueList<KTextEditor :: CompletionEntry> CodeInformationRepository::toEntryList( const QValueList<Tag> & tags )
 {
     QValueList<KTextEditor :: CompletionEntry> entryList;
-    
+
     QValueList<Tag>::ConstIterator it = tags.begin();
     while( it != tags.end() ){
 	Tag tag = *it;
 	++it;
-	
+
 	KTextEditor::CompletionEntry entry;
 	switch( tag.kind() ){
 	    case Tag::Kind_Class:
 		entry.prefix = "class";
 		entry.text = tag.name();
 		break;
-		
+
 	    case Tag::Kind_Namespace:
 		entry.prefix = "namespace";
 		entry.text = tag.name();
 		break;
-		
+
 	    case Tag::Kind_FunctionDeclaration:
 	    case Tag::Kind_Function:
 	    {
 		entry.text = tag.name();
-		
+
 		entry.text += "(";
-		
+
 		CppFunction<Tag> tagInfo( tag );
 		QStringList arguments = tagInfo.arguments();
 		QStringList argumentNames = tagInfo.argumentNames();
-		
+
 		QString signature;
 		for( uint i=0; i<arguments.size(); ++i ){
 		    signature += arguments[ i ];
 		    QString argName = argumentNames[ i ];
 		    if( !argName.isEmpty() )
 			signature += QString::fromLatin1( " " ) + argName;
-		    
+
 		    if( i != (arguments.size()-1) ){
 			signature += ", ";
 		    }
 		}
-		
+
 		if( signature.isEmpty() )
 		    entry.text += ")";
 		else
-		    entry.postfix = signature + ")";		
+		    entry.postfix = signature + ")";
 	    }
 	    break;
-	    
+
 	    case Tag::Kind_Enumerator:
 	    case Tag::Kind_Variable:
 	    case Tag::Kind_VariableDeclaration:
 	        entry.text = tag.name();
 	        break;
-	    
+
 	    default:
 		;
 	}
-	
+
 	entryList << entry;
     }
-    
+
     return entryList;
 }
 
