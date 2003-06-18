e0c44efe7af4bc6bbce43f32c69bf5103b6c19e7
Mario Scalas <mario.scalas@libero.it>
2003-06-18 15:04:06 +0000

    Ok, final fixes ;-)

--- parts/cvs/logform.h
+++ parts/cvs/logform.h
@@ -1,53 +1,48 @@
-/****************************************************************************
-** Form interface generated from reading ui file './logform.ui'
-**
-** Created: Wed Jun 18 16:14:26 2003
-**      by: The User Interface Compiler ($Id$)
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
+/***************************************************************************
+ *   Copyright (C) 200?-2003 by KDevelop Authors                           *
+ *   www.kdevelop.org                                                      *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef LOGFORM_H
 #define LOGFORM_H
 
-#include <qvariant.h>
-#include <qwidget.h>
-
-class QVBoxLayout;
-class QHBoxLayout;
-class QGridLayout;
-class QTextBrowser;
+class QStringList;
 class QProcess;
 
-class LogForm : public QWidget
-{
-    Q_OBJECT
+#include <logformbase.h>
 
+/**
+Implementation for the form displaying 'cvs log' output.
+
+@author KDevelop Authors
+*/
+class LogForm : public LogFormBase
+{
+	Q_OBJECT
 public:
-    LogForm( QWidget* parent = 0, const char* name = 0, WFlags fl = 0 );
+    LogForm( QWidget *parent=0, const char *name=0, int flags=0 );
     ~LogForm();
 
-    QTextBrowser* contents;
+	void start( const QString &workDir, const QStringList &pathsList );
 
 public slots:
-    virtual void init();
-    virtual void destroy();
-    virtual void start( const QString & workDir, const QString & fn );
-    virtual void slotProcessExited();
-    virtual void slotReadStdout();
-    virtual void slotReadStderr();
-    virtual void setText( const QString & text );
-    virtual void linkClicked( const QString & link );
+	void slotProcessExited();
+	void slotReadStdout();
+	void slotReadStderr();
+	void slotLinkClicked( const QString & link );
 
-protected:
-    QString pathName;
-    QProcess* process;
+private:
+	void setText( const QString& text );
 
-    QVBoxLayout* LogFormLayout;
-
-protected slots:
-    virtual void languageChange();
-
+private:
+	QProcess *process;
+	QString pathName;
 };
 
-#endif // LOGFORM_H
+#endif
