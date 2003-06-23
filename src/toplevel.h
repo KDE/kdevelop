bc2679f396fb3397947889ee6ecb23d33dbea407
Amilcar do Carmo Lucas <amilcar@kdevelop.org>
2003-06-23 16:35:15 +0000

    Removed old and redundant documentation

--- src/toplevel.h
+++ src/toplevel.h
@@ -5,43 +5,6 @@
 #include "kdevmainwindow.h"
 
 
-/*! \mainpage GIDEON ARCHITECTURE
-
-KDevelop is an easy to use IDE for developing C/C++ applications under X11. Currently it also supports:
-  -KDE applications
-  -QT applications
-  -Fortran
-  -Java
-  -Perl
-  -Phyton
-  -PHP
-  -Ruby
-  .
-
-For more info goto: http://www.kdevelop.org
-
-\section intro Introduction
-This page explains how the various peaces of gideon work together.
-
-\section install Installed Files
-TBD.
-
-\section framework Framework
-\subsection toplevel_object The Toplevel Opject
-There is only one toplevel object of class KDevMainWindow in Gideon. It can be accessed through the static function
-TopLevel::getInstance() (see TopLevel).
-
-KDevMainWindow has two derived classes. One is used for the SDI mode (Single Document Interface), the other for the
-MDI modes (Multiple Document Interface).They are named TopLevelSDI and TopLevelMDI, respectively.
-
-\subsection SDI Single Document Interface Mode
-
-\subsection MDI Multiple Document Interface Modes
-*/
-
-
-
-
 /**\brief This class handles the single object of type KDevMainWindow.
 
 It is completely static (all methods and attributes).
