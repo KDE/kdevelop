ec3d0f1787a410d8996c7c82fd04cfae56d1adff
Alexander Dymo <adymo@kdevelop.org>
2003-07-06 11:40:09 +0000

    delphi compiler options dialog implemented

--- plugins/dccoptions/dccoptionsplugin.h
+++ plugins/dccoptions/dccoptionsplugin.h
@@ -11,6 +11,8 @@
 #ifndef DCCOPTIONSPLUGIN_H
 #define DCCOPTIONSPLUGIN_H
 
+#include <kdialogbase.h>
+
 #include "kdevcompileroptions.h"
 
 class DccOptionsPlugin : public KDevCompilerOptions
@@ -18,10 +20,37 @@ class DccOptionsPlugin : public KDevCompilerOptions
     Q_OBJECT
 
 public:
-    DccOptionsPlugin( QObject *parent, const char *name, const QStringList &args );
+    DccOptionsPlugin( QObject *parent, const char *name, const QStringList/* &args */);
     ~DccOptionsPlugin();
 
     virtual QString exec(QWidget *parent, const QString &flags);
 };
+
+class GeneralTab;
+class LinkerTab;
+class LocationsTab;
+class Locations2Tab;
+class CodegenTab;
+class DebugOptimTab;
+
+class DccOptionsDialog : public KDialogBase
+{
+public:
+    DccOptionsDialog( QWidget *parent=0, const char *name=0 );
+    ~DccOptionsDialog();
+
+    void setFlags(const QString &flags);
+    QString flags() const;
+
+private:
+    GeneralTab *general;
+    LinkerTab *linker;
+    LocationsTab *locations;
+    Locations2Tab *locations2;
+    CodegenTab *codegen;
+    DebugOptimTab *debug_optim;
+    QStringList unrecognizedFlags;
+};
+
 
 #endif
