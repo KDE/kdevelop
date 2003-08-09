89e0be94ce2e7cf19708295863f8f85fbb4ddd8a
Mario Scalas <mario.scalas@libero.it>
2003-08-09 20:33:36 +0000

    * Added standard KDeveloper's copyright header * Reverted my last changes because they broke konsole respawn.

--- parts/konsole/konsoleviewwidget.cpp
+++ parts/konsole/konsoleviewwidget.cpp
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by KDevelop Authors                                *
+ *   kdevelop-devel@kde.org                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <qlayout.h>
 #include <qframe.h>
 #include <qdir.h>
--- parts/konsole/konsoleviewwidget.h
+++ parts/konsole/konsoleviewwidget.h
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by KDevelop Authors                                *
+ *   kdevelop-devel@kde.org                                                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef __KONSOLEVIEWWIDGET_H__
 #define __KONSOLEVIEWWIDGET_H__
 
