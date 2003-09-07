7f884803dd1958b6820766f67488d6a14ead1e43
Roberto Raggi <roberto@kdevelop.org>
2003-09-07 18:09:12 +0000

    added the PCS importer for KDELibs

---
+++ plugins/pcsimporter/kdelibsimporter/kdevkdelibsimporter.cpp
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Roberto Raggi                                   *
+ *   roberto@kdevelop.org                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "kdevkdelibsimporter.h"
+#include "kdevkdelibsimporter.moc"
+#include "settingsdialog.h"
+
+#include <kgenericfactory.h>
+
+#include <qlabel.h>
+#include <qdir.h>
+
+K_EXPORT_COMPONENT_FACTORY( libkdevkdelibsimporter, KGenericFactory<KDevKDELibsImporter>( "kdevkdelibsimporter" ) );
+
+KDevKDELibsImporter::KDevKDELibsImporter( QObject * parent, const char * name, const QStringList& )
+    : KDevPCSImporter( parent, name )
+{
+}
+
+KDevKDELibsImporter::~KDevKDELibsImporter()
+{
+}
+
+QStringList KDevKDELibsImporter::fileList( const QString& path )
+{
+    QDir dir( path );
+    QStringList lst = dir.entryList( "*.h" );
+    QStringList fileList;
+    for( QStringList::Iterator it=lst.begin(); it!=lst.end(); ++it )
+    {
+        fileList.push_back( dir.absPath() + "/" + (*it) );
+    }
+    return fileList;
+}
+
+
+QStringList KDevKDELibsImporter::fileList()
+{
+    if( !m_settings )
+	return QStringList();
+
+    QStringList files;
+    files += fileList( m_settings->kdeDir() + "/include" );
+    files += fileList( m_settings->kdeDir() + "/include/kio" );
+    files += fileList( m_settings->kdeDir() + "/include/kparts" );
+    files += fileList( m_settings->kdeDir() + "/include/ktexteditor" );
+
+    return files;
+}
+
+QStringList KDevKDELibsImporter::includePaths()
+{
+    if( !m_settings )
+	return QStringList();
+
+    QStringList includePaths;
+    includePaths.push_back( m_settings->kdeDir() );
+    /// @todo add mkspec
+    return includePaths;
+}
+
+QWidget * KDevKDELibsImporter::createSettingsPage( QWidget * parent, const char * name )
+{
+    m_settings = new SettingsDialog( parent, name );
+    connect( m_settings, SIGNAL(enabled(int)), this, SIGNAL(enabled(int)) );
+    return m_settings;
+}
+
---
+++ plugins/pcsimporter/kdelibsimporter/settingsdialog.cpp
@@ -0,0 +1,70 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Roberto Raggi                                   *
+ *   roberto@kdevelop.org                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "settingsdialog.h"
+#include <klistbox.h>
+#include <kcombobox.h>
+#include <qfile.h>
+#include <qdir.h>
+#include <qregexp.h>
+#include <cstdlib>
+
+SettingsDialog::SettingsDialog(QWidget* parent, const char* name, WFlags fl)
+    : SettingsDialogBase(parent,name,fl)
+{
+    QStringList kdedirs;
+    kdedirs.push_back( ::getenv("KDEDIR") );
+    kdedirs.push_back( "/usr/lib/kde/include" );
+    kdedirs.push_back( "/usr/local/kde/include" );
+    kdedirs.push_back( "/usr/local/include" );
+    kdedirs.push_back( "/usr/kde/include" );
+    kdedirs.push_back( "/usr/include/kde" );
+    kdedirs.push_back( "/usr/include" );
+    kdedirs.push_back( "/opt/kde3/include" );
+    kdedirs.push_back( "/opt/kde/include" );
+
+    for( QStringList::Iterator it=kdedirs.begin(); it!=kdedirs.end(); ++it )
+    {
+	QString kdedir = *it;
+	if( !kdedir.isNull() && isValidKDELibsDir(kdedir) )
+	    kdeListBox->insertItem( kdedir );
+    }
+}
+
+SettingsDialog::~SettingsDialog()
+{
+}
+
+/*$SPECIALIZATION$*/
+void SettingsDialog::slotSelectionChanged(QListBoxItem* item)
+{
+    if( !item ){
+	emit enabled( false );
+	return;
+    }
+
+    emit enabled( true );
+}
+
+bool SettingsDialog::isValidKDELibsDir( const QString & path ) const
+{
+    return QFile::exists( path + "/include/kaction.h" );
+}
+
+QString SettingsDialog::kdeDir( ) const
+{
+    return kdeListBox->currentText();
+}
+
+
+#include "settingsdialog.moc"
+
+
