f7e5605f79513a8a94c596d4cefc2efa09a2ae35
Tobias Gläßer <tobi.web@gmx.de>
2003-09-12 13:01:32 +0000

    little character encoding fix

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
   aboutData.addCredit("Luc Willems", I18N_NOOP("Help with Perl support"), "Willems.luc@pandora.be");
   aboutData.addCredit("Marcel Turino", I18N_NOOP("Documentation index view"), "M.Turino@gmx.de");
   aboutData.addCredit("Oliver Kellogg", I18N_NOOP("Ada support"), "Oliver.Kellogg@t-online.de");
-  aboutData.addCredit("Tobias Glaeﬂer", I18N_NOOP("QMake projectmanager patches"), "tobi.web@gmx.de");
+  aboutData.addCredit( "Tobias Gl\303\244\303\237er" , I18N_NOOP("QMake projectmanager patches"), "tobi.web@gmx.de");
 
   KCmdLineArgs::init(argc, argv, &aboutData);
   KCmdLineArgs::addCmdLineOptions( options );
