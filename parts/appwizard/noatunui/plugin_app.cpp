475e77075ccf1a33ef0378021580e4b7d3ecc57d
Dominik Haumann <dhdev@gmx.de>
2003-09-17 13:52:45 +0000

    Initial import: A userinterface plugin template for noatun.

---
+++ parts/appwizard/noatunui/plugin_app.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) $YEAR$ $AUTHOR$ <$EMAIL$>
+ */
+
+#include "plugin_$APPNAMELC$_impl.h"
+
+extern "C"
+{
+    Plugin *create_plugin()
+    {
+        return new $APPNAME$UI;
+    }
+}
+
