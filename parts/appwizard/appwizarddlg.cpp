391811da0b649abb5d581bfa1c78975473f82879
Amilcar do Carmo Lucas <amilcar@kdevelop.org>
2003-09-19 17:32:34 +0000

    Update the GNU GPL license.
    CCMAIL: 64439-done@bugs.kde.org

--- parts/appwizard/appwizarddlg.cpp
+++ parts/appwizard/appwizarddlg.cpp
@@ -256,7 +256,17 @@ void AppWizardDialog::licenseChanged()
                 " *   This program is free software; you can redistribute it and/or modify  *\n"
                 " *   it under the terms of the GNU General Public License as published by  *\n"
                 " *   the Free Software Foundation; either version 2 of the License, or     *\n"
-                " *   (at your option) any later version.                                   *\n";
+                " *   (at your option) any later version.                                   *\n"
+                " *                                                                         *\n"
+                " *   This program is distributed in the hope that it will be useful,       *\n"
+                " *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *\n"
+                " *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *\n"
+                " *   GNU General Public License for more details.                          *\n"
+                " *                                                                         *\n"
+                " *   You should have received a copy of the GNU General Public License     *\n"
+                " *   along with this program; if not, write to the                         *\n"
+                " *   Free Software Foundation, Inc.,                                       *\n"
+                " *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *\n";
             break;
         case 1:
             str +=
@@ -294,7 +304,17 @@ void AppWizardDialog::licenseChanged()
                 " *   This program is free software; you can redistribute it and/or modify  *\n"
                 " *   it under the terms of the GNU Library General Public License as       *\n"
                 " *   published by the Free Software Foundation; either version 2 of the    *\n"
-                " *   License, or (at your option) any later version.                       *\n";
+                " *   License, or (at your option) any later version.                       *\n"
+                " *                                                                         *\n"
+                " *   This program is distributed in the hope that it will be useful,       *\n"
+                " *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *\n"
+                " *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *\n"
+                " *   GNU General Public License for more details.                          *\n"
+                " *                                                                         *\n"
+                " *   You should have received a copy of the GNU General Public License     *\n"
+                " *   along with this program; if not, write to the                         *\n"
+                " *   Free Software Foundation, Inc.,                                       *\n"
+                " *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *\n";
         }
 
     str += " ***************************************************************************/\n";
