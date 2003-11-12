64bdf9c7ff8c96380945a07fb91f47daaf211729
Jens Dagerbo <jens.dagerbo@swipnet.se>
2003-11-12 02:34:19 +0000

    also fix the templates for real

--- languages/cpp/app_templates/kdevpart/kdevpart_part.cpp
+++ languages/cpp/app_templates/kdevpart/kdevpart_part.cpp
@@ -40,7 +40,10 @@ $APPNAME$Part::$APPNAME$Part(QObject *parent, const char *name, const QStringLis
 $APPNAME$Part::~$APPNAME$Part()
 {
 // if you embed a widget, you need to tell the mainwindow when you remove it
-//  mainWindow()->removeView( m_widget );
+//  if ( m_widget )
+//  {
+//    mainWindow()->removeView( m_widget );
+//  }
   delete m_widget;
 }
 
