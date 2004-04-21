188ca8b1e93183120c37cfbfa145f9a55c7167e3
Alexander Dymo <adymo@kdevelop.org>
2004-04-21 18:09:55 +0000

    Added Gtk-- application framework template made by Andrew Patterson.

---
+++ languages/cpp/app_templates/gtk2mmapp/acconfig.h
@@ -0,0 +1,10 @@
+/* acconfig.h
+ * This file is in the public domain.
+ * 
+ * Descriptive text for the C preprocessor macros that
+ * the distributed Autoconf macros can define.
+ * These entries are sometimes used by macros
+ * which glade-- uses.
+ */
+#undef PACKAGE
+#undef VERSION
