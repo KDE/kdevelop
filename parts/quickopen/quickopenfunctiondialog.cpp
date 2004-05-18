7a65bffe69e777cd2bcee4ab8265d2bcaa2c770b
Roberto Raggi <roberto@kdevelop.org>
2004-05-18 16:19:55 +0000

    KDevLanguageSupport has a specialized function for that

--- parts/quickopen/quickopenfunctiondialog.cpp
+++ parts/quickopen/quickopenfunctiondialog.cpp
@@ -31,6 +31,8 @@
 #include <codemodel_utils.h>
 #include <kdevpartcontroller.h>
 #include <kdevproject.h>
+#include <kdevlanguagesupport.h>
+
 #include <doclineedit.h>
 
 #include "quickopenfunctionchooseform.h"
@@ -39,108 +41,98 @@
 QuickOpenFunctionDialog::QuickOpenFunctionDialog( QuickOpenPart *part, QWidget* parent, const char* name, bool modal, WFlags fl)
 : QuickOpenDialog(part, parent, name, modal, fl)
 {
-	nameLabel->setText( i18n("Function &name:") );
-	itemListLabel->setText( i18n("Function &list:") );
+        nameLabel->setText( i18n("Function &name:") );
+        itemListLabel->setText( i18n("Function &list:") );
 
-	FileList fileList = m_part->codeModel()->fileList();
-	m_functionDefList = new FunctionDefinitionList();
-	m_functionStrList = new QStringList();
+        FileList fileList = m_part->codeModel()->fileList();
+        m_functionDefList = new FunctionDefinitionList();
+        m_functionStrList = new QStringList();
 
-	// for each one file, get all function list
-	FileDom fileDom;
-	for( FileList::Iterator it = fileList.begin() ; it!=fileList.end() ; ++it ){
-		fileDom = *it;
-		*m_functionDefList += CodeModelUtils::allFunctionDefinitionsDetailed( fileDom ).functionList;
-	}
-	for( FunctionDefinitionList::ConstIterator it = m_functionDefList->begin() ; it!=m_functionDefList->end(); ++it ){
-		const FunctionDefinitionModel *fmodel = (*it).data();
-		m_functionStrList->append( fmodel->name() );
-	}
-	m_completion = new KCompletion();
-	//m_functionStrList->sort();
-	m_completion->setOrder( KCompletion::Sorted );
-	m_completion->setItems( *m_functionStrList );
+        // for each one file, get all function list
+        FileDom fileDom;
+        for( FileList::Iterator it = fileList.begin() ; it!=fileList.end() ; ++it ){
+                fileDom = *it;
+                *m_functionDefList += CodeModelUtils::allFunctionDefinitionsDetailed( fileDom ).functionList;
+        }
+        for( FunctionDefinitionList::ConstIterator it = m_functionDefList->begin() ; it!=m_functionDefList->end(); ++it ){
+                const FunctionDefinitionModel *fmodel = (*it).data();
+                m_functionStrList->append( fmodel->name() );
+        }
+        m_completion = new KCompletion();
+        //m_functionStrList->sort();
+        m_completion->setOrder( KCompletion::Sorted );
+        m_completion->setItems( *m_functionStrList );
 
-	itemList->insertStringList( m_completion->items() );
-	itemList->setCurrentItem( 0 );
+        itemList->insertStringList( m_completion->items() );
+        itemList->setCurrentItem( 0 );
 }
 
 QuickOpenFunctionDialog::~QuickOpenFunctionDialog()
 {
-	delete m_completion;
-	delete m_functionDefList;
-	delete m_functionStrList;
-	m_completion = 0;
-	m_functionDefList = 0;
-	m_functionStrList = 0;
+        delete m_completion;
+        delete m_functionDefList;
+        delete m_functionStrList;
+        m_completion = 0;
+        m_functionDefList = 0;
+        m_functionStrList = 0;
 }
 
 void QuickOpenFunctionDialog::gotoFile( QString name )
 {
-	FunctionDefinitionModel *fmodel;
-	//FunctionDefinitionList *funcList = new FunctionDefinitionList();
-	funcList = new FunctionDefinitionList();
-	FunctionDefinitionDom fdom;
+        FunctionDefinitionModel *fmodel;
+        //FunctionDefinitionList *funcList = new FunctionDefinitionList();
+        funcList = new FunctionDefinitionList();
+        FunctionDefinitionDom fdom;
 
-	for( FunctionDefinitionList::ConstIterator it = m_functionDefList->begin() ; it!=m_functionDefList->end() ; ++it ){
-		fdom = *it;
-		fmodel = fdom.data();
-		if( fmodel->name() == name ){
-			funcList->append( fdom );
-		}
-	}
-	if( funcList->count() == 1 ){
-		fdom = funcList->first();
-		fmodel = fdom.data();
-		QString fileNameStr = fmodel->fileName();
-		int startline, startcol;
-		fmodel->getStartPosition( &startline, &startcol );
-		m_part->partController()->editDocument( KURL( fileNameStr), startline, startcol );
+        for( FunctionDefinitionList::ConstIterator it = m_functionDefList->begin() ; it!=m_functionDefList->end() ; ++it ){
+                fdom = *it;
+                fmodel = fdom.data();
+                if( fmodel->name() == name ){
+                        funcList->append( fdom );
+                }
+        }
+        if( funcList->count() == 1 ){
+                fdom = funcList->first();
+                fmodel = fdom.data();
+                QString fileNameStr = fmodel->fileName();
+                int startline, startcol;
+                fmodel->getStartPosition( &startline, &startcol );
+                m_part->partController()->editDocument( KURL( fileNameStr), startline, startcol );
 
-	}else if( funcList->count() > 1 ){
-		QString argStr, _argStr;
-		QString fileStr;
-		
-		QuickOpenFunctionChooseForm fdlg( this, name.ascii() );
+        }else if( funcList->count() > 1 ){
+                QString fileStr;
 
-		for( FunctionDefinitionList::Iterator it = funcList->begin() ; it!=funcList->end() ; ++it ){
-			fmodel = (*it).data();
+                QuickOpenFunctionChooseForm fdlg( this, name.ascii() );
 
-			ArgumentList argList = fmodel->argumentList();
-			_argStr = "";
-			
-			for( ArgumentList::Iterator it = argList.begin() ; it!=argList.end() ; ++it ){
-				ArgumentModel *argModel = (*it).data();
-				argStr = _argStr.append( " " ).append( argModel->type() ).append(" ").append( argModel->name() ).append(',');
-			}
-			
-			if( argStr.isEmpty() ) argStr = i18n(" No argument" );
-			fdlg.argBox->insertItem( argStr );
-			fileStr = KURL( fmodel->fileName() ).fileName();
-			fdlg.fileBox->insertItem( fileStr );
-		}
-		if( fdlg.exec() ){
-			int id = fdlg.argBox->currentItem();
-			if( id>-1 && id < funcList->count() ){
-				FunctionDefinitionModel *model = (*funcList)[id].data();
-				int line, col;
-				model->getStartPosition( &line, &col );
-				QString fileNameStr = model->fileName();
-				m_part->partController()->editDocument( KURL(fileNameStr), line );
-			}
-		}
-	}
-	else{
-		KMessageBox::error( this, i18n("Error: cannot find matching name function.") );
-	}
+                for( FunctionDefinitionList::Iterator it = funcList->begin() ; it!=funcList->end() ; ++it ){
+                        fmodel = (*it).data();
 
-	accept();
+                        fdlg.argBox->insertItem( m_part->languageSupport()->formatModelItem(fmodel) );
+                        fileStr = KURL( fmodel->fileName() ).fileName();
+                        fdlg.fileBox->insertItem( fileStr );
+                }
+                if( fdlg.exec() ){
+                        int id = fdlg.argBox->currentItem();
+                        if( id>-1 && id < funcList->count() ){
+                                FunctionDefinitionModel *model = (*funcList)[id].data();
+                                int line, col;
+                                model->getStartPosition( &line, &col );
+                                QString fileNameStr = model->fileName();
+                                m_part->partController()->editDocument( KURL(fileNameStr), line );
+                        }
+                }
+        }
+        else{
+                KMessageBox::error( this, i18n("Error: cannot find matching name function.") );
+        }
+
+        accept();
 }
 void QuickOpenFunctionDialog::slotExecuted(QListBoxItem* item)
 {
-	if( item ){
-		gotoFile( item->text() );
-	}
+        if( item ){
+                gotoFile( item->text() );
+        }
 }
 void QuickOpenFunctionDialog::executed(QListBoxItem*)
 {
@@ -148,8 +140,8 @@ void QuickOpenFunctionDialog::executed(QListBoxItem*)
 
 void QuickOpenFunctionDialog::slotReturnPressed()
 {
-	QListBoxItem *listboxItem = itemList->selectedItem();
-	slotExecuted( listboxItem );
+        QListBoxItem *listboxItem = itemList->selectedItem();
+        slotExecuted( listboxItem );
 }
 
 #include "quickopenfunctiondialog.moc"
