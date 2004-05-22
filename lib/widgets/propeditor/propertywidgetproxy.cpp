ccf71383ab861b10c3735c495abc927433be53b3
Alexander Dymo <adymo@kdevelop.org>
2004-05-22 18:02:16 +0000

    *** empty log message ***

---
+++ lib/widgets/propeditor/propertywidgetproxy.cpp
@@ -0,0 +1,84 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Alexander Dymo                                  *
+ *   cloudtemple@mskat.net                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "propertywidgetproxy.h"
+
+#include <qlayout.h>
+
+#include "propertywidget.h"
+#include "propertymachinefactory.h"
+
+namespace PropertyLib{
+
+PropertyWidgetProxy::PropertyWidgetProxy(QWidget *parent, const char *name)
+    :QWidget(parent, name), mp(0), m_propertyType(Property::Invalid), m_editor(0)
+{
+    p = new Property();
+    m_layout = new QHBoxLayout(this, 0, 0);
+}
+
+PropertyWidgetProxy::~PropertyWidgetProxy()
+{
+    delete mp;
+    delete p;
+}
+
+void PropertyWidgetProxy::setPropertyType(int propertyType)
+{
+    m_propertyType = static_cast<Property::PropertyType>(propertyType);
+    setWidget();
+}
+
+void PropertyWidgetProxy::setPropertyType2(PropertyType propertyType)
+{
+    m_propertyType = propertyType;
+    setWidget();
+}
+
+void PropertyWidgetProxy::setWidget()
+{
+    if (m_editor)
+        delete m_editor;
+    p->setType(m_propertyType);
+    mp = new MultiProperty(p);
+    m_editor = PropertyMachineFactory::getInstance()->machineForProperty(mp)->propertyEditor;
+    if (m_editor)
+    {
+        m_editor->reparent(this, QPoint(0,0), true);
+        m_layout->addWidget(m_editor);
+    }
+}
+
+QVariant PropertyWidgetProxy::value() const
+{
+    if (m_editor)
+        return m_editor->value();
+    else
+        return QVariant();
+}
+
+void PropertyWidgetProxy::setValue(const QVariant &value)
+{
+    if (m_editor)
+        m_editor->setValue(value, false);
+}
+
+}
+
+#include "propertywidgetproxy.moc"
---
+++ lib/widgets/propeditor/propertywidgetproxy.h
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Alexander Dymo                                  *
+ *   cloudtemple@mskat.net                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef PROPERTYWIDGETPROXY_H
+#define PROPERTYWIDGETPROXY_H
+
+#include <qwidget.h>
+#include <qvariant.h>
+
+#include "multiproperty.h"
+
+class QHBoxLayout;
+
+namespace PropertyLib{
+
+class PropertyWidget;
+
+#define PropertyType Property::PropertyType
+
+class PropertyWidgetProxy: public QWidget
+{
+Q_OBJECT
+Q_PROPERTY( int propertyType READ propertyType WRITE setPropertyType DESIGNABLE true )
+Q_PROPERTY( PropertyType propertyType2 READ propertyType2 WRITE setPropertyType2 DESIGNABLE false )
+public:
+    PropertyWidgetProxy(QWidget *parent = 0, const char *name = 0);
+    ~PropertyWidgetProxy();
+    
+    void setPropertyType(int propertyType);
+    int propertyType() const { return m_propertyType; }
+    void setPropertyType2(PropertyType propertyType);
+    PropertyType propertyType2() const { return m_propertyType; }
+    
+    QVariant value() const;
+    void setValue(const QVariant &value);
+
+protected:
+    virtual void setWidget();
+    
+private:
+    Property *p;
+    MultiProperty *mp;
+    
+    Property::PropertyType m_propertyType;
+    PropertyWidget *m_editor;
+    
+    QHBoxLayout *m_layout;
+};
+
+}
+
+#endif
