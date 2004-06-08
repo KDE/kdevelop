12fdb0c5c9263d615b00b308c5af5650958b1467
Ian Reinhart Geiser <geiseri@kde.org>
2004-06-08 04:44:35 +0000

    Yes I hate MSVC this much.  If someone hates their life more than me, MFC support involves a small patch to MinGW, and figuring out the magic combination of #defines to make it work.  Yeah there is the fact that MFC 4.2 wont build with a standard C compiler... so we might have to get greative.

---
+++ languages/cpp/app_templates/win32hello/app.cpp
@@ -0,0 +1,16 @@
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  cout << "Hello, from Win32!" << endl;
+
+  return EXIT_SUCCESS;
+}
