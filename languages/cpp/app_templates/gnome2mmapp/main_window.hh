543fe44992f54a832c351765c9e25a77f12879bc
Alexander Dymo <adymo@kdevelop.org>
2004-06-16 21:16:12 +0000

    Added gnome2mm application template.

---
+++ languages/cpp/app_templates/gnome2mmapp/main_window.hh
@@ -0,0 +1,19 @@
+%{HH_TEMPLATE}
+
+// newer (non customized) versions of this file go to main_window.hh_new
+
+// you might replace
+//    class foo : public foo_glade { ... };
+// by
+//    typedef foo_glade foo;
+// if you didn't make any modifications to the widget
+
+#ifndef _MAIN_WINDOW_HH
+#  include "main_window_glade.hh"
+#  define _MAIN_WINDOW_HH
+class main_window : public main_window_glade
+{  
+protected:
+        virtual gint quit(GdkEventAny *ev);
+};
+#endif
