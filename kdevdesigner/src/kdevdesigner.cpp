b593dc9a03eef8ef14a158102fb9ac926ac8eafd
Andrew Coles <andrew_coles@yahoo.co.uk>
2004-06-29 13:26:09 +0000

    CVS_SILENT
    Corrected typo.

--- kdevdesigner/src/kdevdesigner.cpp
+++ kdevdesigner/src/kdevdesigner.cpp
@@ -74,7 +74,7 @@ KDevDesigner::KDevDesigner()
     {
         // if we couldn't find our Part, we exit since the Shell by
         // itself can't do anything useful
-        KMessageBox::error(this, i18n("Could not find our part."));
+        KMessageBox::error(this, i18n("Could not find the KDevDesigner part."));
         kapp->quit();
         // we return here, cause kapp->quit() only means "exit the
         // next time we enter the event loop...
