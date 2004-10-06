afe7c130f6aca033fd2a4cc141c007b898d54744
Zack Rusin <zack@kde.org>
2004-10-06 19:00:24 +0000

    that actually works

--- lib/widgets/propeditor/pdoublenuminput.cpp
+++ lib/widgets/propeditor/pdoublenuminput.cpp
@@ -77,7 +77,7 @@ void PDoubleNumInput::updateProperty(double val)
 }
 void PDoubleNumInput::updateProperty(int val)
 {
-    emit propertyChanged(m_property, QVariant(val));
+    emit propertyChanged(m_property, QVariant(double(val/(double)pow(m_edit->digits(),10))));
 }
 
 }
