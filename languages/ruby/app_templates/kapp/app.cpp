acf93872d812f82cefc44238d7ec8574e21c03da
Richard Dale <Richard_Dale@tipitina.demon.co.uk>
2004-10-28 04:12:43 +0000

    * Removed redundant extra configuration check for the ruby interpreter * Any command line args passed to the top level C++ stub, are now passed on to   the ruby app.

--- languages/ruby/app_templates/kapp/app.cpp
+++ languages/ruby/app_templates/kapp/app.cpp
@@ -10,15 +10,17 @@ int main(int argc, char **argv) {
     QString appdir = ::locate("data", "%{APPNAMELC}/main.rb", instance);
     QFileInfo program(appdir);
      
-    char ** rubyargs = (char **) calloc(5, sizeof(char *));
+    char ** rubyargs = (char **) calloc(argc+4, sizeof(char *));
     rubyargs[0] = strdup(argv[0]);
     rubyargs[1] = strdup("-KU");
     rubyargs[2] = strdup(QString("-C%1").arg(program.dirPath()).latin1());
     rubyargs[3] = strdup(QString("-I%1").arg(program.dirPath()).latin1());
     rubyargs[4] = strdup(program.fileName().latin1());
-          
+    for (int i = 1; i < argc; i++) {
+        rubyargs[i+4] = argv[i];
+    }
+
     ruby_init();
-    ruby_options(argc + 4, rubyargs); 
-    ruby_script(strdup("%{APPNAMELC}"));
+    ruby_options(argc+4, rubyargs); 
     ruby_run();
 }
