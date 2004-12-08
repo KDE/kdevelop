c16b7bfcab440b936e3e467d7e376b591731b76f
Alexander Dymo <adymo@kdevelop.org>
2004-12-08 22:58:04 +0000

    *** empty log message ***

--- vcs/subversion/integrator/svnintegratordlg.cpp
+++ vcs/subversion/integrator/svnintegratordlg.cpp
@@ -1,6 +1,8 @@
 /***************************************************************************
  *   Copyright (C) 2004 by Alexander Dymo                                  *
  *   adymo@kdevelop.org                                                    *
+ *   Copyright (C) 2004                                                    *
+ *   Mickael Marchand <marchand@kde.org>                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
