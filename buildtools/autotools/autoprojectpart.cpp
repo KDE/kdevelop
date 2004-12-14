c74518f97ba9923fdf40c706ab5120a213e51cd6
Matt Rogers <mattr@kde.org>
2004-12-14 19:00:31 +0000

    Add acinclude.m4 config.h.in and Makefile.in to the list of files to include when creating source tarballs for from the Distribution and Publishing Wizard.
    Thanks for the patch.
    
    BUG: 91566

--- buildtools/autotools/autoprojectpart.cpp
+++ buildtools/autotools/autoprojectpart.cpp
@@ -1329,7 +1329,7 @@ QStringList AutoProjectPart::distFiles() const
 	QString projectDir = projectDirectory();
 	QDir dir(projectDir);
 	QDir admin(projectDir +"/admin");
-	QStringList files = dir.entryList( "Makefile.cvs Makefile.am configure* INSTALL README NEWS TODO ChangeLog COPYING AUTHORS stamp-h.in");
+	QStringList files = dir.entryList( "Makefile.cvs Makefile.am configure* INSTALL README NEWS TODO ChangeLog COPYING AUTHORS stamp-h.in acinclude.m4 config.h.in Makefile.in");
 	QStringList adminFiles = admin.entryList(QDir::Files);
 	QStringList::Iterator idx = adminFiles.begin();
 	for( ; idx != adminFiles.end(); ++idx)
