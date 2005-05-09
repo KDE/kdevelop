ac3bca3a406c730573ad33a9d74e2df8d9e83eb2
Alexander Neundorf <neundorf@kde.org>
2005-05-09 20:20:32 +0000

    svn add seems to add all files in in a subdirectory instead of only the directory itself
    Alex

---
+++ lib/kdevelop/cmake_install.cmake
@@ -0,0 +1,4 @@
+# Install script for directory: /usr/src/kde3-svn/kdevelop/lib/kdevelop
+
+MESSAGE(STATUS "Installing /usr/local/lib/kdevelop.dir")
+FILE(INSTALL DESTINATION "/usr/local/lib" TYPE FILE FILES "/usr/src/kde3-svn/kdevelop/lib/kdevelop/kdevelop.dir")
---
+++ lib/kdevelop/dummy.o.depends.make
@@ -0,0 +1,3 @@
+# Dependencies for kdevelop.dir/../dummy.o
+kdevelop.dir/../dummy.o: ../dummy.cpp
+
