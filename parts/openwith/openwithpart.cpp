0768e6ef0a3b6f8d8082793ff8a2cfe4b80b52fd
Harald Fernengel <harry@kdevelop.org>
2005-08-17 13:42:55 +0000

    r308@angua:  harald | 2005-08-12T22:28:01.833807Z  ----------------------------------------------------------------------  r144 (orig r218):  kdevelop | 2005-07-15 15:27:18 +0200
     cleanup
     ----------------------------------------------------------------------

--- parts/openwith/openwithpart.cpp
+++ parts/openwith/openwithpart.cpp
@@ -1,94 +1,89 @@
 #include "openwithpart.h"
 
 #include <qfile.h>
-//Added by qt3to4:
-#include <Q3PopupMenu>
+#include <qfileinfo.h>
+#include <qmenu.h>
 
-#include <kpopupmenu.h>
+#include <kdevprojectmodel.h>
 #include <kdevgenericfactory.h>
+#include <kdevpartcontroller.h>
+#include <kdevcore.h>
+#include <kdevplugininfo.h>
+#include <urlutil.h>
+
+#include <kiconloader.h>
+#include <kpopupmenu.h>
 #include <kdebug.h>
 #include <kmimetype.h>
 #include <ktrader.h>
 #include <krun.h>
 #include <kaction.h>
 
-#include "kdevpartcontroller.h"
-#include "kdevcore.h"
-#include "kdevplugininfo.h"
-#include "urlutil.h"
 
 static const KDevPluginInfo data("kdevopenwith");
 K_EXPORT_COMPONENT_FACTORY(libkdevopenwith, KDevGenericFactory<OpenWithPart>(data))
 
 OpenWithPart::OpenWithPart(QObject *parent, const char *name, const QStringList &)
-  : KDevPlugin(&data, parent, name ? name : "OpenWithPart")
+  : KDevPlugin(&data, parent)
 {
-  connect(core(), SIGNAL(contextMenu(Q3PopupMenu*,const Context *)),
-	  this, SLOT(fillContextMenu(Q3PopupMenu*,const Context *)));
+  setObjectName(QString::fromUtf8(name));
+  
+  connect(core(), SIGNAL(contextMenu(QMenu*,const Context*)),
+          this, SLOT(fillContextMenu(QMenu*,const Context*)));
 }
-
 
 OpenWithPart::~OpenWithPart()
 {
 }
 
-
-void OpenWithPart::fillContextMenu(Q3PopupMenu *popup, const Context *context)
+void OpenWithPart::fillContextMenu(QMenu *popup, const Context *context)
 {
-  if (!context->hasType( Context::FileContext ))
-    return;
+  kdDebug(9000) << "context: " << context->type() << endl;
 
-  const FileContext *ctx = static_cast<const FileContext*>(context);
-  m_url = ctx->urls().first();
-  if (URLUtil::isDirectory(m_url))
-      return;
+  m_url = KURL();
 
-  popup->insertSeparator();
-
-  KPopupMenu *sub = new KPopupMenu(popup);
-
-  int id = sub->insertItem(i18n("Open as UTF-8"), this, SLOT(openAsEncoding(int)));
-  sub->setWhatsThis(id, i18n("<b>Open as UTF-8</b><p>Open this file in KDevelop as UTF-8 encoded text."));
-
-  QString mimeType = KMimeType::findByURL(m_url, 0, true, true)->name();
-  KTrader::OfferList offers = KTrader::self()->query(mimeType, "Type == 'Application'");
-
-  if (offers.count() > 0)
-  {
-
-    KTrader::OfferList::Iterator it;
-    for (it = offers.begin(); it != offers.end(); ++it)
+  if (context->hasType(Context::FileContext))
     {
-      KAction *action = new KAction((*it)->name(), (*it)->icon(), 0, 0, QFile::encodeName( (*it)->desktopEntryPath() ).data());
-      connect(action, SIGNAL(activated()), this, SLOT(openWithService()));
-      action->plug(sub);
+      const FileContext *ctx = static_cast<const FileContext*>(context);
+      m_url = ctx->urls().first();
     }
-    sub->insertSeparator();
+  else if (context->hasType(Context::ProjectItemContext))
+    {
+      const ProjectItemContext *ctx = static_cast<const ProjectItemContext*>(context);
+      const KDevProjectItem *item = ctx->item();
+      Q_ASSERT(item != 0);
+      if (item->file())
+        m_url.setPath(item->file()->fileInfo().absoluteFilePath());
+    }
+  else
+    {
+      // nothing to do
+      return;
+    }
 
-    id = popup->insertItem(i18n("Open With"), sub);
-    popup->setWhatsThis(id, i18n("<b>Open With</b><p>Lists all applications that can be used to open the selected file."));
-
-    // make sure the generic "Open with ..." entry gets appended to the submenu
-    popup = sub;
-  }
-
-  id = popup->insertItem(i18n("Open With..."), this, SLOT(openWithDialog()));
-  popup->setWhatsThis(id, i18n("<b>Open With...</b><p>Provides a dialog to choose the application to open the selected file."));
+  populateMenu(popup);
 }
 
+void OpenWithPart::populateMenu(QMenu *popup)
+{
+  popup->addSeparator();
+  
+  QAction *action = popup->addAction(i18n("Open With"));
+  action->setWhatsThis(i18n("<b>Open With</b><p>Lists all applications that can be used to open the selected file."));
+
+  connect(action, SIGNAL(triggered()), this, SLOT(openWithDialog()));
+}
 
 void OpenWithPart::openWithService()
 {
-  KService::Ptr ptr = KService::serviceByDesktopPath(sender()->name());
-  if (ptr)
-  {
-    KURL::List list;
-    list << m_url;
-
-    KRun::run(*ptr, list);
-  }
+  if (KService::Ptr ptr = KService::serviceByDesktopPath(sender()->name()))
+    {
+      KURL::List list;
+      list << m_url;
+    
+      KRun::run(*ptr, list);
+    }
 }
-
 
 void OpenWithPart::openWithDialog()
 {
@@ -96,7 +91,6 @@ void OpenWithPart::openWithDialog()
   list << m_url;
   KRun::displayOpenWithDialog(list);
 }
-
 
 void OpenWithPart::openAsEncoding(int /*id*/)
 {
--- parts/openwith/openwithpart.h
+++ parts/openwith/openwithpart.h
@@ -4,33 +4,28 @@
 #include <kurl.h>
 
 #include "kdevplugin.h"
-//Added by qt3to4:
-#include <Q3PopupMenu>
 
-class Q3PopupMenu;
+class QMenu;
 class Context;
-
 
 class OpenWithPart : public KDevPlugin
 {
   Q_OBJECT
-
-public:
-   
+public:   
   OpenWithPart(QObject *parent, const char *name, const QStringList &);
-  ~OpenWithPart();
+  virtual ~OpenWithPart();
 
- 
 private slots:
-
-  void fillContextMenu(Q3PopupMenu *popup, const Context *context);
+  void fillContextMenu(QMenu *popup, const Context *context);
 
   void openWithService();
   void openWithDialog();
   void openAsEncoding(int id);
 
 private:
+  void populateMenu(QMenu *popup);
 
+private:
   KURL m_url;
 };
 
