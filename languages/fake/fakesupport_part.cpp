7f3277484f9ac897f2645a7bbd33a3b7dfa38822
Harald Fernengel <harry@kdevelop.org>
2005-08-17 13:33:30 +0000

    r240@angua:  harald | 2005-08-12T21:58:23.547363Z  ----------------------------------------------------------------------  r73 (orig r84):  kdevelop | 2005-07-03 21:02:43 +0200
     bump version number
     
     ----------------------------------------------------------------------

--- languages/fake/fakesupport_part.cpp
+++ languages/fake/fakesupport_part.cpp
@@ -1,8 +1,9 @@
 
 #include "fakesupport_part.h"
+#include "fakesupport_factory.h"
 
 FakeLanguageSupport::FakeLanguageSupport(QObject *parent, const char *name, const QStringList &)
-    : KDevLanguageSupport(info(), parent)
+    : KDevLanguageSupport(FakeSupportFactory::info(), parent)
 {
     setObjectName(QString::fromUtf8(name));
 }
