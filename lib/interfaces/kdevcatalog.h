5ff40377738a30cf8f758ed7bcb99aac5156b8c5
Harald Fernengel <harry@kdevelop.org>
2005-08-17 13:45:08 +0000

    r322@angua:  harald | 2005-08-12T22:37:12.637204Z  ----------------------------------------------------------------------  r158 (orig r289):  kdevelop | 2005-07-30 19:42:28 +0200
     move files
     ----------------------------------------------------------------------

---
+++ lib/interfaces/kdevcatalog.h
@@ -0,0 +1,81 @@
+/* This file is part of KDevelop
+   Copyright (C) 2003 Roberto Raggi <roberto@kdevelop.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+*/
+
+#ifndef KDEVCATALOG_H
+#define KDEVCATALOG_H
+
+#include <qlist.h>
+#include <qpair.h>
+#include <qvariant.h>
+#include <QByteArray>
+
+#include "tag.h"
+
+/**
+   @file catalog.h
+   KDevCatalog database - the persistant symbol store database.
+*/
+
+/**
+   KDevCatalog objects represent separate symbol databases.
+   KDevCatalogs can be created/loaded/unloaded dynamically.
+   To find a symbol in the repository each catalog should be queried.
+
+   Persistant symbol store is useful to keep information about code that
+   never or rarely changes. System libraries are perfect examples of such code.
+*/
+class KDevCatalog
+{
+ public:
+  typedef QPair<QByteArray, QVariant> QueryArgument;
+
+ public:
+  KDevCatalog();
+  virtual ~KDevCatalog();
+
+  bool isValid() const;
+  QString dbName() const;
+
+  bool enabled() const;
+  void setEnabled( bool en );
+
+  virtual void open( const QString& dbName );
+  virtual void close();
+  virtual void sync();
+
+  QList<QByteArray> indexList() const;
+  void addIndex( const QByteArray& name );
+
+  void addItem( Tag& tag );
+
+  Tag getItemById( const QByteArray& id );
+  QList<Tag> query( const QList<QueryArgument>& args );
+
+  QByteArray generateId();
+
+ private:
+  class _KDevCatalog_Private* d;
+
+ private:
+  KDevCatalog( const KDevCatalog& source );
+  void operator = ( const KDevCatalog& source );
+};
+
+#endif // KDEVCATALOG_H
+
