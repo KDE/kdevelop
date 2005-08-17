782d6ced3c9e97e79a97f04249d02877ca260a16
Harald Fernengel <harry@kdevelop.org>
2005-08-17 13:36:23 +0000

    r276@angua:  harald | 2005-08-12T22:07:44.863006Z  ----------------------------------------------------------------------  r109 (orig r177):  kdevelop | 2005-07-06 20:58:23 +0200
     use the generic pm
     ----------------------------------------------------------------------

---
+++ lib/projectmanager/kdevprojectmanagerdelegate.cpp
@@ -0,0 +1,58 @@
+/* This file is part of KDevelop
+    Copyright (C) 2005 Roberto Raggi <roberto@kdevelop.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+    Boston, MA 02111-1307, USA.
+*/
+
+#include "kdevprojectmanagerdelegate.h"
+#include "kdevprojectmodel.h"
+
+#include <QtGui/QPainter>
+#include <QtCore/qdebug.h>
+
+KDevProjectManagerDelegate::KDevProjectManagerDelegate(QObject *parent)
+  : QItemDelegate(parent)
+{
+}
+
+KDevProjectManagerDelegate::~KDevProjectManagerDelegate()
+{
+}
+
+void KDevProjectManagerDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+  QStyleOptionViewItem opt = option;
+
+  if (const KDevProjectModel *projectModel = qobject_cast<const KDevProjectModel*>(index.model()))
+    {
+      KDevProjectItem *item = projectModel->item(index);
+
+      if (item && item->folder())
+        {
+          opt.font.setBold(true);
+          opt.palette.setColor(QPalette::Text, Qt::blue);
+        }
+      else if (item && item->target())
+        {
+          opt.font.setBold(true);
+          opt.palette.setColor(QPalette::Text, Qt::red);
+        }
+    }
+
+  QItemDelegate::paint(painter, opt, index);
+}
+
+#include "kdevprojectmanagerdelegate.moc"
