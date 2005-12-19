ff14a821ad8108e75dea6adb3d048217f2e2c456
Dirk Mueller <mueller@kde.org>
2005-12-19 21:36:13 +0000

    fix strict aliasing warnings

--- parts/outputviews/directorystatusmessagefilter.cpp
+++ parts/outputviews/directorystatusmessagefilter.cpp
@@ -43,40 +43,40 @@ void DirectoryStatusMessageFilter::processLine( const QString& line )
 bool DirectoryStatusMessageFilter::matchEnterDir( const QString& line, QString& dir )
 {
     // make outputs localised strings in Utf8 for entering/leaving directories...
-    static const unsigned short fr_enter[] =
+    static const QChar fr_enter[] =
         {'E','n','t','r','e',' ','d','a','n','s',' ','l','e',' ','r',0xe9,'p','e','r','t','o','i','r','e'
         };
-    static const unsigned short pl_enter[] =
+    static const QChar pl_enter[] =
         {'W','c','h','o','d','z',0x119,' ','k','a','t','a','l','o','g'
         };
-    static const unsigned short ja_enter[] =
+    static const QChar ja_enter[] =
         {
             0x5165,0x308a,0x307e,0x3059,0x20,0x30c7,0x30a3,0x30ec,0x30af,0x30c8,0x30ea
         };
-    static const unsigned short ko_enter[] =
+    static const QChar ko_enter[] =
         {
             0xb4e4,0xc5b4,0xac10
         };
-    static const unsigned short ko_behind[] =
+    static const QChar ko_behind[] =
         {
             0x20,0xb514,0xb809,0xd1a0,0xb9ac
         };
-    static const unsigned short pt_br_enter[] =
+    static const QChar pt_br_enter[] =
         {
             0x45,0x6e,0x74,0x72,0x61,0x6e,0x64,0x6f,0x20,0x6e,0x6f,0x20,0x64,0x69,0x72,0x65,0x74,0xf3,0x72,0x69,0x6f
         };
-    static const unsigned short ru_enter[] =
+    static const QChar ru_enter[] =
         {
             0x412,0x445,0x43e,0x434,0x20,0x432,0x20,0x43a,0x430,0x442,0x430,0x43b,0x43e,0x433
         };
 
-    static const QString fr_e( (const QChar*)fr_enter, sizeof(fr_enter) / 2 );
-    static const QString pl_e( (const QChar*)pl_enter, sizeof(pl_enter) / 2 );
-    static const QString ja_e( (const QChar*)ja_enter, sizeof(ja_enter) / 2 );
-    static const QString ko_e( (const QChar*)ko_enter, sizeof(ko_enter) / 2 );
-    static const QString ko_b( (const QChar*)ko_behind, sizeof(ko_behind) / 2 );
-    static const QString pt_br_e( (const QChar*)pt_br_enter, sizeof(pt_br_enter) / 2 );
-    static const QString ru_e( (const QChar*)ru_enter, sizeof(ru_enter) / 2 );
+    static const QString fr_e( fr_enter, sizeof(fr_enter) / 2 );
+    static const QString pl_e( pl_enter, sizeof(pl_enter) / 2 );
+    static const QString ja_e( ja_enter, sizeof(ja_enter) / 2 );
+    static const QString ko_e( ko_enter, sizeof(ko_enter) / 2 );
+    static const QString ko_b( ko_behind, sizeof(ko_behind) / 2 );
+    static const QString pt_br_e( pt_br_enter, sizeof(pt_br_enter) / 2 );
+    static const QString ru_e( ru_enter, sizeof(ru_enter) / 2 );
 	static const QString en_e("Entering directory");
 	static const QString de_e1("Wechsel in das Verzeichnis Verzeichnis");
 	static const QString de_e2("Wechsel in das Verzeichnis");
@@ -120,35 +120,35 @@ bool DirectoryStatusMessageFilter::matchEnterDir( const QString& line, QString& 
 // (locale and Utf8 aware).
 bool DirectoryStatusMessageFilter::matchLeaveDir( const QString& line, QString& dir )
 {
-    static const unsigned short fr_leave[] =
+    static const QChar fr_leave[] =
         { 'Q','u','i','t','t','e',' ','l','e',' ','r',0xe9,'p','e','r','t','o','i','r','e'
         };
-    static const unsigned short ja_leave[] =
+    static const QChar ja_leave[] =
         {
             0x51fa,0x307e,0x3059,0x20,0x30c7,0x30a3,0x30ec,0x30af,0x30c8,0x30ea
         };
-    static const unsigned short pt_br_leave[] =
+    static const QChar pt_br_leave[] =
         {'S','a','i','n','d','o',' ','d','o',' ','d','i','r','e','t',0xf3,'r','i','o'
         };
-    static const unsigned short ru_leave[] =
+    static const QChar ru_leave[] =
         {
             0x412,0x44b,0x445,0x43e,0x434,0x20,0x438,0x437,0x20,0x43a,0x430,0x442,0x430,0x43b,0x43e,0x433
         };
-    static const unsigned short ko_leave[] =
+    static const QChar ko_leave[] =
         {
             0xb098,0xac10
         };
-    static const unsigned short ko_behind[] =
+    static const QChar ko_behind[] =
         {
             0x20,0xb514,0xb809,0xd1a0,0xb9ac
         };
 
-    static const QString fr_l( (const QChar*)fr_leave, sizeof(fr_leave) / 2 );
-    static const QString ja_l( (const QChar*)ja_leave, sizeof(ja_leave) / 2 );
-    static const QString ko_l( (const QChar*)ko_leave, sizeof(ko_leave) / 2 );
-    static const QString ko_b( (const QChar*)ko_behind, sizeof(ko_behind) / 2 );
-    static const QString pt_br_l( (const QChar*)pt_br_leave, sizeof(pt_br_leave) / 2 );
-    static const QString ru_l( (const QChar*)ru_leave, sizeof(ru_leave) / 2 );
+    static const QString fr_l( fr_leave, sizeof(fr_leave) / 2 );
+    static const QString ja_l( ja_leave, sizeof(ja_leave) / 2 );
+    static const QString ko_l( ko_leave, sizeof(ko_leave) / 2 );
+    static const QString ko_b( ko_behind, sizeof(ko_behind) / 2 );
+    static const QString pt_br_l( pt_br_leave, sizeof(pt_br_leave) / 2 );
+    static const QString ru_l( ru_leave, sizeof(ru_leave) / 2 );
 	static const QString en_l("Leaving directory");
 	static const QString de_l1("Verlassen des Verzeichnisses Verzeichnis");
 	static const QString de_l2("Verlassen des Verzeichnisses");
