169c7aceb2d20c31735218f30e3e090a8e4919d7
Alexander Dymo <adymo@kdevelop.org>
2006-03-18 23:20:13 +0000

    Collect all parser tests in the single directory. Make tests compile with cmake (use -D KDE4_BUILD_TESTS=1). Tests can be run with "make test" or "ctest -V". Remove qmake project files.

--- cmake/modules/KDevelopMacros.cmake
+++ cmake/modules/KDevelopMacros.cmake
@@ -38,3 +38,37 @@ macro(kdevelop_add_app_templates _templateNames)
 
     endforeach(_templateName)
 endmacro(kdevelop_add_app_templates)
+
+
+
+macro(kdevelop_add_test)
+
+    qt4_get_moc_inc_dirs(_moc_INCS)
+
+    foreach(_source ${ARGN})
+        get_filename_component(_abs_file ${_source} ABSOLUTE)
+        get_filename_component(_baseName ${_abs_file} NAME_WE)
+
+        file(READ ${_abs_file} _contents)
+        string(REGEX MATCHALL "#include +[^ ]+\\.moc[\">]" _match "${_contents}")
+
+        if(_match)
+            foreach (_current_MOC_INC ${_match})
+
+                string(REGEX MATCH "[^ <\"]+\\.moc" _current_MOC "${_current_MOC_INC}")
+                set(_moc ${CMAKE_CURRENT_BINARY_DIR}/${_current_MOC})
+                add_custom_command(
+                    OUTPUT ${_moc}
+                    COMMAND ${QT_MOC_EXECUTABLE}
+                    ARGS ${_moc_INCS} ${CMAKE_CURRENT_SOURCE_DIR}/${_source} -o ${_moc}
+                    DEPENDS ${_abs_file}
+                )
+                MACRO_ADD_FILE_DEPENDENCIES(${_abs_FILE} ${_moc})
+                macro_additional_clean_files(${_moc})
+
+            endforeach (_current_MOC_INC)
+        endif(_match)
+    endforeach(_source)
+
+endmacro(kdevelop_add_test _sources)
+
