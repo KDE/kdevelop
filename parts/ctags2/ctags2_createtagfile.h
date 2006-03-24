120283da909024be54048aff722c4d91e31cdb36
Jens Dagerbo <jens.dagerbo@swipnet.se>
2006-03-24 20:28:05 +0000

    Adding license notice

--- parts/ctags2/ctags2_createtagfile.h
+++ parts/ctags2/ctags2_createtagfile.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2004 by Jens Dagerbo                                    *
+ *   Copyright (C) 2006 by Jens Dagerbo                                    *
  *   jens.dagerbo@swipnet.se                                               *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
--- parts/ctags2/ctags2_selecttagfile.h
+++ parts/ctags2/ctags2_selecttagfile.h
@@ -1,12 +1,13 @@
-// Description:
-//
-//
-// Author: The KDevelop Authors <kdevelop-devel@kdevelop.org>, (C) 2005
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
-
+/***************************************************************************
+ *   Copyright (C) 2006 by Jens Dagerbo                                    *
+ *   jens.dagerbo@swipnet.se                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef CTAGS2_SELECTTAGFILE_H
 #define CTAGS2_SELECTTAGFILE_H
@@ -17,14 +18,16 @@ class SelectTagFile : public SelectTagFileBase {
 	Q_OBJECT
 
 	public:
-    SelectTagFile( QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0 );
-    ~SelectTagFile();
+	SelectTagFile( QWidget* parent = 0, const char* name = 0, bool modal = FALSE, WFlags fl = 0 );
+	~SelectTagFile();
 	QString name();
 	QString tagsfilePath();
 
 public slots:
-    virtual void validate();
+	virtual void validate();
 };
 
 
 #endif
+
+// kate: space-indent off; indent-width 4; tab-width 4; show-tabs off;
