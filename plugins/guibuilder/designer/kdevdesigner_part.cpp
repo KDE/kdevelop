08ee0f33edc3bfb99cfd6570877d4471bd817a57
Matt Rogers <mattr@kde.org>
2006-04-01 06:33:42 +0000

    use KDevGenericFactory rather than a custom factory

--- plugins/guibuilder/designer/kdevdesigner_part.cpp
+++ plugins/guibuilder/designer/kdevdesigner_part.cpp
@@ -1,18 +1,26 @@
 
 #include "kdevdesigner_part.h"
-#include <kaboutdata.h>
+
+#include "kdevplugininfo.h"
+#include "kdevgenericfactory.h"
+
 #include <kinstance.h>
 #include <klocale.h>
 
-KInstance*  KDevDesignerPartFactory::s_instance = 0L;
-KAboutData* KDevDesignerPartFactory::s_about = 0L;
+typedef KDevGenericFactory<KDevDesignerPart> KDevDesignerFactory;
+static const KDevPluginInfo data( "kdevdesignerpart" );
+K_EXPORT_COMPONENT_FACTORY( kdevdesignerpart,
+                            KDevDesignerFactory( data ) );
 
-KDevDesignerPart::KDevDesignerPart(QWidget *parentWidget, QObject *parent, const char *name, const QStringList &)
+KDevDesignerPart::KDevDesignerPart(QWidget *parentWidget,
+                                   const char* widgetName,
+                                   QObject *parent,
+                                   const char *name, const QStringList&)
   : KParts::ReadWritePart(parent),
     m_parentWidget(parentWidget)
 {
   setObjectName(name);
-  setInstance(KDevDesignerPartFactory::instance());
+  setInstance(KDevDesignerFactory::instance());
 }
 
 KDevDesignerPart::~KDevDesignerPart()
@@ -29,45 +37,4 @@ bool KDevDesignerPart::saveFile()
   return false;
 }
 
-KDevDesignerPartFactory::KDevDesignerPartFactory()
-{
-}
-  
-KDevDesignerPartFactory::~KDevDesignerPartFactory()
-{
-  delete s_instance;
-  delete s_about;
-
-  s_instance = 0L;
-  s_about = 0L;
-}
-  
-KParts::Part* KDevDesignerPartFactory::createPartObject(QWidget *parentWidget, const char *widgetName,
-        QObject *parent, const char *name,
-        const char *classname, const QStringList &args)
-{
-  if (KDevDesignerPart* part = new KDevDesignerPart(parentWidget, parent, name, args))
-    {
-      if (QLatin1String(classname) == QLatin1String("KParts::ReadOnlyPart"))
-          part->setReadWrite(false);
-
-      return part;
-    }
-
-  return 0;
-}
-
-KInstance* KDevDesignerPartFactory::instance()
-{
-  if (s_instance == 0)
-    {
-      s_about = new KAboutData("kdevdesignerpart", I18N_NOOP("KDevDesignerPart"), "0.1");
-      s_about->addAuthor("Roberto Raggi", 0, "roberto@kdevelop.org");
-      s_about->addAuthor("Trolltech AS", 0, "info@trolltech.com");
-      s_instance = new KInstance(s_about);
-    }
-
-  return s_instance;
-}
-  
 #include "kdevdesigner_part.moc"
--- plugins/guibuilder/designer/kdevdesigner_part.h
+++ plugins/guibuilder/designer/kdevdesigner_part.h
@@ -8,7 +8,8 @@ class KDevDesignerPart: public KParts::ReadWritePart
 {
   Q_OBJECT
 public:
-  KDevDesignerPart(QWidget *parentWidget, QObject *parent, const char *name, const QStringList &args);
+  KDevDesignerPart(QWidget *parentWidget, const char* widgetName, 
+                   QObject *parent, const char *name, const QStringList &args);
   virtual ~KDevDesignerPart();
 
 protected:
@@ -19,26 +20,6 @@ private:
   QWidget *m_parentWidget;
 };
 
-class KAboutData;
-class KInstance;
 
-
-class KDevDesignerPartFactory : public KParts::Factory
-{
-  Q_OBJECT
-public:
-  KDevDesignerPartFactory();
-  virtual ~KDevDesignerPartFactory();
-
-  virtual KParts::Part* createPartObject(QWidget *parentWidget, const char *widgetName,
-        QObject *parent, const char *name,
-        const char *classname, const QStringList &args);
-
-  static KInstance* instance();
-
-private:
-  static KInstance* s_instance;
-  static KAboutData* s_about;
-};
 
 #endif // KDEVDESIGNER_PART_H
