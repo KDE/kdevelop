8f94ce0a383e3d191713eb80510e08bcd0d6b6a7
Roberto Raggi <roberto@kdevelop.org>
2006-04-02 16:21:55 +0000

    nicer example

--- languages/cpp/parser/rpp/test/t006.cpp
+++ languages/cpp/parser/rpp/test/t006.cpp
@@ -1,10 +1,9 @@
-/* a comment
-   here
-   here
-*/
-#define MY_COOL_MACRO \
-  line1 \
-  line2 \
-  line3 \
-  line4
-MY_COOL_MACRO MY_COOL_MACRO
+
+#define code(more) \
+ int main () \
+ { more \
+ }
+
+code(int x;
+     int y)
+
