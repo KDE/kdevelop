580eb3b5846b9ab8240f288b0eb1e68fe09342f7
Matt Rogers <mattr@kde.org>
2006-04-11 00:16:30 +0000

    add support for reading TOC from CHM documentation.

--- parts/documentation/plugins/chm/docchmplugin.cpp
+++ parts/documentation/plugins/chm/docchmplugin.cpp
@@ -24,9 +24,13 @@
 #include <kconfig.h>
 #include <klocale.h>
 #include <klistview.h>
-
+#include <kio/netaccess.h>
+#include <iostream>
+#include <fstream>
+#include <qvaluevector.h>
 #include <kdevgenericfactory.h>
 #include <kdevplugininfo.h>
+#include <qregexp.h>
 
 #include "../../../../config.h"
 
@@ -38,7 +42,7 @@ DocCHMPlugin::DocCHMPlugin(QObject* parent, const char* name, QStringList // arg
                            )
     :DocumentationPlugin(DocCHMPluginFactory::instance()->config(), parent, name)
 {
-    setCapabilities(CustomDocumentationTitles);
+    setCapabilities(CustomDocumentationTitles); // | Index | FullTextSearch | ProjectDocumentation
     autoSetup();
 }
 
@@ -46,10 +50,12 @@ DocCHMPlugin::~DocCHMPlugin()
 {
 }
 
+
 DocumentationCatalogItem* DocCHMPlugin::createCatalog(KListView* contents, const QString& title, const QString& url)
 {
     DocumentationCatalogItem *item = new DocumentationCatalogItem(this, contents, title);
     item->setURL(KURL(url));
+    
     return item;
 }
 
@@ -69,8 +75,36 @@ QString DocCHMPlugin::pluginName() const
     return i18n("CHM Documentation Collection");
 }
 
+
+QString DocCHMPlugin::getSpecialData(const QString& name, KURL url) {
+    QString ret = "";
+    url.setProtocol("ms-its");
+    
+    url.addPath("/:" + name);
+    QString tmpFile;
+    if( KIO::NetAccess::download( url, tmpFile, 0 ) )
+    {
+        std::filebuf fb;
+        fb.open (tmpFile.ascii(), std::ios::in);
+        std::istream is(&fb);
+        char buf[5000] = " ";
+        while(is.good()) {
+            is.getline(buf, 5000);
+            ret += buf; ret += "\n";
+        }
+        fb.close();
+        KIO::NetAccess::removeTempFile( tmpFile );
+    } else {
+        kdDebug(9002) << "DocCHMPlugin::getSpecialData could not download data from " << url.url() << endl;
+    }
+    return ret;
+}
+
+
+///this currently is useless, because htdig cannot use kioslaves
 QStringList DocCHMPlugin::fullTextSearchLocations()
 {
+    //return QStringList::split( '\n', getSpecialData("contents", m_url));
     return QStringList();
 }
 
@@ -92,9 +126,64 @@ void DocCHMPlugin::createIndex(IndexBox* // index
     return;
 }
 
-void DocCHMPlugin::createTOC(DocumentationCatalogItem* // item
-                             )
+
+static KListViewItem* chainEnd(KListViewItem *parent) {
+    if(parent == 0) return 0;
+    KListViewItem* ret = dynamic_cast<KListViewItem*>(parent->firstChild());
+    if(ret == 0) return 0;
+    while(ret->nextSibling() != 0) {
+        ret = dynamic_cast<KListViewItem*>(ret->nextSibling());
+    }
+    return ret;
+}
+
+static QString decodeHTML(const QString& s) {
+    QRegExp rx(QString::fromLatin1("&#(\\d+);|&nbsp;"));
+    QString out = s;
+    int pos = rx.search(out);
+    while(pos > -1) {
+        QString found = rx.cap(0);
+        if(found != "&nbsp;") {
+            out.replace(pos, found.length(), static_cast<char>(rx.cap(1).toInt()));
+        }else{
+            out.replace(pos, found.length(), " ");
+        }
+        pos = rx.search(out, pos+1);
+    }
+    return out;
+}
+
+
+void DocCHMPlugin::createTOC(DocumentationCatalogItem* item)
 {
+    QStringList lines = QStringList::split("\n", getSpecialData("catalog", item->url()) );
+    if(lines.count() % 4 != 0) { kdDebug(9002) << "DocCHMPlugin::createTOC: wrong count of strings"; return;}
+    
+    QValueVector<DocumentationItem*> items;
+    items.push_back(item);
+    for(QStringList::Iterator it = lines.begin(); it != lines.end();) {
+        bool ok1 = true, ok2 = true;
+        int parent = (*it).toInt(&ok1);
+        ++it;
+        int current = (*it).toInt(&ok2);
+        ++it;
+        if(int(items.size()) != current || !ok1 || !ok2 || parent < 0 || parent >= int(items.size()) || current < 0 || current != int(items.size())) {
+            kdDebug(9002) << "DocCHMPlugin::createTOC error while parsing output of ioslave" << endl;
+            break;
+        }
+        
+        QString& name(*it);
+        ++it;
+        KURL url(*it);
+        ++it;
+        
+        items.push_back(new DocumentationItem(
+                DocumentationItem::Document, items[parent], chainEnd(items[parent]), decodeHTML(name)));
+        items[current]->setURL(url);
+        if(parent != 0) items[parent]->setType(DocumentationItem::Book);
+    }
+    
+
     return;
 }
 
