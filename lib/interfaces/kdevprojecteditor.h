81f03581969752f6620a17840a701574c18092f2
Matt Rogers <mattr@kde.org>
2006-04-28 19:03:48 +0000

    add docu

--- lib/interfaces/kdevprojecteditor.h
+++ lib/interfaces/kdevprojecteditor.h
@@ -50,15 +50,72 @@ public:
 
     virtual Features features() const = 0;
 
+    /**
+     * Add a folder to the project
+     *
+     * Adds the folder specified by @p folder to @p parent and modifies the
+     * underlying build system if needed
+     */
     virtual bool addFolder(KDevProjectFolderItem *folder, KDevProjectFolderItem *parent) = 0;
+    
+    /**
+     * Add a target to a folder
+     * 
+     * Adds the target specified by @p target to the folder @p parent and
+     * modifies the underlying build system if needed
+     */
     virtual bool addTarget(KDevProjectTargetItem *target, KDevProjectFolderItem *parent) = 0;
+    
+    /**
+     * Add a file to a folder
+     * 
+     * Adds the file specified by @p file to the folder @p parent and modifies
+     * the underlying build system if needed. The file is not added to a target
+     */
     virtual bool addFile(KDevProjectFileItem *file, KDevProjectFolderItem *parent) = 0;
+
+    /**
+     * Add a file to a target
+     * 
+     * Adds the file specified by @pfile to the target @p parent and modifies
+     * the underlying build system if needed.
+     */
     virtual bool addFile(KDevProjectFileItem *file, KDevProjectTargetItem *parent) = 0;
 
+    /**
+     * Remove a folder from the project
+     * 
+     * Removes the folder specified by @p folder from folder @p parent and
+     * modifies the underlying build system if needed.
+     */
     virtual bool removeFolder(KDevProjectFolderItem *folder, KDevProjectFolderItem *parent) = 0;
+
+    /**
+     * Remove a target from a folder
+     * 
+     * Removes the target specified by @p target from the folder @p parent and
+     * modifies the underlying build system if needed.
+     */
     virtual bool removeTarget(KDevProjectTargetItem *target, KDevProjectFolderItem *parent) = 0;
+
+    /**
+     * Remove a file from a folder
+     * 
+     * Removes the file specified by @p file from the folder @p parent and 
+     * modifies the underlying build system if needed. If the file being 
+     * removed is also part of a target, it should be removed from the target as well
+     */
     virtual bool removeFile(KDevProjectFileItem *file, KDevProjectFolderItem *parent) = 0;
+
+    /**
+     * Remove a file from a target
+     * 
+     * Removes the file specified by @p file from the folder @parent and
+     * modifies the underlying build system if needed. The file is not removed
+     * from the folder it is in
+     */
     virtual bool removeFile(KDevProjectFileItem *file, KDevProjectTargetItem *parent) = 0;
 };
 
 #endif
+//kate: space-indent on; indent-width 4; auto-insert-doxygen on; indent-mode cstyle;
