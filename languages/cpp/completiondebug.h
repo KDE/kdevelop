13c2e66a0bac58d81d2b171cb8694e44c435fde5
David Nolden <david.nolden.kde@art-master.de>
2006-05-25 21:00:26 +0000

    revert my bad change

--- languages/cpp/completiondebug.h
+++ languages/cpp/completiondebug.h
@@ -1,9 +1,7 @@
-
 /***************************************************************************
    copyright            : (C) 2006 by David Nolden
    email                : david.nolden.kdevelop@art-master.de
 ***************************************************************************/
-
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -12,109 +10,87 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-
 #ifndef __COMPLETIONDEBUG_H__
 #define __COMPLETIONDEBUG_H__
-
 #include <qstringlist.h>
 #include <kdebug.h>
-
 namespace CompletionDebug {
-
-/**A helper-class for debugging, which can be used to prevent too deep recursion
-   and to format the output nicely with a prefix for each function that is entered */
 template <class StreamType>
-class KDDebugState {
-private:
- StreamType m_stream;
- QStringList m_prefixStack;
- QString currPrefix;
+  class KDDebugState {
+  private:
+  StreamType m_stream;
+  QStringList m_prefixStack;
  
- void computePrefix() {
-	 currPrefix = "";
-	 for( QStringList::iterator it = m_prefixStack.begin(); it != m_prefixStack.end() ; ++it )
-		 currPrefix += *it;
- }
-public:
- typedef StreamType KStreamType;
- KDDebugState();
+  public:
+  typedef StreamType KStreamType;
+  KDDebugState();
  
- KDDebugState( StreamType stream ) : m_stream( stream ) {
- }
+  KDDebugState( StreamType stream ) : m_stream( stream ) {
+  }
  
- void push( const QString & txt ) {
-	 currPrefix += txt;
-	 m_prefixStack.push_back( txt );
- }
+  void push( const QString & txt ) {
+    m_prefixStack.push_back( txt );
+  }
  
- void pop() {
-	 currPrefix.truncate( currPrefix.length() - m_prefixStack.back().length() );
-	 m_prefixStack.pop_back();
-	 computePrefix();
- };
+  void pop() {
+    m_prefixStack.pop_back();
+  };
  
- StreamType& dbg() {
-	 m_stream << currPrefix;
-	 return m_stream;
- }
+  StreamType& dbg() {
+    for( QStringList::iterator it = m_prefixStack.begin(); it != m_prefixStack.end() ; ++it )
+      m_stream << *it;
+  
+    return m_stream;
+  }
  
- int depth() {
-	 return m_prefixStack.size();
- }
+  int depth() {
+    return m_prefixStack.size();
+  }
 };
-
 #ifndef NDEBUG 
 template<>
   KDDebugState<kdbgstream>::KDDebugState();
 #endif
-
 template<>
   KDDebugState<kndbgstream>::KDDebugState();
-
 #if defined(VERBOSE) && !defined(NDEBUG)
-typedef KDDebugState<kdbgstream> DBGStreamType;
+ typedef KDDebugState<kdbgstream> DBGStreamType;
 #else
-typedef KDDebugState<kndbgstream> DBGStreamType;
+ typedef KDDebugState<kndbgstream> DBGStreamType;
 #endif
-
-///Class to help indent the debug-output correctly
-extern DBGStreamType dbgState;
-
-extern const int completionMaxDepth;
-
-class Debug {
-private:
- DBGStreamType& m_state;
-public:
- Debug( const QString& prefix = "#", DBGStreamType& st = dbgState ) : m_state( st ) {
-  m_state.push( prefix );
+ ///Class to help indent the debug-output correctly
+ extern DBGStreamType dbgState;
+ extern const int completionMaxDepth;
+ class Debug {
+ private:
+   DBGStreamType& m_state;
+ public:
+   Debug( const QString& prefix = "#", DBGStreamType& st = dbgState ) : m_state( st ) {
+     m_state.push( prefix );
+   };
+   ~Debug() {
+     m_state.pop();
+   }
+ 
+   DBGStreamType::KStreamType& dbg() {
+     return m_state.dbg();
+   }
+ 
+   int depth() {
+     return m_state.depth();
+   }
+ 
+   operator bool() {
+     bool r = depth() < completionMaxDepth;
+  
+     if( !r )
+       dbg() << "recursion is too deep";
+  
+     return r;
+   }
  };
- ~Debug() {
-  m_state.pop();
- }
- 
- DBGStreamType::KStreamType& dbg() {
-  return m_state.dbg();
- }
- 
- int depth() {
-  return m_state.depth();
- }
- 
- operator bool() {
-  bool r = depth() < completionMaxDepth;
-  
-  if( !r )
-   dbg() << "recursion is too deep";
-  
-  return r;
- }
-};
-
-DBGStreamType::KStreamType& dbg();
-
-DBGStreamType::KStreamType& dbgMajor();
-
+ DBGStreamType::KStreamType& dbg();
+ DBGStreamType::KStreamType& dbgMajor();
 }
 #endif 
 // kate: indent-mode csands; tab-width 4;
