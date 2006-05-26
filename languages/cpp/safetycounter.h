5ca0ee4f8c9d915adc03e754e4c922b87af7ea0b
Andras Mantia <amantia@kde.org>
2006-05-26 14:07:23 +0000

    Little cleanup.

--- languages/cpp/safetycounter.h
+++ languages/cpp/safetycounter.h
@@ -16,12 +16,14 @@
 #ifndef __SAFETYCOUNTER_H__
 #define __SAFETYCOUNTER_H__
 
+#include <kdebug.h>
+
 struct SafetyCounter {
   int safetyCounter;
   const int maxSafetyCounter;
   
-SafetyCounter( int max = 10000 ) : safetyCounter(0), maxSafetyCounter(max) {
-}
+  SafetyCounter( int max = 10000 ) : safetyCounter(0), maxSafetyCounter(max) {
+  }
   
   void init() {
     safetyCounter = 0;
@@ -35,11 +37,10 @@ SafetyCounter( int max = 10000 ) : safetyCounter(0), maxSafetyCounter(max) {
   operator bool() {
     safetyCounter++;
     bool ret = safetyCounter < maxSafetyCounter;
-	if( !ret ) {
-        kdDebug( 9007) << "WARNING: Safety-depth-counter reached count > " << maxSafetyCounter << ", operation stopped" << endl;
-		if( safetyCounter == maxSafetyCounter ) kdDebug( 9007 ) << endl << kdBacktrace() << endl;
-	}
-		
+	  if( !ret ) {
+      kdDebug( 9007) << "WARNING: Safety-depth-counter reached count > " << maxSafetyCounter << ", operation stopped" << endl;
+		  if( safetyCounter == maxSafetyCounter ) kdDebug( 9007 ) << endl << kdBacktrace() << endl;
+	  }
     
     return ret;
   }
