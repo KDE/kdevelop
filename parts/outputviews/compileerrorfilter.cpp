7d72700f101c2ecbb6d6ac6c36276a4802b153be
David Nolden <david.nolden.kde@art-master.de>
2006-08-08 02:31:41 +0000

    also detect warning-messages in german

--- parts/outputviews/compileerrorfilter.cpp
+++ parts/outputviews/compileerrorfilter.cpp
@@ -89,7 +89,8 @@ void CompileErrorFilter::processLine( const QString& line )
 	                lineNum = regExp.cap( format->lineGroup ).toInt() - 1;
 	                text    = regExp.cap( format->textGroup );
 			compiler = format->compiler;
-			if (regExp.cap(3).contains("warning", false))
+			QString cap = regExp.cap(3);
+			if (cap.contains("warning", false) || cap.contains("Warnung", false))
 				isWarning = true;
 			if (regExp.cap(3).contains("instantiated from", false)) {
 				isInstantiationInfo = true;
