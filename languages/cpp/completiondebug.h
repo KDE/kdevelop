66274fc9fbe255b229203ef2e76566974e2be112
David Nolden <david.nolden.kde@art-master.de>
2006-09-07 19:17:34 +0000

    only warn and print the backtrace once for each completion when the maximum-depth or maximum-count is reached(because the max-deth may be reached thousands of times afterwards, which can lock up the GUI). Now we can deal with classes inheriting themselves hundreds of times without problems. :)

--- languages/cpp/completiondebug.h
+++ languages/cpp/completiondebug.h
@@ -1,7 +1,7 @@
 /***************************************************************************
-   copyright            : (C) 2006 by David Nolden
-   email                : david.nolden.kdevelop@art-master.de
-***************************************************************************/
+copyright            : (C) 2006 by David Nolden
+email                : david.nolden.kdevelop@art-master.de
+***************************************************************************/ 
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -13,10 +13,12 @@
 #ifndef __COMPLETIONDEBUG_H__
 #define __COMPLETIONDEBUG_H__
 
-///With verbose shut on, the whole type-resolution-process is nicely traced for easy debugging.
+///With verbose shut on, the whole type-resolution-process is nicely traced for easy debugging(at cost of speed).
 //#define VERBOSE
 #define VERBOSEMAJOR
-#define DEPTHBACKTRACE
+
+///When defined, a backtrace is printed the first time the maximum depth is reached for the first time.
+#define DEPTHBACKTRACE 
 //#define NOFULLVERBOSE
 
 #include <qstringlist.h>
@@ -24,161 +26,174 @@
 
 namespace CompletionDebug {
 template <class StreamType>
-  class KDDebugState {
+class KDDebugState {
   private:
-  StreamType m_stream;
-  kndbgstream m_nstream;
-  QStringList m_prefixStack;
-	int m_counter;
-	int m_depth;
-	bool m_enabled;
-	  
+    StreamType m_stream;
+    kndbgstream m_nstream;
+    QStringList m_prefixStack;
+    int m_counter;
+    int m_depth;
+    bool m_enabled;
+		bool m_hadWarning;
+
   public:
-  typedef StreamType KStreamType;
-  KDDebugState();
- 
-  KDDebugState( StreamType stream ) : m_stream( stream ), m_counter(0), m_depth(0), m_enabled(true) {
-  }
- 
-  void push( const QString & txt ) {
+    typedef StreamType KStreamType;
+    KDDebugState();
+
+		KDDebugState( StreamType stream ) : m_stream( stream ), m_counter( 0 ), m_depth( 0 ), m_enabled( true ), m_hadWarning( false ) {}
+
+    void push( const QString & txt ) {
       m_prefixStack.push_back( txt );
-	  pushDepth();
-  }
- 
-  void pop() {
-      m_prefixStack.pop_back();
-	  popDepth();
-  };
-
-  inline void pushDepth() {
-  	m_depth++;
-  }
-	
-  inline void popDepth() {
-  	m_depth--;
-  }
- 
-  StreamType& dbg() {
-#ifndef VERBOSE
-    if( !m_enabled ) return m_nstream;
-#endif
-    m_stream << "(" << m_counter << ")";
-    for( QStringList::iterator it = m_prefixStack.begin(); it != m_prefixStack.end() ; ++it )
-      m_stream << *it;
-
-	  m_counter++;
-	  return m_stream;
-  }
-
-	void setState( bool enabled ) {
-		m_enabled = enabled;
-	}
-
-	  bool state() {
-		  return m_enabled;
-	  }
-	  
-#ifndef NDEBUG
-  void outputPrefix( kdbgstream& target ) {
-    target << "(" << m_counter << ")";
-    for( QStringList::iterator it = m_prefixStack.begin(); it != m_prefixStack.end() ; ++it )
-      target << *it;
-    
-    m_counter++;
-  }
-#endif
-    
-  void clearCounter() {
-      m_counter = 0;
-  }
-	  
-  int depth() {
-    return m_depth;
-  }
-};
-#ifndef NDEBUG 
-template<>
-  KDDebugState<kdbgstream>::KDDebugState();
-#endif
-template<>
-  KDDebugState<kndbgstream>::KDDebugState();
-#if defined(VERBOSE) && !defined(NDEBUG) && !defined(NOFULLVERBOSE)
- typedef KDDebugState<kdbgstream> DBGStreamType;
-#else
- typedef KDDebugState<kndbgstream> DBGStreamType;
-#endif
- ///Class to help indent the debug-output correctly
- extern DBGStreamType dbgState;
- extern const int completionMaxDepth;
- class LogDebug {
- private:
-   DBGStreamType& m_state;
-   int m_max;
- public:
- LogDebug( const QString& prefix = "#", int max = completionMaxDepth, DBGStreamType& st = dbgState ) : m_state( st ), m_max( max ) {
-     m_state.push( prefix );
-   };
-   ~LogDebug() {
-     m_state.pop();
-   }
- 
-   DBGStreamType::KStreamType& dbg() {
-     return m_state.dbg();
-   }
- 
-   int depth() {
-     return m_state.depth();
-   }
- 
-   operator bool() {
-     bool r = depth() < m_max;
-  
-	if( !r ) {
-      dbg() << "recursion is too deep" << endl;
-#ifdef DEPTHBACKTRACE
-	   kdDebug( 9007 ) << kdBacktrace() << endl;
-#endif
-	}
-  
-     return r;
-   }
- };
-
-
-///does not care about the logging, but still counts the depth
-  class DepthDebug {
-    DBGStreamType& m_state;
-	int m_max;
-  public:
-  	DepthDebug( const QString& prefix = "#", int max = completionMaxDepth, DBGStreamType& st = dbgState ) : m_state( st ), m_max( max ) {
-      Q_UNUSED( prefix );
-	  	m_state.pushDepth();
-    };
-	  ~DepthDebug() {
-	  	m_state.popDepth();
-	  }
-    
-    kndbgstream dbg() {
-      return kndDebug();
+      pushDepth();
     }
-  
+
+    void pop() {
+      m_prefixStack.pop_back();
+      popDepth();
+    };
+
+    inline void pushDepth() {
+      m_depth++;
+    }
+
+    inline void popDepth() {
+      m_depth--;
+    }
+
+		bool hadWarning() {
+			return m_hadWarning;
+		}
+
+		void setHadWarning( bool had ) {
+			m_hadWarning = had;
+		}
+
+    StreamType& dbg() {
+#ifndef VERBOSE
+      if ( !m_enabled )
+        return m_nstream;
+#endif
+
+      m_stream << "(" << m_counter << ")";
+      for ( QStringList::iterator it = m_prefixStack.begin(); it != m_prefixStack.end() ; ++it )
+        m_stream << *it;
+
+      m_counter++;
+      return m_stream;
+    }
+
+    void setState( bool enabled ) {
+      m_enabled = enabled;
+    }
+
+    bool state() {
+      return m_enabled;
+    }
+
+#ifndef NDEBUG
+    void outputPrefix( kdbgstream& target ) {
+      target << "(" << m_counter << ")";
+      for ( QStringList::iterator it = m_prefixStack.begin(); it != m_prefixStack.end() ; ++it )
+        target << *it;
+
+      m_counter++;
+    }
+#endif
+
+    void clearCounter() {
+      m_counter = 0;
+    }
+
+    int depth() {
+      return m_depth;
+    }
+};
+#ifndef NDEBUG
+template <>
+KDDebugState<kdbgstream>::KDDebugState();
+#endif
+template <>
+KDDebugState<kndbgstream>::KDDebugState();
+#if defined(VERBOSE) && !defined(NDEBUG) && !defined(NOFULLVERBOSE)
+
+typedef KDDebugState<kdbgstream> DBGStreamType;
+#else
+
+typedef KDDebugState<kndbgstream> DBGStreamType;
+#endif 
+///Class to help indent the debug-output correctly
+extern DBGStreamType dbgState;
+extern const int completionMaxDepth;
+class LogDebug {
+  private:
+    DBGStreamType& m_state;
+    int m_max;
+  public:
+    LogDebug( const QString& prefix = "#", int max = completionMaxDepth, DBGStreamType& st = dbgState ) : m_state( st ), m_max( max ) {
+      m_state.push( prefix );
+    };
+    ~LogDebug() {
+      m_state.pop();
+    }
+
+    DBGStreamType::KStreamType& dbg() {
+      return m_state.dbg();
+    }
+
     int depth() {
       return m_state.depth();
     }
-    
+
     operator bool() {
       bool r = depth() < m_max;
-      
-	if( !r ) {
-        kdDebug( 9007 ) << "recursion is too deep" << endl;
+
+	    if ( !r && !m_state.hadWarning() ) {
+		    m_state.setHadWarning( true );
+		    dbg() << "recursion is too deep" << endl;
 #ifdef DEPTHBACKTRACE
-		kdDebug( 9007 ) << kdBacktrace() << endl;
+        kdDebug( 9007 ) << kdBacktrace() << endl;
 #endif
-	}
-      
+      }
       return r;
     }
-  };
+};
+
+
+///does not care about the logging, but still counts the depth
+class DepthDebug {
+    DBGStreamType& m_state;
+    int m_max;
+  public:
+    DepthDebug( const QString& prefix = "#", int max = completionMaxDepth, DBGStreamType& st = dbgState ) : m_state( st ), m_max( max ) {
+      Q_UNUSED( prefix );
+      m_state.pushDepth();
+    };
+
+    ~DepthDebug() {
+      m_state.popDepth();
+    }
+
+    kndbgstream dbg() {
+      return kndDebug();
+    }
+
+    int depth() {
+      return m_state.depth();
+    }
+
+    operator bool() {
+      bool r = depth() < m_max;
+
+	    if ( !r && !m_state.hadWarning() ) {
+		    m_state.setHadWarning( true );
+		    dbg() << "recursion is too deep" << endl;
+#ifdef DEPTHBACKTRACE
+		    kdDebug( 9007 ) << kdBacktrace() << endl;
+#endif
+	    }
+	    return r;
+    }
+};
 
 #ifndef VERBOSEMAJOR
 #define ifVerboseMajor(x) /**/
@@ -188,12 +203,14 @@ template<>
 
 
 #ifdef VERBOSE
- typedef LogDebug Debug;
- DBGStreamType::KStreamType& dbg();
+
+typedef LogDebug Debug;
+DBGStreamType::KStreamType& dbg();
 #define ifVerbose(x) x
 #else
- DBGStreamType::KStreamType& dbg();
- typedef DepthDebug Debug;
+
+DBGStreamType::KStreamType& dbg();
+typedef DepthDebug Debug;
 #define ifVerbose(x) /**/
 #endif
 #ifndef NDEBUG
@@ -203,4 +220,4 @@ kndbgstream dbgMajor();
 #endif
 }
 #endif 
-// kate: indent-mode csands; tab-width 4;
+// kate: indent-mode csands; tab-width 2;
