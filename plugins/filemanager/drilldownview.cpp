b287e07704b1e5df4d0ca96c5b4477d4c8f07c55
Alexander Dymo <adymo@kdevelop.org>
2006-09-26 18:38:17 +0000

    Reintroduced animations. Fixed FSF address.

--- plugins/filemanager/drilldownview.cpp
+++ plugins/filemanager/drilldownview.cpp
@@ -14,10 +14,10 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
  *   GNU General Public License for more details.                          *
  *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
  *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
 #include "drilldownview.h"
 
@@ -27,6 +27,7 @@ DrillDownView::DrillDownView(QWidget *parent)
     :QListView(parent)
 {
     connect(&animation, SIGNAL(frameChanged(int)), this, SLOT(slide(int)));
+    connect(&animation, SIGNAL(finished()), this, SLOT(update()));
     animation.setDuration(200);
 }
 
@@ -57,74 +58,56 @@ void DrillDownView::slide(int x)
     viewport()->scroll(lastPosition - x, 0);
     lastPosition = x;
 }
-/*
-QModelIndex DrillDownView::moveCursor(CursorAction cursorAction, Qt::KeyboardModifiers modifiers)
-{
-    kDebug() << k_funcinfo << endl;
-    if (animation.state() == QTimeLine::Running)
-        return QListView::moveCursor(cursorAction, modifiers);
-    QModelIndex current = currentIndex();
-    if (cursorAction == MoveLeft && current.parent().isValid())
-    {
-        oldView = QPixmap::grabWidget(viewport());
-        return current.parent();
-    }
-    if (cursorAction == MoveRight && model() && model()->hasChildren(current))
-    {
-        kDebug() << "moving right" << endl;
-        oldView = QPixmap::grabWidget(viewport());
-//         model()->fetchMore(current);
-        return model()->index(0, 0, current);
-    }
-    return QListView::moveCursor(cursorAction, modifiers);
-}
 
-void DrillDownView::currentChanged(const QModelIndex &current, const QModelIndex &previous)
-{
-    kDebug() << k_funcinfo << endl;
-    if ((current.isValid() && previous.isValid())
-        && (current.parent() == previous || previous.parent() == current))
-    {
-        setUpdatesEnabled(false);
-        setRootIndex(currentIndex().parent());
-        setCurrentIndex(currentIndex());
-        executeDelayedItemsLayout();
-        // Force the hiding/showing of scrollbars
-        setVerticalScrollBarPolicy(verticalScrollBarPolicy());
-        newView = QPixmap::grabWidget(viewport());
-        setUpdatesEnabled(true);
-        int length = qMax(oldView.width(), newView.width());
-        lastPosition = (previous.parent() == current) ? length : 0;
-        animation.setFrameRange(0, length);
-        animation.stop();
-        animation.setDirection(previous.parent() == current ? QTimeLine::Backward : QTimeLine::Forward);
-        animation.start();
-    }
-    else
-    {
-        QListView::currentChanged(current, previous);
-    }
-}
-*/
 void DrillDownView::keyPressEvent(QKeyEvent *event)
 {
     QModelIndex current = currentIndex();
     if (current.isValid())
     {
+        Qt::Key moveDirection = Qt::Key_Any;
         if ((event->key() == Qt::Key_Right) && (model()->hasChildren(current)))
-        {
-            setUpdatesEnabled(false);
-            setRootIndex(current);
-            if (model()->canFetchMore(current))
-                model()->fetchMore(current);
-            setUpdatesEnabled(true);
-        }
+            moveDirection = Qt::Key_Right;
         else if (event->key() == Qt::Key_Left)
+            moveDirection = Qt::Key_Left;
+
+        if (moveDirection != Qt::Key_Any)
         {
+            //eat event if animation is running
+            if (animation.state() == QTimeLine::Running)
+                return;
+
+            bool animate = true;
             setUpdatesEnabled(false);
-            QModelIndex root = rootIndex();
-            if (root.isValid())
-                setRootIndex(root.parent());
+            if (moveDirection == Qt::Key_Right)
+            {
+                setRootIndex(current);
+                if (model()->canFetchMore(current))
+                {
+                    animate = false;
+                    model()->fetchMore(current);
+                }
+            }
+            else
+            {
+                QModelIndex root = rootIndex();
+                if (root.isValid())
+                    setRootIndex(root.parent());
+            }
+            if (animate)
+            {
+                executeDelayedItemsLayout();
+                // Force the hiding/showing of scrollbars
+                setVerticalScrollBarPolicy(verticalScrollBarPolicy());
+                newView = QPixmap::grabWidget(viewport());
+                setUpdatesEnabled(true);
+                int length = qMax(oldView.width(), newView.width());
+                lastPosition = moveDirection == Qt::Key_Left ? length : 0;
+                animation.setFrameRange(0, length);
+                animation.stop();
+                animation.setDirection(moveDirection == Qt::Key_Right ?
+                    QTimeLine::Forward : QTimeLine::Backward);
+                animation.start();
+            }
             setUpdatesEnabled(true);
         }
     }
