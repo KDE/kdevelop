17cc632989f748fa12a75ccb585953940d0db4ba
Jakob Petsovits <jpetso@gmx.at>
2006-09-26 17:13:53 +0000

    Add a module for finding Flex, and only build the java, csharp, and ruby language parts if a minimum version of Flex is required (which supports the C++ class lexers which are not working in the very old Flex versions).
    Thanks to Alex Neundorf for assisting me on that! :)
    
    I set the minimum version to 2.5.31 which is the minimum
    version that I know is working. If you know that a lower
    version is working, please replace the required version
    string in languages/CMakeLists.txt.

---
+++ cmake/modules/FindFlex.cmake
@@ -0,0 +1,32 @@
+# - Try to find Flex
+# Once done this will define
+#
+#  FLEX_FOUND - system has Flex
+#  FLEX_EXECUTABLE - path of the flex executable
+#  FLEX_VERSION - the output of "flex --version", like "flex 2.5.31\n"
+#
+
+
+FIND_PROGRAM(FLEX_EXECUTABLE NAMES flex)
+
+INCLUDE(MacroEnsureVersion)
+
+IF(FLEX_EXECUTABLE)
+    SET(FLEX_FOUND TRUE)
+
+    EXECUTE_PROCESS(COMMAND ${FLEX_EXECUTABLE} --version
+        OUTPUT_VARIABLE FLEX_VERSION
+    )
+#    MESSAGE(STATUS "Flex version: ${FLEX_VERSION}")
+ENDIF(FLEX_EXECUTABLE)
+
+IF(FLEX_FOUND)
+  IF(NOT Flex_FIND_QUIETLY)
+    MESSAGE(STATUS "Found Flex: ${FLEX_EXECUTABLE}")
+  ENDIF(NOT Flex_FIND_QUIETLY)
+ELSE(FLEX_FOUND)
+  IF(Flex_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "Could not find Flex")
+  ENDIF(Flex_FIND_REQUIRED)
+ENDIF(FLEX_FOUND)
+
