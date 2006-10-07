4f05ff7816b3cdf2e783f04cfaced363570be19c
Andreas Pakulat <apaku@gmx.de>
2006-10-07 22:10:11 +0000

    - print out the AST to stdout after trying to parse, this makes runner easier to use than the viewer for simple tests

--- buildtools/lib/parsers/qmake/tests/runner.cpp
+++ buildtools/lib/parsers/qmake/tests/runner.cpp
@@ -18,13 +18,116 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 #include <qmakeast.h>
+#include <qmakeastvisitor.h>
 #include <qmakedriver.h>
+#include <kdebug.h>
 
+class PrintAST : QMake::ASTVisitor
+{
+public:
+  PrintAST() : QMake::ASTVisitor()
+  {
+      indent = 0;
+  }
+
+  virtual void processProject( QMake::ProjectAST* p )
+  {
+      QMake::ASTVisitor::processProject(p);
+  }
+private:
+  virtual void enterRealProject( QMake::ProjectAST* p )
+  {
+      kdDebug(9024) << getIndent() << "--------- Entering Project: " << replaceWs(p->fileName()) << " --------------" << endl;
+      indent += 4;
+      QMake::ASTVisitor::enterRealProject(p);
+  }
+
+  virtual void leaveRealProject( QMake::ProjectAST* p )
+  {
+      indent -= 4;
+      kdDebug(9024) << getIndent() << "--------- Leaving Project: " << replaceWs(p->fileName()) << " --------------" << endl;
+      QMake::ASTVisitor::leaveRealProject(p);
+  }
+
+  virtual void enterScope( QMake::ProjectAST* p )
+  {
+      kdDebug(9024) << getIndent() << "--------- Entering Scope: " << replaceWs(p->scopedID) << " --------------" << endl;
+      indent += 4;
+      QMake::ASTVisitor::enterScope(p);
+  }
+
+  virtual void leaveScope( QMake::ProjectAST* p )
+  {
+      indent -= 4;
+      kdDebug(9024) << getIndent() << "--------- Leaving Scope: " << replaceWs(p->scopedID) << " --------------" << endl;
+      QMake::ASTVisitor::leaveScope(p);
+  }
+
+  virtual void enterFunctionScope( QMake::ProjectAST* p )
+  {
+      kdDebug(9024) << getIndent() << "--------- Entering FunctionScope: " << replaceWs(p->scopedID) << "(" << replaceWs(p->args) << ")"<< " --------------" << endl;
+      indent += 4;
+      QMake::ASTVisitor::enterFunctionScope(p);
+  }
+
+  virtual void leaveFunctionScope( QMake::ProjectAST* p )
+  {
+      indent -= 4;
+      kdDebug(9024) << getIndent() << "--------- Leaving FunctionScope: " << replaceWs(p->scopedID) << "(" << replaceWs(p->args) << ")"<< " --------------" << endl;
+      QMake::ASTVisitor::leaveFunctionScope(p);
+  }
+
+  QString replaceWs(QString s)
+  {
+      return s.replace("\n", "%nl").replace("\t", "%tab").replace(" ", "%spc");
+  }
+
+  virtual void processAssignment( QMake::AssignmentAST* a)
+  {
+      kdDebug(9024) << getIndent() << "Assignment: " << replaceWs(a->scopedID) << " " << replaceWs(a->op) << " "
+          << replaceWs(a->values.join("|"))<< endl;
+      QMake::ASTVisitor::processAssignment(a);
+  }
+
+  virtual void processNewLine( QMake::NewLineAST* n)
+  {
+      kdDebug(9024) << getIndent() << "Newline " << endl;
+      QMake::ASTVisitor::processNewLine(n);
+  }
+
+  virtual void processComment( QMake::CommentAST* a)
+  {
+      kdDebug(9024) << getIndent() << "Comment: " << replaceWs(a->comment) << endl;
+      QMake::ASTVisitor::processComment(a);
+  }
+
+  virtual void processInclude( QMake::IncludeAST* a)
+  {
+      kdDebug(9024) << getIndent() << "Include: " << replaceWs(a->projectName) << endl;
+      QMake::ASTVisitor::processInclude(a);
+  }
+
+  QString getIndent()
+  {
+      QString ind;
+      for( int i = 0 ; i < indent ; i++)
+          ind += " ";
+      return ind;
+  }
+  int indent;
+};
 int main(int argc, char *argv[])
 {
     QMake::ProjectAST *projectAST;
+    int ret;
     if (argc > 1)
-        return QMake::Driver::parseFile(argv[1], &projectAST);
+    {
+        ret = QMake::Driver::parseFile(argv[1], &projectAST);
+        PrintAST pa;
+        if ( ret == 0 )
+          pa.processProject(projectAST);
+        return ret;
+    }
     else
         return 0;
 }
