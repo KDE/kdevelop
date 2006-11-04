0065a703cdbac13011ac5f4ec8d5a1ff1a3d07d0
Matt Rogers <mattr@kde.org>
2006-11-04 05:30:00 +0000

    add some data for the custom command test.

--- buildtools/importers/cmake/tests/cmakeasttest.cpp
+++ buildtools/importers/cmake/tests/cmakeasttest.cpp
@@ -20,7 +20,465 @@
 
 #include "cmakeasttest.h"
 #include "cmakeast.h"
+#include "cmakelistsparser.h"
 
 QTEST_MAIN( CMakeAstTests )
+
+void CMakeAstTests::testCustomCommandAstParsing()
+{
+//    QFETCH( CMakeFunctionDesc, func );
+
+}
+
+void CMakeAstTests::testCustomCommandAstParsing_data()
+{
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+    CMakeFunctionDesc func1;
+    func.name = "add_custom_command";
+    QStringList argList;
+    argList << "output" << "foo" << "command" << "bar";
+    func.arguments = argList;
+    func.filePath = QString();
+    func.line = 0;
+
+    CMakeFunctionDesc func1;
+    func1.name = "add_custom_command";
+    QStringList argList;
+    argList << "output" << "foo" << "command" << "bar";
+    argList << "main_dependency" << "dep1" << "depends" << "dep1" << "dep2";
+    argList << "working_directory" << "dir1" << "comment" << "some comment";
+    argList << "verbatim" << "append";
+    func1.arguments = argList;
+
+    CMakeFunctionDesc func2;
+    func2.name = "ADD_CUSTOM_COMMAND";
+    QStringList argList;
+    argList << "OUTPUT" << "foo" << "COMMAND" << "bar";
+    argList << "MAIN_DEPENDENCY" << "dep1" << "DEPENDS" << "dep1" << "dep2";
+    argList << "WORKING_DIRECTORY" << "dir1" << "COMMENT" << "some comment";
+    argList << "VERBATIM" << "APPEND";
+    func2.arguments = argList;
+
+    QTest::newRow( "no optional" ) << func;
+    QTest::newRow( "all optional" ) << func1;
+    QTest::newRow( "all optional uppercase" ) << func2;
+
+}
+
+
+void CMakeAstTests::testCustomTargetAstParsing()
+{
+}
+
+void CMakeAstTests::testAddDefintionAstParsing()
+{
+}
+
+void CMakeAstTests::testAddDependenciesAstParsing()
+{
+}
+
+void CMakeAstTests::testAddExecutableAstParsing()
+{
+}
+
+void CMakeAstTests::testAddLibraryAstParsing()
+{
+}
+
+void CMakeAstTests::testAddSubdirectoryAstParsing()
+{
+}
+
+void CMakeAstTests::testAddTestAstParsing()
+{
+}
+
+void CMakeAstTests::testAuxSourceDirectoryAstParsing()
+{
+}
+
+void CMakeAstTests::testBuildCommandAstParsing()
+{
+}
+
+void CMakeAstTests::testBuildNameAstParsing()
+{
+}
+
+void CMakeAstTests::testMinimumVersionAstParsing()
+{
+}
+
+void CMakeAstTests::testConfigureFileAstParsing()
+{
+}
+
+void CMakeAstTests::testCreateTestSourceListAstParsing()
+{
+}
+
+void CMakeAstTests::testElseAstParsing()
+{
+}
+
+void CMakeAstTests::testEnableLanguageAstParsing()
+{
+}
+
+void CMakeAstTests::testEnableTestingAstParsing()
+{
+
+}
+
+void CMakeAstTests::testEndForeachAstParsing()
+{
+
+}
+
+void CMakeAstTests::testEndIfAstParsing()
+{
+
+}
+
+void CMakeAstTests::testEndMacroAstParsing()
+{
+
+}
+
+void CMakeAstTests::testEndWhileAstParsing()
+{
+
+}
+
+void CMakeAstTests::testExecProgramAstParsing()
+{
+
+}
+
+void CMakeAstTests::testExecuteProcessAstParsing()
+{
+
+}
+
+void CMakeAstTests::testExportLibraryDepsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testFileAstParsing()
+{
+
+}
+
+void CMakeAstTests::testFindFileAstParsing()
+{
+
+}
+
+void CMakeAstTests::testFindLibraryAstParsing()
+{
+
+}
+
+void CMakeAstTests::testFindPackageAstParsing()
+{
+
+}
+
+void CMakeAstTests::testFindPathAstParsing()
+{
+
+}
+
+void CMakeAstTests::testFindProgramAstParsing()
+{
+
+}
+
+void CMakeAstTests::testFltkWrapUiAstParsing()
+{
+
+}
+
+void CMakeAstTests::testForeachAstParsing()
+{
+
+}
+
+void CMakeAstTests::testGetCmakePropertyAstParsing()
+{
+
+}
+
+void CMakeAstTests::testGetDirPropertyAstParsing()
+{
+
+}
+
+void CMakeAstTests::testGetFilenameComponentAstParsing()
+{
+
+}
+
+void CMakeAstTests::testGetSourceFilePropAstParsing()
+{
+
+}
+
+void CMakeAstTests::testGetTargetPropAstParsing()
+{
+
+}
+
+void CMakeAstTests::testGetTestPropAstParsing()
+{
+
+}
+
+void CMakeAstTests::testIfAstParsing()
+{
+
+}
+
+void CMakeAstTests::testIncludeAstParsing()
+{
+
+}
+
+void CMakeAstTests::testIncludeDirectoriesAstParsing()
+{
+
+}
+
+void CMakeAstTests::testIncludeExtMSProjectAstParsing()
+{
+
+}
+
+void CMakeAstTests::testIncludeRegexAstParsing()
+{
+
+}
+
+void CMakeAstTests::testInstallAstParsing()
+{
+
+}
+
+void CMakeAstTests::testInstallFilesAstParsing()
+{
+
+}
+
+void CMakeAstTests::testInstallProgramsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testInstallTargetsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testLinkDirsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testLinkLibsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testListAstParsing()
+{
+
+}
+
+void CMakeAstTests::testLoadCacheAstParsing()
+{
+
+}
+
+void CMakeAstTests::testLoadCommandAstParsing()
+{
+
+}
+
+void CMakeAstTests::testMacroAstParsing()
+{
+
+}
+
+void CMakeAstTests::testMakeDirAstParsing()
+{
+
+}
+
+void CMakeAstTests::testMarkAdvancedAstParsing()
+{
+
+}
+
+void CMakeAstTests::testMathAstParsing()
+{
+
+}
+
+void CMakeAstTests::testMessageAstParsing()
+{
+
+}
+
+void CMakeAstTests::testOptionAstParsing()
+{
+
+}
+
+void CMakeAstTests::testOutputReqFilesAstParsing()
+{
+
+}
+
+void CMakeAstTests::testProjectAstParsing()
+{
+
+}
+
+void CMakeAstTests::testQtWrapCppAstParsing()
+{
+
+}
+
+void CMakeAstTests::testQtWrapUiAstParsing()
+{
+
+}
+
+void CMakeAstTests::testRemoveAstParsing()
+{
+
+}
+
+void CMakeAstTests::testRemoveDefinitionsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSeparateArgsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSetAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSetDirPropsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSetSourceFilePropsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSetTargetPropsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSetTestsPropsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSiteNameAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSourceGroupAstParsing()
+{
+
+}
+
+void CMakeAstTests::testStringAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSubdirDependsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testSubdirsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testTargetLinkLibsAstParsing()
+{
+
+}
+
+void CMakeAstTests::testTryCompileAstParsing()
+{
+
+}
+
+void CMakeAstTests::testTryRunAstParsing()
+{
+
+}
+
+void CMakeAstTests::testUseMangledMesaAstParsing()
+{
+
+}
+
+void CMakeAstTests::testUtilitySourceAstParsing()
+{
+
+}
+
+void CMakeAstTests::testVarRequiresAstParsing()
+{
+
+}
+
+void CMakeAstTests::testVtkMakeAstParsing()
+{
+
+}
+
+void CMakeAstTests::testVtkWrapJavaParsing()
+{
+
+}
+
+void CMakeAstTests::testVtkWrapPythonParsing()
+{
+
+}
+
+void CMakeAstTests::testVtkWrapTclParsing()
+{
+
+}
+
+void CMakeAstTests::testWhileParsing()
+{
+
+}
+
+void CMakeAstTests::testWriteFileParsing()
+{
+
+}
 
 #include "cmakeasttest.moc"
--- buildtools/importers/cmake/tests/cmakeasttest.h
+++ buildtools/importers/cmake/tests/cmakeasttest.h
@@ -31,93 +31,93 @@ public:
     virtual ~CMakeAstTests() {}
 
 private slots:
-    void testCustomCommandAstParsing() { QFAIL( "no magic" ); }
-    void testCustomTargetAstParsing() { QFAIL( "no magic" ); }
-    void testAddDefintionAstParsing() { QFAIL( "no magic" ); }
-    void testAddDependenciesAstParsing() { QFAIL( "no magic" ); }
-    void testAddExecutableAstParsing() { QFAIL( "no magic" ); }
-    void testAddLibraryAstParsing() { QFAIL( "no magic" ); }
-    void testAddSubdirectoryAstParsing() { QFAIL( "no magic" ); }
-    void testAddTestAstParsing() { QFAIL( "no magic" ); }
-    void testAuxSourceDirectoryAstParsing() { QFAIL( "no magic" ); }
-    void testBuildCommandAstParsing() { QFAIL( "no magic" ); }
-    void testBuildNameAstParsing() { QFAIL( "no magic" ); }
-    void testMinimumVersionAstParsing() { QFAIL( "no magic" ); }
-    void testConfigureFileAstParsing() { QFAIL( "no magic" ); }
-    void testCreateTestSourceListAstParsing() { QFAIL( "no magic" ); }
-    void testElseAstParsing() { QFAIL( "no magic" ); }
-    void testEnableLanguageAstParsing() { QFAIL( "no magic" ); }
-    void testEnableTestingAstParsing() { QFAIL( "no magic" ); }
-    void testEndForeachAstParsing() { QFAIL( "no magic" ); }
-    void testEndIfAstParsing() { QFAIL( "no magic" ); }
-    void testEndMacroAstParsing() { QFAIL( "no magic" ); }
-    void testEndWhileAstParsing() { QFAIL( "no magic" ); }
-    void testExecProgramAstParsing() { QFAIL( "no magic" ); }
-    void testExecuteProcessAstParsing() { QFAIL( "no magic" ); }
-    void testExportLibraryDepsAstParsing() { QFAIL( "no magic" ); }
-    void testFileAstParsing() { QFAIL( "no magic" ); }
-    void testFindFileAstParsing() { QFAIL( "no magic" ); }
-    void testFindLibraryAstParsing() { QFAIL( "no magic" ); }
-    void testFindPackageAstParsing() { QFAIL( "no magic" ); }
-    void testFindPathAstParsing() { QFAIL( "no magic" ); }
-    void testFindProgramAstParsing() { QFAIL( "no magic" ); }
-    void testFltkWrapUiAstParsing() { QFAIL( "no magic" ); }
-    void testForeachAstParsing() { QFAIL( "no magic" ); }
-    void testGetCmakePropertyAstParsing() { QFAIL( "no magic" ); }
-    void testGetDirPropertyAstParsing() { QFAIL( "no magic" ); }
-    void testGetFilenameComponentAstParsing() { QFAIL( "no magic" ); }
-    void testGetSourceFilePropAstParsing() { QFAIL( "no magic" ); }
-    void testGetTargetPropAstParsing() { QFAIL( "no magic" ); }
-    void testGetTestPropAstParsing() { QFAIL( "no magic" ); }
-    void testIfAstParsing() { QFAIL( "no magic" ); }
-    void testIncludeAstParsing() { QFAIL( "no magic" ); }
-    void testIncludeDirectoriesAstParsing() { QFAIL( "no magic" ); }
-    void testIncludeExtMSProjectAstParsing() { QFAIL( "no magic" ); }
-    void testIncludeRegexAstParsing() { QFAIL( "no magic" ); }
-    void testInstallAstParsing() { QFAIL( "no magic" ); }
-    void testInstallFilesAstParsing() { QFAIL( "no magic" ); }
-    void testInstallProgramsAstParsing() { QFAIL( "no magic" ); }
-    void testInstallTargetsAstParsing() { QFAIL( "no magic" ); }
-    void testLinkDirsAstParsing() { QFAIL( "no magic" ); }
-    void testLinkLibsAstParsing() { QFAIL( "no magic" ); }
-    void testListAstParsing() { QFAIL( "no magic" ); }
-    void testLoadCacheAstParsing() { QFAIL( "no magic" ); }
-    void testLoadCommandAstParsing() { QFAIL( "no magic" ); }
-    void testMacroAstParsing() { QFAIL( "no magic" ); }
-    void testMakeDirAstParsing() { QFAIL( "no magic" ); }
-    void testMarkAdvancedAstParsing() { QFAIL( "no magic" ); }
-    void testMathAstParsing() { QFAIL( "no magic" ); }
-    void testMessageAstParsing() { QFAIL( "no magic" ); }
-    void testOptionAstParsing() { QFAIL( "no magic" ); }
-    void testOutputReqFilesAstParsing() { QFAIL( "no magic" ); }
-    void testProjectAstParsing() { QFAIL( "no magic" ); }
-    void testQtWrapCppAstParsing() { QFAIL( "no magic" ); }
-    void testQtWrapUiAstParsing() { QFAIL( "no magic" ); }
-    void testRemoveAstParsing() { QFAIL( "no magic" ); }
-    void testRemoveDefinitionsAstParsing() { QFAIL( "no magic" ); }
-    void testSeparateArgsAstParsing() { QFAIL( "no magic" ); }
-    void testSetAstParsing() { QFAIL( "no magic" ); }
-    void testSetDirPropsAstParsing() { QFAIL( "no magic" ); }
-    void testSetSourceFilePropsAstParsing() { QFAIL( "no magic" ); }
-    void testSetTargetPropsAstParsing() { QFAIL( "no magic" ); }
-    void testSetTestsPropsAstParsing() { QFAIL( "no magic" ); }
-    void testSiteNameAstParsing() { QFAIL( "no magic" ); }
-    void testSourceGroupAstParsing() { QFAIL( "no magic" ); }
-    void testStringAstParsing() { QFAIL( "no magic" ); }
-    void testSubdirDependsAstParsing() { QFAIL( "no magic" ); }
-    void testSubdirsAstParsing() { QFAIL( "no magic" ); }
-    void testTargetLinkLibsAstParsing() { QFAIL( "no magic" ); }
-    void testTryCompileAstParsing() { QFAIL( "no magic" ); }
-    void testTryRunAstParsing() { QFAIL( "no magic" ); }
-    void testUseMangledMesaAstParsing() { QFAIL( "no magic" ); }
-    void testUtilitySourceAstParsing() { QFAIL( "no magic" ); }
-    void testVarRequiresAstParsing() { QFAIL( "no magic" ); }
-    void testVtkMakeAstParsing() { QFAIL( "no magic" ); }
-    void testVtkWrapJavaParsing() { QFAIL( "no magic" ); }
-    void testVtkWrapPythonParsing() { QFAIL( "no magic" ); }
-    void testVtkWrapTclParsing() { QFAIL( "no magic" ); }
-    void testWhileParsing() { QFAIL( "no magic" ); }
-    void testWriteFileParsing() { QFAIL( "no magic" ); }
+    void testCustomCommandAstParsing();
+    void testCustomTargetAstParsing();
+    void testAddDefintionAstParsing();
+    void testAddDependenciesAstParsing();
+    void testAddExecutableAstParsing();
+    void testAddLibraryAstParsing();
+    void testAddSubdirectoryAstParsing();
+    void testAddTestAstParsing();
+    void testAuxSourceDirectoryAstParsing();
+    void testBuildCommandAstParsing();
+    void testBuildNameAstParsing();
+    void testMinimumVersionAstParsing();
+    void testConfigureFileAstParsing();
+    void testCreateTestSourceListAstParsing();
+    void testElseAstParsing();
+    void testEnableLanguageAstParsing();
+    void testEnableTestingAstParsing();
+    void testEndForeachAstParsing();
+    void testEndIfAstParsing();
+    void testEndMacroAstParsing();
+    void testEndWhileAstParsing();
+    void testExecProgramAstParsing();
+    void testExecuteProcessAstParsing();
+    void testExportLibraryDepsAstParsing();
+    void testFileAstParsing();
+    void testFindFileAstParsing();
+    void testFindLibraryAstParsing();
+    void testFindPackageAstParsing();
+    void testFindPathAstParsing();
+    void testFindProgramAstParsing();
+    void testFltkWrapUiAstParsing();
+    void testForeachAstParsing();
+    void testGetCmakePropertyAstParsing();
+    void testGetDirPropertyAstParsing();
+    void testGetFilenameComponentAstParsing();
+    void testGetSourceFilePropAstParsing();
+    void testGetTargetPropAstParsing();
+    void testGetTestPropAstParsing();
+    void testIfAstParsing();
+    void testIncludeAstParsing();
+    void testIncludeDirectoriesAstParsing();
+    void testIncludeExtMSProjectAstParsing();
+    void testIncludeRegexAstParsing();
+    void testInstallAstParsing();
+    void testInstallFilesAstParsing();
+    void testInstallProgramsAstParsing();
+    void testInstallTargetsAstParsing();
+    void testLinkDirsAstParsing();
+    void testLinkLibsAstParsing();
+    void testListAstParsing();
+    void testLoadCacheAstParsing();
+    void testLoadCommandAstParsing();
+    void testMacroAstParsing();
+    void testMakeDirAstParsing();
+    void testMarkAdvancedAstParsing();
+    void testMathAstParsing();
+    void testMessageAstParsing();
+    void testOptionAstParsing();
+    void testOutputReqFilesAstParsing();
+    void testProjectAstParsing();
+    void testQtWrapCppAstParsing();
+    void testQtWrapUiAstParsing();
+    void testRemoveAstParsing();
+    void testRemoveDefinitionsAstParsing();
+    void testSeparateArgsAstParsing();
+    void testSetAstParsing();
+    void testSetDirPropsAstParsing();
+    void testSetSourceFilePropsAstParsing();
+    void testSetTargetPropsAstParsing();
+    void testSetTestsPropsAstParsing();
+    void testSiteNameAstParsing();
+    void testSourceGroupAstParsing();
+    void testStringAstParsing();
+    void testSubdirDependsAstParsing();
+    void testSubdirsAstParsing();
+    void testTargetLinkLibsAstParsing();
+    void testTryCompileAstParsing();
+    void testTryRunAstParsing();
+    void testUseMangledMesaAstParsing();
+    void testUtilitySourceAstParsing();
+    void testVarRequiresAstParsing();
+    void testVtkMakeAstParsing();
+    void testVtkWrapJavaParsing();
+    void testVtkWrapPythonParsing();
+    void testVtkWrapTclParsing();
+    void testWhileParsing();
+    void testWriteFileParsing();
 
 };
 
