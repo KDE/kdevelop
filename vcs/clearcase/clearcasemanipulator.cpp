40f9cdff84d77656cc39f77780ad14f92b4c26bf
Alexander Dymo <adymo@kdevelop.org>
2006-12-02 22:33:38 +0000

    MacOS build fix (no getline function on Darwin)

--- vcs/clearcase/clearcasemanipulator.cpp
+++ vcs/clearcase/clearcasemanipulator.cpp
@@ -18,7 +18,50 @@
 
 #include <stdlib.h>
 #include <libgen.h>
+#include <errno.h>
 
+#ifdef Q_OS_MACX
+//this function is taken from GNU libc
+//it does not exist on macos
+int getline(char **lineptr, size_t *n, FILE *stream)
+{
+    static char line[256];
+    char *ptr;
+    unsigned int len;
+
+   if (lineptr == NULL || n == NULL)
+   {
+      errno = EINVAL;
+      return -1;
+   }
+
+   if (ferror (stream))
+      return -1;
+
+   if (feof(stream))
+      return -1;
+
+   fgets(line,256,stream);
+
+   ptr = strchr(line,'\n');
+   if (ptr)
+      *ptr = '\0';
+
+   len = strlen(line);
+
+   if ((len+1) < 256)
+   {
+      ptr = (char*)realloc(*lineptr, 256);
+      if (ptr == NULL)
+         return(-1);
+      *lineptr = ptr;
+      *n = 256;
+   }
+
+   strcpy(*lineptr,line);
+   return(len);
+}
+#endif
 
 const char ClearcaseManipulator::CT_DESC_SEPARATOR = ';';
 
