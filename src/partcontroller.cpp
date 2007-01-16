1d7bc1e7e7fab5accde4aade82682ea8787d168b
Jens Dagerbo <jens.dagerbo@swipnet.se>
2007-01-16 20:36:45 +0000

    Fix partial url lookup. Thanks to Achim Spangler for the patch!
    BUG: 140161

--- src/partcontroller.cpp
+++ src/partcontroller.cpp
@@ -285,17 +285,19 @@ void PartController::editDocumentInternal( const KURL & inputUrl, int lineNum,
 		if ( API::getInstance()->project() )
 		{
 			if (url.isRelativeURL(url.url())) {
-				KURL relURL(API::getInstance()->project()->projectDirectory(), url.url());
+				KURL relURL(API::getInstance()->project()->projectDirectory());
+				relURL.addPath( url.url() );
 
 				kdDebug() << k_funcinfo << "Looking for file in project dir: " << API::getInstance()->project()->projectDirectory() << " url " << url.url() << " transformed to " << relURL.url() << ": " << done << endl;
-				if (relURL.isValid() && KIO::NetAccess::exists(url, false, 0)) {
+				if (relURL.isValid() && KIO::NetAccess::exists(relURL, false, 0)) {
 					url = relURL;
 					done = true;
 				}
 				else {
-					KURL relURL(API::getInstance()->project()->buildDirectory(), url.url());
+					KURL relURL(API::getInstance()->project()->buildDirectory());
+					relURL.addPath( url.url() );
 					kdDebug() << k_funcinfo << "Looking for file in build dir: " << API::getInstance()->project()->buildDirectory() << " url " << url.url() << " transformed to " << relURL.url() << ": " << done << endl;
-					if (relURL.isValid() && KIO::NetAccess::exists(url, false, 0)) {
+					if (relURL.isValid() && KIO::NetAccess::exists(relURL, false, 0)) {
 						url = relURL;
 						done = true;
 					}
