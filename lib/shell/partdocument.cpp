a7581a23fd66cb5279a8eb8fb9f80b413cb333b8
Alexander Dymo <adymo@kdevelop.org>
2007-02-12 21:05:35 +0000

    here go missing files...

---
+++ lib/shell/partdocument.cpp
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Alexander Dymo  <adymo@kdevelop.org>            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+#include "partdocument.h"
+
+#include <ktexteditor/document.h>
+
+#include "partcontroller.h"
+
+namespace KDevelop {
+
+struct PartDocumentPrivate {
+    PartController *partController;
+    QMap<QWidget*, KParts::Part*> partForWidget;
+};
+
+PartDocument::PartDocument(PartController *partController, Sublime::Controller *controller, const KUrl &url)
+    :Sublime::PartDocument(controller, url)
+{
+    d = new PartDocumentPrivate();
+    d->partController = partController;
+}
+
+PartDocument::~PartDocument()
+{
+    delete d;
+}
+
+QWidget *PartDocument::createViewWidget(QWidget *parent)
+{
+    Q_UNUSED( parent );
+    KParts::Part *part;
+    if (url().isEmpty())
+        part = d->partController->createTextPart(url(), "", false);
+    else
+        part = d->partController->createPart(url());
+    d->partController->addPart(part);
+    QWidget *w = part->widget();
+    d->partForWidget[w] = part;
+    return w;
+}
+
+KParts::Part *PartDocument::partForWidget(QWidget *w)
+{
+    return d->partForWidget[w];
+}
+
+}
+
+// kate: space-indent on; indent-width 4; tab-width 4; replace-tabs on
---
+++ lib/shell/partdocument.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Alexander Dymo  <adymo@kdevelop.org>            *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+#ifndef KDEV_PARTDOCUMENT_H
+#define KDEV_PARTDOCUMENT_H
+
+#include <sublime/partdocument.h>
+
+namespace Sublime {
+class Controller;
+}
+
+namespace KParts {
+class Part;
+}
+
+namespace KDevelop {
+
+class PartController;
+
+//FIXME adymo: merge with Sublime::PartDocument or refactor
+class PartDocument: public Sublime::PartDocument {
+public:
+    PartDocument(PartController *partController, Sublime::Controller *controller, const KUrl &url);
+    virtual ~PartDocument();
+
+    virtual QWidget *createViewWidget(QWidget *parent = 0);
+    KParts::Part *partForWidget(QWidget *w);
+
+private:
+    class PartDocumentPrivate *d;
+};
+
+}
+
+#endif
+
+// kate: space-indent on; indent-width 4; tab-width 4; replace-tabs on
