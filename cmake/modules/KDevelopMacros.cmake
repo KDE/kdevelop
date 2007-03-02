b37d9992022ddf7c53f20183348c4df59be2b2ff
Alexander Dymo <adymo@kdevelop.org>
2007-03-02 21:23:52 +0000

    Commented out broken apptemplate dependency calculation, fixed template installation paths.

--- cmake/modules/KDevelopMacros.cmake
+++ cmake/modules/KDevelopMacros.cmake
@@ -1,6 +1,6 @@
 
 macro(kdevelop_add_app_templates _templateNames)
-    foreach(_templateName ${ARGN})
+    foreach(_templateName ${ARGV})
 
         get_filename_component(_tmp_file ${_templateName} ABSOLUTE)
         get_filename_component(_baseName ${_tmp_file} NAME_WE)
@@ -8,33 +8,31 @@ macro(kdevelop_add_app_templates _templateNames)
 
         add_custom_target(${_baseName} ALL DEPENDS ${_template})
 
-	file(GLOB _files ${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}/*)
-	set(_deps)
-	foreach(_file ${_files})
-	    get_filename_component(_fileName ${_file} NAME)
-	    string(COMPARE NOTEQUAL ${_fileName} ${_templateName}.kdevtemplate _v1)
-	    string(COMPARE NOTEQUAL ${_fileName} ${_templateName}.png _v2)
-	    string(COMPARE NOTEQUAL ${_fileName} .kdev_ignore _v3)
-	    string(COMPARE NOTEQUAL ${_fileName} Makefile.am _v4)
-	    string(SUBSTRING ${_fileName} 0 4 _svn)
-	    string(COMPARE NOTEQUAL ${_svn} .svn _v5)
-	    if ( ${_v1} AND ${_v2} AND ${_v3} AND ${_v4} AND ${_v5} )
-		set(_deps ${_deps} ${_file})
-	    endif ( ${_v1} AND ${_v2} AND ${_v3} AND ${_v4} AND ${_v5} )
-	endforeach(_file)
+        #file(GLOB _files ${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}/*)
+        #set(_deps)
+        #foreach(_file ${_files})
+        #    get_filename_component(_fileName ${_file} NAME)
+        #    string(COMPARE NOTEQUAL ${_fileName} .kdev_ignore _v1)
+        #    string(SUBSTRING ${_fileName} 0 4 _svn)
+        #    string(COMPARE NOTEQUAL ${_svn} .svn _v2)
+        #    if ( ${_v1} AND ${_v2} )
+        #    set(_deps ${_deps} ${_file})
+        #    endif ( ${_v1} AND ${_v2} )
+        #endforeach(_file)
 
         add_custom_command(OUTPUT ${_template}
             COMMAND tar ARGS -c -C ${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}
-                --exclude ${_templateName}.kdevtemplate --exclude ${_templateName}.png
-                --exclude .kdev_ignore --exclude .svn --exclude Makefile.am
+                --exclude .kdev_ignore --exclude .svn
                 -j -f ${_template} .
-#	    DEPENDS ${_deps}
+            #DEPENDS ${_deps}
         )
 
-install( FILES ${_templateName}/${_templateName}.kdevtemplate DESTINATION ${DATA_INSTALL_DIR}/kdevappwizard/templates )
-install( FILES ${_templateName}/${_templateName}.png DESTINATION ${DATA_INSTALL_DIR}/kdevappwizard )
-install( FILES ${_template} DESTINATION ${DATA_INSTALL_DIR}/kdevappwizard )
-	macro_additional_clean_files(${_template})
+        #install( FILES ${_templateName}/${_templateName}.kdevtemplate
+        #    DESTINATION ${DATA_INSTALL_DIR}/kdevappwizard/templates )
+        #install( FILES ${_templateName}/${_templateName}.png
+        #    DESTINATION ${DATA_INSTALL_DIR}/kdevappwizard )
+        install( FILES ${_template} DESTINATION ${DATA_INSTALL_DIR}/kdevappwizard/templates )
+        macro_additional_clean_files(${_template})
 
     endforeach(_templateName)
 endmacro(kdevelop_add_app_templates)
