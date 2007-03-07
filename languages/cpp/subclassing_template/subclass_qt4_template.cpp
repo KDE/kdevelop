7c0a201e0378a41da790a3df4cfe7624967f98d5
Andreas Pakulat <apaku@gmx.de>
2007-03-07 00:18:45 +0000

    Fix templates

--- languages/cpp/subclassing_template/subclass_qt4_template.cpp
+++ languages/cpp/subclassing_template/subclass_qt4_template.cpp
@@ -1,6 +1,6 @@
 
 
-#include "$NEWFILENAME$.h"
+#include "$NEWFILENAMELC$.h"
 
 $NEWCLASS$::$NEWCLASS$(QWidget* parent, Qt::WFlags fl)
 : $QTBASECLASS$( parent, fl ), Ui::$BASECLASS$()
--- languages/cpp/subclassing_template/subclass_template.cpp
+++ languages/cpp/subclassing_template/subclass_template.cpp
@@ -1,6 +1,6 @@
 
 
-#include "$NEWFILENAME$.h"
+#include "$NEWFILENAMELC$.h"
 
 $NEWCLASS$::$NEWCLASS$(QWidget* parent, const char* name$CAN_BE_MODAL_CPP1$, WFlags fl)
 : $BASECLASS$(parent,name$CAN_BE_MODAL_CPP2$,fl)
