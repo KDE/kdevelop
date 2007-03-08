688c697d7ea5573f7129d3c945bf61ee6ce00052
Jakob Petsovits <jpetso@gmx.at>
2007-03-08 18:11:17 +0000

    Fix kdevitemmodel.h include, so the kdev-pg based parsers compile (but not their language parts).

--- lib/kdevcodemodel.h
+++ lib/kdevcodemodel.h
@@ -23,7 +23,7 @@
 #ifndef KDEVCODEMODEL_H
 #define KDEVCODEMODEL_H
 
-#include "kdevitemmodel.h"
+#include "util/kdevitemmodel.h"
 #include <ktexteditor/cursor.h>
 #include <ktexteditor/rangefeedback.h>
 
