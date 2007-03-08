b7a2d31a8505467e62ddf85ebf5bc9bcdeeaf55c
Andreas Pakulat <apaku@gmx.de>
2007-03-08 00:22:33 +0000

    Fix a bug in the projectbuilder interface KDevelop say hello to QMake Builder (with its own interface), it doesn't do anything yet, though...
    svn path=/trunk/KDE/kdevelop/buildtools/builders/qmakebuilder/; revision=640431

---
+++ qmakebuilder/qmakebuilder.h
@@ -0,0 +1,61 @@
+/* KDevelop QMake Support
+ *
+ * Copyright 2006 Andreas Pakulat <apaku@gmx.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef QMAKEBUILDER_H
+#define QMAKEBUILDER_H
+
+#include "iplugin.h"
+#include "iqmakebuilder.h"
+#include <QtCore/QList>
+#include <QtCore/QPair>
+
+class QStringList;
+class KDialog;
+namespace KDevelop{ class ProjectBaseItem; }
+
+/**
+@author Andreas Pakulat
+*/
+class QMakeBuilder : public KDevelop::IPlugin, public IQMakeBuilder
+{
+    Q_OBJECT
+    Q_INTERFACES( IQMakeBuilder )
+    Q_INTERFACES( KDevelop::IProjectBuilder )
+
+public:
+    QMakeBuilder(QObject *parent = 0, const QStringList &args = QStringList());
+    virtual ~QMakeBuilder();
+
+    virtual bool build(KDevelop::ProjectBaseItem *dom);
+    virtual bool clean(KDevelop::ProjectBaseItem *dom);
+
+    virtual void registerExtensions();
+    virtual void unregisterExtensions();
+    virtual QStringList extensions() const;
+private slots:
+    void commandFinished(const QString &command);
+    void commandFailed(const QString &command);
+
+private:
+};
+
+#endif // QMAKEBUILDER_H
+
+// kate: space-indent on; indent-width 4; tab-width: 4; replace-tabs on; auto-insert-doxygen on
