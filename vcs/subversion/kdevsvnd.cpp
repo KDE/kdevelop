b53c58d16f01b1df6927564b208a7e99fce0870d
Dukju Ahn <dukjuahn@gmail.com>
2007-04-15 08:24:31 +0000

    removing unneeded __TIME__ debug symbol

--- vcs/subversion/kdevsvnd.cpp
+++ vcs/subversion/kdevsvnd.cpp
@@ -49,7 +49,7 @@ KDevSvnd::~KDevSvnd()
 
 QString KDevSvnd::commitDialog(QString modifiedFiles) {
 	CommitDlg commitDlg;
-	commitDlg.setCaption(i18n("Enter Commit Log Message ") + QString( __TIME__) );
+	commitDlg.setCaption(i18n("Enter Commit Log Message "));
 	commitDlg.listMessage->setText( modifiedFiles );
 	int result = commitDlg.exec();
 	if ( result == QDialog::Accepted ) {
