1c78b2112766d465e95f4dcdc5d7a0e9ce808563
Matthew Woehlke <mw_triad@users.sourceforge.net>
2007-05-07 14:55:54 +0000

    VCS Interfaces: better doc for VcsItemEvent, also nuke action() in VcsChange (it doesn't belong there)

--- lib/plugins/vcs/interfaces/vcshelpers.h
+++ lib/plugins/vcs/interfaces/vcshelpers.h
@@ -116,7 +116,9 @@ public:
 
 /**
  * Small container class that contains information about a history event of a
- * single repository item.
+ * single repository item. This class contains information that is common to a
+ * VcsEvent (which represents a single item) and a VcsChange (which may have
+ * information about several items).
  */
 class VcsItemEvent
 {
@@ -151,7 +153,6 @@ class VcsChange
     VcsRevision revision();
     QString user();
     QDate date();
-    VcsActions actions();
     QString message();
     QList<VcsItemEvent> items();
 };
