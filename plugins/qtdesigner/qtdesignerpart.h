6fada65c78f710a3f6ab437027f0c2363ddc6c6a
Andreas Pakulat <apaku@gmx.de>
2007-05-13 01:18:22 +0000

    Remove unused class Add copyright headers based on svn log information

--- plugins/qtdesigner/qtdesignerpart.h
+++ plugins/qtdesigner/qtdesignerpart.h
@@ -1,3 +1,23 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Harald Fernengel <harry@kdevelop.org>           *
+ *   Copyright (C) 2006 by Matt Rogers <mattr@kde.org>                     *
+ *   Copyright (C) 2007 by Andreas Pakulat <apaku@gmx.de>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
 #ifndef QTDESIGNER_PART_H
 #define QTDESIGNER_PART_H
 
@@ -16,7 +36,7 @@ class KAction;
 
 namespace KDevelop
 {
-  class Document;
+  class IDocument;
 }
 
 template<class T> class QList;
@@ -24,7 +44,7 @@ template<class T> class QList;
 typedef QHash<QAction*, QAction*> DesignerActionHash;
 
 
-class QtDesignerPart: public KParts::ReadWritePart
+class QtDesignerPart: public KDevelop::IPlugin
 {
   Q_OBJECT
 public:
