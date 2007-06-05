b4b08a83f8478b6aaff4240d851c8899f94cb3a7
Andreas Pakulat <apaku@gmx.de>
2007-06-05 19:21:07 +0000

    Add messages.sh script to the platform.

---
+++ Messages.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+find . -type d | fgrep -v '.svn' | sed -e 's,$,/,' > dirs
+msh=`find . -name Messages.sh`
+for dir in $msh; do
+  dir=`dirname $dir`
+  if test "$dir" != "."; then
+    egrep -v "^$dir" dirs > dirs.new && mv dirs.new dirs
+  fi
+done
+dirs=`cat dirs`
+find $dirs -maxdepth 1 -name "*.cpp" -print > files
+find $dirs -maxdepth 1 -name "*.cc" -print >> files
+find $dirs -maxdepth 1 -name "*.h" -print >> files
+$EXTRACTRC `find $dirs -maxdepth 1 \( -name \*.rc -o -name \*.ui -o -name \*.ui3 -o -name \*.ui4 -o -name \*.kcfg \) ` >> rc.cpp || exit 11
+echo rc.cpp >> files
+$XGETTEXT --files-from=files -o $podir/kdevelop.pot
+rm -f rc.cpp dirs
+
