cc1bd4c792aea4710ec736ad7f715c62f3f0a034
Matt Rogers <mattr@kde.org>
2007-06-07 13:42:15 +0000

    add data that should result in successful parsing when handled by the file AST. data should will result in unsuccessful parsing is also added, but commented out since it's just a copy and paste from the good data.

--- buildtools/managers/cmake/tests/cmake_fileast_test.cpp
+++ buildtools/managers/cmake/tests/cmake_fileast_test.cpp
@@ -34,6 +34,60 @@ void FileAstTest::testGoodParse()
 
 void FileAstTest::testGoodParse_data()
 {
+    const int NUM_TESTDATA = 13;
+    CMakeFunctionDesc funcs[NUM_TESTDATA];
+    QStringList args[NUM_TESTDATA];
+
+    for ( int i = 0; i < NUM_TESTDATA; i++ )
+        funcs[i].name = "FILE";
+
+    //write file command
+    args[0] << "WRITE" << "somefile.cpp" << "\"the things to write\"";
+
+    //append file command
+    args[1] << "APPEND" << "somefile.cpp" << "\"the things to append\"";
+
+    //read file command
+    args[2] << "READ" << "somefile.cpp" << "MY_VAR";
+
+    //glob files command. does not search in files only for a pattern
+    args[3] << "GLOB" << "MY_VAR" << "*.cpp";
+    args[4] << "GLOB" << "MY_VAR" << "RELATIVE" << "/path/to/something"
+            << "*.cpp"; //RELATIVE is optional
+
+    //recursive glob
+    args[5] << "GLOB" << "MY_VAR" << "*.cpp";
+    args[6] << "GLOB" << "MY_VAR" << "RELATIVE" << "/path/to/something"
+            << "*.cpp"; //RELATIVE is optional
+
+    //remove command
+    args[7] << "REMOVE" << "/path/to/file/to/remove.cpp";
+
+    //remove recursive
+    args[8] << "REMOVE_RECURSE" << "/path/to/dir/to/remove/files";
+
+    //make a directory
+    args[9] << "MAKE_DIRECTORY" << "/path/to/dir/to/create";
+
+    //get a relative path
+    args[10] << "RELATIVE_PATH" << "MY_VAR" << "/path/to/foo"
+             << "/path/to/file/to/get/path/for.cpp";
+
+    //get the cmake native path
+    args[11] << "TO_CMAKE_PATH" << "/path/to/file.cpp" << "CMAKE_PATH";
+
+    //get the platform native path
+    args[12] << "TO_NATIVE_PATH" << "/path/to/file.cpp" << "NATIVE_PATH";
+
+
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+
+    for ( int i = 0; i < NUM_TESTDATA; i++)
+    {
+        funcs[i].addArguments( args[i] );
+        QTest::newRow( qPrintable(QString::number(i)) ) << funcs[i];
+    }
+
 }
 
 void FileAstTest::testBadParse()
@@ -46,6 +100,62 @@ void FileAstTest::testBadParse()
 
 void FileAstTest::testBadParse_data()
 {
+/*
+    const int NUM_TESTDATA = 13;
+    CMakeFunctionDesc funcs[NUM_TESTDATA];
+    QStringList args[NUM_TESTDATA];
+
+    for ( int i = 0; i < NUM_TESTDATA; i++ )
+        funcs[i].name = "FILE";
+
+    //write file command
+    args[0] << "WRITE" << "somefile.cpp" << "\"the things to write\"";
+
+    //append file command
+    args[1] << "APPEND" << "somefile.cpp" << "\"the things to append\"";
+
+    //read file command
+    args[2] << "READ" << "somefile.cpp" << "MY_VAR";
+
+    //glob files command. does not search in files only for a pattern
+    args[3] << "GLOB" << "MY_VAR" << "*.cpp";
+    args[4] << "GLOB" << "MY_VAR" << "RELATIVE" << "/path/to/something"
+            << "*.cpp"; //RELATIVE is optional
+
+    //recursive glob
+    args[5] << "GLOB" << "MY_VAR" << "*.cpp";
+    args[6] << "GLOB" << "MY_VAR" << "RELATIVE" << "/path/to/something"
+            << "*.cpp"; //RELATIVE is optional
+
+    //remove command
+    args[7] << "REMOVE" << "/path/to/file/to/remove.cpp";
+
+    //remove recursive
+    args[8] << "REMOVE_RECURSE" << "/path/to/dir/to/remove/files";
+
+    //make a directory
+    args[9] << "MAKE_DIRECTORY" << "/path/to/dir/to/create";
+
+    //get a relative path
+    args[10] << "RELATIVE_PATH" << "MY_VAR" << "/path/to/foo"
+             << "/path/to/file/to/get/path/for.cpp";
+
+    //get the cmake native path
+    args[11] << "TO_CMAKE_PATH" << "/path/to/file.cpp";
+
+    //get the platform native path
+    args[12] << "TO_NATIVE_PATH" << "/path/to/file.cpp";
+
+
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+
+    for ( int i = 0; i < NUM_TESTDATA; i++)
+    {
+        funcs[i].addArguments( args[i] );
+        QTest::newRow( qPrintable(QString::number(i)) ) << funcs[i];
+    }
+*/
 }
 
 #include "cmake_fileast_test.moc"
+//kate: space-indent on; indent-width 4; replace-tabs on; tab-width 4; auto-insert-doxygen: on; indent-mode: csands;
