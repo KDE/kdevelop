2d33c3f08a90d5122bd3ae9c2718483184f15150
Andreas Pakulat <apaku@gmx.de>
2007-06-10 15:45:07 +0000

    Forgot to add the _p header

---
+++ src/language/duchain/ducontext_p.h
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *   This file is part of KDevelop                                         *
+ *   Copyright (C) 2006 Hamish Rodda <rodda@kde.org>                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef ducontext_p_H
+#define ducontext_p_H
+
+namespace KDevelop{
+class DUContext;
+class DUContextPrivate
+{
+public:
+  DUContextPrivate( DUContext* );
+  DUContext::ContextType m_contextType;
+  QualifiedIdentifier m_scopeIdentifier;
+  DUContext* m_parentContext;
+  QList<DUContext*> m_importedParentContexts;
+  QList<DUContext*> m_childContexts;
+  QList<DUContext*> m_importedChildContexts;
+  QList<Declaration*> m_localDeclarations;
+  QList<Definition*> m_localDefinitions;
+  QList<DUContext::UsingNS*> m_usingNamespaces;
+  QList<Use*> m_uses;
+  QList<Use*> m_orphanUses;
+  DUContext* m_context;
+  bool m_inSymbolTable : 1;
+    /**
+   * Adds a child context.
+   *
+   * \note Be sure to have set the text location first, so that
+   * the chain is sorted correctly.
+   */
+  void addChildContext(DUContext* context);
+
+  void addImportedChildContext( DUContext * context );
+  void removeImportedChildContext( DUContext * context );
+
+  void addDeclaration(Declaration* declaration);
+  void removeDeclaration(Declaration* declaration);
+
+  void addUse(Use* use);
+  void removeUse(Use* use);
+};
+}
+
+
+#endif
+
+//kate: space-indent on; indent-width 2; replace-tabs on; auto-insert-doxygen on; indent-mode cstyle;
