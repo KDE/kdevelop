b8b37625bf999433f7d8e1c5fed88518104706e2
Matt Rogers <mattr@kde.org>
2007-07-04 02:44:11 +0000

    add parsing data for VTK_WRAP_TCL (which is deprecated)

--- buildtools/managers/cmake/tests/cmake_vtkwraptclast_test.cpp
+++ buildtools/managers/cmake/tests/cmake_vtkwraptclast_test.cpp
@@ -34,6 +34,47 @@ void VtkWrapTclAstTest::testGoodParse()
 
 void VtkWrapTclAstTest::testGoodParse_data()
 {
+    QTest::addColumn<CMakeFunctionDesc>("function");
+
+    const int NUM_TESTDATA = 6;
+    CMakeFunctionDesc funcs[NUM_TESTDATA];
+    QStringList args[NUM_TESTDATA];
+
+    for ( int i = 0; i < NUM_TESTDATA; i++ )
+        funcs[i].name = "VTK_WRAP_TCL";
+
+    //test lowercase too
+    funcs[NUM_TESTDATA - 1].name = funcs[NUM_TESTDATA - 1].name.toLower();
+
+    args[0] << "myLibrary" << "MySourceList" << "source1.tcl";
+    
+    //SOURCES is optional
+    args[1] << "myLibrary" << "SOURCES" << "MySourceList" << "source1.tcl";
+    
+    //multiple sources
+    args[2] << "myLibrary" << "SOURCES" << "MySourceList" << "source1.tcl"
+            << "source2.tcl";
+
+    //commands are optional
+    args[3] << "myLibrary" << "MySourceList" << "source1.tcl"
+            << "COMMANDS" << "com1";
+
+    //multiple commands
+    args[4] << "myLibrary" << "MySourceList" << "source1.tcl"
+            << "COMMANDS" << "com1" << "com2";
+            
+    //line from the cmake example
+    args[NUM_TESTDATA - 1] << "myLibrary" << "SOURCES"
+            << "MySourceList" << "class1" << "class2"
+            << "COMMANDS" << "first_command" << "second_command";
+    
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+
+    for ( int i = 0; i < NUM_TESTDATA; i++)
+    {
+        funcs[i].addArguments( args[i] );
+        QTest::newRow( qPrintable(QString::number(i)) ) << funcs[i];
+    }
 }
 
 void VtkWrapTclAstTest::testBadParse()
@@ -46,6 +87,32 @@ void VtkWrapTclAstTest::testBadParse()
 
 void VtkWrapTclAstTest::testBadParse_data()
 {
+    QTest::addColumn<CMakeFunctionDesc>("function");
+
+    const int NUM_TESTDATA = 5;
+    CMakeFunctionDesc funcs[NUM_TESTDATA];
+    QStringList args[NUM_TESTDATA];
+
+    for ( int i = 0; i < NUM_TESTDATA; i++ )
+        funcs[i].name = "VTK_WRAP_TCL";
+
+    funcs[NUM_TESTDATA - 1].name = "wrong_name";
+
+    args[0] << "myLibrary"; //no source list and no sources
+    args[1] << "myLibrary" << "MySourceList"; //source list but no sources
+    args[2] << "myLibrary" << "SOURCES" << "MySourceList"; //source list but no sources
+    args[3] << "myLibrary" << "SOURCES" << "MySourceList" << "foo.tcl";
+    args[NUM_TESTDATA - 1] << "myLibrary" << "SOURCES"
+            << "MySourceList" << "class1" << "class2"
+            << "COMMANDS" << "first_command";
+    
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+
+    for ( int i = 0; i < NUM_TESTDATA; i++)
+    {
+        funcs[i].addArguments( args[i] );
+        QTest::newRow( qPrintable(QString::number(i)) ) << funcs[i];
+    }
 }
 
 #include "cmake_vtkwraptclast_test.moc"
