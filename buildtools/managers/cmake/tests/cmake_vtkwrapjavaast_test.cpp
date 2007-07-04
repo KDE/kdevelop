3078a78688962d6bee5b98f5e6cb5daef34435d7
Matt Rogers <mattr@kde.org>
2007-07-04 02:44:29 +0000

    add parsing data for VTK_WRAP_JAVA and VTK_WRAP_PYTHON (both deprecated)

--- buildtools/managers/cmake/tests/cmake_vtkwrapjavaast_test.cpp
+++ buildtools/managers/cmake/tests/cmake_vtkwrapjavaast_test.cpp
@@ -34,6 +34,28 @@ void VtkWrapJavaAstTest::testGoodParse()
 
 void VtkWrapJavaAstTest::testGoodParse_data()
 {
+    QTest::addColumn<CMakeFunctionDesc>("function");
+
+    const int NUM_TESTDATA = 2;
+    CMakeFunctionDesc funcs[NUM_TESTDATA];
+    QStringList args[NUM_TESTDATA];
+
+    for ( int i = 0; i < NUM_TESTDATA; i++ )
+        funcs[i].name = "VTK_WRAP_JAVA";
+
+    //test lowercase too
+    funcs[NUM_TESTDATA - 1].name = funcs[NUM_TESTDATA - 1].name.toLower();
+
+    args[0] << "myLibrary" << "MySourceList" << "source1";
+    args[1] << "myLibrary" << "MySourceList" << "source1" << "source2";
+
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+
+    for ( int i = 0; i < NUM_TESTDATA; i++)
+    {
+        funcs[i].addArguments( args[i] );
+        QTest::newRow( qPrintable(QString::number(i)) ) << funcs[i];
+    }
 }
 
 void VtkWrapJavaAstTest::testBadParse()
@@ -46,6 +68,28 @@ void VtkWrapJavaAstTest::testBadParse()
 
 void VtkWrapJavaAstTest::testBadParse_data()
 {
+    QTest::addColumn<CMakeFunctionDesc>("function");
+
+    const int NUM_TESTDATA = 3;
+    CMakeFunctionDesc funcs[NUM_TESTDATA];
+    QStringList args[NUM_TESTDATA];
+
+    for ( int i = 0; i < NUM_TESTDATA; i++ )
+        funcs[i].name = "VTK_WRAP_JAVA";
+
+    funcs[NUM_TESTDATA - 1].name = "wrong_name";
+
+    args[0] << "myLibrary"; //no source list and no sources
+    args[1] << "myLibrary" << "MySourceList"; //source list but no sources
+    args[NUM_TESTDATA - 1] << "myLibrary" << "MySourceList" << "source1";
+
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+
+    for ( int i = 0; i < NUM_TESTDATA; i++)
+    {
+        funcs[i].addArguments( args[i] );
+        QTest::newRow( qPrintable(QString::number(i)) ) << funcs[i];
+    }
 }
 
 #include "cmake_vtkwrapjavaast_test.moc"
--- buildtools/managers/cmake/tests/cmake_vtkwrappythonast_test.cpp
+++ buildtools/managers/cmake/tests/cmake_vtkwrappythonast_test.cpp
@@ -34,6 +34,28 @@ void VtkWrapPythonAstTest::testGoodParse()
 
 void VtkWrapPythonAstTest::testGoodParse_data()
 {
+    QTest::addColumn<CMakeFunctionDesc>("function");
+
+    const int NUM_TESTDATA = 2;
+    CMakeFunctionDesc funcs[NUM_TESTDATA];
+    QStringList args[NUM_TESTDATA];
+
+    for ( int i = 0; i < NUM_TESTDATA; i++ )
+        funcs[i].name = "VTK_WRAP_PYTHON";
+
+    //test lowercase too
+    funcs[NUM_TESTDATA - 1].name = funcs[NUM_TESTDATA - 1].name.toLower();
+
+    args[0] << "myLibrary" << "MySourceList" << "source1";
+    args[1] << "myLibrary" << "MySourceList" << "source1" << "source2";
+
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+
+    for ( int i = 0; i < NUM_TESTDATA; i++)
+    {
+        funcs[i].addArguments( args[i] );
+        QTest::newRow( qPrintable(QString::number(i)) ) << funcs[i];
+    }
 }
 
 void VtkWrapPythonAstTest::testBadParse()
@@ -46,6 +68,28 @@ void VtkWrapPythonAstTest::testBadParse()
 
 void VtkWrapPythonAstTest::testBadParse_data()
 {
+    QTest::addColumn<CMakeFunctionDesc>("function");
+
+    const int NUM_TESTDATA = 3;
+    CMakeFunctionDesc funcs[NUM_TESTDATA];
+    QStringList args[NUM_TESTDATA];
+
+    for ( int i = 0; i < NUM_TESTDATA; i++ )
+        funcs[i].name = "VTK_WRAP_PYTHON";
+
+    funcs[NUM_TESTDATA - 1].name = "wrong_name";
+
+    args[0] << "myLibrary"; //no source list and no sources
+    args[1] << "myLibrary" << "MySourceList"; //source list but no sources
+    args[NUM_TESTDATA - 1] << "myLibrary" << "MySourceList" << "source1";
+
+    QTest::addColumn<CMakeFunctionDesc>( "function" );
+
+    for ( int i = 0; i < NUM_TESTDATA; i++)
+    {
+        funcs[i].addArguments( args[i] );
+        QTest::newRow( qPrintable(QString::number(i)) ) << funcs[i];
+    }
 }
 
 #include "cmake_vtkwrappythonast_test.moc"
