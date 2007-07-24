865756a9a10d35a1641d338fe9eedbdebab9e417
Dukju Ahn <dukjuahn@gmail.com>
2007-07-24 19:20:11 +0000

    SVN_SILENT : forgot to add copyright.

--- plugins/subversion/svn_copywidgets.cpp
+++ plugins/subversion/svn_copywidgets.cpp
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Dukju Ahn                                       *
+ *   dukjuahn@gmail.com                                                    *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "svn_copywidgets.h"
 #include "svn_revisionwidget.h"
 #include <kurl.h>
