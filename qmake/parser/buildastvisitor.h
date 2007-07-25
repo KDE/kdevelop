d4aef6c09dc3576a4726742e3c690f37da884aa8
Andreas Pakulat <apaku@gmx.de>
2007-07-25 17:27:55 +0000

    Unification of copyright lines, follow KDE format.
    svn path=/trunk/KDE/kdevelop/buildtools/builders/qmakebuilder/; revision=692479

--- qmake/parser/buildastvisitor.h
+++ qmake/parser/buildastvisitor.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   This file is part of KDevelop                                         *
- *   Copyright (C) 2007 Andreas Pakulat <pakulat@rostock.zgdv.de>                     *
+ *   Copyright 2007 Andreas Pakulat <pakulat@rostock.zgdv.de>                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
