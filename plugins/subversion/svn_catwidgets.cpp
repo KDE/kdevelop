8f3b6effa874f8a3510ce03e6c5c7bfdc16ec003
Dukju Ahn <dukjuahn@gmail.com>
2007-07-29 17:00:45 +0000

    SVN_SILENT: forgot to add license.

--- plugins/subversion/svn_catwidgets.cpp
+++ plugins/subversion/svn_catwidgets.cpp
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *   Copyright 2007 Dukju Ahn <dukjuahn@gmail.com>                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "svn_catwidgets.h"
 #include "svn_revisionwidget.h"
 #include <kurlrequester.h>
--- plugins/subversion/svn_catwidgets.h
+++ plugins/subversion/svn_catwidgets.h
@@ -1,3 +1,13 @@
+/***************************************************************************
+ *   Copyright 2007 Dukju Ahn <dukjuahn@gmail.com>                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef SVN_CATWIDGETS_H
 #define SVN_CATWIDGETS_H
 
