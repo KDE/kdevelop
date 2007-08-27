533002628dc91b82cd3faa6d9912a9a010dbdef5
Thiago Macieira <thiago@kde.org>
2007-08-27 18:21:54 +0000

    You don't use KStaticDeleter, so don't include it

--- shell/plugincontroller.cpp
+++ shell/plugincontroller.cpp
@@ -41,7 +41,6 @@ Boston, MA 02110-1301, USA.
 #include <kstandarddirs.h>
 #include <kaction.h>
 #include <kxmlguifactory.h>
-#include <kstaticdeleter.h>
 #include <kmenu.h>
 #include <QtGui/QAction>
 
