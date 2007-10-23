a66cc36b1b7b3294b83aa550025be2eacdc06f8b
Andreas Pakulat <apaku@gmx.de>
2007-10-23 22:58:44 +0000

    This should fix the lag that could be experienced, specially on larger projects. As David pointed out the updatedSourceInfo signal is more like a "the whole code-model changed" thing.
    Everybody who experiences short delays when typing normal code, please svn up and test. I couldn't get the delay to work here the last days so I'm not 100% sure it fixes this problem (but I'm pretty confident)
    CCMAIL:kdevelop-devel@kdevelop.org
    CCMAIL:cyb@lepi.org

--- languages/cpp/cppsupportpart.cpp
+++ languages/cpp/cppsupportpart.cpp
@@ -2533,6 +2533,9 @@ void CppSupportPart::parseEmit( ParseEmitWaiting::Processed files ) {
 				QStringList l = files.res;
 				while(!l.isEmpty() ) {
 					emit addedSourceInfo( l.front() );
+					emit aboutToRemoveSourceInfo( l.front() );
+					emit removedSourceInfo( l.front() );
+					emit addedSourceInfo( l.front() );
 					l.pop_front();
 				}
 
@@ -2542,9 +2545,11 @@ void CppSupportPart::parseEmit( ParseEmitWaiting::Processed files ) {
 				QStringList l = files.res;
 				while( !l.isEmpty() ) {
 					emit codeModelUpdated( l.front() );
+					emit aboutToRemoveSourceInfo( l.front() );
+					emit removedSourceInfo( l.front() );
+					emit addedSourceInfo( l.front() );
 					l.pop_front();
 				}
-				emit updatedSourceInfo();
 			}
 		}
 		kdDebug( 9007 ) << "files in code-model after parseEmit: " << codeModel()->fileList().count() << " before: " << oldFileCount << endl;
