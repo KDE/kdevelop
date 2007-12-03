a4d7a026214c91b1743d8a73ec0d0f2583d5c6e5
Andreas Pakulat <apaku@gmx.de>
2007-12-03 23:17:12 +0000

    Properly depend on all the files in the template. This makes the build update the tar.bz2 properly when changing one of the template-files

--- cmake/modules/KDevelopMacros.cmake
+++ cmake/modules/KDevelopMacros.cmake
@@ -5,42 +5,52 @@ macro(kdevelop_add_app_templates _templateNames)
         get_filename_component(_tmp_file ${_templateName} ABSOLUTE)
         get_filename_component(_baseName ${_tmp_file} NAME_WE)
         if(WIN32)
-			set(_template ${CMAKE_CURRENT_BINARY_DIR}/${_baseName}.zip)
-		else(WIN32)
-			set(_template ${CMAKE_CURRENT_BINARY_DIR}/${_baseName}.tar.bz2)
-		endif(WIN32)
+            set(_template ${CMAKE_CURRENT_BINARY_DIR}/${_baseName}.zip)
+        else(WIN32)
+            set(_template ${CMAKE_CURRENT_BINARY_DIR}/${_baseName}.tar.bz2)
+        endif(WIN32)
+
+
+        file(GLOB _files "${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}/*")
+        set(_deps)
+        message("Found files to check: ${_files}")
+        foreach(_file ${_files})
+            get_filename_component(_fileName ${_file} NAME)
+            message("Checking filename: ${_fileName}")
+            string(COMPARE NOTEQUAL ${_fileName} .kdev_ignore _v1)
+            string(REGEX MATCH "\\.svn" _v2 ${_fileName} )
+            if(WIN32)
+                string(REGEX MATCH "_svn" _v3 ${_fileName} )
+            else(WIN32)
+                set(_v3 FALSE)
+            endif(WIN32)
+            message("Check result: ${_v1} ${_v2} ${_v3}")
+            if ( _v1 AND NOT _v2 AND NOT _v3 )
+                set(_deps ${_deps} ${_file})
+            endif ( _v1 AND NOT _v2 AND NOT _v3 )
+        endforeach(_file)
+
+        message("Using files as deps: ${_deps} ")
 
         add_custom_target(${_baseName} ALL DEPENDS ${_template})
 
-        #file(GLOB _files ${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}/*)
-        #set(_deps)
-        #foreach(_file ${_files})
-        #    get_filename_component(_fileName ${_file} NAME)
-        #    string(COMPARE NOTEQUAL ${_fileName} .kdev_ignore _v1)
-        #    string(SUBSTRING ${_fileName} 0 4 _svn)
-        #    string(COMPARE NOTEQUAL ${_svn} .svn _v2)
-        #    if ( ${_v1} AND ${_v2} )
-        #    set(_deps ${_deps} ${_file})
-        #    endif ( ${_v1} AND ${_v2} )
-        #endforeach(_file)
-
-		if(WIN32)
-			add_custom_command(OUTPUT ${_template}
-	            COMMAND zip ARGS -r
-	                ${_template} .
-					-x .svn .kdev_ignore
+        if(WIN32)
+            add_custom_command(OUTPUT ${_template}
+                COMMAND zip ARGS -r
+                    ${_template} .
+                    -x .svn _svn .kdev_ignore
                 WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}
-	            #DEPENDS ${_deps}
-	        )
-		else(WIN32)
-			add_custom_command(OUTPUT ${_template}
-	            COMMAND tar ARGS -c -C ${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}
-	                --exclude .kdev_ignore --exclude .svn
-	                -j -f ${_template} .
-	            #DEPENDS ${_deps}
-	        )
-		endif(WIN32)
-        
+                DEPENDS ${_deps}
+            )
+        else(WIN32)
+            add_custom_command(OUTPUT ${_template}
+                COMMAND tar ARGS -c -C ${CMAKE_CURRENT_SOURCE_DIR}/${_templateName}
+                    --exclude .kdev_ignore --exclude .svn
+                    -j -f ${_template} .
+                DEPENDS ${_deps}
+            )
+        endif(WIN32)
+
 
         #install( FILES ${_templateName}/${_templateName}.kdevtemplate
         #    DESTINATION ${DATA_INSTALL_DIR}/kdevappwizard/templates )
