6aab35cb06236c65c2bde0d5af7bfaa0576bb08f
Andreas Pakulat <apaku@gmx.de>
2008-01-19 21:35:44 +0000

    Some more improvements, from Andreas Schneider. Also introduces 3 new variables for boost major,minor,subminor versions

--- cmake/modules/FindBoost.cmake
+++ cmake/modules/FindBoost.cmake
@@ -14,7 +14,7 @@
 #
 # The components list needs to be the actual names of boost libraries, that is
 # the part of the actual library files that differ on different libraries. So
-# its "date_time" for "libboost_date_time...". Anything else will result in 
+# its "date_time" for "libboost_date_time...". Anything else will result in
 # errors
 #
 # Variables used by this module, they can change the default behaviour:
@@ -22,11 +22,11 @@
 #                                boost libraries.
 #  Boost_ADDITIONAL_VERSIONS     A list of version numbers to use for searching
 #                                the boost include directory. The default list
-#                                of version numbers is: 
+#                                of version numbers is:
 #                                1.33, 1.33.0, 1.33.1, 1.34, 1.34.0, 1.34.1
 #                                If you want to look for an older or newer
 #                                version set this variable to a list of
-#                                strings, where each string contains a number, i.e. 
+#                                strings, where each string contains a number, i.e.
 #                                SET(Boost_ADDITIONAL_VERSIONS "0.99.0" "1.35.0")
 #  Boost_ROOT                    Preferred installation prefix for searching for Boost,
 #                                set this if the module has problems finding the proper Boost installation
@@ -40,24 +40,28 @@
 #
 # Variables defined by this module:
 #
-#  Boost_FOUND                System has Boost, this means the include dir was found, 
+#  Boost_FOUND                System has Boost, this means the include dir was found,
 #                             as well as all the libraries specified in the COMPONENTS list
 #  Boost_INCLUDE_DIRS         Boost include directories, not cached
 #  Boost_INCLUDE_DIR          This is almost the same as above, but this one is cached and may be
 #                             modified by advanced users
 #  Boost_LIBRARIES            Link these to use the Boost libraries that you specified, not cached
 #  Boost_LIBRARY_DIRS         The path to where the Boost library files are.
-#  Boost_VERSION              The version number of the boost libraries that have been found, 
+#  Boost_VERSION              The version number of the boost libraries that have been found,
 #                             same as in version.hpp from Boost
 #  Boost_LIB_VERSION          The version number in filename form as its appended to the library filenames
-# For each component you list the following variables are set. 
+#  Boost_MAJOR_VERSION        major version number of boost
+#  Boost_MINOR_VERSION        minor version number of boost
+#  Boost_SUBMINOR_VERSION     subminor version number of boost
+
+# For each component you list the following variables are set.
 # ATTENTION: The component names need to be in lower case, just as the boost
 # library names however the cmake variables use upper case for the component
 # part. So you'd get Boost_SERIALIZATION_FOUND for example.
 #
 #  Boost_${COMPONENT}_FOUND             True IF the Boost library "component" was found.
 #  Boost_${COMPONENT}_LIBRARY           The absolute path of the Boost library "component".
-#  Boost_${COMPONENT}_LIBRARY_DEBUG     The absolute path of the debug version of the 
+#  Boost_${COMPONENT}_LIBRARY_DEBUG     The absolute path of the debug version of the
 #                                       Boost library "component".
 #  Boost_${COMPONENT}_LIBRARY_RELEASE   The absolute path of the release version of the
 #                                       Boost library "component"
@@ -110,7 +114,7 @@ MACRO (_Boost_ADJUST_LIB_VARS basename)
       SET(Boost_${basename}_LIBRARY       ${Boost_${basename}_LIBRARY_RELEASE})
       SET(Boost_${basename}_LIBRARIES     ${Boost_${basename}_LIBRARY_RELEASE})
     ENDIF (Boost_${basename}_LIBRARY_RELEASE AND NOT Boost_${basename}_LIBRARY_DEBUG)
-    
+
     # if only the debug version was found, set the release variable also to the debug version
     IF (Boost_${basename}_LIBRARY_DEBUG AND NOT Boost_${basename}_LIBRARY_RELEASE)
       SET(Boost_${basename}_LIBRARY_RELEASE ${Boost_${basename}_LIBRARY_DEBUG})
@@ -120,16 +124,16 @@ MACRO (_Boost_ADJUST_LIB_VARS basename)
 
     IF (Boost_${basename}_LIBRARY)
       SET(Boost_${basename}_LIBRARY ${Boost_${basename}_LIBRARY} CACHE FILEPATH "The Boost ${basename} library")
-      GET_FILENAME_COMPONENT(Boost_LIBRARY_DIRS ${Boost_${basename}_LIBRARY} PATH)
+      GET_FILENAME_COMPONENT(Boost_LIBRARY_DIRS "${Boost_${basename}_LIBRARY}" PATH)
       SET(Boost_${basename}_FOUND 1)
     ENDIF (Boost_${basename}_LIBRARY)
 
   ENDIF (Boost_INCLUDE_DIR )
   # Make variables changeble to the advanced user
   MARK_AS_ADVANCED(
-      Boost_${basename}_LIBRARY 
-      Boost_${basename}_LIBRARY_RELEASE 
-      Boost_${basename}_LIBRARY_DEBUG 
+      Boost_${basename}_LIBRARY
+      Boost_${basename}_LIBRARY_RELEASE
+      Boost_${basename}_LIBRARY_DEBUG
   )
 ENDMACRO (_Boost_ADJUST_LIB_VARS)
 
@@ -168,7 +172,7 @@ ELSE (_boost_IN_CACHE)
     #D:/boost/include
     /sw/local/include
   )
-  
+
   SET(_boost_LIBRARIES_SEARCH_DIRS
     C:/boost/lib
     "C:/Program Files/boost/boost_${Boost_REQUIRED_VERSION}/lib"
@@ -211,27 +215,34 @@ ELSE (_boost_IN_CACHE)
         boost-${_boost_VER}
       )
       STRING(REGEX REPLACE "([0-9]+)\\.([0-9]+)\\.([0-9]+)" "\\1_\\2_\\3" _boost_PATH_SUFFIX ${_boost_PATH_SUFFIX})
-    
+
 
       FIND_PATH(Boost_INCLUDE_DIR
           NAMES         boost/config.hpp
           PATHS         ${_boost_INCLUDE_SEARCH_DIRS}
           PATH_SUFFIXES ${_boost_PATH_SUFFIX}
       )
-    
+
       # Extract Boost_VERSION and Boost_LIB_VERSION from version.hpp
       # Read the whole file:
       #
       SET(BOOST_VERSION 0)
       SET(BOOST_LIB_VERSION "")
       FILE(READ "${Boost_INCLUDE_DIR}/boost/version.hpp" _boost_VERSION_HPP_CONTENTS)
-    
-      STRING(REGEX REPLACE ".*#define BOOST_VERSION ([0-9]+).*" "\\1" Boost_VERSION ${_boost_VERSION_HPP_CONTENTS})
-      STRING(REGEX REPLACE ".*#define BOOST_LIB_VERSION \"([0-9_]+)\".*" "\\1" Boost_LIB_VERSION ${_boost_VERSION_HPP_CONTENTS})
-    
+
+      STRING(REGEX REPLACE ".*#define BOOST_VERSION ([0-9]+).*" "\\1" Boost_VERSION "${_boost_VERSION_HPP_CONTENTS}")
+      STRING(REGEX REPLACE ".*#define BOOST_LIB_VERSION \"([0-9_]+)\".*" "\\1" Boost_LIB_VERSION "${_boost_VERSION_HPP_CONTENTS}")
+
       SET(Boost_LIB_VERSION ${Boost_LIB_VERSION} CACHE STRING "The library version string for boost libraries")
       SET(Boost_VERSION ${Boost_VERSION} CACHE STRING "The version number for boost libraries")
-    
+      
+      IF(NOT "${Boost_VERSION}" STREQUAL "0")
+        MATH(EXPR Boost_MAJOR_VERSION "${Boost_VERSION} / 100000")
+        MATH(EXPR Boost_MINOR_VERSION "${Boost_VERSION} / 100 % 1000")
+        MATH(EXPR Boost_SUBMINOR_VERSION "${Boost_VERSION} % 100")
+      ENDIF(NOT "${Boost_VERSION}" STREQUAL "0")
+
+
     ENDIF( NOT Boost_INCLUDE_DIR )
   ENDFOREACH(_boost_VER)
 
@@ -246,7 +257,7 @@ ELSE (_boost_IN_CACHE)
   ENDIF(MSVC71)
    IF (MSVC80)
     SET (_boost_COMPILER "-vc80")
-  ENDIF(MSVC80) 
+  ENDIF(MSVC80)
   IF (MINGW)
     SET (_boost_COMPILER "-mgw")
   ENDIF(MINGW)
@@ -266,19 +277,19 @@ ELSE (_boost_IN_CACHE)
             ARGS --version
             OUTPUT_VARIABLE _boost_COMPILER_VERSION
         )
-        STRING(REGEX REPLACE ".* ([0-9])\\.([0-9])\\.[0-9] .*" "\\1\\2" 
+        STRING(REGEX REPLACE ".* ([0-9])\\.([0-9])\\.[0-9] .*" "\\1\\2"
                _boost_COMPILER_VERSION ${_boost_COMPILER_VERSION})
         SET (_boost_COMPILER "-gcc${_boost_COMPILER_VERSION}")
       ENDIF (NOT CMAKE_COMPILER_IS_GNUCC)
     ENDIF (APPLE)
   ENDIF(UNIX)
-  
+
   SET (_boost_MULTITHREADED "-mt")
- 
+
   IF( Boost_USE_NONMULTITHREADED )
     SET (_boost_MULTITHREADED "")
   ENDIF( Boost_USE_NONMULTITHREADED )
-  
+
 
   IF (WIN32)
     SET (Boost_ABI_TAG "g")
@@ -293,18 +304,18 @@ ELSE (_boost_IN_CACHE)
     SET( Boost_{UPPERCOMPONENT}_LIBRARY FALSE)
     SET( Boost_{UPPERCOMPONENT}_LIBRARY_RELEASE FALSE)
     SET( Boost_{UPPERCOMPONENT}_LIBRARY_DEBUG FALSE)
-    FIND_LIBRARY(Boost_${UPPERCOMPONENT}_LIBRARY_RELEASE 
-        NAMES 
-              ${Boost_LIB_PREFIX}boost_${COMPONENT}${_boost_COMPILER}${_boost_MULTITHREADED}-${Boost_LIB_VERSION}
-              ${Boost_LIB_PREFIX}boost_${COMPONENT}${_boost_MULTITHREADED}
-        PATHS ${_boost_LIBRARIES_SEARCH_DIRS}
+    FIND_LIBRARY(Boost_${UPPERCOMPONENT}_LIBRARY_RELEASE
+        NAMES  ${Boost_LIB_PREFIX}boost_${COMPONENT}${_boost_COMPILER}${_boost_MULTITHREADED}-${Boost_LIB_VERSION}
+               ${Boost_LIB_PREFIX}boost_${COMPONENT}${_boost_MULTITHREADED}
+               ${Boost_LIB_PREFIX}boost_${COMPONENT}
+        PATHS  ${_boost_LIBRARIES_SEARCH_DIRS}
     )
 
     FIND_LIBRARY(Boost_${UPPERCOMPONENT}_LIBRARY_DEBUG
-        NAMES 
-              ${Boost_LIB_PREFIX}boost_${COMPONENT}${_boost_COMPILER}${_boost_MULTITHREADED}${Boost_ABI_TAG}-${Boost_LIB_VERSION}
-              ${Boost_LIB_PREFIX}boost_${COMPONENT}${_boost_MULTITHREADED}${Boost_ABI_TAG}
-        PATHS ${_boost_LIBRARIES_SEARCH_DIRS}
+        NAMES  ${Boost_LIB_PREFIX}boost_${COMPONENT}${_boost_COMPILER}${_boost_MULTITHREADED}${Boost_ABI_TAG}-${Boost_LIB_VERSION}
+               ${Boost_LIB_PREFIX}boost_${COMPONENT}${_boost_MULTITHREADED}${Boost_ABI_TAG}
+               ${Boost_LIB_PREFIX}boost_${COMPONENT}${Boost_ABI_TAG}
+        PATHS  ${_boost_LIBRARIES_SEARCH_DIRS}
     )
 
     _Boost_ADJUST_LIB_VARS(${UPPERCOMPONENT})
@@ -316,10 +327,10 @@ ELSE (_boost_IN_CACHE)
   SET(Boost_INCLUDE_DIRS
     ${Boost_INCLUDE_DIR}
   )
-  
+
   # MESSAGE(STATUS "Boost_INCLUDE_DIRS: ${Boost_INCLUDE_DIRS}")
   # MESSAGE(STATUS "Boost_LIBRARIES: ${Boost_LIBRARIES}")
- 
+
   SET(Boost_FOUND FALSE)
   IF(Boost_INCLUDE_DIR)
     SET( Boost_FOUND TRUE )
@@ -332,7 +343,7 @@ ELSE (_boost_IN_CACHE)
   ELSE(Boost_INCLUDE_DIR)
     SET( Boost_FOUND FALSE)
   ENDIF(Boost_INCLUDE_DIR)
-  
+
   IF (Boost_FOUND)
       IF (NOT Boost_FIND_QUIETLY)
         MESSAGE(STATUS "Found The Following Boost Libraries:")
@@ -343,22 +354,23 @@ ELSE (_boost_IN_CACHE)
 	    SET(Boost_LIBRARIES ${Boost_LIBRARIES} ${Boost_${UPPERCOMPONENT}_LIBRARY})
           ENDIF ( Boost_${UPPERCOMPONENT}_FOUND )
         ENDFOREACH(COMPONENT)
+	MESSAGE(STATUS "Boost Version: ${Boost_MAJOR_VERSION}.${Boost_MINOR_VERSION}.${Boost_SUBMINOR_VERSION}")
       ENDIF(NOT Boost_FIND_QUIETLY)
   ELSE (Boost_FOUND)
       IF (Boost_FIND_REQUIRED)
         MESSAGE(FATAL_ERROR "Please install the Boost libraries AND development packages")
       ENDIF(Boost_FIND_REQUIRED)
   ENDIF(Boost_FOUND)
-  
+
   # Under Windows, automatic linking is performed, so no need to specify the libraries.
   IF (WIN32)
       SET(Boost_LIBRARIES "")
   ENDIF(WIN32)
-  
+
   # show the Boost_INCLUDE_DIRS AND Boost_LIBRARIES variables only in the advanced view
-  MARK_AS_ADVANCED(Boost_INCLUDE_DIRS 
-      Boost_LIBRARIES 
-      Boost_LIBRARY_DIRS 
+  MARK_AS_ADVANCED(Boost_INCLUDE_DIRS
+      Boost_LIBRARIES
+      Boost_LIBRARY_DIRS
   )
 ENDIF(_boost_IN_CACHE)
 
