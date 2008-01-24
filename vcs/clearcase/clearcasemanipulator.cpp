625fea3fb0012d311b76e485557b31a9cc1957d3
Andreas Pakulat <apaku@gmx.de>
2008-01-24 14:02:52 +0000

    Use our own getline also on Solaris which misses it as well. BUG:140907

--- vcs/clearcase/clearcasemanipulator.cpp
+++ vcs/clearcase/clearcasemanipulator.cpp
@@ -20,7 +20,7 @@
 #include <libgen.h>
 #include <errno.h>
 
-#if defined(Q_OS_MACX) || defined(MACOSX)
+#if defined(Q_OS_MACX) || defined(MACOSX) || defined(Q_OS_SOLARIS)
 //this function is taken from GNU libc
 //it does not exist on macos
 int getline(char **lineptr, size_t *n, FILE *stream)
