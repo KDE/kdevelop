faeb5e1e55ff03a4550ecac9e2ea2d9fe4e0241d
David Nolden <david.nolden.kde@art-master.de>
2008-01-24 18:41:46 +0000

    Add a new function ILanguageSupport::standardContext(...) that is intended to return the default context to use for things like navigation, use-highlighting, etc. if there is multiple versions of a parsed file. The default-implementation is fine for all languages that do don't/can't create different versions of a single parsed file.

--- language/interfaces/ilanguagesupport.cpp
+++ language/interfaces/ilanguagesupport.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright 2007 Alexander Dymo  <adymo@kdevelop.org>            *
+ *   Copyright 2008 David Nolden  <david.nolden.kdevelop@art-master.de>    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU Library General Public License as       *
@@ -16,39 +16,14 @@
  *   Free Software Foundation, Inc.,                                       *
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
  ***************************************************************************/
-#ifndef ILANGUAGESUPPORT_H
-#define ILANGUAGESUPPORT_H
 
-#include <kurl.h>
-
-#include <iextension.h>
-#include "../languageexport.h"
+#include "ilanguagesupport.h"
+#include "duchain.h"
 
 namespace KDevelop {
 
-class ParseJob;
-class ILanguage;
-class TopDUContext;
-
-class KDEVPLATFORMLANGUAGE_EXPORT ILanguageSupport {
-public:
-    virtual ~ILanguageSupport() {}
-
-    /** @return the name of the language.*/
-    virtual QString name() const = 0;
-    /** @return the parse job that is used by background parser to parse given @p url.*/
-    virtual ParseJob *createParseJob(const KUrl &url) = 0;
-    /** @return the language for this support.*/
-    virtual ILanguage *language() = 0;
-    /** @return the standard context used by this language for the given url.
-      * Only important for languages that can parse multiple different versions of a file, like C++ due to the preprocessor.
-      * The default-implementation for other languages is "return DUChain::chainForDocument(url);" */
-    virtual TopDUContext *standardContext(const KUrl& url);
-};
-
+TopDUContext* ILanguageSupport::standardContext(const KUrl& url) {
+  return DUChain::self()->chainForDocument(url);
 }
 
-KDEV_DECLARE_EXTENSION_INTERFACE_NS( KDevelop, ILanguageSupport, "org.kdevelop.ILanguageSupport")
-Q_DECLARE_INTERFACE( KDevelop::ILanguageSupport, "org.kdevelop.ILanguageSupport")
-
-#endif
+}
