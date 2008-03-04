011e97129a086637b2d4525b6256e396fc2a0ab0
Andreas Pakulat <apaku@gmx.de>
2008-03-04 17:36:41 +0000

    Also add Q_OS_FREEBSD as requested from Michael Nottebrock. CCBUG:140907

--- vcs/clearcase/clearcasemanipulator.cpp
+++ vcs/clearcase/clearcasemanipulator.cpp
@@ -20,7 +20,7 @@
 #include <libgen.h>
 #include <errno.h>
 
-#if defined(Q_OS_MACX) || defined(MACOSX) || defined(Q_OS_SOLARIS)
+#if defined(Q_OS_MACX) || defined(MACOSX) || defined(Q_OS_SOLARIS) || defines(Q_OS_FREEBSD)
 //this function is taken from GNU libc
 //it does not exist on macos
 int getline(char **lineptr, size_t *n, FILE *stream)
