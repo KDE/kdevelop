994f0cc7010f6efd53be63f2124ae473399e000c
Andreas Pakulat <apaku@gmx.de>
2008-04-15 08:15:33 +0000

    Merge the duchain stuff, I won't break the existing functionality with it and its just easier to work in trunk/ on this.

---
+++ buildtools/managers/qmake/duchain/contextbuilder.cpp
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *   This file is part of KDevelop                                         *
+ *   Copyright 2008 Andreas Pakulat <apaku@gmx.de>                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include "contextbuilder.h"
+#include "qmakeeditorintegrator.h"
+
+ContextBuilder::ContextBuilder()
+    : QMake::ASTVisitor(), KDevelop::BaseContextBuilder<QMake::AST>( new QMakeEditorIntegrator() )
+{
+}
+
+ContextBuilder::~ContextBuilder()
+{
+}
+
+void ContextBuilder::supportBuild( QMake::AST* node )
+{
+}
+
+void ContextBuilder::setContextOnNode( QMake::AST* node, KDevelop::DUContext* ctx )
+{
+    node->context = ctx;
+}
+
+KDevelop::DUContext* ContextBuilder::contextFromNode( QMake::AST* node )
+{
+    return node->context;
+}
+
+KTextEditor::Range ContextBuilder::editorFindRange( QMake::AST* fromRange, QMake::AST* toRange )
+{
+    QMakeEditorIntegrator* ed = editor<QMakeEditorIntegrator>();
+    return ed->findRange(fromRange, toRange);
+}
+
+const KDevelop::QualifiedIdentifier ContextBuilder::identifierForNode( QMake::AST* node )
+{
+    QMake::ValueAST* val = static_cast<QMake::ValueAST*>( node );
+    setIdentifier( val->value() );
+    return qualifiedIdentifier();
+}
+
---
+++ buildtools/managers/qmake/duchain/contextbuilder.h
@@ -0,0 +1,50 @@
+/***************************************************************************
+ *   This file is part of KDevelop                                         *
+ *   Copyright 2008 Andreas Pakulat <apaku@gmx.de>                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef QMAKE_CONTEXTBUILDER_H
+#define QMAKE_CONTEXTBUILDER_H
+
+#include <language/duchain/basecontextbuilder.h>
+#include "parser/qmakeastvisitor.h"
+#include "parser/ast.h"
+
+#include "qmakeduchainexport.h"
+
+class KUrl;
+
+namespace KDevelop
+{
+class DUContext;
+}
+
+class KDEVQMAKEDUCHAIN_EXPORT ContextBuilder : public QMake::ASTVisitor, public KDevelop::BaseContextBuilder<QMake::AST>
+{
+public:
+    ContextBuilder();
+    ~ContextBuilder();
+protected:
+  virtual void supportBuild( QMake::AST* node );
+  virtual void setContextOnNode( QMake::AST* node, KDevelop::DUContext* ctx );
+  virtual KDevelop::DUContext* contextFromNode( QMake::AST* node );
+  virtual KTextEditor::Range editorFindRange( QMake::AST* fromRange, QMake::AST* toRange );
+  virtual const KDevelop::QualifiedIdentifier identifierForNode( QMake::AST* );
+};
+
+#endif
