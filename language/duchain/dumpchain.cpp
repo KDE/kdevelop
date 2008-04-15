0d8cfd4447ad26df7ff4cc6eb49f51484a971b69
David Nolden <david.nolden.kde@art-master.de>
2008-04-15 12:41:36 +0000

    Change the debug area.

--- language/duchain/dumpchain.cpp
+++ language/duchain/dumpchain.cpp
@@ -47,7 +47,7 @@ DumpChain::~ DumpChain( )
 
 void DumpChain::dump( DUContext * context, bool imported )
 {
-  kDebug(9007) << QString(indent * 2, ' ') << (imported ? "==import==> Context " : "New Context ") << context << "\"" <<  context->localScopeIdentifier() << "\" [" << context->scopeIdentifier() << "]" << context->range().textRange() << " " << (dynamic_cast<TopDUContext*>(context) ? "top-context" : "");
+  kDebug() << QString(indent * 2, ' ') << (imported ? "==import==> Context " : "New Context ") << context << "\"" <<  context->localScopeIdentifier() << "\" [" << context->scopeIdentifier() << "]" << context->range().textRange() << " " << (dynamic_cast<TopDUContext*>(context) ? "top-context" : "");
   if( !context )
     return;
   if (!imported) {
@@ -55,14 +55,14 @@ void DumpChain::dump( DUContext * context, bool imported )
       
       //IdentifiedType* idType = dynamic_cast<IdentifiedType*>(dec->abstractType().data());
       
-      kDebug(9007) << QString((indent+1) * 2, ' ') << "Declaration: " << dec->toString() << /*(idType ? (" (type-identity: " + idType->identifier().toString() + ")") : QString()) <<*/ " [" << dec->qualifiedIdentifier() << "]" << dec << "(internal ctx" << dec->internalContext() << ")" << dec->range().textRange() << "," << (dec->isDefinition() ? "defined, " : (dec->definition() ? "" : "no definition, ")) << dec->uses().count() << "use(s).";
+      kDebug() << QString((indent+1) * 2, ' ') << "Declaration: " << dec->toString() << /*(idType ? (" (type-identity: " + idType->identifier().toString() + ")") : QString()) <<*/ " [" << dec->qualifiedIdentifier() << "]" << dec << "(internal ctx" << dec->internalContext() << ")" << dec->range().textRange() << "," << (dec->isDefinition() ? "defined, " : (dec->definition() ? "" : "no definition, ")) << dec->uses().count() << "use(s).";
       if (dec->definition())
-        kDebug(9007) << QString((indent+1) * 2 + 1, ' ') << "Definition:" << dec->definition()->range().textRange();
+        kDebug() << QString((indent+1) * 2 + 1, ' ') << "Definition:" << dec->definition()->range().textRange();
       QMap<HashedString, QList<SimpleRange> > uses = dec->uses();
       for(QMap<HashedString, QList<SimpleRange> >::const_iterator it = uses.begin(); it != uses.end(); ++it) {
-        kDebug(9007) << QString((indent+2) * 2, ' ') << "File:" << it.key().str();
+        kDebug() << QString((indent+2) * 2, ' ') << "File:" << it.key().str();
         foreach (const SimpleRange& range, *it)
-          kDebug(9007) << QString((indent+2) * 2+1, ' ') << "Use:" << range.textRange();
+          kDebug() << QString((indent+2) * 2+1, ' ') << "Use:" << range.textRange();
       }
     }
   }
