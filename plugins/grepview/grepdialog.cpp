52afb0b2f72df2024934c4df7e40fdd720b2dfc8
Alexander Dymo <adymo@kdevelop.org>
2008-05-05 21:13:38 +0000

    Add more file extensions for the list of ruby exts in grep widget (make this list same as in kdev3).

--- plugins/grepview/grepdialog.cpp
+++ plugins/grepview/grepdialog.cpp
@@ -63,7 +63,7 @@ QStringList __filepatterns = QStringList()
     << "*.pl"
     << "*.pp,*.pas"
     << "*.py"
-    << "*.js,*.css,*.yml,*.rb,*.rhtml,*.rjs,*.rxml"
+    << "*.js,*.css,*.yml,*.rb,*.rhtml,*.html.erb,*.rjs,*.js.rjs,*.rxml,*.xml.builder"
     << "CMakeLists.txt,*.cmake"
     << "*";
 
