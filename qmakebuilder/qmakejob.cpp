660acd57a2638e50831e2df98e0a97714e3aad34
Hamish Rodda <rodda@kde.org>
2008-06-09 06:21:19 +0000

    Large refactoring to allow running state of plugins to be shown to the user, and jobs to be killed...:
    Ported to new IRun*
    - gdb
    - valgrind
    
    Ported to KJob
    - build plugins (make, qmake, cmake)
    - grep plugin
    
    I tried to add the new Stop action to the main toolbar, but for some reason it doesn't show up??? (is there some code to remove actions from the main toolbar?)
    
    svn path=/trunk/KDE/kdevelop/buildtools/builders/qmakebuilder/; revision=818666

---
+++ qmakebuilder/qmakejob.cpp
@@ -0,0 +1,109 @@
+/* KDevelop QMake Support
+ *
+ * Copyright 2006-2007 Andreas Pakulat <apaku@gmx.de>
+ * Copyright 2008 Hamish Rodda <rodda@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "qmakejob.h"
+
+#include "imakebuilder.h"
+
+#include <config.h>
+
+#include <QtCore/QStringList>
+#include <QtCore/QSignalMapper>
+
+
+#include <projectmodel.h>
+
+#include <iproject.h>
+#include <icore.h>
+#include <iplugincontroller.h>
+#include <ioutputview.h>
+#include <outputmodel.h>
+#include <commandexecutor.h>
+#include <QtDesigner/QExtensionFactory>
+
+#include <kpluginfactory.h>
+#include <kpluginloader.h>
+#include <kconfig.h>
+#include <kconfiggroup.h>
+#include <kdialog.h>
+#include <kglobal.h>
+#include <klocale.h>
+#include <kdebug.h>
+
+QMakeJob::QMakeJob(QObject *parent)
+    : OutputJob(parent)
+{
+}
+
+void QMakeJob::start()
+{
+    if( !m_project ) {
+        setError(NoProjectError);
+        setErrorText(i18n("No project specified."));
+        return emitResult();
+    }
+
+    setStandardToolView(KDevelop::IOutputView::BuildView);
+    setTitle(i18n("QMake: %1", m_project->name()));
+    setBehaviours(KDevelop::IOutputView::AllowUserClose | KDevelop::IOutputView::AutoScroll);
+    setModel(new KDevelop::OutputModel(this), KDevelop::IOutputView::TakeOwnership);
+    startOutput();
+
+    m_item = m_project->projectItem();
+    QString cmd = qmakeBinary( m_project );
+    m_cmd = new KDevelop::CommandExecutor(cmd, this);
+    connect(m_cmd, SIGNAL(receivedStandardError(const QStringList&)),
+            m_model, SLOT(appendLines(const QStringList&) ) );
+    connect(m_cmd, SIGNAL(receivedStandardOutput(const QStringList&)),
+            m_model, SLOT(appendLines(const QStringList&) ) );
+    m_cmd->setWorkingDirectory( m_project->folder().toLocalFile() );
+    connect( m_cmd, SIGNAL( failed() ), this, SLOT( slotFailed() ) );
+    connect( m_cmd, SIGNAL( completed() ), this, SLOT( slotCompleted() ) );
+    m_cmd->start();
+}
+
+QString QMakeJob::qmakeBinary( KDevelop::IProject* project )
+{
+    KSharedConfig::Ptr cfg = project->projectConfiguration();
+    KConfigGroup group(cfg.data(), "QMake Builder");
+    KUrl v = group.readEntry("QMake Binary", KUrl( "file:///usr/bin/qmake" ) );
+    return v.toLocalFile();
+}
+
+void QMakeJob::setProject(KDevelop::IProject* project)
+{
+    m_project = project;
+}
+
+void QMakeJob::slotFailed()
+{
+    setError(ConfigureError);
+    // FIXME need more detail i guess
+    setErrorText(i18n("Configure error"));
+    emitResult();
+}
+
+void QMakeJob::slotCompleted()
+{
+    emitResult();
+}
+
+#include "qmakejob.moc"
---
+++ qmakebuilder/qmakejob.h
@@ -0,0 +1,68 @@
+/* KDevelop QMake Support
+ *
+ * Copyright 2006-2007 Andreas Pakulat <apaku@gmx.de>
+ * Copyright 2008 Hamish Rodda <rodda@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef QMAKEJOB_H
+#define QMAKEJOB_H
+
+#include <outputjob.h>
+
+namespace KDevelop{
+    class ProjectBaseItem;
+    class CommandExecutor;
+    class OutputModel;
+    class IProject;
+}
+
+/**
+@author Andreas Pakulat
+@author Hamish Rodda (KJob porting)
+*/
+class QMakeJob : public KDevelop::OutputJob
+{
+    Q_OBJECT
+
+public:
+    QMakeJob(QObject *parent = 0);
+
+    enum ErrorTypes {
+        NoProjectError = UserDefinedError,
+        ConfigureError
+    };
+
+    void setProject(KDevelop::IProject* project);
+    
+    virtual void start();
+    
+    QString qmakeBinary( KDevelop::IProject* project );
+
+private Q_SLOTS:
+    void slotFailed();
+    void slotCompleted();
+    
+private:
+    KDevelop::IProject* m_project;
+    KDevelop::CommandExecutor* m_cmd;
+    KDevelop::ProjectBaseItem* m_item;
+    KDevelop::OutputModel* m_model;
+};
+
+#endif // QMAKEJOB_H
+
