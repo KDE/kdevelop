189f114e4f9a307a5d14f36c6c538d55f8d5bff2
Andreas Pakulat <apaku@gmx.de>
2008-08-18 11:03:21 +0000

    Add my copyright.

--- plugins/subversion/kdevsvnplugin.h
+++ plugins/subversion/kdevsvnplugin.h
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright 2007 Dukju Ahn <dukjuahn@gmail.com>                         *
+ *   Copyright 2008 Andreas Pakulat <apaku@gmx.de>                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
