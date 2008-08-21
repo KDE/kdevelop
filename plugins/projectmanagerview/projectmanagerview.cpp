f77bb5b3700497d6aa5a9b0df973b09243dc2f0a
Andreas Pakulat <apaku@gmx.de>
2008-08-21 20:14:20 +0000

    Don't try to delete a project twice if two files in it were selected.

--- plugins/projectmanagerview/projectmanagerview.cpp
+++ plugins/projectmanagerview/projectmanagerview.cpp
@@ -49,6 +49,7 @@
 #include <project/interfaces/iprojectbuilder.h>
 #include <project/projectmodel.h>
 
+#include "tests/common/modeltest.h"
 #include "projectproxymodel.h"
 #include "projectbuildsetwidget.h"
 #include "projectmanagerviewplugin.h"
@@ -150,6 +151,7 @@ ProjectManagerView::ProjectManagerView( ProjectManagerViewPlugin *plugin, QWidge
     d->m_modelFilter = new ProjectProxyModel( this );
     d->m_modelFilter->setSourceModel(overviewModel);
     d->m_modelFilter->setDynamicSortFilter(true);
+    ModelTest* test = new ModelTest( overviewModel );
     d->m_projectOverview->setModel( d->m_modelFilter );
     d->m_projectOverview->setSortingEnabled(true);
     d->m_projectOverview->sortByColumn( 0, Qt::AscendingOrder );
