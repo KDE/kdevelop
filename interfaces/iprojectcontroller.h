fa04aba87d3f108eaebcf8fd7370dd5a0e283535
Andreas Pakulat <apaku@gmx.de>
2008-08-22 13:47:14 +0000

    Add api documentation.

--- interfaces/iprojectcontroller.h
+++ interfaces/iprojectcontroller.h
@@ -45,20 +45,66 @@ public:
     Q_SCRIPTABLE virtual int projectCount() const = 0;
     Q_SCRIPTABLE virtual QList<IProject*> projects() const = 0;
 
+    /**
+     * Provides access to the model representing the open projects
+     * @returns the model containing the projects and their items
+     */
     Q_SCRIPTABLE virtual ProjectModel* projectModel() = 0;
+
+    /**
+     * Find an open project using the name of the project
+     * @param name the name of the project to be found
+     * @returns the project or null if no project with that name is open
+     */
     Q_SCRIPTABLE virtual IProject* findProjectByName( const QString& name ) = 0;
-    Q_SCRIPTABLE virtual IProject* findProjectForUrl( const KUrl& ) const = 0;
+
+    /**
+     * Finding an open project for a given file
+     * @param url the url of a file belonging to an open project
+     * @returns the first open project containing the url or null if no such
+     * project can be found
+     */
+    Q_SCRIPTABLE virtual IProject* findProjectForUrl( const KUrl& url ) const = 0;
 
 public Q_SLOTS:
-    virtual bool openProject( const KUrl &KDev4ProjectFile = KUrl() ) = 0;
+    /**
+     * open the project from the given kdev4 project file. This only reads
+     * the file and starts creating the project model from it. The opening process
+     * is finished once @ref projectOpened signal is emitted.
+     * @param url a kdev4 project file top open
+     * @returns true if the given project could be opened, false otherwise
+     */
+    virtual bool openProject( const KUrl & url = KUrl() ) = 0;
+    /**
+     * close the given project. Closing the project is done in steps and
+     * the @ref projectClosing and @ref projectClosed signals are emitted. Only when
+     * the latter signal is emitted its guaranteed that the project has been closed.
+     * The @ref IProject object will be deleted after the closing has finished.
+     * @returns true if the project could be closed, false otherwise
+     */
     virtual bool closeProject( IProject* ) = 0;
     virtual bool configureProject( IProject* ) = 0;
 //     virtual void changeCurrentProject( KDevelop::ProjectBaseItem* ) = 0;
 
 Q_SIGNALS:
-    void projectOpened( KDevelop::IProject* );
-    void projectClosing( KDevelop::IProject* );
-    void projectClosed( KDevelop::IProject* );
+    /**
+     * emitted after a project is completely opened and the project model
+     * has been populated.
+     * @param project the project that has been opened.
+     */
+    void projectOpened( KDevelop::IProject* project );
+    /**
+     * emitted when starting to close a project.
+     * @param project the project that is going to be closed.
+     */
+    void projectClosing( KDevelop::IProject* project );
+    /**
+     * emitted when a project has been closed completely. At this
+     * point the project object is still valid, the deletion will be done
+     * delayed during the next run of the event loop.
+     * @param project the project that has been closed.
+     */
+    void projectClosed( KDevelop::IProject* project );
 };
 
 }
