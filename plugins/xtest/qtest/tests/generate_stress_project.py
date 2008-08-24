a22dbc28982abcb78161a1cf93a1d46391600afd
Manuel Breugelmans <mbr.nxi@gmail.com>
2008-08-24 15:13:44 +0000

    Extend runner performance script.

--- plugins/xtest/qtest/tests/generate_stress_project.py
+++ plugins/xtest/qtest/tests/generate_stress_project.py
@@ -30,7 +30,7 @@ CurrentInstallDir=file:///usr/local
 #    chdir("..")
 
 
-def generate_root_cmakelists():
+def generate_root_cmakelists(suites):
     f = open("CMakeLists.txt", 'w')
     f.write(\
 '''
@@ -38,14 +38,13 @@ project(QTestStressTest)
 cmake_minimum_required(VERSION 2.6)
 find_package(Qt4 REQUIRED)
 enable_testing()
-
-add_subdirectory(suite_qt)
-add_subdirectory(suite_kde)
-add_subdirectory(suite_kde_gui)
 ''')
+    for suite in suites:
+        f.write("add_subdirectory(" + suite + ")\n")
+    f.write("\n")
     f.close()
 
-def qt_cmake():
+def generate_cmakelists_qt():
     f = open("CMakeLists.txt", 'w')
     f.write(\
 '''
@@ -55,7 +54,7 @@ set(EXECUTABLE_OUTPUT_PATH ${CMAKE_CURRENT_BINARY_DIR})
 ''')
     f.close()
 
-def qt_build_rule(name):
+def append_build_rule_qt(name):
     f = open("CMakeLists.txt", 'a')
     cpp = name + ".cpp"
     moc = name + ".moc"
@@ -70,7 +69,7 @@ target_link_libraries(''' + target + ''' ${QT_QTTEST_LIBRARY})
 ''')
     f.close()
 
-def qt_case_cpp(name):
+def generate_testcase_qt(name):
     f = open(name + ".cpp", 'w')
     f.write(\
 '''
@@ -88,7 +87,7 @@ QTEST_MAIN(''' + name + ''')
 ''')
     f.close()
 
-def kde_cmake():
+def generate_cmakelists_kde():
     f = open("CMakeLists.txt", 'w')
     f.write(\
 '''
@@ -99,7 +98,7 @@ set(EXECUTABLE_OUTPUT_PATH ${CMAKE_CURRENT_BINARY_DIR})
 ''')
     f.close()
 
-def kde_build_rule(name):
+def append_build_rule_kde(name):
     f = open("CMakeLists.txt", 'a')
     cpp = name + ".cpp"
     moc = name + ".moc"
@@ -108,11 +107,10 @@ def kde_build_rule(name):
 '''
 kde4_add_unit_test(''' + target + " " + cpp + " ${CMAKE_CURRENT_BINARY_DIR}/" + moc + ''')
 target_link_libraries(''' + target + ''' ${QT_QTTEST_LIBRARY} ${KDE4_KDECORE_LIBS})
-
 ''')
     f.close()
 
-def kde_case(name):
+def generate_testcase_kde(name):
     f = open(name + ".h", 'w')
     f.write(\
 '''
@@ -135,7 +133,7 @@ QTEST_KDEMAIN_CORE(''' + name + ''')
 ''')
     f.close()
 
-def kde_gui_build_rule(name):
+def append_build_rule_kde_gui(name):
     f = open("CMakeLists.txt", 'a')
     cpp = name + ".cpp"
     moc = name + ".moc"
@@ -148,7 +146,7 @@ target_link_libraries(''' + target + ''' ${QT_QTTEST_LIBRARY} ${KDE4_KDECORE_LIB
 ''')
     f.close()
 
-def kde_gui_case(name):
+def generate_testcase_kde_gui(name):
     f = open(name + ".h", 'w')
     f.write(\
 '''
@@ -171,16 +169,72 @@ QTEST_KDEMAIN(''' + name + ''', GUI)
 ''')
     f.close()
 
-def generate_suite(name, count, cmake_gen, app_build_rule, case_cpp_gen):
+def generate_testcase_with_body(name, body):
+    f = open(name + ".h", 'w')
+    f.write(\
+'''
+#include <qtest_kde.h>
+#include <KDebug>
+#ifndef ''' + name + '''_H
+#define ''' + name + '''_H
+class ''' + name + ''' : public QObject
+{ Q_OBJECT
+private slots:''')
+    f.write(body)
+    f.write(\
+'''};
+#endif
+''');
+    f.close()
+    f = open(name + ".cpp", 'w')
+    f.write(\
+'''#include "''' + name + '''.h"
+#include "''' + name + '''.moc"
+QTEST_KDEMAIN_CORE(''' + name + ''')
+''')
+    f.close()
+
+def generate_testcase_commands(name, count):
+    body = ""
+    for i in range(1, count):
+        body += '    void foo%(#)04d() {}\n' %{"#":i}
+    generate_testcase_with_body(name, body+"\n")
+
+def generate_testcase_spam(name, count):
+    body = '''void foo() {
+'''
+    for i in range(1, count):
+        body += '        kDebug() << "%(#)04d";\n' %{"#":i}
+    body += '''
+    }'''
+    generate_testcase_with_body(name, body)
+
+def generate_testcase_asserts(name, count):
+    body = '''void foo() {
+'''
+    for i in range(1, count, 3):
+        body +=\
+'''        QCOMPARE(1,1);
+           QVERIFY(1);
+           QVERIFY2(1, "failure message");
+'''
+    body += "QCOMPARE(1,0);}\n"
+    generate_testcase_with_body(name, body)
+
+def construct_name(base, count):
+    return '%(b)s_%(#)04d' % {"b" : base, "#":count}
+
+def generate_suite(name, generate_cmakelists, generate_testcase, count, append_build_rule):
     mkdir("suite_" + name)
     chdir("suite_" + name)
-    cmake_gen()
+    generate_cmakelists()
     for i in range(1,count):
-        case_name = 'case_%(n)s_%(#)04d' % {"n":name, "#":i}
-        case_cpp_gen(case_name)
-        app_build_rule(case_name)
+        case_name = construct_name('case_' + name, i)
+        generate_testcase(case_name)
+        append_build_rule(case_name)
         print "# Generated " + case_name
     chdir("..")
+    last_case.append(construct_name('case_' + name, count-1))
 
 #################### ENTRY POINT #########################
 
@@ -193,22 +247,70 @@ system("rm -rf stress") # ;-)
 mkdir("stress")
 chdir("stress")
 generate_kdev4_files()
-generate_root_cmakelists()
+suites = ["suite_qt", "suite_kde_core", "suite_kde_gui", "suite_spam", "suite_asserts", "suite_commands"]
+generate_root_cmakelists(suites)
+last_case = []
 
-generate_suite("qt",  250, qt_cmake, qt_build_rule, qt_case_cpp)
-generate_suite("kde", 250, kde_cmake, kde_build_rule, kde_case)
-generate_suite("kde_gui", 250, kde_cmake, kde_gui_build_rule, kde_gui_case)
+generate_suite('qt',\
+        generate_cmakelists_qt,\
+        generate_testcase_qt, 250, \
+        append_build_rule_qt)
+
+generate_suite('kde_core', \
+        generate_cmakelists_kde, \
+        generate_testcase_kde, 250, \
+        append_build_rule_kde)
+
+generate_suite('kde_gui', \
+        generate_cmakelists_kde, \
+        generate_testcase_kde_gui, 250, \
+        append_build_rule_kde_gui)
+
+generate_suite('spam', \
+        generate_cmakelists_kde, \
+        lambda x: generate_testcase_spam(x, 50), 250, \
+        append_build_rule_kde)
+
+generate_suite('asserts', \
+        generate_cmakelists_kde, \
+        lambda x: generate_testcase_asserts(x,50), 250, \
+        append_build_rule_kde)
+
+mkdir("suite_commands")
+chdir("suite_commands")
+generate_cmakelists_kde()
+append_build_rule_kde("case_commands")
+generate_testcase_commands("case_commands", 250)
+print "# Generated case_commands"
+chdir("..")
+last_case.append('case_commands')
 
 chdir(root + "/stress")
 mkdir("build")
 chdir("build")
-make = popen("cmake .. -DKDE4_BUILD_TESTS=on && make -j2")
+
+cmake = popen("cmake .. -DKDE4_BUILD_TESTS=on")
+print "# Running cmake"
+cmake.close()
+
+before = time()
+make = popen("make -j2")
+build_times = []
+last_case_ = last_case[:]
 while 1:
     line = make.readline()
     if line == "": break
     #print line,
     if line.find("Built target") != -1:
         print "# " + line[line.find("Built target"):],
+    if len(last_case_) != 0 and line.find("Built target " + last_case_[0]) != -1:
+        build_times.append(time())
+        last_case_ = last_case_[1:]
 
 after = time()
 print '# Total elapsed: %(#)d sec' % {"#":after-before}
+prev = before
+for i in range(0, len(last_case)):
+    built_at = build_times[i]
+    print '## Built time for %(c)s: %(#)d sec' % {"c":last_case[i], "#":built_at-prev}
+    prev = built_at
