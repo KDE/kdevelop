b6a655e092befe1588dd4ad520970829a5dbff5f
Cédric Pasteur <cedric.pasteur@free.fr>
2008-09-04 19:13:01 +0000

    Style and indentation fixes

--- plugins/astyle/astyle_plugin.cpp
+++ plugins/astyle/astyle_plugin.cpp
@@ -58,38 +58,38 @@
 
 using namespace KDevelop;
 
-K_PLUGIN_FACTORY(AStyleFactory, registerPlugin<AStylePlugin>(); )
+K_PLUGIN_FACTORY(AStyleFactory, registerPlugin<AStylePlugin>();)
 K_EXPORT_PLUGIN(AStyleFactory("kdevastyle"))
 
-AStylePlugin::AStylePlugin( QObject *parent, const QVariantList& )
-: IPlugin( AStyleFactory::componentData(), parent)
+AStylePlugin::AStylePlugin(QObject *parent, const QVariantList&)
+		: IPlugin(AStyleFactory::componentData(), parent)
 {
-    setXMLFile("kdevastyle.rc");
+	setXMLFile("kdevastyle.rc");
 
-    m_formatTextAction = actionCollection()->addAction("edit_astyle_reformat");
-    m_formatTextAction->setText(i18n("&Reformat Source"));
-    m_formatTextAction->setToolTip(i18n("Reformat source using AStyle"));
-    m_formatTextAction->setWhatsThis(i18n("<b>Reformat source</b><p>Source reformatting "
-        "functionality using <b>astyle</b> library. "
-    /*    "Also available in <b>New Class</b> and <b>Subclassing</b> wizards."*/));
-    connect(m_formatTextAction, SIGNAL(triggered()), this, SLOT(beautifySource()));
+	m_formatTextAction = actionCollection()->addAction("edit_astyle_reformat");
+	m_formatTextAction->setText(i18n("&Reformat Source"));
+	m_formatTextAction->setToolTip(i18n("Reformat source using AStyle"));
+	m_formatTextAction->setWhatsThis(i18n("<b>Reformat source</b><p>Source reformatting "
+	                                      "functionality using <b>astyle</b> library. "
+	                                      /*    "Also available in <b>New Class</b> and <b>Subclassing</b> wizards."*/));
+	connect(m_formatTextAction, SIGNAL(triggered()), this, SLOT(beautifySource()));
 
-    m_formatFilesAction = actionCollection()->addAction("tools_astyle");
-    m_formatFilesAction->setText(i18n("Format files"));
-    m_formatFilesAction->setToolTip(i18n("Format file(s) using the current theme"));
-    m_formatFilesAction->setWhatsThis(i18n("<b>Format files</b><p>Formatting functionality using <b>astyle</b> library. "
-        /*"Also available in <b>New Class</b> and <b>Subclassing</b> wizards."*/));
-    connect(m_formatFilesAction, SIGNAL(triggered()), this, SLOT(formatItem()));
+	m_formatFilesAction = actionCollection()->addAction("tools_astyle");
+	m_formatFilesAction->setText(i18n("Format files"));
+	m_formatFilesAction->setToolTip(i18n("Format file(s) using the current theme"));
+	m_formatFilesAction->setWhatsThis(i18n("<b>Format files</b><p>Formatting functionality using <b>astyle</b> library. "
+	                                       /*"Also available in <b>New Class</b> and <b>Subclassing</b> wizards."*/));
+	connect(m_formatFilesAction, SIGNAL(triggered()), this, SLOT(formatItem()));
 
-    m_formatTextAction->setEnabled(false);
-    m_formatFilesAction->setEnabled(true);
+	m_formatTextAction->setEnabled(false);
+	m_formatFilesAction->setEnabled(true);
 
 //     m_defaultFormatExtensions = "*.cpp *.h *.hpp,*.c *.h,*.cxx *.hxx,*.c++ *.h++,*.cc *.hh,*.C *.H,*.diff ,*.inl,*.java,*.moc,*.patch,*.tlh,*.xpm";
 //     m_searchExtensions.insert("*", "*");
-    m_formatter = new AStyleFormatter();
+	m_formatter = new AStyleFormatter();
 
-    connect(core()->partManager(), SIGNAL(activePartChanged(KParts::Part*)),
-             this, SLOT(activePartChanged(KParts::Part*)));
+	connect(core()->partManager(), SIGNAL(activePartChanged(KParts::Part*)),
+	        this, SLOT(activePartChanged(KParts::Part*)));
 }
 
 AStylePlugin::~AStylePlugin()
@@ -98,86 +98,89 @@ AStylePlugin::~AStylePlugin()
 
 void AStylePlugin::beautifySource()
 {
-    m_formatter->loadConfig(KGlobal::config());
-    
-    KTextEditor::Document *doc =
-        core()->documentController()->activeDocument()->textDocument();
-    if(!doc)
-        return;
+	m_formatter->loadConfig(KGlobal::config());
 
-    bool has_selection = false;
+	KTextEditor::Document *doc =
+	  core()->documentController()->activeDocument()->textDocument();
+	if (!doc)
+		return;
 
-    KTextEditor::View *view = doc->views().first();
-    if(view && view->selection())
-        has_selection = true;
+	bool has_selection = false;
 
-    // put the selection back to the same indent level.
-    // taking note of the config options.
-    if(has_selection) {
-        unsigned int indentCount=0;
-        QString indentWith("");
-        QString original = view->selectionText();
-        
-        for (; indentCount < original.length(); indentCount++){
-            QChar ch = original[indentCount];
-            if(!ch.isSpace())
-                break;
-                
-            if(ch == QChar('\n') || ch == QChar('\r'))
-                indentWith="";
-            else
-                indentWith+=original[indentCount];   
-        }
-        replaceSpacesWithTab(indentWith);
+	KTextEditor::View *view = doc->views().first();
+	if (view && view->selection())
+		has_selection = true;
 
-        QString output = m_formatter->formatSource(view->selectionText(), indentWith);
-        
-        //remove the final newline character, unless it should be there
-        if(!original.endsWith('\n'))
-            output.resize(output.length()-1);
-        //there was a selection, so only change the part of the text related to it
-        view->removeSelectionText();
-        doc->insertText(view->cursorPosition(), output);
-    } else {
-        KTextEditor::Cursor cursor = view->cursorPosition();
-        doc->setText(m_formatter->formatSource(doc->text()));
-        view->setCursorPosition(cursor);
-    }
+	// put the selection back to the same indent level.
+	// taking note of the config options.
+	if (has_selection) {
+		unsigned int indentCount = 0;
+		QString indentWith("");
+		QString original = view->selectionText();
+
+		for (; indentCount < original.length(); indentCount++) {
+			QChar ch = original[indentCount];
+			if (!ch.isSpace())
+				break;
+
+			if (ch == QChar('\n') || ch == QChar('\r'))
+				indentWith = "";
+			else
+				indentWith += original[indentCount];
+		}
+		replaceSpacesWithTab(indentWith);
+
+		QString output = m_formatter->formatSource(view->selectionText(), indentWith);
+
+		//remove the final newline character, unless it should be there
+		if (!original.endsWith('\n'))
+			output.resize(output.length() - 1);
+		//there was a selection, so only change the part of the text related to it
+		view->removeSelectionText();
+		doc->insertText(view->cursorPosition(), output);
+	} else {
+		KTextEditor::Cursor cursor = view->cursorPosition();
+		QString output = m_formatter->formatSource(doc->text());
+		//doc->replaceText ( doc->documentRange(), output );
+		bool success = doc->setText(output);
+		kDebug() << "ddddddddddddddddddddddd        " << success << endl;
+		view->setCursorPosition(cursor);
+	}
 }
 
 void AStylePlugin::replaceSpacesWithTab(QString &input)
 {
-    int wsCount = m_formatter->option("FillCount").toInt();
-    if(m_formatter->option("Fill").toString() == "Tabs") {
-        // tabs and wsCount spaces to be a tab
-        QString replace;
-        for (int i =0; i < wsCount;i++)
-            replace+=' ';
+	int wsCount = m_formatter->option("FillCount").toInt();
+	if (m_formatter->option("Fill").toString() == "Tabs") {
+		// tabs and wsCount spaces to be a tab
+		QString replace;
+		for (int i = 0; i < wsCount;i++)
+			replace += ' ';
 
-        input = input.replace(replace, QChar('\t'));
-        input = input.remove(' ');
-    } else {
-        if(m_formatter->option("FillForce").toBool()){
-            //convert tabs to spaces
-            QString replace;
-            for (int i =0;i < wsCount;i++)
-                replace+=' ';
+		input = input.replace(replace, QChar('\t'));
+		input = input.remove(' ');
+	} else {
+		if (m_formatter->option("FillForce").toBool()) {
+			//convert tabs to spaces
+			QString replace;
+			for (int i = 0;i < wsCount;i++)
+				replace += ' ';
 
-            input = input.replace(QChar('\t'), replace);
-        }
-    }
+			input = input.replace(QChar('\t'), replace);
+		}
+	}
 }
 
-void AStylePlugin::activePartChanged ( KParts::Part *part )
+void AStylePlugin::activePartChanged(KParts::Part *part)
 {
-    bool enabled = false;
-    KParts::ReadWritePart *rw_part = dynamic_cast<KParts::ReadWritePart*> ( part );
+	bool enabled = false;
+	KParts::ReadWritePart *rw_part = dynamic_cast<KParts::ReadWritePart*>(part);
 
-    if(rw_part) {
-        KTextEditor::Document *doc = dynamic_cast<KTextEditor::Document*>(rw_part);
+	if (rw_part) {
+		KTextEditor::Document *doc = dynamic_cast<KTextEditor::Document*>(rw_part);
 
-        if(doc) {
-            // check for the everything case..
+		if (doc) {
+			// check for the everything case..
 //             if(!m_formatter->hasExtension("*")) {
 //                 QString extension = rw_part->url().path();
 //                 int pos = extension.lastIndexOf( '.' );
@@ -188,119 +191,119 @@ void AStylePlugin::activePartChanged ( KParts::Part *part )
 //             } else {
 //                 enabled = true;
 //             }
-            enabled = true;
-        }
-    }
+			enabled = true;
+		}
+	}
 
-    m_formatTextAction->setEnabled(enabled);
+	m_formatTextAction->setEnabled(enabled);
 }
 
-KDevelop::ContextMenuExtension 
+KDevelop::ContextMenuExtension
 AStylePlugin::contextMenuExtension(KDevelop::Context* context)
 {
-    ContextMenuExtension ext;
-    m_urls.clear();
-    m_prjItems.clear();
-    
-    if(context->hasType(Context::EditorContext))
-        ext.addAction(ContextMenuExtension::EditGroup, m_formatTextAction);
-     else if(context->hasType(Context::FileContext)) {
-        FileContext* filectx = dynamic_cast<FileContext*>(context);
-        m_urls = filectx->urls();
-        ext.addAction(ContextMenuExtension::EditGroup, m_formatFilesAction);
-     } else if(context->hasType(Context::CodeContext)) {
-     } else if(context->hasType(Context::ProjectItemContext)) {
-         ProjectItemContext* prjctx = dynamic_cast<ProjectItemContext*>(context);
-         m_prjItems = prjctx->items();
-         ext.addAction(ContextMenuExtension::ExtensionGroup, m_formatFilesAction);
-     }    
-    return ext;
+	ContextMenuExtension ext;
+	m_urls.clear();
+	m_prjItems.clear();
+
+	if (context->hasType(Context::EditorContext))
+		ext.addAction(ContextMenuExtension::EditGroup, m_formatTextAction);
+	else if (context->hasType(Context::FileContext)) {
+		FileContext* filectx = dynamic_cast<FileContext*>(context);
+		m_urls = filectx->urls();
+		ext.addAction(ContextMenuExtension::EditGroup, m_formatFilesAction);
+	} else if (context->hasType(Context::CodeContext)) {
+	} else if (context->hasType(Context::ProjectItemContext)) {
+		ProjectItemContext* prjctx = dynamic_cast<ProjectItemContext*>(context);
+		m_prjItems = prjctx->items();
+		ext.addAction(ContextMenuExtension::ExtensionGroup, m_formatFilesAction);
+	}
+	return ext;
 }
 
 void AStylePlugin::formatItem()
 {
-    if(m_prjItems.isEmpty())
-        return;
-    
-    //get a list of all files in this folder recursively
-    QList<ProjectFolderItem*> folders;
-    foreach (ProjectBaseItem *item, m_prjItems) {
-        if(!item)
-            continue;
-        if(item->folder())
-            folders.append(item->folder());
-        else if(item->file())
-            m_urls.append(item->file()->url());
-        else if(item->target()) {
-            foreach (ProjectFileItem *f, item->fileList())
-                 m_urls.append(f->url());
-        }
-    }
-    
-    while(!folders.isEmpty()) {
-        ProjectFolderItem *item = folders.takeFirst();
-        foreach (ProjectFolderItem *f, item->folderList())
-            folders.append(f);
-        foreach (ProjectTargetItem *f, item->targetList()) {
-            foreach (ProjectFileItem *child, f->fileList())
-                m_urls.append(child->url());  
-        }
-        foreach (ProjectFileItem *f, item->fileList())
-            m_urls.append(f->url());        
-   }
-   
-   formatFiles(m_urls);
+	if (m_prjItems.isEmpty())
+		return;
+
+	//get a list of all files in this folder recursively
+	QList<ProjectFolderItem*> folders;
+	foreach(ProjectBaseItem *item, m_prjItems) {
+		if (!item)
+			continue;
+		if (item->folder())
+			folders.append(item->folder());
+		else if (item->file())
+			m_urls.append(item->file()->url());
+		else if (item->target()) {
+			foreach(ProjectFileItem *f, item->fileList())
+			m_urls.append(f->url());
+		}
+	}
+
+	while (!folders.isEmpty()) {
+		ProjectFolderItem *item = folders.takeFirst();
+		foreach(ProjectFolderItem *f, item->folderList())
+		folders.append(f);
+		foreach(ProjectTargetItem *f, item->targetList()) {
+			foreach(ProjectFileItem *child, f->fileList())
+			m_urls.append(child->url());
+		}
+		foreach(ProjectFileItem *f, item->fileList())
+		m_urls.append(f->url());
+	}
+
+	formatFiles(m_urls);
 }
 
 void AStylePlugin::formatFiles(KUrl::List &list)
 {
-    m_formatter->loadConfig(KGlobal::config());
+	m_formatter->loadConfig(KGlobal::config());
 
-    for(int fileCount = 0; fileCount < list.size(); fileCount++) {
-        // check mimetype
-        KMimeType::Ptr mime = KMimeType::findByUrl(list[fileCount]);
-        kDebug() << "Checking file " << list[fileCount].pathOrUrl() << " of mime type " << mime->name() << endl;
-        if(!mime->is("text/x-chdr") && !mime->is("text/x-c++src"))
-            continue;
-        
-        // if the file is opened in the editor, format the text in the editor without saving it
-        IDocumentController *docController = ICore::self()->documentController();
-        IDocument *doc = docController->documentForUrl(list[fileCount]);
-        if(doc) {
-            kDebug() << "Processing file " << list[fileCount].pathOrUrl() << "opened in editor" << endl;
-            KTextEditor::Cursor cursor = doc->cursorPosition();
-            doc->textDocument()->setText(m_formatter->formatSource(doc->textDocument()->text()));
-            doc->setCursorPosition(cursor);
-            return;
-        }
-        
-        kDebug() << "Processing file " << list[fileCount].pathOrUrl() << endl;
-        QString tmpFile, output;
-        if(KIO::NetAccess::download(list[fileCount], tmpFile, 0)) {
-            QFile file(tmpFile);
-            // read file
-            if(file.open(QFile::ReadOnly)) {
-                QTextStream is(&file);
-                output = m_formatter->formatSource(is.readAll());
-                file.close();
-            } else
-                KMessageBox::error(0, i18n("Unable to read %1").arg(list[fileCount].prettyUrl()));
-            
-            //write new content
-            if(file.open(QFile::WriteOnly|QIODevice::Truncate)) {
-                QTextStream os(&file);
-                os << output;
-                file.close();
-            } else
-                KMessageBox::error(0, i18n("Unable to write to %1").arg(list[fileCount].prettyUrl()));
-            
-            if(!KIO::NetAccess::upload(tmpFile, list[fileCount], 0))
-                KMessageBox::error(0, KIO::NetAccess::lastErrorString());
-            
-            KIO::NetAccess::removeTempFile(tmpFile);
-        } else
-            KMessageBox::error(0, KIO::NetAccess::lastErrorString());
-    }
+	for (int fileCount = 0; fileCount < list.size(); fileCount++) {
+		// check mimetype
+		KMimeType::Ptr mime = KMimeType::findByUrl(list[fileCount]);
+		kDebug() << "Checking file " << list[fileCount].pathOrUrl() << " of mime type " << mime->name() << endl;
+		if (!mime->is("text/x-chdr") && !mime->is("text/x-c++src"))
+			continue;
+
+		// if the file is opened in the editor, format the text in the editor without saving it
+		IDocumentController *docController = ICore::self()->documentController();
+		IDocument *doc = docController->documentForUrl(list[fileCount]);
+		if (doc) {
+			kDebug() << "Processing file " << list[fileCount].pathOrUrl() << "opened in editor" << endl;
+			KTextEditor::Cursor cursor = doc->cursorPosition();
+			doc->textDocument()->setText(m_formatter->formatSource(doc->textDocument()->text()));
+			doc->setCursorPosition(cursor);
+			return;
+		}
+
+		kDebug() << "Processing file " << list[fileCount].pathOrUrl() << endl;
+		QString tmpFile, output;
+		if (KIO::NetAccess::download(list[fileCount], tmpFile, 0)) {
+			QFile file(tmpFile);
+			// read file
+			if (file.open(QFile::ReadOnly)) {
+				QTextStream is(&file);
+				output = m_formatter->formatSource(is.readAll());
+				file.close();
+			} else
+				KMessageBox::error(0, i18n("Unable to read %1").arg(list[fileCount].prettyUrl()));
+
+			//write new content
+			if (file.open(QFile::WriteOnly | QIODevice::Truncate)) {
+				QTextStream os(&file);
+				os << output;
+				file.close();
+			} else
+				KMessageBox::error(0, i18n("Unable to write to %1").arg(list[fileCount].prettyUrl()));
+
+			if (!KIO::NetAccess::upload(tmpFile, list[fileCount], 0))
+				KMessageBox::error(0, KIO::NetAccess::lastErrorString());
+
+			KIO::NetAccess::removeTempFile(tmpFile);
+		} else
+			KMessageBox::error(0, KIO::NetAccess::lastErrorString());
+	}
 }
 
 
