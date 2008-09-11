506cc5e97ce0df0eb4cf2851519a96b435ec5aa5
Cédric Pasteur <cedric.pasteur@free.fr>
2008-09-11 14:22:01 +0000

    Move all plugins in plugins/formatters/
    Signed-off-by: Cedric Pasteur <cedric.pasteur@free.fr>

---
+++ plugins/formatters/indent_plugin.cpp
@@ -0,0 +1,143 @@
+/* This file is part of KDevelop
+*  Copyright (C) 2008 Cédric Pasteur <cedric.pasteur@free.fr>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+ */
+
+#include "indent_plugin.h"
+
+#include <KGenericFactory>
+#include <KProcess>
+
+using namespace KDevelop;
+
+K_PLUGIN_FACTORY(IndentFactory, registerPlugin<IndentPlugin>();)
+K_EXPORT_PLUGIN(IndentFactory("kdevindent"))
+
+IndentPlugin::IndentPlugin(QObject *parent, const QVariantList&)
+		: IPlugin(IndentFactory::componentData(), parent)
+{
+	KDEV_USE_EXTENSION_INTERFACE(ISourceFormatter)
+}
+
+IndentPlugin::~IndentPlugin()
+{
+}
+
+QString IndentPlugin::name()
+{
+	return "indent";
+}
+
+QString IndentPlugin::caption()
+{
+	return "GNU Indent";
+}
+
+QString IndentPlugin::description()
+{
+	return i18n("Indent and Format C Program Source.\n"
+		"The `indent' program can be used to make code easier to read."
+		" It can also convert from one style of writing C to another."
+		"<b>indent</b> understands a substantial amount about the syntax of C,"
+		" but it also attempts to cope with incomplete and misformed syntax.\n"
+		"Home Page: <a href=\"http://www.gnu.org/software/indent/\">"
+		"http://www.gnu.org/software/indent</a>/");
+}
+
+QString IndentPlugin::highlightModeForMime(const KMimeType::Ptr &mime)
+{
+	return "C++";
+}
+
+QString IndentPlugin::formatSource(const QString &text, const KMimeType::Ptr &)
+{
+	KProcess proc;
+	QTextStream ios(&proc, QIODevice::ReadWrite);
+	proc.setProgram("indent", m_options);
+	proc.setOutputChannelMode(KProcess::OnlyStdoutChannel);
+
+	proc.start();
+	if(!proc.waitForStarted()) {
+		kDebug() << "Unable to start indent" << endl;
+		return text;
+	}
+
+	ios << text;
+	if(!proc.waitForFinished()) {
+		kDebug() << "Process doesn't finish" << endl;
+		return text;
+	}
+	proc.closeWriteChannel();
+
+	QString output = ios.readAll();
+	kDebug() << "read " << output << endl;
+	return text;
+}
+
+QMap<QString, QString> IndentPlugin::predefinedStyles(const KMimeType::Ptr &)
+{
+	QMap<QString, QString> styles;
+	styles.insert("orig", "Original Berkeley indent style");
+	styles.insert("GNU", "GNU");
+	styles.insert("KR", "Kernighan & Ritchie");
+
+	return styles;
+}
+
+void IndentPlugin::setStyle(const QString &name, const QString &content)
+{
+	if (!name.isEmpty()) {
+		m_options.clear();
+		if(name == "KR")
+			m_options << "-kr";
+		else if(name == "orig")
+			m_options << "-orig";
+	}
+	else
+		m_options = content.split(' ');
+}
+
+SettingsWidget* IndentPlugin::editStyleWidget(const KMimeType::Ptr &mime)
+{
+	return new IndentPreferences();
+}
+
+QString IndentPlugin::previewText(const KMimeType::Ptr &)
+{
+	return "int foo (){puts(\"Hi\");}\n/* The procedure bar is even less interesting.  */"
+	"char * bar () { puts(\"Hello\");}";
+}
+
+ISourceFormatter::IndentationType IndentPlugin::indentationType()
+{
+	if(m_options.contains("-nut"))
+		return ISourceFormatter::IndentWithSpaces;
+	else
+		return ISourceFormatter::IndentWithTabs;
+}
+
+int IndentPlugin::indentationLength()
+{
+	int idx = m_options.firstIndexOf("^-i\\d+");
+	if(idx < 0)
+		return 2;
+	return m_options[idx].mid(2).toInt();
+}
+
+#include "indent_plugin.moc"
+
+// kate: indent-mode cstyle; space-indent off; tab-width 4;
