8089565ef9d7b7300deed684789cfaa95d80e9d5
Manuel Breugelmans <mbr.nxi@gmail.com>
2008-09-16 20:16:03 +0000

    Apparently this gets optimized away on some machines, ie does not cause a segfault. Now it should always crash..

--- plugins/xtest/qtest/tests/suite1/segFault.cpp
+++ plugins/xtest/qtest/tests/suite1/segFault.cpp
@@ -1,12 +1,13 @@
 #include <QtTest/QTest>
+#include <iostream>
 
 class SegFault : public QObject
 {
 Q_OBJECT
 private slots:
     void command() {
-        int* i = 0, *j;
-        *j = *i;
+        int* i = 0;
+	std::cout << *i;
     }
 };
 
