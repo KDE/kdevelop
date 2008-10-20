536d504d81ad6e89c497ce1051aa7dd81304d334
Matt Rogers <mattr@kde.org>
2008-10-20 02:07:04 +0000

    build

--- kross/wrappers/krosstopducontext.cpp
+++ kross/wrappers/krosstopducontext.cpp
@@ -72,7 +72,7 @@ class KrossKDevelopTopDUContext : public QObject, public Kross::WrapperInterface
 		Q_SCRIPTABLE void addProblem(const KSharedPtr< KDevelop::Problem >& x0) { wrapped->addProblem(x0); }
 		Q_SCRIPTABLE void clearProblems() { wrapped->clearProblems(); }
 		Q_SCRIPTABLE bool imports(const KDevelop::DUContext* x0, const KDevelop::SimpleCursor& x1) const { return wrapped->imports(x0, x1); }
-		Q_SCRIPTABLE void importTrace(const KDevelop::TopDUContext* x0, KDevVarLengthArray< KDevelop::ImportTraceItem, 40 >& x1) const { wrapped->importTrace(x0, x1); }
+		Q_SCRIPTABLE void importTrace(const KDevelop::TopDUContext* x0, KDevelop::ImportTrace& x1) const { wrapped->importTrace(x0, x1); }
 		Q_SCRIPTABLE KDevVarLengthArray< KDevelop::ImportTraceItem, 40 > importTrace(const KDevelop::TopDUContext* x0) const { return wrapped->importTrace(x0); }
 		Q_SCRIPTABLE KDevelop::TopDUContext::Features features() const { return wrapped->features(); }
 		Q_SCRIPTABLE void setFeatures(KDevelop::TopDUContext::Features x0) { wrapped->setFeatures(x0); }
