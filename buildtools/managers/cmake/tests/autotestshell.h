76b9e96c28f33f15d5fa45e02725923516f25ded
Manuel Breugelmans <mbr.nxi@gmail.com>
2008-10-26 21:04:18 +0000

    Add a shell-test which shows that kde4_add_unit_test() calls are not processed anymore. The test does pass at rev 875433, so the regression is most likely introduced by 875538.
    Pretty 'serious' since test-targets are gone from the project-tree view. Obviously 
    zero-conf in the qtestrunner is broken cause of this, too ;)

---
+++ buildtools/managers/cmake/tests/autotestshell.h
@@ -0,0 +1,61 @@
+/***************************************************************************
+ *   Copyright 2008 Harald Fernengel <harry@kdevelop.org>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef AUTOTESTSHELL_H
+#define AUTOTESTSHELL_H
+
+#include <shell/shellextension.h>
+#include <shell/core.h>
+
+/* This is a dummy shell for unit tests. It basically does nothing :)
+
+   You can initialize it in initTestCase() to get a minimal shell to run
+   your autotests.
+
+   Example of a minimal KDevPlatform app:
+
+   void Mytest::initTestCase()
+   {
+       AutoTestShell::init();
+       KDevelop::Core::initialize();
+   }
+
+ */
+
+// NOTE Copied from kdevplatform/tests/common since this is not installed.
+
+class AutoTestShell : public KDevelop::ShellExtension
+{
+public:
+    QString xmlFile() { return QString(); }
+    QString defaultProfile() { return "kdevtest"; }
+    KDevelop::AreaParams defaultArea() {
+        KDevelop::AreaParams params;
+        params.name = "test";
+        params.title = "Test";
+        return params;
+    }
+    QString projectFileExtension() { return QString(); }
+    QString projectFileDescription() { return QString(); }
+
+    static void init() { s_instance = new AutoTestShell; }
+};
+
+#endif
+
