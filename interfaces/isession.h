848b72d6b4e815d3811b4f707d46512d0c89833e
Andreas Pakulat <apaku@gmx.de>
2008-10-30 16:49:50 +0000

    Ooops, make this function const

--- interfaces/isession.h
+++ interfaces/isession.h
@@ -40,7 +40,7 @@ public:
     ISession( QObject* parent = 0 );
     virtual ~ISession();
 
-    virtual QString name() = 0;
+    virtual QString name() const = 0;
     virtual KUrl pluginDataArea( const IPlugin* ) = 0;
     virtual KSharedConfig::Ptr config() = 0;
 };
