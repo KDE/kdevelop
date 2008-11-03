dcc675f228c7401be16fa349618f22f44b83ed76
Aleix Pol Gonzalez <aleixpol@gmail.com>
2008-11-03 06:51:13 +0000

    Compile the file you're on. Still there is a little issue when delivering the job with kross.

--- kross/examples/compileone/CompileOne/CompileOne.py
+++ kross/examples/compileone/CompileOne/CompileOne.py
@@ -3,52 +3,29 @@ import IPlugin, ICore, Kross, DUChain
 from urlparse import urlparse
 
 forms=Kross.module("forms")
-lastcalled=None
 
-urls=None
-act=None
-
-#def compileFile(tamare):
-	#pass
-	#project=ICore.projectController().findProjectForUrl(in)
-	#includes=project.buildSystemManager().includeDirectories()
-	#line="g++ -o/tmp/somefile "+urls+" -I"+includes.join(" -I")
-	#run="/tmp/somefile"
-
-def parsingFinished(job):
-	pass
-	#context=DUChain.chainForDocument(urls);
-	#main=context.findDeclarations("main")
-	#if len(main)>0:
-		#compileFile(urls)
-
-def triggered():
-	print "action triggered for"+str(urls)
-	ICore.documentController().backgroundParser().addDocument(urls)
+def compileCurrent():
+	print "triggered"
+	comp=ICore.documentController().activeDocument().url()
+	project=ICore.projectController().findProjectForUrl(comp)
+	
+	if project is not None:
+		items=project.filesForUrl(comp)
+		
+		for item in items:
+			buildjob=project.buildSystemManager().builder(item)
+			
+			print "registering: "+str(buildjob)+"\n\n"
+			print "runco:"+ICore.runController().objectName
+			ICore.runController().registerJob(buildjob)
 
 def contextMenuExtension(ctx, cme):
-	global act, urls
-	act=forms.createAction(0)
-	
-	a=act.connect("triggered()", triggered)
-	print "nanana"+ctx.objectName+str(a)
-	print "papapa"+str(dir(ctx))+"\n\n"
-	
-	if ctx.hasType(1):
-		act.text="I am an editor action"
-	elif ctx.hasType(2):
-		act.text="I am an editor action"
-	else:
-		act.text="I am another kind of action"+ctx.url()
-	urls=ctx.url()
-	cme.addAction(cme.ExtensionGroup, act)
+	comp=ICore.documentController().activeDocument().url()
+	project=ICore.projectController().findProjectForUrl(comp)
+	if project is not None:
+		cme.addAction(cme.ExtensionGroup, act)
 
-print str(dir(ICore))+"\n\n"
-print str(dir(ICore.documentController()))+"\n\n"
-print str(dir(ICore.languageController()))+"\n\n"
-#print "------ "+ICore.languageController().className()+"\n\n"
-#print str(ICore.languageController().documentActivated())+"\n\n"
-#print str(ICore.languageController().objectName)+"\n\n"
-#print str(dir(ICore.languageController().language("C++")))+"\n\n"
-print str(ICore.languageController().backgroundParser())
-#ICore.languageController().backgroundParser().connect("parseJobFinished(KDevelop::ParseJob* ))", parsingFinished);
+
+act=forms.createAction(0)
+act.text="Compile this file"
+act.connect("triggered()", compileCurrent)
