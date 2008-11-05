c2619c53968af662e35d65ca7333d6710a1a3f15
Laurent Montel <montel@kde.org>
2008-11-05 12:32:52 +0000

    Minor fix

--- cmake/modules/FindCppUnit.cmake
+++ cmake/modules/FindCppUnit.cmake
@@ -1,11 +1,9 @@
 
 find_path(CPPUNIT_INCLUDE_DIR
-    TestSuite.h
-    /usr/include/cppunit /usr/local/include/cppunit)
+    TestSuite.h PATH_SUFFIXES cppunit )
 
 find_library(CPPUNIT_LIBRARY
-    NAMES cppunit-1.12 cppunit
-    PATH /usr/lib /usr/local/lib)
+    NAMES cppunit-1.12 cppunit )
 
 if (CPPUNIT_INCLUDE_DIR AND CPPUNIT_LIBRARY)
     set(CPPUNIT_FOUND on)
