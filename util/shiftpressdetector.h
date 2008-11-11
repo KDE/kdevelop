d4e53ca5382b706745f256eb63b7c1d52e13a97a
David Nolden <david.nolden.kde@art-master.de>
2008-11-11 01:41:44 +0000

    Umm, looks like I forgot to check something in once again..

--- util/shiftpressdetector.h
+++ util/shiftpressdetector.h
@@ -19,21 +19,30 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 #include <QtGui/qevent.h>
+#include <qdatetime.h>
 
 
 ///A helper-class to detect shift-presses without anything else pressed.
 ///Just feed in the eventsa, and look at he return-value.
 struct ShiftPressDetector {
     public:
-        inline ShiftPressDetector() : m_hadOtherKey(true) {
+        ///@param secure If this is true, the detector will only trigger of no other key was pressed in 500 ms before the shift key.
+        ///This is useful to prevent mis-triggering.
+        inline ShiftPressDetector(bool secure = false) : m_hadOtherKey(true), m_lastOtherKey(QTime::currentTime()), m_secure(secure) {
         }
         ///Must be called with all key-events
         ///Returns true if the shift-key was released and no other key was pressed between its press and release.
         inline bool checkKeyEvent(QKeyEvent* e) {
+            
+            if(e->key() != Qt::Key_Shift)
+                m_lastOtherKey = QTime::currentTime();
+            
             if(e->type() == QEvent::KeyPress) {
                 m_hadOtherKey = true;
-                if (e->key() == Qt::Key_Shift && (e->modifiers() & (~Qt::ShiftModifier)) == 0)
-                    m_hadOtherKey = false;
+                if (e->key() == Qt::Key_Shift && (e->modifiers() & (~Qt::ShiftModifier)) == 0) {
+                    if(m_secure && m_lastOtherKey.msecsTo(QTime::currentTime()) > 500)
+                        m_hadOtherKey = false;
+                }
                 
             }else if(e->type() == QEvent::KeyRelease) {
                 if(e->key() == Qt::Key_Shift && !m_hadOtherKey)
@@ -49,4 +58,6 @@ struct ShiftPressDetector {
         }
     private:
     bool m_hadOtherKey;
+    QTime m_lastOtherKey;
+    bool m_secure;
 };
