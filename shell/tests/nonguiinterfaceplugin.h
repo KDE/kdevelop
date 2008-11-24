8427b55e446462b362b1cbb4d6ea9aa9af387454
Andreas Pakulat <apaku@gmx.de>
2008-11-24 00:05:14 +0000

    Add a plugin that can be used by the plugincontroller later to have a reliable plugin to test against.

---
+++ shell/tests/nonguiinterfaceplugin.h
@@ -0,0 +1,49 @@
+/*
+ * This file is part of KDevelop
+ *
+ * Copyright 2007 Hamish Rodda <rodda@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef NONGUIINTERFACEPLUGIN
+#define NONGUIINTERFACEPLUGIN
+
+#include <interfaces/iplugin.h>
+#include <interfaces/iextension.h>
+#include <QtCore/QVariant>
+#include <QtCore/QObject>
+
+class ITestNonGuiInterface
+{
+public:
+    virtual ~ITestNonGuiInterface() {}
+};
+
+KDEV_DECLARE_EXTENSION_INTERFACE( ITestNonGuiInterface, "org.kdevelop.tests.TestNonGuiInterface" )
+Q_DECLARE_INTERFACE( ITestNonGuiInterface, "org.kdevelop.tests.TestNonGuiInterface" )
+
+
+class NonGuiInterfacePlugin : public KDevelop::IPlugin, ITestNonGuiInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(ITestNonGuiInterface)
+public:
+    NonGuiInterfacePlugin( QObject* parent, const QVariantList& = QVariantList() );
+};
+
+#endif
+
