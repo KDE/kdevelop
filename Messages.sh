14051bee8a44acd319ddeb7e04b2e912cafdeb52
Andreas Pakulat <apaku@gmx.de>
2008-11-26 19:54:08 +0000

    Trying to fix Messages.sh, only run extractrc when there really are .rc or .ui files.

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env bash
 subdirs="interfaces kross language outputview project shell sublime util vcs veritas"
-$EXTRACTRC `find $subdirs -name \*.rc` >> rc.cpp || exit 11
-$EXTRACTRC `find $subdirs -name \*.ui` >> rc.cpp || exit 12
+rcfiles="`find $subdirs -name \*.rc`"
+uifiles="`find $subdirs -name \*.ui`"
+if [[ "$rcfiles" != "" ]] ; then
+    $EXTRACTRC "$rcfiles" >> rc.cpp || exit 11
+fi
+if [[ "$uifiles" != "" ]] ; then
+    $EXTRACTRC  >> rc.cpp || exit 12
+fi
 $XGETTEXT -kaliasLocal `find $subdirs -name \*.cc -o -name \*.cpp -o -name \*.h` rc.cpp -o $podir/kdevplatform.pot
 rm -f rc.cpp
--- plugins/Messages.sh
+++ plugins/Messages.sh
@@ -1,7 +1,14 @@
 #! /usr/bin/env bash
 for plugin in `find -maxdepth 1 -type d -not -name . -printf "%f\n"` ; do
-    $EXTRACTRC `find $plugin -name \*.rc` >> rc.cpp || exit 11
-    $EXTRACTRC `find $plugin -name \*.ui` >> rc.cpp || exit 12
+
+    rcfiles="`find $plugin -name \*.rc`"
+    uifiles="`find $plugin -name \*.ui`"
+    if [[ "$rcfiles" != "" ]] ; then
+        $EXTRACTRC "$rcfiles" >> rc.cpp || exit 11
+    fi
+    if [[ "$uifiles" != "" ]] ; then
+        $EXTRACTRC  >> rc.cpp || exit 12
+    fi
     $XGETTEXT -kaliasLocal `find $plugin -name \*.cc -o -name \*.cpp -o -name \*.h` rc.cpp -o $podir/kdev${plugin}.pot
     rm -f rc.cpp
 done
