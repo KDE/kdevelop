d1394db7b216a3b4a12d4497ef9966783a948782
David Nolden <david.nolden.kde@art-master.de>
2009-01-05 04:16:46 +0000

    Increase version, because of re-enabled environment management

--- language/duchain/repositories/itemrepository.cpp
+++ language/duchain/repositories/itemrepository.cpp
@@ -35,7 +35,7 @@ namespace KDevelop {
 
 uint staticItemRepositoryVersion() {
   //Increase this to reset incompatible item-repositories
-  return 43;
+  return 44;
 }
 
 AbstractItemRepository::~AbstractItemRepository() {
