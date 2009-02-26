83ec70e976eff5e287cc4b379a152445b6ed7518
David Nolden <david.nolden.kde@art-master.de>
2009-02-26 02:37:36 +0000

    - Use an easier way to remove the template parameters(Just don't read them from the qualified identifier) - Return a nice html-formatted short-description that can be integrated into the navigation-widget. Extracting the related parts required quite a bit of hacking, and the html has to be transformed so it fits into the navigation-widget better. The result is great. Now you can see the Qt documentation for every class/function just by pointing the cursor at a use of it. :-)

--- documentation/qthelp/qthelpplugin.cpp
+++ documentation/qthelp/qthelpplugin.cpp
@@ -1,5 +1,6 @@
 /*  This file is part of KDevelop
     Copyright 2009 Aleix Pol <aleixpol@kde.org>
+    Copyright 2009 David Nolden <david.nolden.kdevelop@art-master.de>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -35,6 +36,7 @@
 #include <language/duchain/duchainlock.h>
 
 #include "qthelpnetwork.h"
+#include <QWebFrame>
 
 K_PLUGIN_FACTORY(QtHelpFactory, registerPlugin<QtHelpPlugin>(); )
 K_EXPORT_PLUGIN(QtHelpFactory(KAboutData("kdevqthelp","kdevqthelp", ki18n("QtHelp"), "0.1", ki18n("Check Qt Help documentation"), KAboutData::License_GPL)))
@@ -46,7 +48,128 @@ class QtHelpDocumentation : public KDevelop::IDocumentation
 			: m_info(info), m_engine(e), m_name(name) { Q_ASSERT(!m_info.isEmpty()); }
 		
 		virtual QString name() const { return m_name; }
-		virtual QString description() const { return QStringList(m_info.keys()).join(", "); }
+		virtual QString description() const {
+            HelpNetworkAccessManager access(m_engine, 0);
+            QUrl url(m_info.values().first());
+            QByteArray data = m_engine->fileData(url);
+
+            //Extract a short description from the html data
+            QString dataString = QString::fromLatin1(data); ///@todo encoding
+            QString fragment = url.fragment();
+            
+            QString p = "((\\\")|(\\\'))";
+            QString exp = "< a name = " + p + fragment + p + " > < / a >";
+            QString optionalSpace = "( )*";
+            exp.replace(" ", optionalSpace);
+            QRegExp findFragment(exp);
+            int pos = findFragment.indexIn(dataString);
+            if(fragment.isEmpty()) {
+                pos = 0;
+            }else{
+                //Check if there is a title opening-tag right before the fragment, and if yes add it, so we have a nicely formatted caption
+                QString titleRegExp("< h\\d class = \".*\" >");
+                titleRegExp.replace(" ", optionalSpace);
+                QRegExp findTitle(titleRegExp);
+                int titleStart = findTitle.lastIndexIn(dataString, pos);
+                int titleEnd = titleStart + findTitle.matchedLength();
+                if(titleStart != -1) {
+                    QString between = dataString.mid(titleEnd, pos-titleEnd).trimmed();
+//                     if(between.isEmpty())
+                        pos = titleStart;
+                }
+            }
+            
+            if(pos != -1) {
+                
+                QString exp = "< a name = " + p + "((\\S)*)" + p + " > < / a >";
+                exp.replace(" ", optionalSpace);
+                QRegExp nextFragmentExpression(exp);
+                int endPos = nextFragmentExpression.indexIn(dataString, pos+(fragment.size() ? findFragment.matchedLength() : 0));
+                if(endPos == -1)
+                    endPos = dataString.size();
+
+                {
+                    //Find the end of the last paragraph or newline, so we don't add prefixes of the following fragment
+                    QString newLineRegExp("< br / > | < / p >");
+                    newLineRegExp.replace(" ", optionalSpace);
+                    QRegExp lastNewLine(newLineRegExp);
+                    int newEnd = dataString.lastIndexOf(lastNewLine, endPos);
+                    if(newEnd != -1 && newEnd > pos)
+                        endPos = newEnd + lastNewLine.matchedLength();
+                }
+                
+                {
+                    //Find the title, and start from there
+                    QString titleRegExp("< h\\d class = \"title\" >");
+                    titleRegExp.replace(" ", optionalSpace);
+                    QRegExp findTitle(titleRegExp);
+                    int idx = findTitle.indexIn(dataString);
+                    if(idx > pos && idx < endPos)
+                        pos = idx;
+                }
+             
+                
+                QString thisFragment = dataString.mid(pos, endPos - pos);
+                
+                {
+                    //Completely remove the first large header found, since we don't need a header
+                    QString headerRegExp("< h\\d.*>.*< / h\\d >");
+                    headerRegExp.replace(" ", optionalSpace);
+                    QRegExp findHeader(headerRegExp);
+                    findHeader.setMinimal(true);
+                    int idx = findHeader.indexIn(thisFragment);
+                    if(idx != -1) {
+                        thisFragment.remove(idx, findHeader.matchedLength());
+                    }
+                }
+                
+                {
+                    //Replace all gigantic header-font sizes with <big>
+
+                    {
+                        QString sizeRegExp("< h\\d ");
+                        sizeRegExp.replace(" ", optionalSpace);
+                        QRegExp findSize(sizeRegExp);
+                        thisFragment.replace(findSize, "<big ");
+                    }
+                    {
+                        QString sizeCloseRegExp("< / h\\d >");
+                        sizeCloseRegExp.replace(" ", optionalSpace);
+                        QRegExp closeSize(sizeCloseRegExp);
+                        thisFragment.replace(closeSize, "</big><br />");
+                    }
+                }
+                
+                {
+                    //Replace paragraphs by newlines
+                    
+                    QString begin("< p >");
+                    begin.replace(" ", optionalSpace);
+                    
+                    QRegExp findBegin(begin);
+                    thisFragment.replace(findBegin, "");
+
+                    QString end("< /p >");
+                    end.replace(" ", optionalSpace);
+                    
+                    QRegExp findEnd(end);
+                    thisFragment.replace(findEnd, "<br />");
+                }
+                
+                {
+                    //Remove links, because they won't work
+                    QString link("< a href = " + p + ".*" + p);
+                    link.replace(" ", optionalSpace);
+                    QRegExp exp(link, Qt::CaseSensitive);
+                    exp.setMinimal(true);
+                    thisFragment.replace(exp, "<a ");
+                }
+                
+                return thisFragment;
+            }
+            
+            return QStringList(m_info.keys()).join(", ");
+        }
 		virtual QWidget* documentationWidget(QWidget* parent)
 		{
 // 			QTemporaryFile file;
@@ -98,7 +221,6 @@ QString qtDocsLocation(const QString& qmake)
 QtHelpPlugin::QtHelpPlugin(QObject* parent, const QVariantList& args)
 	: KDevelop::IPlugin(QtHelpFactory::componentData(), parent)
 	, m_engine(KStandardDirs::locateLocal("data", "qthelpcollection", QtHelpFactory::componentData()))
-    , m_rx("<[a-zA-Z, ]*>")
 {
 	QStringList qmakes;
     KStandardDirs::findAllExe(qmakes, "qmake");
@@ -128,26 +250,18 @@ QtHelpPlugin::QtHelpPlugin(QObject* parent, const QVariantList& args)
 	kDebug() << "registered" << b << m_engine.error() << m_engine.registeredDocumentations();
 }
 
-QString QtHelpPlugin::removeTemplateInstantiation(const QString& id) const
-{
-    m_rx.indexIn(id);
-    QString ret(id);
-    foreach(const QString& match, m_rx.capturedTexts())
-    {
-        ret.replace(match, QString());
-    }
-    kDebug() << "result:" << ret << "from:" << id;
-    return ret;
-}
-
 KSharedPtr< KDevelop::IDocumentation > QtHelpPlugin::documentationForDeclaration(KDevelop::Declaration* dec)
 {
 	KDevelop::DUChainReadLocker lock(KDevelop::DUChain::lock());
 	
-	if(dec && dec->internalContext()) {
-		QString id = dec->internalContext()->scopeIdentifier(true).toString();
+	if(dec) {
+        KDevelop::QualifiedIdentifier qid = dec->qualifiedIdentifier();
+        QStringList idLIst;
+        for(int a = 0; a < qid.count(); ++a)
+            idLIst << qid.at(a).identifier().str(); //Copy over the identifier components, without the template-parameters
+        
+		QString id = idLIst.join("::");
 		if(!id.isEmpty()) {
-            id=removeTemplateInstantiation(id);
 			QMap<QString, QUrl> links=m_engine.linksForIdentifier(id);
             
             kDebug() << "doc_found" << id << links;
