feda530fe6296d204c270e1644dcebe703d3f90d
Laurent Montel <montel@kde.org>
2009-03-25 12:21:43 +0000

    Extract string from kcfg

--- tools/documentview/Messages.sh
+++ tools/documentview/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$EXTRACTRC `find . -name \*.rc` `find . -name \*.ui` >>rc.cpp
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >>rc.cpp
 $XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevdocumentview.pot
 rm -f rc.cpp
