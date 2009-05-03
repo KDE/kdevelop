86769603ceeec525445032f008a1e30b9e0632cf
Andreas Pakulat <apaku@gmx.de>
2009-05-03 17:59:13 +0000

    Implement signals connections and read/write config settings

--- debuggers/valgrind/valgrindconfig.cpp
+++ debuggers/valgrind/valgrindconfig.cpp
@@ -135,6 +135,21 @@ ValgrindConfigPage::ValgrindConfigPage(QWidget* parent)
 {
     ui = new Ui::ValgrindConfig();
     ui->setupUi( this );
+    
+    connect( ui->valgrindExecutable, SIGNAL(textChanged(QString)), SIGNAL(changed()) );
+    connect( ui->valgrindExecutable, SIGNAL(urlSelected(KUrl)), SIGNAL(changed()) );
+    connect( ui->valgrindParameters, SIGNAL(textEdited(KUrl)), SIGNAL(changed()) );
+    connect( ui->freeBlockList, SIGNAL(valueChanged(int)), SIGNAL(changed()) );
+    connect( ui->happensBefore, SIGNAL(currentIndexChanged(int)), SIGNAL(changed()) );
+    connect( ui->leakResolution, SIGNAL(currentIndexChanged(int)), SIGNAL(changed()) );
+    connect( ui->leakSummary, SIGNAL(currentIndexChanged(int)), SIGNAL(changed()) );
+    connect( ui->limitErrors, SIGNAL(toggled(bool)), SIGNAL(changed()) );
+    connect( ui->maxStackFrame, SIGNAL(valueChanged(int)), SIGNAL(changed()) );
+    connect( ui->numCallers, SIGNAL(valueChanged(int)), SIGNAL(changed()) );
+    connect( ui->separateThreads, SIGNAL(toggled(bool)), SIGNAL(changed()) );
+    connect( ui->showReachable, SIGNAL(toggled(bool)), SIGNAL(changed()) );
+    connect( ui->simulateCache, SIGNAL(toggled(bool)), SIGNAL(changed()) );
+    connect( ui->simulateHWPref, SIGNAL(toggled(bool)), SIGNAL(changed()) );
 }
 
 KIcon ValgrindConfigPage::icon() const
@@ -144,12 +159,37 @@ KIcon ValgrindConfigPage::icon() const
 
 void ValgrindConfigPage::loadFromConfiguration(const KConfigGroup& cfg)
 {
-
+    ui->valgrindExecutable->setUrl( cfg.readEntry( "Valgrind Executable", KUrl( "/usr/bin/valgrind" ) ) );
+    ui->valgrindParameters->setText( cfg.readEntry( "Valgrind Arguments", "" ) );
+    ui->numCallers->setValue( cfg.readEntry( "Framestack Depth", 12 ) );
+    ui->maxStackFrame->setValue( cfg.readEntry( "Maximum Framestack Size", 2000000 ) );
+    ui->limitErrors->setChecked( cfg.readEntry( "Limit Errors", false ) );
+    ui->leakSummary->setCurrentIndex( cfg.readEntry( "Leak Summary", 1 ) );
+    ui->leakResolution->setCurrentIndex( cfg.readEntry( "Leak Resolution Matching", 0 ) );
+    ui->showReachable->setChecked( cfg.readEntry("Show Reachable Blocks", false ) );
+    ui->freeBlockList->setValue( cfg.readEntry( "Free Block List Size", 5000000 ) );
+    ui->separateThreads->setChecked( cfg.readEntry( "Separate Thread Reporting", false ) );
+    ui->simulateCache->setChecked( cfg.readEntry( "Full Cache Simulation", false ) );
+    ui->simulateHWPref->setChecked( cfg.readEntry( "Simulate Hardware Prefetcher", false ) );
+    ui->happensBefore->setCurrentIndex( cfg.readEntry("Extra Synchronization Events", 0 ) );
 }
 
-void ValgrindConfigPage::saveToConfiguration(KConfigGroup ) const
+void ValgrindConfigPage::saveToConfiguration( KConfigGroup cfg ) const
 {
-
+    
+    cfg.writeEntry( "Valgrind Executable", ui->valgrindExecutable->url() );
+    cfg.writeEntry( "Valgrind Arguments", ui->valgrindParameters->text() );
+    cfg.writeEntry( "Framestack Depth", ui->numCallers->value() );
+    cfg.writeEntry( "Maximum Framestack Size", ui->maxStackFrame->value() );
+    cfg.writeEntry( "Limit Errors", ui->limitErrors->isChecked() );
+    cfg.writeEntry( "Leak Summary", ui->leakSummary->currentIndex() );
+    cfg.writeEntry( "Leak Resolution Matching", ui->leakResolution->currentIndex() );
+    cfg.writeEntry( "Show Reachable Blocks", ui->showReachable->isChecked() );
+    cfg.writeEntry( "Free Block List Size", ui->freeBlockList->value() );
+    cfg.writeEntry( "Separate Thread Reporting", ui->separateThreads->isChecked() );
+    cfg.writeEntry( "Full Cache Simulation", ui->simulateCache->isChecked() );
+    cfg.writeEntry( "Simulate Hardware Prefetcher", ui->simulateHWPref->isChecked() );
+    cfg.writeEntry( "Extra Synchronization Events", ui->happensBefore->currentIndex() );
 }
 
 QString ValgrindConfigPage::title() const
