9bd453b62dcedfe2f7b08df4f168991cca042306
Andreas Pakulat <apaku@gmx.de>
2009-05-05 18:33:57 +0000

    Remove the copied code, it doesn't make the slightest sense to copy it as it is already a public and installed library. So we can just use it.

--- debuggers/gdb/ksysguard/config-ksysguard.h.cmake
+++
@@ -1,8 +0,0 @@
-/* Define to 1 if you have the <sys/ptrace.h> header file. */
-#cmakedefine HAVE_SYS_PTRACE_H 1
-
-/* Define to 1 if you have the <sys/endian.h> header file. */
-#cmakedefine HAVE_SYS_ENDIAN_H 1
-
-/* Define to 1 if you have the <byteswap.h> header file. */
-#cmakedefine HAVE_BYTESWAP_H 1
--- debuggers/gdb/ksysguard/processui/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp *.cc -o $podir/processui.pot
-rm -f rc.cpp
