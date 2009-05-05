4c7caf74f492b2c3985521de455c399ff578e127
Andreas Pakulat <apaku@gmx.de>
2009-05-05 20:27:39 +0000

    Even better use KDE_IS_VERSION, thanks Pino for noticing.

--- debuggers/gdb/processselection.cpp
+++ debuggers/gdb/processselection.cpp
@@ -40,7 +40,7 @@ ProcessSelectionDialog::ProcessSelectionDialog(QWidget *parent)
     connect(m_processList->treeView()->selectionModel(), SIGNAL(selectionChanged(QItemSelection,QItemSelection)), SLOT(selectionChanged()));
     m_processList->treeView()->setSelectionMode(QAbstractItemView::SingleSelection);
     m_processList->setState(ProcessFilter::UserProcesses);
-#if KDE_VERSION > KDE_MAKE_VERSION( 4, 2, 0 )
+#if KDE_IS_VERSION( 4, 2, 0 )
     m_processList->setKillButtonVisible(false);
 #endif
     //m_processList->setPidFilter(qApp->pid());
