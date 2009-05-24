9db9cc3bf1e412095696456ce5511b469f0747a6
Andreas Pakulat <apaku@gmx.de>
2009-05-24 08:16:49 +0000

    Move the qmake manager to playground as QMake support is not working as expected and will possibly not be part of the release.

--- projectmanagers/qmake/Messages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-files=`find . -name \*.rc -o -name \*.ui`
-if [ "x$files" != "x" ]; then
-       $EXTRACTRC $files >> rc.cpp
-fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevqmake.pot
-rm -f rc.cpp
