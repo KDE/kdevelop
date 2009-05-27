c8e7986917ccbda92604a3f50262b8294769236a
Ramon Zarazua <killerfox512@gmail.com>
2009-05-27 19:51:36 +0000

    Changed IAstParseSession to IAstContainer, and moved it to where it should be.

--- language/interfaces/iastcontainer.h
+++ language/interfaces/iastcontainer.h
@@ -15,30 +15,23 @@
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.*/
 
-#ifndef KDEVELOP_IASTPARSESESSION_H
-#define KDEVELOP_IASTPARSESESSION_H
+#ifndef KDEVELOP_IASTCONTAINER_H
+#define KDEVELOP_IASTCONTAINER_H
 
 namespace KDevelop
 {
 
 /**
- * \brief Represents A single parse session that created an AST for a particular language
+ * \brief Represents A single parse session that created a language-specific AST for a single translation unit
  *
- * This class is mainly just used as a tag to keep pointers to language specific parse sessions
+ * This class is mainly just used as a tag to keep pointers to an AST of a translation unit(file)
  * in a KDevelop::TopDUContext, and passed over to Refactoring plugins so they can manipulate
  * their language-specific AstChangeSets.
  */
-class IAstParseSession
+class IAstContainer
 {
-  public:
-    /**
-     * Return top-most Ast node to access the rest of the AST, the user MUST know 
-     * what the type of the node will be returned, depending on the particular Ast
-     * represented
-     */
-    virtual void * topAstNode(void) = 0;
 };
 
 }
 
-#endif // KDEVELOP_IASTPARSESESSION_H
+#endif // KDEVELOP_IASTCONTAINER_H
