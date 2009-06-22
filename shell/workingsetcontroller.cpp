91fda72330b0f4330b4fe4e070148b31e14bb294
David Nolden <david.nolden.kde@art-master.de>
2009-06-22 10:05:39 +0000

    - Disable the "Delete" button if the working set is still active in some area BUG: 197484

--- shell/workingsetcontroller.cpp
+++ shell/workingsetcontroller.cpp
@@ -754,7 +754,7 @@ void WorkingSetToolTipWidget::updateFileButtons()
     
     m_mergeButton->setEnabled(!allOpen && mainWindow->area()->workingSet() != m_set->id());
     m_subtractButton->setEnabled(!noneOpen && mainWindow->area()->workingSet() != m_set->id());
-    m_deleteButton->setEnabled(mainWindow->area()->workingSet() != m_set->id());
+    m_deleteButton->setEnabled(!m_set->hasConnectedAreas());
     
     if(m_set->id() == mainWindow->area()->workingSet()) {
         disconnect(m_openButton, SIGNAL(clicked(bool)), m_setButton, SLOT(loadSet()));
