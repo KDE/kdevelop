350c93d0ade342355ea86222963314f223b93621
Niko Sams <niko.sams@gmail.com>
2009-06-24 20:12:58 +0000

    Remove all locals after debugger stopped.
    Fixes bug that locals are shown dead (grayed out) in second debug run.

--- debugger/variable/variablecollection.h
+++ debugger/variable/variablecollection.h
@@ -131,6 +131,7 @@ class KDEVPLATFORMDEBUGGER_EXPORT Locals : public TreeItem
 public:
     Locals(TreeModel* model, TreeItem* parent);
     void updateLocals(QStringList locals);
+    using TreeItem::clear;
 
 private:
     void fetchMoreChildren() {}
