a64a6ae887a726878701fd57053f4c2344ce5571
Andreas Pakulat <apaku@gmx.de>
2009-06-28 18:06:39 +0000

    Change the load test to really only test parsing and evaluating the cmake files, no involvement of the cmake plugin itself. Makes things easier and should produce better valgrind-callgrind results as the GUI doesn't need to load.

--- projectmanagers/cmake/tests/cmakeloadprojecttest.cpp
+++ projectmanagers/cmake/tests/cmakeloadprojecttest.cpp
@@ -1,84 +1,76 @@
-/***************************************************************************
- *   Copyright 2009 Andreas Pakulat <apaku@gmx.de>                         *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Library General Public License as       *
- *   published by the Free Software Foundation; either version 2 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this program; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
- ***************************************************************************/
+/* KDevelop CMake Support
+ *
+ * Copyright 2006 Matt Rogers <mattr@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
 
 #include "cmakeloadprojecttest.h"
 
-#include <qtest_kde.h>
-#include <QtTest/QtTest>
+#include <language/duchain/duchainlock.h>
+#include <language/duchain/duchain.h>
+#include <language/duchain/topducontext.h>
 
-#include <kactioncollection.h>
-#include <kxmlguifactory.h>
-#include <kdebug.h>
-#include <kapplication.h>
 
-#include <shell/core.h>
-#include <interfaces/iprojectcontroller.h>
+#include "cmListFileLexer.h"
+#include "cmakelistsparser.h"
+#include "cmakeprojectvisitor.h"
+#include "cmakeast.h"
+
+QTEST_MAIN( CMakeLoadProjectTest )
+
+using namespace KDevelop;
 
 CMakeLoadProjectTest::CMakeLoadProjectTest()
 {
-    s_instance = this;
 }
 
-QString CMakeLoadProjectTest::xmlFile()
+CMakeLoadProjectTest::~CMakeLoadProjectTest()
 {
-    return "kdevelopui.rc";
-}
-KDevelop::AreaParams CMakeLoadProjectTest::defaultArea()
-{
-    KDevelop::AreaParams p = { "code", i18n("Code" ) };
-    return p;
-}
-QString CMakeLoadProjectTest::projectFileExtension()
-{
-    return "kdev4";
-}
-QString CMakeLoadProjectTest::projectFileDescription()
-{
-    return i18n("KDevelop4 Project files");
-}
-QStringList CMakeLoadProjectTest::defaultPlugins()
-{
-    return QStringList() << "KDevCMakeManager" << "KDevProjectManagerView";
 }
 
-int main(int argc, char** argv)
+void CMakeLoadProjectTest::testTinyCMakeProject()
 {
-    KAboutData aboutdata( "cmakeloadprojecttest", 0, ki18n("cmakeloadprojecttest"), "0.0.0", ki18n("small utility to do manual performance checks of cmake project load times"), KAboutData::License_GPL);
-    KCmdLineArgs::init( argc, argv, &aboutdata );
-    KCmdLineOptions options;
-    options.add( "project <project>", ki18n("url to project") );
-    KCmdLineArgs::addCmdLineOptions( options );
-    KCmdLineArgs* args = KCmdLineArgs::parsedArgs();
-    KApplication app;
-    CMakeLoadProjectTest shell;
-    Core::initialize();
-    Core* core = Core::self();
-    QString project = args->getOption( "project" );
-    if( project.isEmpty() ) 
+    KDevelop::ReferencedTopDUContext m_fakeContext;
     {
-        qWarning() << "Need a project to load";
-        exit(1);
+        DUChainWriteLocker lock(DUChain::lock());
+        m_fakeContext = new TopDUContext(IndexedString("test"), SimpleRange(0,0,0,0));
+        DUChain::self()->addDocumentChain(m_fakeContext);
     }
-    core->projectController()->openProject( KUrl( project ) );
-    args->clear();
-    return app.exec();
+
+    QString projectfile = CMAKE_TESTS_PROJECTS_DIR"/tiny_project/CMakeLists.txt";
+    CMakeFileContent code=CMakeListsParser::readCMakeFile(projectfile);
+    QVERIFY(code.count() != 0);
+
+    MacroMap mm;
+    VariableMap vm;
+    CacheValues cv;
+    vm.insert("CMAKE_CURRENT_SOURCE_DIR", QStringList("."));
+
+    CMakeProjectVisitor v(projectfile, m_fakeContext);
+    v.setVariableMap(&vm);
+    v.setMacroMap(&mm);
+    v.setCacheValues(&cv);
+//     v.setModulePath();
+    v.walk(code, 0);
+
+    ReferencedTopDUContext ctx=v.context();
+    QVERIFY(ctx);
 
 }
 
+#include "cmakeloadprojecttest.moc"
 
--- projectmanagers/cmake/tests/cmakeloadprojecttest.h
+++ projectmanagers/cmake/tests/cmakeloadprojecttest.h
@@ -1,37 +1,48 @@
-/***************************************************************************
- *   Copyright 2009 Andreas Pakulat <apaku@gmx.de>                         *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Library General Public License as       *
- *   published by the Free Software Foundation; either version 2 of the    *
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU Library General Public     *
- *   License along with this program; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
- ***************************************************************************/
+/* KDevelop CMake Support
+ *
+ * Copyright 2006 Matt Rogers <mattr@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 #ifndef CMAKELOADPROJECTTEST_H
 #define CMAKELOADPROJECTTEST_H
 
-#include <QObject>
-#include <shell/shellextension.h>
+#include <QtTest/QtTest>
 
-using namespace KDevelop;
+class CMakeAst;
 
-class CMakeLoadProjectTest : public KDevelop::ShellExtension {
+/**
+ * A test for the CMake parser and lexer loading three different real projects
+ * Useful for doing call-analyzation and memory checking with valgrind
+ * @author Matt Rogers <mattr@kde.org>
+ */
+class CMakeLoadProjectTest : public QObject
+{
+Q_OBJECT
 public:
     CMakeLoadProjectTest();
-    virtual QString xmlFile();
-    virtual KDevelop::AreaParams defaultArea();
-    virtual QString projectFileExtension();
-    virtual QString projectFileDescription();
-    virtual QStringList defaultPlugins();
+    ~CMakeLoadProjectTest();
+
+private slots:
+    void testTinyCMakeProject();
+    //void testSmallQ4Project();
+    //void testSmallKDE4Project();
+private:
+
 };
 
 #endif
