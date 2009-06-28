24292b3a7a3b6cebffed10a2a88272fa406a6da7
Andreas Pakulat <apaku@gmx.de>
2009-06-28 18:06:12 +0000

    It doesn't make any sense to translate the project file extension.

--- app/kdevideextension.cpp
+++ app/kdevideextension.cpp
@@ -51,7 +51,7 @@ KDevelop::AreaParams KDevIDEExtension::defaultArea()
 
 QString KDevIDEExtension::projectFileExtension()
 {
-    return i18n("kdev4");
+    return "kdev4";
 }
 
 QString KDevIDEExtension::projectFileDescription()
