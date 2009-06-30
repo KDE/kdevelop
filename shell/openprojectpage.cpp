2c9b83c15de0428a286fd6204b892f8d18b0f832
Andreas Pakulat <apaku@gmx.de>
2009-06-30 20:37:33 +0000

    Use the new API from KDE 4.4 to read the configuration from a custom config group.

--- shell/openprojectpage.cpp
+++ shell/openprojectpage.cpp
@@ -70,18 +70,16 @@ OpenProjectPage::OpenProjectPage( const KUrl& startUrl, QWidget* parent )
 
     fileWidget->setMode( KFile::Modes( KFile::File | KFile::Directory | KFile::ExistingOnly ) );
 
-    // TODO for 4.4: (don't dare to introduce new API for 4.3 after the rc1)
-    // Provide a setDefaultView function on KDirOperator so we don't have to
-    // put it into the config and hence allow the user to store his favourite
-    // setting.
     KConfigGroup projectdialogsettings = KGlobal::config()->group( "Open Project Dialog Settings" );
+#if KDE_IS_VERSION(4,2,62) && !KDE_IS_VERSION(4,3,60)
     projectdialogsettings.writeEntry( "View Style", "Simple" );
-#if KDE_IS_VERSION(4,2,62)
     // Enforce "short view" for now as KFileWidget has no API to read its config from a different
     // but the global-default for KFileDialog.
     // TODO for 4.4: Add the needed API to KFileWidget
     fileWidget->dirOperator()->readConfig( projectdialogsettings );
     fileWidget->dirOperator()->setViewConfig( projectdialogsettings );
+#elif KDE_IS_VERSION(4,3,60)
+    fileWidget->readConfig( projectdialogsettings );
 #endif
 
     layout->addWidget( fileWidget );
