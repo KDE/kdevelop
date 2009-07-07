cbed22f2315dc5041ebb01ca05b98633bf600242
Milian Wolff <mail@milianw.de>
2009-07-07 21:47:56 +0000

    fix compile warnings: CodeHighlightingInstance contains virtual function, hence requires a virtual dtor instantiate m_useClassCache and m_highlighting in correct order

--- language/highlighting/codehighlighting.h
+++ language/highlighting/codehighlighting.h
@@ -88,7 +88,9 @@ enum Contexts {
 
 class KDEVPLATFORMLANGUAGE_EXPORT CodeHighlightingInstance : public HighlightingEnumContainer {
   public:
-    CodeHighlightingInstance(const CodeHighlighting* highlighting) : m_highlighting(highlighting), m_useClassCache(false) {
+    CodeHighlightingInstance(const CodeHighlighting* highlighting) : m_useClassCache(false), m_highlighting(highlighting) {
+    }
+    virtual ~CodeHighlightingInstance(){
     }
 
     virtual void highlightDeclaration(KDevelop::Declaration* declaration, const QColor &color) const;
