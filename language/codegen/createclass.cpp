cfd7d26a1c85616de0f73cf70920e25faa088f5b
Andreas Pakulat <apaku@gmx.de>
2009-07-26 00:51:46 +0000

    --debug

--- language/codegen/createclass.cpp
+++ language/codegen/createclass.cpp
@@ -86,17 +86,13 @@ void CreateClassWizard::setup()
 
 int CreateClassWizard::nextId() const
 {
-    qDebug() << "fetching next id" << dynamic_cast<ClassIdentifierPage*>( currentPage() );
     if( ClassIdentifierPage* idpage = dynamic_cast<ClassIdentifierPage*>( currentPage() ) )
     {
-        qDebug() << "inheritance list:" << idpage->inheritanceList();
         if( idpage->inheritanceList().isEmpty() )
         {
-            qDebug() << "returning nextid of next id" << idpage->nextId() << page( idpage->nextId() ) << page( idpage->nextId() )->nextId();
             return page( idpage->nextId() )->nextId();
         }
     }
-    qDebug() << "doing default";
     return QWizard::nextId();
 }
 
