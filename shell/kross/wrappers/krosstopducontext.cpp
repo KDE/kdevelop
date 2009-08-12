4745713eb5e08e860eb8ce67d94f2f79c560a200
Aleix Pol Gonzalez <aleixpol@gmail.com>
2009-08-12 19:43:02 +0000

    Revert my last commit since looks like doesn't let kdevplatform to compile.

--- shell/kross/wrappers/krosstopducontext.cpp
+++ shell/kross/wrappers/krosstopducontext.cpp
@@ -45,11 +45,11 @@ class KrossKDevelopTopDUContext : public QObject, public Kross::WrapperInterface
 	Q_FLAGS(Flags NoFlags UpdatingContext LastFlag)
 
 	Q_ENUMS(Features)
-	Q_FLAGS(Features VisibleDeclarationsAndContexts AllDeclarationsAndContexts AllDeclarationsContextsAndUses AllDeclarationsContextsAndUsesForRecursive AST AllDeclarationsContextsUsesAndAST ForceUpdate ForceUpdateRecursive)
+	Q_FLAGS(Features VisibleDeclarationsAndContexts AllDeclarationsAndContexts AllDeclarationsContextsAndUses AllDeclarationsContextsAndUsesForRecursive AST ForceUpdate ForceUpdateRecursive)
 
 	public:
 		enum KrossFlags { NoFlags=KDevelop::TopDUContext::NoFlags, UpdatingContext=KDevelop::TopDUContext::UpdatingContext, LastFlag=KDevelop::TopDUContext::LastFlag };
-		enum KrossFeatures { VisibleDeclarationsAndContexts=KDevelop::TopDUContext::VisibleDeclarationsAndContexts, AllDeclarationsAndContexts=KDevelop::TopDUContext::AllDeclarationsAndContexts, AllDeclarationsContextsAndUses=KDevelop::TopDUContext::AllDeclarationsContextsAndUses, AllDeclarationsContextsAndUsesForRecursive=KDevelop::TopDUContext::AllDeclarationsContextsAndUsesForRecursive, AST=KDevelop::TopDUContext::AST, AllDeclarationsContextsUsesAndAST=KDevelop::TopDUContext::AllDeclarationsContextsUsesAndAST, ForceUpdate=KDevelop::TopDUContext::ForceUpdate, ForceUpdateRecursive=KDevelop::TopDUContext::ForceUpdateRecursive };
+		enum KrossFeatures { VisibleDeclarationsAndContexts=KDevelop::TopDUContext::VisibleDeclarationsAndContexts, AllDeclarationsAndContexts=KDevelop::TopDUContext::AllDeclarationsAndContexts, AllDeclarationsContextsAndUses=KDevelop::TopDUContext::AllDeclarationsContextsAndUses, AllDeclarationsContextsAndUsesForRecursive=KDevelop::TopDUContext::AllDeclarationsContextsAndUsesForRecursive, AST=KDevelop::TopDUContext::AST, ForceUpdate=KDevelop::TopDUContext::ForceUpdate, ForceUpdateRecursive=KDevelop::TopDUContext::ForceUpdateRecursive };
 		KrossKDevelopTopDUContext(KDevelop::TopDUContext* obj, QObject* parent=0) : QObject(parent), wrapped(obj)		{ setObjectName("KDevelop::TopDUContext"); }
 		void* wrappedObject() const { return wrapped; }
 
