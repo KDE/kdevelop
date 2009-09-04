bf69a1c60d3ac23b03b056492af1ece47a786c6a
Vladimir Prus <ghost@cs.msu.su>
2009-09-04 12:34:33 +0000

    Remove floatsam.

--- debugger/variable/variablecollection.h
+++ debugger/variable/variablecollection.h
@@ -171,10 +171,6 @@ private:
 class KDEVPLATFORMDEBUGGER_EXPORT VariableCollection : public TreeModel
 {
     Q_OBJECT
-#if 0
-    friend class AbstractVariableItem;
-    friend class VariableItem;
-#endif
 
 public:
     VariableCollection(IDebugController* parent);
@@ -191,91 +187,6 @@ public Q_SLOTS:
 private Q_SLOTS:
     void updateAutoUpdate(KDevelop::IDebugSession* session = 0);
 
-    
-#if 0
-    void addItem(AbstractVariableItem* item);
-    void deleteItem(AbstractVariableItem* item);
-
-    // Item model reimplementations
-    virtual bool canFetchMore ( const QModelIndex & parent ) const;
-    virtual int columnCount( const QModelIndex & parent = QModelIndex() ) const;
-    virtual QVariant data( const QModelIndex & index, int role = Qt::DisplayRole ) const;
-    virtual void fetchMore ( const QModelIndex & parent );
-    virtual Qt::ItemFlags flags( const QModelIndex & index ) const;
-    virtual bool hasChildren( const QModelIndex & parent = QModelIndex() ) const;
-    virtual QVariant headerData( int section, Qt::Orientation orientation, int role = Qt::DisplayRole ) const;
-    virtual QModelIndex index( int row, int column, const QModelIndex & parent = QModelIndex() ) const;
-    virtual QModelIndex parent( const QModelIndex & index ) const;
-    virtual int rowCount( const QModelIndex & parent = QModelIndex() ) const;
-    virtual bool setData( const QModelIndex & index, const QVariant & value, int role = Qt::EditRole );
-
-    AbstractVariableItem* itemForIndex(const QModelIndex& index) const;
-
-    FrameItem* findFrame(int frameNo, int threadNo) const;
-    WatchItem* findWatch();
-
-    AbstractVariableItem* itemForVariableObject(const QString& variableObject) const;
-    void addVariableObject(const QString& variableObject, AbstractVariableItem* item);
-    void removeVariableObject(const QString& variableObject);
-
-    VariableItem* createVariableItem(const QString& type, AbstractVariableItem* parent);
-
-
-
-Q_SIGNALS:
-    void toggleWatchpoint(const QString &varName);
-
-public Q_SLOTS:
-    void slotAddWatchVariable(const QString& watchVar);
-    void slotEvaluateExpression(const QString& expression);
-
-    void slotEvent(event_t);
-
-protected:
-    // Methods for AbstractVariableItem to access
-    void prepareInsertItems(AbstractVariableItem* parent, int index, int count);
-    void completeInsertItems();
-    void prepareRemoveItems(AbstractVariableItem* parent, int index, int count);
-    void completeRemoveItems();
-    void dataChanged(AbstractVariableItem* item, int column);
-
-private:
-    // Callbacks for gdb commands
-    void frameIdReady(const QStringList&);
-    void handleVarUpdate(const GDBMI::ResultRecord&);
-    void handleEvaluateExpression(const QStringList&);
-    void variablesFetchDone();
-    void fetchSpecialValuesDone();
-
-    /** This is called when address of expression for which
-        popup is created is known.
-
-        If there's no address (for rvalue), does nothing
-        (leaving "Data breakpoint" item disabled).
-        Otherwise, enabled that item, and check is we
-        have data breakpoint for that address already.
-    */
-    void handleAddressComputed(const GDBMI::ResultRecord& r);
-
-private:
-    /** Get (if exists) and create (otherwise) frame root for
-        the specified frameNo/threadNo combination.
-    */
-    FrameItem* currentFrame();
-    void updateCurrentFrame();
-
-    AbstractVariableItem* parentForIndex(const QModelIndex& index) const;
-    QModelIndex indexForItem(AbstractVariableItem* item, int column = 0) const;
-
-    int activeFlag_;
-    int iOutRadix;
-    bool justPaused_;
-
-    // Root of all recently printed expressions.
-    AbstractVariableItem* recentExpressions_;
-#endif
-
-
 private Q_SLOTS:
     void textDocumentCreated( KDevelop::IDocument*);
     void viewCreated(KTextEditor::Document*, KTextEditor::View*);
@@ -285,12 +196,6 @@ private:
     VariablesRoot* universe_;
     QPointer<VariableToolTip> activeTooltip_;
     bool m_widgetVisible;
-
-#if 0
-    QMap<QString, AbstractVariableItem*> m_variableObjectMap;
-
-    QList<AbstractVariableItem*> m_items;
-#endif
 };
 
 }
