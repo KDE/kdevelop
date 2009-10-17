04eba48055a9ed543809dbb3f26599cb8b5283a7
Andreas Pakulat <apaku@gmx.de>
2009-10-17 13:37:41 +0000

    Move this plugin to the platform as its useful for other kdevplatform apps too.

--- tools/documentview/Messages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >>rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevdocumentview.pot
-rm -f rc.cpp
