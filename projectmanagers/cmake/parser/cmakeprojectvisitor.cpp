81eb0cd3a069a06a57b9b9d59275665fec1796c2
Aleix Pol Gonzalez <aleixpol@gmail.com>
2009-11-19 22:54:20 +0000

    also provide the range for the deprecated-command problem.

--- projectmanagers/cmake/parser/cmakeprojectvisitor.cpp
+++ projectmanagers/cmake/parser/cmakeprojectvisitor.cpp
@@ -2046,7 +2046,7 @@ int CMakeProjectVisitor::walk(const CMakeFileContent & fc, int line, bool isClea
             KSharedPtr<Problem> p(new Problem);
             p->setDescription(i18n("%1 is a deprecated command and should not be used", func.name));
             p->setRange(it->nameRange());
-            p->setFinalLocation(DocumentRange(url.prettyUrl()));
+            p->setFinalLocation(DocumentRange(url.prettyUrl(), KTextEditor::Range(fc.first().range().start.textCursor(), fc.last().range().end.textCursor())));
             m_topctx->addProblem(p);
         }
         element->setContent(fc, line);
