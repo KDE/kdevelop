00a6916220d717c2aae52ec8aae531724aca274f
Milian Wolff <mail@milianw.de>
2009-12-07 19:47:22 +0000

    use a no-gui KApplication, will lead to crash later on though when we have no XServer (in uiController)

--- util/duchainify/main.cpp
+++ util/duchainify/main.cpp
@@ -135,7 +135,7 @@ int main(int argc, char** argv)
 
     KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
     
-    KApplication app;
+    KApplication app(false);
 
     AutoTestShell::init();
     Core::initialize(KDevelop::Core::NoUi);
