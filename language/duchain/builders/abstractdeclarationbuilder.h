25e513836869e9700f94d95ef91276ceaef273c7
Niko Sams <niko.sams@gmail.com>
2009-12-16 19:56:54 +0000

    DUContext::findLocalDeclarations has TopDUChontext* as 3rd parameter and ABstractType::Ptr as 4th but was called with 3rd and 4th parameter exchanged. Cpp still built (why??), but Php did not.
    Please review.

--- language/duchain/builders/abstractdeclarationbuilder.h
+++ language/duchain/builders/abstractdeclarationbuilder.h
@@ -109,7 +109,7 @@ protected:
       LockedSmartInterface iface = LanguageSpecificDeclarationBuilderBase::editor()->smart();
       SimpleRange translated = LanguageSpecificDeclarationBuilderBase::editor()->translate(iface, newRange);
 
-      QList<Declaration*> declarations = LanguageSpecificDeclarationBuilderBase::currentContext()->findLocalDeclarations(localId, SimpleCursor::invalid(), AbstractType::Ptr(), this->topContext(), DUContext::NoFiltering);
+      QList<Declaration*> declarations = LanguageSpecificDeclarationBuilderBase::currentContext()->findLocalDeclarations(localId, SimpleCursor::invalid(), this->topContext(), AbstractType::Ptr(), DUContext::NoFiltering);
       foreach( Declaration* dec, declarations ) {
 
         if( LanguageSpecificDeclarationBuilderBase::wasEncountered(dec) )
