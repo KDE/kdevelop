4db9c696835266412c915649391118321462d8cf
Andreas Pakulat <apaku@gmx.de>
2009-12-17 00:19:19 +0000

    Increase Plugin version to 8

--- interfaces/iplugin.h
+++ interfaces/iplugin.h
@@ -43,7 +43,7 @@ namespace Sublime {
  * to make sure old source (or binary) incompatible plugins are not loaded.
  * Increase this if it is necessary that old plugins stop working.
  */
-#define KDEVELOP_PLUGIN_VERSION 7
+#define KDEVELOP_PLUGIN_VERSION 8
 
 /**
  * This macro adds an extension interface to register with the extension manager
