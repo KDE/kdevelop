fc552ef34158e84f1525a6656abad519f19e7b7f
Andreas Pakulat <apaku@gmx.de>
2010-02-17 01:39:46 +0100

    Add a simple project loading test which suceeds.
    This is a start for testing that the config-reading, path-matching
    etc. work correctly.

---
+++ tests/custombuildsystemplugintest.cpp
@@ -0,0 +1,71 @@
+/************************************************************************
+ * KDevelop4 Custom Buildsystem Support                                 *
+ *                                                                      *
+ * Copyright 2010 Andreas Pakulat <apaku@gmx.de>                        *
+ *                                                                      *
+ * This program is free software; you can redistribute it and/or modify *
+ * it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation; either version 3 of the License, or    *
+ * (at your option) any later version.                                  *
+ *                                                                      *
+ * This program is distributed in the hope that it will be useful, but  *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     *
+ * General Public License for more details.                             *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with this program; if not, see <http://www.gnu.org/licenses/>. *
+ ************************************************************************/
+
+#include "custombuildsystemplugintest.h"
+
+#include <QtTest/QtTest>
+
+#include <qtest_kde.h>
+
+#include <tests/autotestshell.h>
+#include <tests/testcore.h>
+#include <tests/kdevsignalspy.h>
+#include <interfaces/iprojectcontroller.h>
+#include <KDebug>
+#include <interfaces/iproject.h>
+#include <project/interfaces/ibuildsystemmanager.h>
+#include <project/projectmodel.h>
+
+using KDevelop::Core;
+using KDevelop::ICore;
+using KDevelop::IProject;
+using KDevelop::TestCore;
+using KDevelop::AutoTestShell;
+using KDevelop::KDevSignalSpy;
+
+void CustomBuildSystemPluginTest::init()
+{
+    AutoTestShell::init();
+    m_core = new KDevelop::TestCore();
+    m_core->initialize( Core::Default );
+}
+
+void CustomBuildSystemPluginTest::cleanup()
+{
+    m_core->cleanup();
+    delete m_core;
+}
+
+void CustomBuildSystemPluginTest::loadSimpleProject()
+{
+    KUrl projecturl( PROJECTS_SOURCE_DIR"/simpleproject/simpleproject.kdev4" );
+    KDevSignalSpy* projectSpy = new KDevSignalSpy( ICore::self()->projectController(), SIGNAL( projectOpened( KDevelop::IProject* ) ) );
+    ICore::self()->projectController()->openProject( projecturl );
+    // Wait for the project to be opened
+    if( !projectSpy->wait( 20000 ) ) {
+        kFatal() << "Expected project to be loaded within 20 seconds, but this didn't happen";
+    }
+    IProject* project = ICore::self()->projectController()->projects().at( 0 );
+    KUrl::List includes = project->buildSystemManager()->includeDirectories( project->projectItem() );
+    QCOMPARE( includes, KUrl::List( QStringList() << "/usr/include/mydir" ) );
+}
+
+QTEST_KDEMAIN(CustomBuildSystemPluginTest, GUI)
+
+#include "custombuildsystemplugintest.moc"
