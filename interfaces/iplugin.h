cd686d2ba3b90c43a6d6e2b44c694bcc88e725ca
Andreas Pakulat <apaku@gmx.de>
2010-04-02 16:13:48 +0000

    Prepare 1.0 RC1 release.
    Increase the version number to 0.10.0 for rc1
    Also increase plugin version number to make sure we're only loading
    release-ready and maintained plugins.

--- interfaces/iplugin.h
+++ interfaces/iplugin.h
@@ -43,7 +43,7 @@ namespace Sublime {
  * to make sure old source (or binary) incompatible plugins are not loaded.
  * Increase this if it is necessary that old plugins stop working.
  */
-#define KDEVELOP_PLUGIN_VERSION 8
+#define KDEVELOP_PLUGIN_VERSION 9
 
 /**
  * This macro adds an extension interface to register with the extension manager
