293bfe949fe9588f3c40f8db485d6436b125f006
Milian Wolff <mail@milianw.de>
2010-04-21 15:40:42 +0000

    add todo to make typeForDeclaration virtual for 4.1

--- language/highlighting/codehighlighting.h
+++ language/highlighting/codehighlighting.h
@@ -106,6 +106,7 @@ class KDEVPLATFORMLANGUAGE_EXPORT CodeHighlightingInstance : public Highlighting
     KDevelop::Declaration* localClassFromCodeContext(KDevelop::DUContext* context) const;
     /**
      * @param context Should be the context from where the declaration is used, if a use is highlighted.
+     * TODO: make this virtual for 4.1
      * */
     Types typeForDeclaration(KDevelop::Declaration* dec, KDevelop::DUContext* context) const;
 
