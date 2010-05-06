9c61af094efa6050098d0db25908b392e308149a
Tobias Koenig <tokoe@kde.org>
2010-05-06 13:33:08 +0200

    Add support for parsing of Q_PROPERTY macro

--- languages/cpp/parser/default_visitor.h
+++ languages/cpp/parser/default_visitor.h
@@ -104,6 +104,7 @@ protected:
   virtual void visitWhileStatement(WhileStatementAST *);
   virtual void visitWinDeclSpec(WinDeclSpecAST *);
   virtual void visitSignalSlotExpression(SignalSlotExpressionAST *);
+  virtual void visitQPropertyDeclaration(QPropertyDeclarationAST *);
 
 private:
   typedef void (Visitor::*visitor_fun_ptr)(AST *);
