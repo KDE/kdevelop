8890f1326a5de51252069cfb5503c07e348dac06
Niko Sams <niko.sams@gmail.com>
2010-05-13 20:25:55 +0200

    Don't run test in NoUI mode, as the plugin uses GUI mode.

--- plugins/genericprojectmanager/test/reloadtest.cpp
+++ plugins/genericprojectmanager/test/reloadtest.cpp
@@ -37,8 +37,7 @@ void ProjectLoadTest::init()
 {
     KDevelop::AutoTestShell::init();
     m_core = new KDevelop::TestCore();
-    m_core->initialize( KDevelop::Core::NoUi );
-
+    m_core->initialize( KDevelop::Core::Default );
 }
 
 void ProjectLoadTest::cleanup()
