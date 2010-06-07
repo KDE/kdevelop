48f8905633a13472f3ff2cd6f0284460845b527b
Milian Wolff <mail@milianw.de>
2010-06-07 21:36:48 +0200

    add missing file, fix build - sorry

---
+++ plugins/snippet/snippetfeatures.h
@@ -0,0 +1,21 @@
+/***************************************************************************
+ *   Copyright 2007 Robert Gruber <rgruber@users.sourceforge.net>          *
+ *   Copyright 2010 Milian Wolff <mail@milianw.de>                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ 
+#ifndef SNIPPETFEATURES_H
+#define SNIPPETFEATURES_H
+
+#include <kdeversion.h>
+
+#if KDE_IS_VERSION(4, 4, 60)
+  #define SNIPPETS_HAVE_TPLIFACE2
+#endif
+
+#endif // SNIPPETFEATURES_H
