13e37014b184f33ad880973ba026e10249a42999
Aleix Pol <aleixpol@kde.org>
2010-06-18 18:50:58 +0200

    Add missing files

---
+++ plugins/subversion/svnlocationwidget.cpp
@@ -0,0 +1,25 @@
+/***************************************************************************
+ *   Copyright 2010 Aleix Pol Gonzalez <aleixpol@kde.org>                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "svnlocationwidget.h"
+#include <vcs/vcslocation.h>
+
+SvnLocationWidget::SvnLocationWidget(QWidget* parent, Qt::WindowFlags f)
+    : StandardVcsLocationWidget(parent, f)
+{}
+
+KDevelop::VcsLocation SvnLocationWidget::location() const
+{
+    KUrl loc=url();
+    KDevelop::VcsLocation ret;
+    ret.setRepositoryServer(loc.prettyUrl());
+    return ret;
+}
+
