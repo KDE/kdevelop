58d93fa4b5e86b13a9f5011fcb15ba63415a8e90
Aleix Pol <aleixpol@kde.org>
2010-07-09 00:53:12 +0200

    Added some checks to improve error feedback to the UI.

--- projectmanagers/cmake/cmakemanager.cpp
+++ projectmanagers/cmake/cmakemanager.cpp
@@ -88,6 +88,8 @@
 
 #include <language/highlighting/codehighlighting.h>
 #include <interfaces/iruncontroller.h>
+#include <vcs/interfaces/ibasicversioncontrol.h>
+#include <vcs/vcsjob.h>
 
 using namespace KDevelop;
 
@@ -610,35 +612,6 @@ QList<KDevelop::ProjectFolderItem*> CMakeManager::parse( KDevelop::ProjectFolder
     return folderList;
 }
 
-bool CMakeManager::reload(KDevelop::ProjectFolderItem* folder)
-{
-    CMakeFolderItem* item=dynamic_cast<CMakeFolderItem*>(folder);
-    if ( !item ) {
-        ProjectBaseItem* it = folder;
-        while(!item && it->parent()) {
-            it = it->parent();
-            item = dynamic_cast<CMakeFolderItem*>(it);
-        }
-    }
-
-    if (!item || item == item->project()->projectItem()) {
-        folder->project()->reloadModel();
-    } else {
-        CMakeFolderItem* former=item->formerParent();
-        QString buildDir=item->buildDir();
-        ProjectFolderItem* parent=static_cast<ProjectFolderItem*>(item->parent());
-        KUrl url=item->url();
-        IProject* project=item->project();
-
-        parent->removeRow(item->row());
-        CMakeFolderItem* fi=new CMakeFolderItem(project, url.toLocalFile(), buildDir, 0);
-
-        fi->setFormerParent(former);
-        reimport(fi, parent->url());
-    }
-    return true;
-}
-
 QList<KDevelop::ProjectTargetItem*> CMakeManager::targets() const
 {
     QList<KDevelop::ProjectTargetItem*> ret;
@@ -723,6 +696,35 @@ KDevelop::IProjectBuilder * CMakeManager::builder(KDevelop::ProjectFolderItem *)
     vm->remove("CMAKE_CURRENT_SOURCE_DIR");
     vm->remove("CMAKE_CURRENT_BINARY_DIR");
 }*/
+
+bool CMakeManager::reload(KDevelop::ProjectFolderItem* folder)
+{
+    CMakeFolderItem* item=dynamic_cast<CMakeFolderItem*>(folder);
+    if ( !item ) {
+        ProjectBaseItem* it = folder;
+        while(!item && it->parent()) {
+            it = it->parent();
+            item = dynamic_cast<CMakeFolderItem*>(it);
+        }
+    }
+
+    if (!item || item == item->project()->projectItem()) {
+        folder->project()->reloadModel();
+    } else {
+        CMakeFolderItem* former=item->formerParent();
+        QString buildDir=item->buildDir();
+        ProjectFolderItem* parent=static_cast<ProjectFolderItem*>(item->parent());
+        KUrl url=item->url();
+        IProject* project=item->project();
+
+        parent->removeRow(item->row());
+        CMakeFolderItem* fi=new CMakeFolderItem(project, url.toLocalFile(), buildDir, 0);
+
+        fi->setFormerParent(former);
+        reimport(fi, parent->url());
+    }
+    return true;
+}
 
 void CMakeManager::reimport(KDevelop::ProjectFolderItem* fi, const KUrl& parent)
 {
@@ -1342,11 +1344,11 @@ bool CMakeManager::renameFile(ProjectFileItem* it, const KUrl& newUrl)
         ret = ret || hasChanges;
     }
 
-    if(ret && e.exec())
+    ret &= e.exec()==KDialog::Accepted;
+    if(ret)
     {
         bool ret=e.applyAllChanges();
-        if(ret)
-            ret=KDevelop::renameUrl(it->project(), it->url(), newUrl);
+        ret = ret && KDevelop::renameUrl(it->project(), it->url(), newUrl);
     }
 
     return ret;
@@ -1358,6 +1360,7 @@ bool CMakeManager::renameFolder(ProjectFolderItem* _it, const KUrl& newUrl)
     {
         return KDevelop::renameUrl(_it->project(), _it->url(), newUrl);
     }
+    
     CMakeFolderItem* it=static_cast<CMakeFolderItem*>(_it);
     KUrl lists=it->formerParent()->url();
     lists.addPath("CMakeLists.txt");
@@ -1374,15 +1377,13 @@ bool CMakeManager::renameFolder(ProjectFolderItem* _it, const KUrl& newUrl)
     KTextEditor::Range r=cmit->descriptor().argRange().textRange();
     kDebug(9042) << "For " << lists << " rename " << r;
     
-    e.document()->replaceText(r, newName);
+    bool ret = e.document()->replaceText(r, newName);
     
-
-    bool ret=e.exec();
+    ret= ret && e.exec() == QDialog::Accepted;
     if(ret)
     {
-        ret=e.applyAllChanges();
-        if(ret)
-            ret=KDevelop::renameUrl(it->project(), it->url(), newUrl);
+        ret = e.applyAllChanges();
+        ret = ret && KDevelop::renameUrl(it->project(), it->url(), newUrl);
     }
     return ret;
 }
