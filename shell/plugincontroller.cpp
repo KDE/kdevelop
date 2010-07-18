3f4d8392cb7317e4953ee77c764feed883f9d9d1
Milian Wolff <mail@milianw.de>
2010-07-18 15:52:30 +0200

    fix conditional: plugin is only disabled when it is user selectable
    TODO: why is isGlobalPlugin checked here?

--- shell/plugincontroller.cpp
+++ shell/plugincontroller.cpp
@@ -214,7 +214,7 @@ bool PluginController::isEnabled( const KPluginInfo& info )
     KConfigGroup grp = Core::self()->activeSession()->config()->group( pluginControllerGrp );
     bool isEnabled = grp.readEntry( info.pluginName()+"Enabled", ShellExtension::getInstance()->defaultPlugins().isEmpty() || ShellExtension::getInstance()->defaultPlugins().contains( info.pluginName() ) );
     //kDebug() << "read config:" << isEnabled << "is global plugin:" << isGlobalPlugin( info ) << "default:" << ShellExtension::getInstance()->defaultPlugins().isEmpty()  << ShellExtension::getInstance()->defaultPlugins().contains( info.pluginName() );
-    return !isGlobalPlugin( info ) || isUserSelectable( info ) || isEnabled;
+    return !isGlobalPlugin( info ) || !isUserSelectable( info ) || isEnabled;
 }
 
 void PluginController::initialize()
