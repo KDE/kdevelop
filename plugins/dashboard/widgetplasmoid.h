e0252cd3a4d1e648de0d44e178b69971ea14559a
Aleix Pol <aleixpol@kde.org>
2010-07-28 19:44:23 +0200

    Add a new class to identify the plasmoids created from a widget.

---
+++ plugins/dashboard/widgetplasmoid.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of KDevelop
+ * Copyright 2010 Aleix Pol Gonzalez <aleixpol@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef WIDGETPLASMOID_H
+#define WIDGETPLASMOID_H
+
+#include <plasma/applet.h>
+
+namespace KDevelop {
+    class IDashboardWidgetFactory;
+    class IProject;
+}
+
+class WidgetPlasmoid : public Plasma::Applet
+{
+    public:
+        WidgetPlasmoid(KDevelop::IDashboardWidgetFactory* fact, QGraphicsItem* parent = 0, const QString& serviceId = QString(), uint appletId = 0);
+        
+        virtual void init();
+        KDevelop::IProject* project() const;
+        
+    private:
+        KDevelop::IDashboardWidgetFactory* m_fact;
+};
+
+#endif // WIDGETPLASMOID_H
