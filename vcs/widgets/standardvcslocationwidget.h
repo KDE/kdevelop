e87f6a57d55eea113414bd4b9ba609d6954df2af
Aleix Pol <aleixpol@kde.org>
2010-08-23 21:32:43 +0200

    Add APIDOCS for the whole widget. Add a projectName method to provide the VcsLocationWidget's project name. Adapt the standard implementation to the last changes.

--- vcs/widgets/standardvcslocationwidget.h
+++ vcs/widgets/standardvcslocationwidget.h
@@ -37,6 +37,7 @@ class KDEVPLATFORMVCS_EXPORT StandardVcsLocationWidget : public VcsLocationWidge
         virtual VcsLocation location() const;
         virtual bool isCorrect() const;
         KUrl url() const;
+        virtual QString projectName() const;
         
     public slots:
         void textChanged(const QString& str);
