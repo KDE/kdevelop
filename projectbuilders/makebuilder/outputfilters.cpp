62f8ff16092e884e49468440bbb37492485799df
Aleix Pol <aleixpol@kde.org>
2010-08-24 19:16:22 +0200

    Although it's not being used, shortened text is 4, 3 is the column number.

--- projectbuilders/makebuilder/outputfilters.cpp
+++ projectbuilders/makebuilder/outputfilters.cpp
@@ -16,7 +16,7 @@
 // @todo could get these from emacs compile.el
 QList<ErrorFormat> ErrorFormat::errorFormats = QList<ErrorFormat>()
     // GCC - another case, eg. for #include "pixmap.xpm" which does not exists
-    << ErrorFormat( "([^: \t]+):([0-9]+):(?:[0-9]+):([^0-9]+)", 1, 2, 3 )
+    << ErrorFormat( "([^: \t]+):([0-9]+):(?:[0-9]+):([^0-9]+)", 1, 2, 4 )
     // GCC
     << ErrorFormat( "([^: \t]+):([0-9]+):([^0-9]+)", 1, 2, 3 )
     // ICC
