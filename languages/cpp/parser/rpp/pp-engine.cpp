KDE/kdevelop

commit b4972c595228167f37fc1fdd18f760d1fb76f8e7

Potentially prevent hitting the end of the input-stream. CCBUG: 250383

languages/cpp/parser/rpp/pp-engine.cpp
@@ -372,6 +372,7 @@ void pp::handle_define (Stream& input)
         macro->definitionList().append(KDevelop::IndexedString::fromIndex(input.current()));
         ++input;
       } while (!input.atEnd() && input != '"' && input != '\n');
+      continue;
     }
 
     macro->definitionList().append(KDevelop::IndexedString::fromIndex(input.current()));

commit 25dce5b8b2e503d1807308de0378a51e4f30674d

Fix a regression caused by commit b4972c595228167f37fc1fdd18f760d1fb76f8e7, which led to missing highlighting in some files.

languages/cpp/parser/rpp/pp-engine.cpp
@@ -372,7 +372,13 @@ void pp::handle_define (Stream& input)
         macro->definitionList().append(KDevelop::IndexedString::fromIndex(input.current()));
         ++input;
       } while (!input.atEnd() && input != '"' && input != '\n');
-      continue;
+      
+      if(!input.atEnd())
+      {
+        macro->definitionList().append(KDevelop::IndexedString::fromIndex(input.current()));
+        ++input;
+        continue;
+      }
     }
 
     macro->definitionList().append(KDevelop::IndexedString::fromIndex(input.current()));