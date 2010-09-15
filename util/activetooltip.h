f33c77310fb4a5fd394af20b13519a675e2c0e77
David nolden <david.nolden.kde@art-master.de>
2010-09-15 12:25:09 +0200

    Add copyright notice

--- util/activetooltip.h
+++ util/activetooltip.h
@@ -1,5 +1,6 @@
 /* This file is part of the KDE project
    Copyright 2007 Vladimir Prus
+   Copyright 2009-2010 David Nolden
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
