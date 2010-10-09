384d9ea9291777137a4a43429c677a1453af2ead
Milian Wolff <mail@milianw.de>
2010-10-09 16:10:19 +0200

    cleanup code

--- plugins/grepview/grepfindthread.cpp
+++ plugins/grepview/grepfindthread.cpp
@@ -16,17 +16,15 @@ using KDevelop::IndexedString;
 // the abort parameter must be volatile so that it
 // is evaluated every time - optimization might prevent that
 
-static KUrl::List thread_getProjectFiles(const KUrl dir, bool recursive, const QString inclString, const QString exclString, volatile bool &abort)
+static KUrl::List thread_getProjectFiles(const KUrl dir, bool recursive, const QStringList include,
+                                         const QStringList exlude, volatile bool &abort)
 {
     KDevelop::IProject *project = KDevelop::ICore::self()->projectController()->findProjectForUrl( dir );
     KUrl::List res;
     if(!project)
         return res;
-    QStringList exclList;
-    foreach(const QString &sub, exclString.split(',', QString::SkipEmptyParts))
-        exclList << QString("*%1*").arg(sub);
-    QSet<IndexedString> fileSet = project->fileSet();
-    QStringList inclList = inclString.split(',');
+
+    const QSet<IndexedString> fileSet = project->fileSet();
     foreach( const IndexedString &item, fileSet )
     {
         if(abort)
@@ -36,21 +34,22 @@ static KUrl::List thread_getProjectFiles(const KUrl dir, bool recursive, const Q
             continue;
         if( !recursive && !url.upUrl().equals(dir, KUrl::CompareWithoutTrailingSlash))
             continue;
-        if( QDir::match(inclList, url.fileName()) && !QDir::match(exclList, url.toLocalFile()) )
+        if( QDir::match(include, url.fileName()) && !QDir::match(exlude, url.toLocalFile()) )
             res << url;
     }
-    qSort(res);
+
     return res;
 }
 
-static KUrl::List thread_findFilesInternal(const QDir& dir, bool recursive, const QStringList& patList, const QStringList& exclList, volatile bool &abort)
+static KUrl::List thread_findFiles(const QDir& dir, bool recursive, const QStringList& include,
+                                           const QStringList& exclude, volatile bool &abort)
 {
-    QFileInfoList infos = dir.entryInfoList(patList, QDir::NoDotAndDotDot|QDir::Files|QDir::Readable);
+    QFileInfoList infos = dir.entryInfoList(include, QDir::NoDotAndDotDot|QDir::Files|QDir::Readable);
     KUrl::List dirFiles;
     foreach(const QFileInfo &currFile, infos)
     {
         QString currName = currFile.canonicalFilePath();
-        if(!QDir::match(exclList, currName))
+        if(!QDir::match(exclude, currName))
             dirFiles << currName;
     }
     if(recursive)
@@ -63,24 +62,10 @@ static KUrl::List thread_findFilesInternal(const QDir& dir, bool recursive, cons
             QString canonical = currDir.canonicalFilePath();
             if(!KUrl(dir.canonicalPath()).isParentOf(canonical))
                 continue;
-            dirFiles << thread_findFilesInternal(canonical, true, patList, exclList, abort);
+            dirFiles << thread_findFiles(canonical, true, include, exclude, abort);
         }
     }
     return dirFiles;
-}
-
-static KUrl::List thread_findFiles(const QDir dir, bool recursive, const QString inclString, const QString exclString, volatile bool &abort)
-{
-    QStringList exclList;
-    foreach(const QString &sub, exclString.split(',', QString::SkipEmptyParts))
-        exclList << QString("*%1*").arg(sub);
-    KUrl::List list = thread_findFilesInternal(
-        dir, recursive,
-        inclString.split(',', QString::SkipEmptyParts),
-        exclList,
-        abort);
-    qSort(list);
-    return list;
 }
 
 GrepFindFilesThread::GrepFindFilesThread(QObject* parent, const KUrl& startDir, bool recursive, const QString& pats, const QString& excl, bool onlyProject)
@@ -101,10 +86,17 @@ bool GrepFindFilesThread::triesToAbort() const
 
 void GrepFindFilesThread::run()
 {
+    QStringList include = m_patString.split(',', QString::SkipEmptyParts);
+    QStringList exclude;
+    foreach(const QString &sub, m_exclString.split(',', QString::SkipEmptyParts))
+        exclude << QString("*%1*").arg(sub);
+
     if(m_project)
-        m_files = thread_getProjectFiles(m_directory, m_recursive, m_patString, m_exclString, m_tryAbort);
+        m_files = thread_getProjectFiles(m_directory, m_recursive, include, exclude, m_tryAbort);
     else
-        m_files = thread_findFiles(m_directory.toLocalFile(), m_recursive, m_patString, m_exclString, m_tryAbort);
+        m_files = thread_findFiles(m_directory.toLocalFile(), m_recursive, include, exclude, m_tryAbort);
+
+    qSort(m_files);
 }
 
 KUrl::List GrepFindFilesThread::files() const {
