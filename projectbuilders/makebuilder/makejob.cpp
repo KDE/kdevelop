4dfe70ef71b668aa496f1710a4e58c46ec97f091
Milian Wolff <mail@milianw.de>
2010-11-22 14:37:19 +0100

    wait for finished after killing process, prevents Qt warnings about process still running while it gets deleted

--- projectbuilders/makebuilder/makejob.cpp
+++ projectbuilders/makebuilder/makejob.cpp
@@ -68,6 +68,11 @@ MakeJob::MakeJob(MakeBuilder* builder, KDevelop::ProjectBaseItem* item, CommandT
     setObjectName(title);
 }
 
+MakeJob::~MakeJob()
+{
+    Q_ASSERT(!m_process || m_process->state() == KProcess::NotRunning);
+}
+
 void MakeJob::start()
 {
     kDebug(9037) << "Building with make" << m_command << m_overrideTarget;
@@ -307,6 +312,7 @@ bool MakeJob::doKill()
     model()->addLine( i18n("*** Aborted ***") );
     m_killed = true;
     m_process->kill();
+    m_process->waitForFinished();
     return true;
 }
 
