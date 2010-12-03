c5d10162207daed72e503e2f9ce5cefe08a5ab4d
Pino Toscano <pino@kde.org>
2010-12-03 21:51:01 +0100

    i18n: do not extract messages from sources and ui files in the qmakebuilder and tests subdirectories

--- Messages.sh
+++ Messages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-files=`find . -name \*.rc -o -name \*.ui`
+files=`find . -name \*.rc -o -name \*.ui | grep -v '/qmakebuilder/' | grep -v '/tests/'`
 if [ "x$files" != "x" ]; then
        $EXTRACTRC $files >> rc.cpp
 fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevqmake.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/qmakebuilder/' | grep -v '/tests/'` -o $podir/kdevqmake.pot
 rm -f rc.cpp
