8f6053cee6bcf3d9f85a1e0781dfe5e711ec6906
Pino Toscano <pino@kde.org>
2010-12-11 15:14:58 +0100

    i18n: extract also from *.kcfg files

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp` -o $podir/kdevcustombuildsystem.pot
 rm -f rc.cpp
