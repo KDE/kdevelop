c53cd44796c12304ccf71693e414c6a1c99be54c
Milian Wolff <mail@milianw.de>
2010-12-12 19:58:31 +0100

    add testcase for deadlock in ForegroundLock with QMutex
    comment out #define USE_PTHREAD_MUTEX in foregroundlock.cpp , then run the testcase. Locks up reliably for me.
    With the alternative mutex it seems to work fine though.
    
    CCBUG: 252659

---
+++ interfaces/tests/test_foregroundlock.cpp
@@ -0,0 +1,72 @@
+/* This file is part of KDevelop
+    Copyright 2010 Milian Wolff <mail@milianw.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "test_foregroundlock.h"
+
+#include <QtTest/QtTest>
+
+#include <QDebug>
+#include <QThread>
+
+#include <unistd.h>
+
+#include "../foregroundlock.h"
+
+QTEST_MAIN(KDevelop::TestForegroundLock)
+
+using namespace KDevelop;
+
+//BEGIN Helper Threads
+
+class TryLockThread : public QThread {
+    Q_OBJECT
+public:
+    virtual void run() {
+        ForegroundLock lock(false);
+        for(int i = 0; i < 1000; ++i) {
+            if (lock.tryLock()) {
+                lock.unlock();
+            }
+            usleep(qrand() % 20);
+        }
+    }
+};
+
+void TestForegroundLock::testTryLock()
+{
+    TryLockThread t1;
+    TryLockThread t2;
+    TryLockThread t3;
+
+    ForegroundLock lock(true);
+
+    t1.start();
+    t2.start();
+    t3.start();
+
+    lock.unlock();
+
+    while(t1.isRunning() || t2.isRunning() || t3.isRunning()) {
+        lock.relock();
+        usleep(10);
+        lock.unlock();
+    }
+}
+
+#include "moc_test_foregroundlock.cpp"
+#include "test_foregroundlock.moc"
---
+++ interfaces/tests/test_foregroundlock.h
@@ -0,0 +1,34 @@
+/* This file is part of KDevelop
+    Copyright 2010 Milian Wolff <mail@milianw.de>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef TEST_FOREGROUNDLOCK_H
+#define TEST_FOREGROUNDLOCK_H
+
+#include <QObject>
+
+namespace KDevelop {
+class TestForegroundLock : public QObject {
+    Q_OBJECT
+
+private slots:
+    void testTryLock();
+};
+}
+
+
+#endif // TEST_FOREGROUNDLOCK_H
