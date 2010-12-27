cd91f1d35afb518ab35f37d63ba43f7a5380177f
Aleix Pol <aleixpol@kde.org>
2010-12-27 02:14:45 +0100

    Provide some values in the dummy implementations for the TestProject.

--- tests/testproject.cpp
+++ tests/testproject.cpp
@@ -11,4 +11,16 @@ void KDevelop::TestProject::set_projectFileUrl(const KUrl& url) {
     m_projectFileUrl = url;
 }
 
+const KUrl KDevelop::TestProject::folder() const
+{
+    KUrl ret = m_projectFileUrl.upUrl();
+    ret.adjustPath(KUrl::AddTrailingSlash);
+    return ret;
+}
+
+bool KDevelop::TestProject::inProject(const KUrl& url) const
+{
+    return folder().isParentOf(url);
+}
+
 #include "testproject.moc"
