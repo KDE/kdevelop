d26b4f0bd6cdcf907ebf7e4c13984e6a38629fed
Aleix Pol <aleixpol@kde.org>
2010-12-27 03:14:51 +0100

    Fix the CMakeManagerTest so that it makes something meaningful at least.

--- projectmanagers/cmake/tests/cmakemanagertest.cpp
+++ projectmanagers/cmake/tests/cmakemanagertest.cpp
@@ -30,37 +30,36 @@
 
 QTEST_KDEMAIN(CMakeManagerTest, GUI )
 
+#define WAIT_FOR_OPEN_SIGNAL \
+{\
+    bool gotSignal = QTest::kWaitForSignal(ICore::self()->projectController(), SIGNAL(projectOpened(KDevelop::IProject*)), 30000);\
+    QVERIFY2(gotSignal, "Timeout while waiting for opened signal");\
+} void(0)
 
 using namespace KDevelop;
 
-CMakeManagerTest::CMakeManagerTest(QObject* parent): QObject(parent) {
+CMakeManagerTest::CMakeManagerTest(QObject* parent): QObject(parent)
+{
     AutoTestShell::init();
     KDevelop::Core::initialize(0, KDevelop::Core::Default);
 }
 
-CMakeManagerTest::~CMakeManagerTest() {
+CMakeManagerTest::~CMakeManagerTest()
+{}
 
-}
-
-void CMakeManagerTest::testWithBuildDirProject() {
+void CMakeManagerTest::testWithBuildDirProject()
+{
     // Import project
-    TestProject project;
-    KSharedConfig::Ptr config = project.projectConfiguration();
-    KConfigGroup group(config.data()->group("CMake"));
-    group.writeEntry("ProjectRootRelative", "./");
-    group.writeEntry("CurrentBuildDir", CMAKE_TESTS_PROJECTS_DIR  "/with_build_dir/builddir");
-    project.set_projectFileUrl(KUrl(CMAKE_TESTS_PROJECTS_DIR "/with_build_dir/with_build_dir.kdev4"));
-    IPlugin* i = Core::self()->pluginController()->loadPlugin( "KDevCMakeManager" );
-    IProjectFileManager* manager = i->extension<IProjectFileManager>();
-    manager->import(&project);
-    ICMakeManager* cmakemanager = i->extension<ICMakeManager>();
-
-    // Check that cache has been loaded by checking 2 arbitrary vraibles
-    QString binary_dir = cmakemanager->cacheValue(&project, "with_build_dir_BINARY_DIR").first;
-    QString source_dir = cmakemanager->cacheValue(&project, "with_build_dir_SOURCE_DIR").first;
-    QString expected_binary_dir("/home/xxx/kdesvn/src/KDE/kdevelop/projectmanagers/cmake/tests/manual/with_build_dir/builddir");
-    QString expected_source_Dir("/home/xxx/kdesvn/src/KDE/kdevelop/projectmanagers/cmake/tests/manual/with_build_dir");
-    QCOMPARE(source_dir, expected_source_Dir);
-    QCOMPARE(binary_dir, expected_binary_dir);
+    QList< ProjectFolderItem* > items;
+    
+    KUrl url(CMAKE_TESTS_PROJECTS_DIR "/with_build_dir/with_build_dir.kdev4");
+    ICore::self()->projectController()->openProject(url);
+    
+    KUrl expected_source_Dir(CMAKE_TESTS_PROJECTS_DIR "/with_build_dir/");
+    WAIT_FOR_OPEN_SIGNAL;
+    
+    IProject* project = ICore::self()->projectController()->findProjectByName("with_build_dir");
+    QCOMPARE(url, project->projectFileUrl());
+    QCOMPARE(expected_source_Dir, project->folder());
 }
 
