e0a459491f9c95b44f420f7970e480e65b906515
Benjamin Port <port.benjamin@gmail.com>
2011-01-16 19:58:09 +0100

    Remove coding style change

--- shell/documentationcontroller.cpp
+++ shell/documentationcontroller.cpp
@@ -1,7 +1,7 @@
 /*
    Copyright 2009 Aleix Pol Gonzalez <aleixpol@kde.org>
    Copyright 2010 Benjamin Port <port.benjamin@gmail.com>
-
+   
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public
    License version 2 as published by the Free Software Foundation.
@@ -26,6 +26,7 @@
 #include <interfaces/iuicontroller.h>
 #include <shell/core.h>
 #include <sublime/view.h>
+
 #include <ktexteditor/document.h>
 #include <ktexteditor/view.h>
 #include <KDebug>
@@ -48,15 +49,15 @@ class DocumentationViewFactory: public KDevelop::IToolViewFactory
     public:
         DocumentationViewFactory()
         {}
-
+        
         virtual QWidget* create( QWidget *parent = 0 )
         {
             return new DocumentationView( parent );
         }
-
+        
         virtual Qt::DockWidgetArea defaultPosition() { return Qt::RightDockWidgetArea; }
         virtual QString id() const { return "org.kdevelop.DocumentationView"; }
-
+        
     private:
 };
 
@@ -83,17 +84,17 @@ void KDevelop::DocumentationController::doShowDocumentation()
     IDocument* doc = ICore::self()->documentController()->activeDocument();
     if(!doc)
       return;
-  
+    
     KTextEditor::Document* textDoc = doc->textDocument();
     if(!textDoc)
       return;
-  
+    
     KTextEditor::View* view = textDoc->activeView();
     if(!view)
       return;
-  
+    
     KDevelop::DUChainReadLocker lock( DUChain::lock() );
-  
+    
     Declaration *dec = DUChainUtils::declarationForDefinition( DUChainUtils::itemUnderCursor( doc->url(), SimpleCursor(view->cursorPosition()) ) );
     
     KSharedPtr< IDocumentation > documentation = documentationForDeclaration(dec);
@@ -108,38 +109,38 @@ Q_DECLARE_METATYPE(KSharedPtr<KDevelop::IDocumentation>)
 KDevelop::ContextMenuExtension KDevelop::DocumentationController::contextMenuExtension ( Context* context )
 {
     ContextMenuExtension menuExt;
-
+    
     qRegisterMetaType<KSharedPtr<KDevelop::IDocumentation> >("KSharedPtr<KDevelop::IDocumentation>");
-
+    
     DeclarationContext* ctx = dynamic_cast<DeclarationContext*>(context);
     if(ctx) {
         DUChainReadLocker lock(DUChain::lock());
         if(!ctx->declaration().data())
             return menuExt;
-
+        
         KSharedPtr< IDocumentation > doc = documentationForDeclaration(ctx->declaration().data());
         if(doc) {
             menuExt.addAction(ContextMenuExtension::ExtensionGroup, m_showDocumentation);;
         }
     }
-
+    
     return menuExt;
 }
 
 KSharedPtr< KDevelop::IDocumentation > DocumentationController::documentationForDeclaration(Declaration* decl)
 {
     KSharedPtr<KDevelop::IDocumentation> ret;
-
+    
     foreach(IDocumentationProvider* doc, documentationProviders())
     {
         kDebug(9529) << "Documentation provider found:" << doc;
         ret=doc->documentationForDeclaration(decl);
-
+        
         kDebug(9529) << "Documentation proposed: " << ret;
         if(ret)
             break;
     }
-
+    
     return ret;
 }
 
@@ -148,7 +149,7 @@ QList< IDocumentationProvider* > DocumentationController::documentationProviders
 {
     QList<IPlugin*> plugins=ICore::self()->pluginController()->allPluginsForExtension("org.kdevelop.IDocumentationProvider");
     QList<IPlugin*> pluginsProvider=ICore::self()->pluginController()->allPluginsForExtension("org.kdevelop.IDocumentationProviderProvider");
-
+    
     QList<IDocumentationProvider*> ret;
     foreach(IPlugin* p, pluginsProvider)
     {
@@ -159,7 +160,7 @@ QList< IDocumentationProvider* > DocumentationController::documentationProviders
         }
         ret.append(docProvider->providers());
     }
-
+    
     foreach(IPlugin* p, plugins)
     {
         IDocumentationProvider *doc=p->extension<IDocumentationProvider>();
@@ -169,7 +170,7 @@ QList< IDocumentationProvider* > DocumentationController::documentationProviders
         }
         ret.append(doc);
     }
-
+    
     return ret;
 }
 
@@ -180,7 +181,7 @@ void KDevelop::DocumentationController::showDocumentation(KSharedPtr< KDevelop::
         kWarning() << "Could not add documentation toolview";
         return;
     }
-
+    
     DocumentationView* view = dynamic_cast<DocumentationView*>(w);
     if( !view ) {
         kWarning() << "Could not cast toolview" << w << "to DocumentationView class!";
