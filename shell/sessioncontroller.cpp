KDE/kdevelop

commit cc015af70355097e92075dfadc661f40fdedccd6

- Add the basis of a shell environment whic allows controlling a connected kdevelop instance through very easy and comfortable shell commands. When the environment is active, you can:   * open/create arbitrary files from the shell right into kdevelop   * raise the window   * start a grepview search - Allow putting a semicolon-separated list of items in the "Directory" line of the grep-dialog. - Rename the label to "Location(s)", as it is not strictly a directory anymore, already for some time. - Expose via dbus:  * Name of the current session  * An interface to start a search in the grepview plugin  * An interface to open documents

shell/sessioncontroller.cpp
@@ -66,6 +66,7 @@ Boston, MA 02110-1301, USA.
 #if KDE_IS_VERSION(4,5,60)
     #define HAVE_RECOVERY_INTERFACE
     #include <ktexteditor/recoveryinterface.h>
+#include <QDBusConnection>
 #endif
 
 const int recoveryStorageInterval = 10; ///@todo Make this configurable
@@ -594,6 +595,9 @@ SessionController::SessionController( QObject *parent )
     
     setXMLFile("kdevsessionui.rc");
 
+    QDBusConnection::sessionBus().registerObject( "/kdevelop/SessionController",
+        this, QDBusConnection::ExportScriptableSlots );
+
     if (Core::self()->setupFlags() & Core::NoUi) return;
 
     KAction* action = actionCollection()->addAction( "new_session", this, SLOT( newSession() ) );
@@ -990,6 +994,14 @@ QString SessionController::showSessionChooserDialog(QString headerText, bool onl
     
     return QString();
 }
+
+QString SessionController::sessionName()
+{
+    if(!activeSession())
+        return QString();
+    return activeSession()->description();
+}
+
 
 }
 #include "sessioncontroller.moc"

plugins/grepview/grepdialog.cpp
@@ -347,7 +347,16 @@ QList< KUrl > GrepDialog::getDirectoryChoice() const
         foreach(IProject* project, ICore::self()->projectController()->projects())
             ret << project->folder();
     }else{
-        ret << directoryRequester->url();
+        QStringList semicolonSeparatedFileList = text.split(";");
+        if(!semicolonSeparatedFileList.isEmpty() && QFileInfo(semicolonSeparatedFileList[0]).exists())
+        {
+            // We use QFileInfo to make sure this is really a semicolon-separated file list, not a file containing
+            // a semicolon in the name.
+            foreach(QString file, semicolonSeparatedFileList)
+                ret << KUrl::fromPath(file);
+        }else{
+            ret << directoryRequester->url();
+        }
     }
     return ret;
 }
@@ -358,6 +367,11 @@ bool GrepDialog::isPartOfChoice(KUrl url) const
         if(choice.isParentOf(url) || choice.equals(url))
             return true;
     return false;
+}
+
+void GrepDialog::start()
+{
+    performAction(SearchButton);
 }
 
 void GrepDialog::performAction(KDialog::ButtonCode button)
@@ -391,11 +405,15 @@ void GrepDialog::performAction(KDialog::ButtonCode button)
     GrepJob* job = m_plugin->newGrepJob();
     
     QString descriptionOrUrl(directoryRequester->lineEdit()->text());
+    QString description = descriptionOrUrl;
+    // Shorten the description
+    if(descriptionOrUrl != allOpenFilesString && descriptionOrUrl != allOpenProjectsString && choice.size() > 1)
+        description = i18n("%1, and %2 more items", choice[0].pathOrUrl(), choice.size()-1);
     
     GrepOutputViewFactory *m_factory = new GrepOutputViewFactory();
     GrepOutputView *toolView = (GrepOutputView*)ICore::self()->uiController()->
                                findToolView(i18n("Find/Replace in Files"), m_factory, IUiController::CreateAndRaise);
-    GrepOutputModel* outputModel = toolView->renewModel(patternString(), descriptionOrUrl);
+    GrepOutputModel* outputModel = toolView->renewModel(patternString(), description);
     toolView->setPlugin(m_plugin);
     
     connect(job, SIGNAL(showErrorMessage(QString, int)),

plugins/grepview/grepdialog.h
@@ -48,6 +48,8 @@ public:
     bool recursiveFlag() const;
     bool caseSensitiveFlag() const;
 
+    void start();
+    
 private Q_SLOTS:
     void performAction(KDialog::ButtonCode button);
     void templateTypeComboActivated(int);

plugins/grepview/grepviewplugin.cpp
@@ -41,6 +41,7 @@
 #include <project/projectmodel.h>
 #include <language/interfaces/editorcontext.h>
 #include <outputview/ioutputview.h>
+#include <QDBusConnection>
 
 K_PLUGIN_FACTORY(GrepViewFactory, registerPlugin<GrepViewPlugin>(); )
 K_EXPORT_PLUGIN(GrepViewFactory(KAboutData("kdevgrepview","kdevgrepview", ki18n("Find/Replace In Files"), "0.1", ki18n("Support for running grep over a list of files"), KAboutData::License_GPL)))
@@ -50,6 +51,8 @@ GrepViewPlugin::GrepViewPlugin( QObject *parent, const QVariantList & )
 {
     setXMLFile("kdevgrepview.rc");
 
+    QDBusConnection::sessionBus().registerObject( "/org/kdevelop/GrepViewPlugin",
+        this, QDBusConnection::ExportScriptableSlots );
 
     KAction *action = actionCollection()->addAction("edit_grep");
     action->setText(i18n("Find/replace in Fi&les..."));
@@ -70,6 +73,12 @@ GrepViewPlugin::GrepViewPlugin( QObject *parent, const QVariantList & )
 
 GrepViewPlugin::~GrepViewPlugin()
 {
+}
+
+void GrepViewPlugin::startSearch(QString pattern, QString directory, bool showOptions)
+{
+    m_directory = directory;
+    showDialog(false, pattern, showOptions);
 }
 
 KDevelop::ContextMenuExtension GrepViewPlugin::contextMenuExtension(KDevelop::Context* context)
@@ -111,12 +120,16 @@ KDevelop::ContextMenuExtension GrepViewPlugin::contextMenuExtension(KDevelop::Co
     return extension;
 }
 
-void GrepViewPlugin::showDialog(bool setLastUsed)
+void GrepViewPlugin::showDialog(bool setLastUsed, QString pattern, bool showOptions)
 {
     GrepDialog* dlg = new GrepDialog( this, core()->uiController()->activeMainWindow(), setLastUsed );
     KDevelop::IDocument* doc = core()->documentController()->activeDocument();
     
-    if(!setLastUsed)
+    if(!pattern.isEmpty())
+    {
+        dlg->setPattern(pattern);
+    }
+    else if(!setLastUsed)
     {
         QString pattern;
         if( doc )
@@ -173,7 +186,12 @@ void GrepViewPlugin::showDialog(bool setLastUsed)
         }
     }
 
-    dlg->show();
+    if(showOptions)
+        dlg->show();
+    else{
+        dlg->start();
+        dlg->deleteLater();
+    }
 }
 
 void GrepViewPlugin::showDialogFromMenu()

plugins/grepview/grepviewplugin.h
@@ -31,7 +31,7 @@ public:
 
     void rememberSearchDirectory(QString const & directory);
     virtual KDevelop::ContextMenuExtension contextMenuExtension(KDevelop::Context* context);
-    void showDialog(bool setLastUsed = false);
+    void showDialog(bool setLastUsed = false, QString pattern = QString(), bool showOptions = true);
     
     /**
      * Returns a new instance of GrepJob. Since the plugin supports only one job at the same time,
@@ -39,7 +39,12 @@ public:
      */
     GrepJob *newGrepJob();
     GrepJob *grepJob();
-
+public Q_SLOTS:
+    ///@param pattern the pattern to search
+    ///@param directory the directory, or a semicolon-separated list of files
+    ///@param showDIalog whether the search dialog should be shown. if false,
+    ///                  the parameters of the last search will be used.
+    Q_SCRIPTABLE void startSearch(QString pattern, QString directory, bool showOptions);
 private Q_SLOTS:
     void showDialogFromMenu();
     void showDialogFromProject();

plugins/grepview/grepwidget.ui
@@ -178,7 +178,7 @@ p, li { white-space: pre-wrap; }
    <item row="4" column="0">
     <widget class="QLabel" name="label_5">
      <property name="text">
-      <string>Directory:</string>
+      <string>Location(s):</string>
      </property>
      <property name="alignment">
       <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
@@ -197,7 +197,7 @@ p, li { white-space: pre-wrap; }
       </sizepolicy>
      </property>
      <property name="toolTip">
-      <string>Select in which directory you want to search.</string>
+      <string>Select the location where you want to search. It can be a directory, a file, or a semicolon separated list of directories/files.</string>
      </property>
     </widget>
    </item>

shell/documentcontroller.cpp
@@ -550,6 +550,16 @@ IDocument* DocumentController::openDocumentFromText( const QString& data )
     return d;
 }
 
+bool DocumentController::openDocumentFromTextSimple( QString text )
+{
+    return (bool)openDocumentFromText( text );
+}
+
+bool DocumentController::openDocumentSimple( QString url )
+{
+    return (bool)openDocument( KUrl(url) );
+}
+
 IDocument* DocumentController::openDocument( const KUrl& inputUrl, const QString& prefName )
 {
     return d->openDocumentInternal( inputUrl, prefName );

shell/documentcontroller.h
@@ -135,6 +135,10 @@ public Q_SLOTS:
     void closeAllOtherDocuments();
     void reloadAllDocuments();
 
+    // DBUS-compatible versions of openDocument
+    virtual Q_SCRIPTABLE bool openDocumentSimple( QString url );
+    virtual Q_SCRIPTABLE bool openDocumentFromTextSimple( QString text );
+    
 private Q_SLOTS:
     virtual void slotOpenDocument(const KUrl &url);
 

commit 49a9a8cf88954e64698e96553e0f4b5f4f9b7290

Fix build under kdelibs 4.5.
Has been broken since cc015af703. I suspect this #include was auto-added
in the wrong place by KDevelop, when using the QDBusConnection class.

shell/sessioncontroller.cpp
@@ -59,6 +59,7 @@ Boston, MA 02110-1301, USA.
 #include <ktexteditor/document.h>
 #include <sublime/area.h>
 #include <QLabel>
+#include <QDBusConnection>
 
 
 #include <kdeversion.h>
@@ -66,7 +67,6 @@ Boston, MA 02110-1301, USA.
 #if KDE_IS_VERSION(4,5,60)
     #define HAVE_RECOVERY_INTERFACE
     #include <ktexteditor/recoveryinterface.h>
-#include <QDBusConnection>
 #endif
 
 const int recoveryStorageInterval = 10; ///@todo Make this configurable