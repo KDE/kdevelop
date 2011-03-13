1f1d1b4fa1f19acc9610c0165c8dbe58598e6a7d
David nolden <david.nolden.kde@art-master.de>
2011-03-13 16:41:10 +0100

    Extend the shell integration so that it is possible to ssh to another host, and still keep connected to the KDevelop instance. This requires forwarding of the DBUS session. This is done using the added dbus_socket_transformer utility, which transforms the dbus abstract unix domain socket into a standard TCP socket on the client side, then we can forward it using standard ssh port forwarding, and on the target side we transform the connection back into an abstract unix domain socket, which can then be used by the shell environment to contact the KDevelop instance on the client machine. Problem: Currently this seems to only work when client and host machine have the same user-management, due to DBUS authentication. Also, kdevelop must be installed on both sides in the same path.

---
+++ util/dbus_socket_transformer/main.cpp
@@ -0,0 +1,336 @@
+/***************************************************************************
+ *   Copyright 2011 David Nolden <david.nolden.kdevelop@art-master.de>                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include <stdlib.h>
+#include <QString>
+#include <QLocalSocket>
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <fcntl.h>
+#include <assert.h>
+
+/**
+ * The goal of this utility is transforming the abstract unix-socket which is used by dbus
+ * into a TCP socket which can be forwarded to a target machine by ssh tunneling, and then on
+ * the target machine back into an abstract unix socket.
+ * 
+ * This tool basically works similar to the "socat" utility, except that it works properly
+ * for this special case. It is merely responsible for the transformation between abstract unix
+ * sockets and tcp sockets.
+ * */
+
+const bool debug = false;
+
+/**
+ * Shuffles all data between the two file-descriptors until one of them fails or reaches EOF.
+ * */
+void shuffleBetweenStreams(int side1, int side2)
+{
+    char buffer[500];
+    char buffer2[500];
+    
+    // Set non-blocking mode
+    int opts = fcntl(side1,F_GETFL);
+    opts |= O_NONBLOCK;
+    fcntl(side1, F_SETFL, opts);
+
+    opts = fcntl(side2,F_GETFL);
+    opts |= O_NONBLOCK;    
+    fcntl(side2, F_SETFL, opts);
+    
+    while(true)
+    {
+        int r1 = read(side1, buffer, 500);
+        int r2 = read(side2, buffer2, 500);
+
+        if(r1 < -1 || r1 == 0)
+        {
+            if(debug)
+                std::cerr << "stream 1 failed: " << r1 << std::endl;
+            return;
+        }
+        if(r2 < -1 || r2 == 0)
+        {
+            if(debug)
+                std::cerr << "stream 2 failed: " << r2 << std::endl;
+            return;
+        }
+        
+        if(r1 > 0)
+        {
+            if(debug)
+                std::cerr << "transferring " << r1 << " from 1 to 2" << std::endl;
+
+            opts = fcntl(side2,F_GETFL);
+            opts ^= O_NONBLOCK;    
+            fcntl(side2, F_SETFL, opts);
+            
+            int w2 = send(side2, buffer, r1, MSG_NOSIGNAL);
+            
+            if(w2 < 0)
+            {
+                if(debug)
+                    std::cerr << "writing to side 2 failed, ending: " << w2 << std::endl;
+                return;
+            }
+            assert(w2 == r1);
+            
+            opts = fcntl(side2,F_GETFL);
+            opts |= O_NONBLOCK;    
+            fcntl(side2, F_SETFL, opts);
+        }
+
+        if(r2 > 0)
+        {
+            if(debug)
+                std::cerr << "transferring " << r2 << " from 2 to 1" << std::endl;
+            opts = fcntl(side1,F_GETFL);
+            opts ^= O_NONBLOCK;    
+            fcntl(side1, F_SETFL, opts);
+            
+            int w1 = send(side1, buffer2, r2, MSG_NOSIGNAL);
+            
+            if(w1 < 0)
+            {
+                if(debug)
+                    std::cerr << "writing to side 1 failed, ending: " << w1 << std::endl;
+                return;
+            }
+            assert(w1 == r2);
+            
+            opts = fcntl(side1,F_GETFL);
+            opts |= O_NONBLOCK;    
+            fcntl(side1, F_SETFL, opts);
+        }
+        usleep(1000);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int serverfd;
+    
+    if(argc < 2)
+    {
+        std::cerr << "need arguments:" << std::endl;
+        std::cerr << "[port]                    -   Open a server on this TCP port and forward connections to the local DBUS session"
+                     "                              (the DBUS_SESSION_BUS_ADDRESS environment variable must be set)";
+        std::cerr << "[port]  [fake dbus path]  -   Open a server on the fake dbus path and forward connections to the given local TCP port";
+        std::cerr << ""
+                     "The last argument may be the --bind-only option, in which case the application only tries to"
+                     "open the server, but does not wait for clients to connect. This is useful to test whether the"
+                     "server port/path is available.";
+        return 10;
+    }
+    
+    bool waitForClients = true;
+    
+    if(QString(argv[argc-1]) == "--bind-only")
+    {
+        waitForClients = false;
+        argc -= 1;
+    }
+
+    QString dbusAddress(getenv("DBUS_SESSION_BUS_ADDRESS"));
+
+    QString path;
+    
+    if(argc == 2)
+    {
+        if(waitForClients)
+            std::cout << "forwarding from the local TCP port " << argv[1] << " to the local DBUS session at " << dbusAddress.toLocal8Bit().data() << std::endl;
+        
+        if(dbusAddress.isEmpty())
+        {
+            std::cerr << "The DBUS_SESSION_BUS_ADDRESS environment variable is not set" << std::endl;
+            return 1;
+        }
+        
+        // Open a TCP server
+        
+        QString abstractPrefix("unix:abstract=");
+        
+        if(!dbusAddress.startsWith(abstractPrefix))
+        {
+            std::cerr << "DBUS_SESSION_BUS_ADDRESS does not seem to use an abstract unix domain socket as expected" << std::endl;
+            return 2;
+        }
+        
+        path = dbusAddress.mid(abstractPrefix.size());
+        if(path.contains(",guid="))
+            path.truncate(path.indexOf(",guid="));
+        
+        // Mark it as an abstract unix domain socket
+        path = path;
+        
+        serverfd = socket(AF_INET, SOCK_STREAM, 0);
+        
+        if (serverfd < 0) 
+        {
+            if(waitForClients)
+                std::cerr << "ERROR opening server socket" << std::endl;
+            return 3;
+        }
+        
+        int portno = atoi(argv[1]);
+        
+        sockaddr_in server_addr;
+        
+        bzero((char *) &server_addr, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+        server_addr.sin_port = htons(portno);
+        
+        if (bind(serverfd, (struct sockaddr *) &server_addr,
+                sizeof(server_addr)) < 0) 
+        {
+            if(waitForClients)
+                std::cerr << "ERROR opening the server" << std::endl;
+            return 7;
+        }
+    }else if(argc == 3)
+    {
+        if(waitForClients)
+            std::cout << "forwarding from the local abstract unix domain socket " << argv[2] << " to the local TCP port " << argv[1] << std::endl;
+        // Open a unix domain socket server
+        serverfd = socket(AF_UNIX, SOCK_STREAM, 0);
+        
+        if (serverfd < 0) 
+        {
+            if(waitForClients)
+                std::cerr << "ERROR opening server socket" << std::endl;
+            return 3;
+        }
+
+        path = QString(argv[2]);
+
+        sockaddr_un serv_addr;
+        bzero((char *) &serv_addr, sizeof(serv_addr));
+        serv_addr.sun_family = AF_UNIX;
+        serv_addr.sun_path[0] = '\0'; // Mark as an abstract socket
+        strcpy(serv_addr.sun_path+1, path.toLocal8Bit().data());
+        
+        if(debug)
+            std::cout << "opening at " << path.toLocal8Bit().data() << std::endl;
+        
+        if (bind(serverfd,(sockaddr *) &serv_addr, sizeof (serv_addr.sun_family) + 1 + path.toLocal8Bit().length()) < 0)
+        {
+            if(waitForClients)
+                std::cerr << "ERROR opening the server" << std::endl;
+            return 7;
+        }
+    }else{
+        std::cerr << "Wrong arguments";
+        return 1;
+    }
+    
+    listen(serverfd, 10);
+
+    while(waitForClients)
+    {
+        if(debug)
+            std::cerr << "waiting for client" << std::endl;
+        sockaddr_in cli_addr;
+        socklen_t clilen = sizeof(cli_addr);
+        int connectedclientsockfd = accept(serverfd, 
+                    (struct sockaddr *) &cli_addr, 
+                    &clilen);
+        
+        if(connectedclientsockfd < 0)
+        {
+            std::cerr << "ERROR on accept" << std::endl;
+            return 8;
+        }
+        
+        if(debug)
+            std::cerr << "got client" << std::endl;
+
+        int sockfd;
+        
+        int addrSize;
+        sockaddr* useAddr = 0;
+        sockaddr_un serv_addru;
+        sockaddr_in serv_addrin;
+        
+        if(argc == 2)
+        {
+            sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+            if (sockfd < 0) 
+            {
+                std::cerr << "ERROR opening socket" << std::endl;
+                return 3;
+            }
+            bzero((char *) &serv_addru, sizeof(serv_addru));
+            serv_addru.sun_family = AF_UNIX;
+            serv_addru.sun_path[0] = '\0'; // Mark as an abstract socket
+            strcpy(serv_addru.sun_path+1, path.toLocal8Bit().data());
+            addrSize = sizeof (serv_addru.sun_family) + 1 + path.toLocal8Bit().length();
+            useAddr = (sockaddr*)&serv_addru;
+            
+            if(debug)
+                std::cout << "connecting to " << path.toLocal8Bit().data() << std::endl;
+        }else{
+            sockfd = socket(AF_INET, SOCK_STREAM, 0);
+            if (sockfd < 0) 
+            {
+                std::cerr << "ERROR opening socket" << std::endl;
+                return 3;
+            }
+            int port = atoi(argv[1]);
+            hostent *server = gethostbyname("localhost");
+            if(server == NULL)
+            {
+                std::cerr << "failed to get server" << std::endl;
+                return 5;
+            }
+            bzero((char *) &serv_addrin, sizeof(serv_addrin));
+            serv_addrin.sin_family = AF_INET;
+            serv_addrin.sin_addr.s_addr = INADDR_ANY;
+            serv_addrin.sin_port = htons(port);
+            bcopy((char *)server->h_addr,  (char *)&serv_addrin.sin_addr.s_addr, server->h_length);
+            addrSize = sizeof (serv_addrin);
+            useAddr = (sockaddr*)&serv_addrin;
+            
+            if(debug)
+                std::cout << "connecting to port " << port << std::endl;
+        }
+        
+        int res;
+        if ((res = connect(sockfd, useAddr, addrSize)) < 0)
+        {
+            if(res == ECONNREFUSED)
+                std::cerr << "ERROR while connecting: connection refused" << std::endl;
+            else if(res == ENOENT)
+                std::cerr << "ERROR while connecting: no such file or directory" << std::endl;
+            else
+                std::cerr << "ERROR while connecting" << std::endl;
+            return 5;
+        }
+
+        shuffleBetweenStreams(connectedclientsockfd, sockfd);
+        close(sockfd);
+        close(connectedclientsockfd);
+    }
+    return 0;
+}
--- util/kdevplatform_shell_environment.sh
+++ util/kdevplatform_shell_environment.sh
@@ -22,6 +22,10 @@ source ~/.bashrc
 
 export BASE_SHELL_PROMPT=$PS1
 
+if ! [ "$APPLICATION_HOST" ]; then
+    export APPLICATION_HOST=$(hostname)
+fi
+
 if ! [ "$KDEV_DBUS_ID" ]; then
     echo "The required environment variable KDEV_DBUS_ID is not set. This variable defines the dbus id of the application instance instance which is supposed to be attached."
     exit 5
@@ -33,8 +37,8 @@ function getSessionName {
 
 function updateShellPrompt {
     # Makes the shell prompt show the current name of this session
-    SESSION_NAME=$(getSessionName)
-    PS1="$SESSION_NAME   - $BASE_SHELL_PROMPT"
+#     SESSION_NAME=$(getSessionName)
+    PS1="!$BASE_SHELL_PROMPT" # Just somehow set a mark that this session is attached
 }
 
 function help {
@@ -46,6 +50,8 @@ function help {
     echo "create [file] [[text]]              - Create and open a new file with the given optional text"
     echo "search [pattern] [[locations]] ...  - Search for the given pattern here or at the optionally given location(s)."
     echo "dsearch [pattern] [[locations]] ... - Same as search, but starts the search instantly instead of showing the dialog."
+    echo "fssh [ssh arguments]                - Connect to a remote host via ssh, keeping the control-connection alive."
+    echo "                                    - The whole dbus environment will be forwarded, KDevelop needs to be installed on both sides."
     echo ""
 }
 
@@ -147,9 +153,82 @@ function dsearch {
     qdbus $KDEV_DBUS_ID /org/kdevelop/GrepViewPlugin org.kdevelop.kdevelop.GrepViewPlugin.startSearch "$PATTERN" "$LOCATION" false
 }
 
-# Initialization:
+##### SSH DBUS FORWARDING --------------------------------------------------------------------------------------------------------------------
+
+DBUS_SOCKET_TRANSFORMER=$KDEV_BASEDIR/kdev_dbus_socket_transformer
+
+# We need this, to make sure that our forwarding-loops won't get out of control
+# This configures the shell to kill background jobs when it is terminated
+shopt -s huponexit
+
+# TODO: This random number can lead to conflicts, but since only ssh notices these conflicts
+#       during forwarding, it is very hard to deal with them.
+export DBUS_FORWARDING_TCP_TARGET_PORT=$((5000+($RANDOM%50000)))
+
+export DBUS_ABSTRACT_SOCKET_TARGET_BASE_PATH=/tmp/dbus-forwarded-$USER-$APPLICATION_HOST
+
+export DBUS_FORWARDING_TCP_LOCAL_PORT=9000
+export DBUS_FORWARDING_TCP_MAX_LOCAL_PORT=10000
+export DBUS_ABSTRACT_SOCKET_TARGET_INDEX=1
+export DBUS_ABSTRACT_SOCKET_MAX_TARGET_INDEX=1000
+
+function getDBusAbstractSocketSuffix {
+    # From something like DBUS_SESSION_BUS_ADDRESS=unix:abstract=/tmp/dbus-wYmSkVH7FE,guid=b214dad39e0292a4299778d64d761a5b
+    # extract the /tmp/dbus-wYmSkVH7FE
+    echo $DBUS_SESSION_BUS_ADDRESS | sed 's/unix\:abstract\=.*\(,guid\=.*\)/\1/'
+}
+
+function keepForwardingDBusToTCPSocket {
+    while ! $KDEV_BASEDIR/kdev_dbus_socket_transformer $DBUS_FORWARDING_TCP_LOCAL_PORT --bind-only; do
+        if (($DBUS_FORWARDING_TCP_LOCAL_PORT<$DBUS_FORWARDING_TCP_MAX_LOCAL_PORT)); then
+            export DBUS_FORWARDING_TCP_LOCAL_PORT=$(($DBUS_FORWARDING_TCP_LOCAL_PORT+1))
+#             echo "Increased local port to " $DBUS_FORWARDING_TCP_LOCAL_PORT;
+        else
+            echo "Failed to allocate a local TCP port";
+            return 1;
+        fi
+    done
+        
+    $KDEV_BASEDIR/kdev_dbus_socket_transformer $DBUS_FORWARDING_TCP_LOCAL_PORT&
+    return 0;
+}
+
+function keepForwardingDBusFromTCPSocket {
+
+    while ! $KDEV_BASEDIR/kdev_dbus_socket_transformer $FORWARD_DBUS_FROM_PORT ${DBUS_ABSTRACT_SOCKET_TARGET_BASE_PATH}-${DBUS_ABSTRACT_SOCKET_TARGET_INDEX} --bind-only; do
+        if ((${DBUS_ABSTRACT_SOCKET_TARGET_INDEX}<${DBUS_ABSTRACT_SOCKET_MAX_TARGET_INDEX})); then
+            export DBUS_ABSTRACT_SOCKET_TARGET_INDEX=$(($DBUS_ABSTRACT_SOCKET_TARGET_INDEX+1))
+        else
+            echo "Failed to allocate a local path for the abstract dbus socket";
+            return 1;
+        fi
+    done
+    
+    local PATH=${DBUS_ABSTRACT_SOCKET_TARGET_BASE_PATH}-${DBUS_ABSTRACT_SOCKET_TARGET_INDEX}
+    export DBUS_SESSION_BUS_ADDRESS=unix:abstract=$PATH${DBUS_SOCKET_SUFFIX}
+    $KDEV_BASEDIR/kdev_dbus_socket_transformer $FORWARD_DBUS_FROM_PORT $PATH&
+}
+
+function fssh {
+    echo "forwarding from $APPLICATION_HOST"
+    keepForwardingDBusToTCPSocket # Should be automatically terminated when the function exits
+    echo "calling" ssh $@ -t -R localhost:$DBUS_FORWARDING_TCP_TARGET_PORT:localhost:$DBUS_FORWARDING_TCP_LOCAL_PORT \
+           "APPLICATION=$APPLICATION KDEV_BASEDIR=$KDEV_BASEDIR KDEV_DBUS_ID=$KDEV_DBUS_ID FORWARD_DBUS_FROM_PORT=$DBUS_FORWARDING_TCP_TARGET_PORT APPLICATION_HOST=$APPLICATION_HOST DBUS_SOCKET_SUFFIX=$(getDBusAbstractSocketSuffix) bash --init-file $KDEV_BASEDIR/kdevplatform_shell_environment.sh -i"
+    ssh $@ -t -R localhost:$DBUS_FORWARDING_TCP_TARGET_PORT:localhost:$DBUS_FORWARDING_TCP_LOCAL_PORT \
+           "APPLICATION=$APPLICATION KDEV_BASEDIR=$KDEV_BASEDIR KDEV_DBUS_ID=$KDEV_DBUS_ID FORWARD_DBUS_FROM_PORT=$DBUS_FORWARDING_TCP_TARGET_PORT APPLICATION_HOST=$APPLICATION_HOST DBUS_SOCKET_SUFFIX=$(getDBusAbstractSocketSuffix) bash --init-file $KDEV_BASEDIR/kdevplatform_shell_environment.sh -i"
+    kill %1 # Kill the forwarding loop
+}
+
+if [ "$FORWARD_DBUS_FROM_PORT" ]; then
+    echo "Initializing DBUS forwarding to host $APPLICATION_HOST"
+    export DBUS_SESSION_BUS_ADDRESS=unix:abstract=${DBUS_ABSTRACT_SOCKET_TARGET_BASE_PATH}-${DBUS_ABSTRACT_SOCKET_TARGET_INDEX}${DBUS_SOCKET_SUFFIX}
+    keepForwardingDBusFromTCPSocket
+fi
+
+##### INITIALIZATION --------------------------------------------------------------------------------------------------------------------
 
 updateShellPrompt
+echo "Initialized"
 
 help
 
