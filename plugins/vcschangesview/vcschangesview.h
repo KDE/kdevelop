9fd0a4d7b464ecd2124dc2038939ef25d4e17393
Aleix Pol <aleixpol@kde.org>
2011-06-04 15:35:53 +0200

    Moved the rest of the vcsprojectintegration plugin into KDevPlatform.

---
+++ plugins/vcschangesview/vcschangesview.h
@@ -0,0 +1,45 @@
+/*
+ * This file is part of KDevelop
+ * Copyright 2010 Aleix Pol Gonzalez <aleixpol@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef VCSCHANGESVIEW_H
+#define VCSCHANGESVIEW_H
+
+#include <QTreeView>
+
+class VcsProjectIntegrationPlugin;
+class KUrl;
+namespace KDevelop { class IProject; }
+
+class VcsChangesView : public QTreeView
+{
+    Q_OBJECT
+    public:
+        explicit VcsChangesView(VcsProjectIntegrationPlugin* plugin, QWidget* parent = 0);
+        
+    public slots:
+        void popupContextMenu( const QPoint &pos );
+        void selectCurrentDocument();
+        
+    signals:
+        void reload(const QList<KDevelop::IProject*>& p);
+        void reload(const QList<KUrl>& p);
+};
+
+#endif // VCSCHANGESVIEW_H
---
+++ plugins/vcschangesview/vcschangesviewplugin.h
@@ -0,0 +1,54 @@
+/*  This file is part of KDevelop
+    Copyright 2010 Aleix Pol <aleixpol@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#ifndef VCSPROJECTINTEGRATIONPLUGIN_H
+#define VCSPROJECTINTEGRATIONPLUGIN_H
+
+#include <interfaces/iplugin.h>
+#include <vcs/interfaces/ibasicversioncontrol.h>
+#include <QVariant>
+#include <QStandardItemModel>
+
+class QModelIndex;
+class KUrl;
+class KJob;
+
+namespace KDevelop
+{
+    class IProject;
+    class IDocument;
+    class VcsStatusInfo;
+    class ProjectChangesModel;
+}
+
+class VcsProjectIntegrationPlugin : public KDevelop::IPlugin
+{
+    Q_OBJECT
+    public:
+        VcsProjectIntegrationPlugin(QObject *parent, const QVariantList & args);
+        KDevelop::ProjectChangesModel* model() const;
+        
+    private slots:
+        void activated(const QModelIndex& idx);
+        
+    private:
+        KDevelop::ProjectChangesModel* m_model;
+};
+
+#endif
