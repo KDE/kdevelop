ff8b78764b0612ff07f8b59eb4317d23d01ea42c
Niko Sams <niko.sams@gmail.com>
2011-06-07 22:51:29 +0200

    set Commit-Message Textfield LineWrapMode to NoWrap
    because else the user might have the impression the message
    is wraped -  but it is not. This is especially bad as commit
    messages should wrap at 72 chars.

--- vcs/widgets/vcsdiffpatchsources.cpp
+++ vcs/widgets/vcsdiffpatchsources.cpp
@@ -63,6 +63,7 @@ VCSCommitDiffPatchSource::VCSCommitDiffPatchSource(VCSDiffUpdater* updater, cons
 
     m_commitMessageEdit = new QTextEdit;
     m_commitMessageEdit.data()->setFont( KGlobalSettings::fixedFont() );
+    m_commitMessageEdit.data()->setLineWrapMode(QTextEdit::NoWrap);
     
     QHBoxLayout* titleLayout = new QHBoxLayout;
     titleLayout->addWidget(new QLabel(i18n("Commit Message:")));
