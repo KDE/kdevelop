a6dca86625759aeb59861bea4b05e6b24927837b
Milian Wolff <mail@milianw.de>
2011-07-19 16:54:45 +0200

    improve TestCore API and usability
    it's now enough to call TestCore::initialize() in most cases
    
    we will furthermore init the core with a temporary session
    by default which is named after the currently running application

--- tests/testcore.cpp
+++ tests/testcore.cpp
@@ -26,7 +26,11 @@
 #include <shell/plugincontroller.h>
 #include <shell/partcontroller.h>
 #include <shell/projectcontroller.h>
+#include <interfaces/isession.h>
 #include "../shell/core_p.h"
+
+#include <KGlobal>
+#include <QCoreApplication>
 
 namespace KDevelop
 {
@@ -34,12 +38,34 @@ namespace KDevelop
 TestCore::TestCore()
  : Core( new CorePrivate(this) )
 {
-    KDevelop::Core::m_self = this;
+    Core::m_self = this;
 }
 
 void TestCore::initialize( Core::Setup mode, const QString& session )
 {
+    if (!Core::m_self) {
+        new TestCore;
+    }
+
+    TestCore* core = dynamic_cast<TestCore*>(Core::m_self);
+    Q_ASSERT(core);
+    core->initializeNonStatic(mode, session);
+}
+
+void TestCore::initializeNonStatic(Core::Setup mode, const QString& _session)
+{
+    QString session = _session;
+    if (_session.isEmpty()) {
+        // use a distinct session name for unit test sessions
+        // they are temporary (see below) but still - we want to make sure
+        session = "test-" + qAppName();
+    }
+
     d->initialize( mode, session );
+
+    if (_session.isEmpty()) {
+        activeSession()->setTemporary(true);
+    }
 }
 
 void TestCore::setSessionController( SessionController* ctrl )
--- tests/testcore.h
+++ tests/testcore.h
@@ -37,17 +37,29 @@ class UiController;
 /**
  * \class TestCore testcore.h
  *
- * This class allows to replace certain or all parts of the
+ * This is an ICore implementation which should be used inside unit tests.
+ * By default a temporary session named after currently running application
+ * will be instantiated.
+ *
+ * The simplest code which should suffice for most use cases is:
+ *
+ * \code
+ * TestCore::initialize();
+ * \endcode
+ *
+ * This class also allows to replace certain or all parts of the
  * shell library with custom implementation to make it possible
  * to write plugin tests more easily.
  *
- * usage is as follows:
+ * The usage is then as follows:
  * \code
  * TestCore* core = new TestCore();
- * //replace plugin controller
+ * //replace plugin controller or similar
  * core->setPluginController( new MyCustomPluginController( core ) );
- * core->initialize();
+ * //initialize
+ * core->initialize(Core::Default);
  * ... //test code
+ * // if you want to cleanup manually:
  * core->cleanup();
  * delete core;
  * \endcode
@@ -60,7 +72,17 @@ class KDEVPLATFORMTESTS_EXPORT TestCore : public Core
     Q_OBJECT
 public:
     TestCore();
-    void initialize( Core::Setup, const QString& session = "" );
+    /**
+     * Initialize the test core with the given setup @p mode.
+     * Optionally launch with a user given session.
+     *
+     * @p mode Use Core::NoUi when your unit test does not require any UI
+     * @p session By default a temporary session will be created called "test-%appname".
+     *            If @p session is not empty, a non-temporary session with the given name
+     *            will be opened.
+     */
+    static void initialize( Core::Setup mode = Core::Default, const QString& session = "" );
+
     void setSessionController( SessionController* );
     void setPluginController( PluginController* );
     void setRunController( RunController* );
@@ -69,6 +91,9 @@ public:
     void setProjectController( ProjectController* );
     void setLanguageController( LanguageController* );
     void setUiController( UiController* );
+
+private:
+    void initializeNonStatic( Core::Setup mode, const QString& session );
 };
 
 }
