99e8ceaef95099f12543f815838a9f8f1c049ba8
Pino Toscano <pino@kde.org>
2011-07-21 11:31:48 +0200

    i18n: do not extract messages for sources in "tests" subdirectories

--- languages/cpp/Messages.sh
+++ languages/cpp/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 $EXTRACTRC `find . -name \*.rc` `find . -name \*.ui` >>rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevcpp.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kdevcpp.pot
 rm -f rc.cpp
