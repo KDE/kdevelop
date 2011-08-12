6b5128080299b53bb3091d0185e7d6f121e49b52
Aleix Pol <aleixpol@kde.org>
2011-08-12 11:53:37 +0200

    Add missing files from last commit

---
+++ sublime/holdupdates.h
@@ -0,0 +1,73 @@
+/* This file is part of KDevelop
+    Copyright 2010 Aleix Pol Gonzalez <aleixpol@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License version 2 as published by the Free Software Foundation.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef HOLDUPDATES_H
+#define HOLDUPDATES_H
+
+#include "sublimeexport.h"
+
+class QWidget;
+
+namespace Sublime
+{
+/**
+This class makes it possible to have nested scoped calls to QWidget::setUpdatesEnabled.
+
+For that matter: 
+
+@code 
+{
+    ...
+    setUpdatesEnabled(false);
+    someCall();
+    setUpdatesEnabled(true);
+    ...
+}
+@endcode
+
+Would be equivalent to 
+
+@code
+{
+    ...
+    HoldUpdates hu(this);
+    someCall();
+    hu.stop();//if we need to, otherwise we can let it fall out of scope
+    ...
+}
+@endcode
+
+if someCall was using HoldUpdates too, it will make sure to only call setUpdatesEnabled(true) in case it's needed.
+
+ */
+class SUBLIME_EXPORT HoldUpdates {
+    public:
+        HoldUpdates(QWidget* w);
+        ~HoldUpdates();
+         void stop();
+    
+    private:
+        bool m_wasupdating;
+        bool m_done;
+        QWidget* m_w;
+};
+
+}
+
+#endif // HOLDUPDATES_H
+
