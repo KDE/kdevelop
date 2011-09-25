d85deaa944a304d609afc65a9253203193cf94da
Albert Astals Cid <tsdgeos@terra.es>
2011-09-25 13:41:21 +0200

    Adapt Messages.sh to work with files with spaces
    CCMAIL: niko.sams@gmail.com

--- debuggers/gdb/Messages.sh
+++ debuggers/gdb/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 $EXTRACTRC `find . -name \*.rc` `find . -name \*.ui` >>rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kdevgdb.pot
+find . \( -name \*.cc -o -name \*.cpp -o -name \*.h \) -print0 | xargs -0 $XGETTEXT -o $podir/kdevgdb.pot
 rm -f rc.cpp
