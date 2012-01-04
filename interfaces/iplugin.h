28bac3de1de8dacc6a2925b9f98e925f84978003
Milian Wolff <mail@milianw.de>
2012-01-04 15:24:09 +0100

    set plugin version to 13 in preparation of 4.3 release

--- interfaces/iplugin.h
+++ interfaces/iplugin.h
@@ -42,7 +42,7 @@ namespace Sublime {
  * to make sure old source (or binary) incompatible plugins are not loaded.
  * Increase this if it is necessary that old plugins stop working.
  */
-#define KDEVELOP_PLUGIN_VERSION 12
+#define KDEVELOP_PLUGIN_VERSION 13
 
 /**
  * This macro adds an extension interface to register with the extension manager
