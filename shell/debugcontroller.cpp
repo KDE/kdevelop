ed40d42135b7c1d8f5bdf6913e1172c0f8202493
Niko Sams <niko.sams@gmail.com>
2012-01-14 17:03:53 +0100

    add icon to context menu

--- shell/debugcontroller.cpp
+++ shell/debugcontroller.cpp
@@ -286,7 +286,7 @@ void DebugController::setupActions()
     connect(action, SIGNAL(triggered(bool)), this, SLOT(stepOut()));
     ac->addAction("debug_stepout", action);
 
-    m_toggleBreakpoint = action = new KAction(i18n("Toggle Breakpoint"), this);
+    m_toggleBreakpoint = action = new KAction(KIcon("script-error"), i18n("Toggle Breakpoint"), this);
     action->setToolTip(i18n("Toggle breakpoint"));
     action->setWhatsThis(i18n("<b>Toggle breakpoint</b><p>Toggles the breakpoint at the current line in editor.</p>"));
     connect(action, SIGNAL(triggered(bool)), this, SLOT(toggleBreakpoint()));
