136cb8a18a8d2dd01b04b3e484cbff61d14ff0ee
Miha Čančula <miha@noughmad.eu>
2012-02-09 14:24:32 +0100

    Implement the TestController interface in Shell

---
+++ shell/testcontroller.cpp
@@ -0,0 +1,58 @@
+/*  This file is part of KDevelop
+    Copyright 2012 Miha Čančula <miha@noughmad.eu>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "testcontroller.h"
+#include "interfaces/itestsuite.h"
+
+using namespace KDevelop;
+
+class KDevelop::TestControllerPrivate
+{
+public:
+    QList<ITestSuite*> suites;
+};
+
+TestController::TestController(QObject* parent): ITestController(parent),
+d(new TestControllerPrivate)
+{
+
+}
+
+TestController::~TestController()
+{
+    delete d;
+}
+
+QList< KDevelop::ITestSuite* > TestController::testSuites() const
+{
+    return d->suites;
+}
+
+void TestController::removeTestSuite(KDevelop::ITestSuite* suite)
+{
+    d->suites.removeAll(suite);
+    emit testSuiteAdded(suite);
+}
+
+void TestController::addTestSuite(KDevelop::ITestSuite* suite)
+{
+    d->suites.append(suite);
+    emit testSuiteRemoved(suite);
+}
+
