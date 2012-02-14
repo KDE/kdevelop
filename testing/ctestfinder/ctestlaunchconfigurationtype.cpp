57548b97a6e83d8b34dc6721dd356fe35a43d38f
Miha Čančula <miha@noughmad.eu>
2012-02-14 12:31:00 +0100

    Configure the launch configuration from project items

--- testing/ctestfinder/ctestlaunchconfigurationtype.cpp
+++ testing/ctestfinder/ctestlaunchconfigurationtype.cpp
@@ -27,7 +27,9 @@
 #include <KIcon>
 #include <KDebug>
 #include "ctestlauncher.h"
+#include "ctestsuite.h"
 #include <interfaces/iplugincontroller.h>
+#include <interfaces/itestsuite.h>
 #include <project/projectmodel.h>
 
 using namespace KDevelop;
@@ -44,29 +46,36 @@ CTestLaunchConfigurationType::~CTestLaunchConfigurationType()
 
 bool CTestLaunchConfigurationType::canLaunch(const KUrl& file) const
 {
+    kDebug() << file;
     ITestController* controller = ICore::self()->pluginController()->pluginForExtension("org.kdevelop.ITestController")->extension<ITestController>();
-    ITestSuite* suite = controller->testSuiteForUrl(file);
-    if (suite)
+    if (controller->testSuiteForUrl(file))
     {
-        return true;
+        kDebug() << "Suite found, can launch";
     }
-    return false;
+    return dynamic_cast< CTestSuite* >(controller->testSuiteForUrl(file));
 }
 
 void CTestLaunchConfigurationType::configureLaunchFromCmdLineArguments(KConfigGroup config, const QStringList& args) const
 {
-    kDebug() << args;
-    // TODO
+    kWarning() << "Configuring CTest from CmdLine is not supported" << args;
 }
 
 void CTestLaunchConfigurationType::configureLaunchFromItem(KConfigGroup config, KDevelop::ProjectBaseItem* item) const
 {
-    kDebug() << item->url();
-    // TODO
+    ITestController* controller = ICore::self()->pluginController()->pluginForExtension("org.kdevelop.ITestController")->extension<ITestController>();
+    KUrl url = item->target()->executable()->builtUrl();
+    kDebug() << url;
+    CTestSuite* suite = dynamic_cast<CTestSuite*>(controller->testSuiteForUrl(url));
+    config.writeEntry("TestExecutable", suite->url().toLocalFile());
+    config.writeEntry("TestSuiteName", suite->name());
+    config.writeEntry("TestArguments", suite->arguments());
+    config.writeEntry("TestArguments", suite->cases());
+    config.sync();
 }
 
 bool CTestLaunchConfigurationType::canLaunch(KDevelop::ProjectBaseItem* item) const
 {
+    kDebug() << "Can launch item?" << item;
     if( item->target() && item->target()->executable() ) {
         return canLaunch( item->target()->executable()->builtUrl() );
     }
