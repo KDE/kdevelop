cf1af9fb89e075721ede978549e8205437781f3b
Milian Wolff <mail@milianw.de>
2012-03-12 16:01:52 +0100

    some cleanup
    - use qDeleteAll
    - foreach() iterates over values by default, no need to be explicit
    - mark copies as const, should prevent operator[] from detaching

--- language/duchain/duchain.cpp
+++ language/duchain/duchain.cpp
@@ -528,7 +528,7 @@ public:
     QMutexLocker l(&m_chainsMutex);
     
     //Add those information that have not been added to the stored lists yet
-    foreach(ParsingEnvironmentFilePointer file, m_fileEnvironmentInformations.values(url))
+    foreach(const ParsingEnvironmentFilePointer& file, m_fileEnvironmentInformations)
       if(!ret.contains(file))
         ret << file;
 
