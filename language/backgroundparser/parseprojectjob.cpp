8fc6b536f7f30e74e7b5fc325d4624fb2470657e
Milian Wolff <mail@milianw.de>
2012-03-22 20:04:36 +0100

    prevent UI-lockup when opening huge projects
    when adding e.g. the ~180k files in WebKit, our UI used to hang
    on my machine for more than a second which felt bad.
    
    now we process events after adding one thousand files to the
    background parser, and the UI stays responsive during that - neat

--- language/backgroundparser/parseprojectjob.cpp
+++ language/backgroundparser/parseprojectjob.cpp
@@ -24,6 +24,7 @@
 #include <interfaces/iproject.h>
 #include <klocalizedstring.h>
 #include <interfaces/icompletionsettings.h>
+#include <QApplication>
 
 using namespace KDevelop;
 
@@ -76,9 +77,18 @@ void ParseProjectJob::start() {
 
     TopDUContext::Features processingLevel = files.size() < ICore::self()->languageController()->completionSettings()->minFilesForSimplifiedParsing() ?
                                     KDevelop::TopDUContext::VisibleDeclarationsAndContexts : KDevelop::TopDUContext::SimplifiedVisibleDeclarationsAndContexts;
-    
-    foreach(const KDevelop::IndexedString& url, files)
+
+    // prevent UI-lockup by processing events after some files
+    // esp. noticeable when dealing with huge projects
+    const int processAfter = 1000;
+    int processed = 0;
+    foreach(const KDevelop::IndexedString& url, files) {
         KDevelop::ICore::self()->languageController()->backgroundParser()->addDocument( url.toUrl(), processingLevel, BackgroundParser::WorstPriority, this );
+        if (++processed = processAfter) {
+            QApplication::processEvents();
+            processed = 0;
+        }
+    }
 }
 
 #include "parseprojectjob.moc"
