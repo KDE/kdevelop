00081e52f35645e51698041aa4033555b88f7a87
Morten Danielsen Volden <mvolden2@gmail.com>
2012-04-16 11:12:30 +0200

    Move ErrorFormat and ActionFormat from OutputFilters.cpp to OutputFormats.cpp under Kdevplatform. Include the classes in KDevelop namespace

---
+++ outputview/outputformats.cpp
@@ -0,0 +1,55 @@
+/***************************************************************************
+ *   Copyright 1999-2001 Bernd Gehrmann <bernd@kdevelop.org>               *
+ *   Copyright 2007 Dukju Ahn <dukjuahn@gmail.com>                         *
+ *   Copyright (C) 2012  Morten Danielsen Volden mvolden2@gmail.com        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "outputformats.h"
+
+#include <klocale.h>
+
+namespace KDevelop
+{
+
+ErrorFormat::ErrorFormat( const QString& regExp, int file, int line, int text, int column )
+    : expression( regExp )
+    , fileGroup( file )
+    , lineGroup( line )
+    , columnGroup( column )
+    , textGroup( text )
+{}
+
+ErrorFormat::ErrorFormat( const QString& regExp, int file, int line, int text, const QString& comp, int column )
+    : expression( regExp )
+    , fileGroup( file )
+    , lineGroup( line )
+    , columnGroup( column )
+    , textGroup( text )
+    , compiler( comp )
+{}
+
+ActionFormat::ActionFormat( const QString& _action, const QString& _tool, const QString& regExp, int file )
+    : action( _action )
+    , expression( regExp )
+    , tool( _tool )
+    , toolGroup(-1)
+    , fileGroup( file )
+{
+}
+
+ActionFormat::ActionFormat( const QString& _action, int tool, int file, const QString& regExp)
+    : action( _action )
+    , expression( regExp )
+    , toolGroup( tool )
+    , fileGroup( file )
+{
+}
+
+}
+
---
+++ outputview/outputformats.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *   Copyright 1999-2001 Bernd Gehrmann <bernd@kdevelop.org>               *
+ *   Copyright 2007 Dukju Ahn <dukjuahn@gmail.com>                         *
+ *   Copyright (C) 2012  Morten Danielsen Volden mvolden2@gmail.com        *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef OUTPUTFORMATS_H
+#define OUTPUTFORMATS_H
+
+#include <QString>
+#include <QRegExp>
+
+namespace KDevelop
+{
+
+class ActionFormat
+{
+    public:
+        //static QList<ActionFormat> actionFormats;
+        ActionFormat( const QString&, const QString&, const QString& regExp, int file);
+        ActionFormat( const QString&, int tool, int file, const QString& regExp);
+        QString action;
+        QRegExp expression;
+        QString tool;
+        int toolGroup;
+        int fileGroup;
+};
+
+class ErrorFormat
+{
+    public:
+        //static QList<ErrorFormat> errorFormats;
+        ErrorFormat( const QString&, int, int, int, int column=-1 );
+        ErrorFormat( const QString&, int, int, int, const QString&, int column=-1 );
+        QRegExp expression;
+        int fileGroup;
+        int lineGroup, columnGroup;
+        int textGroup;
+        QString compiler;
+};
+
+}
+#endif
+
+
+
