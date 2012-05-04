KDE/kdevelop

commit 880452719b9c5072e7273959d60f857397063caa

properly update local scope identifier when context is reused
prior to this patch, if you renamed a class or similar, the local context
would keep it's old, outdated identifier leading to all sorts of issues.
most notable code generation would use the old name e.g.

a unit test is added in kdevelop/languages/cpp

language/duchain/builders/abstractcontextbuilder.h
@@ -618,8 +618,11 @@ protected:
 
         setInSymbolTable( ret );
       }else{
+        DUChainWriteLocker writeLock( DUChain::lock() );
         if(ret->parentContext())
           ret->setRange( range );
+        if ( !identifier.isEmpty() && ret->localScopeIdentifier() != identifier )
+          ret->setLocalScopeIdentifier( identifier );
       }
     }
 

commit bfb0b798d6fb098c3628462102cd918a8702f7cb

Don't reuse contexts when local scope identifier changed.
My last commit that 'fixed' the rename issue of a class introduced
a serious regression that lead to assertions in e.g. the CodeModel
when a context was renamed that contained other contexts/declarations
that where already put into the CodeModel:

In such cases, these items would try to update their item in the
codemodel but since their QID was changed could not find the associated
item.

Looking at DUContext::setLocalScopeIdentifier this is apparently not
supported anyways. Hence we solve this bug by ensuring that we never
re-use a context when it's local scope identifier changed, but the
range stayed equal.

Fixes a regression introduced by 880452719b9c5072e7273959d60f857397063caa.

BUG: 299230

language/duchain/builders/abstractcontextbuilder.h
@@ -588,8 +588,14 @@ protected:
           DUContext* child = childContexts.at( currentIndex );
           RangeInRevision childRange = child->range();
 
-          //For unnamed child-ranges, we still do range-comparison, because we cannot distinguish them in other ways
-          if ( (childRange == range && !childRange.isEmpty()) || (child->type() == type && child->localScopeIdentifier() == identifier && !identifier.isEmpty()) )
+          if (child->type() != type) {
+            continue;
+          }
+          // We cannot update a contexts local scope identifier, that will break many other parts, like e.g.
+          // the CodeModel of child contexts or declarations.
+          // For unnamed child-ranges, we still do range-comparison, because we cannot distinguish them in other ways
+          if ((!identifier.isEmpty() && child->localScopeIdentifier() == identifier)
+              || (identifier.isEmpty() && !childRange.isEmpty() && childRange == range))
           {
             // Match
             ret = child;
@@ -619,10 +625,9 @@ protected:
         setInSymbolTable( ret );
       }else{
         DUChainWriteLocker writeLock( DUChain::lock() );
+        Q_ASSERT(ret->localScopeIdentifier() == identifier);
         if(ret->parentContext())
           ret->setRange( range );
-        if ( !identifier.isEmpty() && ret->localScopeIdentifier() != identifier )
-          ret->setLocalScopeIdentifier( identifier );
       }
     }
 