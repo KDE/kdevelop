1811c17e078393b639f7b72499f332bf0d97bbb8
Miha Čančula <miha@noughmad.eu>
2012-05-21 14:37:17 +0200

    Add documentation for ProjectTestJob

--- util/projecttestjob.h
+++ util/projecttestjob.h
@@ -34,22 +34,71 @@ class TestResult;
 class IProject;
 class ITestSuite;
 
+/**
+ * A combined result of a project test job
+ * 
+ * @sa ProjectTestJob
+ * 
+ **/
 struct KDEVPLATFORMUTIL_EXPORT ProjectTestResult
 {
+    /**
+     * The total number of test suites launched in this job
+     **/
     int total;
+    /**
+     * The number of passed test suites in this job. 
+     **/
     int passed;
+    /**
+     * The number of failed test suites in this job. 
+     **/
     int failed;
+    /**
+     * The number of errors in this job. 
+     **/
     int error;
 };
 
+/**
+ * @brief A job that tests an entire project and reports the total result
+ * 
+ * Launches all test suites in the specified project without raising the output window. 
+ * Instead of providing individual test results, it combines and simplifies them. 
+ * 
+ **/
 class KDEVPLATFORMUTIL_EXPORT ProjectTestJob : public KJob
 {
     Q_OBJECT
 public:
+    /**
+     * Create a project test job
+     *
+     * @param project The project to be tested
+     * @param parent This job's parent object, or 0 for no parent.
+     * 
+     **/
     explicit ProjectTestJob(IProject* project, QObject* parent = 0);
+    
+    /**
+     * Destructor
+     *
+     **/
     virtual ~ProjectTestJob();
+    
+    /**
+     * Start this job. 
+     **/
     virtual void start();
     
+    /**
+     * @brief The result of this job
+     * 
+     * This function only returns a correnct result after all the tests are completed. 
+     * It is therefore best to call this after the KJob::result() signal is emitted. 
+     * 
+     * @sa ProjectTestResult
+     **/
     ProjectTestResult testResult();
     
 private Q_SLOTS:
