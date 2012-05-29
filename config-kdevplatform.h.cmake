34dc37ec3b37c4895f5164235812a1a367b779c7
Andreas Pakulat <apaku@gmx.de>
2012-05-29 11:48:36 +0200

    Fix generation of the config-file
    With the #define x @x@ the generated config file would still always have
    a #define x in it, even if @x@ is 0. This results in both ext-hashmap and
    unordered-map to be enabled even if only the latter was found and hence
    results in compile-errors.
    
    Instead if one wants to base a define in a configure_file-generated header
    on the value of a cmake variable #cmakedefine should be used.
    
    This fixes building of kdevplatform on systems where unordered_map produces
    a warning without the -std=c++0x compiler flag which in turn makes
    check_include_file_cxx fail to find the header.

--- config-kdevplatform.h.cmake
+++ config-kdevplatform.h.cmake
@@ -1,8 +1,8 @@
 /* Define to 1 if you have Kompare support, 0 otherwise */
-#define HAVE_KOMPARE @HAVE_KOMPARE@
+#cmakedefine HAVE_KOMPARE
 
 /* Define to 1 if the unordered_map header is present, 0 otherwise */
-#define HAVE_UNORDERED_MAP @HAVE_UNORDERED_MAP@
+#cmakedefine HAVE_UNORDERED_MAP
 
 /* Define to 1 if the ext/hash_map header is present, 0 otherwise */
-#define HAVE_EXT_HASH_MAP @HAVE_EXT_HASH_MAP@
+#cmakedefine HAVE_EXT_HASH_MAP
