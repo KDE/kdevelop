175f616f847b5b2d645c13812909fd9c10e0095d
Andreas Pakulat <apaku@gmx.de>
2012-08-09 21:29:43 +0200

    Restore working prev/next output marker behaviour
    This was broken during the refactoring when moving the code from kdevelop
    into kdevplatform. The shuffling of code apparently led to the wrong
    lists size being appended to the set. The original code appended the size
    of the actual lines in the model (not the error-items) to the list, which
    is the same as the row-number of the newly-inserted item. This was
    transformed into adding the size of the error-items-list, which simply
    makes no sense. The code using this list actually expects row-numbers for
    the model in the set.

--- outputview/outputmodel.cpp
+++ outputview/outputmodel.cpp
@@ -247,7 +247,7 @@ void OutputModel::addLineBatch()
             item = d->m_filter->actionInLine(line);
         }
         if( item.type == FilteredItem::ErrorItem ) {
-            d->m_activateableItems.insert(d->m_activateableItems.size());
+            d->m_activateableItems.insert(d->m_filteredItems.size());
         }
 
         d->m_filteredItems << item;
