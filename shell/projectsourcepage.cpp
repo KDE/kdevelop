5a4a8fae4e98150ffab1a62933fd1f337652b2e8
Miquel Sabaté <mikisabate@gmail.com>
2012-08-17 13:09:08 +0200

    Fixed crash in the ProjectSourcePage when a NULL job has been returned.
    This may happen, for example, in the KDE project provider when no repository
    has been selected. In this case, don't crash, just do nothing and we're done.
    
    REVIEW: 106059

--- shell/projectsourcepage.cpp
+++ shell/projectsourcepage.cpp
@@ -132,7 +132,7 @@ VcsJob* ProjectSourcePage::jobPerCurrent()
     } else if(m_providerWidget) {
         job=m_providerWidget->createWorkingCopy(url);
     }
-    Q_ASSERT(job);
+
     return job;
 }
 
@@ -149,6 +149,8 @@ void ProjectSourcePage::getVcsProject()
     }
     
     VcsJob* job=jobPerCurrent();
+    if (!job)
+        return;
     
     m_ui->sources->setEnabled(false);
     m_ui->sourceBox->setEnabled(false);
