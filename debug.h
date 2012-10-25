254432bfa4b2c498b9abad62bf38eb8e9c36a41f
Milian Wolff <mail@milianw.de>
2012-10-25 20:50:37 +0200

    add debug.h file - oops

---
+++ debug.h
@@ -0,0 +1,32 @@
+/*************************************************************************************
+ *  Copyright (C) 2012 by Aleix Pol <aleixpol@kde.org>                               *
+ *  Copyright (C) 2012 by Milian Wolff <mail@milianw.de>                             *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <KDebug>
+
+static int debugArea()
+{
+    static int s_area = KDebug::registerArea("kdevelop (QML/JS support)");
+    return s_area;
+}
+#define debug() kDebug(debugArea())
+
+#endif // DEBUG_H
