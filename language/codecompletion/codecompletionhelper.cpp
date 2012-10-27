c7aca94d7790c6fce0b4bd5bdfb4b9a1d301c0d8
Sven Brauch <svenbrauch@googlemail.com>
2012-10-27 15:57:00 +0200

    Fix C++ code completion bug: don't insert a ";" if there's already one
    The functionality for that was there, but it didn't work since it
    checked for the character at the position where it would be after
    the text was inserted by the completion action, but before it actually
    inserted that text.

--- language/codecompletion/codecompletionhelper.cpp
+++ language/codecompletion/codecompletionhelper.cpp
@@ -78,7 +78,7 @@ void insertFunctionParenText(KTextEditor::Document* document, const KTextEditor:
     if (funcType) {
       if (IntegralType::Ptr type = funcType->returnType().cast<IntegralType>()) {
         if (type->dataType() == IntegralType::TypeVoid) {
-          const QChar nextChar = document->character(jumpPos);
+          const QChar nextChar = document->character(pos);
           if (nextChar != ';' && nextChar != ')' && nextChar != ',') {
             closingParen += ';';
           }
