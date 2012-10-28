0e2de87776eb43642a52966c4608ef90c079e546
Simon St James <kdedevel@etotheipiplusone.com>
2012-10-28 22:26:40 +0100

    Restore editor config on load. BUG: 292202

--- shell/partcontroller.cpp
+++ shell/partcontroller.cpp
@@ -150,6 +150,7 @@ KTextEditor::Document* PartController::createTextPart(const QString &encoding)
         args.setMimeType( QString::fromLatin1( "text/plain;" ) + encoding );
         doc->setArguments( args );
     }
+    doc->editor()->readConfig();
 
     return doc;
 }
