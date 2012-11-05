ac119617303b5f33d2b96cb34ea12f391e33b556
Pino Toscano <pino@kde.org>
2012-11-05 09:42:00 +0100

    i18n: do not extract messages from 'tests' subdirs

--- projectmanagers/custom-buildsystem/Messages.sh
+++ projectmanagers/custom-buildsystem/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/kdevcustombuildsystem.pot
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg | grep -v '/tests/'` >> rc.cpp
+$XGETTEXT `find . -name \*.cpp | grep -v '/tests/'` -o $podir/kdevcustombuildsystem.pot
 rm -f rc.cpp
