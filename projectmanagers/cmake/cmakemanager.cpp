KDE/kdevelop

commit 227a84561732293d12398051f4b042c600dcf3ab

Cleaned the implementation that reacted to deleted directories
It was too complex and escentially wrong. Now it works properly, it seems.

projectmanagers/cmake/cmakemanager.cpp
@@ -1139,38 +1139,39 @@ void CMakeManager::cleanupToDelete(IProject* p)
     Q_ASSERT(isReloading(p));
     
     for(QSet<QString>::iterator it=m_toDelete.begin(), itEnd=m_toDelete.end(); it!=itEnd; ) {
-        IndexedString url(*it);
-        if(p->fileSet().contains(url)) {
-            deleteAllLater(castToBase(p->itemsForUrl(url.toUrl())));
+        QList<ProjectBaseItem*> items = p->itemsForUrl(*it);
+        if(!items.isEmpty()) {
+            deleteAllLater(items);
             it=m_toDelete.erase(it);
         } else 
             ++it;
     }
     
-    QHash<KUrl, KUrl>::const_iterator it=m_renamed.constBegin(), itEnd=m_renamed.constEnd();
-    for(; it!=itEnd; ++it) {
+    for(QHash<KUrl, KUrl>::iterator it=m_renamed.begin(), itEnd=m_renamed.end(); it!=itEnd; ++it) {
         QList<ProjectBaseItem*> items=p->itemsForUrl(it.key());
-        foreach(ProjectBaseItem* item, items) {
-            if(item->file())
-                emit fileRenamed(it.value(), item->file());
-            else
-                emit folderRenamed(it.value(), item->folder());
+        if(!items.isEmpty()) {
+            foreach(ProjectBaseItem* item, items) {
+                if(item->file())
+                    emit fileRenamed(it.value(), item->file());
+                else
+                    emit folderRenamed(it.value(), item->folder());
+            }
+            it = m_renamed.erase(it);
+        } else {
+            ++it;
         }
     }
 }
 
 void CMakeManager::deletedWatched(const QString& path)
 {
-    KUrl dirurl(path);
-    IProject* p=ICore::self()->projectController()->findProjectForUrl(dirurl);
+    KUrl url(path);
+    IProject* p=ICore::self()->projectController()->findProjectForUrl(url);
     
     if(p && !isReloading(p)) {
-        dirurl.adjustPath(KUrl::AddTrailingSlash);
-        if(p->folder()==dirurl) {
+        if(p->folder().equals(url, KUrl::CompareWithoutTrailingSlash)) {
             ICore::self()->projectController()->closeProject(p);
         } else {
-            KUrl url(path);
-            
             if(url.fileName()=="CMakeLists.txt") {
                 QList<ProjectFolderItem*> folders = p->foldersForUrl(url.upUrl());
                 foreach(ProjectFolderItem* folder, folders) 
@@ -1206,24 +1207,21 @@ void CMakeManager::filesystemBuffererTimeout()
     m_fileSystemChangedBuffer.clear();
 }
 
-void CMakeManager::realDirectoryChanged(const QString& _dir)
+QString addTrailingSlash(const QString& path)
 {
-    KUrl dir(_dir);
-    IProject* p=ICore::self()->projectController()->findProjectForUrl(dir);
+    return (path.isEmpty() || path.endsWith('/')) ? path : path+'/';
+}
+
+void CMakeManager::realDirectoryChanged(const QString& dir)
+{
+    IProject* p=ICore::self()->projectController()->findProjectForUrl(KUrl(dir));
     if(!p || isReloading(p))
         return;
     
-    QList< ProjectBaseItem* > items = p->itemsForUrl(dir);
-    QStringList files;
-    foreach(ProjectBaseItem* it, items) {
-        files.append(it->url().toLocalFile());
-    }
-    foreach(const QString& file, files) {
-        if(!QFile::exists(file))
-            deletedWatched(file);
-        else
-            dirtyFile(file);
-    }
+    if(!QFile::exists(dir))
+        deletedWatched(addTrailingSlash(dir));
+    else
+        dirtyFile(dir);
 }
 
 void CMakeManager::dirtyFile(const QString & dirty)

commit 85a793d0d909c42e50c37ea14550bad286b3be67

Fix crash caused by wrong usage of STL iterators
Before this patch it might have been possible to iterate beyond the
end() of the hash(es).

Also see 'erase'  in http://doc.qt.digia.com/qq/qq12-qt4-iterators.html.

Regression introduced by: 227a8456

BUG: 309442
REVIEW: 107236

BT:
(...)
10 0x00007ff556999af3 in qt_assert_x (where=0x7ff556b6778a "QHash",
what=0x7ff556b67773 "Iterating beyond end()", file=0x7ff556b67740
"/home/krf/devel/src/qt/src/corelib/tools/qhash.cpp", line=293) at
/home/krf/devel/src/qt/src/corelib/global/qglobal.cpp:2021
11 0x00007ff5569cbdfd in QHashData::nextNode (node=0x9b3e7e0) at
/home/krf/devel/src/qt/src/corelib/tools/qhash.cpp:293
12 0x00007ff5255211fc in QHash<KUrl, KUrl>::iterator::operator++
(this=this@entry=0x7fffce6aa370) at
/home/krf/devel/build/qt4.8-shared-debug/include/QtCore/../../../../src/qt/src/corelib/tools/qhash.h:355
13 0x00007ff52551ada4 in CMakeManager::cleanupToDelete
(this=this@entry=0x5cfdda0, p=0x8cedfe0) at
/home/krf/devel/src/kdevelop/projectmanagers/cmake/cmakemanager.cpp:1150
(...)

projectmanagers/cmake/cmakemanager.cpp
@@ -1137,8 +1137,8 @@ bool CMakeManager::isReloading(IProject* p)
 void CMakeManager::cleanupToDelete(IProject* p)
 {
     Q_ASSERT(isReloading(p));
-    
-    for(QSet<QString>::iterator it=m_toDelete.begin(), itEnd=m_toDelete.end(); it!=itEnd; ) {
+
+    for(QSet<QString>::iterator it=m_toDelete.begin(); it!=m_toDelete.end(); ) {
         QList<ProjectBaseItem*> items = p->itemsForUrl(*it);
         if(!items.isEmpty()) {
             deleteAllLater(items);
@@ -1146,8 +1146,8 @@ void CMakeManager::cleanupToDelete(IProject* p)
         } else 
             ++it;
     }
-    
-    for(QHash<KUrl, KUrl>::iterator it=m_renamed.begin(), itEnd=m_renamed.end(); it!=itEnd; ++it) {
+
+    for(QHash<KUrl, KUrl>::iterator it=m_renamed.begin(); it != m_renamed.end(); ) {
         QList<ProjectBaseItem*> items=p->itemsForUrl(it.key());
         if(!items.isEmpty()) {
             foreach(ProjectBaseItem* item, items) {