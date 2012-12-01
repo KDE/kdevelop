96f1d015f3ebc0e9927a4f4361085ac72c538e3e
Kevin Funk <kevin@kfunk.org>
2012-12-01 14:54:10 +0100

    CMake: Fix path handling bugs
    KUrl::isRelativeUrl() *always* returns true even in case the path is a
    absolute path (see apidocs). We want QUrl::isRelative() here.
    
    Before this patch it was possible that even absolute paths were modified
    and folder->url() being prepended.

--- projectmanagers/cmake/cmakemanager.cpp
+++ projectmanagers/cmake/cmakemanager.cpp
@@ -929,17 +929,19 @@ QList<KDevelop::ProjectFolderItem*> CMakeManager::parse( KDevelop::ProjectFolder
                 if(sFile.startsWith("#[") || sFile.isEmpty())
                     continue;
 
-                KUrl sourceFile;
-                if(KUrl::isRelativeUrl(sFile)) {
+                KUrl sourceFile(sFile);
+
+                // important: we want the behavior of KUrl::isRelative(), *not* KUrl::isRelativeUrl()
+                if(sourceFile.isRelative()) {
                     sourceFile = folder->url();
                     sourceFile.addPath( sFile );
                     if(!QFile::exists(sourceFile.toLocalFile())) {
                         sourceFile = buildDirectory(folder);
                         sourceFile.addPath( sFile );
                     }
-                } else
-                    sourceFile = sFile;
+                }
                 sourceFile.cleanPath();
+
                 tfiles += sourceFile;
                 kDebug(9042) << "..........Adding:" << sourceFile << sFile << folder->url();
             }
