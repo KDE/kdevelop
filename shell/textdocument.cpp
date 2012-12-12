KDE/kdevelop

commit d77c94c1402916f8f00b02104f71187330548ecd

Simplify the code that initializes the text document cursor
We were replicating the logic for checking if the cursor is initialized,
now we have it all in one place.

shell/textdocument.cpp
@@ -671,8 +671,16 @@ QString KDevelop::TextView::viewState() const
     }
 }
 
-void KDevelop::TextView::setInitialRange(const KTextEditor::Range& range) {
-    d->initialRange = range;
+void KDevelop::TextView::setInitialRange(const KTextEditor::Range& range)
+{
+    if(d->editor->isInitialized()) {
+        if (range.isEmpty())
+            d->editor->editorView()->setCursorPosition( range.start() );
+        else
+            d->editor->editorView()->setSelection( range );
+    } else {
+        d->initialRange = range;
+    }
 }
 
 KTextEditor::Range KDevelop::TextView::initialRange() const
@@ -685,17 +693,10 @@ void KDevelop::TextView::setState(const QString & state)
     static QRegExp reCursor("Cursor=([\\d]+),([\\d]+)");
     static QRegExp reSelection("Selection=([\\d]+),([\\d]+),([\\d]+),([\\d]+)");
     if (reCursor.exactMatch(state)) {
-        if (d->editor && d->isInitialized())
-            d->editor->editorView()->setCursorPosition(KTextEditor::Cursor(reCursor.cap(1).toInt(), reCursor.cap(2).toInt()));
-        else
-            setInitialRange(KTextEditor::Range(KTextEditor::Cursor(reCursor.cap(1).toInt(), reCursor.cap(2).toInt()), 0));
+        setInitialRange(KTextEditor::Range(KTextEditor::Cursor(reCursor.cap(1).toInt(), reCursor.cap(2).toInt()), 0));
     } else if (reSelection.exactMatch(state)) {
         KTextEditor::Range range(reSelection.cap(1).toInt(), reSelection.cap(2).toInt(), reSelection.cap(3).toInt(), reSelection.cap(4).toInt());
-        if (d->editor && d->isInitialized()) {
-            d->editor->editorView()->setCursorPosition(range.end());
-            d->editor->editorView()->setSelection(range);
-        } else
-            setInitialRange(range);
+        setInitialRange(range);
     }
 }
 
@@ -811,6 +812,11 @@ KTextEditor::View* KDevelop::TextEditorWidget::editorView()
     if(!d->view)
         initialize();
     return d->view;
+}
+
+bool KDevelop::TextEditorWidget::isInitialized() const
+{
+    return d->view!=0;
 }
 
 #include "textdocument.moc"

shell/documentcontroller.cpp
@@ -378,13 +378,8 @@ struct DocumentControllerPrivate {
             }
             
             KDevelop::TextView* textView = dynamic_cast<KDevelop::TextView*>(partView);
-            if(textView && textView->textView()) {
+            if(textView) {
                 applyRange = false;
-                if (range.isEmpty())
-                    doc->setCursorPosition( range.start() );
-                else
-                    doc->setTextSelection( range );
-            }else if(textView) {
                 textView->setInitialRange(range);
             }
             

shell/textdocument.h
@@ -126,6 +126,7 @@ public:
     KTextEditor::View* editorView();
 
     QString status() const;
+    bool isInitialized() const;
 
 Q_SIGNALS:
     void statusChanged();

commit d0bc25d31e3f9c7a2c0ef88aa1d3be73b660a197

Fix activating documents with a non-empty range
This was broken with commit d77c94c1402916f8f00b02104f71187330548ecd which
replaced doc->setTextSelection with textView->setInitialRange in
documentcontroller.cpp without taking the extra logic from
TextDocument::setTextSelection into account. The setTextSelection function
not only sets the selection, but also sets the cursor which causes the
Kate view to scroll to the selected range. Without that the selection would
be set, but the view would not scroll leaving the user to think that the
activation didn't happen.

This affects at least the grepview plugin which tries to select the found
string in the occurrence that has been clicked upon.

shell/textdocument.cpp
@@ -67,6 +67,19 @@ namespace KDevelop {
 
 const int MAX_DOC_SETTINGS = 20;
 
+// This sets cursor position and selection on the view to the given
+// range. Selection is set only for non-empty ranges
+// Factored into a function since its needed in 3 places already
+static void selectAndReveal( KTextEditor::View* view, const KTextEditor::Range& range ) {
+    Q_ASSERT(view);
+    if (range.isValid()) {
+        view->setCursorPosition(range.start());
+        if (!range.isEmpty()) {
+            view->setSelection(range);
+        }
+    }
+}
+
 struct TextDocumentPrivate {
     TextDocumentPrivate(TextDocument *textDocument)
         : encoding(""), m_textDocument(textDocument)
@@ -594,8 +607,7 @@ void TextDocument::setTextSelection(const KTextEditor::Range &range)
     KTextEditor::View *view = d->document->activeView();
 
     if (view) {
-        view->setCursorPosition(range.start());
-        view->setSelection( range );
+        selectAndReveal(view, range);
     }
 }
 
@@ -675,10 +687,7 @@ QString KDevelop::TextView::viewState() const
 void KDevelop::TextView::setInitialRange(const KTextEditor::Range& range)
 {
     if(d->editor && d->editor->isInitialized()) {
-        if (range.isEmpty())
-            d->editor->editorView()->setCursorPosition( range.start() );
-        else
-            d->editor->editorView()->setSelection( range );
+        selectAndReveal(d->editor->editorView(), range);
     } else {
         d->initialRange = range;
     }
@@ -764,14 +773,7 @@ void KDevelop::TextEditorWidget::initialize()
     TextDocument* doc = static_cast<TextDocument*>(d->textView->document());
     KTextEditor::View* view = qobject_cast<KTextEditor::View*>(doc->createViewWidget(this));
     KTextEditor::Range ir = d->textView->initialRange();
-    if(ir.isValid()) {
-        if(ir.isEmpty())
-            view->setCursorPosition(ir.start());
-        else {
-            view->setCursorPosition(ir.end());
-            view->setSelection(ir);
-        }
-    }
+    selectAndReveal(view, ir);
     setEditorView(view);
 }
 