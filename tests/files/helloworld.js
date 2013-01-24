d6401f58bcfa3f480a07b59167a7142ff6532d96
Milian Wolff <mail@milianw.de>
2013-01-24 23:53:23 +0100

    Fix json test, extend it with expected failures

--- tests/files/helloworld.js
+++ tests/files/helloworld.js
@@ -1,14 +1,27 @@
 /**
- * "type" : { "toString"  : "void" },
- * "useCount" : 0
+ * "type" : { "toString"  : "function void ()" },
+ * "returnType" : { "toString"  : "void" }
  */
 function helloWorld()
 {
   alert("Hello World!");
 }
 
+/**
+ * "EXPECT_FAIL" : { "type" : { "toString" : "int" } }
+ */
 var a = 5;
+
+/**
+ * "EXPECT_FAIL" : {
+ *   "type" : { "toString" : "function String ()" },
+ *   "returnType" : { "toString" : "String" }
+ * }
+ */
 function testVariables()
 {
+ /**
+  * "EXPECT_FAIL" : { "type" : { "toString" : "String" } }
+  */
   var b = "some text";
 }
