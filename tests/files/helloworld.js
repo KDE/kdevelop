4eb2192d7504f9156c697366c11dc3abb53999b8
Milian Wolff <mail@milianw.de>
2013-01-25 00:05:16 +0100

    Fix json test: use proper syntax for EXPECT_FAIL

--- tests/files/helloworld.js
+++ tests/files/helloworld.js
@@ -8,29 +8,28 @@ function helloWorld()
 }
 
 /**
- * "EXPECT_FAIL" : { "type" : { "toString" : "int" } }
+ * "EXPECT_FAIL" : { "type" : "int type is not properly deduced" },
+ * "type" : { "toString" : "int" }
  */
 var a = 5;
 
 /**
- * "EXPECT_FAIL" : {
- *   "type" : { "toString" : "function String ()" },
- *   "returnType" : { "toString" : "String" }
- * }
+ * "EXPECT_FAIL" : { "returnType" : "return type is not properly deduced" },
+ * "returnType" : { "toString" : "String" }
  */
 function testVariables()
 {
  /**
-  * "EXPECT_FAIL" : { "type" : { "toString" : "String" } }
+  * "EXPECT_FAIL" : { "type" : "string type is not properly deduced" },
+  * "type" : { "toString" : "String" }
   */
   var b = "some text";
+  return b;
 }
 
 /**
- * "EXPECT_FAIL" : {
- *   "type" : { "toString" : "function mixed (mixed)" },
- *   "returnType" : { "toString" : "mixed" }
- * }
+ * "EXPECT_FAIL" : { "type" : "neither arg type nor ret type is properly deduced" },
+ * "type": { "toString" : "function mixed (mixed)" }
  */
 function testReturnMixedArg(arg)
 {
