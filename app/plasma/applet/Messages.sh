7913a1110a914c62e19a313db129b35849bb0199
Lasse Liehu <lasse.liehu@gmail.com>
2013-02-26 21:47:44 +0200

    Fix i18n catalog name of the KDevelop sessions applet
    REVIEW: 109174
    (cherry picked from commit df723ad6ec7c9a5099698d28bb712b23e726fa33)

--- app/plasma/applet/Messages.sh
+++ app/plasma/applet/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 # only need to change the name of the applet
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/kdevelopsessions.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_kdevelopsessions.pot
