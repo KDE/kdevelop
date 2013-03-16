fa2f936dc9fa01c062c70dcdc96cb0cf604896ff
Kevin Funk <kevin@kfunk.org>
2013-03-16 22:31:41 +0100

    Set title on refactoring dialog

--- languages/cpp/codegen/progressdialogs.cpp
+++ languages/cpp/codegen/progressdialogs.cpp
@@ -58,6 +58,8 @@ RefactoringProgressDialog::RefactoringProgressDialog(QString action) {
   
   resize(380, 90);//layoutV->sizeHint().height());
   setLayout(layoutV);
+
+  setWindowTitle(tr("Refactoring"));
   
   connect(m_cancelButton, SIGNAL(clicked(bool)), this, SLOT(reject()));
 }
