788f59b941a96a3b0239cf08e628d11e42b12fb3
Milian Wolff <mail@milianw.de>
2013-03-22 10:54:11 +0100

    Add license header to test files.

--- languages/cpp/cppduchain/tests/data/testADLEllipsis.cpp
+++ languages/cpp/cppduchain/tests/data/testADLEllipsis.cpp
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 void test(double) {}
 void test(...) {}
 void test(char) {}
--- languages/cpp/cppduchain/tests/data/testDecltypeTypedef.cpp
+++ languages/cpp/cppduchain/tests/data/testDecltypeTypedef.cpp
@@ -1,3 +1,6 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
 template<typename U> U foo();
 
 template<typename T>
