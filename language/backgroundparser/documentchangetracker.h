9ea4d058b40b0eec1a4c3a70ea8ee3486087cde3
Milian Wolff <mail@milianw.de>
2013-05-24 12:01:53 +0100

    Cleanup (rtrim) and constify RevisionLockerAndClearer API.

--- language/backgroundparser/documentchangetracker.h
+++ language/backgroundparser/documentchangetracker.h
@@ -47,7 +47,7 @@ class DocumentChangeTracker;
 
 class RevisionLockerAndClearerPrivate;
 
-/** 
+/**
  * Helper class that locks a revision, and clears it on its destruction within the foreground thread.
  * Just delete it using deleteLater().
  * */
@@ -56,12 +56,12 @@ public:
     typedef KSharedPtr<RevisionLockerAndClearer> Ptr;
 
     ~RevisionLockerAndClearer();
-    
+
     /**
      * Returns the revision number
      * */
     qint64 revision() const;
-    
+
     /**
      * Whether the revision is still being held. It may have been lost due to document-reloads,
      * in which case the revision must not be used.
@@ -70,47 +70,52 @@ public:
     /**
      * Transform a range from this document revision to the given @p to.
      * */
-    RangeInRevision transformToRevision(const RangeInRevision& range, const Ptr& to);
+    RangeInRevision transformToRevision(const RangeInRevision& range, const Ptr& to) const;
+
     /**
      * Transform a cursor from this document revision to the given @p to.
      * If a zero target revision is given, the transformation is done to the current document revision.
      * */
-    CursorInRevision transformToRevision(const CursorInRevision& cursor, const Ptr& to, KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert);
-    
+    CursorInRevision transformToRevision(const CursorInRevision& cursor, const Ptr& to,
+                                         KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert) const;
+
     /**
       * Transforms the given range from this revision into the current revision.
       */
-    SimpleRange transformToCurrentRevision(const RangeInRevision& range);
-    
+    SimpleRange transformToCurrentRevision(const RangeInRevision& range) const;
+
     /**
     * Transforms the given cursor from this revision into the current revision.
     */
-    SimpleCursor transformToCurrentRevision(const CursorInRevision& cursor, KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert);
-    
+    SimpleCursor transformToCurrentRevision(const CursorInRevision& cursor,
+                                            KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert) const;
+
     /**
      * Transform ranges from the given document revision @p from to the this one.
      * If a zero @p from revision is given, the transformation is done from the current document revision.
      * */
-    RangeInRevision transformFromRevision(const RangeInRevision& range, const Ptr& from = Ptr());
+    RangeInRevision transformFromRevision(const RangeInRevision& range, const Ptr& from = Ptr()) const;
     /**
      * Transform ranges from the given document revision @p from to the this one.
      * If a zero @p from revision is given, the transformation is done from the current document revision.
      * */
-    CursorInRevision transformFromRevision(const CursorInRevision& cursor, const Ptr& from = Ptr(), KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert);
-    
+    CursorInRevision transformFromRevision(const CursorInRevision& cursor, const Ptr& from = Ptr(),
+                                           KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert) const;
+
     /**
     * Transforms the given range from the current revision into this revision.
     */
-    RangeInRevision transformFromCurrentRevision(const SimpleRange& range);
-    
+    RangeInRevision transformFromCurrentRevision(const SimpleRange& range) const;
+
     /**
     * Transforms the given cursor from the current revision into this revision.
     */
-    CursorInRevision transformFromCurrentRevision(const SimpleCursor& cursor, KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert);
-    
+    CursorInRevision transformFromCurrentRevision(const SimpleCursor& cursor,
+                                                  KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert) const;
+
 private:
     friend class DocumentChangeTracker;
-    
+
     RevisionLockerAndClearerPrivate* m_p;
 };
 
@@ -145,7 +150,7 @@ public:
      * not-yet-finished statements, like for example for-loops for parsing.
      * */
     virtual QList<QPair<KDevelop::SimpleRange, QString> > completions() const;
-    
+
     /**
      * Resets the tracking to the current revision.
      * */
@@ -153,11 +158,11 @@ public:
 
     /**
      * Returns the document revision at which reset() was called last.
-     * 
+     *
      * The revision is being locked by the tracker in MovingInterface,
      * it will be unlocked as soon as reset() is called, so if you want to use
      * the revision afterwards, you have to lock it before calling reset.
-     * 
+     *
      * zero is returned if the revisions were invalidated after the last call.
      * */
     RevisionReference revisionAtLastReset() const;
@@ -166,55 +171,55 @@ public:
      * Returns the document text at the last reset
      * */
     QString textAtLastReset() const;
-    
+
     /**
      * Returns the current revision (which is not locked by the tracker)
      * */
     RevisionReference currentRevision();
-    
+
     /**
      * Returns the range that was changed since the last reset
      * */
     virtual KTextEditor::Range changedRange() const;
-    
+
     /**
      * Whether the changes that happened since the last reset are significant enough to require an update
      * */
     virtual bool needUpdate() const;
-    
+
     /**
      * Returns the tracked document
      **/
     KTextEditor::Document* document() const;
-    
+
     KTextEditor::MovingInterface* documentMovingInterface() const;
-    
+
     /**
      * Returns the revision object which locks the revision representing the on-disk state.
      * Returns a zero object if the file is not on disk.
      * */
     RevisionReference diskRevision() const;
-    
+
     /**
      * Returns whether the given revision is being current held, so that it can be used
      * for transformations in MovingInterface
      * */
     bool holdingRevision(qint64 revision) const;
-    
+
     /**
      * Use this fuction to acquire a revision. As long as the returned object is stored somewhere,
-     * the revision can be used for transformations in MovingInterface, and especially for 
+     * the revision can be used for transformations in MovingInterface, and especially for
      * DocumentChangeTracker::transformBetweenRevisions.
-     * 
+     *
      * Returns a zero revision object if the revision could not be acquired (it wasn't held).
      * */
     RevisionReference acquireRevision(qint64 revision);
-    
+
     /**
      * Safely maps the given range between the two given revisions.
      * The mapping is only done if both the from- and to- revision are held,
      * else the original range is returned.
-     * 
+     *
      * @warning: Make sure that you actually hold the referenced revisions, else no transformation will be done.
      * @note It is much less error-prone to use RevisionReference->transformToRevision() and RevisionReference->transformFromRevision() directly.
      * */
@@ -228,7 +233,7 @@ public:
     RangeInRevision transformToRevision(SimpleRange range, qint64 toRevision) const;
     /// Transform the cursor from the current revision into the given one
     CursorInRevision transformToRevision(SimpleCursor cursor, qint64 toRevision, KTextEditor::MovingCursor::InsertBehavior behavior = KTextEditor::MovingCursor::StayOnInsert) const;
-    
+
 protected:
     QString m_textAtLastReset;
     RevisionReference m_revisionAtLastReset;
@@ -236,11 +241,11 @@ protected:
     QString m_currentCleanedInsertion;
     KTextEditor::Cursor m_lastInsertionPosition;
     KTextEditor::MovingRange* m_changedRange;
-    
+
     KTextEditor::Document* m_document;
     KTextEditor::MovingInterface* m_moving;
     KDevelop::IndexedString m_url;
-    
+
     void updateChangedRange(KTextEditor::Range changed);
 public slots:
     virtual void textInserted( KTextEditor::Document*,KTextEditor::Range );
@@ -250,13 +255,13 @@ public slots:
     void aboutToInvalidateMovingInterfaceContent ( KTextEditor::Document* document );
     void documentSavedOrUploaded(KTextEditor::Document*,bool);
 private:
-    
+
     virtual bool checkMergeTokens(const KTextEditor::Range& range, QString oldText, QString newText);
-    
+
     friend class RevisionLockerAndClearerPrivate;
     void lockRevision(qint64 revision);
     void unlockRevision(qint64 revision);
-    
+
     QMap<qint64, int> m_revisionLocks;
     ILanguageSupport::WhitespaceSensitivity m_whitespaceSensitivity;
 };
