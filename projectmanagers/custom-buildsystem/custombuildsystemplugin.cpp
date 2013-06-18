5f67848030bea61c18512f53ba2b0d80d304f20a
Andreas Pakulat <apaku@gmx.de>
2013-06-18 15:04:03 +0200

    Also synchronize the initial group
    Lets see wether this helps by making sure the base group is valid when
    fetching it.

--- projectmanagers/custom-buildsystem/custombuildsystemplugin.cpp
+++ projectmanagers/custom-buildsystem/custombuildsystemplugin.cpp
@@ -188,13 +188,16 @@ QList<ProjectTargetItem*> CustomBuildSystem::targets( ProjectFolderItem* ) const
 
 KConfigGroup CustomBuildSystem::configuration( IProject* project ) const
 {
+    // The syncs are ugly and actually not necessary as far as support from
+    // KConfig* goes, but when using a debug-compiled kdelibs KConfig* has
+    // various (IMO) incorrect assert()'s when trying to read entries from a
+    // KConfigGroup that does not exist in the config files. There does not
+    // seem to be any problem doing so with a non-debug kdelibs, i.e. KConfig*
+    // can handle this case just fine. So someone (who can patch kdelibs)
+    // should probably remove those useless assert()'s.
     KConfigGroup grp = project->projectConfiguration()->group( ConfigConstants::customBuildSystemGroup );
+    grp.sync();
     KConfigGroup currentConfig = grp.group( grp.readEntry( ConfigConstants::currentConfigKey ) );
-    // This is ugly and actually not necessary as far as support from KConfig* goes, but when using a debug-compiled
-    // kdelibs KConfig* has various (IMO) incorrect assert()'s when trying to read entries from a KConfigGroup
-    // that does not exist in the config files. There does not seem to be any problem doing so with a non-debug
-    // kdelibs, i.e. KConfig* can handle this case just fine. So someone (who can patch kdelibs) should probably remove
-    // those useless assert()'s.
     currentConfig.sync();
     return currentConfig;
 }
