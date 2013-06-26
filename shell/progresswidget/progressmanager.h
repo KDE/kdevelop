8b882d608a8a668f4a9767ab66aabe24c83e1abb
Aleix Pol <aleixpol@kde.org>
2013-06-26 02:16:08 +0200

    Relicense the files copied from kdepim/libkdepim
    Use LGPL 2+, following the KDE Licensing Policies.
    
    CCMAIL: adam@kde.org
    CCMAIL: faure@kde.org

--- shell/progresswidget/progressmanager.h
+++ shell/progresswidget/progressmanager.h
@@ -1,30 +1,24 @@
-/*
-  progressmanager.h
-
-  This file is part of libkdepim.
-
-  Copyright (C) 2004 Till Adam <adam@kde.org>
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Library General Public
-  License as published by the Free Software Foundation; either
-  version 2 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Library General Public License for more details.
-
-  You should have received a copy of the GNU Library General Public License
-  along with this library; see the file COPYING.LIB.  If not, write to
-  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-  Boston, MA 02110-1301, USA.
-*/
+/***************************************************************************
+ *   (C) 2004 Till Adam <adam@kde.org>                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
 
 #ifndef KDEVPLATFORM_PROGRESSMANAGER_H
 #define KDEVPLATFORM_PROGRESSMANAGER_H
-
-
 
 #include <QObject>
 #include <QString>
--- shell/progresswidget/statusbarprogresswidget.h
+++ shell/progresswidget/statusbarprogresswidget.h
@@ -1,37 +1,27 @@
 #ifndef KDEVPLATFORM_STATUSBARPROGRESSWIDGET_H
 #define KDEVPLATFORM_STATUSBARPROGRESSWIDGET_H
-/*
-  statusbarprogresswidget.h
+/***************************************************************************
+ *      (C) 2004 Till Adam <adam@kde.org>                                  *
+ *              Don Sanders                                                *
+ *              David Faure <dfaure@kde.org>                               *
+ *      Copyright 2004 David Faure <faure@kde.org>                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Library General Public License as       *
+ *   published by the Free Software Foundation; either version 2 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU Library General Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
 
-  (C) 2004 Till Adam <adam@kde.org>
-           Don Sanders
-           David Faure <dfaure@kde.org>
-  Copyright 2004 David Faure <faure@kde.org>
-
-  KMail is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License, version 2
-  or above, as published by the Free Software Foundation.
-
-  KMail is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-
-  In addition, as a special exception, the copyright holders give
-  permission to link the code of this program with any edition of
-  the Qt library by Trolltech AS, Norway (or with modified versions
-  of Qt that use the same license as Qt), and distribute linked
-  combinations including the two.  You must obey the GNU General
-  Public License in all respects for all of the code used other than
-  Qt.  If you modify this file, you may extend this exception to
-  your version of the file, but you are not obligated to do so.  If
-  you do not wish to do so, delete this exception statement from
-  your version.
-*/
 /**
   *  A specialized progress widget class, heavily based on
   *  kio_littleprogress_dlg (it looks similar)
