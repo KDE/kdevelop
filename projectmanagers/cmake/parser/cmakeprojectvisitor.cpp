5e487399601a73bb28ab322d1713dc29d2e39418
Aleix Pol <aleixpol@kde.org>
2013-12-13 03:03:39 +0100

    less code
    what was I thinking?

--- projectmanagers/cmake/parser/cmakeprojectvisitor.cpp
+++ projectmanagers/cmake/parser/cmakeprojectvisitor.cpp
@@ -2357,7 +2357,7 @@ int CMakeProjectVisitor::walk(const CMakeFileContent & fc, int line, bool isClea
             KSharedPtr<Problem> p(new Problem);
             p->setDescription(i18n("%1 is a deprecated command and should not be used", func.name));
             p->setRange(it->nameRange());
-            p->setFinalLocation(DocumentRange(url, KDevelop::RangeInRevision(it->nameRange().start, it->nameRange().end).castToSimpleRange()));
+            p->setFinalLocation(DocumentRange(url, it->nameRange().castToSimpleRange()));
             m_topctx->addProblem(p);
         }
         element->setContent(fc, line);
