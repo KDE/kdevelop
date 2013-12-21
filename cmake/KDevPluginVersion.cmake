d2fabad65c67cda33d06ba091f81cd2f7be99fca
Christoph Thielecke <christoph.thielecke@gmx.de>
2013-12-21 13:06:27 +0100

    detect KDEVELOP_PLUGIN_VERSION too if its in ipluginversion.h

--- cmake/KDevPluginVersion.cmake
+++ cmake/KDevPluginVersion.cmake
@@ -1,10 +1,19 @@
 find_file( KDEV_IPLUGIN_H kdevplatform/interfaces/iplugin.h )
-
 if(KDEV_IPLUGIN_H)
         file(STRINGS ${KDEV_IPLUGIN_H} KDEV_PLUGIN_VERSION
                 REGEX "#define KDEVELOP_PLUGIN_VERSION.*" )
-        string(REGEX REPLACE ".*\\ ([0-9]+)"
-                "\\1" KDEV_PLUGIN_VERSION ${KDEV_PLUGIN_VERSION})
+        if(KDEV_PLUGIN_VERSION)
+            string(REGEX REPLACE ".*\\ ([0-9]+)"
+                    "\\1" KDEV_PLUGIN_VERSION ${KDEV_PLUGIN_VERSION})
+        else(${KDEV_PLUGIN_VERSION})
+            find_file( KDEV_IPLUGINVERSION_H kdevplatform/interfaces/ipluginversion.h )
+            if(KDEV_IPLUGINVERSION_H)
+                    file(STRINGS ${KDEV_IPLUGINVERSION_H} KDEV_PLUGIN_VERSION
+                            REGEX "#define KDEVELOP_PLUGIN_VERSION.*" )
+                    string(REGEX REPLACE ".*\\ ([0-9]+)"
+                            "\\1" KDEV_PLUGIN_VERSION ${KDEV_PLUGIN_VERSION})
+            endif(KDEV_IPLUGINVERSION_H)
+        endif(KDEV_PLUGIN_VERSION)
 endif(KDEV_IPLUGIN_H)
 
 if(NOT DEFINED KDEV_PLUGIN_VERSION)
