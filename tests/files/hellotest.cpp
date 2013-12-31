3d7f8d099d03437b19953c647f28be2421efb768
Olivier JG <olivier.jg@gmail.com>
2013-12-31 13:09:58 +0100

    More json duchain testing

--- tests/files/hellotest.cpp
+++ tests/files/hellotest.cpp
@@ -1,8 +1,57 @@
 /**
- * "toString" : "int () main",
+ * "identifier" : "main",
  * "internalContext" : { "type" : "Other" }
  */
 int main()
 {
     return 0;
+}
+
+/**
+ * "identifier" : "foo",
+ * "internalContext" : { "type" : "Class" }
+ */
+class foo
+{
+
+};
+
+/**
+ * "identifier" : "bar",
+ * "internalContext" : { "type" : "Namespace" }
+ */
+namespace bar
+{
+    /**
+     * "identifier" : "foo"
+     */
+    void foo();
+
+    /**
+     * "identifier" : "foostruct",
+     * "internalContext" : { "type" : "Class" }
+     */
+    struct foostruct
+    {
+        /**
+         * "identifier" : "fooenum",
+         * "internalContext" : { "type" : "Enum" }
+         */
+        enum fooenum
+        {
+        };
+
+        /**
+         * "identifier" : "foomethod"
+         */
+        int foomethod();
+
+        /**
+         * "identifier" : "deffoomethod",
+         * "internalContext" : { "type" : "Other" }
+         */
+        int deffoomethod()
+        {
+        }
+    };
 }
