66b837e83acd2837ee00f16730e0570cd807561e
Milian Wolff <mail@milianw.de>
2014-01-02 19:04:31 +0100

    Add default include paths from clang.
    This is required on my system in order to find e.g. stddef.h

--- clangparsejob.cpp
+++ clangparsejob.cpp
@@ -42,8 +42,83 @@
 #include "clanglanguagesupport.h"
 
 #include <QReadLocker>
+#include <QProcess>
 
 using namespace KDevelop;
+
+namespace {
+// TODO: investigate why this is required to find e.g. stddef.h
+KUrl::List defaultIncludes()
+{
+    static KUrl::List includePaths;
+
+    if (!includePaths.isEmpty()) {
+        return includePaths;
+    }
+
+    QProcess proc;
+    proc.setProcessChannelMode(QProcess::MergedChannels);
+
+    // The following command will spit out a bnuch of information we don't care
+    // about before spitting out the include paths.  The parts we care about
+    // look like this:
+    // #include "..." search starts here:
+    // #include <...> search starts here:
+    //  /usr/lib/gcc/i486-linux-gnu/4.1.2/../../../../include/c++/4.1.2
+    //  /usr/lib/gcc/i486-linux-gnu/4.1.2/../../../../include/c++/4.1.2/i486-linux-gnu
+    //  /usr/lib/gcc/i486-linux-gnu/4.1.2/../../../../include/c++/4.1.2/backward
+    //  /usr/local/include
+    //  /usr/lib/gcc/i486-linux-gnu/4.1.2/include
+    //  /usr/include
+    // End of search list.
+    proc.start("clang++", {"-std=c++11", "-xc++", "-E", "-v", "/dev/null"});
+    if (!proc.waitForStarted(1000) || !proc.waitForFinished(1000)) {
+        return {};
+    }
+
+    // We'll use the following constants to know what we're currently parsing.
+    enum Status {
+        Initial,
+        FirstSearch,
+        Includes,
+        Finished
+    };
+    Status mode = Initial;
+
+    foreach(const QString& line, QString::fromLocal8Bit(proc.readAllStandardOutput()).split('\n')) {
+        switch (mode) {
+            case Initial:
+                if (line.indexOf("#include \"...\"") != -1) {
+                    mode = FirstSearch;
+                }
+                break;
+            case FirstSearch:
+                if (line.indexOf("#include <...>") != -1) {
+                    mode = Includes;
+                    break;
+                }
+            case Includes:
+                //if (!line.indexOf(QDir::separator()) == -1 && line != "." ) {
+                //Detect the include-paths by the first space that is prepended. Reason: The list may contain relative paths like "."
+                if (!line.startsWith(" ") ) {
+                    // We've reached the end of the list.
+                    mode = Finished;
+                } else {
+                    // This is an include path, add it to the list.
+                    includePaths << QDir::cleanPath(line.trimmed());
+                }
+                break;
+            default:
+                break;
+        }
+        if (mode == Finished) {
+            break;
+        }
+    }
+
+    return includePaths;
+}
+}
 
 ClangParseJob::ClangParseJob(const IndexedString& url, ILanguageSupport* languageSupport)
 : ParseJob(url, languageSupport)
@@ -54,6 +129,8 @@ ClangParseJob::ClangParseJob(const IndexedString& url, ILanguageSupport* languag
         m_includes = bsm->includeDirectories(item);
         m_defines = bsm->defines(item);
     }
+
+    m_includes += defaultIncludes();
 }
 
 ClangLanguageSupport* ClangParseJob::clang() const
