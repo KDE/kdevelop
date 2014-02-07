3b72b6a5ee2efce27ae33fa9a8326c2f03e91417
Alex Richardson <arichardson.kde@gmail.com>
2014-02-07 16:38:35 +0100

    Don't include QJson header
    REVIEW: 115539

--- tests/json/declarationvalidator.cpp
+++ tests/json/declarationvalidator.cpp
@@ -15,7 +15,6 @@
    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.
 */
-#include <qjson/parser.h>
 #include "language/duchain/declaration.h"
 #include "declarationvalidator.h"
 #include "testsuite.h"
