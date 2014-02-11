be12a65310c8d3a0a54a3942284c6e1f674cd2ac
Kevin Funk <kfunk@kde.org>
2014-02-11 16:19:58 +0100

    Update my copyright info

--- util/autoorientedsplitter.h
+++ util/autoorientedsplitter.h
@@ -1,5 +1,5 @@
 /*  This file is part of KDevelop
-    Copyright 2014 Kevin Funk <kevin@kfunk.org>
+    Copyright 2014 Kevin Funk <kfunk@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
