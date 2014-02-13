b9a819c649ac80d3a1afb4c460c168914f66d057
Kevin Funk <kfunk@kde.org>
2014-02-13 21:36:17 +0100

    Remove more dead code

--- languages/cpp/examples/baz.h
+++
@@ -1,4 +0,0 @@
-#ifndef CPP_EXAMPLES_BAZ_H
-#define CPP_EXAMPLES_BAZ_H
-class Baz { public: void baz(); };
-#endif // CPP_EXAMPLES_BAZ_H
--- languages/cpp/examples/foo.h
+++
@@ -1,4 +0,0 @@
-#ifndef CPP_EXAMPLES_FOO_H
-#define CPP_EXAMPLES_FOO_H
-class Foo { public: void foo(); };
-#endif // CPP_EXAMPLES_FOO_H
