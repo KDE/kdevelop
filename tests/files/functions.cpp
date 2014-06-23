3d8e449e1c13f07ad6d19ff152d51172e31e8206
Kevin Funk <kfunk@kde.org>
2014-06-23 17:45:22 +0200

    Add JSON test for definition<->declaration

--- tests/files/functions.cpp
+++ tests/files/functions.cpp
@@ -18,3 +18,10 @@ class foo
 void bar2() {}
 /// "isVirtual" : false
 void bar1();
+
+/// "definition" : { "internalContext" : { "type" : "Function", "localDeclarationCount" : 1 } }
+void someFunc(int a);
+/// "declaration" : { "null" : true }
+void someFunc(int a, int f) { }
+/// "declaration" : { "internalContext" : { "type" : "Function", "localDeclarationCount" : 1 } }
+void someFunc(int a) { }
