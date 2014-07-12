45793d3d8f76d774cdd841fda659062b2da6c53b
Sergey Kalinichev <kalinichev.so.0@gmail.com>
2014-07-12 16:41:14 +0400

    Add a simple test for CustomMake. Also add missing file.

---
+++ languages/plugins/custom-definesandincludes/makefileresolver/tests/test_custommake.cpp
@@ -0,0 +1,75 @@
+/*
+ * This file is part of KDevelop
+ *
+ * Copyright 2014 Sergey Kalinichev <kalinichev.so.0@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "test_custommake.h"
+
+#include <QtTest/QtTest>
+
+#include <qtest_kde.h>
+
+#include <QFile>
+#include <QTextStream>
+#include <KDebug>
+#include <KTempDir>
+
+#include <tests/autotestshell.h>
+#include <tests/testcore.h>
+
+#include "../makefileresolver.h"
+
+namespace {
+void createFile( QFile& file )
+{
+    file.remove();
+    if ( !file.open( QIODevice::ReadWrite ) ) {
+        kFatal() << "Cannot create the file " << file.fileName();
+    }
+}
+}
+
+void TestCustomMake::initTestCase() {
+    KDevelop::AutoTestShell::init();
+    KDevelop::TestCore::initialize(KDevelop::Core::NoUi);
+}
+
+void TestCustomMake::cleanupTestCase() {
+    KDevelop::TestCore::shutdown();
+}
+
+void TestCustomMake::testIncludeDirectories() {
+    KTempDir tempDir;
+    {
+        QFile file( tempDir.name() + "Makefile" );
+        createFile( file );
+        QFile testfile( tempDir.name() + "testfile.cpp" );
+        createFile(testfile);
+        QTextStream stream1( &file );
+        stream1 << "testfile.o:\n\t g++ testfile.cpp -I/testFile1 -I/testFile2 -o testfile";
+    }
+
+    MakeFileResolver mf;
+    auto result = mf.resolveIncludePath(tempDir.name() + "testfile.cpp");
+    QVERIFY(result.paths.contains("/testFile1"));
+    QVERIFY(result.paths.contains("/testFile2"));
+    QCOMPARE(result.paths.size(), 2);
+}
+
+QTEST_KDEMAIN(TestCustomMake, NoGUI)
