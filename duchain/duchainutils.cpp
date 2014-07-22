b9a5fef2e36d76c1844d49ba047916be67316bd0
Kevin Funk <kfunk@kde.org>
2014-07-22 15:36:17 +0200

    Introduce DUChainUtils::functionSignatureRange
    This will be used in the upcoming commit

---
+++ duchain/duchainutils.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2014  Kevin Funk <kfunk@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "duchainutils.h"
+
+#include <language/duchain/declaration.h>
+#include <language/duchain/duchainutils.h>
+#include <language/duchain/ducontext.h>
+#include <language/duchain/functiondefinition.h>
+#include <language/editor/simplerange.h>
+#include <language/codegen/coderepresentation.h>
+
+#include <KDebug>
+
+using namespace KDevelop;
+
+namespace ClangIntegration {
+
+KDevelop::SimpleRange DUChainUtils::functionSignatureRange(const Declaration* decl)
+{
+    if (!decl->isFunctionDeclaration()) {
+        kWarning() << "Invalid declaration:" << decl;
+        return {};
+    }
+
+    auto functionContext = decl->internalContext();
+    Q_ASSERT(functionContext);
+    auto childContexts = functionContext->childContexts();
+    if (childContexts.isEmpty()) {
+        return functionContext->rangeInCurrentRevision();
+    }
+
+    const auto start = functionContext->rangeInCurrentRevision().start;
+    const auto end = childContexts[0]->rangeInCurrentRevision().start;
+    return {start, end};
+}
+
+}
---
+++ tests/test_duchainutils.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2014  Kevin Funk <kfunk@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "test_duchainutils.h"
+
+#include <qtest_kde.h>
+
+#include <language/duchain/duchainlock.h>
+#include <tests/testcore.h>
+#include <tests/autotestshell.h>
+#include <tests/testfile.h>
+#include <tests/testhelpers.h>
+
+#include "../duchain/duchainutils.h"
+
+using namespace KDevelop;
+
+QTEST_KDEMAIN(TestDUChainUtils, NoGUI);
+
+Q_DECLARE_METATYPE(SimpleRange);
+
+void TestDUChainUtils::initTestCase()
+{
+    QVERIFY(qputenv("KDEV_DISABLE_PLUGINS", "kdevcppsupport"));
+    AutoTestShell::init();
+    TestCore::initialize(Core::NoUi);
+}
+
+void TestDUChainUtils::cleanupTestCase()
+{
+    TestCore::shutdown();
+}
+
+void TestDUChainUtils::getFunctionSignatureRange()
+{
+    QFETCH(QString, code);
+    QFETCH(SimpleRange, expectedRange);
+    {
+        TestFile file(code, "cpp");
+        file.parse();
+        QVERIFY(file.waitForParsed());
+
+        DUChainReadLocker lock;
+        QVERIFY(file.topContext());
+
+        const auto functionDecl = file.topContext()->localDeclarations()[0];
+        const auto range = ClangIntegration::DUChainUtils::functionSignatureRange(functionDecl);
+        QCOMPARE(range, expectedRange);
+    }
+}
+
+void TestDUChainUtils::getFunctionSignatureRange_data()
+{
+    QTest::addColumn<QString>("code");
+    QTest::addColumn<SimpleRange>("expectedRange");
+
+    QTest::newRow("function-declaration")
+        << "void func(\nint a, int b\n);\n"
+        << SimpleRange(0, 0, 2, 1);
+    QTest::newRow("function-definition")
+        << "void func(\nint a, int b\n) {}\n"
+        << SimpleRange(0, 0, 2, 2);
+
+}
+
+#include "test_duchainutils.moc"
