edd2b8f6a753336424ef3a1bf06dceb7fa52f5ed
Denis Steckelmacher <steckdenis@yahoo.fr>
2014-08-14 11:52:31 +0200

    Add unit tests for the code-completion of require() and QML modules

--- codecompletion/tests/qmlcompletiontest.h
+++ codecompletion/tests/qmlcompletiontest.h
@@ -33,6 +33,8 @@ private slots:
     void testContainsDeclaration_data();
     void testDoesNotContainDeclaration();
     void testDoesNotContainDeclaration_data();
+    void testContainsText();
+    void testContainsText_data();
 };
 
 }
