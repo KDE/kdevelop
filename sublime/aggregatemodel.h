KDE/kdevelop

commit 8cc00b0e8fedeb397c4861e593ef01c1a3c69f87

Get pretty targets for KDevplatform

sublime/CMakeLists.txt
@@ -27,13 +27,15 @@ set(sublime_LIB_SRCS
     idealbuttonbarwidget.cpp
 )
 
-add_library(sublime SHARED ${sublime_LIB_SRCS})
-generate_export_header(sublime EXPORT_FILE_NAME sublimeexport.h)
-target_link_libraries(sublime
+add_library(KDevPlatformSublime ${sublime_LIB_SRCS})
+add_library(KDev::Sublime ALIAS KDevPlatformSublime)
+
+generate_export_header(KDevPlatformSublime EXPORT_FILE_NAME KDev::Sublimeexport.h)
+target_link_libraries(KDevPlatformSublime
         LINK_PRIVATE KF5::KIOWidgets
         LINK_PUBLIC KF5::Parts KF5::KDELibs4Support)
-set_target_properties(sublime PROPERTIES VERSION ${KDEVPLATFORM_LIB_VERSION} SOVERSION ${KDEVPLATFORM_LIB_SOVERSION})
-install(TARGETS sublime EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
+set_target_properties(KDevPlatformSublime PROPERTIES VERSION ${KDEVPLATFORM_LIB_VERSION} SOVERSION ${KDEVPLATFORM_LIB_SOVERSION} EXPORT_NAME Sublime)
+install(TARGETS KDevPlatformSublime EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
 
 
 ########### install files ###############

sublime/tests/CMakeLists.txt
@@ -2,53 +2,53 @@ set( areaoperationtest_SRCS areaoperationtest.cpp areaprinter.cpp )
 add_executable(sublime-areaoperationtest ${areaoperationtest_SRCS})
 add_test(sublime-areaoperationtest sublime-areaoperationtest)
 ecm_mark_as_test(sublime-areaoperationtest)
-target_link_libraries(sublime-areaoperationtest Qt5::Test KF5::Parts sublime )
+target_link_libraries(sublime-areaoperationtest Qt5::Test KF5::Parts KDev::Sublime )
 
 
 set( areawalkertest_SRCS areawalkertest.cpp areaprinter.cpp )
 add_executable(sublime-areawalkertest ${areawalkertest_SRCS})
 add_test(sublime-areawalkertest sublime-areawalkertest)
 ecm_mark_as_test(sublime-areawalkertest)
-target_link_libraries(sublime-areawalkertest Qt5::Test KF5::Parts sublime )
+target_link_libraries(sublime-areawalkertest Qt5::Test KF5::Parts KDev::Sublime )
 
 
 set( viewtest_SRCS viewtest.cpp )
 add_executable(sublime-viewtest ${viewtest_SRCS})
-add_test(sublime-viewtest sublime-viewtest)
+add_test(sublime-viewtest KDev::Sublime-viewtest)
 ecm_mark_as_test(sublime-viewtest)
-target_link_libraries(sublime-viewtest Qt5::Test KF5::Parts sublime )
+target_link_libraries(sublime-viewtest Qt5::Test KF5::Parts KDev::Sublime )
 
 
 set( documenttest_SRCS documenttest.cpp )
 add_executable(sublime-documenttest ${documenttest_SRCS})
 add_test(sublime-documenttest sublime-documenttest)
 ecm_mark_as_test(sublime-documenttest)
-target_link_libraries(sublime-documenttest Qt5::Test KF5::Parts sublime )
+target_link_libraries(sublime-documenttest Qt5::Test KF5::Parts KDev::Sublime )
 
 
 set( controllertest_SRCS controllertest.cpp )
 add_executable(sublime-controllertest ${controllertest_SRCS})
 add_test(sublime-controllertest sublime-controllertest)
 ecm_mark_as_test(sublime-controllertest)
-target_link_libraries(sublime-controllertest Qt5::Test KF5::Parts sublime )
+target_link_libraries(sublime-controllertest Qt5::Test KF5::Parts KDev::Sublime )
 
 
 set( viewactivationtest_SRCS viewactivationtest.cpp )
 add_executable(sublime-viewactivationtest ${viewactivationtest_SRCS})
 add_test(sublime-viewactivationtest sublime-viewactivationtest)
 ecm_mark_as_test(sublime-viewactivationtest)
-target_link_libraries(sublime-viewactivationtest Qt5::Test KF5::Parts sublime )
+target_link_libraries(sublime-viewactivationtest Qt5::Test KF5::Parts KDev::Sublime )
 
 
 set( aggregatemodeltest_SRCS aggregatemodeltest.cpp )
 add_executable(sublime-aggregatemodeltest ${aggregatemodeltest_SRCS})
 add_test(sublime-aggregatemodeltest sublime-aggregatemodeltest)
 ecm_mark_as_test(sublime-aggregatemodeltest)
-target_link_libraries(sublime-aggregatemodeltest Qt5::Test KF5::Parts sublime kdevplatformtests )
+target_link_libraries(sublime-aggregatemodeltest Qt5::Test KF5::Parts KDev::Sublime KDev::Tests )
 
 set( toolviewtoolbartest_SRCS toolviewtoolbartest.cpp )
 add_executable(sublime-toolviewtoolbartest ${toolviewtoolbartest_SRCS})
 add_test(sublime-toolviewtoolbartest sublime-toolviewtoolbartest)
 ecm_mark_as_test(sublime-toolviewtoolbartest)
-target_link_libraries(sublime-toolviewtoolbartest Qt5::Test KF5::Parts sublime )
+target_link_libraries(sublime-toolviewtoolbartest Qt5::Test KF5::Parts KDev::Sublime )
 

tests/CMakeLists.txt
@@ -3,7 +3,7 @@ add_definitions( -DKDE_DEFAULT_DEBUG_AREA=9532 )
 
 include_directories( ${KDevPlatform_SOURCE_DIR}/shell )
 
-set(kdevplatformtests_LIB_SRCS
+set(kdevtests_LIB_SRCS
     autotestshell.cpp
     kdevsignalspy.cpp
     testcore.cpp
@@ -15,23 +15,26 @@ set(kdevplatformtests_LIB_SRCS
     modeltest.cpp
 )
 
-add_library(kdevplatformtests SHARED ${kdevplatformtests_LIB_SRCS})
-generate_export_header(kdevplatformtests EXPORT_FILE_NAME kdevplatformtestsexport.h)
-target_link_libraries(kdevplatformtests LINK_PUBLIC
-    kdevplatformshell
-    kdevplatformlanguage
-    kdevplatformproject
+add_library(KDevPlatformTests ${kdevtests_LIB_SRCS})
+add_library(KDev::Tests ALIAS KDevPlatformTests)
+
+generate_export_header(KDevPlatformTests EXPORT_FILE_NAME KDev::Testsexport.h)
+target_link_libraries(KDevPlatformTests LINK_PUBLIC
+    KDev::Shell
+    KDev::Language
+    KDev::Project
 )
-target_link_libraries(kdevplatformtests LINK_PRIVATE
+target_link_libraries(KDevPlatformTests LINK_PRIVATE
     Qt5::Core
     Qt5::Test
 )
 
-set_target_properties(kdevplatformtests PROPERTIES
+set_target_properties(KDevPlatformTests PROPERTIES
     VERSION ${KDEVPLATFORM_LIB_VERSION}
     SOVERSION ${KDEVPLATFORM_LIB_SOVERSION}
+    EXPORT_NAME Tests
 )
-install(TARGETS kdevplatformtests EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
+install(TARGETS KDevPlatformTests EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
 
 install(FILES
     autotestshell.h
@@ -43,7 +46,7 @@ install(FILES
     testlanguagecontroller.h
     testhelpers.h
     testplugincontroller.h
-    ${CMAKE_CURRENT_BINARY_DIR}/kdevplatformtestsexport.h
+    ${CMAKE_CURRENT_BINARY_DIR}/KDev::Testsexport.h
     DESTINATION ${INCLUDE_INSTALL_DIR}/kdevplatform/tests/
     COMPONENT Devel
 )

tests/json/CMakeLists.txt
@@ -1,24 +1,27 @@
 include_directories(${KDevPlatform_SOURCE_DIR}/shell ${QJSON_INCLUDE_DIR})
 
-set(kdevplatformjsontests_LIB_SRCS
+set(KDevPlatformJsonTests_LIB_SRCS
     delayedoutput.cpp
     declarationvalidator.cpp
     testsuite.cpp
 )
 
-add_library(kdevplatformjsontests SHARED ${kdevplatformjsontests_LIB_SRCS})
-generate_export_header(kdevplatformjsontests EXPORT_FILE_NAME kdevplatformjsontestsexport.h)
-target_link_libraries(kdevplatformjsontests
+add_library(KDevPlatformJsonTests ${KDevPlatformJsonTests_LIB_SRCS})
+add_library(KDev::JsonTests ALIAS KDevPlatformJsonTests)
+
+generate_export_header(KDevPlatformJsonTests EXPORT_FILE_NAME KDevPlatformJsonTestsexport.h)
+target_link_libraries(KDevPlatformJsonTests
   LINK_PUBLIC
     Qt5::Core
-    kdevplatformlanguage
+    KDev::Language
 )
-set_target_properties(kdevplatformjsontests PROPERTIES
+set_target_properties(KDevPlatformJsonTests PROPERTIES
     VERSION ${KDEVPLATFORM_LIB_VERSION}
     SOVERSION ${KDEVPLATFORM_LIB_SOVERSION}
+    EXPORT_NAME JsonTests
 )
 
-install(TARGETS kdevplatformjsontests EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
+install(TARGETS KDevPlatformJsonTests EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
 
 install(FILES
     declarationvalidator.h
@@ -29,7 +32,7 @@ install(FILES
     jsondeclarationtests.h
     jsonducontexttests.h
     jsontypetests.h
-    ${CMAKE_CURRENT_BINARY_DIR}/kdevplatformjsontestsexport.h
+    ${CMAKE_CURRENT_BINARY_DIR}/KDevPlatformJsonTestsexport.h
     DESTINATION ${INCLUDE_INSTALL_DIR}/kdevplatform/tests/json/
     COMPONENT Devel
 )

CMakeLists.txt
@@ -94,19 +94,19 @@ endif()
 include_directories(${CMAKE_CURRENT_SOURCE_DIR}/util/)
 
 # Now set the usual KDEVPLATFORM_XXX_LIBRARIES variable so we can more easily move plugins around
-set(KDEVPLATFORM_SUBLIME_LIBRARIES sublime)
-set(KDEVPLATFORM_INTERFACES_LIBRARIES kdevplatforminterfaces)
-set(KDEVPLATFORM_LANGUAGE_LIBRARIES kdevplatformlanguage)
-set(KDEVPLATFORM_PROJECT_LIBRARIES kdevplatformproject)
-set(KDEVPLATFORM_UTIL_LIBRARIES kdevplatformutil)
-set(KDEVPLATFORM_OUTPUTVIEW_LIBRARIES kdevplatformoutputview)
-set(KDEVPLATFORM_VCS_LIBRARIES kdevplatformvcs)
-set(KDEVPLATFORM_SHELL_LIBRARIES kdevplatformshell)
-set(KDEVPLATFORM_TESTS_LIBRARIES kdevplatformtests)
-set(KDEVPLATFORM_JSONTESTS_LIBRARIES kdevplatformjsontests)
-set(KDEVPLATFORM_DEBUGGER_LIBRARIES kdevplatformdebugger)
-set(KDEVPLATFORM_DOCUMENTATION_LIBRARIES kdevplatformdocumentation)
-set(KDEVPLATFORM_SERIALIZATION_LIBRARIES kdevplatformserialization)
+set(KDEVPLATFORM_SUBLIME_LIBRARIES KDev::Sublime)
+set(KDEVPLATFORM_INTERFACES_LIBRARIES KDev::Interfaces)
+set(KDEVPLATFORM_LANGUAGE_LIBRARIES KDev::Language)
+set(KDEVPLATFORM_PROJECT_LIBRARIES KDev::Project)
+set(KDEVPLATFORM_UTIL_LIBRARIES KDev::Util)
+set(KDEVPLATFORM_OUTPUTVIEW_LIBRARIES KDev::OutputView)
+set(KDEVPLATFORM_VCS_LIBRARIES KDev::Vcs)
+set(KDEVPLATFORM_SHELL_LIBRARIES KDev::Shell)
+set(KDEVPLATFORM_TESTS_LIBRARIES KDev::Tests)
+set(KDEVPLATFORM_JSONTESTS_LIBRARIES KDev::JsonTests)
+set(KDEVPLATFORM_DEBUGGER_LIBRARIES KDev::Debugger)
+set(KDEVPLATFORM_DOCUMENTATION_LIBRARIES KDev::Documentation)
+set(KDEVPLATFORM_SERIALIZATION_LIBRARIES KDev::Serialization)
 
 add_subdirectory(sublime)
 add_subdirectory(interfaces)
@@ -155,7 +155,7 @@ install( FILES
         DESTINATION "${CMAKECONFIG_INSTALL_DIR}" )
 install( EXPORT KDevPlatformTargets
          DESTINATION "${CMAKECONFIG_INSTALL_DIR}"
-         NAMESPACE KDevPlatformImport__
+         NAMESPACE KDev::
          FILE KDevPlatformTargets.cmake )
 
 include(CTest)

debugger/CMakeLists.txt
@@ -3,7 +3,7 @@ add_definitions( -DKDE_DEFAULT_DEBUG_AREA=9531 )
 
 ########### next target ###############
 
-set( kdevplatformdebugger_LIB_SRCS
+set( KDevPlatformDebugger_LIB_SRCS
     interfaces/idebugsession.cpp
     interfaces/iframestackmodel.cpp
     interfaces/ibreakpointcontroller.cpp
@@ -22,19 +22,23 @@ set( kdevplatformdebugger_LIB_SRCS
     framestack/framestackmodel.cpp
     framestack/framestackwidget.cpp
 )
-add_library(kdevplatformdebugger SHARED ${kdevplatformdebugger_LIB_SRCS})
-generate_export_header(kdevplatformdebugger EXPORT_FILE_NAME debuggerexport.h)
-target_link_libraries(kdevplatformdebugger LINK_PUBLIC
-    kdevplatforminterfaces
-    kdevplatformutil
+add_library(KDevPlatformDebugger ${KDevPlatformDebugger_LIB_SRCS})
+generate_export_header(KDevPlatformDebugger EXPORT_FILE_NAME debuggerexport.h)
+target_link_libraries(KDevPlatformDebugger LINK_PUBLIC
+    KDev::Interfaces
+    KDev::Util
 LINK_PRIVATE
     Qt5::Core
     KF5::TextEditor
-    sublime
+    KDev::Sublime
 )
+add_library(KDev::Debugger ALIAS KDevPlatformDebugger)
 
-set_target_properties(kdevplatformdebugger PROPERTIES VERSION ${KDEVPLATFORM_LIB_VERSION} SOVERSION ${KDEVPLATFORM_LIB_SOVERSION})
-install(TARGETS kdevplatformdebugger EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
+set_target_properties(KDevPlatformDebugger PROPERTIES
+    VERSION ${KDEVPLATFORM_LIB_VERSION} SOVERSION ${KDEVPLATFORM_LIB_SOVERSION}
+    EXPORT_NAME Debugger
+)
+install(TARGETS KDevPlatformDebugger EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
 
 ########### install files ###############
 

documentation/CMakeLists.txt
@@ -3,22 +3,23 @@ set_package_properties(Qt5WebKitWidgets PROPERTIES
     URL "http://qt-project.org/"
     TYPE REQUIRED)
 
-set(kdevplatformdocumentation_LIB_SRCS
+set(KDevPlatformDocumentation_LIB_SRCS
     standarddocumentationview.cpp
     documentationfindwidget.cpp
     documentationview.cpp
 )
 
-qt5_wrap_ui(kdevplatformdocumentation_LIB_SRCS documentationfindwidget.ui)
-add_library(kdevplatformdocumentation SHARED ${kdevplatformdocumentation_LIB_SRCS})
-generate_export_header(kdevplatformdocumentation EXPORT_FILE_NAME documentationexport.h)
+qt5_wrap_ui(KDevPlatformDocumentation_LIB_SRCS documentationfindwidget.ui)
+add_library(KDevPlatformDocumentation ${KDevPlatformDocumentation_LIB_SRCS})
+add_library(KDev::Documentation ALIAS KDevPlatformDocumentation)
+generate_export_header(KDevPlatformDocumentation EXPORT_FILE_NAME documentationexport.h)
 
-install(TARGETS kdevplatformdocumentation EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
+install(TARGETS KDevPlatformDocumentation EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
 
-target_link_libraries(kdevplatformdocumentation
-        LINK_PUBLIC kdevplatforminterfaces Qt5::WebKitWidgets)
+target_link_libraries(KDevPlatformDocumentation
+        LINK_PUBLIC KDev::Interfaces Qt5::WebKitWidgets)
 
-set_target_properties(kdevplatformdocumentation PROPERTIES VERSION ${KDEVPLATFORM_LIB_VERSION} SOVERSION ${KDEVPLATFORM_LIB_SOVERSION})
+set_target_properties(KDevPlatformDocumentation PROPERTIES VERSION ${KDEVPLATFORM_LIB_VERSION} SOVERSION ${KDEVPLATFORM_LIB_SOVERSION} EXPORT_NAME Documentation)
 
 install(FILES
     ${CMAKE_CURRENT_BINARY_DIR}/documentationexport.h

interfaces/CMakeLists.txt
@@ -1,5 +1,5 @@
 add_definitions( -DKDE_DEFAULT_DEBUG_AREA=9502 )
-set(kdevplatforminterfaces_LIB_SRCS
+set(KDevPlatformInterfaces_LIB_SRCS
     iassistant.cpp
     context.cpp
     iplugin.cpp
@@ -42,9 +42,10 @@ set(kdevplatforminterfaces_LIB_SRCS
 )
 configure_file(ipluginversion.h.in ${CMAKE_CURRENT_BINARY_DIR}/ipluginversion.h)
 
-add_library(kdevplatforminterfaces SHARED ${kdevplatforminterfaces_LIB_SRCS})
-generate_export_header(kdevplatforminterfaces EXPORT_FILE_NAME interfacesexport.h)
-target_link_libraries(kdevplatforminterfaces
+add_library(KDevPlatformInterfaces ${KDevPlatformInterfaces_LIB_SRCS})
+add_library(KDev::Interfaces ALIAS KDevPlatformInterfaces)
+generate_export_header(KDevPlatformInterfaces EXPORT_FILE_NAME interfacesexport.h)
+target_link_libraries(KDevPlatformInterfaces
 LINK_PUBLIC
     KF5::Parts
     KF5::TextEditor
@@ -56,12 +57,12 @@ LINK_PRIVATE
     KF5::KCMUtils
     KF5::ThreadWeaver
 )
-target_include_directories(kdevplatforminterfaces INTERFACE "$<INSTALL_INTERFACE:${INCLUDE_INSTALL_DIR}/kdevplatform>")
+target_include_directories(KDevPlatformInterfaces INTERFACE "$<INSTALL_INTERFACE:${INCLUDE_INSTALL_DIR}/kdevplatform>")
 # TODO: this is ugly (sort-of cyclic dependecy):
-target_include_directories(kdevplatforminterfaces PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/../util") # for utilexport.h
+target_include_directories(KDevPlatformInterfaces PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/../util") # for utilexport.h
 
-set_target_properties(kdevplatforminterfaces PROPERTIES VERSION ${KDEVPLATFORM_LIB_VERSION} SOVERSION ${KDEVPLATFORM_LIB_SOVERSION})
-install(TARGETS kdevplatforminterfaces EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
+set_target_properties(KDevPlatformInterfaces PROPERTIES VERSION ${KDEVPLATFORM_LIB_VERSION} SOVERSION ${KDEVPLATFORM_LIB_SOVERSION} EXPORT_NAME Interfaces)
+install(TARGETS KDevPlatformInterfaces EXPORT KDevPlatformTargets ${INSTALL_TARGETS_DEFAULT_ARGS} )
 
 install(FILES
     iassistant.h

commit ad65c20a513b33d77390fada86b4272c7ef16e9c

Correct file names for some export headers
Some export header filenames and SUBLIME_EXPORT definition was broken by 8cc00b0e8fedeb397c4861e593ef01c1a3c69f87, this fixes it.

REVIEW: 119778

doc/api/Doxyfile.local
@@ -14,7 +14,7 @@ PREDEFINED = KDEVPLATFORMEDITOR_EXPORT="" \
         KDEVPLATFORMOUTPUTVIEW_EXPORT="" \
         KDEVPLATFORMPROJECT_EXPORT="" \
         KDEVPLATFORMSHELL_EXPORT="" \
-        SUBLIME_EXPORT="" \
+        KDEVPLATFORMSUBLIME_EXPORT="" \
         KDEVPLATFORMUTIL_EXPORT="" \
         KDEVPLATFORMVCS_EXPORT="" \
 	KDE_DEPRECATED="" \

sublime/CMakeLists.txt
@@ -30,7 +30,7 @@ set(sublime_LIB_SRCS
 add_library(KDevPlatformSublime ${sublime_LIB_SRCS})
 add_library(KDev::Sublime ALIAS KDevPlatformSublime)
 
-generate_export_header(KDevPlatformSublime EXPORT_FILE_NAME KDev::Sublimeexport.h)
+generate_export_header(KDevPlatformSublime EXPORT_FILE_NAME sublimeexport.h)
 target_link_libraries(KDevPlatformSublime
         LINK_PRIVATE KF5::KIOWidgets
         LINK_PUBLIC KF5::Parts KF5::KDELibs4Support)

sublime/aggregatemodel.h
@@ -67,7 +67,7 @@ they will look as:
 @note It is impossible to aggregate any model, aggregation works only for standard models.
 @note Currently aggregate model displays only 1 column.
 */
-class SUBLIME_EXPORT AggregateModel: public QAbstractItemModel {
+class KDEVPLATFORMSUBLIME_EXPORT AggregateModel: public QAbstractItemModel {
     Q_OBJECT
 public:
     AggregateModel(QObject *parent = 0);

sublime/area.h
@@ -56,7 +56,7 @@ MainWindow *mw = new MainWindow(controller);
 controller->show(area, mw);
 @endcode
 */
-class SUBLIME_EXPORT Area: public QObject {
+class KDEVPLATFORMSUBLIME_EXPORT Area: public QObject {
     Q_OBJECT
 public:
     /**Creates and area with given @p name and adds it to the @p controller.

sublime/areaindex.h
@@ -103,7 +103,7 @@ is visible at the time.
         root_index (view1, view2, view3, view4)
 @endcode
 */
-class SUBLIME_EXPORT AreaIndex {
+class KDEVPLATFORMSUBLIME_EXPORT AreaIndex {
 public:
     ~AreaIndex();
     AreaIndex(const AreaIndex &index);
@@ -183,7 +183,7 @@ private:
 This is the special index class returned by @ref Area::rootIndex().
 Doesn't provide any additional functionality beyond AreaIndex.
 */
-class SUBLIME_EXPORT RootAreaIndex: public AreaIndex {
+class KDEVPLATFORMSUBLIME_EXPORT RootAreaIndex: public AreaIndex {
 public:
     RootAreaIndex();
 private:

sublime/container.h
@@ -40,7 +40,7 @@ class Document;
 This container is placed inside mainwindow splitters to show widgets
 for views in the area.
 */
-class SUBLIME_EXPORT Container: public QWidget {
+class KDEVPLATFORMSUBLIME_EXPORT Container: public QWidget {
 Q_OBJECT
 public:
     Container(QWidget *parent = 0);

sublime/controller.h
@@ -86,7 +86,7 @@ Controller has to exist before any area, document or mainwindow can be created.
 There's no point in having two controllers for one application unless they
 need to show completely different sets of areas.
 */
-class SUBLIME_EXPORT Controller: public QObject, public MainWindowOperator {
+class KDEVPLATFORMSUBLIME_EXPORT Controller: public QObject, public MainWindowOperator {
     Q_OBJECT
 public:
     Controller(QObject *parent = 0);

sublime/document.h
@@ -41,7 +41,7 @@ class Controller;
 Subclass from Document and implement createViewWidget() method
 to return a new widget for a view.
 */
-class SUBLIME_EXPORT Document: public QObject {
+class KDEVPLATFORMSUBLIME_EXPORT Document: public QObject {
     Q_OBJECT
 public:
     /**Creates a document and adds it to a @p controller.*/

sublime/holdupdates.h
@@ -55,7 +55,7 @@ Would be equivalent to
 if someCall was using HoldUpdates too, it will make sure to only call setUpdatesEnabled(true) in case it's needed.
 
  */
-class SUBLIME_EXPORT HoldUpdates {
+class KDEVPLATFORMSUBLIME_EXPORT HoldUpdates {
     public:
         HoldUpdates(QWidget* w);
         ~HoldUpdates();

sublime/mainwindow.h
@@ -45,7 +45,7 @@ MainWindow w(controller);
 controller->showArea(area, &w);
 @endcode
 */
-class SUBLIME_EXPORT MainWindow: public KParts::MainWindow {
+class KDEVPLATFORMSUBLIME_EXPORT MainWindow: public KParts::MainWindow {
 Q_OBJECT
 public:
     /**Creates a mainwindow and adds it to the controller.*/

sublime/mainwindowoperator.h
@@ -33,7 +33,7 @@ class MainWindow;
 
 /**Proxy class to give descendants access to MainWindow::setArea
 and MainWindow::hideArea methods. Just inherit it and use the methods.*/
-class SUBLIME_EXPORT MainWindowOperator {
+class KDEVPLATFORMSUBLIME_EXPORT MainWindowOperator {
 protected:
     /**Sets the area of main window and fills it with views.*/
     void setArea(MainWindow *w, Area *area);

sublime/sublimedefs.h
@@ -43,7 +43,7 @@ namespace Sublime {
     };
     Q_DECLARE_FLAGS(Positions, Position)
 
-    SUBLIME_EXPORT Position dockAreaToPosition(Qt::DockWidgetArea dockArea);
+    KDEVPLATFORMSUBLIME_EXPORT Position dockAreaToPosition(Qt::DockWidgetArea dockArea);
 
 }
 

sublime/tooldocument.h
@@ -34,7 +34,7 @@ class ToolDocument;
 /**
 @short Factory to create widgets for toolviews
 */
-class SUBLIME_EXPORT ToolFactory {
+class KDEVPLATFORMSUBLIME_EXPORT ToolFactory {
 public:
     virtual ~ToolFactory() {}
     virtual QWidget* create(ToolDocument *doc, QWidget *parent = 0) = 0;
@@ -65,7 +65,7 @@ private:
 /**
 @short Document to represent and manage widgets as toolviews
 */
-class SUBLIME_EXPORT ToolDocument: public Document {
+class KDEVPLATFORMSUBLIME_EXPORT ToolDocument: public Document {
 public:
     /**Initializes tool document with given @p factory. Document takes
     ownership over the factory and deletes it together with itself*/

sublime/urldocument.h
@@ -30,7 +30,7 @@ namespace Sublime {
 /**
 @short Basic document that has an URL.
 */
-class SUBLIME_EXPORT UrlDocument: public Document {
+class KDEVPLATFORMSUBLIME_EXPORT UrlDocument: public Document {
 public:
     UrlDocument(Controller *controller, const KUrl &url);
     ~UrlDocument();

sublime/view.h
@@ -43,7 +43,7 @@ If you create a subclass of View you need to override Sublime::View::createWidge
 provide a custom widget for your view.
 
 */
-class SUBLIME_EXPORT View: public QObject {
+class KDEVPLATFORMSUBLIME_EXPORT View: public QObject {
     Q_OBJECT
 public:
     enum WidgetOwnership {

sublime/tests/CMakeLists.txt
@@ -14,7 +14,7 @@ target_link_libraries(sublime-areawalkertest Qt5::Test KF5::Parts KDev::Sublime 
 
 set( viewtest_SRCS viewtest.cpp )
 add_executable(sublime-viewtest ${viewtest_SRCS})
-add_test(sublime-viewtest KDev::Sublime-viewtest)
+add_test(sublime-viewtest sublime-viewtest)
 ecm_mark_as_test(sublime-viewtest)
 target_link_libraries(sublime-viewtest Qt5::Test KF5::Parts KDev::Sublime )
 

tests/CMakeLists.txt
@@ -18,7 +18,7 @@ set(kdevtests_LIB_SRCS
 add_library(KDevPlatformTests ${kdevtests_LIB_SRCS})
 add_library(KDev::Tests ALIAS KDevPlatformTests)
 
-generate_export_header(KDevPlatformTests EXPORT_FILE_NAME KDev::Testsexport.h)
+generate_export_header(KDevPlatformTests EXPORT_FILE_NAME kdevplatformtestsexport.h)
 target_link_libraries(KDevPlatformTests LINK_PUBLIC
     KDev::Shell
     KDev::Language
@@ -46,7 +46,7 @@ install(FILES
     testlanguagecontroller.h
     testhelpers.h
     testplugincontroller.h
-    ${CMAKE_CURRENT_BINARY_DIR}/KDev::Testsexport.h
+    ${CMAKE_CURRENT_BINARY_DIR}/kdevplatformtestsexport.h
     DESTINATION ${INCLUDE_INSTALL_DIR}/kdevplatform/tests/
     COMPONENT Devel
 )

tests/json/CMakeLists.txt
@@ -9,7 +9,7 @@ set(KDevPlatformJsonTests_LIB_SRCS
 add_library(KDevPlatformJsonTests ${KDevPlatformJsonTests_LIB_SRCS})
 add_library(KDev::JsonTests ALIAS KDevPlatformJsonTests)
 
-generate_export_header(KDevPlatformJsonTests EXPORT_FILE_NAME KDevPlatformJsonTestsexport.h)
+generate_export_header(KDevPlatformJsonTests EXPORT_FILE_NAME kdevplatformjsontestsexport.h)
 target_link_libraries(KDevPlatformJsonTests
   LINK_PUBLIC
     Qt5::Core
@@ -32,7 +32,7 @@ install(FILES
     jsondeclarationtests.h
     jsonducontexttests.h
     jsontypetests.h
-    ${CMAKE_CURRENT_BINARY_DIR}/KDevPlatformJsonTestsexport.h
+    ${CMAKE_CURRENT_BINARY_DIR}/kdevplatformjsontestsexport.h
     DESTINATION ${INCLUDE_INSTALL_DIR}/kdevplatform/tests/json/
     COMPONENT Devel
 )