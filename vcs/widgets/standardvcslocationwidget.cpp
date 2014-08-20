0b693e02d2ccd16357be603f03c32f537921e69e
Kevin Funk <kfunk@kde.org>
2014-08-20 11:31:29 +0200

    Minor: Fix wording

--- vcs/widgets/standardvcslocationwidget.cpp
+++ vcs/widgets/standardvcslocationwidget.cpp
@@ -32,7 +32,7 @@ StandardVcsLocationWidget::StandardVcsLocationWidget(QWidget* parent, Qt::Window
 {
     setLayout(new QVBoxLayout(this));
     m_urlWidget = new KUrlRequester(this);
-    m_urlWidget->setClickMessage(i18n("Introduce the repository URL..."));
+    m_urlWidget->setClickMessage(i18n("Enter the repository URL..."));
     layout()->addWidget(m_urlWidget);
     
     connect(m_urlWidget, SIGNAL(textChanged(QString)), SLOT(textChanged(QString)));
