KDE/kdevelop

commit b9cd67a9d6bec3517ca7e170b7ee477607305519

Remove redundant API

plugins/quickopen/projectfilequickopen.cpp
@@ -240,7 +240,7 @@ ProjectFileDataProvider::ProjectFileDataProvider()
 
 void ProjectFileDataProvider::projectClosing( IProject* project )
 {
-    foreach(ProjectFileItem* file, project->files()) {
+    foreach(ProjectFileItem* file, project->projectItem()->fileList()) {
         fileRemovedFromSet(file);
     }
 }
@@ -249,7 +249,7 @@ void ProjectFileDataProvider::projectOpened( IProject* project )
 {
     const int processAfter = 1000;
     int processed = 0;
-    foreach(ProjectFileItem* file, project->files()) {
+    foreach(ProjectFileItem* file, project->projectItem()->fileList()) {
         fileAddedToSet(file);
         if (++processed == processAfter) {
             // prevent UI-lockup when a huge project was imported

shell/project.cpp
@@ -485,7 +485,6 @@ Path Project::path() const
 
 void Project::reloadModel()
 {
-    qDebug() << "reload!!!!!!!" << d->loading;
     if (d->loading) {
         d->scheduleReload = true;
         return;
@@ -580,25 +579,6 @@ bool Project::inProject( const IndexedString& path ) const
     return !d->itemsForPath( path ).isEmpty();
 }
 
-ProjectFileItem* Project::fileAt( int num ) const
-{
-    QList<ProjectFileItem*> files;
-    if ( d->topItem )
-        files = d->recurseFiles( d->topItem );
-
-    if( !files.isEmpty() && num >= 0 && num < files.count() )
-        return files.at( num );
-    return 0;
-}
-
-QList<ProjectFileItem *> Project::files() const
-{
-    QList<ProjectFileItem *> files;
-    if ( d->topItem )
-        files = d->recurseFiles( d->topItem );
-    return files;
-}
-
 QList< ProjectBaseItem* > Project::itemsForUrl(const KUrl& url) const
 {
     return d->itemsForPath(IndexedString(url));
@@ -639,14 +619,6 @@ QList<ProjectFolderItem*> Project::foldersForPath(const IndexedString& folder) c
             items << dynamic_cast<ProjectFolderItem*>( item );
     }
     return items;
-}
-
-int Project::fileCount() const
-{
-    QList<ProjectFileItem*> files;
-    if ( d->topItem )
-        files = d->recurseFiles( d->topItem );
-    return files.count();
 }
 
 IProjectFileManager* Project::projectFileManager() const

interfaces/iproject.h
@@ -105,13 +105,6 @@ public:
      */
     Q_SCRIPTABLE virtual ProjectFolderItem* projectItem() const = 0;
 
-    /** Get how much files are there in the project */
-    Q_SCRIPTABLE virtual int fileCount() const = 0;
-
-    /** Get the item corresponding to the @p pos file */
-    Q_SCRIPTABLE virtual ProjectFileItem* fileAt( int pos) const = 0;
-
-    /** Get a list of all files in the project */
     Q_SCRIPTABLE virtual QList<ProjectFileItem*> files() const = 0;
 
     /**

plugins/genericprojectmanager/test/reloadtest.cpp
@@ -161,7 +161,7 @@ void ProjectLoadTest::addRemoveFiles()
     }
 
     QTest::qWait(2000);
-    QCOMPARE(project->fileCount(), 1);
+    QCOMPARE(project->projectItem()->fileList().count(), 1);
 }
 
 void ProjectLoadTest::removeDirRecursive()
@@ -206,7 +206,7 @@ void ProjectLoadTest::removeDirRecursive()
     }
 
     QTest::qWait(2000);
-    QCOMPARE(project->fileCount(), 1);
+    QCOMPARE(project->projectItem()->fileList().count(), 1);
 }
 
 void createFile(const QString& path)

plugins/problemreporter/watcheddocumentset.cpp
@@ -159,7 +159,7 @@ void CurrentProjectSet::setCurrentDocumentInternal(const KDevelop::IndexedString
     if (projectForUrl && projectForUrl != m_currentProject) {
         m_documents.clear();
         m_currentProject = projectForUrl;
-        QList<ProjectFileItem*> files = m_currentProject->files();
+        QList<ProjectFileItem*> files = m_currentProject->projectItem()->fileList();
         foreach (ProjectFileItem* file, files) {
             m_documents.insert(file->indexedPath());
         }
@@ -176,7 +176,7 @@ AllProjectSet::AllProjectSet(ProblemModel* parent)
     : ProjectSet(parent)
 {
     foreach(const IProject* project, model()->plugin()->core()->projectController()->projects()) {
-        foreach (ProjectFileItem* file, project->files()) {
+        foreach (ProjectFileItem* file, project->projectItem()->fileList()) {
             m_documents.insert(file->indexedPath());
         }
         trackProjectFiles(project);

shell/project.h
@@ -65,12 +65,6 @@ public:
     /// Destructor.
     virtual ~Project();
 
-    /** Get a list of all files in the project */
-    virtual int fileCount() const;
-    virtual ProjectFileItem* fileAt( int i ) const;
-
-    virtual QList<ProjectFileItem*> files() const;
-
     virtual QList< ProjectBaseItem* > itemsForPath(const IndexedString& path) const;
     virtual QList< ProjectFileItem* > filesForPath(const IndexedString& file) const;
     virtual QList< ProjectFolderItem* > foldersForPath(const IndexedString& folder) const;

commit bde2c950f62e85cd0266f055762c1fab3e70703e

Fix test_quickopen{,bench}
Regression introduced by b9cd67a9d6bec3517ca7e170b7ee477607305519

plugins/quickopen/projectfilequickopen.cpp
@@ -36,6 +36,7 @@
 #include <language/duchain/parsingenvironment.h>
 
 #include <project/projectmodel.h>
+#include <project/projectutils.h>
 
 #include "../openwith/iopenwith.h"
 #include <KIconThemes/KIconLoader>
@@ -240,7 +241,7 @@ ProjectFileDataProvider::ProjectFileDataProvider()
 
 void ProjectFileDataProvider::projectClosing( IProject* project )
 {
-    foreach(ProjectFileItem* file, project->projectItem()->fileList()) {
+    foreach(ProjectFileItem* file, KDevelop::allFiles(project->projectItem())) {
         fileRemovedFromSet(file);
     }
 }
@@ -249,7 +250,7 @@ void ProjectFileDataProvider::projectOpened( IProject* project )
 {
     const int processAfter = 1000;
     int processed = 0;
-    foreach(ProjectFileItem* file, project->projectItem()->fileList()) {
+    foreach(ProjectFileItem* file, KDevelop::allFiles(project->projectItem())) {
         fileAddedToSet(file);
         if (++processed == processAfter) {
             // prevent UI-lockup when a huge project was imported

project/projectutils.cpp
@@ -101,6 +101,36 @@ void populateParentItemsMenu( ProjectBaseItem* item, QMenu* menu )
     }
 }
 
+QList<ProjectFileItem*> allFiles(ProjectBaseItem* projectItem)
+{
+    QList<ProjectFileItem*> files;
+    if ( ProjectFolderItem * folder = projectItem->folder() )
+    {
+        QList<ProjectFolderItem*> folder_list = folder->folderList();
+        for ( QList<ProjectFolderItem*>::Iterator it = folder_list.begin(); it != folder_list.end(); ++it )
+        {
+            files += allFiles( ( *it ) );
+        }
+
+        QList<ProjectTargetItem*> target_list = folder->targetList();
+        for ( QList<ProjectTargetItem*>::Iterator it = target_list.begin(); it != target_list.end(); ++it )
+        {
+            files += allFiles( ( *it ) );
+        }
+
+        files += folder->fileList();
+    }
+    else if ( ProjectTargetItem * target = projectItem->target() )
+    {
+        files += target->fileList();
+    }
+    else if ( ProjectFileItem * file = projectItem->file() )
+    {
+        files.append( file );
+    }
+    return files;
+}
+
 }
 
 #include "projectutils.moc"

project/projectutils.h
@@ -20,17 +20,29 @@
 
 #ifndef KDEVPLATFORM_PROJECTUTILS_H
 #define KDEVPLATFORM_PROJECTUTILS_H
+
 #include "projectexport.h"
+
+#include <QList>
 
 class QMenu;
 
 namespace KDevelop {
+
 class ProjectBaseItem;
+class ProjectFileItem;
+
 /**
  * Adds menu entries for all parent folders of the given item, each containing all the project
  * items for the folder, at the end of the given menu.
  * */
 KDEVPLATFORMPROJECT_EXPORT void populateParentItemsMenu( ProjectBaseItem* item, QMenu* menu );
+
+/**
+ * Returns all the files that have @p projectItem as ancestor
+ */
+KDEVPLATFORMPROJECT_EXPORT QList<ProjectFileItem*> allFiles(ProjectBaseItem* projectItem);
+
 }
 
 #endif // KDEVPLATFORM_PROJECTUTILS_H

shell/project.cpp
@@ -175,36 +175,6 @@ public:
         }
     }
 
-    QList<ProjectFileItem*> recurseFiles( ProjectBaseItem * projectItem )
-    {
-        QList<ProjectFileItem*> files;
-        if ( ProjectFolderItem * folder = projectItem->folder() )
-        {
-            QList<ProjectFolderItem*> folder_list = folder->folderList();
-            for ( QList<ProjectFolderItem*>::Iterator it = folder_list.begin(); it != folder_list.end(); ++it )
-            {
-                files += recurseFiles( ( *it ) );
-            }
-
-            QList<ProjectTargetItem*> target_list = folder->targetList();
-            for ( QList<ProjectTargetItem*>::Iterator it = target_list.begin(); it != target_list.end(); ++it )
-            {
-                files += recurseFiles( ( *it ) );
-            }
-
-            files += folder->fileList();
-        }
-        else if ( ProjectTargetItem * target = projectItem->target() )
-        {
-            files += target->fileList();
-        }
-        else if ( ProjectFileItem * file = projectItem->file() )
-        {
-            files.append( file );
-        }
-        return files;
-    }
-
     QList<ProjectBaseItem*> itemsForPath( const IndexedString& path ) const
     {
         if ( path.isEmpty() ) {