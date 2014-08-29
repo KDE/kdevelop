fa9c448017f9af98f67e50c3b81f87d8dc8cbb6d
l10n daemon script <scripty@kde.org>
2014-08-29 08:53:22 +0000

    SVN_SILENT made messages (.desktop file)

--- plugins/welcomepage/kdevwelcomepageplugin.desktop.cmake
+++ plugins/welcomepage/kdevwelcomepageplugin.desktop.cmake
@@ -1,8 +1,36 @@
 [Desktop Entry]
 Type=Service
 Name=KDevelop Welcome Page
+Name[cs]=Uvítací stránka KDevelop
+Name[en_GB]=KDevelop Welcome Page
+Name[es]=Página de bienvenida de KDevelop
+Name[nl]=Welkomstpagina van KDevelop
+Name[pt]=Página de Boas-Vindas do KDevelop
+Name[pt_BR]=Página de boas-vindas do KDevelop
+Name[sk]=Uvítacia stránka KDevelop
+Name[sv]=KDevelop välkomstsida
+Name[uk]=Сторінка вітання KDevelop
+Name[x-test]=xxKDevelop Welcome Pagexx
 GenericName=Welcome Page
+GenericName[en_GB]=Welcome Page
+GenericName[es]=Página de bienvenida
+GenericName[nl]=Welkomstpagina
+GenericName[pt]=Página de Boas-Vindas
+GenericName[pt_BR]=Página de boas-vindas
+GenericName[sk]=Uvítacia stránka
+GenericName[sv]=Välkomstsida
+GenericName[uk]=Сторінка вітання
+GenericName[x-test]=xxWelcome Pagexx
 Comment=Configure which macros and include directories/files will be added to the parser during project parsing.
+Comment[en_GB]=Configure which macros and include directories/files will be added to the parser during project parsing.
+Comment[es]=Configurar las macros y los directorios/archivos a incluir que se añadirán al analizador sintáctico durante el análisis de los proyectos.
+Comment[nl]=Stel in welke macro's en ingevoegde mappen/bestanden toegevoegd zullen worden aan de parser bij het ontleden van het project.
+Comment[pt]=Define quais as macros e ficheiros/pastas incluídos que serão adicionados ao processador durante o processamento do projecto.
+Comment[pt_BR]=Configura quais macros e arquivos/pastas incluídos serão adicionados ao processador durante o processamento do projeto.
+Comment[sk]=Nastaviť, ktoré makrá a zahrnuté adresáre/súbory sa pridajú do spracovača počas spracovania projektu.
+Comment[sv]=Anpassa vilka makron och filer eller kataloger att inkludera som läggs till i tolken under projekttolkning.
+Comment[uk]=Налаштуйте які макроси та включені каталоги або файли буде додано для обробника під час обробки проекту.
+Comment[x-test]=xxConfigure which macros and include directories/files will be added to the parser during project parsing.xx
 
 Icon=kdevelop
 ServiceTypes=KDevelop/Plugin
