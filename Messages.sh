5dbdc607e24f797e87bbdbcf3312147772997f86
Denis Steckelmacher <steckdenis@yahoo.fr>
2014-09-18 12:00:45 +0200

    Don't translate useless strings in lib/, but translate i18n() calls in the QML property widgets

--- Messages.sh
+++ Messages.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-files=`find . -name \*.rc -o -name \*.ui | grep -v '/tests/'`
-if [ "x$files" != "x" ]; then
-       $EXTRACTRC $files >> rc.cpp
-fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kdevqmljssupport.pot
-$XGETTEXT_QT `find libs -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -j -o $podir/kdevqmljssupport.pot
-rm -f rc.cpp
+$XGETTEXT `find duchain codecompletion navigation -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/kdevqmljssupport.pot
