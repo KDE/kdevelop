509358afeb8a96414005fbf680556c4ee3850de6
Kevin Funk <kfunk@kde.org>
2014-11-09 10:39:32 +0100

    Make compile

--- shell/projectsourcepage.cpp
+++ shell/projectsourcepage.cpp
@@ -161,7 +161,8 @@ void ProjectSourcePage::checkoutVcsProject()
     m_ui->get->setEnabled(false);
     m_ui->creationProgress->setValue(m_ui->creationProgress->minimum());
     connect(job, &VcsJob::result, this, &ProjectSourcePage::projectReceived);
-    connect(job, static_cast<void(VcsJob::*)(KJob*,unsigned long)>(&VcsJob::percent), this, &ProjectSourcePage::progressChanged);
+    // Can't use new signal-slot syntax, KJob::percent is private :/
+    connect(job, SIGNAL(percent(KJob*,ulong)), SLOT(progressChanged(KJob*,ulong)));
     connect(job, &VcsJob::infoMessage, this, &ProjectSourcePage::infoMessage);
     ICore::self()->runController()->registerJob(job);
 }
