54e9a21c798f85ae5f4b715a73e687f88a44f213
Kevin Funk <kfunk@kde.org>
2014-11-17 22:24:42 +0100

    Reformat source

--- shell/settings/editstyledialog.cpp
+++ shell/settings/editstyledialog.cpp
@@ -1,22 +1,23 @@
 /* This file is part of KDevelop
-*  Copyright (C) 2008 Cédric Pasteur <cedric.pasteur@free.fr>
+ *  Copyright (C) 2008 Cédric Pasteur <cedric.pasteur@free.fr>
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
 
-*/
+ */
+
 #include "editstyledialog.h"
 
 #include <QPushButton>
@@ -32,32 +33,34 @@ Boston, MA 02110-1301, USA.
 #include <interfaces/isourceformatter.h>
 #include <KConfigGroup>
 
-using KDevelop::ISourceFormatter;
-using KDevelop::SettingsWidget;
-using KDevelop::SourceFormatterStyle;
+using namespace KDevelop;
 
 EditStyleDialog::EditStyleDialog(ISourceFormatter* formatter, const QMimeType& mime,
-        const SourceFormatterStyle& style, QWidget* parent)
-		: QDialog(parent), m_sourceFormatter(formatter), m_mimeType(mime), m_style( style )
+                                 const SourceFormatterStyle& style, QWidget* parent)
+    : QDialog(parent)
+    , m_sourceFormatter(formatter)
+    , m_mimeType(mime)
+    , m_style(style)
 {
-	m_content = new QWidget();
-	m_ui.setupUi(m_content);
+    m_content = new QWidget();
+    m_ui.setupUi(m_content);
 
-	QVBoxLayout *mainLayout = new QVBoxLayout(this);
-	mainLayout->addWidget(m_content);
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    mainLayout->addWidget(m_content);
 
-    auto buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok|QDialogButtonBox::Cancel);
+    auto buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
     auto okButton = buttonBox->button(QDialogButtonBox::Ok);
     okButton->setDefault(true);
     connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
     connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
     mainLayout->addWidget(buttonBox);
 
-	m_settingsWidget = m_sourceFormatter->editStyleWidget(mime);
-	init();
+    m_settingsWidget = m_sourceFormatter->editStyleWidget(mime);
+    init();
 
-	if (m_settingsWidget)
-		m_settingsWidget->load(style);
+    if (m_settingsWidget) {
+        m_settingsWidget->load(style);
+    }
 }
 
 EditStyleDialog::~EditStyleDialog()
@@ -66,57 +69,56 @@ EditStyleDialog::~EditStyleDialog()
 
 void EditStyleDialog::init()
 {
-	// add plugin settings widget
-	if(m_settingsWidget) {
-		QVBoxLayout *layout = new QVBoxLayout(m_ui.settingsWidgetParent);
-		layout->addWidget(m_settingsWidget);
-		m_ui.settingsWidgetParent->setLayout(layout);
-		connect(m_settingsWidget, &SettingsWidget::previewTextChanged,
-			this, &EditStyleDialog::updatePreviewText);
-	}
+    // add plugin settings widget
+    if (m_settingsWidget) {
+        QVBoxLayout* layout = new QVBoxLayout(m_ui.settingsWidgetParent);
+        layout->addWidget(m_settingsWidget);
+        m_ui.settingsWidgetParent->setLayout(layout);
+        connect(m_settingsWidget, &SettingsWidget::previewTextChanged,
+                this, &EditStyleDialog::updatePreviewText);
+    }
 
-	m_document = KTextEditor::Editor::instance()->createDocument(this);
-	m_document->setReadWrite(false);
-	m_document->setHighlightingMode(m_style.modeForMimetype(m_mimeType));
+    m_document = KTextEditor::Editor::instance()->createDocument(this);
+    m_document->setReadWrite(false);
+    m_document->setHighlightingMode(m_style.modeForMimetype(m_mimeType));
 
-	m_view = m_document->createView(m_ui.textEditor);
-	QVBoxLayout *layout2 = new QVBoxLayout(m_ui.textEditor);
-	layout2->addWidget(m_view);
-	m_ui.textEditor->setLayout(layout2);
-	m_view->show();
+    m_view = m_document->createView(m_ui.textEditor);
+    QVBoxLayout* layout2 = new QVBoxLayout(m_ui.textEditor);
+    layout2->addWidget(m_view);
+    m_ui.textEditor->setLayout(layout2);
+    m_view->show();
 
-	KTextEditor::ConfigInterface *iface =
-	    qobject_cast<KTextEditor::ConfigInterface*>(m_view);
-	if (iface) {
-		iface->setConfigValue("dynamic-word-wrap", false);
-		iface->setConfigValue("icon-bar", false);
-	}
+    KTextEditor::ConfigInterface* iface =
+        qobject_cast<KTextEditor::ConfigInterface*>(m_view);
+    if (iface) {
+        iface->setConfigValue("dynamic-word-wrap", false);
+        iface->setConfigValue("icon-bar", false);
+    }
 
-	if (m_sourceFormatter) {
-		QString text = m_sourceFormatter->previewText(m_style, m_mimeType);
-		updatePreviewText(text);
-	}
+    if (m_sourceFormatter) {
+        QString text = m_sourceFormatter->previewText(m_style, m_mimeType);
+        updatePreviewText(text);
+    }
 }
 
 void EditStyleDialog::updatePreviewText(const QString &text)
 {
-	m_document->setReadWrite(true);
-	m_style.setContent( content() );
-	if (m_sourceFormatter) {
-		m_document->setText(m_sourceFormatter->formatSourceWithStyle( m_style, text, QUrl(), m_mimeType ));
-	} else {
-		m_document->setText( i18n( "No Source Formatter available" ) );
-	}
+    m_document->setReadWrite(true);
+    m_style.setContent(content());
+    if (m_sourceFormatter) {
+        m_document->setText(m_sourceFormatter->formatSourceWithStyle(m_style, text, QUrl(), m_mimeType));
+    } else {
+        m_document->setText(i18n("No Source Formatter available"));
+    }
 
-	m_view->setCursorPosition( KTextEditor::Cursor( 0, 0 ) );
-	m_document->setReadWrite(false);
+    m_view->setCursorPosition(KTextEditor::Cursor(0, 0));
+    m_document->setReadWrite(false);
 }
 
 QString EditStyleDialog::content()
 {
-	if(m_settingsWidget)
-		return m_settingsWidget->save();
-	return QString();
+    if (m_settingsWidget) {
+        return m_settingsWidget->save();
+    }
+    return QString();
 }
-
-// kate: indent-mode cstyle; space-indent off; tab-width 4;
--- shell/settings/editstyledialog.h
+++ shell/settings/editstyledialog.h
@@ -1,22 +1,23 @@
 /* This file is part of KDevelop
-*  Copyright (C) 2008 Cédric Pasteur <cedric.pasteur@free.fr>
+ *  Copyright (C) 2008 Cédric Pasteur <cedric.pasteur@free.fr>
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; see the file COPYING.  If not, write to
-the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301, USA.
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
 
-*/
+ */
+
 #ifndef KDEVPLATFORM_EDITSTYLEDIALOG_H
 #define KDEVPLATFORM_EDITSTYLEDIALOG_H
 
@@ -25,49 +26,47 @@ Boston, MA 02110-1301, USA.
 #include <interfaces/isourceformatter.h>
 #include "ui_editstyledialog.h"
 
-namespace KTextEditor
-{
-	class Document;
-	class View;
+namespace KTextEditor {
+class Document;
+class View;
 }
-namespace KDevelop
-{
-        class ISourceFormatter;
-        class SettingsWidget;
-        class SourceFormatterStyle;
+namespace KDevelop {
+class ISourceFormatter;
+class SettingsWidget;
+class SourceFormatterStyle;
 }
 
 /** \short A simple dialog to add preview around a \ref SettingsWidget
-*/
-class EditStyleDialog : public QDialog
+ */
+class EditStyleDialog
+    : public QDialog
 {
-		Q_OBJECT
+    Q_OBJECT
 
-	public:
-		EditStyleDialog(KDevelop::ISourceFormatter* formatter, const QMimeType& mime,
-		        const KDevelop::SourceFormatterStyle&, QWidget* parent = 0);
-		virtual ~EditStyleDialog();
+public:
+    EditStyleDialog(KDevelop::ISourceFormatter* formatter, const QMimeType& mime,
+                    const KDevelop::SourceFormatterStyle&, QWidget* parent = 0);
+    virtual ~EditStyleDialog();
 
-		/** \return The string representing the style given by the \ref SettingsWidget.
-		*/
-		QString content();
+    /** \return The string representing the style given by the \ref SettingsWidget.
+     */
+    QString content();
 
-	protected:
-		void init();
+protected:
+    void init();
 
-	public slots:
-		void updatePreviewText(const QString &text);
+public slots:
+    void updatePreviewText(const QString &text);
 
-	private:
-		KDevelop::ISourceFormatter *m_sourceFormatter;
-		KTextEditor::View *m_view;
-		KTextEditor::Document *m_document;
-		KDevelop::SettingsWidget *m_settingsWidget;
-		QMimeType m_mimeType;
-		QWidget *m_content;
-		Ui::EditStyle m_ui;
-		KDevelop::SourceFormatterStyle m_style;
+private:
+    KDevelop::ISourceFormatter* m_sourceFormatter;
+    KTextEditor::View* m_view;
+    KTextEditor::Document* m_document;
+    KDevelop::SettingsWidget* m_settingsWidget;
+    QMimeType m_mimeType;
+    QWidget* m_content;
+    Ui::EditStyle m_ui;
+    KDevelop::SourceFormatterStyle m_style;
 };
 
 #endif // KDEVPLATFORM_EDITSTYLEDIALOG_H
-// kate: indent-mode cstyle; space-indent off; tab-width 4;
