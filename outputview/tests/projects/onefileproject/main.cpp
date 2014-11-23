e12fef9fcd9d6d4e903e5f8dbc26301438e57757
Milian Wolff <mail@milianw.de>
2014-11-23 17:59:44 +0100

    Remove useless code that triggers cpp-check warnings.
    We just need the file to be there, the contents are irrelevant for
    our tests.

--- outputview/tests/projects/onefileproject/main.cpp
+++ outputview/tests/projects/onefileproject/main.cpp
@@ -1,29 +1,4 @@
-#include <iostream>
-
-int main( int argc, char **argv )
+int main()
 {
-    RingBuffer myBuffer;
-    std::vector<uint64_t> testVector;
-
-    std::srand( std::time ( 0 ) );
-
-    fillBuffer( myBuffer, testVector );
-    testWithOnlyknown( myBuffer, testVector );
-    testWithSomeKnown( myBuffer, testVector);
-
-    std::cout << "Done Testing" << std::endl;
-    int i;
-    if(i > 100)
-    {
-        /// This is of course only to test cpp check
-        std::string input_string;
-        char *str = (char *)malloc(strlen(input_string)+1);
-        strcpy(str, input_string); /* What if malloc() fails? */
-        if(input_string == "")
-        {
-        }
-    }
-
     return 0;
 }
-
