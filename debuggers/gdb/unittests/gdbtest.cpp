c8ef0aabef85714d9b35203b2bd96303d8697475
Kevin Funk <kfunk@kde.org>
2014-12-04 09:31:15 +0100

    Hack for making build with CMake 2.8 and Ninja
    CMake 2.8 creates faulty build.ninja files if a path contains spaces.
    This leads to an error when ninja is invoked later on.
    
    Excert:
    CMake 2.8.12.2 (wrong):
      DEP_FILE = debuggers/gdb/unittests/path with
    space/CMakeFiles/spacedebugee.dir/spacedebugee.cpp.o.d
    CMake 3.0+:
      DEP_FILE = debuggers/gdb/unittests/path\ with\
    space/CMakeFiles/spacedebugee.dir/spacedebugee.cpp.o.d

--- debuggers/gdb/unittests/gdbtest.cpp
+++ debuggers/gdb/unittests/gdbtest.cpp
@@ -1835,6 +1835,7 @@ void GdbTest::testDebugInExternalTerminal()
 // see: https://bugs.kde.org/show_bug.cgi?id=339231
 void GdbTest::testPathWithSpace()
 {
+#ifdef HAVE_PATH_WITH_SPACES_TEST
     TestDebugSession* session = new TestDebugSession;
 
     auto debugee = findExecutable("path with space/spacedebugee");
@@ -1847,6 +1848,7 @@ void GdbTest::testPathWithSpace()
     QCOMPARE(session->breakpointController()->breakpointState(b), KDevelop::Breakpoint::CleanState);
     session->run();
     WAIT_FOR_STATE(session, DebugSession::EndedState);
+#endif
 }
 
 void GdbTest::waitForState(GDBDebugger::DebugSession *session, DebugSession::DebuggerState state,
