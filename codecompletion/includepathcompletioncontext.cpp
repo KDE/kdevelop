KDE/kdevelop

commit 721ef97d98c0cbacdb7ad2caafefb7ebaeb6d8cd

Use ClangHelpers::headerExtensions and ::sourceExtensions for buddies.
Also cleanup the code here and there a bit more.

REVIEW: 121648

codecompletion/includepathcompletioncontext.cpp
@@ -103,8 +103,7 @@ QList<KDevelop::IncludeItem> includeItemsForUrl(const QUrl& url, const IncludePa
                 continue;
             }
 
-            const auto suffix = dirIterator.fileInfo().suffix();
-            if (!suffix.isEmpty() && !ClangHelpers::headerExtensions().contains(suffix)) {
+            if (!ClangHelpers::isHeader(item.name)) {
                 continue;
             }
 

documentfinderhelpers.cpp
@@ -21,7 +21,7 @@
 
 #include "documentfinderhelpers.h"
 
-#include "duchain/clangparsingenvironment.h"
+#include "duchain/clanghelpers.h"
 
 #include <language/duchain/duchain.h>
 #include <language/duchain/declaration.h>
@@ -32,7 +32,6 @@
 #include <KDesktopFile>
 #include <KConfigGroup>
 
-
 #include <QSet>
 #include <QStandardPaths>
 #include <QMimeDatabase>
@@ -40,36 +39,7 @@
 
 using namespace KDevelop;
 
-/**
- * @return all extensions which match the given @p mimeType.
- */
-QSet<QString> getExtensionsByMimeType(const QString& mimeType)
-{
-    QMimeDatabase db;
-    auto mime = db.mimeTypeForName(mimeType);
-    if (!mime.isValid()) {
-        return {};
-    }
-
-    QSet<QString> extensions;
-    foreach(const auto& pattern, mime.globPatterns()) {
-        if (pattern.startsWith("*.")) {
-            extensions << pattern.mid(2);
-        }
-    }
-
-    return extensions;
-}
-
-QSet<QString> getHeaderFileExtensions()
-{
-    return getExtensionsByMimeType("text/x-c++hdr") | getExtensionsByMimeType("text/x-chdr");
-}
-
-QSet<QString> getSourceFileExtensions()
-{
-    return getExtensionsByMimeType("text/x-c++src") | getExtensionsByMimeType("text/x-csrc");
-}
+namespace {
 
 enum FileType {
     Unknown, ///< Doesn't belong to C++
@@ -194,9 +164,9 @@ QPair<QString,FileType> basePathAndTypeForUrl(const QUrl &url)
         basePath = path.left(idxDot);
         if (idxDot + 1 < path.length()) {
             QString extension = path.mid(idxDot + 1);
-            if (getHeaderFileExtensions().contains(extension)) {
+            if (ClangHelpers::isHeader(extension)) {
                 fileType = Header;
-            } else if (getSourceFileExtensions().contains(extension)) {
+            } else if (ClangHelpers::isSource(extension)) {
                 fileType = Source;
             }
         }
@@ -205,6 +175,8 @@ QPair<QString,FileType> basePathAndTypeForUrl(const QUrl &url)
     }
 
     return qMakePair(basePath, fileType);
+}
+
 }
 
 QStringList DocumentFinderHelpers::mimeTypesList()
@@ -271,10 +243,14 @@ QVector< QUrl > DocumentFinderHelpers::getPotentialBuddies(const QUrl &url)
 
     // Depending on the buddy's file type we either generate source extensions (for headers)
     // or header extensions (for sources)
-    const auto& extensions = ( type.second == Header ? getSourceFileExtensions() : getHeaderFileExtensions() );
+    const auto& extensions = ( type.second == Header ? ClangHelpers::sourceExtensions() : ClangHelpers::headerExtensions() );
     QVector< QUrl > buddies;
     for(const QString& extension : extensions) {
-        buddies.append(QUrl::fromLocalFile(type.first + '.' + extension));
+        if (!extension.contains('.')) {
+            buddies.append(QUrl::fromLocalFile(type.first + '.' + extension));
+        } else {
+            buddies.append(QUrl::fromLocalFile(type.first + extension));
+        }
     }
 
     // Also ask DUChain for a guess

commit 21254339b37493cb5235521f9f46de191f7172b2

Do not apply header-check to folders or files without extension.
This fixes the unit test which also spotted that "iostream" was
not included in the code completion. The reason was that this
is not detected as a header by us, as it has no extension.

Furthermore, we also excluded subfolders here, as they are also not
matched by the isHeader check.

This regression was introduced by me in rev 721ef97d98c0cbacdb7.

codecompletion/includepathcompletioncontext.cpp
@@ -103,11 +103,16 @@ QList<KDevelop::IncludeItem> includeItemsForUrl(const QUrl& url, const IncludePa
                 continue;
             }
 
-            if (!ClangHelpers::isHeader(item.name)) {
+            const auto info = dirIterator.fileInfo();
+            item.isDirectory = info.isDir();
+
+            // filter files that are not a header
+            // note: system headers sometimes don't have any extension, and we still want to show those
+            if (!item.isDirectory && item.name.contains('.') && !ClangHelpers::isHeader(item.name)) {
                 continue;
             }
 
-            const QString fullPath = dirIterator.fileInfo().canonicalFilePath();
+            const QString fullPath = info.canonicalFilePath();
             if (foundIncludePaths.contains(fullPath)) {
                 continue;
             } else {
@@ -115,7 +120,6 @@ QList<KDevelop::IncludeItem> includeItemsForUrl(const QUrl& url, const IncludePa
             }
 
             item.basePath = searchPath.toUrl();
-            item.isDirectory = dirIterator.fileInfo().isDir();
             item.pathNumber = pathNumber;
 
             includeItems << item;