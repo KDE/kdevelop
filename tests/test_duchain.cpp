KDE/kdevelop

commit af32e19b9b566e4d46f344d80aee91ac02d5e911

Try to handle the ElaboratedType.
This type currently is not exposed through the libclang interface.
So let's use clang_getCanonicalType on it to get at least some data.

Of course the clang_getCanonicalType will strip away all typedef
information, but it's still better than nothing at all.

REVIEW: 123914

duchain/builder.cpp
@@ -488,10 +488,21 @@ struct Visitor
         return t;
     }
 
-    template<CXTypeKind TK, EnableIf<TK == CXType_Vector || TK == CXType_Unexposed> = dummy>
+    template<CXTypeKind TK, EnableIf<TK == CXType_Vector> = dummy>
     AbstractType *createType(CXType type, CXCursor /*parent*/)
     {
         return createDelayedType(type);
+    }
+
+    template<CXTypeKind TK, EnableIf<TK == CXType_Unexposed> = dummy>
+    AbstractType *createType(CXType type, CXCursor parent)
+    {
+        // Maybe it's the ElaboratedType. E.g.: "struct Type foo();" or "NS::Type foo();" or "void foo(enum Enum e);" e.t.c.
+        auto oldType = type;
+
+        type = clang_getCanonicalType(type);
+        bool isElaboratedType = type.kind != CXType_FunctionProto && type.kind != CXType_FunctionNoProto && type.kind != CXType_Unexposed && type.kind != CXType_Invalid;
+        return !isElaboratedType ? createDelayedType(oldType) : makeType(type, parent);
     }
 
     template<CXCursorKind CK, EnableIf<CursorKindTraits::isIdentifiedType(CK) && !CursorKindTraits::isAliasType(CK) && CK != CXCursor_EnumConstantDecl> = dummy>
@@ -1057,6 +1068,7 @@ AbstractType *Visitor::makeType(CXType type, CXCursor parent)
     UseKind(CXType_ObjCId);
     UseKind(CXType_ObjCClass);
     UseKind(CXType_ObjCSel);
+    UseKind(CXType_NullPtr);
     case CXType_Invalid:
         return nullptr;
     default:

tests/test_duchain.cpp
@@ -30,6 +30,7 @@
 #include <language/duchain/parsingenvironment.h>
 #include <language/duchain/problem.h>
 #include <language/duchain/types/integraltype.h>
+#include <language/duchain/types/functiontype.h>
 #include <language/duchain/duchainutils.h>
 #include <language/duchain/classdeclaration.h>
 #include <language/duchain/abstractfunctiondeclaration.h>
@@ -108,6 +109,7 @@ struct ExpectedComment
     QString comment;
 };
 Q_DECLARE_METATYPE(ExpectedComment)
+Q_DECLARE_METATYPE(AbstractType::WhichType)
 
 void TestDUChain::testComments()
 {
@@ -155,6 +157,50 @@ void TestDUChain::testComments_data()
     QTest::newRow("comment-doxygen-tags")
         << "/** @see bar()\n@param a foo\n*/\nvoid foo(int a);\nvoid bar();"
         << ExpectedComment{"foo", "bar()\na\nfoo"};
+}
+
+void TestDUChain::testElaboratedType()
+{
+    QFETCH(QString, code);
+    QFETCH(AbstractType::WhichType, type);
+
+    TestFile file(code, "cpp");
+    QVERIFY(file.parseAndWait());
+
+    DUChainReadLocker lock;
+    auto top = file.topContext();
+    QVERIFY(top);
+    QCOMPARE(file.topContext()->localDeclarations().size(), 2);
+
+    auto decl = file.topContext()->localDeclarations()[1];
+    QVERIFY(decl);
+
+    auto function = dynamic_cast<FunctionDeclaration*>(decl);
+    QVERIFY(function);
+
+    auto functionType = function->type<FunctionType>();
+    QVERIFY(functionType);
+
+    QVERIFY(functionType->returnType()->whichType() != AbstractType::TypeDelayed);
+
+    QEXPECT_FAIL("typedef", "After using clang_getCanonicalType on ElaboratedType all typedef information get's stripped away", Continue);
+    QCOMPARE(functionType->returnType()->whichType(), type);
+}
+
+void TestDUChain::testElaboratedType_data()
+{
+    QTest::addColumn<QString>("code");
+    QTest::addColumn<AbstractType::WhichType>("type");
+
+    QTest::newRow("namespace")
+        << "namespace NS{struct Type{};} struct NS::Type foo();"
+        << AbstractType::TypeStructure;
+    QTest::newRow("enum")
+        << "enum Enum{}; enum Enum foo();"
+        << AbstractType::TypeEnumeration;
+    QTest::newRow("typedef")
+        << "namespace NS{typedef int type;} NS::type foo();"
+        << AbstractType::TypeAlias;
 }
 
 void TestDUChain::testInclude()

tests/test_duchain.h
@@ -40,6 +40,9 @@ private slots:
 
     void testComments();
     void testComments_data();
+    void testElaboratedType();
+    void testElaboratedType_data();
+
     void testInclude();
     void testIncludeLocking();
     void testReparse();

commit 5e8146d8d40bbb61992642a559d7521d9a13e2b9

Fix regression introduced by af32e19b

duchain/builder.cpp
@@ -515,7 +515,8 @@ struct Visitor
         auto oldType = type;
 
         type = clang_getCanonicalType(type);
-        bool isElaboratedType = type.kind != CXType_FunctionProto && type.kind != CXType_FunctionNoProto && type.kind != CXType_Unexposed && type.kind != CXType_Invalid;
+        bool isElaboratedType = type.kind != CXType_FunctionProto && type.kind != CXType_FunctionNoProto && type.kind != CXType_Unexposed && type.kind != CXType_Invalid && type.kind != CXType_Record;
+
         return !isElaboratedType ? createDelayedType(oldType) : makeType(type, parent);
     }
 

tests/test_duchain.cpp
@@ -181,6 +181,7 @@ void TestDUChain::testElaboratedType()
     auto functionType = function->type<FunctionType>();
     QVERIFY(functionType);
 
+    QEXPECT_FAIL("namespace", "The ElaboratedType is not exposed through the libclang interface, not much we can do here", Abort);
     QVERIFY(functionType->returnType()->whichType() != AbstractType::TypeDelayed);
 
     QEXPECT_FAIL("typedef", "After using clang_getCanonicalType on ElaboratedType all typedef information get's stripped away", Continue);
@@ -354,6 +355,12 @@ void TestDUChain::testTemplate()
 
     QCOMPARE(file.topContext()->findDeclarations(QualifiedIdentifier("foo")).size(), 1);
     QCOMPARE(file.topContext()->findDeclarations(QualifiedIdentifier("foo::bar")).size(), 1);
+
+    auto mainCtx = file.topContext()->localDeclarations().last()->internalContext()->childContexts().first();
+    QVERIFY(mainCtx);
+    auto myFoo = mainCtx->localDeclarations().first();
+    QVERIFY(myFoo);
+    QCOMPARE(myFoo->abstractType()->toString().remove(' '), QStringLiteral("foo<int>"));
 }
 
 void TestDUChain::testNamespace()