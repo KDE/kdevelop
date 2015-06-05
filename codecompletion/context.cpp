b8f1c436d6186bed0e8dcbd643f47a44af447d68
Milian Wolff <mail@milianw.de>
2015-06-05 21:11:41 +0200

    Remove obsolete include

--- codecompletion/context.cpp
+++ codecompletion/context.cpp
@@ -35,9 +35,7 @@
 #include "../util/clangdebug.h"
 #include "../util/clangtypes.h"
 #include "../duchain/parsesession.h"
-#include "../duchain/cursorkindtraits.h"
 #include "../duchain/navigationwidget.h"
-
 
 #include <memory>
 
