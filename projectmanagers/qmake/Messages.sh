36b71941f8cdb2b87b3f913a951d4db8c4055ee9
Kevin Funk <kfunk@kde.org>
2015-08-19 11:33:34 +0200

    QMake: Fix translation domains

--- projectmanagers/qmake/Messages.sh
+++ projectmanagers/qmake/Messages.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-files=`find . -name \*.rc -o -name \*.ui | grep -v '/qmakebuilder/' | grep -v '/app_templates/' | grep -v '/tests/'`
-if [ "x$files" != "x" ]; then
-       $EXTRACTRC $files >> rc.cpp
-fi
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/qmakebuilder/' | grep -v '/tests/'` -o $podir/kdevqmake.pot
+$EXTRACTRC `find . -name \*.rc -o -name \*.ui | grep -v '/tests/'` >>rc.cpp
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kdevqmakemanager.pot
 rm -f rc.cpp
