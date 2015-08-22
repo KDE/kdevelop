e0b46fd256f312d2e45036046dc6ecd847e3262d
Kevin Funk <kfunk@kde.org>
2015-08-22 18:47:48 +0200

    qmljs: Fixup Messages.sh

--- languages/qmljs/Messages.sh
+++ languages/qmljs/Messages.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-$XGETTEXT `find duchain codecompletion navigation -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/kdevqmljssupport.pot
+$EXTRACTRC `find . -name \*.rc` `find . -name \*.ui` >>rc.cpp
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp -o -name \*.h | grep -v '/tests/'` -o $podir/kdevqmljs.pot
+rm -f rc.cpp
