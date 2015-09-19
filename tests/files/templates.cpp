KDE/kdevelop

commit d0d35de27f6fbab9ecc04ab4e1896bdad935e222

Adjust to Clang changes regarding auto type deduction
See also: http://reviews.llvm.org/D11976

duchain/builder.cpp
@@ -115,10 +115,11 @@ AbstractType* createDelayedType(CXType type)
 {
     auto t = new DelayedType;
 
-    // TODO: Fix clang_getTypeSpelling not to prepend type modifiers for auto types
     QString typeName = ClangString(clang_getTypeSpelling(type)).toString();
+#if CINDEX_VERSION_MINOR < 30
     typeName.remove(QStringLiteral("const "));
     typeName.remove(QStringLiteral("volatile "));
+#endif
 
     t->setIdentifier(IndexedTypeIdentifier(typeName));
     return t;
@@ -613,8 +614,12 @@ struct Visitor
         auto numTA = clang_Type_getNumTemplateArguments(type);
         Q_ASSERT(numTA != -1);
 
-        auto cst = new ClassSpecializationType;
         auto typeDecl = clang_getTypeDeclaration(type);
+
+        if (!declaration && typeDecl.kind == CXCursor_NoDeclFound) {
+            // clang_getTypeDeclaration doesn't handle all types, fall back to delayed type...
+            return createDelayedType(type);
+        }
 
         QStringList typesStr;
         QString tStr = ClangString(clang_getTypeSpelling(type)).toString();
@@ -624,6 +629,8 @@ struct Visitor
             typesStr.append(*iter);
             ++iter;
         }
+
+        auto cst = new ClassSpecializationType;
 
         for (int i = 0; i < numTA; i++) {
             auto argumentType = clang_Type_getTemplateArgumentAsType(type, i);

tests/files/templates.cpp
@@ -1,3 +1,5 @@
+#include <clang-c/Index.h>
+
 /// "toString" : "struct myTemplate",
 /// "kind" : "Type"
 template<class T>
@@ -32,10 +34,20 @@ using TypeAliasTemplate = T;
 
 /// "type" : { "toString" : "Class_volatile_const< int >" }
 Class_volatile_const<int> instance;
-/// "type" : { "toString" : "TemplateTest< const TemplateTest< int, 100 >&, 30 >" }
-TemplateTest<const TemplateTest<int, 100>&, 30> tst;
+
+myTemplate<myTemplate<int>& > templRefParam;
+/// "type" : { "toString" : "myTemplate< myTemplate< int >& >", "EXPECT_FAIL": {"toString": "For some reasons reference gets lost. Need to investigate it further."} }
+auto autoTemplRefParam = templRefParam;
 /// "type" : { "toString" : "VariadicTemplate< int, double, bool >", "EXPECT_FAIL": {"toString": "No way to get variadic template arguments with LibClang"} }
 VariadicTemplate<int, double, bool> variadic;
+
+/// "type" : { "toString" : "TemplateTest< const TemplateTest< int, 100 >, 30 >" }
+TemplateTest<const TemplateTest<int, 100>, 30> tst;
+#if CINDEX_VERSION_MINOR >= 30
+auto tst2 = tst;
+/// "type" : { "toString" : "TemplateTest< const TemplateTest< int, 100 >, 30 >" }
+auto tst3 = tst2;
+#endif
 
 /*This example used to crash while building the type of Bar*/
 /// "type" : { "toString" : "Bar" }

tests/files/types.cpp
@@ -90,9 +90,4 @@ int main(int argc, char** argv)
     __int128 i128;
     /// "toString" : "unsigned __int128 ui128"
     unsigned __int128 ui128;
-    // TODO: get the actual type here somehow?
-    /// "toString" : "auto autoVar"
-    auto autoVar = 123;
-    /// "toString" : "const volatile auto autoVar2"
-    const volatile auto autoVar2 = 321;
 }

tests/test_duchain.cpp
@@ -407,7 +407,7 @@ void TestDUChain::testNamespace()
 
 void TestDUChain::testAutoTypeDeduction()
 {
-    TestFile file("auto foo = 5;\n", "cpp");
+    TestFile file("const volatile auto foo = 5;\n", "cpp");
     QVERIFY(file.parseAndWait());
 
     DUChainReadLocker lock;
@@ -418,8 +418,15 @@ void TestDUChain::testAutoTypeDeduction()
     QCOMPARE(ctx->findDeclarations(QualifiedIdentifier("foo")).size(), 1);
     Declaration* decl = ctx->findDeclarations(QualifiedIdentifier("foo"))[0];
     QCOMPARE(decl->identifier(), Identifier("foo"));
+#if CINDEX_VERSION_MINOR < 30
     QEXPECT_FAIL("", "No type deduction here unfortunately, missing API in Clang", Continue);
+#endif
     QVERIFY(decl->type<IntegralType>());
+#if CINDEX_VERSION_MINOR < 30
+    QCOMPARE(decl->toString(), QStringLiteral("const volatile auto foo"));
+#else
+    QCOMPARE(decl->toString(), QStringLiteral("const volatile int foo"));
+#endif
 }
 
 void TestDUChain::testTypeDeductionInTemplateInstantiation()
@@ -454,7 +461,9 @@ void TestDUChain::testTypeDeductionInTemplateInstantiation()
     QCOMPARE(decl->identifier(), Identifier("f"));
     decl = ctx->localDeclarations()[2];
     QCOMPARE(decl->identifier(), Identifier("i"));
+#if CINDEX_VERSION_MINOR < 30
     QEXPECT_FAIL("", "No type deduction here unfortunately, missing API in Clang", Continue);
+#endif
     QVERIFY(decl->type<IntegralType>());
 }
 

commit 46ad76d163cee74d6dc70d06f893c2b3f40ea3da

Fix not deduced auto type names
This fixes a regresion caused by d0d35de2

duchain/builder.cpp
@@ -116,10 +116,8 @@ AbstractType* createDelayedType(CXType type)
     auto t = new DelayedType;
 
     QString typeName = ClangString(clang_getTypeSpelling(type)).toString();
-#if CINDEX_VERSION_MINOR < 31
     typeName.remove(QStringLiteral("const "));
     typeName.remove(QStringLiteral("volatile "));
-#endif
 
     t->setIdentifier(IndexedTypeIdentifier(typeName));
     return t;

tests/files/templates.cpp
@@ -49,6 +49,13 @@ auto tst2 = tst;
 auto tst3 = tst2;
 #endif
 
+template<class Type>
+void test()
+{
+    /// "type" : { "toString" : "const volatile auto" }
+    const volatile auto type = Type();
+}
+
 /*This example used to crash while building the type of Bar*/
 /// "type" : { "toString" : "Bar" }
 template <typename T>