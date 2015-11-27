f78c39655cb3d5dacca43fde60c7e748efb10820
Milian Wolff <mail@milianw.de>
2015-11-27 21:36:30 +0100

    Don't access declaration without holding the DUChain lock.
    This hopefully fixes a crash in the declaration builder.
    
    BUG: 355685

--- languages/cpp/cppduchain/declarationbuilder.cpp
+++ languages/cpp/cppduchain/declarationbuilder.cpp
@@ -812,6 +812,8 @@ void DeclarationBuilder::classTypeOpened(AbstractType::Ptr type) {
 
 void DeclarationBuilder::closeDeclaration(bool forceInstance)
 {
+  bool assignInternalContext = false;
+
   {
     DUChainWriteLocker lock(DUChain::lock());
       
@@ -859,15 +861,16 @@ void DeclarationBuilder::closeDeclaration(bool forceInstance)
           ctx->deleteAllInstantiations();
       }
     }
+
+    if (lastContext() && !m_onlyComputeSimplified && currentDeclaration()->isFunctionDeclaration()) {
+      currentDeclaration<AbstractFunctionDeclaration>()->setInternalFunctionContext(lastContext());
+    }
+
+    assignInternalContext = lastContext()) && (lastContext()->type() != DUContext::Other || currentDeclaration()->isFunctionDeclaration());
   }
 
-  if (lastContext())
-  {
-    if (!m_onlyComputeSimplified && currentDeclaration()->isFunctionDeclaration())
-      currentDeclaration<AbstractFunctionDeclaration>()->setInternalFunctionContext(lastContext());
-
-    if(lastContext()->type() != DUContext::Other || currentDeclaration()->isFunctionDeclaration())
-      eventuallyAssignInternalContext();
+  if (assignInternalContext) {
+    eventuallyAssignInternalContext();
   }
 
   ifDebugCurrentFile( DUChainReadLocker lock(DUChain::lock()); kDebug() << "closing declaration" << currentDeclaration()->toString() << "type" << (currentDeclaration()->abstractType() ? currentDeclaration()->abstractType()->toString() : QString("notype")) << "last:" << (lastType() ? lastType()->toString() : QString("(notype)")); )
