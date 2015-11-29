482215c19bf3a536d967184f08abf9d9a65ff5da
Kevin Funk <kfunk@kde.org>
2015-11-29 15:23:50 +0100

    Dont hide/restore docks on double-click with RMB
    BUG: 356016

--- sublime/container.cpp
+++ sublime/container.cpp
@@ -50,7 +50,10 @@ class ContainerTabBar : public QTabBar
     Q_OBJECT
 
 public:
-    ContainerTabBar(Container* container) : QTabBar(container), m_container(container) {
+    ContainerTabBar(Container* container)
+        : QTabBar(container), m_container(container)
+    {
+        installEventFilter(this);
     }
 
     virtual bool event(QEvent* ev) override {
@@ -81,6 +84,25 @@ public:
             return;
         }
         QTabBar::mousePressEvent(event);
+    }
+
+    bool eventFilter(QObject* obj, QEvent* event) override
+    {
+        if (obj != this) {
+            return QObject::eventFilter(obj, event);
+        }
+
+        // TODO Qt6: Move to mouseDoubleClickEvent when fixme in qttabbar.cpp is resolved
+        // see "fixme Qt 6: move to mouseDoubleClickEvent(), here for BC reasons." in qtabbar.cpp
+        if (event->type() == QEvent::MouseButtonDblClick) {
+            // block tabBarDoubleClicked signals with RMB, see https://bugs.kde.org/show_bug.cgi?id=356016
+            auto mouseEvent = static_cast<const QMouseEvent*>(event);
+            if (mouseEvent->button() == Qt::MidButton) {
+                return true;
+            }
+        }
+
+        return QObject::eventFilter(obj, event);
     }
 
 Q_SIGNALS:
