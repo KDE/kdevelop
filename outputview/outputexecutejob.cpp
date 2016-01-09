eed623d762a981735effee081830ac3fcdd5676e
Kevin Funk <kfunk@kde.org>
2016-01-10 00:33:29 +0100

    OutputExecuteJob: Use "Finished" again
    Otherwise this looks too cumbersome for normal `make` invocations and
    the like.

--- outputview/outputexecutejob.cpp
+++ outputview/outputexecutejob.cpp
@@ -359,7 +359,7 @@ void OutputExecuteJob::childProcessExited( int exitCode, QProcess::ExitStatus ex
     } else {
         d->m_status = JobSucceeded;
         d->m_lineMaker->flushBuffers();
-        model()->appendLine( i18n("*** Finished with exit code 0 ***") );
+        model()->appendLine( i18n("*** Finished ***") );
         emitResult();
     }
 }
