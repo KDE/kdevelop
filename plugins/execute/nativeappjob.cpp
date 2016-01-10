bd76c16dbb227606d8415b1c455a6754104b8978
Kevin Funk <kfunk@kde.org>
2016-01-10 13:33:33 +0100

    Minor: Q_NULLPTR -> nullptr

--- plugins/execute/nativeappjob.cpp
+++ plugins/execute/nativeappjob.cpp
@@ -154,7 +154,7 @@ void NativeAppJob::start()
     foreach(KJob* j, ICore::self()->runController()->currentJobs()) {
         NativeAppJob* job = findNativeJob(j);
         if (job && job != this && job->m_cfgname == m_cfgname) {
-            QMessageBox::StandardButton button = QMessageBox::question(Q_NULLPTR, i18n("Job already running"), i18n("'%1' is already being executed. Should we kill the previous instance?", m_cfgname));
+            QMessageBox::StandardButton button = QMessageBox::question(nullptr, i18n("Job already running"), i18n("'%1' is already being executed. Should we kill the previous instance?", m_cfgname));
             if (button != QMessageBox::No)
                 j->kill();
         }
