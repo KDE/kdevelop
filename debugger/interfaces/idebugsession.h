08152a8fb68c40c39ef7d3b3e7caa8382f34ae0d
Kevin Funk <kfunk@kde.org>
2016-01-14 08:46:23 +0100

    Minor: Code reformatting

--- debugger/interfaces/idebugsession.h
+++ debugger/interfaces/idebugsession.h
@@ -54,24 +54,25 @@ public:
         EndedState
     };
 
-    enum event_t { program_state_changed = 1, 
-                   program_exited, 
-                   debugger_exited,
-                   // Emitted when the thread or frame that is selected in UI
-                   // changes.
-                   thread_or_frame_changed, 
-                   debugger_busy, 
-                   debugger_ready,
-                   // Raised when debugger believe that program start running.
-                   // Can be used to hide current line indicator.
-                   // Don't count on this being raise in all cases where
-                   // program is running.
-                   program_running,
-                   // Raise when the debugger is in touch with the program,
-                   // and should have access to its debug symbols. The program
-                   // is not necessary running yet, or might already exited,
-                   // or be otherwise dead.
-                   connected_to_program
+    enum event_t {
+        program_state_changed = 1,
+        program_exited,
+        debugger_exited,
+        // Emitted when the thread or frame that is selected in UI
+        // changes.
+        thread_or_frame_changed,
+        debugger_busy,
+        debugger_ready,
+        // Raised when debugger believe that program start running.
+        // Can be used to hide current line indicator.
+        // Don't count on this being raise in all cases where
+        // program is running.
+        program_running,
+        // Raise when the debugger is in touch with the program,
+        // and should have access to its debug symbols. The program
+        // is not necessary running yet, or might already exited,
+        // or be otherwise dead.
+        connected_to_program
     };
 
 public:
--- debugger/interfaces/iframestackmodel.h
+++ debugger/interfaces/iframestackmodel.h
@@ -30,67 +30,68 @@
 
 namespace KDevelop {
 
-    class IFrameStackModelPrivate;
-    
-    class KDEVPLATFORMDEBUGGER_EXPORT IFrameStackModel : public QAbstractItemModel
-    {
-        Q_OBJECT    
-    public:
-        
-        /** Stack frame */
-        struct FrameItem {
-            int nr;
-            QString name;
-            QUrl file;
-            /* If -1, it means that file is not necessary a source file,
-               but possibly a solib name.  */
-            int line;
-        };
-        
-        explicit IFrameStackModel(IDebugSession *session);
-        ~IFrameStackModel() override;
-        
-        IDebugSession* session() const;
-        
-        /** Sets the current thread to the specified number,
-           and sets the current frame to 0.  
-           Note that nothing prevents us from introducing 
-           setCurrentThreadAndFrame, but for all the cases when we
-           switch to a different thread we want frame 0.  */
-        virtual void setCurrentThread(int threadNumber) = 0;
-        virtual void setCurrentThread(const QModelIndex &index) = 0;
-        virtual int currentThread() const = 0;
-        virtual QModelIndex currentThreadIndex() const = 0;
+class IFrameStackModelPrivate;
 
-        /* Return the frame we wish to operate on.  This is always
-           in context of the current thread.  This may be -1 if
-           no frame is selected. This should only the be the case
-           if the thread has no stack as such -- e.g. because it's
-           running, or because it's exited.  */
-        virtual int currentFrame() const = 0;
-        virtual QModelIndex currentFrameIndex() const = 0;
-        virtual void setCurrentFrame(int frame) = 0;
-        
-        virtual FrameItem frame(const QModelIndex &index) = 0;
-        
-        virtual void fetchThreads() = 0;
-        virtual void fetchFrames(int threadNumber, int from, int to) = 0;
-        virtual void fetchMoreFrames() = 0;
+class KDEVPLATFORMDEBUGGER_EXPORT IFrameStackModel : public QAbstractItemModel
+{
+    Q_OBJECT
 
-    Q_SIGNALS:                            
-        /* FIXME: It might make for a more conscise interface if those
-           two were removed, and the clients react to thread_or_frame_changed
-           event and compare the current thread/frame in the framestack model
-           with the one they are displaying.  */
-        void currentThreadChanged(int thread);
-        void currentFrameChanged(int frame);
-        
-    private:
-        friend class IDebugSession;
-        QScopedPointer<IFrameStackModelPrivate> d;
-
-        virtual void handleEvent(IDebugSession::event_t event) = 0;
+public:
+    /** Stack frame */
+    struct FrameItem {
+        int nr;
+        QString name;
+        QUrl file;
+        /* If -1, it means that file is not necessary a source file,
+           but possibly a solib name.  */
+        int line;
     };
+
+    explicit IFrameStackModel(IDebugSession *session);
+    ~IFrameStackModel() override;
+
+    IDebugSession* session() const;
+
+    /** Sets the current thread to the specified number,
+       and sets the current frame to 0.
+       Note that nothing prevents us from introducing
+       setCurrentThreadAndFrame, but for all the cases when we
+       switch to a different thread we want frame 0.  */
+    virtual void setCurrentThread(int threadNumber) = 0;
+    virtual void setCurrentThread(const QModelIndex &index) = 0;
+    virtual int currentThread() const = 0;
+    virtual QModelIndex currentThreadIndex() const = 0;
+
+    /** Return the frame we wish to operate on.  This is always
+       in context of the current thread.  This may be -1 if
+       no frame is selected. This should only the be the case
+       if the thread has no stack as such -- e.g. because it's
+       running, or because it's exited.  */
+    virtual int currentFrame() const = 0;
+    virtual QModelIndex currentFrameIndex() const = 0;
+    virtual void setCurrentFrame(int frame) = 0;
+
+    virtual FrameItem frame(const QModelIndex &index) = 0;
+
+    virtual void fetchThreads() = 0;
+    virtual void fetchFrames(int threadNumber, int from, int to) = 0;
+    virtual void fetchMoreFrames() = 0;
+
+Q_SIGNALS:
+    /* FIXME: It might make for a more conscise interface if those
+       two were removed, and the clients react to thread_or_frame_changed
+       event and compare the current thread/frame in the framestack model
+       with the one they are displaying.  */
+    void currentThreadChanged(int thread);
+    void currentFrameChanged(int frame);
+
+private:
+    friend class IDebugSession;
+    QScopedPointer<IFrameStackModelPrivate> d;
+
+    virtual void handleEvent(IDebugSession::event_t event) = 0;
+};
+
 }
 
 #endif
