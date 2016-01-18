8be5a540f62cafb835378d86eb8df5441a8105f5
Kevin Funk <kfunk@kde.org>
2016-01-18 19:27:42 +0100

    OutputExecuteJob: Handle progress properly
    Fixes:
    * Don't print "Build finished", this isn't necessarily a Build
    * Don't emit invalid percentage or invalid status messages

--- outputview/outputexecutejob.cpp
+++ outputview/outputexecutejob.cpp
@@ -392,11 +392,10 @@ void OutputExecuteJobPrivate::childProcessStderr()
 
 void OutputExecuteJobPrivate::emitProgress(const IFilterStrategy::Progress& progress)
 {
-    m_owner->emitPercent(progress.percent, 100);
-
-    if (progress.percent == 100) {
-        m_owner->infoMessage(m_owner, i18n("Build finished"));
-    } else {
+    if (progress.percent != -1) {
+        m_owner->emitPercent(progress.percent, 100);
+    }
+    if (!progress.status.isEmpty()) {
         m_owner->infoMessage(m_owner, progress.status);
     }
 }
