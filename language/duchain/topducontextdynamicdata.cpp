KDE/kdevelop

commit 49e4b656f0e54bff882f03efe04feedff5994ed1

Share code between partial data loading functions.
Lambdas are pretty helpful in this context imo.

language/duchain/topducontextdynamicdata.cpp
@@ -140,6 +140,22 @@ QString pathForTopContext(const uint topContextIndex)
   return basePath() + QString::number(topContextIndex);
 }
 
+template<typename F>
+void loadPartialData(const uint topContextIndex, F callback)
+{
+  QFile file(pathForTopContext(topContextIndex));
+  if (file.open(QIODevice::ReadOnly)) {
+     uint readValue;
+     file.read((char*)&readValue, sizeof(uint));
+     // now readValue is filled with the top-context data size
+
+     // We only read the most needed stuff, not the whole top-context data
+     QByteArray data = file.read(sizeof(TopDUContextData));
+     const TopDUContextData* topData = reinterpret_cast<const TopDUContextData*>(data.constData());
+     callback(topData);
+  }
+}
+
 }
 
 //BEGIN DUChainItemStorage
@@ -410,64 +426,38 @@ void KDevelop::TopDUContextDynamicData::unmap() {
   m_mappedDataSize = 0;
 }
 
-QList<IndexedDUContext> TopDUContextDynamicData::loadImporters(uint topContextIndex) {
-  QList<IndexedDUContext> ret;
-
-  QFile file(pathForTopContext(topContextIndex));
-  if(file.open(QIODevice::ReadOnly)) {
-     uint readValue;
-     file.read((char*)&readValue, sizeof(uint));
-     //now readValue is filled with the top-context data size
-
-     //We only read the most needed stuff, not the whole top-context data
-     QByteArray data = file.read(readValue);
-     const TopDUContextData* topData = reinterpret_cast<const TopDUContextData*>(data.constData());
-     FOREACH_FUNCTION(const IndexedDUContext& importer, topData->m_importers)
-      ret << importer;
-  }
-
-  return ret;
-}
-
-QList<IndexedDUContext> TopDUContextDynamicData::loadImports(uint topContextIndex) {
-  QList<IndexedDUContext> ret;
-
-  QFile file(pathForTopContext(topContextIndex));
-  if(file.open(QIODevice::ReadOnly)) {
-     uint readValue;
-     file.read((char*)&readValue, sizeof(uint));
-     //now readValue is filled with the top-context data size
-
-     //We only read the most needed stuff, not the whole top-context data
-     QByteArray data = file.read(readValue);
-     const TopDUContextData* topData = reinterpret_cast<const TopDUContextData*>(data.constData());
-     FOREACH_FUNCTION(const DUContext::Import& import, topData->m_importedContexts)
-      ret << import.indexedContext();
-  }
-
-  return ret;
-}
-
 bool TopDUContextDynamicData::fileExists(uint topContextIndex)
 {
   return QFile::exists(pathForTopContext(topContextIndex));
 }
 
+QList<IndexedDUContext> TopDUContextDynamicData::loadImporters(uint topContextIndex) {
+  QList<IndexedDUContext> ret;
+  loadPartialData(topContextIndex, [&ret] (const TopDUContextData* topData) {
+    ret.reserve(topData->m_importersSize());
+    FOREACH_FUNCTION(const IndexedDUContext& importer, topData->m_importers)
+      ret << importer;
+  });
+  return ret;
+}
+
+QList<IndexedDUContext> TopDUContextDynamicData::loadImports(uint topContextIndex) {
+  QList<IndexedDUContext> ret;
+  loadPartialData(topContextIndex, [&ret] (const TopDUContextData* topData) {
+    ret.reserve(topData->m_importedContextsSize());
+    FOREACH_FUNCTION(const DUContext::Import& import, topData->m_importedContexts)
+      ret << import.indexedContext();
+  });
+  return ret;
+}
+
 IndexedString TopDUContextDynamicData::loadUrl(uint topContextIndex) {
-  QFile file(pathForTopContext(topContextIndex));
-  if(file.open(QIODevice::ReadOnly)) {
-     uint readValue;
-     file.read((char*)&readValue, sizeof(uint));
-     //now readValue is filled with the top-context data size
-
-     //We only read the most needed stuff, not the whole top-context data
-     QByteArray data = file.read(sizeof(TopDUContextData));
-     const TopDUContextData* topData = reinterpret_cast<const TopDUContextData*>(data.constData());
-     Q_ASSERT(topData->m_url.isEmpty() || topData->m_url.index() >> 16);
-     return topData->m_url;
-  }
-
-  return IndexedString();
+  IndexedString url;
+  loadPartialData(topContextIndex, [&url] (const TopDUContextData* topData) {
+    Q_ASSERT(topData->m_url.isEmpty() || topData->m_url.index() >> 16);
+    url = topData->m_url;
+  });
+  return url;
 }
 
 void TopDUContextDynamicData::loadData() const {

commit 14df9ba1a88e41193bf65f9d87f2849747f664b3

TopContextDynamicData: Fix bug in loadPartialData
Detected using ASAN

Regression introduced by 49e4b656f0e54bff882f03efe04feedff5994ed1

==31328==ERROR: AddressSanitizer: heap-buffer-overflow on address
0x60b0007dc6e4 at pc 0x7f8c81dd210f bp 0x7ffc0dba13f0 sp 0x7ffc0dba13e8
READ of size 8 at 0x60b0007dc6e4 thread T0
    #0 0x7f8c81dd210e in
QList<KDevelop::IndexedDUContext>::node_construct(QList<KDevelop::IndexedDUContext>::Node*,
KDevelop::IndexedDUContext const&)
/usr/include/x86_64-linux-gnu/qt5/QtCore/qlist.h:405:39
    #1 0x7f8c81dd210e in
QList<KDevelop::IndexedDUContext>::append(KDevelop::IndexedDUContext
const&) /usr/include/x86_64-linux-gnu/qt5/QtCore/qlist.h:569
    #2 0x7f8c81dd210e in
QList<KDevelop::IndexedDUContext>::operator<<(KDevelop::IndexedDUContext
const&) /usr/include/x86_64-linux-gnu/qt5/QtCore/qlist.h:355
    #3 0x7f8c81dd210e in
KDevelop::TopDUContextDynamicData::loadImporters(unsigned
int)::$_0::operator()(KDevelop::TopDUContextData const*) const
/home/kfunk/devel/src/kf5/kdevplatform-stable/language/duchain/topducontextdynamicdata.cpp:505
    #4 0x7f8c81dd210e in void (anonymous
namespace)::loadPartialData<KDevelop::TopDUContextDynamicData::loadImporters(unsigned
int)::$_0>(unsigned int,
KDevelop::TopDUContextDynamicData::loadImporters(unsigned int)::$_0)
/home/kfunk/devel/src/kf5/kdevplatform-stable/language/duchain/topducontextdynamicdata.cpp:175
    #5 0x7f8c81dd210e in
KDevelop::TopDUContextDynamicData::loadImporters(unsigned int)
/home/kfunk/devel/src/kf5/kdevplatform-stable/language/duchain/topducontextdynamicdata.cpp:502
    #6 0x7f8c81e7bc8d in KDevelop::ParsingEnvironmentFile::importers()
const
/home/kfunk/devel/src/kf5/kdevplatform-stable/language/duchain/parsingenvironment.cpp:205:11
    #7 0x7f8c81d42a12 in
KDevelop::DUChainPrivate::addContextsForRemoval(QSet<unsigned int>&,
KDevelop::IndexedTopDUContext)
/home/kfunk/devel/src/kf5/kdevplatform-stable/language/duchain/duchain.cpp:1015:81

language/duchain/topducontextdynamicdata.cpp
@@ -160,20 +160,25 @@ QString pathForTopContext(const uint topContextIndex)
   return basePath() + QString::number(topContextIndex);
 }
 
+enum LoadType {
+  PartialLoad, ///< Only load the direct member data
+  FullLoad     ///< Load everything, including appended lists
+};
 template<typename F>
-void loadPartialData(const uint topContextIndex, F callback)
+void loadTopDUContextData(const uint topContextIndex, LoadType loadType, F callback)
 {
   QFile file(pathForTopContext(topContextIndex));
-  if (file.open(QIODevice::ReadOnly)) {
-     uint readValue;
-     file.read((char*)&readValue, sizeof(uint));
-     // now readValue is filled with the top-context data size
-
-     // We only read the most needed stuff, not the whole top-context data
-     QByteArray data = file.read(sizeof(TopDUContextData));
-     const TopDUContextData* topData = reinterpret_cast<const TopDUContextData*>(data.constData());
-     callback(topData);
+  if (!file.open(QIODevice::ReadOnly)) {
+    return;
   }
+
+  uint readValue;
+  file.read((char*)&readValue, sizeof(uint));
+  // now readValue is filled with the top-context data size
+  Q_ASSERT(readValue >= sizeof(TopDUContextData));
+  const QByteArray data = file.read(loadType == FullLoad ? readValue : sizeof(TopDUContextData));
+  const TopDUContextData* topData = reinterpret_cast<const TopDUContextData*>(data.constData());
+  callback(topData);
 }
 
 template<typename T>
@@ -499,7 +504,7 @@ bool TopDUContextDynamicData::fileExists(uint topContextIndex)
 
 QList<IndexedDUContext> TopDUContextDynamicData::loadImporters(uint topContextIndex) {
   QList<IndexedDUContext> ret;
-  loadPartialData(topContextIndex, [&ret] (const TopDUContextData* topData) {
+  loadTopDUContextData(topContextIndex, FullLoad, [&ret] (const TopDUContextData* topData) {
     ret.reserve(topData->m_importersSize());
     FOREACH_FUNCTION(const IndexedDUContext& importer, topData->m_importers)
       ret << importer;
@@ -509,7 +514,7 @@ QList<IndexedDUContext> TopDUContextDynamicData::loadImporters(uint topContextIn
 
 QList<IndexedDUContext> TopDUContextDynamicData::loadImports(uint topContextIndex) {
   QList<IndexedDUContext> ret;
-  loadPartialData(topContextIndex, [&ret] (const TopDUContextData* topData) {
+  loadTopDUContextData(topContextIndex, FullLoad, [&ret] (const TopDUContextData* topData) {
     ret.reserve(topData->m_importedContextsSize());
     FOREACH_FUNCTION(const DUContext::Import& import, topData->m_importedContexts)
       ret << import.indexedContext();
@@ -519,7 +524,7 @@ QList<IndexedDUContext> TopDUContextDynamicData::loadImports(uint topContextInde
 
 IndexedString TopDUContextDynamicData::loadUrl(uint topContextIndex) {
   IndexedString url;
-  loadPartialData(topContextIndex, [&url] (const TopDUContextData* topData) {
+  loadTopDUContextData(topContextIndex, PartialLoad, [&url] (const TopDUContextData* topData) {
     Q_ASSERT(topData->m_url.isEmpty() || topData->m_url.index() >> 16);
     url = topData->m_url;
   });