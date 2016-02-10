b930278bb44b37742bcbc19b12efeeb8026e1a3a
Pedro Ferreira <esteaqui@hotmail.com>
2016-02-10 17:17:32 +0100

    Add support for CXType_FunctionNoProto
    Add support for CXType_FunctionNoProto as having the same behaviour as CXType_FunctionProto.
    Also, undefine a local macro. This doesn't fix any problems, but I believe its a Good Thing to clean up local macros when we're done with them.
    
    REVIEW: 127027
    CCBUG: 357615

--- languages/clang/duchain/builder.cpp
+++ languages/clang/duchain/builder.cpp
@@ -477,7 +477,7 @@ struct Visitor
         return ref;
     }
 
-    template<CXTypeKind TK, EnableIf<TK == CXType_FunctionProto> = dummy>
+    template<CXTypeKind TK, EnableIf<TK == CXType_FunctionProto || TK == CXType_FunctionNoProto> = dummy>
     AbstractType *createType(CXType type, CXCursor parent)
     {
         auto func = new FunctionType;
@@ -1202,7 +1202,7 @@ void Visitor::setIdTypeDecl(CXCursor typeCursor, IdentifiedType* idType) const
 
 AbstractType *Visitor::makeType(CXType type, CXCursor parent)
 {
-    #define UseKind(TypeKind) case TypeKind: return dispatchType<TypeKind>(type, parent)
+#define UseKind(TypeKind) case TypeKind: return dispatchType<TypeKind>(type, parent)
     switch (type.kind) {
     UseKind(CXType_Void);
     UseKind(CXType_Bool);
@@ -1232,6 +1232,7 @@ AbstractType *Visitor::makeType(CXType type, CXCursor parent)
     UseKind(CXType_DependentSizedArray);
     UseKind(CXType_LValueReference);
     UseKind(CXType_RValueReference);
+    UseKind(CXType_FunctionNoProto);
     UseKind(CXType_FunctionProto);
     UseKind(CXType_Record);
     UseKind(CXType_Enum);
@@ -1255,6 +1256,7 @@ AbstractType *Visitor::makeType(CXType type, CXCursor parent)
         qCWarning(KDEV_CLANG) << "Unhandled type:" << type.kind << clang_getTypeSpelling(type);
         return nullptr;
     }
+#undef UseKind
 }
 
 RangeInRevision rangeInRevisionForUse(CXCursor cursor, CXCursorKind referencedCursorKind, CXSourceRange useRange, const QSet<unsigned int>& macroExpansionLocations)
