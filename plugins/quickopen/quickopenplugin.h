3cbd4d8d52d72c7ca3c4ff6a6b81e06b9b0ff8c3
Kevin Funk <kfunk@kde.org>
2016-02-12 23:43:19 +0100

    Factor out QuickOpenWidget into own files

--- plugins/quickopen/quickopenplugin.h
+++ plugins/quickopen/quickopenplugin.h
@@ -22,18 +22,13 @@
 #ifndef KDEVPLATFORM_PLUGIN_QUICKOPENPLUGIN_H
 #define KDEVPLATFORM_PLUGIN_QUICKOPENPLUGIN_H
 
-#include <QtCore/QVariant>
-#include <QtCore/QTimer>
+#include <QVariant>
 #include <QPointer>
-#include <QMenu>
-#include <QTime>
 
 #include <interfaces/iplugin.h>
 
 #include <language/interfaces/iquickopen.h>
 #include <language/interfaces/quickopendataprovider.h>
-
-#include "ui_quickopen.h"
 
 class QAction;
 
@@ -135,80 +130,6 @@ private:
     QPointer<QObject> m_currentWidgetHandler;
     QAction* m_quickOpenDeclaration;
     QAction* m_quickOpenDefinition;
-};
-
-///Will delete itself once the dialog is closed, so use QPointer when referencing it permanently
-class QuickOpenWidget : public QMenu {
-  Q_OBJECT
-  public:
-  /**
-   * @param initialItems List of items that should initially be enabled in the quickopen-list. If empty, all are enabled.
-   * @param initialScopes List of scopes that should initially be enabled in the quickopen-list. If empty, all are enabled.
-   * @param listOnly when this is true, the given items will be listed, but all filtering using checkboxes is disabled.
-   * @param noSearchFied when this is true, no search-line is shown.
-   * */
-  QuickOpenWidget( QString title, QuickOpenModel* model, const QStringList& initialItems, const QStringList& initialScopes, bool listOnly = false, bool noSearchField = false );
-  ~QuickOpenWidget() override;
-  void setPreselectedText(const QString &text);
-    void prepareShow();
-
-  void setAlternativeSearchField(QLineEdit* alterantiveSearchField);
-    
-  //Shows OK + Cancel. By default they are hidden  
-  void showStandardButtons(bool show);
-    void showSearchField(bool show);
-    
-  signals:
-  void scopesChanged( const QStringList& scopes );
-  void itemsChanged( const QStringList& scopes );
-  void ready();
-
-  private slots:
-  void callRowSelected();
-
-  void updateTimerInterval( bool cheapFilterChange );
-
-  void accept();
-  void textChanged( const QString& str );
-  void updateProviders();
-  void doubleClicked ( const QModelIndex & index );
-
-  void applyFilter();
-
-  private:
-  void showEvent(QShowEvent *) override;
-  
-  bool eventFilter ( QObject * watched, QEvent * event ) override;
-
-  void avoidMenuAltFocus();
-
-  QuickOpenModel* m_model;
-  bool m_expandedTemporary, m_hadNoCommandSinceAlt;
-  QTime m_altDownTime;
-  QString m_preselectedText;
-  QTimer m_filterTimer;
-  QString m_filter;
-  public:
-  Ui::QuickOpen o;
-  
-  friend class QuickOpenWidgetDialog;
-  friend class QuickOpenPlugin;
-  friend class QuickOpenLineEdit;
-};
-
-class QuickOpenWidgetDialog : public QObject {
-  Q_OBJECT
-  public:
-    QuickOpenWidgetDialog( QString title, QuickOpenModel* model, const QStringList& initialItems, const QStringList& initialScopes, bool listOnly = false, bool noSearchField = false );
-    ~QuickOpenWidgetDialog() override;
-    ///Shows the dialog
-    void run();
-    QuickOpenWidget* widget() const {
-      return m_widget;
-    }
-  private:
-  QDialog* m_dialog; //Warning: m_dialog is also the parent
-  QuickOpenWidget* m_widget;
 };
 
 class QuickOpenWidgetCreator;
