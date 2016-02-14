5b036d689b93eae31972dea04c0cd4d8431ba13c
Milian Wolff <mail@milianw.de>
2016-02-15 00:15:35 +0100

    Fixup colorization of class members for clang's DUChain structure.
    The code in CodeHighlightingInstance::localClassFromCodeContext did
    not work for the DUContext hierarchy build by kdev-clang: It puts
    the function-body Other contexts into the Function context that
    contains the arguments. This is very convenient, but was not supported
    by the code that looks for the accompanying class during highlighting.
    
    With this patch applied, we finally get the orangish colors for
    members back! They no longer are colored like other globals!

--- language/highlighting/codehighlighting.cpp
+++ language/highlighting/codehighlighting.cpp
@@ -338,8 +338,13 @@ KDevelop::Declaration* CodeHighlightingInstance::localClassFromCodeContext(KDeve
 
   DUContext* startContext = context;
 
-  while( context->parentContext() && context->type() == DUContext::Other && context->parentContext()->type() == DUContext::Other )
-  { //Move context to the top context of type "Other". This is needed because every compound-statement creates a new sub-context.
+  while( context->type() == DUContext::Other )
+  {
+    //Move context to the top context of type "Other". This is needed because every compound-statement creates a new sub-context.
+    auto parent = context->parentContext();
+    if (!parent || (parent->type() != DUContext::Other && parent->type() != DUContext::Function)) {
+      break;
+    }
     context = context->parentContext();
   }
 
