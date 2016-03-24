KDE/kdevelop

commit 8ff872eae18ac9e56cbdaaacd763f0de19fa3806

Introduce/use KTextEditorHelpers::extractCursor
This is a copy of the command-line parsing code for filenames in the
kdevelop binary. It can be used to extract the line + column information
out of a string like "myfilename.cpp:1:2".

You can now also enter this in quick open, to open a file at a
particular line *and* column directly.

util/texteditorhelpers.cpp
@@ -21,7 +21,9 @@
 
 #include "texteditorhelpers.h"
 
-#include "KTextEditor/View"
+#include <KTextEditor/View>
+
+#include <QRegularExpression>
 
 namespace KDevelop {
 
@@ -41,12 +43,31 @@ int getLineHeight(const KTextEditor::View* view, int curLine)
 
 }
 
-QRect getItemBoundingRect(const KTextEditor::View* view, KTextEditor::Range itemRange)
+QRect KTextEditorHelpers::getItemBoundingRect(const KTextEditor::View* view, const KTextEditor::Range& itemRange)
 {
   QPoint startPoint = view->mapToGlobal(view->cursorToCoordinate(itemRange.start()));
   QPoint endPoint = view->mapToGlobal(view->cursorToCoordinate(itemRange.end()));
   endPoint.ry() += getLineHeight(view, itemRange.start().line());
   return QRect(startPoint, endPoint);
+}
+
+KTextEditor::Cursor KTextEditorHelpers::extractCursor(const QString& input, int* pathLength)
+{
+    static const QRegularExpression pattern(QStringLiteral(":(\\d+)(?::(\\d+))?$"));
+    const auto match = pattern.match(input);
+    if (!match.isValid()) {
+        if (pathLength)
+            *pathLength = input.length();
+        return {};
+    }
+
+    int line = match.captured(1).toInt() - 1;
+    // don't use an invalid column when the line is valid
+    int column = qMax(0, match.captured(2).toInt() - 1);
+
+    if (pathLength)
+        *pathLength = match.capturedLength();
+    return {line, column};
 }
 
 }

debugger/variable/variablecollection.cpp
@@ -536,7 +536,7 @@ QString VariableProvider::textHint(KTextEditor::View* view, const KTextEditor::C
         w = view;
 
     m_collection->activeTooltip_ = new VariableToolTip(w, global+QPoint(30,30), expression);
-    m_collection->activeTooltip_->setHandleRect(getItemBoundingRect(view, expressionRange));
+    m_collection->activeTooltip_->setHandleRect(KTextEditorHelpers::getItemBoundingRect(view, expressionRange));
     return QString();
 }
 

plugins/contextbrowser/contextbrowser.cpp
@@ -468,7 +468,7 @@ void ContextBrowserPlugin::showToolTip(KTextEditor::View* view, KTextEditor::Cur
       DUChainReadLocker lock;
       itemRange = DUChainUtils::itemRangeUnderCursor(viewUrl, KTextEditor::Cursor(position));
     }
-    tooltip->setHandleRect(getItemBoundingRect(view, itemRange));
+    tooltip->setHandleRect(KTextEditorHelpers::getItemBoundingRect(view, itemRange));
     tooltip->resize( navigationWidget->sizeHint() + QSize(10, 10) );
     qCDebug(PLUGIN_CONTEXTBROWSER) << "tooltip size" << tooltip->size();
     m_currentToolTip = tooltip;

plugins/problemreporter/problemhighlighter.cpp
@@ -132,7 +132,7 @@ QString ProblemTextHintProvider::textHint(View* view, const Cursor& pos)
                     = new KDevelop::NavigationToolTip(view, QCursor::pos() + QPoint(20, 40), widget);
 
                 tooltip->resize(widget->sizeHint() + QSize(10, 10));
-                tooltip->setHandleRect(getItemBoundingRect(view, m_currentHintRange));
+                tooltip->setHandleRect(KTextEditorHelpers::getItemBoundingRect(view, m_currentHintRange));
                 tooltip->connect(tooltip, &ActiveToolTip::destroyed, [&] () { m_currentHintRange = {}; });
                 ActiveToolTip::showToolTip(tooltip, 99, QStringLiteral("problem-tooltip"));
                 return QString();

plugins/quickopen/projectfilequickopen.cpp
@@ -35,6 +35,7 @@
 #include <language/duchain/duchainlock.h>
 #include <serialization/indexedstring.h>
 #include <language/duchain/parsingenvironment.h>
+#include <util/texteditorhelpers.h>
 
 #include <project/projectmodel.h>
 #include <project/projectutils.h>
@@ -89,12 +90,12 @@ bool ProjectFileData::execute( QString& filterText )
 {
     const QUrl url = m_file.path.toUrl();
     IOpenWith::openFiles(QList<QUrl>() << url);
-    QString path;
-    uint lineNumber;
-    if (extractLineNumber(filterText, path, lineNumber)) {
+
+    auto cursor = KTextEditorHelpers::extractCursor(filterText);
+    if (cursor.isValid()) {
         IDocument* doc = ICore::self()->documentController()->documentForUrl(url);
         if (doc) {
-            doc->setCursorPosition(KTextEditor::Cursor(lineNumber - 1, 0));
+            doc->setCursorPosition(cursor);
         }
     }
     return true;
@@ -199,9 +200,9 @@ BaseFileDataProvider::BaseFileDataProvider()
 
 void BaseFileDataProvider::setFilterText( const QString& text )
 {
-    QString path(text);
-    uint lineNumber;
-    extractLineNumber(text, path, lineNumber);
+    int pathLength;
+    KTextEditorHelpers::extractCursor(text, &pathLength);
+    QString path(text.mid(0, pathLength));
     if ( path.startsWith(QLatin1String("./")) || path.startsWith(QLatin1String("../")) ) {
         // assume we want to filter relative to active document's url
         IDocument* doc = ICore::self()->documentController()->activeDocument();

util/texteditorhelpers.h
@@ -31,8 +31,22 @@ class View;
 
 namespace KDevelop {
 
-// Return item's bounding rect in global screen coordinates
-QRect KDEVPLATFORMUTIL_EXPORT getItemBoundingRect(const KTextEditor::View* view, KTextEditor::Range itemRange);
+namespace KTextEditorHelpers {
+
+/// @return Item's bounding rect in global screen coordinates
+QRect KDEVPLATFORMUTIL_EXPORT getItemBoundingRect(const KTextEditor::View* view, const KTextEditor::Range& itemRange);
+
+/**
+ * @brief Try parsing a string such as "path_to_file:line_num:column_num".
+ *
+ * Both line_num and column_num may be empty. Thus, other valid inputs are: "path_to_file", "path_to_file:line_num"
+ * @param input Source string
+ * @param pathLength Set to the length of the "path_to_file" string, you can use this to extract the raw path from the input string
+ * @return Cursor representing line_num and column_num
+ * */
+KTextEditor::Cursor KDEVPLATFORMUTIL_EXPORT extractCursor(const QString& input, int* pathLength = nullptr);
+
+}
 
 }
 

commit d311f255e5080332370562f0db51c1da2ba5389f

Fix KTextEditorHelpers::extractCursor
Add unit tests.
Fixes project file quick open again.

Regression introduced by 8ff872e

util/texteditorhelpers.cpp
@@ -55,18 +55,18 @@ KTextEditor::Cursor KTextEditorHelpers::extractCursor(const QString& input, int*
 {
     static const QRegularExpression pattern(QStringLiteral(":(\\d+)(?::(\\d+))?$"));
     const auto match = pattern.match(input);
-    if (!match.isValid()) {
+    if (!match.hasMatch()) {
         if (pathLength)
             *pathLength = input.length();
-        return {};
+        return KTextEditor::Cursor::invalid();
     }
 
-    int line = match.captured(1).toInt() - 1;
+    int line = match.capturedRef(1).toInt() - 1;
     // don't use an invalid column when the line is valid
     int column = qMax(0, match.captured(2).toInt() - 1);
 
     if (pathLength)
-        *pathLength = match.capturedLength();
+        *pathLength = match.capturedStart(0);
     return {line, column};
 }
 

util/tests/CMakeLists.txt
@@ -10,6 +10,9 @@ ecm_add_test(test_objectlist.cpp
 ecm_add_test(test_stringhandler.cpp
     LINK_LIBRARIES Qt5::Test KDev::Util)
 
+ecm_add_test(test_texteditorhelpers.cpp
+    LINK_LIBRARIES Qt5::Test KDev::Util)
+
 ecm_add_test(test_path.cpp
     LINK_LIBRARIES Qt5::Test KF5::KIOCore KDev::Tests KDev::Util)
 

util/tests/test_texteditorhelpers.cpp
@@ -1,0 +1,68 @@
+/*
+ * Copyright 2016 Kevin Funk <kfunk@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "test_texteditorhelpers.h"
+
+#include "texteditorhelpers.h"
+
+#include <QtTest>
+
+QTEST_MAIN(TestKTextEditorHelpers);
+
+using namespace KDevelop;
+
+void TestKTextEditorHelpers::testExtractCursor()
+{
+    QFETCH(QString, input);
+    QFETCH(KTextEditor::Cursor, expectedCursor);
+    QFETCH(QString, expectedPath);
+
+    int pathLen;
+    const auto cursor = KTextEditorHelpers::extractCursor(input, &pathLen);
+    QCOMPARE(cursor, expectedCursor);
+    QCOMPARE(input.mid(0, pathLen), expectedPath);
+}
+
+void TestKTextEditorHelpers::testExtractCursor_data()
+{
+    QTest::addColumn<QString>("input");
+    QTest::addColumn<KTextEditor::Cursor>("expectedCursor");
+    QTest::addColumn<QString>("expectedPath");
+
+    // valid input
+    QTest::newRow("file")
+        << QStringLiteral("widget.cpp")
+        << KTextEditor::Cursor::invalid()
+        << QStringLiteral("widget.cpp");
+    QTest::newRow("file:line")
+        << QStringLiteral("widget.cpp:12")
+        << KTextEditor::Cursor(11, 0)
+        << QStringLiteral("widget.cpp");
+    QTest::newRow("file:line:column")
+        << QStringLiteral("widget.cpp:12:5")
+        << KTextEditor::Cursor(11, 4)
+        << QStringLiteral("widget.cpp");
+    // partially invalid input
+    QTest::newRow("file:")
+        << QStringLiteral("widget.cpp:")
+        << KTextEditor::Cursor::invalid()
+        << QStringLiteral("widget.cpp:");
+}

util/tests/test_texteditorhelpers.h
@@ -1,0 +1,36 @@
+/*
+ * Copyright 2016 Kevin Funk <kfunk@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TESTKTEXTEDITORHELPERS_H
+#define TESTKTEXTEDITORHELPERS_H
+
+#include <QObject>
+
+class TestKTextEditorHelpers : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testExtractCursor();
+    void testExtractCursor_data();
+};
+
+#endif