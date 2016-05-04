94a73b0cb63892b485d654e73030989d44f0a858
Peifeng Yu <7437103@gmail.com>
2016-05-03 21:26:00 -0400

    Add mock objects for IDebugSession related classes

---
+++ tests/testdebugsession.h
@@ -0,0 +1,111 @@
+/*
+ * This file is part of KDevelop
+ *
+ * Copyright 2016  Aetf <aetf@unlimitedcodeworks.xyz>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TESTDEBUGSESSION_H
+#define TESTDEBUGSESSION_H
+
+#include "testsexport.h"
+
+#include <debugger/interfaces/idebugsession.h>
+
+#include <memory>
+
+using std::unique_ptr;
+
+namespace KDevelop {
+
+class IBreakpointController;
+class IVariableController;
+class IFrameStackModel;
+
+/**
+ * \class TestDebugSession testdebugsession.h
+ *
+ * This is an IDebugSession implementation that should be used only inside
+ * unit tests.
+ *
+ * The initial debug state is NotStartedState, one can use normal slots to
+ * change the state, for example calling runToCursor() changes to PausedState.
+ * See corresponding implementation for what state a specific slot changes to.
+ *
+ * This class also allows one to replace IBreakpointController, IVariableController,
+ * or IFrameStackModel implementation with custom implementation to make it possible
+ * to write tests more easily.
+ *
+ * The usage is as follows:
+ * \code
+ * AutoTestShell::init();
+ * TestCore::initialize(Core::NoUi);
+ * TestDebugSession* session = new TestDebugSession();
+ * //replace variable controller or similar
+ * session->setVariableController(new MyCustomVariableController(session));
+ * ... //test code
+ * delete session;
+ * TestCore::shutdown();
+ * \endcode
+ *
+ * @note It is important to set custom controllers right after the creation of the
+ * debug session, and not change them inside the test code, as required by
+ * IDebugSession that controllers do not change during the lifetime of a session
+ */
+class KDEVPLATFORMTESTS_EXPORT TestDebugSession : public IDebugSession
+{
+    Q_OBJECT
+
+public:
+    TestDebugSession();
+    ~TestDebugSession() override;
+
+    void setBreakpointController(IBreakpointController *breakpointController);
+    void setVariableController(IVariableController *variableController);
+    void setFrameStackModel(IFrameStackModel *frameStackModel);
+
+    DebuggerState state() const override;
+
+    bool restartAvaliable() const override;
+    IBreakpointController* breakpointController() const override;
+    IVariableController* variableController() const override;
+    IFrameStackModel* frameStackModel() const override;
+
+public Q_SLOTS:
+    void restartDebugger() override;
+    void stopDebugger() override;
+    void interruptDebugger() override;
+    void run() override;
+    void runToCursor() override;
+    void jumpToCursor() override;
+    void stepOver() override;
+    void stepIntoInstruction() override;
+    void stepInto() override;
+    void stepOverInstruction() override;
+    void stepOut() override;
+
+private:
+    IBreakpointController *m_breakpointController;
+    IVariableController *m_variableController;
+    IFrameStackModel *m_frameStackModel;
+    DebuggerState m_sessionState;
+};
+
+} // end of namespace KDevelop
+#endif // TESTDEBUGSESSION_H
---
+++ tests/testvariablecontroller.h
@@ -0,0 +1,66 @@
+/*
+ * This file is part of KDevelop
+ *
+ * Copyright 2016  Aetf <aetf@unlimitedcodeworks.xyz>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TESTVARIABLECONTROLLER_H
+#define TESTVARIABLECONTROLLER_H
+
+#include "testsexport.h"
+
+#include <debugger/interfaces/ivariablecontroller.h>
+
+namespace KDevelop {
+
+/**
+ * Dummy VariableController that does nothing.
+ */
+class KDEVPLATFORMTESTS_EXPORT TestVariableController : public IVariableController
+{
+    Q_OBJECT
+
+public:
+    explicit TestVariableController(IDebugSession* parent);
+
+    Variable* createVariable(TreeModel* model, TreeItem* parent,
+                             const QString& expression,
+                             const QString& display = {}) override;
+
+    KTextEditor::Range expressionRangeUnderCursor(KTextEditor::Document* doc,
+                                                  const KTextEditor::Cursor& cursor) override;
+
+    void addWatch(Variable* variable) override;
+    void addWatchpoint(Variable* variable) override;
+
+    /**
+     * Number of times update() has been called since creation.
+     */
+    int updatedTimes() const;
+
+protected:
+    void update() override;
+
+private:
+    int m_updatedTimes;
+};
+
+} // end of namespace KDevelop
+#endif // TESTVARIABLECONTROLLER_H
