fd75b43a13068e0f0bac5466406763e5450356fb
Peifeng Yu <7437103@gmail.com>
2016-05-25 23:32:16 -0400

    Move GdbFrameStackModel to MIFrameStackModel

---
+++ debuggers/common/miframestackmodel.h
@@ -0,0 +1,51 @@
+/*
+ * Implementation of thread and frame model that are common to debuggers using MI.
+ * 
+ * Copyright 2009 Vladimir Prus <ghost@cs.msu.su>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef KDEVELOP_MI_FRAMESTACKMODEL_H
+#define KDEVELOP_MI_FRAMESTACKMODEL_H
+
+#include <debugger/framestack/framestackmodel.h>
+
+namespace KDevDebugger {
+
+namespace MI {
+struct ResultRecord;
+}
+
+class DebugSessionBase;
+class MIFrameStackModel : public KDevelop::FrameStackModel
+{
+public:
+    MIFrameStackModel(DebugSessionBase* session);
+
+    DebugSessionBase* session();
+
+protected: // FrameStackModel overrides
+    void fetchThreads() override;
+    void fetchFrames(int threadNumber, int from, int to) override;
+
+private:
+    void handleThreadInfo(const MI::ResultRecord& r);
+};
+
+} // end of namespace KDevDebugger
+
+#endif
--- debuggers/gdb/gdbframestackmodel.cpp
+++ debuggers/gdb/gdbframestackmodel.cpp
@@ -1,155 +1,37 @@
 /*
- * GDB-specific implementation of thread and frame model.
- * 
- * Copyright 2009 Vladimir Prus <ghost@cs.msu.su>
+ * GDB-specific implementation of frame stack model
+ * Copyright 2016  Aetf <aetf@unlimitedcodeworks.xyz>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
- * License along with this program; if not, write to the
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include "gdbframestackmodel.h"
 
 #include "debugsession.h"
-#include "mi/micommand.h"
 
-#include <KLocalizedString>
-
-using namespace KDevelop;
 using namespace KDevDebugger::GDB;
-using namespace KDevDebugger::MI;
 
-QString getFunctionOrAddress(const Value &frame)
-{
-    if (frame.hasField("func"))
-        return frame["func"].literal();
-    else
-        return frame["addr"].literal();
-}
-
-QPair<QString, int> getSource(const Value &frame)
-{
-    QPair<QString, int> ret(QString(), -1);
-    if (frame.hasField("fullname"))
-        ret=qMakePair(frame["fullname"].literal(), frame["line"].toInt()-1);
-    else if (frame.hasField("file"))
-        ret=qMakePair(frame["file"].literal(), frame["line"].toInt()-1);
-    else if (frame.hasField("from"))
-        ret.first=frame["from"].literal();
-
-    return ret;
-}
-
-GdbFrameStackModel::GdbFrameStackModel(DebugSession* session)
-    : FrameStackModel(session)
+GdbFrameStackModel::GdbFrameStackModel(DebugSession *session)
+    : MIFrameStackModel(session)
 {
 }
 
 DebugSession* GdbFrameStackModel::session()
 {
     return static_cast<DebugSession *>(FrameStackModel::session());
-}
-
-void GdbFrameStackModel::fetchThreads()
-{
-    session()->addCommand(
-        new MICommand(ThreadInfo, "",
-                    this,
-                    &GdbFrameStackModel::handleThreadInfo));    
-}
-
-void GdbFrameStackModel::handleThreadInfo(const ResultRecord& r)
-{
-    const Value& threads = r["threads"];
-
-    // Traverse GDB threads in backward order -- since GDB
-    // reports them in backward order. We want UI to
-    // show thread IDs in the natural order.
-    // FIXME: make the code independent of whatever craziness
-    // gdb might have tomorrow.
-
-    QList<KDevelop::FrameStackModel::ThreadItem> threadsList;
-    int gidx = threads.size()-1;
-    for (; gidx >= 0; --gidx) {
-        KDevelop::FrameStackModel::ThreadItem i;
-        const Value & threadMI = threads[gidx];
-        i.nr = threadMI["id"].toInt();
-        if (threadMI["state"].literal() == "stopped") {
-            i.name = getFunctionOrAddress(threads[gidx]["frame"]);
-        } else {
-            i.name = i18n("(running)");
-        }
-        threadsList << i;
-    }
-    setThreads(threadsList);
-    if (r.hasField("current-thread-id")) {
-        int currentThreadId = r["current-thread-id"].toInt();
-
-        setCurrentThread(currentThreadId);
-
-        if (session()->hasCrashed()) {
-            setCrashedThreadIndex(currentThreadId);
-        }
-    }
-}
-
-struct FrameListHandler : public MICommandHandler
-{
-    FrameListHandler(GdbFrameStackModel* model, int thread, int to)
-        : model(model), m_thread(thread) , m_to(to) {}
-
-    void handle(const ResultRecord &r) override
-    {
-        const Value& stack = r["stack"];
-        int first = stack[0]["level"].toInt();
-        QList<KDevelop::FrameStackModel::FrameItem> frames;
-        for (int i = 0; i< stack.size(); ++i) {
-            const Value& frame = stack[i];
-            KDevelop::FrameStackModel::FrameItem f;
-            f.nr = frame["level"].toInt();
-            f.name = getFunctionOrAddress(frame);
-            QPair<QString, int> loc = getSource(frame);
-            f.file = QUrl::fromLocalFile(loc.first);
-            f.line = loc.second;
-            frames << f;
-        }
-        bool hasMore = false;
-        if (!frames.isEmpty()) {
-            if (frames.last().nr == m_to+1) {
-                frames.takeLast();
-                hasMore = true;
-            }
-        }
-        if (first == 0) {
-            model->setFrames(m_thread, frames);
-        } else {
-            model->insertFrames(m_thread, frames);
-        }
-        model->setHasMoreFrames(m_thread, hasMore);
-    }
-private:
-    GdbFrameStackModel* model;
-    int m_thread;
-    int m_to;
-};
-
-void GdbFrameStackModel::fetchFrames(int threadNumber, int from, int to)
-{
-    //to+1 so we know if there are more
-    QString arg = QString("%1 %2").arg(from).arg(to+1);
-    MICommand *c = new MICommand(StackListFrames, arg,
-                                   new FrameListHandler(this, threadNumber, to));
-    c->setThread(threadNumber);
-    session()->addCommand(c);
 }
