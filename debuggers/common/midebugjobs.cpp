d99579df74904ed822145aa4b34e5ee20c6503b4
Peifeng Yu <7437103@gmail.com>
2016-08-13 01:02:30 +0800

    LLDB/GDB plugin: stop dumping debugger console stream into the Debug view, we know have more user friendly output for program exit reason

--- debuggers/common/midebugjobs.cpp
+++ debuggers/common/midebugjobs.cpp
@@ -59,10 +59,6 @@ MIDebugJob::MIDebugJob(MIDebuggerPlugin* p, ILaunchConfiguration* launchcfg,
     m_session = p->createSession();
     connect(m_session, &MIDebugSession::inferiorStdoutLines, this, &MIDebugJob::stdoutReceived);
     connect(m_session, &MIDebugSession::inferiorStderrLines, this, &MIDebugJob::stderrReceived);
-    connect(m_session, &MIDebugSession::debuggerInternalCommandOutput,
-            this, [this](const QString &output){
-                this->stdoutReceived(output.split(QRegularExpression("[\r\n]"), QString::SkipEmptyParts));
-            });
 
     connect(m_session, &MIDebugSession::finished, this, &MIDebugJob::done);
 
