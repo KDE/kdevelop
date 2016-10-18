249dd5abb6e31483e596321094a0afd26435e2a5
Alex Richardson <arichardson.kde@gmail.com>
2016-10-18 17:22:41 +0100

    Add virtual destructor to QtHelpProviderAbstract

--- documentation/qthelp/qthelpproviderabstract.h
+++ documentation/qthelp/qthelpproviderabstract.h
@@ -29,13 +29,13 @@
 #include <QVariantList>
 #include <QHelpEngine>
 
-
 class QtHelpProviderAbstract : public QObject, public KDevelop::IDocumentationProvider
 {
     Q_OBJECT
     Q_INTERFACES( KDevelop::IDocumentationProvider )
 public:
     QtHelpProviderAbstract(QObject *parent, const QString &collectionFileName, const QVariantList & args);
+    virtual ~QtHelpProviderAbstract();
     KDevelop::IDocumentation::Ptr documentationForDeclaration (KDevelop::Declaration*) const override;
 
     KDevelop::IDocumentation::Ptr documentationForIndex(const QModelIndex& idx) const override;
