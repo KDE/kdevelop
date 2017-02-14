4adabcfedec468f00227d7a0b46fae168ebc70f3
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-02-14 07:22:56 +0100

    Turn static variables pointing to const chars into static const arrays
    char[] is a type of its own and can be used to denote the array directly
    saves one indirection compared to using the char* type

--- projectmanagers/qmake/qmakeconfig.cpp
+++ projectmanagers/qmake/qmakeconfig.cpp
@@ -30,14 +30,14 @@
 #include <util/path.h>
 #include "debug.h"
 
-const char* QMakeConfig::CONFIG_GROUP = "QMake_Builder";
+const char QMakeConfig::CONFIG_GROUP[] = "QMake_Builder";
 
-const char* QMakeConfig::QMAKE_BINARY = "QMake_Binary";
-const char* QMakeConfig::BUILD_FOLDER = "Build_Folder";
-const char* QMakeConfig::INSTALL_PREFIX = "Install_Prefix";
-const char* QMakeConfig::EXTRA_ARGUMENTS = "Extra_Arguments";
-const char* QMakeConfig::BUILD_TYPE = "Build_Type";
-const char* QMakeConfig::ALL_BUILDS = "All_Builds";
+const char QMakeConfig::QMAKE_BINARY[] = "QMake_Binary";
+const char QMakeConfig::BUILD_FOLDER[] = "Build_Folder";
+const char QMakeConfig::INSTALL_PREFIX[] = "Install_Prefix";
+const char QMakeConfig::EXTRA_ARGUMENTS[] = "Extra_Arguments";
+const char QMakeConfig::BUILD_TYPE[] = "Build_Type";
+const char QMakeConfig::ALL_BUILDS[] = "All_Builds";
 
 namespace {
 
