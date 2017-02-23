16225d023c8f9f7fe9e716c95a6572fc174680d2
Aleix Pol <aleixpol@kde.org>
2017-02-23 16:01:02 +0100

    Fix path

--- app_templates/cpp/CMake/cmake_qt5-qml2/src/main.cpp
+++ app_templates/cpp/CMake/cmake_qt5-qml2/src/main.cpp
@@ -4,6 +4,6 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/src/qml/main.qml")));
+    QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/qml/main.qml")));
     return app.exec();
 }
