e8f21880252edfb97a93278550c9fa411ae632a9
Kevin Funk <kfunk@kde.org>
2017-03-18 01:49:15 +0100

    Revert "Reformat file properly"
    Sorry, this has been done in master already
    
    This reverts commit 2a6ced5df0c1ac9be3266cdcc55b2c224fc23a40.

--- release-scripts/update-versions.sh
+++ release-scripts/update-versions.sh
@@ -6,13 +6,13 @@ set PATCH_VERSION 0
 set BRANCH 5.1
 
 function do_replace
-    git checkout $BRANCH
-    sed -i "s/set(.*$argv""_VERSION_MAJOR .*)/set($argv""_VERSION_MAJOR $MAJOR_VERSION)/g" CMakeLists.txt
-    sed -i "s/set(.*$argv""_VERSION_MINOR .*)/set($argv""_VERSION_MINOR $MINOR_VERSION)/g" CMakeLists.txt
-    sed -i "s/set(.*$argv""_VERSION_PATCH .*)/set($argv""_VERSION_PATCH $PATCH_VERSION)/g" CMakeLists.txt
-    git diff
-    git commit -a -m "Update version number to $MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
-    git push --set-upstream origin $BRANCH
+        git checkout $BRANCH
+	sed -i "s/set(.*$argv""_VERSION_MAJOR .*)/set($argv""_VERSION_MAJOR $MAJOR_VERSION)/g" CMakeLists.txt
+	sed -i "s/set(.*$argv""_VERSION_MINOR .*)/set($argv""_VERSION_MINOR $MINOR_VERSION)/g" CMakeLists.txt
+	sed -i "s/set(.*$argv""_VERSION_PATCH .*)/set($argv""_VERSION_PATCH $PATCH_VERSION)/g" CMakeLists.txt
+        git diff
+        git commit -a -m "Update version number to $MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION"
+        git push --set-upstream origin $BRANCH
 end
 
 cs kdev-python
@@ -23,3 +23,4 @@ cs kdevelop
 do_replace KDEVELOP
 cs kdevplatform
 do_replace KDEVPLATFORM
+
