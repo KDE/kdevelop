ae90b4f1370d799b12ceb06164c45bb07b9b9808
Burkhard Lück <lueck@hube-lueck.de>
2017-04-16 18:25:02 +0200

    fix message extraction in debuggers/common
    exclude tests dir, no i18n() calls, but breaks extraction due to spaces in filenames/paths

--- debuggers/common/Messages.sh
+++ debuggers/common/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 $EXTRACTRC `find . -name \*.ui` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/kdevdebuggercommon.pot
+$XGETTEXT `find . -name \*.cpp | grep -v tests` -o $podir/kdevdebuggercommon.pot
 rm -f rc.cpp
