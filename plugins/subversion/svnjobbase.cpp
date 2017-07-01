KDE/kdevelop

commit 7cda7c95d5bfa93f34c5a52df17480c248a8367c

Fix crashy dialogs (found by krazy and by hand)
Summary:
- use a wrapper class which simplifies the use of QPointer
  for the creation of local QDialogs
- some false positives are marked
- some dialogs not identified by krazy have the main window
  as parent, they may still cause a crash, when the program is killed

Test Plan:
Before:
- open "About KDevelop Platform" dialog
- "killall kdevelop" -> kdevelop closes (as intended), but with crash
After:
- open same dialog
- "killall kdevelop" -> kdevelop silently quits

Reviewers: #kdevelop, apol

Reviewed By: #kdevelop, apol

Subscribers: apol, anthonyfieroni, kdevelop-devel

Differential Revision: https://phabricator.kde.org/D6308

plugins/subversion/svnjobbase.cpp
@@ -68,11 +68,12 @@ void SvnJobBase::askForLogin( const QString& realm )
     qCDebug(PLUGIN_SVN) << "login";
     KPasswordDialog dlg( nullptr, KPasswordDialog::ShowUsernameLine | KPasswordDialog::ShowKeepPassword );
     dlg.setPrompt( i18n("Enter Login for: %1", realm ) );
-    dlg.exec();
-    internalJob()->m_login_username = dlg.username();
-    internalJob()->m_login_password = dlg.password();
-    internalJob()->m_maySave = dlg.keepPassword();
-    internalJob()->m_guiSemaphore.release( 1 );
+    if (dlg.exec()) { // krazy:exclude=crashy
+        internalJob()->m_login_username = dlg.username();
+        internalJob()->m_login_password = dlg.password();
+        internalJob()->m_maySave = dlg.keepPassword();
+        internalJob()->m_guiSemaphore.release( 1 );
+    }
 }
 
 void SvnJobBase::showNotification( const QString& path, const QString& msg )

language/codegen/basicrefactoring.cpp
@@ -309,8 +309,7 @@ BasicRefactoring::NameAndCollector BasicRefactoring::newNameForDeclaration(const
     const auto text = renameDialog.edit->text().trimmed();
     RefactoringProgressDialog refactoringProgress(i18n("Renaming \"%1\" to \"%2\"", declarationName, text), collector.data());
     if (!collector->isReady()) {
-        refactoringProgress.exec();
-        if (refactoringProgress.result() != QDialog::Accepted) {
+        if (refactoringProgress.exec() != QDialog::Accepted) { // krazy:exclude=crashy
             return {};
         }
     }

plugins/appwizard/appwizardplugin.cpp
@@ -46,6 +46,7 @@
 #include <interfaces/idocumentcontroller.h>
 #include <interfaces/context.h>
 #include <interfaces/contextmenuextension.h>
+#include <util/scopeddialog.h>
 #include <vcs/vcsjob.h>
 #include <vcs/interfaces/icentralizedversioncontrol.h>
 #include <vcs/interfaces/idistributedversioncontrol.h>
@@ -84,16 +85,16 @@ void AppWizardPlugin::slotNewProject()
 {
     model()->refresh();
 
-    AppWizardDialog dlg(core()->pluginController(), m_templatesModel);
+    ScopedDialog<AppWizardDialog> dlg(core()->pluginController(), m_templatesModel);
 
-    if (dlg.exec() == QDialog::Accepted)
+    if (dlg->exec() == QDialog::Accepted)
     {
-        QString project = createProject( dlg.appInfo() );
+        QString project = createProject( dlg->appInfo() );
         if (!project.isEmpty())
         {
             core()->projectController()->openProject(QUrl::fromLocalFile(project));
 
-            KConfig templateConfig(dlg.appInfo().appTemplate);
+            KConfig templateConfig(dlg->appInfo().appTemplate);
             KConfigGroup general(&templateConfig, "General");
             const QStringList fileArgs = general.readEntry("ShowFilesAfterGeneration").split(QLatin1Char(','), QString::SkipEmptyParts);
             for (const auto& fileArg : fileArgs) {

plugins/appwizard/projectselectionpage.cpp
@@ -26,6 +26,7 @@
 #include <language/codegen/templatepreviewicon.h>
 
 #include <util/multilevellistview.h>
+#include <util/scopeddialog.h>
 
 #include "ui_projectselectionpage.h"
 #include "projecttemplatesmodel.h"
@@ -305,15 +306,15 @@ void ProjectSelectionPage::loadFileClicked()
         QStringLiteral("application/x-bzip-compressed-tar"),
         QStringLiteral("application/zip")
     };
-    QFileDialog fileDialog(this, i18n("Load Template From File"));
-    fileDialog.setMimeTypeFilters(supportedMimeTypes);
-    fileDialog.setFileMode(QFileDialog::ExistingFiles);
+    ScopedDialog<QFileDialog> fileDialog(this, i18n("Load Template From File"));
+    fileDialog->setMimeTypeFilters(supportedMimeTypes);
+    fileDialog->setFileMode(QFileDialog::ExistingFiles);
 
-    if (!fileDialog.exec()) {
+    if (!fileDialog->exec()) {
         return;
     }
 
-    for (const auto& fileName : fileDialog.selectedFiles()) {
+    for (const auto& fileName : fileDialog->selectedFiles()) {
         QString destination = m_templatesModel->loadTemplateFile(fileName);
         QModelIndexList indexes = m_templatesModel->templateIndexes(destination);
         if (indexes.size() > 2)
@@ -326,10 +327,12 @@ void ProjectSelectionPage::loadFileClicked()
 
 void ProjectSelectionPage::moreTemplatesClicked()
 {
-    KNS3::DownloadDialog dialog(QStringLiteral("kdevappwizard.knsrc"), this);
-    dialog.exec();
+    ScopedDialog<KNS3::DownloadDialog> dialog(QStringLiteral("kdevappwizard.knsrc"), this);
 
-    auto entries = dialog.changedEntries();
+    if (!dialog->exec())
+        return;
+
+    auto entries = dialog->changedEntries();
     if (entries.isEmpty()) {
         return;
     }

plugins/externalscript/externalscriptview.cpp
@@ -32,6 +32,8 @@
 #include <KLocalizedString>
 #include <KMessageBox>
 
+#include <util/scopeddialog.h>
+
 
 ExternalScriptView::ExternalScriptView( ExternalScriptPlugin* plugin, QWidget* parent )
     : QWidget( parent ), m_plugin( plugin )
@@ -135,9 +137,8 @@ bool ExternalScriptView::eventFilter( QObject* obj, QEvent* e )
 void ExternalScriptView::addScript()
 {
   ExternalScriptItem* item = new ExternalScriptItem;
-  EditExternalScript dlg( item, this );
-  int ret = dlg.exec();
-  if ( ret == QDialog::Accepted) {
+  KDevelop::ScopedDialog<EditExternalScript> dlg( item, this );
+  if ( dlg->exec() == QDialog::Accepted) {
     m_plugin->model()->appendRow( item );
   } else {
     delete item;
@@ -170,9 +171,8 @@ void ExternalScriptView::editScript()
     return;
   }
 
-  EditExternalScript dlg( item, this );
-  int ret = dlg.exec();
-  if (ret == QDialog::Accepted) {
+  KDevelop::ScopedDialog<EditExternalScript> dlg( item, this );
+  if (dlg->exec() == QDialog::Accepted) {
     item->save();
   }
 }

plugins/filetemplates/templateclassassistant.cpp
@@ -45,12 +45,12 @@
 #include <project/projectmodel.h>
 #include <project/interfaces/iprojectfilemanager.h>
 #include <project/interfaces/ibuildsystemmanager.h>
+#include <util/scopeddialog.h>
 
 #include <QDialog>
 #include <QDialogButtonBox>
 #include <QLabel>
 #include <QListWidget>
-#include <QPointer>
 #include <QPushButton>
 #include <QVBoxLayout>
 
@@ -198,7 +198,7 @@ void TemplateClassAssistantPrivate::addFilesToTarget (const QHash< QString, QUrl
     else if (targets.size() > 1)
     {
         // More than one candidate target, show the chooser dialog
-        QPointer<QDialog> d = new QDialog;
+        ScopedDialog<QDialog> d;
 
         auto mainLayout = new QVBoxLayout(d);
         mainLayout->addWidget(new QLabel(i18n("Choose one target to add the file or cancel if you do not want to do so.")));
@@ -215,8 +215,8 @@ void TemplateClassAssistantPrivate::addFilesToTarget (const QHash< QString, QUrl
         QPushButton *okButton = buttonBox->button(QDialogButtonBox::Ok);
         okButton->setDefault(true);
         okButton->setShortcut(Qt::CTRL | Qt::Key_Return);
-        d->connect(buttonBox, &QDialogButtonBox::accepted, d.data(), &QDialog::accept);
-        d->connect(buttonBox, &QDialogButtonBox::rejected, d.data(), &QDialog::reject);
+        d->connect(buttonBox, &QDialogButtonBox::accepted, d, &QDialog::accept);
+        d->connect(buttonBox, &QDialogButtonBox::rejected, d, &QDialog::reject);
         mainLayout->addWidget(buttonBox);
 
         if(d->exec() == QDialog::Accepted)

plugins/filetemplates/templateselectionpage.cpp
@@ -32,6 +32,7 @@
 #include <interfaces/iproject.h>
 #include <interfaces/iprojectcontroller.h>
 #include <interfaces/isession.h>
+#include <util/scopeddialog.h>
 
 #include "ui_templateselection.h"
 
@@ -159,16 +160,16 @@ void TemplateSelectionPagePrivate::loadFileClicked()
         QStringLiteral("application/x-bzip-compressed-tar"),
         QStringLiteral("application/zip")
     };
-    QFileDialog dlg(page);
-    dlg.setMimeTypeFilters(filters);
-    dlg.setFileMode(QFileDialog::ExistingFiles);
+    ScopedDialog<QFileDialog> dlg(page);
+    dlg->setMimeTypeFilters(filters);
+    dlg->setFileMode(QFileDialog::ExistingFiles);
 
-    if (!dlg.exec())
+    if (!dlg->exec())
     {
         return;
     }
     
-    foreach(const QString& fileName, dlg.selectedFiles())
+    foreach(const QString& fileName, dlg->selectedFiles())
     {
         QString destination = model->loadTemplateFile(fileName);
         QModelIndexList indexes = model->templateIndexes(destination);

plugins/git/gitplugin.cpp
@@ -516,7 +516,7 @@ void GitPlugin::addNotVersionedFiles(const QDir& dir, const QList<QUrl>& files)
 
     if(!toadd.isEmpty()) {
         VcsJob* job = add(toadd);
-        job->exec();
+        job->exec(); // krazy:exclude=crashy
     }
 }
 

commit 27fc9e3396cce1324f4f6c7b53693ac5daedfe2f

Fix releasing semaphore (bug introduced by commit 7cda7c95d5bf)

plugins/subversion/svnjobbase.cpp
@@ -72,8 +72,11 @@ void SvnJobBase::askForLogin( const QString& realm )
         internalJob()->m_login_username = dlg.username();
         internalJob()->m_login_password = dlg.password();
         internalJob()->m_maySave = dlg.keepPassword();
-        internalJob()->m_guiSemaphore.release( 1 );
+    } else {
+        internalJob()->m_login_username.clear();
+        internalJob()->m_login_password.clear();
     }
+    internalJob()->m_guiSemaphore.release( 1 );
 }
 
 void SvnJobBase::showNotification( const QString& path, const QString& msg )