KDE/kdevelop

commit a8bbae4b0072a92bdf9f5a6a91a6e200310e0efb

Give more QMenus a widget parent, so their base window can be queried
As needed for wayland

sublime/container.cpp
@@ -589,7 +589,7 @@ void Container::contextMenu( const QPoint& pos )
 
     int currentTab = d->tabBar->tabAt(pos);
 
-    QMenu menu;
+    QMenu menu(senderWidget);
 
     Sublime::View* view = viewForWidget(widget(currentTab));
     emit tabContextMenuRequested(view, &menu);

plugins/contextbrowser/contextbrowserview.cpp
@@ -93,7 +93,7 @@ void ContextBrowserView::declarationMenu() {
         if(navigationContext && navigationContext->declaration().data()) {
             KDevelop::DeclarationContext* c = new KDevelop::DeclarationContext(navigationContext->declaration().data());
             lock.unlock();
-            QMenu menu;
+            QMenu menu(this);
             QList<ContextMenuExtension> extensions = ICore::self()->pluginController()->queryPluginsForContextMenuExtensions( c );
 
             ContextMenuExtension::populateMenu(&menu, extensions);

plugins/externalscript/externalscriptview.cpp
@@ -105,7 +105,7 @@ void ExternalScriptView::validateActions()
 
 void ExternalScriptView::contextMenu( const QPoint& pos )
 {
-  QMenu menu;
+  QMenu menu(this);
   menu.addActions( actions() );
 
   menu.exec( scriptTree->mapToGlobal( pos ) );

plugins/grepview/grepoutputview.cpp
@@ -384,7 +384,7 @@ void GrepOutputView::clearSearchHistory()
 void GrepOutputView::modelSelectorContextMenu(const QPoint& pos)
 {
     QPoint globalPos = modelSelector->mapToGlobal(pos);
-    QMenu myMenu;
+    QMenu myMenu(this);
     myMenu.addAction(m_clearSearchHistory);
     myMenu.exec(globalPos);
 }

plugins/projectmanagerview/projectbuildsetwidget.cpp
@@ -145,7 +145,7 @@ void ProjectBuildSetWidget::showContextMenu( const QPoint& p )
     }
 
 
-    QMenu m;
+    QMenu m(this);
     m.setTitle( i18n("Build Set") );
     m.addAction( QIcon::fromTheme(QStringLiteral("list-remove")), i18n( "Remove From Build Set" ), this, SLOT(removeItems()) );
 

plugins/vcschangesview/vcschangesview.cpp
@@ -92,7 +92,7 @@ void VcsChangesView::popupContextMenu( const QPoint &pos )
         }
     }
 
-    QPointer<QMenu> menu = new QMenu;
+    QPointer<QMenu> menu = new QMenu(this);
     QAction* refreshAction = menu->addAction(QIcon::fromTheme(QStringLiteral("view-refresh")), i18n("Refresh"));
     QList<ContextMenuExtension> extensions;
     if(!urls.isEmpty()) {

shell/launchconfigurationdialog.cpp
@@ -209,7 +209,7 @@ void LaunchConfigurationDialog::doTreeContextMenu(QPoint point)
         QModelIndex selected = tree->selectionModel()->selectedRows().first();
         if ( selected.parent().isValid() && ! selected.parent().parent().isValid() ) {
             // only display the menu if a launch config is clicked
-            QMenu menu;
+            QMenu menu(tree);
             QAction* rename = new QAction(QIcon::fromTheme(QStringLiteral("edit-rename")), i18n("Rename configuration"), &menu);
             QAction* delete_ = new QAction(QIcon::fromTheme(QStringLiteral("edit-delete")), i18n("Delete configuration"), &menu);
             connect(rename, &QAction::triggered, this, &LaunchConfigurationDialog::renameSelected);

shell/mainwindow_p.cpp
@@ -409,7 +409,7 @@ void MainWindowPrivate::tabToolTipRequested(Sublime::View* view, Sublime::Contai
 
 void MainWindowPrivate::dockBarContextMenuRequested(Qt::DockWidgetArea area, const QPoint& position)
 {
-    QMenu menu;
+    QMenu menu(m_mainWindow);
     menu.addSection(QIcon::fromTheme(QStringLiteral("window-new")), i18n("Add Tool View"));
     QHash<IToolViewFactory*, Sublime::ToolDocument*> factories =
         Core::self()->uiControllerInternal()->factoryDocuments();

commit 2c8b356bb9014dc780474f2bc644569db1e1a8eb

sublime: Fix crash when splitting views
Crash likely introduced by a8bbae4b0072a92bdf9f5a6a91a6e200310e0efb

The problem was the double-deletion of the QMenu instance (first via
QObject child-parent relationship triggered by the deletion of the
parent of the QMenu instance after invoking an action in said QMenu,
second via cleanup at end of the current scope)

ASAN trace:
==25944==ERROR: AddressSanitizer: attempting free on address which was not malloc()-ed: 0x7ffcf4d4ae80 in thread T0
    #0 0x7f9061a85f5f in QVector<QExplicitlySharedDataPointer<KDevelop::StaticAssistant> >::~QVector() /usr/include/x86_64-linux-gnu/qt5/QtCore/qvector.h:75:37
    #0 0x510e20 in operator delete(void*, unsigned long) (/home/kfunk/devel/install/kf5-stable/bin/kdevelop+0x510e20)
    #1 0x7f0d17e4bcd0 in QObjectPrivate::deleteChildren() (/usr/lib/x86_64-linux-gnu/libQt5Core.so.5+0x2a6cd0)
    #2 0x7f0d18b3c3da in QWidget::~QWidget() (/usr/lib/x86_64-linux-gnu/libQt5Widgets.so.5+0x1963da)
    #3 0x7f0d1e4a7684 in Sublime::ContainerTabBar::~ContainerTabBar() /home/kfunk/devel/src/kf5/kdevplatform-stable/sublime/container.cpp:50:7
    #4 0x7f0d1e4a76a8 in Sublime::ContainerTabBar::~ContainerTabBar() /home/kfunk/devel/src/kf5/kdevplatform-stable/sublime/container.cpp:50:7
    #5 0x7f0d17e4bcd0 in QObjectPrivate::deleteChildren() (/usr/lib/x86_64-linux-gnu/libQt5Core.so.5+0x2a6cd0)
    #6 0x7f0d18b3c3da in QWidget::~QWidget() (/usr/lib/x86_64-linux-gnu/libQt5Widgets.so.5+0x1963da)
    #7 0x7f0d1e49b0e2 in Sublime::Container::~Container() /home/kfunk/devel/src/kf5/kdevplatform-stable/sublime/container.cpp:349:1
    #8 0x7f0d1e49b128 in Sublime::Container::~Container() /home/kfunk/devel/src/kf5/kdevplatform-stable/sublime/container.cpp:347:1
    #9 0x7f0d1e4fd95f in Sublime::MainWindowPrivate::viewAdded(Sublime::AreaIndex*, Sublime::View*) /home/kfunk/devel/src/kf5/kdevplatform-stable/sublime/mainwindow_p.cpp:552:21

sublime/container.cpp
@@ -590,27 +590,28 @@ void Container::contextMenu( const QPoint& pos )
 
     int currentTab = d->tabBar->tabAt(pos);
 
-    QMenu menu(senderWidget);
+    QPointer<QMenu> menu = new QMenu(senderWidget);
+    menu->setAttribute(Qt::WA_DeleteOnClose);
 
     Sublime::View* view = viewForWidget(widget(currentTab));
-    emit tabContextMenuRequested(view, &menu);
+    emit tabContextMenuRequested(view, menu);
 
-    menu.addSeparator();
+    menu->addSeparator();
     QAction* copyPathAction = nullptr;
     QAction* closeTabAction = nullptr;
     QAction* closeOtherTabsAction = nullptr;
     if (view) {
-        copyPathAction = menu.addAction(QIcon::fromTheme(QStringLiteral("edit-copy")),
+        copyPathAction = menu->addAction(QIcon::fromTheme(QStringLiteral("edit-copy")),
                                         i18n("Copy Filename"));
-        menu.addSeparator();
-        closeTabAction = menu.addAction(QIcon::fromTheme(QStringLiteral("document-close")),
+        menu->addSeparator();
+        closeTabAction = menu->addAction(QIcon::fromTheme(QStringLiteral("document-close")),
                                         i18n("Close File"));
-        closeOtherTabsAction = menu.addAction(QIcon::fromTheme(QStringLiteral("document-close")),
+        closeOtherTabsAction = menu->addAction(QIcon::fromTheme(QStringLiteral("document-close")),
                                               i18n("Close Other Files"));
     }
-    QAction* closeAllTabsAction = menu.addAction( QIcon::fromTheme(QStringLiteral("document-close")), i18n( "Close All Files" ) );
+    QAction* closeAllTabsAction = menu->addAction( QIcon::fromTheme(QStringLiteral("document-close")), i18n( "Close All Files" ) );
 
-    QAction* triggered = menu.exec(senderWidget->mapToGlobal(pos));
+    QAction* triggered = menu->exec(senderWidget->mapToGlobal(pos));
 
     if (triggered) {
         if ( triggered == closeTabAction ) {