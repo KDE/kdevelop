52e289486d28b21936e3c13070287459af94b9fe
Kevin Funk <kfunk@kde.org>
2017-07-14 13:25:27 +0200

    WelcomePage: Disable SSE2 check for >=Clang 4.0
    Better be safe than sorry. I hope I covered all special cases by now...

--- plugins/welcomepage/welcomepageplugin.cpp
+++ plugins/welcomepage/welcomepageplugin.cpp
@@ -38,7 +38,8 @@ namespace {
 WelcomePageWidget* createWelcomePageWidget(QWidget* parent)
 {
     // don't attempt to load any QML if CPU doesn't have SSE2 support (cf. bug 381999)
-#if defined(Q_OS_LINUX) && INTPTR_MAX == INT32_MAX && (defined(Q_CC_GNU) || (defined(Q_CC_CLANG) && __clang_major__ >= 3 && __clang_minor__ >= 7))
+    // Note: Clang 4.0 moved the definition of __cpu_model (used by __builtin_cpu_supporst) into compiler-rt lib it seems -- let's just disable this feature on this compiler
+#if defined(Q_OS_LINUX) && INTPTR_MAX == INT32_MAX && (defined(Q_CC_GNU) || (defined(Q_CC_CLANG) && __clang_major__ >= 3 && __clang_minor__ >= 7 && __clang_major__ < 4))
     if (!__builtin_cpu_supports("sse2")) {
         qWarning() << "Welcome Page won't load any QML -- lacking SSE2 support on this processor";
         return nullptr;
