12f5de1c9706b7b5b49528b57acfe4a350110839
Kevin Funk <kfunk@kde.org>
2017-08-01 10:07:48 +0200

    UsesWidget: Use <code> to achieve monospace font
    "Monospace" is not a font family, it is a style (it's a predefined alias
    of fontconfig, so it is unavailable if Qt is configured not to use fontconfig).
    
    Also see:
      http://lists.qt-project.org/pipermail/qt-interest-old/2011-October/036282.html
    
    BUG: 378614
    FIXED-IN: 5.1.2

--- language/duchain/navigation/useswidget.cpp
+++ language/duchain/navigation/useswidget.cpp
@@ -95,10 +95,10 @@ QString highlightAndEscapeUseText(QString line, int cutOff, KTextEditor::Range r
   const QColor background(251, 250, 150);
   const QColor foreground(0, 0, 0);
 
-  return "<span style=\"font-family:'monospace'\">" + line.left(range.start().column()).toHtmlEscaped()
+  return "<code>" + line.left(range.start().column()).toHtmlEscaped()
                     + "<span style=\"background-color:" + background.name() + ";color:" + foreground.name() + ";\">"
                     + line.mid(range.start().column(), range.end().column() - range.start().column()).toHtmlEscaped()
-                    + "</span>" + line.mid(range.end().column(), line.length() - range.end().column()).toHtmlEscaped() + "</span>";
+                    + "</span>" + line.mid(range.end().column(), line.length() - range.end().column()).toHtmlEscaped() + "</code>";
 }
 
 /**
