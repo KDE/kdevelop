beedc97ee6ceac1da6e66035062bd5d8cc236283
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-08-02 15:12:58 +0200

    Don't use doxygen comment style for license header

--- language/duchain/types/containertypes.cpp
+++ language/duchain/types/containertypes.cpp
@@ -1,4 +1,4 @@
-/**
+/*
  * This file is part of KDevelop
  * Copyright (C) 2011-2014 Sven Brauch <svenbrauch@googlemail.com>
  *
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU Library General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- **/
+ */
 
 #include "containertypes.h"
 
