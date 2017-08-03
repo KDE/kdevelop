3f8944908a201d7354996395f2e3d9004f705054
Friedrich W. H. Kossebau <kossebau@kde.org>
2017-08-03 17:05:17 +0200

    Fix CLang DUCHain builder to set abstract property on right object
    setAbstract has been the setter of some broken shadowed property on
    ClassFunctionDeclaration's base class ClassMemberDeclaration

--- languages/clang/duchain/builder.cpp
+++ languages/clang/duchain/builder.cpp
@@ -1071,7 +1071,7 @@ void Visitor::setDeclData(CXCursor cursor, ClassFunctionDeclaration* decl) const
 {
     setDeclData<CK>(cursor, static_cast<AbstractFunctionDeclaration*>(decl));
     setDeclData<CK>(cursor, static_cast<ClassMemberDeclaration*>(decl));
-    decl->setAbstract(clang_CXXMethod_isPureVirtual(cursor));
+    decl->setIsAbstract(clang_CXXMethod_isPureVirtual(cursor));
     decl->setStatic(clang_CXXMethod_isStatic(cursor));
     decl->setVirtual(clang_CXXMethod_isVirtual(cursor));
 
