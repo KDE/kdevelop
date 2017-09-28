903789c9a74fc064c8633863c31f7d51cc546dae
Kevin Funk <kfunk@kde.org>
2017-09-28 22:07:05 +0200

    welcomepage: Introduce categorized logging

--- plugins/welcomepage/declarative/kdevelopdashboarddeclarativeplugin.cpp
+++ plugins/welcomepage/declarative/kdevelopdashboarddeclarativeplugin.cpp
@@ -17,16 +17,46 @@
  *************************************************************************************/
 
 #include "kdevelopdashboarddeclarativeplugin.h"
+
+#include "debug.h"
 #include "icoreobject.h"
+
 #include <qqml.h>
 #include <vcs/models/brancheslistmodel.h>
 #include <interfaces/iproject.h>
 #include <interfaces/icore.h>
 
+class Logger : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Logger(const QLoggingCategory &category, QObject *parent = nullptr)
+        : QObject(parent)
+        , m_category(category)
+    {}
+
+    Q_INVOKABLE void log(const QString &message)
+    {
+        qCDebug(m_category) << message;
+    }
+
+private:
+    const QLoggingCategory &m_category;
+};
+
 void KDevplatformDeclarativePlugin::registerTypes(const char* uri)
 {
-    qmlRegisterSingletonType<KDevelop::ICore>(uri, 1, 0, "ICore", [](QQmlEngine*, QJSEngine*) -> QObject* { return KDevelop::ICore::self(); });
+    static const QLoggingCategory loggingCategory{"kdevelop.plugins.welcomepage"};
+
+    qmlRegisterSingletonType<KDevelop::ICore>(uri, 1, 0, "ICore", [](QQmlEngine*, QJSEngine*) -> QObject* {
+        return KDevelop::ICore::self();
+    });
+    qmlRegisterSingletonType<KDevelop::ICore>(uri, 1, 0, "Logger", [](QQmlEngine*, QJSEngine*) -> QObject* {
+        return new Logger(loggingCategory);
+    });
     qmlRegisterType<KDevelop::BranchesListModel>(uri, 1, 0, "BranchesListModel");
 
     qmlRegisterType<KDevelop::IProject>();
 }
+
+#include "kdevelopdashboarddeclarativeplugin.moc"
