KDE/kdevelop

commit 3a74e103e03afea5946e96d12540ce32cb2f67bc

Avoid emptry entries in project files filter list
Summary:
Some project file manager plugins like KDevGenericManager do not have a list
of project file name filters. They need to be skipped when accumulating the
lists of file name filters instead of adding empty entries.

Test Plan:
Use "Open/Import Project..." action when running in environment with KDE_FULL_SESSION
being set and select the dropdown list of the filter selection.
No more empty entries after this patch.

Reviewers: #kdevelop, mwolff

Reviewed By: #kdevelop, mwolff

Subscribers: mwolff, kdevelop-devel

Differential Revision: https://phabricator.kde.org/D11912

kdevplatform/shell/openprojectdialog.cpp
@@ -98,6 +98,10 @@ OpenProjectDialog::OpenProjectDialog(bool fetch, const QUrl& startUrl,
     foreach(const KPluginMetaData& info, plugins)
     {
         QStringList filter = KPluginMetaData::readStringList(info.rawData(), QStringLiteral("X-KDevelop-ProjectFilesFilter"));
+        // some project file manager plugins like KDevGenericManager have no file filter set
+        if (filter.isEmpty()) {
+            continue;
+        }
         QString desc = info.value(QStringLiteral("X-KDevelop-ProjectFilesFilterDescription"));
 
         m_projectFilters.insert(info.name(), filter);

commit ab2fcd3b27890dfdc0b33f9eb31163e62bdd34e9

shell: Display generic project managers again
Summary:
This fixes a regression in 3a74e103e03a which prevents empty filter lines
in the file dialog, but also removes plugins with empty filters from
the list of project managers.

This commit adds a second list for generic project managers to avoid
storing these in the project filters list.

Test Plan: Launch KDevelop, open the Import / Open Project dialog, select an empty folder, and click next. The Generic Project Manager should be shown in the dropbox.

Reviewers: #kdevelop, mwolff, kossebau

Reviewed By: #kdevelop, mwolff, kossebau

Subscribers: kfunk, brauch, mwolff, kossebau, kdevelop-devel

Differential Revision: https://phabricator.kde.org/D12279

kdevplatform/shell/openprojectdialog.cpp
@@ -97,15 +97,18 @@ OpenProjectDialog::OpenProjectDialog(bool fetch, const QUrl& startUrl,
     QVector<KPluginMetaData> plugins = ICore::self()->pluginController()->queryExtensionPlugins(QStringLiteral("org.kdevelop.IProjectFileManager"));
     foreach(const KPluginMetaData& info, plugins)
     {
+        m_projectPlugins.insert(info.name(), info);
+
         QStringList filter = KPluginMetaData::readStringList(info.rawData(), QStringLiteral("X-KDevelop-ProjectFilesFilter"));
+
         // some project file manager plugins like KDevGenericManager have no file filter set
         if (filter.isEmpty()) {
+            m_genericProjectPlugins << info.name();
             continue;
         }
         QString desc = info.value(QStringLiteral("X-KDevelop-ProjectFilesFilterDescription"));
 
         m_projectFilters.insert(info.name(), filter);
-        m_projectPlugins.insert(info.name(), info);
         allEntry += filter;
         filters << filterFormat.arg(filter.join(QStringLiteral(" ")), desc);
     }
@@ -260,13 +263,11 @@ void OpenProjectDialog::validateOpenUrl( const QUrl& url_ )
                     choices.append({file + QString(" (%1)").arg(plugin), meta.pluginId(), meta.iconName(), file});
                 }
             }
-            Q_FOREACH ( const auto& plugin, m_projectFilters.keys() ) {
-                qCDebug(SHELL) << plugin << m_projectFilters.value(plugin);
-                if ( m_projectFilters.value(plugin).isEmpty() ) {
-                    // that works in any case
-                    auto meta = m_projectPlugins.value(plugin);
-                    choices.append({plugin, meta.pluginId(), meta.iconName()});
-                }
+            // add managers that work in any case (e.g. KDevGenericManager)
+            Q_FOREACH ( const auto& plugin, m_genericProjectPlugins ) {
+                qCDebug(SHELL) << plugin;
+                auto meta = m_projectPlugins.value(plugin);
+                choices.append({plugin, meta.pluginId(), meta.iconName()});
             }
             page->populateProjectFileCombo(choices);
         }

kdevplatform/shell/openprojectdialog.h
@@ -80,6 +80,7 @@ private:
     QStringList m_fileList;
     QMap<QString, QStringList> m_projectFilters;
     QMap<QString, KPluginMetaData> m_projectPlugins;
+    QStringList m_genericProjectPlugins;
 
     KDevelop::OpenProjectPage* openPageWidget = nullptr;
     KDevelop::ProjectSourcePage* sourcePageWidget = nullptr;