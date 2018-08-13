fd49ad513a1e8f4c193d2471ca79c814fc4167bf
Friedrich W. H. Kossebau <kossebau@kde.org>
2018-08-13 20:49:37 +0200

    Gdb plugin: remove outdated bogus code adding a \n, just to be removed
    The <br> is added here, the returned string is not (any more) handled

--- plugins/gdb/gdboutputwidget.cpp
+++ plugins/gdb/gdboutputwidget.cpp
@@ -187,12 +187,6 @@ void GDBOutputWidget::slotUserCommandStdout(const QString& line)
 namespace {
     QString colorify(QString text, const QColor& color)
     {
-        // Make sure the newline is at the end of the newly-added
-        // string. This is so that we can always correctly remove
-        // newline inside 'flushPending'.
-        if (!text.endsWith('\n'))
-          text.append('\n');
-
         if (text.endsWith('\n'))
         {
             text.remove(text.length()-1, 1);
