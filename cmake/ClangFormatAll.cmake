c68bc80d27d88a96692c29145dfcc26fd358690e
Kevin Funk <kfunk@kde.org>
2018-08-28 06:28:58 +0200

    Minor: Fix message() call in CMake
    ERROR is no valid mode

--- cmake/ClangFormatAll.cmake
+++ cmake/ClangFormatAll.cmake
@@ -10,7 +10,7 @@ find_program (CLANG_FMT_CMD
         NAMES "clang-format"
         )
 if(NOT CLANG_FMT_CMD)
-    message(ERROR "clang-format not found!")
+    message(WARNING "clang-format not found!")
 else()
     add_custom_target( ClangFormatAll
         COMMAND ${CLANG_FMT_CMD} -style=file -i ${ALL_SOURCE_FILES}
