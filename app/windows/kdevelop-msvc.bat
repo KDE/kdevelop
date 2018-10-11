1600eabfc3dab2b6a98cafa341ffcb1515337e78
Kevin Funk <kfunk@kde.org>
2018-10-11 08:35:52 +0200

    windows: Streamline batch file a bit
    Fix coding style

--- app/windows/kdevelop-msvc.bat
+++ app/windows/kdevelop-msvc.bat
@@ -1,29 +1,29 @@
-setlocal EnableDelayedExpansion
+setlocal enabledelayedexpansion
 setlocal enableextensions
 
 if DEFINED VS140COMNTOOLS (
-    SET "base=%VS140COMNTOOLS%"
+    set "base=%VS140COMNTOOLS%"
 ) else ( IF DEFINED VS120COMNTOOLS (
-    SET "base=%VS120COMNTOOLS%"
+    set "base=%VS120COMNTOOLS%"
 ) else ( IF DEFINED VS110COMNTOOLS (
-    SET "base=%VS110COMNTOOLS%"
+    set "base=%VS110COMNTOOLS%"
 ) else ( IF DEFINED VS100COMNTOOLS (
-    SET "base=%VS100COMNTOOLS%"
+    set "base=%VS100COMNTOOLS%"
 ) else (
     START CMD /C "The Microsoft Visual C++ compiler was not found on your system, you might not be able to compile programs. && PAUSE"
 ) )))
 
-SET script="!base!\..\..\VC\vcvarsall.bat"
-CALL %script%
+set script="!base!\..\..\VC\vcvarsall.bat"
+call %script%
 
-FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\Software\KDE\KDevelop" /v Install_Dir`) DO (
+for /F "usebackq tokens=3*" %%A in (`REG QUERY "HKEY_LOCAL_MACHINE\Software\KDE\KDevelop" /v Install_Dir`) do (
     set appdir=%%A %%B
 )
 
 if NOT DEFINED appdir (
-    FOR /F "usebackq tokens=3*" %%A IN (`REG QUERY "HKEY_LOCAL_MACHINE\Software\Wow6432Node\KDE\KDevelop" /v Install_Dir`) DO (
+    for /F "usebackq tokens=3*" %%A in (`REG QUERY "HKEY_LOCAL_MACHINE\Software\Wow6432Node\KDE\KDevelop" /v Install_Dir`) do (
         set appdir=%%A %%B
     )
 )
 
-START "" "%appdir%\bin\kdevelop.exe"
+start "" "%appdir%\bin\kdevelop.exe"
