b2d202225cdbbb22a6f15e4dfa4b68a6fec28636
Pino Toscano <pino@kde.org>
2019-05-27 08:33:31 +0200

    astyle: support the system astyle library
    Summary:
    Find the astyle library installed in the system, and use it instead of
    the embedded copy (which is still used if the system library is not
    available).
    
    Test Plan:
    Builds fine with, and without a system libastyle (the embedded copy is used in the latter case).
    `test_astyle` passes in both cases.
    I did not try the plugin for real, though.
    
    Reviewers: mwolff, kossebau, apol
    
    Reviewed By: mwolff, apol
    
    Subscribers: lbeltrame, arrowd, cullmann, mwolff, kossebau, kdevelop-devel
    
    Tags: #kdevelop
    
    Differential Revision: https://phabricator.kde.org/D17760

---
+++ cmake/modules/FindLibAStyle.cmake
@@ -0,0 +1,74 @@
+# - Find LibAStyle
+# Find the Artistic Style library.
+#
+# This module defines
+#  LibAStyle_FOUND - whether the astyle library was found
+#  LibAStyle_LIBRARIES - the astyle library
+#  LibAStyle_INCLUDE_DIR - the include path of the astyle library
+#  LibAStyle_CFLAGS - the compiler flags needed when building with the astyle library
+
+#=============================================================================
+# Copyright 2018-2019 Pino Toscano <pino@kde.org>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+
+find_library(LibAStyle_LIBRARIES
+  NAMES astyle
+)
+
+find_path(LibAStyle_INCLUDE_DIR
+  NAMES astyle.h
+)
+
+# astyle is built without RTTI
+set(LibAStyle_CFLAGS "-fno-rtti")
+
+# fallback in case the version is not detected later on
+set(_libastyle_version "0")
+
+if(LibAStyle_LIBRARIES AND LibAStyle_INCLUDE_DIR)
+  include(CMakePushCheckState)
+  include(CheckCXXSourceCompiles)
+  cmake_push_check_state(RESET)
+  set(CMAKE_REQUIRED_FLAGS "${LibAStyle_CFLAGS}")
+  set(CMAKE_REQUIRED_LIBRARIES "${LibAStyle_LIBRARIES}")
+  set(CMAKE_REQUIRED_INCLUDES "${LibAStyle_INCLUDE_DIR}")
+  check_cxx_source_compiles("
+#include <astyle.h>
+int main()
+{
+  astyle::ASPeekStream foo(nullptr);
+  return 0;
+}" LIBASTYLE_HAS_ASPEEKSTREAM)
+  if(LIBASTYLE_HAS_ASPEEKSTREAM)
+    set(_libastyle_version "3.1")
+  endif()
+  cmake_pop_check_state()
+  if(NOT (_libastyle_version STREQUAL "0"))
+    message(STATUS "LibAStyle version autodetected as ${_libastyle_version} or greater")
+  endif()
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibAStyle
+  FOUND_VAR LibAStyle_FOUND
+  REQUIRED_VARS LibAStyle_LIBRARIES LibAStyle_INCLUDE_DIR
+  VERSION_VAR _libastyle_version
+)
+
+mark_as_advanced(LibAStyle_INCLUDE_DIR LibAStyle_LIBRARIES)
+
+if(LibAStyle_FOUND)
+  add_library(astylelib UNKNOWN IMPORTED)
+  set_target_properties(astylelib PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${LibAStyle_INCLUDE_DIR}"
+    IMPORTED_LOCATION "${LibAStyle_LIBRARIES}"
+    INTERFACE_COMPILE_OPTIONS "${LibAStyle_CFLAGS}"
+  )
+endif()
