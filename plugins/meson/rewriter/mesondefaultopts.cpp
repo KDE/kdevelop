40ee1e2714e39ecb29c054b89073904d266e8238
Daniel Mensinger <daniel@mensinger-ka.de>
2019-06-05 18:03:06 +0200

    meson: Added meson default_options rewriter action

---
+++ plugins/meson/rewriter/mesondefaultopts.cpp
@@ -0,0 +1,54 @@
+/* This file is part of KDevelop
+    Copyright 2019 Daniel Mensinger <daniel@mensinger-ka.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#include "mesondefaultopts.h"
+#include <QJsonObject>
+
+MesonRewriterDefaultOpts::MesonRewriterDefaultOpts(MesonRewriterDefaultOpts::Action action)
+    : m_action(action)
+{
+}
+
+MesonRewriterDefaultOpts::~MesonRewriterDefaultOpts() {}
+
+QJsonObject MesonRewriterDefaultOpts::command()
+{
+    QJsonObject res;
+
+    res[QStringLiteral("type")] = QStringLiteral("default_options");
+    res[QStringLiteral("operation")] = m_action == SET ? QStringLiteral("set") : QStringLiteral("delete");
+    res[QStringLiteral("options")] = m_data;
+
+    return res;
+}
+
+MesonRewriterDefaultOpts::Action MesonRewriterDefaultOpts::action() const
+{
+    return m_action;
+}
+
+void MesonRewriterDefaultOpts::clear()
+{
+    m_data = QJsonObject();
+}
+
+void MesonRewriterDefaultOpts::set(QString name, QString value)
+{
+    m_data[name] = value;
+}
---
+++ plugins/meson/rewriter/mesondefaultopts.h
@@ -0,0 +1,47 @@
+/* This file is part of KDevelop
+    Copyright 2019 Daniel Mensinger <daniel@mensinger-ka.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA 02110-1301, USA.
+*/
+
+#pragma once
+
+#include "mesonactionbase.h"
+
+class MesonRewriterDefaultOpts;
+
+using MesonDefaultOptsPtr = std::shared_ptr<MesonRewriterDefaultOpts>;
+
+class MesonRewriterDefaultOpts : public MesonRewriterActionBase
+{
+public:
+    enum Action { SET, DELETE };
+
+public:
+    explicit MesonRewriterDefaultOpts(Action action);
+    virtual ~MesonRewriterDefaultOpts();
+
+    QJsonObject command() override;
+
+    Action action() const;
+
+    void set(QString name, QString value);
+    void clear();
+
+private:
+    Action m_action;
+    QJsonObject m_data;
+};
