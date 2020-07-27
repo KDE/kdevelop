cdb0681f8a67a468ae4d3836164b56b946e61a0e
Igor Kushnir <igorkuo@gmail.com>
2020-07-27 16:39:15 +0300

    Can not add the cpp file because of the kdevformatsource standalone executable linking issues

--- kdevplatform/util/wildcardhelpers.h
+++ kdevplatform/util/wildcardhelpers.h
@@ -4,11 +4,52 @@
     SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
+/****************************************************************************
+**
+** Copyright (C) 2016 Giuseppe D'Angelo <dangelog@gmail.com>.
+** Copyright (C) 2016 Klarälvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author Giuseppe D'Angelo <giuseppe.dangelo@kdab.com>
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the QtCore module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 3 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL3 included in the
+** packaging of this file. Please review the following information to
+** ensure the GNU Lesser General Public License version 3 requirements
+** will be met: https://www.gnu.org/licenses/lgpl-3.0.html.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 2.0 or (at your option) the GNU General
+** Public license version 3 or any later version approved by the KDE Free
+** Qt Foundation. The licenses are as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL2 and LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-2.0.html and
+** https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #ifndef KDEVPLATFORM_WILDCARDHELPERS_H
 #define KDEVPLATFORM_WILDCARDHELPERS_H
 
-#include "utilexport.h"
-
+#include <QRegExp>
+#include <QRegularExpression>
 #include <QString>
 #include <QStringList>
 
@@ -16,7 +57,91 @@
 
 namespace WildcardHelpers {
 #if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
-KDEVPLATFORMUTIL_EXPORT QString wildcardToRegularExpression(const QString& pattern);
+inline QString wildcardToRegularExpression(const QString& pattern)
+{
+    const int wclen = pattern.length();
+    QString rx;
+    rx.reserve(wclen + wclen / 16);
+    int i = 0;
+    const QChar *wc = pattern.unicode();
+
+#ifdef Q_OS_WIN
+    const QLatin1Char nativePathSeparator('\\');
+    const QLatin1String starEscape("[^/\\\\]*");
+    const QLatin1String questionMarkEscape("[^/\\\\]");
+#else
+    const QLatin1Char nativePathSeparator('/');
+    const QLatin1String starEscape("[^/]*");
+    const QLatin1String questionMarkEscape("[^/]");
+#endif
+
+    while (i < wclen) {
+        const QChar c = wc[i++];
+        switch (c.unicode()) {
+        case '*':
+            if (i < wclen && wc[i] == QLatin1Char('*')) {
+                // Match zero or more of any characters including path separators.
+                // Note that ".*" can not be used here because it does not match
+                // newlines by default.
+                rx += QLatin1String("[\\d\\D]*");
+                ++i;
+            } else {
+                rx += starEscape;
+            }
+            break;
+        case '?':
+            rx += questionMarkEscape;
+            break;
+        case '\\':
+#ifdef Q_OS_WIN
+        case '/':
+            rx += QLatin1String("[/\\\\]");
+            break;
+#endif
+        case '$':
+        case '(':
+        case ')':
+        case '+':
+        case '.':
+        case '^':
+        case '{':
+        case '|':
+        case '}':
+            rx += QLatin1Char('\\');
+            rx += c;
+            break;
+        case '[':
+            rx += c;
+            // Support for the [!abc] or [!a-c] syntax
+            if (i < wclen) {
+                if (wc[i] == QLatin1Char('!')) {
+                    rx += QLatin1Char('^');
+                    ++i;
+                }
+
+                if (i < wclen && wc[i] == QLatin1Char(']'))
+                    rx += wc[i++];
+
+                while (i < wclen && wc[i] != QLatin1Char(']')) {
+                    // The '/' appearing in a character class invalidates the
+                    // regular expression parsing. It also concerns '\\' on
+                    // Windows OS types.
+                    if (wc[i] == QLatin1Char('/') || wc[i] == nativePathSeparator)
+                        return rx;
+                    if (wc[i] == QLatin1Char('\\'))
+                        rx += QLatin1Char('\\');
+                    rx += wc[i++];
+                }
+            }
+            break;
+        default:
+            rx += c;
+            break;
+        }
+    }
+
+    return QRegularExpression::anchoredPattern(rx);
+}
 #endif
 
 /**
@@ -24,7 +149,17 @@ KDEVPLATFORMUTIL_EXPORT QString wildcardToRegularExpression(const QString& patte
  * function treats its first argument as a single pattern, not a filter with
  * multiple patterns separated by spaces or semicolons.
  */
-KDEVPLATFORMUTIL_EXPORT bool matchSinglePattern(const QString& pattern, const QString& fileName);
+inline bool matchSinglePattern(const QString& pattern, const QString& fileName)
+{
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+    QRegularExpression rx(WildcardHelpers::wildcardToRegularExpression(pattern),
+                          QRegularExpression::CaseInsensitiveOption);
+    return rx.match(fileName).hasMatch();
+#else
+    QRegExp rx(pattern, Qt::CaseInsensitive, QRegExp::Wildcard);
+    return rx.exactMatch(fileName);
+#endif
+}
 
 /**
  *  @brief Works exactly as the QDir::match() overload with the same signature
