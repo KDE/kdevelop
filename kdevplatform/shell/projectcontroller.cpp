58aeae417f9ea808af534c6dbee62266e430b4c4
Igor Kushnir <igorkuo@gmail.com>
2020-09-04 22:05:06 +0300

    Remove a duplicate `m_currentlyOpening.removeAll()` call
    ProjectController::abortOpeningProject() calls
    `m_currentlyOpening.removeAll()` with the same URL argument right after
    the removed call.

--- kdevplatform/shell/projectcontroller.cpp
+++ kdevplatform/shell/projectcontroller.cpp
@@ -353,7 +353,6 @@ public:
                          q, &ProjectController::projectAboutToBeOpened);
         if ( !project->open( Path(url) ) )
         {
-            m_currentlyOpening.removeAll(url);
             q->abortOpeningProject(project);
             project->deleteLater();
         }
