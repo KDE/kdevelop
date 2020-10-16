KDE/kdevelop

commit 300ea0143f4dcf5537cac824e823bee1ce6077fc

Do not open documents after DocumentController::cleanup()
For now d->shuttingDown is checked in just one of many
DocumentController::openDocument() overloads and alternatives, because
this is the only overload that is known to be called after cleanup() and
cause crashes. If similar crashes happen because of other ways of
opening a document, the check can be moved or reused elsewhere.
Or perhaps a different strategy will be needed to fix these hypothetical
future crashes: e.g. simply returning nullptr doesn't work if the caller
dereferences the returned document pointer without checking.

BUG: 425994
FIXED-IN: 5.6.1

kdevplatform/shell/documentcontroller.cpp
@@ -94,6 +94,7 @@ public:
 
     // used to map urls to open docs
     QHash< QUrl, IDocument* > documents;
+    bool shuttingDown = false;
 
     QHash< QString, IDocumentFactory* > factories;
 
@@ -577,6 +578,8 @@ void DocumentController::cleanup()
 {
     Q_D(DocumentController);
 
+    d->shuttingDown = true;
+
     if (d->fileOpenRecent)
         d->fileOpenRecent->saveEntries( KConfigGroup(KSharedConfig::openConfig(), "Recent Files" ) );
 
@@ -698,6 +701,20 @@ IDocument* DocumentController::openDocument( const QUrl & inputUrl,
         const QString& encoding, IDocument* buddy)
 {
     Q_D(DocumentController);
+
+    if (d->shuttingDown) {
+        // When a user exits KDevelop during debugging, a code breakpoint can be hit,
+        // and as a consequence DebugController::showStepInSource() be called
+        // in the event loop started by Core::cleanup() => BackgroundParser::waitForIdle().
+        // Oblivious to the application state, DebugController then tries to open a document,
+        // which eventually results in a crash inside a slot connected to either
+        // &IDocumentController::textDocumentCreated or &IDocumentController::documentLoaded
+        // (these signals are emitted in the process of opening a document).
+        // Even had there been no crash, we should not open documents after cleanup(),
+        // because we will never close them.
+        qCDebug(SHELL) << "refusing to open document" << inputUrl << "after cleanup()";
+        return nullptr;
+    }
 
     return d->openDocumentInternal(inputUrl, QString(), range, encoding, activationParams, buddy);
 }

commit 60ffbc7c5704bbac16e999f7254af3d07fe3721a

Fix a new test_documentcontroller test failure
300ea0143f4dcf5537cac824e823bee1ce6077fc introduced
DocumentControllerPrivate::shuttingDown variable. It is set to false in
constructor and to true in DocumentController::cleanup(). However
TestDocumentController::init() calls DocumentController::initialize()
before each test function; TestDocumentController::cleanup() calls
DocumentController::cleanup() after each test function. So the second
test function fails to open a document after shutdown.

Let us set DocumentControllerPrivate::shuttingDown to false in
DocumentController::initialize() to fix the test failure.

kdevplatform/shell/documentcontroller.cpp
@@ -572,6 +572,9 @@ DocumentController::DocumentController( QObject *parent )
 
 void DocumentController::initialize()
 {
+    Q_D(DocumentController);
+
+    d->shuttingDown = false; // required by test_documentcontroller
 }
 
 void DocumentController::cleanup()