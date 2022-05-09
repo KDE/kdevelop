10bba0c02ab6b9cc2d96bc8f4229fe80bdf42b89
Igor Kushnir <igorkuo@gmail.com>
2022-05-09 11:37:35 +0300

    Don't bump KDEV_ITEMREPOSITORY_VERSION in each patch version
    Since b8e3021561710cfbaf50854e32027e3c95fafeba KDEVELOP_VERSION_PATCH is
    greater than 255 (e.g. 220770). The way KDEV_ITEMREPOSITORY_VERSION is
    currently calculated assumes that KDEVELOP_VERSION_PATCH is less than or
    equal to 255. Otherwise, the values of KDEV_ITEMREPOSITORY_VERSION
    calculated for different KDevelop versions may coincide.
    
    This commit resolves the potential issue by making
    KDEV_ITEMREPOSITORY_VERSION depend only on the major and the minor
    KDevelop versions. This has an extra benefit of clearing data
    repositories less often. The item repository format shouldn't change in
    patch versions anyway. On the unlikely occasion it does change, the
    responsible developer should remember to bump the
    KDEV_ITEMREPOSITORY_INCREMENT variable.

--- kdevplatform/config-kdevplatform.h.cmake
+++ kdevplatform/config-kdevplatform.h.cmake
@@ -4,8 +4,7 @@
 #include "kdevelop_version.h"
 
 #define KDEV_ITEMREPOSITORY_VERSION (KDEVELOP_VERSION_MAJOR << 24) \
-                                  + (KDEVELOP_VERSION_MINOR << 16) \
-                                  + (KDEVELOP_VERSION_PATCH << 8) \
+                                  + (@KDEVELOP_OWN_MINOR_VERSION@ << 16) \
                                   + @KDEV_ITEMREPOSITORY_INCREMENT@
 
 #endif
