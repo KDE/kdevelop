630be51eea7ef4e2bc8b0d2eb901a3ba20da6d74
Igor Kushnir <igorkuo@gmail.com>
2022-09-25 21:28:02 +0300

    Reformat isourceformattercontroller.h using clang-format

--- kdevplatform/interfaces/isourceformattercontroller.h
+++ kdevplatform/interfaces/isourceformattercontroller.h
@@ -15,8 +15,7 @@
 class QUrl;
 class QMimeType;
 
-namespace KDevelop
-{
+namespace KDevelop {
 class ISourceFormatter;
 class SourceFormatterStyle;
 
@@ -24,39 +23,39 @@ class SourceFormatterStyle;
  */
 class KDEVPLATFORMINTERFACES_EXPORT ISourceFormatterController : public QObject
 {
-		Q_OBJECT
+    Q_OBJECT
 
-	public:
-		explicit ISourceFormatterController(QObject *parent = nullptr);
-		~ISourceFormatterController() override;
+public:
+    explicit ISourceFormatterController(QObject* parent = nullptr);
+    ~ISourceFormatterController() override;
 
-		/** \return The formatter corresponding to the language
-		* of the document corresponding to the \p url.
-		* The language is then activated and the style is loaded.
-		* The source formatter is then ready to use.
-		* If mimetype of url is known already, use
-		* formatterForUrl(const QUrl& url, const QMimeType& mime) instead.
-		*/
-		virtual ISourceFormatter* formatterForUrl(const QUrl &url) = 0;
-		/** \return The formatter corresponding to the language
-		* of the document corresponding to the \p url.
-		* The language is then activated and the style is loaded.
-		* The source formatter is then ready to use.
-		* @param mime known mimetype of the url
-		*/
-		virtual ISourceFormatter* formatterForUrl(const QUrl& url, const QMimeType& mime) = 0;
-		///\return @c true if there are formatters at all, @c false otherwise
-		virtual bool hasFormatters() const = 0;
+    /** \return The formatter corresponding to the language
+     * of the document corresponding to the \p url.
+     * The language is then activated and the style is loaded.
+     * The source formatter is then ready to use.
+     * If mimetype of url is known already, use
+     * formatterForUrl(const QUrl& url, const QMimeType& mime) instead.
+     */
+    virtual ISourceFormatter* formatterForUrl(const QUrl& url) = 0;
+    /** \return The formatter corresponding to the language
+     * of the document corresponding to the \p url.
+     * The language is then activated and the style is loaded.
+     * The source formatter is then ready to use.
+     * @param mime known mimetype of the url
+     */
+    virtual ISourceFormatter* formatterForUrl(const QUrl& url, const QMimeType& mime) = 0;
+    ///\return @c true if there are formatters at all, @c false otherwise
+    virtual bool hasFormatters() const = 0;
 
-		virtual KDevelop::SourceFormatterStyle styleForUrl(const QUrl& url, const QMimeType& mime) = 0;
+    virtual KDevelop::SourceFormatterStyle styleForUrl(const QUrl& url, const QMimeType& mime) = 0;
 
-		///Set whether or not source formatting is disabled with \arg disable
-		virtual void disableSourceFormatting(bool disable) = 0;
-		///\return Whether or not source formatting is enabled
-		virtual bool sourceFormattingEnabled() = 0;
+    ///Set whether or not source formatting is disabled with \arg disable
+    virtual void disableSourceFormatting(bool disable) = 0;
+    ///\return Whether or not source formatting is enabled
+    virtual bool sourceFormattingEnabled() = 0;
 
-	Q_SIGNALS:
-		void hasFormattersChanged(bool hasFormatters);
+Q_SIGNALS:
+    void hasFormattersChanged(bool hasFormatters);
 };
 
 }
