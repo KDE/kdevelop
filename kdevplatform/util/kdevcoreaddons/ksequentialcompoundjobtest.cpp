1caf64f853c0914f5ed91a6ef0e93e5642ccea7f
Igor Kushnir <igorkuo@gmail.com>
2023-01-08 22:06:19 +0200

    KSequentialCompoundJobTest: expand run-job tests

--- kdevplatform/util/kdevcoreaddons/ksequentialcompoundjobtest.cpp
+++ kdevplatform/util/kdevcoreaddons/ksequentialcompoundjobtest.cpp
@@ -1,6 +1,7 @@
 /*
     This file is part of the KDE project
     SPDX-FileCopyrightText: 2014 Milian Wolff <mail@milianw.de>
+    SPDX-FileCopyrightText: 2023 Igor Kushnir <igorkuo@gmail.com>
 
     SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 */
@@ -17,10 +18,13 @@ using namespace KDevCoreAddons;
 
 struct JobSpy {
     explicit JobSpy(const KJob *job)
-        : finished(job, &KJob::finished)
+        : percentChanged(job, &KJob::percentChanged)
+        , finished(job, &KJob::finished)
         , result(job, &KJob::result)
     {
     }
+
+    QSignalSpy percentChanged;
     QSignalSpy finished;
     QSignalSpy result;
 };
@@ -30,12 +34,27 @@ void KSequentialCompoundJobTest::initTestCase()
     QStandardPaths::setTestModeEnabled(true);
 }
 
+void KSequentialCompoundJobTest::runZeroJobs()
+{
+    QPointer master(new KSimpleSequentialCompoundJob);
+    JobSpy masterSpy(master);
+
+    QCOMPARE(masterSpy.finished.count(), 0);
+    QCOMPARE(masterSpy.result.count(), 0);
+    master->start();
+    QCOMPARE(masterSpy.finished.count(), 1);
+    QCOMPARE(masterSpy.result.count(), 1);
+
+    QTest::qWait(1);
+    QVERIFY(!master);
+}
+
 void KSequentialCompoundJobTest::runOneJob()
 {
     QPointer slave(new TestJob);
 
     QPointer master(new KSimpleSequentialCompoundJob);
-    master->addSubjob(slave);
+    QVERIFY(master->addSubjob(slave));
     JobSpy masterSpy(master);
 
     QSignalSpy startedSpy(slave, &TestJob::started);
@@ -46,7 +65,13 @@ void KSequentialCompoundJobTest::runOneJob()
     QCOMPARE(slaveSpy.finished.count(), 0);
     QCOMPARE(masterSpy.finished.count(), 0);
 
+    QCOMPARE(masterSpy.percentChanged.count(), 0);
+    QCOMPARE(master->percent(), 0);
+
     slave->emitResult();
+
+    QCOMPARE(masterSpy.percentChanged.count(), 1);
+    QCOMPARE(master->percent(), 100);
 
     QCOMPARE(masterSpy.finished.count(), 1);
     QCOMPARE(masterSpy.result.count(), 1);
@@ -59,6 +84,9 @@ void KSequentialCompoundJobTest::runOneJob()
 
     QVERIFY(!slave);
     QVERIFY(!master);
+
+    QCOMPARE(slaveSpy.percentChanged.count(), 0);
+    QCOMPARE(masterSpy.percentChanged.count(), 1);
 }
 
 void KSequentialCompoundJobTest::runTwoJobs()
@@ -67,8 +95,8 @@ void KSequentialCompoundJobTest::runTwoJobs()
     QPointer slave2(new TestJob);
 
     QPointer master(new KSimpleSequentialCompoundJob);
-    master->addSubjob(slave1);
-    master->addSubjob(slave2);
+    QVERIFY(master->addSubjob(slave1));
+    QVERIFY(master->addSubjob(slave2));
     JobSpy masterSpy(master);
 
     QSignalSpy started1Spy(slave1, &TestJob::started);
@@ -83,7 +111,14 @@ void KSequentialCompoundJobTest::runTwoJobs()
     QCOMPARE(slave2Spy.finished.count(), 0);
     QCOMPARE(masterSpy.finished.count(), 0);
 
+    QCOMPARE(masterSpy.percentChanged.count(), 0);
+    QCOMPARE(master->percent(), 0);
+
     slave1->emitResult();
+
+    QCOMPARE(masterSpy.percentChanged.count(), 1);
+    QCOMPARE(master->percent(), 50);
+
     QCOMPARE(started1Spy.count(), 1);
     QCOMPARE(slave1Spy.finished.count(), 1);
     QCOMPARE(started2Spy.count(), 1);
@@ -91,6 +126,9 @@ void KSequentialCompoundJobTest::runTwoJobs()
     QCOMPARE(masterSpy.finished.count(), 0);
 
     slave2->emitResult();
+
+    QCOMPARE(masterSpy.percentChanged.count(), 2);
+    QCOMPARE(master->percent(), 100);
 
     QCOMPARE(masterSpy.finished.count(), 1);
     QCOMPARE(masterSpy.result.count(), 1);
@@ -108,6 +146,10 @@ void KSequentialCompoundJobTest::runTwoJobs()
     QVERIFY(!slave1);
     QVERIFY(!slave2);
     QVERIFY(!master);
+
+    QCOMPARE(slave1Spy.percentChanged.count(), 0);
+    QCOMPARE(slave2Spy.percentChanged.count(), 0);
+    QCOMPARE(masterSpy.percentChanged.count(), 2);
 }
 
 QTEST_GUILESS_MAIN(KSequentialCompoundJobTest)
--- kdevplatform/util/kdevcoreaddons/ksequentialcompoundjobtest.h
+++ kdevplatform/util/kdevcoreaddons/ksequentialcompoundjobtest.h
@@ -1,6 +1,7 @@
 /*
     This file is part of the KDE project
     SPDX-FileCopyrightText: 2014 Milian Wolff <mail@milianw.de>
+    SPDX-FileCopyrightText: 2023 Igor Kushnir <igorkuo@gmail.com>
 
     SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 */
@@ -33,6 +34,7 @@ class KSequentialCompoundJobTest : public QObject
 private Q_SLOTS:
     void initTestCase();
 
+    void runZeroJobs();
     void runOneJob();
     void runTwoJobs();
 };
