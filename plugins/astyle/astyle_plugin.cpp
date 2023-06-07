85deeb5e51a29b6d52f8b110080ffe8ca46e81b4
Igor Kushnir <igorkuo@gmail.com>
2023-06-07 13:59:13 +0300

    AStylePlugin: don't concatenate preview/sample strings
    If these preview/sample functions are called (when the user configures
    source formatters and styles), they are likely to be called many times.
    So it makes sense to avoid repeated QString concatenation at runtime.

--- plugins/astyle/astyle_plugin.cpp
+++ plugins/astyle/astyle_plugin.cpp
@@ -17,185 +17,206 @@
 #include <QMimeType>
 #include <QUrl>
 
-static const char formattingCxxSample[] =
-    "void func(){\n"
-    "\tif(isFoo(a,b))\n"
-    "\tbar(a,b);\n"
-    "if(isFoo)\n"
-    "\ta=bar((b-c)*a,*d--);\n"
-    "if(  isFoo( a,b ) )\n"
-    "\tbar(a, b);\n"
-    "if (isFoo) {isFoo=false;cat << isFoo <<endl;}\n"
-    "if(isFoo)DoBar();if (isFoo){\n"
-    "\tbar();\n"
-    "}\n"
-    "\telse if(isBar()){\n"
-    "\tannotherBar();\n"
-    "}\n"
-    "int var = 1;\n"
-    "int *ptr = &var;\n"
-    "int& ref = i;\n"
-    "\n"
-    "QList<int>::const_iterator it = list.begin();\n"
-    "}\n"
-    "namespace A {\n"
-    "namespace B {\n"
-    "class someClass {\n"
-    "void foo() {\n"
-    "  if (true) {\n"
-    "    func();\n"
-    "  } else {\n"
-    "    // bla\n"
-    "  }\n"
-    "}\n"
-    "};\n"
-    "}\n"
-    "}\n";
+namespace {
 
-static const char formattingObjCSample[] =
-    "void func(){\n"
-    "\tif(isFoo(a,b))\n"
-    "\tbar(a,b);\n"
-    "if(isFoo)\n"
-    "\ta=bar((b-c)*a,*d--);\n"
-    "if(  isFoo( a,b ) )\n"
-    "\tbar(a, b);\n"
-    "if (isFoo) {isFoo=false;cat << isFoo <<endl;}\n"
-    "if(isFoo)DoBar();if (isFoo){\n"
-    "\tbar();\n"
-    "}\n"
-    "\telse if(isBar()){\n"
-    "\tannotherBar();\n"
-    "}\n"
-    "int var = 1;\n"
-    "int *ptr = &var;\n"
-    "\n"
-    "}\n"
-    "@implementation someClass \n"
-    "+ (someClass*) someClassWithFoo:(int)foo\n"
-    "{\n"
-    "  someClass *this;\n"
-    "  if (foo) {\n"
-    "    this = [[someClass alloc] initWith:foo];\n"
-    "  } else {\n"
-    "    // bla\n"
-    "  }\n"
-    "  return self;\n"
-    "}\n"
-    "@end\n";
+QString formattingCppSample()
+{
+    return QStringLiteral(
+        "void func(){\n"
+        "\tif(isFoo(a,b))\n"
+        "\tbar(a,b);\n"
+        "if(isFoo)\n"
+        "\ta=bar((b-c)*a,*d--);\n"
+        "if(  isFoo( a,b ) )\n"
+        "\tbar(a, b);\n"
+        "if (isFoo) {isFoo=false;cat << isFoo <<endl;}\n"
+        "if(isFoo)DoBar();if (isFoo){\n"
+        "\tbar();\n"
+        "}\n"
+        "\telse if(isBar()){\n"
+        "\tannotherBar();\n"
+        "}\n"
+        "int var = 1;\n"
+        "int *ptr = &var;\n"
+        "int& ref = i;\n"
+        "\n"
+        "QList<int>::const_iterator it = list.begin();\n"
+        "}\n"
+        "namespace A {\n"
+        "namespace B {\n"
+        "class someClass {\n"
+        "void foo() {\n"
+        "  if (true) {\n"
+        "    func();\n"
+        "  } else {\n"
+        "    // bla\n"
+        "  }\n"
+        "}\n"
+        "};\n"
+        "}\n"
+        "}\n");
+}
 
-static const char indentingCxxSample[] =
-    "#define foobar(A)\\\n"
-    "{Foo();Bar();}\n"
-    "#define anotherFoo(B)\\\n"
-    "return Bar()\n"
-    "\n"
-    "namespace Bar\n"
-    "{\n"
-    "class Foo\n"
-    "{public:\n"
-    "Foo();\n"
-    "virtual ~Foo();\n"
-    "};\n"
-    "void bar(int foo)\n"
-    "{\n"
-    "switch (foo)\n"
-    "{\n"
-    "case 1:\n"
-    "a+=1;\n"
-    "break;\n"
-    "case 2:\n"
-    "{\n"
-    "a += 2;\n"
-    " break;\n"
-    "}\n"
-    "}\n"
-    "if (isFoo)\n"
-    "{\n"
-    "bar();\n"
-    "}\n"
-    "else\n"
-    "{\n"
-    "anotherBar();\n"
-    "}\n"
-    "}\n"
-    "int foo()\n"
-    "\twhile(isFoo)\n"
-    "\t\t{\n"
-    "\t\t\t// ...\n"
-    "\t\t\tgoto error;\n"
-    "\t\t/* .... */\n"
-    "\t\terror:\n"
-    "\t\t\t//...\n"
-    "\t\t}\n"
-    "\t}\n"
-    "fooArray[]={ red,\n"
-    "\tgreen,\n"
-    "\tdarkblue};\n"
-    "fooFunction(barArg1,\n"
-    "\tbarArg2,\n"
-    "\tbarArg3);\n"
-    "struct foo{ int bar() {} };\n";
+QString formattingObjCSample()
+{
+    return QStringLiteral(
+        "void func(){\n"
+        "\tif(isFoo(a,b))\n"
+        "\tbar(a,b);\n"
+        "if(isFoo)\n"
+        "\ta=bar((b-c)*a,*d--);\n"
+        "if(  isFoo( a,b ) )\n"
+        "\tbar(a, b);\n"
+        "if (isFoo) {isFoo=false;cat << isFoo <<endl;}\n"
+        "if(isFoo)DoBar();if (isFoo){\n"
+        "\tbar();\n"
+        "}\n"
+        "\telse if(isBar()){\n"
+        "\tannotherBar();\n"
+        "}\n"
+        "int var = 1;\n"
+        "int *ptr = &var;\n"
+        "\n"
+        "}\n"
+        "@implementation someClass \n"
+        "+ (someClass*) someClassWithFoo:(int)foo\n"
+        "{\n"
+        "  someClass *this;\n"
+        "  if (foo) {\n"
+        "    this = [[someClass alloc] initWith:foo];\n"
+        "  } else {\n"
+        "    // bla\n"
+        "  }\n"
+        "  return self;\n"
+        "}\n"
+        "@end\n");
+}
 
-static const char indentingObjCSample[] =
-    "#import <objc/Object.h>\n"
-    "\n"
-    "#define foobar(A)\\\n"
-    "\t{Foo();Bar();}\n"
-    "#define anotherFoo(B)\\\n"
-    "\treturn Bar()\n"
-    "\n"
-    "@interface Foo : Bar {\n"
-    "@private\n"
-    "\tid Baz;\n"
-    "}\n"
-    "- (void) init;\n"
-    "- (NSString*) description;\n"
-    "@property (retain) id Baz;\n"
-    "@end\n"
-    "\n"
-    "@interface Foo (Bar)\n"
-    "- (void)bar:(int) foo;\n"
-    "@end\n"
-    "\n"
-    "@implementation Foo (Bar)\n"
-    "\n"
-    "- (void) bar:(int) foo\n"
-    "{\n"
-    "\tswitch (foo) {\n"
-    "case 1:\n"
-    "a += 1;\n"
-    "break;\n"
-    "case 2: {\n"
-    "a += 2;\n"
-    "break;\n"
-    "}\n"
-    "}\n"
-    "if (isFoo) {\n"
-    "bar();\n"
-    "} else {\n"
-    "[anotherBar withFoo:self];\n"
-    "}\n"
-    "}\n"
-    "\n"
-    "@end\n"
-    "int foo()\n"
-    "while (isFoo)\n"
-    "{\n"
-    "\t// ...\n"
-    "\tgoto error;\n"
-    "\t/* .... */\n"
-    "error:\n"
-    "\t//...\n"
-    "}\n"
-    "\n"
-    "fooArray[] = { red,\n"
-    "\tgreen,\n"
-    "\tdarkblue};\n"
-    "fooFunction(barArg1,\n"
-    "\tbarArg2,\n"
-    "\tbarArg3);\n"
-    "struct foo { int bar() {} };\n";
+QString indentingCppSample()
+{
+    return QStringLiteral(
+        "#define foobar(A)\\\n"
+        "{Foo();Bar();}\n"
+        "#define anotherFoo(B)\\\n"
+        "return Bar()\n"
+        "\n"
+        "namespace Bar\n"
+        "{\n"
+        "class Foo\n"
+        "{public:\n"
+        "Foo();\n"
+        "virtual ~Foo();\n"
+        "};\n"
+        "void bar(int foo)\n"
+        "{\n"
+        "switch (foo)\n"
+        "{\n"
+        "case 1:\n"
+        "a+=1;\n"
+        "break;\n"
+        "case 2:\n"
+        "{\n"
+        "a += 2;\n"
+        " break;\n"
+        "}\n"
+        "}\n"
+        "if (isFoo)\n"
+        "{\n"
+        "bar();\n"
+        "}\n"
+        "else\n"
+        "{\n"
+        "anotherBar();\n"
+        "}\n"
+        "}\n"
+        "int foo()\n"
+        "\twhile(isFoo)\n"
+        "\t\t{\n"
+        "\t\t\t// ...\n"
+        "\t\t\tgoto error;\n"
+        "\t\t/* .... */\n"
+        "\t\terror:\n"
+        "\t\t\t//...\n"
+        "\t\t}\n"
+        "\t}\n"
+        "fooArray[]={ red,\n"
+        "\tgreen,\n"
+        "\tdarkblue};\n"
+        "fooFunction(barArg1,\n"
+        "\tbarArg2,\n"
+        "\tbarArg3);\n"
+        "struct foo{ int bar() {} };\n");
+}
+
+QString indentingObjCSample()
+{
+    return QStringLiteral(
+        "#import <objc/Object.h>\n"
+        "\n"
+        "#define foobar(A)\\\n"
+        "\t{Foo();Bar();}\n"
+        "#define anotherFoo(B)\\\n"
+        "\treturn Bar()\n"
+        "\n"
+        "@interface Foo : Bar {\n"
+        "@private\n"
+        "\tid Baz;\n"
+        "}\n"
+        "- (void) init;\n"
+        "- (NSString*) description;\n"
+        "@property (retain) id Baz;\n"
+        "@end\n"
+        "\n"
+        "@interface Foo (Bar)\n"
+        "- (void)bar:(int) foo;\n"
+        "@end\n"
+        "\n"
+        "@implementation Foo (Bar)\n"
+        "\n"
+        "- (void) bar:(int) foo\n"
+        "{\n"
+        "\tswitch (foo) {\n"
+        "case 1:\n"
+        "a += 1;\n"
+        "break;\n"
+        "case 2: {\n"
+        "a += 2;\n"
+        "break;\n"
+        "}\n"
+        "}\n"
+        "if (isFoo) {\n"
+        "bar();\n"
+        "} else {\n"
+        "[anotherBar withFoo:self];\n"
+        "}\n"
+        "}\n"
+        "\n"
+        "@end\n"
+        "int foo()\n"
+        "while (isFoo)\n"
+        "{\n"
+        "\t// ...\n"
+        "\tgoto error;\n"
+        "\t/* .... */\n"
+        "error:\n"
+        "\t//...\n"
+        "}\n"
+        "\n"
+        "fooArray[] = { red,\n"
+        "\tgreen,\n"
+        "\tdarkblue};\n"
+        "fooFunction(barArg1,\n"
+        "\tbarArg2,\n"
+        "\tbarArg3);\n"
+        "struct foo { int bar() {} };\n");
+}
+
+QString previewText(const QString& indentingSample, const QString& formattingSample)
+{
+    return QLatin1String("// Indentation\n") + indentingSample + QLatin1String("\t// Formatting\n") + formattingSample;
+}
+
+} // unnamed namespace
 
 using namespace KDevelop;
 
@@ -299,18 +320,14 @@ SettingsWidget* AStylePlugin::editStyleWidget(const QMimeType& mime) const
 
 QString AStylePlugin::previewText(const SourceFormatterStyle& /*style*/, const QMimeType& mime) const
 {
-    AStylePreferences::Language lang;
     if (mime.inherits(QStringLiteral("text/x-objcsrc")) || mime.inherits(QStringLiteral("text/x-objc++src"))) {
-       lang = AStylePreferences::ObjC;
+        static const QString text = ::previewText(indentingObjCSample(), formattingObjCSample());
+        return text;
     } else {
-       lang = AStylePreferences::CPP;
+        static const QString text = ::previewText(indentingCppSample(), formattingCppSample());
+        return text;
     }
     // TODO: add previews for the other supported languages
-    return
-      QLatin1String("// Indentation\n") +
-      indentingSample(lang) +
-      QLatin1String("\t// Formatting\n") +
-      formattingSample(lang);
 }
 
 AStylePlugin::Indentation AStylePlugin::indentation(const SourceFormatterStyle& style, const QUrl& url,
@@ -340,9 +357,9 @@ QString AStylePlugin::formattingSample(AStylePreferences::Language lang)
 {
    switch (lang) {
       case AStylePreferences::ObjC:
-         return QLatin1String(formattingObjCSample);
+        return formattingObjCSample();
       default:
-         return QLatin1String(formattingCxxSample);
+        return formattingCppSample();
    }
    Q_UNREACHABLE();
 }
@@ -351,9 +368,9 @@ QString AStylePlugin::indentingSample(AStylePreferences::Language lang)
 {
    switch (lang) {
       case AStylePreferences::ObjC:
-         return QLatin1String(indentingObjCSample);
+        return indentingObjCSample();
       default:
-         return QLatin1String(indentingCxxSample);
+        return indentingCppSample();
    }
    Q_UNREACHABLE();
 }
