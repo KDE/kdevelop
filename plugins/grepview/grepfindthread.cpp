KDE/kdevelop

commit c0d4221d344b47f29a7862c4bc086e0a7a8bae7c

Find in Files: Allow depth definition
Initial patch by Adrián Chaves Fernández.
Thanks for the patch!

More changes:
* Factor out "bool isInDirectory(...)"
* Remove the anonymous enum polluting the root namespace

CCBUG: 160143
REVIEW: 105856

plugins/grepview/grepfindthread.cpp
@@ -14,10 +14,28 @@
 
 using KDevelop::IndexedString;
 
+/**
+ * @return Return true in case @p url is in @p dir within a maximum depth of @p maxDepth
+ */
+static bool isInDirectory(const KUrl& url, const KUrl& dir, int maxDepth)
+{
+    KUrl folderUrl = url.upUrl();
+
+    int currentLevel = maxDepth;
+    while(currentLevel > 0) {
+        folderUrl = folderUrl.upUrl();
+        if ( folderUrl.equals(dir, KUrl::CompareWithoutTrailingSlash) ) {
+            return true;
+        }
+        currentLevel--;
+    }
+    return false;
+}
+
 // the abort parameter must be volatile so that it
 // is evaluated every time - optimization might prevent that
 
-static KUrl::List thread_getProjectFiles(const KUrl dir, bool recursive, const QStringList include,
+static KUrl::List thread_getProjectFiles(const KUrl dir, int depth, const QStringList include,
                                          const QStringList exlude, volatile bool &abort)
 {
     ///@todo This is not thread-safe!
@@ -34,10 +52,20 @@ static KUrl::List thread_getProjectFiles(const KUrl dir, bool recursive, const Q
         KUrl url = item.toUrl();
         if( !url.equals(dir) )
         {
-            if( recursive && !dir.isParentOf(url) )
+            if ( depth == 0 ) {
+                if ( !url.upUrl().equals(dir, KUrl::CompareWithoutTrailingSlash) ) {
+                    continue;
+                }
+            } else if ( !dir.isParentOf(url) ) {
                 continue;
-            if( !recursive && !url.upUrl().equals(dir, KUrl::CompareWithoutTrailingSlash))
-                continue;
+            } else if ( depth > 0 ) {
+                // To ensure the current file is within the defined depth limit, navigate up the tree for as many levels
+                // as the depth value, trying to find "dir", which is the project folder. If after all the loops there
+                // is no match, it means the current file is deeper down the project tree than the limit depth, and so
+                // it must be skipped.
+                if(!isInDirectory(url, dir, depth))
+                    continue;
+            }
         }
         if( QDir::match(include, url.fileName()) && !QDir::match(exlude, url.toLocalFile()) )
             res << url;
@@ -46,8 +74,8 @@ static KUrl::List thread_getProjectFiles(const KUrl dir, bool recursive, const Q
     return res;
 }
 
-static KUrl::List thread_findFiles(const QDir& dir, bool recursive, const QStringList& include,
-                                           const QStringList& exclude, volatile bool &abort)
+static KUrl::List thread_findFiles(const QDir& dir, int depth, const QStringList& include,
+                                   const QStringList& exclude, volatile bool &abort)
 {
     QFileInfoList infos = dir.entryInfoList(include, QDir::NoDotAndDotDot|QDir::Files|QDir::Readable);
     
@@ -61,7 +89,7 @@ static KUrl::List thread_findFiles(const QDir& dir, bool recursive, const QStrin
         if(!QDir::match(exclude, currName))
             dirFiles << currName;
     }
-    if(recursive)
+    if(depth != 0)
     {
         static const QDir::Filters dirFilter = QDir::NoDotAndDotDot|QDir::AllDirs|QDir::Readable|QDir::NoSymLinks;
         foreach(const QFileInfo &currDir, dir.entryInfoList(QStringList(), dirFilter))
@@ -71,14 +99,29 @@ static KUrl::List thread_findFiles(const QDir& dir, bool recursive, const QStrin
             QString canonical = currDir.canonicalFilePath();
             if(!KUrl(dir.canonicalPath()).isParentOf(canonical))
                 continue;
-            dirFiles << thread_findFiles(canonical, true, include, exclude, abort);
+
+            if ( depth > 0 ) {
+                depth--;
+            }
+
+            dirFiles << thread_findFiles(canonical, depth, include, exclude, abort);
         }
     }
     return dirFiles;
 }
 
-GrepFindFilesThread::GrepFindFilesThread(QObject* parent, const QList<KUrl>& startDirs, bool recursive, const QString& pats, const QString& excl, bool onlyProject)
-    : QThread(parent), m_startDirs(startDirs), m_patString(pats), m_exclString(excl), m_recursive(recursive), m_project(onlyProject), m_tryAbort(false)
+GrepFindFilesThread::GrepFindFilesThread(QObject* parent,
+                                         const QList<KUrl>& startDirs,
+                                         int depth, const QString& pats,
+                                         const QString& excl,
+                                         bool onlyProject)
+: QThread(parent)
+, m_startDirs(startDirs)
+, m_patString(pats)
+, m_exclString(excl)
+, m_depth(depth)
+, m_project(onlyProject)
+, m_tryAbort(false)
 {
     setTerminationEnabled(false);
 }
@@ -103,10 +146,10 @@ void GrepFindFilesThread::run()
     foreach(KUrl directory, m_startDirs)
     {
         if(m_project)
-            m_files += thread_getProjectFiles(directory, m_recursive, include, exclude, m_tryAbort);
+            m_files += thread_getProjectFiles(directory, m_depth, include, exclude, m_tryAbort);
         else
         {
-            m_files += thread_findFiles(directory.toLocalFile(), m_recursive, include, exclude, m_tryAbort);
+            m_files += thread_findFiles(directory.toLocalFile(), m_depth, include, exclude, m_tryAbort);
         }
     }
 

plugins/grepview/grepdialog.cpp
@@ -157,7 +157,7 @@ GrepDialog::GrepDialog( GrepViewPlugin * plugin, QWidget *parent, bool setLastUs
     syncButton->setIcon(KIcon("dirsync"));
     syncButton->setMenu(createSyncButtonMenu());
 
-    recursiveCheck->setChecked(cg.readEntry("recursive", true));
+    depthSpin->setValue(cg.readEntry("depth", -1));
     limitToProjectCheck->setChecked(cg.readEntry("search_project_files", true));
 
     filesCombo->addItems(cg.readEntry("file_patterns", filepatterns));
@@ -267,7 +267,7 @@ GrepDialog::~GrepDialog()
     // memorize the last patterns and paths
     cg.writeEntry("LastSearchItems", qCombo2StringList(patternCombo));
     cg.writeEntry("regexp", regexCheck->isChecked());
-    cg.writeEntry("recursive", recursiveCheck->isChecked());
+    cg.writeEntry("depth", depthSpin->value());
     cg.writeEntry("search_project_files", limitToProjectCheck->isChecked());
     cg.writeEntry("case_sens", caseSensitiveCheck->isChecked());
     cg.writeEntry("exclude_patterns", qCombo2StringList(excludeCombo));
@@ -341,9 +341,9 @@ bool GrepDialog::regexpFlag() const
     return regexCheck->isChecked();
 }
 
-bool GrepDialog::recursiveFlag() const
+int GrepDialog::depthValue() const
 {
-    return recursiveCheck->isChecked();
+    return depthSpin->value();
 }
 
 bool GrepDialog::caseSensitiveFlag() const
@@ -459,7 +459,7 @@ void GrepDialog::performAction(KDialog::ButtonCode button)
 
     job->setProjectFilesFlag( useProjectFilesFlag() );
     job->setRegexpFlag( regexpFlag() );
-    job->setRecursive( recursiveFlag() );
+    job->setDepth( depthValue() );
     job->setCaseSensitive( caseSensitiveFlag() );
 
     ICore::self()->runController()->registerJob(job);

plugins/grepview/grepdialog.h
@@ -45,8 +45,9 @@ public:
 
     bool useProjectFilesFlag() const;
     bool regexpFlag() const;
-    bool recursiveFlag() const;
     bool caseSensitiveFlag() const;
+
+    int depthValue() const;
 
     void start();
     

plugins/grepview/grepfindthread.h
@@ -12,12 +12,13 @@ public:
      * @brief Constructor
      * @param[in] parent Parent
      * @param[in] startDirs Root directories or files of the search
-     * @param[in] recursive Whether the search should be recursive
+     * @param[in] depth Depth for the search. Possible values are -1 (recursive), 0 (no recursion), or integers
+     *                  from 1 on indicating the level of subfolders allowed in the recursion.
      * @param[in] patterns Space-separated list of wildcard patterns to search for
      * @param[in] exclusions Space-separated list of wildcard patterns to exclude. Matches the whole path.
      * @param[in] onlyProject Whether the search should only consider project files.
      */
-    GrepFindFilesThread(QObject *parent, const QList<KUrl> &startDirs, bool recursive,
+    GrepFindFilesThread(QObject *parent, const QList<KUrl> &startDirs, int depth,
                     const QString &patterns, const QString &exclusions,
                     bool onlyProject);
     /**
@@ -53,7 +54,7 @@ private:
     QList<KUrl> m_startDirs;
     QString m_patString;
     QString m_exclString;
-    bool m_recursive;
+    int m_depth;
     bool m_project;
     KUrl::List m_files;
     volatile bool m_tryAbort;

plugins/grepview/grepjob.cpp
@@ -178,7 +178,7 @@ void GrepJob::slotWork()
             QMetaObject::invokeMethod(this, "slotWork", Qt::QueuedConnection);
             break;
         case WorkCollectFiles:
-            m_findThread = new GrepFindFilesThread(this, m_directoryChoice, m_recursiveFlag, m_filesString, m_excludeString, m_useProjectFilesFlag);
+            m_findThread = new GrepFindFilesThread(this, m_directoryChoice, m_depthValue, m_filesString, m_excludeString, m_useProjectFilesFlag);
             emit showMessage(this, i18n("Collecting files..."));
             connect(m_findThread, SIGNAL(finished()), this, SLOT(slotFindFinished()));
             m_findThread->start();
@@ -301,9 +301,9 @@ void GrepJob::setCaseSensitive(bool caseSensitive)
     m_caseSensitiveFlag = caseSensitive;
 }
 
-void GrepJob::setRecursive(bool recursive)
+void GrepJob::setDepth(int depth)
 {
-    m_recursiveFlag = recursive;
+    m_depthValue = depth;
 }
 
 void GrepJob::setRegexpFlag(bool regexpFlag)

plugins/grepview/grepjob.h
@@ -58,7 +58,7 @@ public:
     void setFilesString(const QString &filesString);
     void setExcludeString(const QString &excludeString);
     void setDirectoryChoice(const QList<KUrl> &choice);
-    void setRecursive(bool recursive);
+    void setDepth(int depth);
     void setRegexpFlag(bool regexpFlag);
     void setCaseSensitive(bool caseSensitive);
     void setProjectFilesFlag(bool projectFilesFlag);
@@ -111,9 +111,9 @@ private:
 
     bool m_useProjectFilesFlag;
     bool m_regexpFlag;
-    bool m_recursiveFlag;
     bool m_caseSensitiveFlag;
-    
+    int m_depthValue;
+
     bool m_findSomething;
 };
 

plugins/grepview/grepwidget.ui
@@ -212,25 +212,31 @@ p, li { white-space: pre-wrap; }
     </widget>
    </item>
    <item row="5" column="0">
-    <widget class="QLabel" name="label_6">
+    <widget class="QLabel" name="depthLabel">
      <property name="text">
-      <string>Recursive:</string>
+      <string>Depth:</string>
      </property>
      <property name="alignment">
       <set>Qt::AlignRight|Qt::AlignTrailing|Qt::AlignVCenter</set>
      </property>
      <property name="buddy">
-      <cstring>recursiveCheck</cstring>
+      <cstring>depthSpin</cstring>
      </property>
     </widget>
    </item>
    <item row="5" column="1">
-    <widget class="QCheckBox" name="recursiveCheck">
-     <property name="text">
-      <string/>
+    <widget class="QSpinBox" name="depthSpin">
+     <property name="toolTip">
+       <string extracomment="Maximum level of recursion. For folder-based searches, that means the level of subfolders. Choose 0 to look only in files inside the current folder, or Full to search also on the subfoldersâ€™s files, theirs subfoldersâ€™ files, and so on."/>
      </property>
-     <property name="checked">
-      <bool>true</bool>
+     <property name="specialValueText">
+       <string extracomment="Full recursion will be used. For folder-based searches, that means searching in the target folder and all the subfolders, their subfolders, and so on.">Full</string>
+     </property>
+     <property name="minimum">
+       <number>-1</number>
+     </property>
+     <property name="value">
+       <number>-1</number>
      </property>
     </widget>
    </item>

plugins/grepview/tests/findreplacetest.cpp
@@ -163,7 +163,7 @@ void FindReplaceTest::testReplace()
     job->setFilesString("*");
     job->setExcludeString("");
     job->setDirectoryChoice(QList<KUrl>() << KUrl(dir.path()));
-    job->setRecursive(true);
+    job->setDepth(-1); // fully recursive
     job->setRegexpFlag(true);
     job->setCaseSensitive(true);
     job->setProjectFilesFlag(false);

commit 8d8aada8d9e711d4632fd094b99a85e3997a0f0c

GrepFindFilesThread: fix limited-depth file search
The depth is decremented repeatedly within the loop that iterates over
subdirectories of the current directory. For example, if the current
directory has 5 subdirectories, the depth would decrease by 5 instead of
by 1. Decrement the depth once before the loop instead. The initial
implementation of the Depth option in
c0d4221d344b47f29a7862c4bc086e0a7a8bae7c introduced this mistake.

plugins/grepview/grepfindthread.cpp
@@ -141,6 +141,10 @@ void FileFinder::findFiles(const QDir& dir, int depth, QList<QUrl>& results)
     }
     if(depth != 0)
     {
+        if (depth > 0) {
+            --depth;
+        }
+
         constexpr QDir::Filters dirFilter = QDir::NoDotAndDotDot|QDir::AllDirs|QDir::Readable|QDir::NoSymLinks|QDir::Hidden;
         const auto dirs = dir.entryInfoList(QStringList(), dirFilter);
         for (const QFileInfo& currDir : dirs) {
@@ -150,10 +154,6 @@ void FileFinder::findFiles(const QDir& dir, int depth, QList<QUrl>& results)
             QString canonical = currDir.canonicalFilePath();
             if (!canonical.startsWith(dir.canonicalPath()))
                 continue;
-
-            if ( depth > 0 ) {
-                depth--;
-            }
 
             findFiles(canonical, depth, results);
         }