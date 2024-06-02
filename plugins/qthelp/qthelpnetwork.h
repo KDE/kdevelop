d64e7ba2864a8a0a460de1ce02d07568731e2fbf
Milian Wolff <mail@milianw.de>
2024-06-02 21:26:41 +0200

    Format qthelpnetwork.h with clang-format

--- plugins/qthelp/qthelpnetwork.h
+++ plugins/qthelp/qthelpnetwork.h
@@ -17,34 +17,39 @@
 
 class HelpNetworkReply : public QNetworkReply
 {
-		Q_OBJECT
-	public:
-		HelpNetworkReply(const QNetworkRequest &request, const QByteArray &fileData, const QString &mimeType);
-		
-		void abort() override {}
-		qint64 bytesAvailable() const override { return data.length() + QNetworkReply::bytesAvailable(); }
-		
-	protected:
-		qint64 readData(char *data, qint64 maxlen) override;
-		
-	private:
-		QByteArray data;
-		qint64 origLen;
+    Q_OBJECT
+public:
+    HelpNetworkReply(const QNetworkRequest& request, const QByteArray& fileData, const QString& mimeType);
+
+    void abort() override
+    {
+    }
+    qint64 bytesAvailable() const override
+    {
+        return data.length() + QNetworkReply::bytesAvailable();
+    }
+
+protected:
+    qint64 readData(char* data, qint64 maxlen) override;
+
+private:
+    QByteArray data;
+    qint64 origLen;
 };
 
 class HelpNetworkAccessManager : public QNetworkAccessManager
 {
-		Q_OBJECT
-	public:
-		explicit HelpNetworkAccessManager(QHelpEngineCore *engine, QObject *parent = nullptr);
-        ~HelpNetworkAccessManager() override;
+    Q_OBJECT
+public:
+    explicit HelpNetworkAccessManager(QHelpEngineCore* engine, QObject* parent = nullptr);
+    ~HelpNetworkAccessManager() override;
 
-	protected:
-		QNetworkReply *createRequest(Operation op,
-			const QNetworkRequest &request, QIODevice *outgoingData = nullptr) override;
+protected:
+    QNetworkReply* createRequest(Operation op, const QNetworkRequest& request,
+                                 QIODevice* outgoingData = nullptr) override;
 
-	private:
-		QHelpEngineCore *m_helpEngine;
+private:
+    QHelpEngineCore* m_helpEngine;
 };
 
 #endif
