950e431c06886b1d036ddfee6ab0f95941570f36
Milian Wolff <mail@milianw.de>
2024-06-15 17:56:33 +0200

    Qt6: Fix compiler warning when string-formatting qsizetype
    Fixes:
    ```
    bench_quickopen.cpp:289:21: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘qsizetype’ {aka ‘long long int’} [-Wformat=]
      289 |     QTest::addRow("%d projects", fileCounts.size()) << fileCounts << false;
          |                    ~^            ~~~~~~~~~~~~~~~~~
          |                     |                           |
          |                     int                         qsizetype {aka long long int}
          |                    %lld
    bench_quickopen.cpp:290:21: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘qsizetype’ {aka ‘long long int’} [-Wformat=]
      290 |     QTest::addRow("%d projects and one open file", fileCounts.size()) << fileCounts << true;
          |                    ~^                              ~~~~~~~~~~~~~~~~~
          |                     |                                             |
          |                     int                                           qsizetype {aka long long int}
          |                    %lld
    ```

--- plugins/quickopen/tests/bench_quickopen.cpp
+++ plugins/quickopen/tests/bench_quickopen.cpp
@@ -286,8 +286,8 @@ void BenchQuickOpen::benchProjectFileFilter_files_data()
     }
 
     const FileCounts fileCounts{100, 5000, 800, 244, 5432, 0, 5, 2222};
-    QTest::addRow("%d projects", fileCounts.size()) << fileCounts << false;
-    QTest::addRow("%d projects and one open file", fileCounts.size()) << fileCounts << true;
+    QTest::addRow("%lld projects", fileCounts.size()) << fileCounts << false;
+    QTest::addRow("%lld projects and one open file", fileCounts.size()) << fileCounts << true;
 }
 
 void BenchQuickOpen::benchProjectFileFilter_fileRemovedFromSet_data()
