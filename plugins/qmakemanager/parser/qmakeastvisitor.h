a3a0858a2b2aa7253624bfc69c97fb33c89455b8
Milian Wolff <mail@milianw.de>
2024-06-15 23:13:56 +0200

    Refactor QMake::ASTVisitor::visitNode
    Use a switch instead of a jump table, the end result will probably
    be the same but this code here is much more expressive and does not
    suffer from compiler warnings like this:
    
    ```
    qmakeastvisitor.cpp: In member function ‘virtual void QMake::ASTVisitor::visitNode(QMake::AST*)’:
    qmakeastvisitor.cpp:29:43: warning: ‘<anonymous>’ may be used uninitialized [-Wmaybe-uninitialized]
       29 |         (this->*_S_parser_table[node->type])(node);
          |                 ~~~~~~~~~~~~~~~~~~~~~~~~~~^
    qmakeastvisitor.cpp:29:43: note: ‘<anonymous>’ was declared here
       29 |         (this->*_S_parser_table[node->type])(node);
          |                 ~~~~~~~~~~~~~~~~~~~~~~~~~~^
    ```
    
    Note how we reinterpret_cast'ed the member function pointers
    taking different argument types, pretty wild if you ask me...

--- plugins/qmakemanager/parser/qmakeastvisitor.h
+++ plugins/qmakemanager/parser/qmakeastvisitor.h
@@ -23,18 +23,16 @@ class ValueAST;
 
 class KDEVQMAKEPARSER_EXPORT ASTVisitor
 {
-    using parser_fun_t = void (ASTVisitor::*)(AST*);
-    static parser_fun_t _S_parser_table[];
-    public:
-        virtual ~ASTVisitor();
-        virtual void visitNode( AST* node );
-        virtual void visitProject( ProjectAST* node );
-        virtual void visitAssignment( AssignmentAST* node );
-        virtual void visitValue( ValueAST* node );
-        virtual void visitFunctionCall( FunctionCallAST* node );
-        virtual void visitScopeBody( ScopeBodyAST* node );
-        virtual void visitOr( OrAST* node );
-        virtual void visitSimpleScope( SimpleScopeAST* node );
+public:
+    virtual ~ASTVisitor();
+    virtual void visitNode(AST* node);
+    virtual void visitProject(ProjectAST* node);
+    virtual void visitAssignment(AssignmentAST* node);
+    virtual void visitValue(ValueAST* node);
+    virtual void visitFunctionCall(FunctionCallAST* node);
+    virtual void visitScopeBody(ScopeBodyAST* node);
+    virtual void visitOr(OrAST* node);
+    virtual void visitSimpleScope(SimpleScopeAST* node);
 };
 
 }
