ee38ca57c3131e0ccd82174db2754816b0644800
Milian Wolff <mail@milianw.de>
2024-06-22 21:29:50 +0200

    Fix PersistentSymbolTable::visitFilteredDeclarations for Qt6
    Do not rely on QHash iterator stability which is not guaranteed
    with Qt6 anymore. Instead, use a shared_ptr indirection for the
    inner hash in the `declarationsCache`. That gives us a stable
    location we can reference during iteration. We don't have to copy
    anything either, ensuring we don't blow up the stack size
    accidentally, the QVarlengthArray we have here is 1KB after all.
    
    Note that a unique_ptr would be fine too in theory, but sadly QHash
    does not support move-only value types... We don't want to use
    std::unordered_map either, as that's known to be much slower than
    QHash. So let's just use shared_ptr as it's easy to use and ensures
    we don't leak anything accidentally.

--- kdevplatform/language/duchain/persistentsymboltable.cpp
+++ kdevplatform/language/duchain/persistentsymboltable.cpp
@@ -6,6 +6,8 @@
 
 #include "persistentsymboltable.h"
 
+#include <memory>
+
 #include <QHash>
 
 #include "declaration.h"
@@ -180,7 +182,8 @@ public:
 };
 
 using CachedDeclarations = KDevVarLengthArray<IndexedDeclaration>;
-using CachedDeclarationsByImports = QHash<TopDUContext::IndexedRecursiveImports, CachedDeclarations>;
+/// NOTE: shared_ptr indirection to get stable location to allow safe recursion
+using CachedDeclarationsByImports = QHash<TopDUContext::IndexedRecursiveImports, std::shared_ptr<CachedDeclarations>>;
 using Declarations = ConstantConvenientEmbeddedSet<IndexedDeclaration, IndexedDeclarationHandler>;
 using CachedIndexedRecursiveImports =
     Utils::StorableSet<IndexedTopDUContext, IndexedTopDUContextIndexConversion, RecursiveImportCacheRepository, true>;
@@ -416,24 +419,7 @@ void PersistentSymbolTable::visitFilteredDeclarations(const IndexedQualifiedIden
         const auto declarations = Declarations(repositoryItem->declarations(), repositoryItem->declarationsSize(),
                                                repositoryItem->centralFreeItem);
 
-        // TODO Qt6: revisit when porting as this code - when called recursively - relies on QHash reference stability:
-        // 1. `cachedImports` is a copy of a value inside importsCache, so it doesn't rely on QHash reference stability.
-        // 2. `cached` is a reference to a value inside declarationsCache, but it is not used after the visitor is
-        //    called, so it does not rely on QHash reference stability either.
-        // 3. `cache` is a reference to a value inside a value inside declarationsCache. `cache` is filled before
-        //    `filterIterator` is constructed, so it is recursion-safe. `cache` is not used after the visitor is called,
-        //    so it also does not rely on QHash reference stability.
-        // 4. `filterIterator` contains a copy of the constData() pointer of a KDevVarLengthArray value inside a QHash.
-        //    If QVarLengthArray::size() is less or equal to its Prealloc, constData() points to an array within the
-        //    QVarLengthArray object. A next recursive call to visitFilteredDeclarations() may insert an id into
-        //    declarationsCache or a visibility into a value of declarationsCache. Such an insertion can move the
-        //    QVarLengthArray object in memory and make the copy of constData() pointer in `filterIterator` dangling.
-        //    A possible fix is to store raw pointers KDevVarLengthArray* as values of values of declarationsCache;
-        //    delete the pointers in ~PersistentSymbolTableRepo() and before removing values or values of values of
-        //    declarationsCache. An alternative fix is to create a local copy of the KDevVarLengthArray and pass the
-        //    copy's constData() to filterIterator. Better yet, make CachedDeclarations = QVector<IndexedDeclaration>
-        //    instead of KDevVarLengthArray<IndexedDeclaration>: the QVector::constData() pointer does not change until
-        //    the QVector is modified, which it isn't in this case.
+        // NOTE: cheap copy here to ensure we don't rely on stable iterators which cannot be guaranteed due to possible recursion
         const auto cachedImports = [&]() {
             auto it = repo.importsCache.constFind(visibility);
             if (it == repo.importsCache.constEnd()) {
@@ -452,11 +438,10 @@ void PersistentSymbolTable::visitFilteredDeclarations(const IndexedQualifiedIden
             auto& cached = repo.declarationsCache[id];
             auto cacheIt = cached.constFind(visibility);
             if (cacheIt == cached.constEnd()) {
-                auto insertIt = cached.insert(visibility, {});
+                auto cache = std::make_shared<CachedDeclarations>();
                 {
-                    auto& cache = *insertIt;
                     auto cacheVisitor = [&cache](const IndexedDeclaration& decl) {
-                        cache.append(decl);
+                        cache->append(decl);
                     };
 
                     using FilteredDeclarationCacheVisitor =
@@ -467,10 +452,12 @@ void PersistentSymbolTable::visitFilteredDeclarations(const IndexedQualifiedIden
                     // The visitor visits all the declarations from within its constructor
                     FilteredDeclarationCacheVisitor visitor(cacheVisitor, declarations.iterator(), cachedImports);
                 }
-                cacheIt = insertIt;
+                cacheIt = cached.insert(visibility, std::move(cache));
             }
 
-            return FilteredDeclarationIterator(Declarations::Iterator(cacheIt->constData(), cacheIt->size(), -1),
+            // NOTE: safe reference even during recursion due to shared_ptr indirection
+            const auto& cache = *cacheIt->get();
+            return FilteredDeclarationIterator(Declarations::Iterator(cache.constData(), cache.size(), -1),
                                                cachedImports);
         }();
 
