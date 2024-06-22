fe3974b0ae1c5f6c093e6ab164ef71453c83038a
Milian Wolff <mail@milianw.de>
2024-06-22 21:29:50 +0200

    Fix PersistentSymbolTable::visitFilteredDeclarations for Qt6
    Do not rely on QHash iterator stability which is not guaranteed
    with Qt6 anymore. Instead, replace the QVarLengthArray with a
    QVector to ensure we get a stable container we can iterate over.
    We don't have to copy anything either thanks to Qt container COW.
    
    To ensure the container is kept alive, introduce a `visit` lambda
    and pass that the iterator while holding the `cachedDeclarations`
    alive.
    
    This change was suggested by Igor Kushnir, thanks!

--- kdevplatform/language/duchain/persistentsymboltable.cpp
+++ kdevplatform/language/duchain/persistentsymboltable.cpp
@@ -7,6 +7,7 @@
 #include "persistentsymboltable.h"
 
 #include <QHash>
+#include <QVector>
 
 #include "declaration.h"
 #include "declarationid.h"
@@ -181,7 +182,8 @@ public:
     const PersistentSymbolTableItem& m_item;
 };
 
-using CachedDeclarations = KDevVarLengthArray<IndexedDeclaration>;
+/// NOTE: QVector rather than KDevVarLengthArray to get stable reference to data during iteration
+using CachedDeclarations = QVector<IndexedDeclaration>;
 using CachedDeclarationsByImports = QHash<TopDUContext::IndexedRecursiveImports, CachedDeclarations>;
 using Declarations = ConstantConvenientEmbeddedSet<IndexedDeclaration, IndexedDeclarationHandler>;
 using CachedIndexedRecursiveImports =
@@ -423,28 +425,20 @@ void PersistentSymbolTable::visitFilteredDeclarations(const IndexedQualifiedIden
             return;
         }
 
+        auto visit = [&visitor](FilteredDeclarationIterator&& filterIterator) {
+            for (; filterIterator; ++filterIterator) {
+                if (visitor(*filterIterator) == VisitorState::Break) {
+                    break;
+                }
+            }
+        };
+
         const PersistentSymbolTableItem* repositoryItem = repo.itemFromIndex(index);
         const auto declarations = Declarations(repositoryItem->declarations(), repositoryItem->declarationsSize(),
                                                repositoryItem->centralFreeItem);
 
-        // TODO Qt6: revisit when porting as this code - when called recursively - relies on QHash reference stability:
-        // 1. `cachedImports` is a copy of a value inside importsCache, so it doesn't rely on QHash reference stability.
-        // 2. `cached` is a reference to a value inside declarationsCache, but it is not used after the visitor is
-        //    called, so it does not rely on QHash reference stability either.
-        // 3. `cache` is a reference to a value inside a value inside declarationsCache. `cache` is filled before
-        //    `filterIterator` is constructed, so it is recursion-safe. `cache` is not used after the visitor is called,
-        //    so it also does not rely on QHash reference stability.
-        // 4. `filterIterator` contains a copy of the constData() pointer of a KDevVarLengthArray value inside a QHash.
-        //    If QVarLengthArray::size() is less or equal to its Prealloc, constData() points to an array within the
-        //    QVarLengthArray object. A next recursive call to visitFilteredDeclarations() may insert an id into
-        //    declarationsCache or a visibility into a value of declarationsCache. Such an insertion can move the
-        //    QVarLengthArray object in memory and make the copy of constData() pointer in `filterIterator` dangling.
-        //    A possible fix is to store raw pointers KDevVarLengthArray* as values of values of declarationsCache;
-        //    delete the pointers in ~PersistentSymbolTableRepo() and before removing values or values of values of
-        //    declarationsCache. An alternative fix is to create a local copy of the KDevVarLengthArray and pass the
-        //    copy's constData() to filterIterator. Better yet, make CachedDeclarations = QVector<IndexedDeclaration>
-        //    instead of KDevVarLengthArray<IndexedDeclaration>: the QVector::constData() pointer does not change until
-        //    the QVector is modified, which it isn't in this case.
+        // NOTE: cheap copy here to ensure we don't rely on stable iterators
+        //       which cannot be guaranteed due to possible recursion
         const auto cachedImports = [&]() {
             auto it = repo.importsCache.constFind(visibility);
             if (it != repo.importsCache.constEnd()) {
@@ -456,49 +450,46 @@ void PersistentSymbolTable::visitFilteredDeclarations(const IndexedQualifiedIden
             return cachedImports;
         }();
 
-        auto filterIterator = [&]() {
-            if (declarations.dataSize() <= MinimumCountForCache) {
-                return FilteredDeclarationIterator(declarations.iterator(), cachedImports);
+        if (declarations.dataSize() <= MinimumCountForCache) {
+            // no visibility caching needed
+            visit(FilteredDeclarationIterator(declarations.iterator(), cachedImports));
+            return;
+        }
+
+        // Do visibility caching
+
+        // NOTE: cheap COW copy, gives us safe reference of data even during recursion
+        const auto cachedDeclarations = [&]() {
+            auto& cached = repo.declarationsCache[id];
+            auto cacheIt = cached.constFind(visibility);
+            if (cacheIt != cached.constEnd()) {
+                return *cacheIt;
             }
 
-            // Do visibility caching
-            const auto& cachedDeclarations = [&]() -> const CachedDeclarations& {
-                auto& cached = repo.declarationsCache[id];
-                auto cacheIt = cached.constFind(visibility);
-                if (cacheIt != cached.constEnd()) {
-                    return *cacheIt;
-                }
+            auto cache = CachedDeclarations();
+            {
+                auto cacheVisitor = [&cache](const IndexedDeclaration& decl) {
+                    cache.append(decl);
+                };
 
-                auto cache = CachedDeclarations();
-                {
-                    auto cacheVisitor = [&cache](const IndexedDeclaration& decl) {
-                        cache.append(decl);
-                    };
+                using FilteredDeclarationCacheVisitor =
+                    ConvenientEmbeddedSetTreeFilterVisitor<IndexedDeclaration, IndexedDeclarationHandler,
+                                                           IndexedTopDUContext, CachedIndexedRecursiveImports,
+                                                           DeclarationTopContextExtractor, decltype(cacheVisitor)>;
 
-                    using FilteredDeclarationCacheVisitor =
-                        ConvenientEmbeddedSetTreeFilterVisitor<IndexedDeclaration, IndexedDeclarationHandler,
-                                                               IndexedTopDUContext, CachedIndexedRecursiveImports,
-                                                               DeclarationTopContextExtractor, decltype(cacheVisitor)>;
+                // The visitor visits all the declarations from within its constructor
+                FilteredDeclarationCacheVisitor visitor(cacheVisitor, declarations.iterator(), cachedImports);
+            }
 
-                    // The visitor visits all the declarations from within its constructor
-                    FilteredDeclarationCacheVisitor visitor(cacheVisitor, declarations.iterator(), cachedImports);
-                }
-
-                return *cached.insert(visibility, cache);
-            }();
-
-            Q_ASSERT(verifyNoDummies(cachedDeclarations));
-
-            return FilteredDeclarationIterator(
-                Declarations::Iterator(cachedDeclarations.constData(), cachedDeclarations.size(), -1), cachedImports,
-                true);
+            cached.insert(visibility, cache);
+            return cache;
         }();
 
-        for (; filterIterator; ++filterIterator) {
-            if (visitor(*filterIterator) == VisitorState::Break) {
-                break;
-            }
-        }
+        Q_ASSERT(verifyNoDummies(cachedDeclarations));
+
+        visit(FilteredDeclarationIterator(
+            Declarations::Iterator(cachedDeclarations.constData(), cachedDeclarations.size(), -1), cachedImports,
+            true));
     });
 }
 
