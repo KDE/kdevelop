52d54346c8112a43f59488f41ba184d7a186e1ae
Friedrich W. H. Kossebau <kossebau@kde.org>
2024-07-23 11:29:15 +0200

    App templates: add "CMake Qt6 Quick Application" template

---
+++ app_templates/cpp/CMake/cmake_qt6quick_app/src/main.cpp
@@ -0,0 +1,17 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                     &app, []() { QCoreApplication::exit(-1); },
+                     Qt::QueuedConnection);
+
+    engine.loadFromModule("%{APPNAMEID}", "Main");
+
+    return app.exec();
+}
