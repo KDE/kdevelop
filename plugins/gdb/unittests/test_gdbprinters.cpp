476a226bb0c647efaa954ffdbe3b2a28933f0a78
Milian Wolff <mail@milianw.de>
2024-08-13 10:00:54 +0200

    Fix QtPrintersTest::testQListContainer for std::pair on libc++/FreeBSD
    I.e. allow this test to pass when we encounter the versioned
    std::_1::pair type instead of the unversioned std::pair type.
    
    This commit eliminates regular failures like the following on the
    FreeBSD CI server:
    QDEBUG : "print pairList"  =  "$13 = QList<std::__1::pair<int, int>> (size = 0)"
    FAIL!  : QtPrintersTest::testQListContainer(QList) 'out.contains(QString("%1<std::pair<int, int>> (size = 0)").arg(container).toLocal8Bit())' returned FALSE. ()
       Loc: [kdevelop/plugins/gdb/unittests/test_gdbprinters.cpp(310)]

--- plugins/gdb/unittests/test_gdbprinters.cpp
+++ plugins/gdb/unittests/test_gdbprinters.cpp
@@ -307,10 +307,17 @@ void QtPrintersTest::testQListContainer()
     { // <QPair<int, int> >
         runToLine(51);
         QByteArray out = gdb.execute("print pairList");
-        QVERIFY(out.contains(QString("%1<std::pair<int, int>> (size = 0)").arg(container).toLocal8Bit()));
+
+        // For libc++ we get std::__1::pair instead of the shortened std::pair.
+        // Allow the test to pass with either form.
+        const auto versionedPair = "std::__1::pair";
+        const auto unversionedPair = "std::pair";
+        const auto printedPairTypeName = out.contains(versionedPair) ? versionedPair : unversionedPair;
+
+        QVERIFY(out.contains(QString("%1<%2<int, int>> (size = 0)").arg(container, printedPairTypeName).toLocal8Bit()));
         gdb.execute("next");
         out = gdb.execute("print pairList");
-        QVERIFY(out.contains(QString("%1<std::pair<int, int>> (size = 2)").arg(container).toLocal8Bit()));
+        QVERIFY(out.contains(QString("%1<%2<int, int>> (size = 2)").arg(container, printedPairTypeName).toLocal8Bit()));
         if (container != QLatin1String("QSet")) {
             QVERIFY(out.contains("[0] = {\n    first = 1,\n    second = 2\n  }"));
             QVERIFY(out.contains("[1] = {\n    first = 2,\n    second = 3\n  }"));
@@ -321,7 +328,7 @@ void QtPrintersTest::testQListContainer()
         QVERIFY(!out.contains("[2] = "));
         gdb.execute("next");
         out = gdb.execute("print pairList");
-        QVERIFY(out.contains(QString("%1<std::pair<int, int>> (size = 3)").arg(container).toLocal8Bit()));
+        QVERIFY(out.contains(QString("%1<%2<int, int>> (size = 3)").arg(container, printedPairTypeName).toLocal8Bit()));
         if (container != QLatin1String("QSet")) {
             QVERIFY(out.contains("[0] = {\n    first = 1,\n    second = 2\n  }"));
             QVERIFY(out.contains("[1] = {\n    first = 2,\n    second = 3\n  }"));
