3d41dcb050c15efd9a9f20551c04f9bfc44bad72
Igor Kushnir <igorkuo@gmail.com>
2024-09-29 21:50:11 +0300

    util: test Algorithm::unite()

---
+++ kdevplatform/util/tests/test_algorithm.h
@@ -0,0 +1,28 @@
+/*
+ *  SPDX-FileCopyrightText: 2024 Igor Kushnir <igorkuo@gmail.com>
+ *
+ *  SPDX-License-Identifier: LGPL-2.0-or-later
+ */
+
+#ifndef KDEVPLATFORM_TEST_ALGORITHM_H
+#define KDEVPLATFORM_TEST_ALGORITHM_H
+
+#include <QObject>
+
+/**
+ * Tests namespace Algorithm
+ */
+class TestAlgorithm : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TestAlgorithm(QObject* parent = nullptr);
+
+private Q_SLOTS:
+    void initTestCase();
+
+    void testUnite2String();
+    void testUnite5Int();
+};
+
+#endif // KDEVPLATFORM_TEST_ALGORITHM_H
