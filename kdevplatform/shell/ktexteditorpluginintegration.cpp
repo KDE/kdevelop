9bbd23ecac80ea2081e0edd167037ed8b58d48d2
Igor Kushnir <igorkuo@gmail.com>
2024-10-02 19:00:00 +0300

    KTextEditorIntegration: set config page's plugin and parent
    When the user disables a plugin on the Plugins page of the Configure
    KDevelop dialog, ConfigDialog::removePagesForPlugin() removes the
    plugin's config pages by checking each page's
    KDevelop::ConfigPage::plugin().
    KTextEditorIntegration::Plugin::configPage() does not set the created
    page's plugin. Therefore, disabling a KTextEditor plugin, then modifying
    the plugin's settings and applying changes segfaults KDevelop.
    
    Pass `this` from KTextEditorIntegration::Plugin::configPage() to
    KTextEditorConfigPageAdapter() and forward the IPlugin* pointer to the
    base class constructor KDevelop::ConfigPage(). Now a KTextEditor's
    plugin page is automatically removed when the plugin is disabled.
    
    Pass the `parent` argument to KTextEditorConfigPageAdapter() to prevent
    a leak. Remove the nullptr default value of the QWidget* parent
    parameter of KTextEditorConfigPageAdapter() in order to preclude future
    leaks like this.
    
    Check and forward a possible nullptr return value of
    KTextEditor::Plugin::configPage(). KTextEditorConfigPageAdapter()
    unconditionally dereferences its KTextEditor::ConfigPage* argument, so
    this prevents a potential crash. The same check is present in the other
    function that creates a KTextEditorConfigPageAdapter -
    KDevelop::EditorConfigPage::childPage().

--- kdevplatform/shell/ktexteditorpluginintegration.cpp
+++ kdevplatform/shell/ktexteditorpluginintegration.cpp
@@ -698,7 +698,10 @@ KXMLGUIClient *Plugin::createGUIForMainWindow(Sublime::MainWindow* window)
 
 KDevelop::ConfigPage* Plugin::configPage(int number, QWidget* parent)
 {
-    return new KTextEditorConfigPageAdapter(m_plugin->configPage(number, parent));
+    if (auto* const page = m_plugin->configPage(number, parent)) {
+        return new KTextEditorConfigPageAdapter(page, this, parent);
+    }
+    return nullptr;
 }
 
 int Plugin::configPages() const
