6d2c1ffedaf444774bb7d3e5cd047a65bb2d510c
Igor Kushnir <igorkuo@gmail.com>
2024-12-12 00:08:51 +0200

    grepview: do replacements with fewer check state changes
    GrepOutputModel::doReplacements() unchecks and disables each checked
    text (match) item. Each of these two item changes invokes
    updateCheckState() on it, and thus refreshes every ancestor's state
    twice. Inhibit updateCheckState() while doing replacements and manually
    refresh each ancestor of modified items once to optimize.
    
    Assert that the root item is not unchecked, because the Replace button
    must be disabled and therefore GrepOutputModel::doReplacements() not be
    invoked then. Document doReplacements() accordingly.
    
    Do not fruitlessly iterate over an unchecked file item's children.

--- plugins/grepview/grepoutputmodel.h
+++ plugins/grepview/grepoutputmodel.h
@@ -52,10 +52,14 @@ public:
     /// Recursively apply check state to children
     void propagateState() ;
     /**
+     * Check the children to compute the current enabled and check state of this item.
+     */
+    void refreshState();
+    /**
      * Check the parent item's children to compute its current enabled and check state,
      * then perform the same computations for its ancestor items' states recursively.
      */
-    void refreshParentState();
+    void refreshAncestorStates();
 
     QVariant data ( int role = Qt::UserRole + 1 ) const override;
 
@@ -64,11 +68,6 @@ private:
      * Recursively apply a given check state to children.
      */
     void propagateCheckState(Qt::CheckState checkState);
-    /**
-     * Check the children to compute the current enabled and check state of this item,
-     * then of ancestor items recursively.
-     */
-    void refreshState();
 
     KDevelop::DocumentChangePointer m_change;
 };
@@ -102,6 +101,12 @@ public:
     
 public Q_SLOTS:
     void activate( const QModelIndex &idx );
+
+    /**
+     * Apply replacements to lines represented by checked text items.
+     *
+     * @pre at least one item is checked
+     */
     void doReplacements();
     void setReplacement(const QString &repl);
 
