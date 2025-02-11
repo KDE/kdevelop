91a0339c235245c2fdd42b1871f6c90bf9c1dd58
Friedrich W. H. Kossebau <kossebau@kde.org>
2025-02-11 22:15:36 +0100

    cmake_qt6widgetsapp app template: fix leakage of main window

--- app_templates/cpp/CMake/cmake_qt6widgetsapp/src/main.cpp
+++ app_templates/cpp/CMake/cmake_qt6widgetsapp/src/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    auto w = new %{APPNAMEID};
-    w->show();
+    %{APPNAMEID} w;
+    w.show();
 
     return app.exec();
 }
