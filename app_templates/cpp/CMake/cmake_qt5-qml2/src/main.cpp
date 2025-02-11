478e06dfe2192c5a6b6ff72a52491ad0d34f1fec
Friedrich W. H. Kossebau <kossebau@kde.org>
2025-02-11 21:17:49 +0100

    Drop Qt5- & CMake-based app templates
    Qt5 being EOL May 2025.

--- app_templates/cpp/CMake/cmake_qt5-qml2/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine(QUrl(QStringLiteral("qrc:/qml/main.qml")));
-    return app.exec();
-}
