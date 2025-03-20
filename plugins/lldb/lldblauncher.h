260fd34ca81548ad473c735ec1db9ef4d09f523b
Igor Kushnir <igorkuo@gmail.com>
2025-03-20 10:40:41 +0200

    Extract and improve class KDevMI::MIDebugLauncher
    The implementations of GdbLauncher and LldbLauncher are almost
    identical. Extract a common base class.
    
    GdbLauncher::start() passes nullptr and LldbLauncher::start() -
    qApp->activeWindow() as the dialog parent to
    KMessageBox::warningTwoActions(). The null parent places the message box
    at the center of the primary screen and the qApp->activeWindow() parent
    - at the center of whatever top-level KDevelop window is active,
    possibly a floating dock widget. IUiController::activeMainWindow() is
    the most common parent of KDevelop dialogs. The activeMainWindow()
    parent places the message box at the center of the KDevelop main window.
    Pass activeMainWindow() as the dialog parent in MIDebugLauncher::start()
    too.
    
    LldbLauncher::start() prints a warning and returns nullptr if the
    ILaunchConfiguration* argument is null. GdbLauncher::start() asserts
    that the ILaunchConfiguration* argument is not null, then checks and
    returns nullptr if it *is* null. Assert non-null and do not check the
    argument in MIDebugLauncher::start(), because RunController::execute()
    performs the same check and in case of null returns early before calling
    ILauncher::start().
    
    GdbLauncher::start() asserts that m_execute and m_plugin are not null.
    Move the assertions to MIDebugLauncher() because the two pointer data
    members are constant and should be checked at the first opportunity.
    
    Change the type of the plugin parameter of GdbLauncher() and
    LldbLauncher() from GDB::CppDebuggerPlugin* and LldbDebuggerPlugin*
    respectively to their base class MIDebuggerPlugin* in order to reduce
    the number of includes and forward declarations. Each of the two
    launcher classes resides in its own plugin along with the corresponding
    plugin class, so the risk of accidentally constructing a launcher with a
    wrong plugin type is minimal.
    
    GdbLauncher::m_plugin is a QPointer while LldbLauncher::m_plugin is a
    raw pointer. Make MIDebugLauncher::m_plugin a raw pointer because the
    only use of the QPointer in GdbLauncher::start() asserts and assumes
    that it is not null.
    
    Add two OwningRawPointerContainer constructor overloads taking a
    container and use the rvalue reference overload to make
    MIDebugLauncher::m_configPages const.

--- plugins/lldb/lldblauncher.h
+++ plugins/lldb/lldblauncher.h
@@ -7,28 +7,17 @@
 #ifndef LLDBLAUNCHER_H
 #define LLDBLAUNCHER_H
 
-#include <interfaces/ilauncher.h>
+#include "midebuglauncher.h"
 
-class IExecutePlugin;
 namespace KDevMI { namespace LLDB {
 
-class LldbDebuggerPlugin;
-class LldbLauncher : public KDevelop::ILauncher
+class LldbLauncher : public MIDebugLauncher
 {
 public:
-    LldbLauncher(LldbDebuggerPlugin *plugin, IExecutePlugin *iexec);
-    ~LldbLauncher() override;
-    QList<KDevelop::LaunchConfigurationPageFactory*> configPages() const override;
+    explicit LldbLauncher(MIDebuggerPlugin* plugin, IExecutePlugin* execute);
     QString description() const override;
     QString id() override;
     QString name() const override;
-    KJob* start(const QString &launchMode, KDevelop::ILaunchConfiguration *cfg) override;
-    QStringList supportedModes() const override;
-
-private:
-    QList<KDevelop::LaunchConfigurationPageFactory*> m_factoryList;
-    LldbDebuggerPlugin *m_plugin;
-    IExecutePlugin *m_iexec;
 };
 
 } // end of namespace LLDB
