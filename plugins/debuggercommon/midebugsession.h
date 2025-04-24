99d9ffc85aeaa1ea2600dbbdc7b51c9ef6891914
Igor Kushnir <igorkuo@gmail.com>
2025-04-24 13:26:11 +0300

    Remove unnecessary MIDebugSession::inferiorTtyStdout()
    The signal MIDebugSession::inferiorTtyStdout() simply forwards the
    signal STTY::OutOutput() to the slot
    ProcessLineMaker::slotReceivedStdout(). The intermediary signal
    inferiorTtyStdout() is not and should not be connected to anywhere else.
    
    Remove the unneeded signal and its associated overhead, and connect
    slotReceivedStdout() to OutOutput() directly.

--- plugins/debuggercommon/midebugsession.h
+++ plugins/debuggercommon/midebugsession.h
@@ -77,11 +77,6 @@ Q_SIGNALS:
     void debuggerInternalOutput(const QString& output) const;
 
     /**
-     * Emits when received standard output from inferior's tty
-     */
-    void inferiorTtyStdout(const QByteArray& output);
-
-    /**
      * Emits when the debugger instance state changes
      */
     void debuggerStateChanged(DBGStateFlags oldState, DBGStateFlags newState);
