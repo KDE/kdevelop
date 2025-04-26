cc69d7c976e996413bf0effb436315b995d48347
root <root@localhost.localdomain>
2025-04-26 00:38:57 -0400

    webengine removal 2

--- plugins/qthelp/qthelpdocumentation.cpp
+++ plugins/qthelp/qthelpdocumentation.cpp
@@ -12,24 +12,26 @@
 QtHelpDocumentation::QtHelpDocumentation(QtHelpProviderAbstract* provider,
                                          const QString& name,
                                          const QList<QHelpLink>& info)
-    : m_provider(provider),
-      m_name(name),
-      m_info(info),
-      m_current(info.constBegin()),
-
+    : m_provider(provider)
+    , m_name(name)
+    , m_info(info)
+    , m_current(info.constBegin())
+{
+    // stub
+}
 
 QtHelpDocumentation::QtHelpDocumentation(QtHelpProviderAbstract* provider,
                                          const QString& name,
                                          const QList<QHelpLink>& info,
                                          const QString& key)
-    : m_provider(provider),
-      m_name(name),
-      m_info(info),
-      m_current(info.constBegin()),
- 
+    : m_provider(provider)
+    , m_name(name)
+    , m_info(info)
+    , m_current(info.constBegin())
 {
     Q_UNUSED(key);
 }
+
 
 QString QtHelpDocumentation::description() const{ return m_name;}
 
@@ -65,3 +67,4 @@ QString HomeDocumentation::name() const{ return i18n("webengine disabled");}
 KDevelop::IDocumentationProvider* HomeDocumentation::provider() const{ return nullptr;}
 
 bool HomeDocumentation::eventFilter(QObject* /*obj*/, QEvent* /*event*/){ return false;}
+
