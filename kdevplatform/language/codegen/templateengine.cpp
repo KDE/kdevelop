KDE/kdevelop

commit d551c341203dc118573139ade14dff4f7da1f1f4

templates: Add some debugging output
... to debug template archive loading issues under Windows

kdevplatform/language/codegen/templateengine.cpp
@@ -21,6 +21,7 @@
 
 #include "templateengine.h"
 
+#include "debug.h"
 #include "templateengine_p.h"
 
 #include "codedescription.h"
@@ -44,7 +45,17 @@ TemplateEngine::TemplateEngine()
 {
     d->engine.setSmartTrimEnabled(true);
 
-    addTemplateDirectories(QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, QStringLiteral("kdevcodegen/templates"), QStandardPaths::LocateDirectory));
+    qCDebug(LANGUAGE) << "Generic data locations:" << QStandardPaths::standardLocations(QStandardPaths::GenericDataLocation);
+
+    const auto templateDirectories = QStandardPaths::locateAll(QStandardPaths::GenericDataLocation,
+        QStringLiteral("kdevcodegen/templates"), QStandardPaths::LocateDirectory);
+
+    if (!templateDirectories.isEmpty()) {
+        qCDebug(LANGUAGE) << "Found template directories:" << templateDirectories;
+        addTemplateDirectories(QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, QStringLiteral("kdevcodegen/templates"), QStandardPaths::LocateDirectory));
+    } else {
+        qCWarning(LANGUAGE) << "No template directories found -- templating engine will not work!";
+    }
 
     Grantlee::registerMetaType<KDevelop::VariableDescription>();
     Grantlee::registerMetaType<KDevelop::FunctionDescription>();

plugins/appwizard/appwizardplugin.cpp
@@ -214,6 +214,8 @@ QString AppWizardPlugin::createProject(const ApplicationInfo& info)
     }
 
     QString templateName = templateInfo.baseName();
+    qCDebug(PLUGIN_APPWIZARD) << "Searching archive for template name:" << templateName;
+
     QString templateArchive;
     const QStringList filters = {templateName + QStringLiteral(".*")};
     const QStringList matchesPaths = QStandardPaths::locateAll(QStandardPaths::GenericDataLocation, QStringLiteral("kdevappwizard/templates/"), QStandardPaths::LocateDirectory);
@@ -228,6 +230,8 @@ QString AppWizardPlugin::createProject(const ApplicationInfo& info)
         qCWarning(PLUGIN_APPWIZARD) << "Template name does not exist in the template list";
         return QString();
     }
+
+    qCDebug(PLUGIN_APPWIZARD) << "Using template archive:" << templateArchive;
 
     QUrl dest = info.location;
 

commit 2888de7d0575119c7dffdc90f3d8bac9358c216f

TemplateEngine: don't call QStandardPaths::locateAll() redundantly
d551c341203dc118573139ade14dff4f7da1f1f4 accidentally duplicated the
call to QStandardPaths::locateAll().

kdevplatform/language/codegen/templateengine.cpp
@@ -42,9 +42,7 @@ TemplateEngine::TemplateEngine()
 
     if (!templateDirectories.isEmpty()) {
         qCDebug(LANGUAGE) << "Found template directories:" << templateDirectories;
-        addTemplateDirectories(QStandardPaths::locateAll(QStandardPaths::GenericDataLocation,
-                                                         QStringLiteral("kdevcodegen/templates"),
-                                                         QStandardPaths::LocateDirectory));
+        addTemplateDirectories(templateDirectories);
     } else {
         qCWarning(LANGUAGE) << "No template directories found -- templating engine will not work!";
     }