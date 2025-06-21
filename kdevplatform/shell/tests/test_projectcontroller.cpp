KDE/kdevelop

commit 5f3e04f5c6c870b3b4bafe57b372df74957185ff

Drop VcsFileChangesModel::{indexForUrl, statusInfo}
Dangerous API, e.g. when proxy models are involved. Encourage use of
QAbstractItemModel::match.

plugins/projectmanagerview/vcsoverlayproxymodel.cpp
@@ -59,7 +59,7 @@ QVariant VcsOverlayProxyModel::data(const QModelIndex& proxyIndex, int role) con
         } else {
             ProjectChangesModel* model = ICore::self()->projectController()->changesModel();
             const QUrl url = proxyIndex.data(ProjectModel::UrlRole).toUrl();
-            const QModelIndex idx = model->indexForUrl(url);
+            const auto idx = model->match(model->index(0, 0), ProjectChangesModel::UrlRole, url, 1, Qt::MatchExactly).value(0);
             return idx.sibling(idx.row(), 1).data(Qt::DisplayRole);
         }
     } else

plugins/vcschangesview/vcschangesview.cpp
@@ -146,13 +146,10 @@ void VcsChangesView::selectCurrentDocument()
     
     QUrl url = doc->url();
     IProject* p = ICore::self()->projectController()->findProjectForUrl(url);
-    QModelIndex idx;
-    
-    if(p) {
-        ProjectChangesModel* pcmodel = static_cast<ProjectChangesModel*>(model());
-        idx = pcmodel->indexForUrl(url);
-    }
-    
+    QModelIndex idx = (p ?
+        model()->match(model()->index(0, 0), ProjectChangesModel::UrlRole,
+                       url, 1, Qt::MatchExactly).value(0) :
+        QModelIndex());
     if(idx.isValid()) {
         expand(idx.parent());
         setCurrentIndex(idx);

plugins/patchreview/patchreviewtoolview.cpp
@@ -386,7 +386,7 @@ void PatchReviewToolView::fileItemChanged( QStandardItem* item )
     if (item->column()!=0)
         return;
 
-    QUrl url = m_fileModel->statusInfo(item->index()).url();
+    QUrl url = item->index().data(VcsFileChangesModel::UrlRole).toUrl();
     if (url.isEmpty())
         return;
 
@@ -436,10 +436,9 @@ void PatchReviewToolView::finishReview() {
     m_plugin->finishReview( selectedUrls );
 }
 
-void PatchReviewToolView::fileDoubleClicked( const QModelIndex& idx ) {
-    QModelIndex i = idx.sibling(idx.row(), 0);
-    QUrl file = m_fileModel->statusInfo( i ).url();
-
+void PatchReviewToolView::fileDoubleClicked( const QModelIndex& idx )
+{
+    const QUrl file = idx.data(VcsFileChangesModel::UrlRole).toUrl();
     activate( file );
 }
 
@@ -501,12 +500,10 @@ void PatchReviewToolView::documentActivated( IDocument* doc ) {
     if ( !m_plugin->modelList() )
         return;
 
-    QModelIndex idx = m_fileModel->indexForUrl( doc->url() );
-    if ( idx.isValid() ) {
-        m_editPatch.filesList->setCurrentIndex( idx );
-    } else {
-        m_editPatch.filesList->setCurrentIndex( QModelIndex() );
-    }
+    const auto matches = m_fileSortProxyModel->match(
+        m_fileSortProxyModel->index(0, 0), VcsFileChangesModel::UrlRole,
+        doc->url(), 1, Qt::MatchExactly);
+    m_editPatch.filesList->setCurrentIndex(matches.value(0));
 }
 
 void PatchReviewToolView::runTests()

vcs/models/tests/test_models.cpp
@@ -30,7 +30,7 @@ using namespace KDevelop;
 
 void TestModels::initTestCase()
 {
-    AutoTestShell::init();
+    AutoTestShell::init({"dummy"});
     TestCore::initialize();
 }
 
@@ -41,6 +41,14 @@ void TestModels::cleanupTestCase()
 
 void TestModels::testVcsFileChangesModel()
 {
+    const auto indexForUrl = [](const VcsFileChangesModel* model, const QUrl& url) {
+        return model->match(model->index(0, 0), VcsFileChangesModel::UrlRole,
+                            url, 1, Qt::MatchExactly).value(0);
+    };
+    const auto statusInfo = [](const QModelIndex& idx) {
+        return idx.data(VcsFileChangesModel::VcsStatusInfoRole).value<VcsStatusInfo>();
+    };
+
     VcsFileChangesModel *model = new VcsFileChangesModel(this);
 
     // Newly created model should be empty
@@ -73,9 +81,9 @@ void TestModels::testVcsFileChangesModel()
 
     // Check that all OK
     for(int i = 0; i < 3; i++) {
-        QModelIndex idx = model->indexForUrl(filenames[i]);
+        QModelIndex idx = indexForUrl(model, filenames[i]);
         QVERIFY(idx.isValid());
-        VcsStatusInfo info = model->statusInfo(idx);
+        VcsStatusInfo info = statusInfo(idx);
         QVERIFY(info.url().isValid());
         QCOMPARE(info.url(), filenames[i]);
         QCOMPARE(info.state(), states[i]);
@@ -91,9 +99,9 @@ void TestModels::testVcsFileChangesModel()
 
     // Check that all OK
     for(int i = 0; i < 3; i++) {
-        QModelIndex item = model->indexForUrl(filenames[i]);
+        QModelIndex item = indexForUrl(model, filenames[i]);
         QVERIFY(item.isValid());
-        VcsStatusInfo info = model->statusInfo(item);
+        VcsStatusInfo info = statusInfo(item);
         QCOMPARE(info.url(), filenames[i]);
         QCOMPARE(info.state(), states[i]);
     }
@@ -110,9 +118,9 @@ void TestModels::testVcsFileChangesModel()
     // Check them all
     for(int i = 0; i < 3; i++) {
         if(states[i] != VcsStatusInfo::ItemUpToDate && states[i] != VcsStatusInfo::ItemUnknown) {
-            QModelIndex item = model->indexForUrl(filenames[i]);
+            QModelIndex item = indexForUrl(model, filenames[i]);
             QVERIFY(item.isValid());
-            VcsStatusInfo info = model->statusInfo(item);
+            VcsStatusInfo info = statusInfo(item);
             QCOMPARE(info.url(), filenames[i]);
             QCOMPARE(info.state(), states[i]);
         }

vcs/models/vcsfilechangesmodel.cpp
@@ -172,28 +172,12 @@ QVariant VcsFileChangesModel::data(const QModelIndex &index, int role) const
 QStandardItem* VcsFileChangesModel::fileItemForUrl(QStandardItem* parent, const QUrl& url) const
 {
     for(int i=0, c=parent->rowCount(); i<c; i++) {
-        if(statusInfo(i, parent->index()).url()==url) {
+        QStandardItem* item = parent->child(i);
+        if(indexFromItem(item).data(UrlRole).toUrl() == url) {
             return parent->child(i);
         }
     }
     return 0;
-}
-
-QList<VcsStatusInfo> VcsFileChangesModel::checkedStatuses(QStandardItem *parent) const
-{
-    QList<VcsStatusInfo> ret;
-
-    if(!d->allowSelection)
-        return ret;
-
-    for(int i = 0, c = parent->rowCount(); i < c; i++) {
-        QStandardItem* item = parent->child(i);
-        if(item->checkState() == Qt::Checked) {
-            ret << statusInfo(i, parent->index());
-        }
-    }
-
-    return ret;
 }
 
 void VcsFileChangesModel::setAllChecked(bool checked)
@@ -210,30 +194,22 @@ void VcsFileChangesModel::setAllChecked(bool checked)
 QList<QUrl> VcsFileChangesModel::checkedUrls(QStandardItem *parent) const
 {
     QList<QUrl> ret;
-
     for(int i = 0, c = parent->rowCount(); i < c; i++) {
         QStandardItem* item = parent->child(i);
         if(!d->allowSelection || item->checkState() == Qt::Checked) {
-            ret << statusInfo(i, parent->index()).url();
+            ret << indexFromItem(item).data(UrlRole).toUrl();
         }
     }
-
     return ret;
-}
-
-VcsStatusInfo VcsFileChangesModel::statusInfo(int row, const QModelIndex &parent) const
-{
-    return index(row, 1, parent).data(VcsStatusInfoRole).value<VcsStatusInfo>();
 }
 
 QList<QUrl> VcsFileChangesModel::urls(QStandardItem *parent) const
 {
     QList<QUrl> ret;
-
     for(int i = 0, c = parent->rowCount(); i < c; i++) {
-        ret << statusInfo(i, parent->index()).url();
+        QStandardItem* item = parent->child(i);
+        ret << indexFromItem(item).data(UrlRole).toUrl();
     }
-
     return ret;
 }
 
@@ -246,7 +222,9 @@ void VcsFileChangesModel::checkUrls(QStandardItem *parent, const QList<QUrl>& ur
 
     for(int i = 0, c = parent->rowCount(); i < c; i++) {
         QStandardItem* item = parent->child(i);
-        item->setCheckState(urlSet.contains(statusInfo(i, parent->index()).url()) ? Qt::Checked : Qt::Unchecked);
+        item->setCheckState(urlSet.contains(indexFromItem(item).data(UrlRole).toUrl()) ?
+            Qt::Checked :
+            Qt::Unchecked);
     }
 }
 
@@ -260,35 +238,14 @@ bool VcsFileChangesModel::isCheckable() const
     return d->allowSelection;
 }
 
-QModelIndex VcsFileChangesModel::indexForUrl(const QUrl& url) const
-{
-    return indexForUrl(QModelIndex(), url);
-}
-
-QModelIndex VcsFileChangesModel::indexForUrl(const QModelIndex& parent, const QUrl& url) const
-{
-    for (int i=0, c=rowCount(parent); i<c; ++i) {
-        QModelIndex idx = index(i, 0, parent);
-        if (hasChildren(idx)) {
-            QModelIndex cidx = indexForUrl(idx, url);
-            if (cidx.isValid())
-                return cidx;
-        } else {
-            if (statusInfo(idx.row(), idx.parent()).url() == url) {
-                return idx;
-            }
-        }
-    }
-    return {};
-}
-
 bool VcsFileChangesModel::removeUrl(const QUrl& url)
 {
-    const QModelIndex idx = indexForUrl(url);
-    if(idx.isValid()) {
-        removeRow(idx.row(), idx.parent());
-    }
-    return idx.isValid();
+    const auto matches = match(index(0, 0), UrlRole, url, 1, Qt::MatchExactly);
+    if (matches.isEmpty())
+        return false;
+
+    const auto& idx = matches.first();
+    return removeRow(idx.row(), idx.parent());
 }
 
 }

vcs/models/vcsfilechangesmodel.h
@@ -92,16 +92,9 @@ public:
      * */
     void setAllChecked(bool checked);
 
-    /**
-     * Simple helper to get VcsStatusInfo.
-     */
-    VcsStatusInfo statusInfo(int row, const QModelIndex &parent) const;
-    VcsStatusInfo statusInfo(const QModelIndex &idx) const { return statusInfo(idx.row(), idx.parent()); }
-
     void setIsCheckbable(bool checkable);
     bool isCheckable() const;
 
-    QModelIndex indexForUrl(const QUrl& url) const;
     bool removeUrl(const QUrl& url);
 
 public slots:
@@ -144,7 +137,6 @@ protected:
      * Returns item for particular url.
      */
     QStandardItem* fileItemForUrl(QStandardItem *parent, const QUrl &url) const;
-    QModelIndex indexForUrl(const QModelIndex& parent, const QUrl &url) const;
 
 private:
     QScopedPointer<VcsFileChangesModelPrivate> const d;

commit 91b9dc05c7f304ae515c794064e26f82c7c6e7cb

Tie the lifetime of ProjectChangesModel to VcsChangesView
Not long after the ProjectChangesModel class was added to KDevPlatform,
8c8b4bbf4b6807f7c379cd2366b587a28bdd177f started creating an instance of
the model only when a VcsChangesView instance, which requires it, was
created (i.e. only when the Project Changes tool view widget was
created). Then 8e9c863e76d8229bdccc6a7b56af2f1cbed3d98a reused the
ProjectChangesModel in VcsOverlayProxyModel::data(). A
VcsOverlayProxyModel instance is created whenever a widget for the much
more widely used Projects tool view, present by default in the Code and
the Review sublime areas, is created. The construction of the
ProjectChangesModel instance in VcsOverlayProxyModel::data() caused a
crash due to a nested event loop, so
6d4591fb1733f4490d77d9e6a1e619b31767d49b switched from creating the
instance of ProjectChangesModel on demand to unconditionally creating it
in ProjectController::initialize().

ProjectChangesModel tracks the VCS statuses of all project files and
does so very inefficiently. The inefficiency causes UI freezes discussed
in the linked bug report.

Store a std::weak_ptr<ProjectChangesModel> in ProjectControllerPrivate
and a std::shared_ptr<ProjectChangesModel> in VcsChangesView in order to
keep the slow ProjectChangesModel instance alive only as long as a
VcsChangesView instance exists. Now the user can work around the UI
freezes by removing the Project Changes tool view. The author of all
relevant code, Aleix expressed hope that this optimization will be
restored eventually in a comment under https://phabricator.kde.org/D8852

Do not display the VCS status of non-project items in the Projects tool
view if the ProjectChangesModel instance does not exist. This VCS status
info in the Projects tool view is not essential. No one has complained
about its absence for many years between the regression introduced in
5f3e04f5c6c870b3b4bafe57b372df74957185ff and a recent commit that fixed

kdevplatform/interfaces/iprojectcontroller.h
@@ -14,6 +14,8 @@
 
 #include "interfacesexport.h"
 
+#include <memory>
+
 namespace KDevelop
 {
 
@@ -42,13 +44,18 @@ public:
      * @returns the model containing the projects and their items
      */
     virtual ProjectModel* projectModel() = 0;
-    
-    
+
     /**
-     * @returns an instance to the model that keeps track of the state 
-     * of the files per project.
+     * @return the model that keeps track of the VCS statuses of project files
+     *         or @c nullptr if the model is currently nonexistent
      */
-    virtual ProjectChangesModel* changesModel() = 0;
+    [[nodiscard]] virtual std::shared_ptr<ProjectChangesModel> changesModel() const = 0;
+    /**
+     * @return the model that keeps track of the VCS statuses of project files
+     *
+     * If the model does not currently exist, a new one is created and returned.
+     */
+    virtual std::shared_ptr<ProjectChangesModel> makeChangesModel() = 0;
 
     virtual ProjectBuildSetModel* buildSetModel() = 0;
 

kdevplatform/project/projectchangesmodel.cpp
@@ -55,7 +55,10 @@ ProjectChangesModel::ProjectChangesModel(QObject* parent)
 
 ProjectChangesModel::~ProjectChangesModel()
 {
-    // TODO (once ProjectController is no longer the parent of this): remove all open projects.
+    const auto projects = ICore::self()->projectController()->projects();
+    for (auto* const project : projects) {
+        removeProject(project);
+    }
 }
 
 void ProjectChangesModel::addProject(IProject* p)

kdevplatform/project/projectchangesmodel.h
@@ -22,7 +22,7 @@ class KDEVPLATFORMPROJECT_EXPORT ProjectChangesModel : public VcsFileChangesMode
     Q_OBJECT
     public:
         enum Role { ProjectNameRole = LastItemRole+1 };
-        explicit ProjectChangesModel(QObject* parent);
+        explicit ProjectChangesModel(QObject* parent = nullptr);
         ~ProjectChangesModel() override;
         
         void updateState(KDevelop::IProject* p, const KDevelop::VcsStatusInfo& status);

kdevplatform/shell/projectcontroller.cpp
@@ -93,7 +93,7 @@ public:
     ProjectBuildSetModel* buildset;
     bool m_foundProjectFile; //Temporary flag used while searching the hierarchy for a project file
     bool m_cleaningUp; //Temporary flag enabled while destroying the project-controller
-    ProjectChangesModel* m_changesModel = nullptr;
+    std::weak_ptr<ProjectChangesModel> m_changesModel;
     QHash<IProject*, KJob*> m_parseJobs; // parse jobs that add files from the project to the background parser.
 
     ProjectControllerPrivate(Core* core, ProjectController* p)
@@ -668,8 +668,6 @@ void ProjectController::initialize()
              d->buildset, &ProjectBuildSetModel::saveToProject );
     connect( this, &ProjectController::projectClosed,
              d->buildset, &ProjectBuildSetModel::projectClosed );
-
-    d->m_changesModel = new ProjectChangesModel(this);
 
     loadSettings(false);
     d->dialog = new ProjectDialogProvider(d);
@@ -1251,11 +1249,23 @@ ProjectBuildSetModel* ProjectController::buildSetModel()
     return d->buildset;
 }
 
-ProjectChangesModel* ProjectController::changesModel()
+std::shared_ptr<ProjectChangesModel> ProjectController::changesModel() const
+{
+    Q_D(const ProjectController);
+
+    return d->m_changesModel.lock();
+}
+
+std::shared_ptr<ProjectChangesModel> ProjectController::makeChangesModel()
 {
     Q_D(ProjectController);
 
-    return d->m_changesModel;
+    auto model = d->m_changesModel.lock();
+    if (!model) {
+        model = std::make_shared<ProjectChangesModel>();
+        d->m_changesModel = model;
+    }
+    return model;
 }
 
 void ProjectController::commitCurrentProject()

kdevplatform/shell/projectcontroller.h
@@ -71,7 +71,8 @@ public:
 
     ProjectBuildSetModel* buildSetModel() override;
     ProjectModel* projectModel() override;
-    ProjectChangesModel* changesModel() override;
+    [[nodiscard]] std::shared_ptr<ProjectChangesModel> changesModel() const override;
+    std::shared_ptr<ProjectChangesModel> makeChangesModel() override;
     IProject* findProjectByName( const QString& name ) override;
     IProject* findProjectForUrl( const QUrl& ) const override;
 

plugins/projectmanagerview/vcsoverlayproxymodel.cpp
@@ -54,10 +54,14 @@ QVariant VcsOverlayProxyModel::data(const QModelIndex& proxyIndex, int role) con
             return m_branchName.value(project);
         }
 
-        ProjectChangesModel* model = ICore::self()->projectController()->changesModel();
-        if (const auto* const projectItem = model->projectItem(project)) {
-            const QUrl url = proxyIndex.data(ProjectModel::UrlRole).toUrl();
-            return ProjectChangesModel::statusIndexForUrl(*model, projectItem->index(), url).data(Qt::DisplayRole);
+        // Display the VCS status of the item only if the changes model already exists. The VCS status
+        // is not essential, not worth the overhead and possible UI freezes due to the inefficient
+        // tracking of VCS statuses by ProjectChangesModel. See https://bugs.kde.org/show_bug.cgi?id=486949
+        if (const auto model = ICore::self()->projectController()->changesModel()) {
+            if (const auto* const projectItem = model->projectItem(project)) {
+                const QUrl url = proxyIndex.data(ProjectModel::UrlRole).toUrl();
+                return ProjectChangesModel::statusIndexForUrl(*model, projectItem->index(), url).data(Qt::DisplayRole);
+            }
         }
 
         return {};

plugins/vcschangesview/vcschangesview.cpp
@@ -30,6 +30,7 @@ using namespace KDevelop;
 
 VcsChangesView::VcsChangesView(VcsProjectIntegrationPlugin* plugin, QWidget* parent)
     : QTreeView(parent)
+    , m_model{ICore::self()->projectController()->makeChangesModel()}
 {
     setRootIsDecorated(false);
     setEditTriggers(QAbstractItemView::NoEditTriggers);
@@ -49,6 +50,9 @@ VcsChangesView::VcsChangesView(VcsProjectIntegrationPlugin* plugin, QWidget* par
     QAction* action = plugin->actionCollection()->action(QStringLiteral("locate_document"));
     connect(action, &QAction::triggered, this, &VcsChangesView::selectCurrentDocument);
     connect(this, &VcsChangesView::doubleClicked, this, &VcsChangesView::openSelected);
+
+    connect(m_model.get(), &QAbstractItemModel::rowsInserted, this, &VcsChangesView::expand);
+    setModel(m_model.get());
 }
 
 static void appendActions(QMenu* menu, const QList<QAction*>& actions)
@@ -124,9 +128,9 @@ void VcsChangesView::popupContextMenu( const QPoint &pos )
         QAction* res = menu->exec(viewport()->mapToGlobal(pos));
         if(res == refreshAction) {
             if(!urls.isEmpty())
-                emit reload(urls);
+                m_model->reload(urls);
             else
-                emit reload(projects);
+                m_model->reload(projects);
         }
     }
     delete menu;
@@ -141,10 +145,8 @@ void VcsChangesView::selectCurrentDocument()
     QUrl url = doc->url();
     QModelIndex idx;
     if (const auto* const project = ICore::self()->projectController()->findProjectForUrl(url)) {
-        if (const auto* const projectModel = qobject_cast<ProjectChangesModel*>(model())) {
-            if (const auto* const projectItem = projectModel->projectItem(project)) {
-                idx = ProjectChangesModel::statusIndexForUrl(*projectModel, projectItem->index(), url);
-            }
+        if (const auto* const projectItem = m_model->projectItem(project)) {
+            idx = ProjectChangesModel::statusIndexForUrl(*m_model, projectItem->index(), url);
         }
     }
 
@@ -153,12 +155,6 @@ void VcsChangesView::selectCurrentDocument()
         setCurrentIndex(idx);
     } else
         collapseAll();
-}
-
-void VcsChangesView::setModel(QAbstractItemModel* model)
-{
-    connect(model, &QAbstractItemModel::rowsInserted, this, &VcsChangesView::expand);
-    QTreeView::setModel(model);
 }
 
 void VcsChangesView::openSelected(const QModelIndex& index)

plugins/vcschangesview/vcschangesview.h
@@ -8,28 +8,28 @@
 #define KDEVPLATFORM_PLUGIN_VCSCHANGESVIEW_H
 
 #include <QTreeView>
-#include <QList>
-#include <QUrl>
+
+#include <memory>
 
 class VcsProjectIntegrationPlugin;
-namespace KDevelop { class IProject; }
+
+namespace KDevelop {
+class ProjectChangesModel;
+}
 
 class VcsChangesView : public QTreeView
 {
     Q_OBJECT
     public:
         explicit VcsChangesView(VcsProjectIntegrationPlugin* plugin, QWidget* parent = nullptr);
-        
-        void setModel(QAbstractItemModel* model) override;
-        
+
     public Q_SLOTS:
         void popupContextMenu( const QPoint &pos );
         void selectCurrentDocument();
         void openSelected(const QModelIndex& idx);
-        
-    Q_SIGNALS:
-        void reload(const QList<KDevelop::IProject*>& p);
-        void reload(const QList<QUrl>& p);
+
+private:
+    const std::shared_ptr<KDevelop::ProjectChangesModel> m_model;
 };
 
 #endif // KDEVPLATFORM_PLUGIN_VCSCHANGESVIEW_H

plugins/vcschangesview/vcschangesviewplugin.cpp
@@ -14,7 +14,6 @@
 
 #include <interfaces/icore.h>
 #include <interfaces/iprojectcontroller.h>
-#include <interfaces/iproject.h>
 #include <interfaces/iuicontroller.h>
 #include <project/projectchangesmodel.h>
 #include "vcschangesview.h"
@@ -30,13 +29,7 @@ public:
 
     QWidget* create(QWidget *parent = nullptr) override
     {
-        auto* modif = new VcsChangesView(m_plugin, parent);
-        modif->setModel(m_plugin->model());
-        QObject::connect(modif, QOverload<const QList<KDevelop::IProject*>&>::of(&VcsChangesView::reload),
-                         m_plugin->model(), QOverload<const QList<KDevelop::IProject*>&>::of(&ProjectChangesModel::reload));
-        QObject::connect(modif, QOverload<const QList<QUrl>&>::of(&VcsChangesView::reload), m_plugin->model(),
-                         QOverload<const QList<QUrl>&>::of(&ProjectChangesModel::reload));
-        return modif;
+        return new VcsChangesView(m_plugin, parent);
     }
 
     Qt::DockWidgetArea defaultPosition() const override
@@ -57,7 +50,6 @@ VcsProjectIntegrationPlugin::VcsProjectIntegrationPlugin(QObject* parent, const 
                                                          const QVariantList&)
     : KDevelop::IPlugin(QStringLiteral("kdevvcsprojectintegration"), parent, metaData)
     , m_factory(new VCSProjectToolViewFactory(this))
-    , m_model(nullptr)
 {
     core()->uiController()->addToolView(i18nc("@title:window", "Project Changes"), m_factory);
 
@@ -70,21 +62,16 @@ VcsProjectIntegrationPlugin::VcsProjectIntegrationPlugin(QObject* parent, const 
     reloadaction->setText(i18nc("@action", "Reload View"));
     reloadaction->setIcon(QIcon::fromTheme(QStringLiteral("view-refresh")));
     reloadaction->setToolTip(i18nc("@info:tooltip", "Refresh the view for all projects, in case anything changed"));
+    connect(reloadaction, &QAction::triggered, this, [this] {
+        if (const auto model = core()->projectController()->changesModel()) {
+            model->reloadAll();
+        }
+    });
 }
 
 void VcsProjectIntegrationPlugin::unload()
 {
     core()->uiController()->removeToolView(m_factory);
-}
-
-ProjectChangesModel* VcsProjectIntegrationPlugin::model()
-{
-    if(!m_model) {
-        m_model = ICore::self()->projectController()->changesModel();
-        connect(actionCollection()->action(QStringLiteral("reload_view")), &QAction::triggered, m_model, &ProjectChangesModel::reloadAll);
-    }
-
-    return m_model;
 }
 
 #include "vcschangesviewplugin.moc"

plugins/vcschangesview/vcschangesviewplugin.h
@@ -11,22 +11,15 @@
 
 class VCSProjectToolViewFactory;
 
-namespace KDevelop
-{
-    class ProjectChangesModel;
-}
-
 class VcsProjectIntegrationPlugin : public KDevelop::IPlugin
 {
     Q_OBJECT
     public:
         VcsProjectIntegrationPlugin(QObject* parent, const KPluginMetaData& metaData, const QVariantList& args);
         void unload() override;
-        KDevelop::ProjectChangesModel* model();
 
     private:
         VCSProjectToolViewFactory* const m_factory;
-        KDevelop::ProjectChangesModel* m_model;
 };
 
 #endif

kdevplatform/shell/tests/test_projectcontroller.cpp
@@ -485,6 +485,20 @@ void TestProjectController::prettyFileName()
     QCOMPARE(m_projCtrl->prettyFileName(filePath.toUrl(), ProjectController::FormattingOptions::FormatPlain), QString(m_projName + ':' + relativeFilePath));
 }
 
+void TestProjectController::changesModel()
+{
+    QCOMPARE_EQ(m_projCtrl->changesModel(), nullptr); // the changes model is created only on demand
+    QCOMPARE_EQ(m_projCtrl->changesModel(), nullptr); // calling changesModel() does not create the model
+
+    auto model = m_projCtrl->makeChangesModel();
+    QVERIFY(model); // makeChangesModel() creates and returns the model
+    QCOMPARE_EQ(m_projCtrl->makeChangesModel(), model); // makeChangesModel() returns the existent model if possible
+    QCOMPARE_EQ(m_projCtrl->changesModel(), model); // changesModel() returns the existent model if available
+
+    model.reset();
+    QCOMPARE_EQ(m_projCtrl->changesModel(), nullptr); // resetting the shared pointer destroys the model
+}
+
 ////////////////////// Helpers ///////////////////////////////////////////////
 
 Path TestProjectController::writeProjectConfig(const QString& name)

kdevplatform/shell/tests/test_projectcontroller.h
@@ -48,6 +48,8 @@ private Q_SLOTS:
     void prettyFileName_data();
     void prettyFileName();
 
+    void changesModel();
+
 private:
     KDevelop::Path writeProjectConfig(const QString& name);
 