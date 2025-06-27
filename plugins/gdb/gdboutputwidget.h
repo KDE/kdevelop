629518714eed042eb15ee96f1c0442905c344dea
Igor Kushnir <igorkuo@gmail.com>
2025-06-27 22:03:48 +0300

    GDBOutputWidget: add missing and sort includes

--- plugins/gdb/gdboutputwidget.h
+++ plugins/gdb/gdboutputwidget.h
@@ -10,9 +10,11 @@
 
 #include "dbgglobal.h"
 
+#include <QColor>
 #include <QPlainTextEdit>
-#include <QTimer>
 #include <QStringList>
+#include <QTimer>
+#include <QWidget>
 
 namespace KDevelop {
 class IDebugSession;
