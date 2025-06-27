ee8c880d44dd4424b2894bde81bb01d14a99e4d9
Igor Kushnir <igorkuo@gmail.com>
2025-06-27 18:59:09 +0300

    Extract prepareStandardContextMenuToAddingCustomActions()
    The new helper function deduplicates correct preparation of a standard
    context menu and will be reused in other places in a subsequent commit.
    
    Explain how and why the context menu is prepared in the documentation
    for the new function.

--- kdevplatform/util/shellutils.h
+++ kdevplatform/util/shellutils.h
@@ -11,6 +11,7 @@
 #include <QList>
 #include <QString>
 
+class QMenu;
 class QUrl;
 class QWidget;
 
@@ -49,6 +50,21 @@ bool KDEVPLATFORMUTIL_EXPORT ensureWritable(const QList<QUrl>& urls);
  */
 bool KDEVPLATFORMUTIL_EXPORT restoreAndAutoSaveGeometry(QWidget& widget, const QString& configGroupName,
                                                         const QString& configSubgroupName = QString{});
+
+/**
+ * Prepare a QMenu pointer returned from createStandardContextMenu() to adding custom actions into the menu.
+ *
+ * The member function createStandardContextMenu() of a widget such as QTextEdit may return @c nullptr, in
+ * which case this function assigns @c new QMenu(@p menuParent) to @p standardContextMenu.
+ *
+ * If @p standardContextMenu is not empty, this function adds a separator to the menu.
+ *
+ * @param[in,out] standardContextMenu a pointer returned from createStandardContextMenu()
+ * @param menuParent the parent widget for the possible new context menu, this should be the widget on which
+ *        createStandardContextMenu() was invoked so that the parent of the displayed menu is always the same
+ */
+void KDEVPLATFORMUTIL_EXPORT prepareStandardContextMenuToAddingCustomActions(QMenu*& standardContextMenu,
+                                                                             QWidget* menuParent);
 }
 
 #endif // SHELLUTILS_H
