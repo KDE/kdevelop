KDE/kdevelop

commit eed45f006e53513ff44d7e85b6b6c3bafbc34b3a

Don't switch focus to the code-browser when shift+arrow was done.. The problem: kate uses shortcuts to listen for the arrow-keys, and the events for those never hit the event-filter. Thus watch for the cursorPositionChanged signal and if it is recognized reset the shift-press-detector.

plugins/contextbrowser/browsemanager.cpp
@@ -92,6 +92,8 @@ QList<KTextEditor::View*> EditorViewWatcher::allViews() {
 }
 
 BrowseManager::BrowseManager(ContextController* controller) : QObject(controller), m_controller(controller), m_watcher(this), m_browsing(false), m_browsingByKey(false) {
+    foreach(KTextEditor::View* view, m_watcher.allViews())
+        viewAdded(view);
 }
 
 KTextEditor::View* viewFromWidget(QWidget* widget) {
@@ -231,8 +233,19 @@ void BrowseManager::applyEventFilter(QWidget* object, bool install) {
             applyEventFilter(qobject_cast<QWidget*>(child), install);
 }
 
+void BrowseManager::viewAdded(KTextEditor::View* view) {
+    applyEventFilter(view, true);
+    //We need to listen for cursorPositionChanged, to clear the shift-detector. The problem: Kate listens for the arrow-keys using shortcuts,
+    //so those keys are not passed to the event-filter
+    QObject::connect(view, SIGNAL(cursorPositionChanged(KTextEditor::View*, const KTextEditor::Cursor&)), this, SLOT(cursorPositionChanged()));
+}
+
 void BrowseManager::Watcher::viewAdded(KTextEditor::View* view) {
-    m_manager->applyEventFilter(view, true);
+    m_manager->viewAdded(view);
+}
+
+void BrowseManager::cursorPositionChanged() {
+    m_shiftDetector.clear();
 }
 
 void BrowseManager::setBrowsing(bool enabled) {
@@ -245,9 +258,6 @@ void BrowseManager::setBrowsing(bool enabled) {
     //This collects all the views
     if(enabled) {
         kDebug() << "Enabled browsing-mode";
-        ///Enable browsing, install an event-filter on all events
-        foreach(KTextEditor::View* view, m_watcher.allViews())
-            applyEventFilter(view, true);
     }else{
         kDebug() << "Disabled browsing-mode";
         resetChangedCursor();

plugins/contextbrowser/browsemanager.h
@@ -41,7 +41,7 @@ namespace KDevelop {
 
 struct ShiftPressDetector {
     public:
-        ShiftPressDetector() : m_hadOtherKey(false) {
+        ShiftPressDetector() : m_hadOtherKey(true) {
         }
         ///Must be called with all key-events
         ///Returns true if the shift-key was released and no other key was pressed between its press and release.
@@ -57,6 +57,9 @@ struct ShiftPressDetector {
             }
             
             return false;
+        }
+        void clear() {
+            m_hadOtherKey = true;
         }
     private:
     bool m_hadOtherKey;
@@ -94,7 +97,9 @@ class BrowseManager : public QObject {
     public slots:
         ///Enabled/disables the browsing mode
         void setBrowsing(bool);
+        void cursorPositionChanged();
     private:
+        void viewAdded(KTextEditor::View* view);
         class Watcher : public EditorViewWatcher {
             public:
             Watcher(BrowseManager* manager);

commit 70b796d94b2d9034cd05e5e3bcea4d2238a979ae

BrowseManager: apply event filter once per view, not twice
BrowseManager() creates an instance of Watcher, then calls viewAdded()
for each existing view. Watcher() calls
BrowseManager::applyEventFilter(view, true) for each existing view.
BrowseManager::viewAdded() also calls applyEventFilter(view, true).
Consequently, applyEventFilter(view, true) ends up being called twice
for each existing view.

The duplicate event filter application is inefficient but does not cause
a bug, because the second call to QObject::installEventFilter() with the
same arguments has no effect.

Normally the list of views is empty in BrowseManager(), but not if the
user disables then enables the Code Browser plugin on the Plugins page
of the Configure KDevelop dialog.

eed45f006e53513ff44d7e85b6b6c3bafbc34b3a introduced the duplicate event
filter application, apparently by mistake.

Remove the loop that calls viewAdded() from BrowseManager() and call
Watcher::viewAdded() (which simply calls BrowseManager::viewAdded())
instead of applyEventFilter() in Watcher() to deduplicate and optimize.

plugins/contextbrowser/browsemanager.cpp
@@ -108,11 +108,6 @@ BrowseManager::BrowseManager(ContextBrowserPlugin* controller)
     m_delayedBrowsingTimer->setInterval(300);
 
     connect(m_delayedBrowsingTimer, &QTimer::timeout, this, &BrowseManager::eventuallyStartDelayedBrowsing);
-
-    const auto views = m_watcher.allViews();
-    for (KTextEditor::View* view : views) {
-        viewAdded(view);
-    }
 }
 
 KTextEditor::View* viewFromWidget(QWidget* widget)
@@ -384,7 +379,7 @@ Watcher::Watcher(BrowseManager* manager)
 {
     const auto views = allViews();
     for (KTextEditor::View* view : views) {
-        m_manager->applyEventFilter(view, true);
+        viewAdded(view);
     }
 }
 