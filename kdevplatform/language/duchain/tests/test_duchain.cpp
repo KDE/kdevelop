KDE/kdevelop

commit 2a76d4ff5a32357e8b304bc80ad097c0a83ffaba

add isRValue / setIsRValue to IndexedTypeIdentifier and ReferenceType
also increase DUChain Version number to 70

this is required for proper c++0x support

please review the hash functions, I have no idea if that is OK

language/duchain/types/referencetype.cpp
@@ -76,6 +76,16 @@ void ReferenceType::setBaseType(AbstractType::Ptr type)
   d_func_dynamic()->m_baseType = type->indexed();
 }
 
+bool ReferenceType::isRValue() const
+{
+  return d_func()->m_isRValue;
+}
+
+void ReferenceType::setIsRValue(bool isRValue)
+{
+  d_func_dynamic()->m_isRValue = isRValue;
+}
+
 void ReferenceType::accept0 (TypeVisitor *v) const
 {
   if (v->visit (this))
@@ -93,10 +103,11 @@ QString ReferenceType::toString() const
 {
   AbstractType::Ptr base = baseType();
   QString baseString = (base ? base->toString() : "<notype>");
+  const QString ampersands = d_func()->m_isRValue ? "&&" : "&";
   if(base.cast<IntegralType>() || base.cast<StructureType>())
-    return AbstractType::toString(false) + QString("%1&").arg(baseString);
+    return AbstractType::toString(false) + baseString + ampersands;
   else
-    return QString("%1&").arg(baseString + AbstractType::toString(true));
+    return baseString + AbstractType::toString(true) + ampersands;
 }
 
 AbstractType::WhichType ReferenceType::whichType() const
@@ -106,7 +117,7 @@ AbstractType::WhichType ReferenceType::whichType() const
 
 uint ReferenceType::hash() const
 {
-  return AbstractType::hash() + (d_func()->m_baseType.hash()+1) * 29;
+  return AbstractType::hash() + (d_func()->m_baseType.hash()+1) * 29 + (d_func()->m_isRValue ? 43768 : 0);
 }
 
 }

language/duchain/identifier.cpp
@@ -971,11 +971,16 @@ void QualifiedIdentifier::prepareWrite() {
 }
 
 uint IndexedTypeIdentifier::hash() const {
-    return m_identifier.getIndex() * 13 + (m_isConstant ? 17 : 0) + (m_isReference ? 12371 : 0) + m_pointerConstMask * 89321 + m_pointerDepth * 1023;
+    return m_identifier.getIndex() * 13 + (m_isConstant ? 17 : 0) + (m_isReference ? 12371 : 0) + (m_isRValue ? 4543 : 0) + m_pointerConstMask * 89321 + m_pointerDepth * 1023;
 }
 
 bool IndexedTypeIdentifier::operator==(const IndexedTypeIdentifier& rhs) const {
-    return m_identifier == rhs.m_identifier && m_isConstant == rhs.m_isConstant && m_isReference == rhs.m_isReference && m_pointerConstMask == rhs.m_pointerConstMask && m_pointerDepth == rhs.m_pointerDepth;
+    return m_identifier == rhs.m_identifier
+        && m_isConstant == rhs.m_isConstant
+        && m_isReference == rhs.m_isReference
+        && m_isRValue == rhs.m_isRValue
+        && m_pointerConstMask == rhs.m_pointerConstMask
+        && m_pointerDepth == rhs.m_pointerDepth;
 }
 
 bool IndexedTypeIdentifier::operator!=(const IndexedTypeIdentifier& rhs) const {
@@ -988,6 +993,14 @@ bool IndexedTypeIdentifier::isReference() const {
 
 void IndexedTypeIdentifier::setIsReference(bool isRef) {
   m_isReference = isRef;
+}
+
+bool IndexedTypeIdentifier::isRValue() const {
+  return m_isRValue;
+}
+
+void IndexedTypeIdentifier::setIsRValue(bool isRVal) {
+  m_isRValue = isRVal;
 }
 
 bool IndexedTypeIdentifier::isConstant() const {
@@ -1040,11 +1053,23 @@ QString IndexedTypeIdentifier::toString(bool ignoreExplicitlyGlobal) const {
   return ret;
 }
 
-IndexedTypeIdentifier::IndexedTypeIdentifier(KDevelop::IndexedQualifiedIdentifier identifier) : m_identifier(identifier), m_isConstant(false), m_isReference(false), m_pointerDepth(0), m_pointerConstMask(0) {
-}
+IndexedTypeIdentifier::IndexedTypeIdentifier(KDevelop::IndexedQualifiedIdentifier identifier)
+: m_identifier(identifier)
+, m_isConstant(false)
+, m_isReference(false)
+, m_isRValue(false)
+, m_pointerDepth(0)
+, m_pointerConstMask(0)
+{ }
 
-IndexedTypeIdentifier::IndexedTypeIdentifier(const QString& identifier, bool isExpression) : m_identifier(QualifiedIdentifier(identifier, isExpression)), m_isConstant(false), m_isReference(false), m_pointerDepth(0), m_pointerConstMask(0) {
-}
+IndexedTypeIdentifier::IndexedTypeIdentifier(const QString& identifier, bool isExpression)
+: m_identifier(QualifiedIdentifier(identifier, isExpression))
+, m_isConstant(false)
+, m_isReference(false)
+, m_isRValue(false)
+, m_pointerDepth(0)
+, m_pointerConstMask(0)
+{ }
 
 IndexedIdentifier::IndexedIdentifier() : index(emptyConstantIdentifierPrivateIndex()) {
   if(shouldDoDUChainReferenceCounting(this)) {

language/duchain/identifier.h
@@ -327,6 +327,9 @@ public:
   bool isReference() const;
   void setIsReference(bool);
 
+  bool isRValue() const;
+  void setIsRValue(bool);
+
   bool isConstant() const;
   void setIsConstant(bool);
   
@@ -358,6 +361,7 @@ public:
     IndexedQualifiedIdentifier m_identifier;
     bool m_isConstant : 1;
     bool m_isReference : 1;
+    bool m_isRValue : 1;
     uint m_pointerDepth : 5;
     uint m_pointerConstMask : 25;
 };

language/duchain/repositories/itemrepository.cpp
@@ -42,7 +42,7 @@ const int crashesBeforeCleanup = 2;
 
 uint staticItemRepositoryVersion() {
   //Increase this to reset incompatible item-repositories
-  return 69;
+  return 70;
 }
 
 AbstractItemRepository::~AbstractItemRepository() {

language/duchain/types/referencetype.h
@@ -61,6 +61,20 @@ public:
    */
   void setBaseType(AbstractType::Ptr baseType);
 
+  /**
+   * Checks whether this type is an rvalue- or lvalue-reference type.
+   * 
+   * \returns true for rvalue-references, false for lvalue-references
+   */
+  bool isRValue() const;
+
+  /**
+   * Sets whether this type is an rvalue- or lvalue-reference type.
+   * 
+   * \param isRValue true for rvalue-references, false for lvalue-references
+   */
+  void setIsRValue(bool isRValue);
+
   virtual QString toString() const;
 
   virtual uint hash() const;

language/duchain/types/typesystem.cpp
@@ -88,12 +88,12 @@ PointerTypeData::PointerTypeData( const PointerTypeData& rhs )
 {
 }
 
-ReferenceTypeData::ReferenceTypeData() : m_baseType(0)
+ReferenceTypeData::ReferenceTypeData() : m_baseType(0), m_isRValue( false )
 {
 }
 
 ReferenceTypeData::ReferenceTypeData( const ReferenceTypeData& rhs )
-  : AbstractTypeData( rhs ), m_baseType( rhs.m_baseType )
+  : AbstractTypeData( rhs ), m_baseType( rhs.m_baseType ), m_isRValue( rhs.m_isRValue )
 {
 }
 

language/duchain/types/typesystemdata.h
@@ -134,6 +134,8 @@ public:
   ReferenceTypeData( const ReferenceTypeData& rhs );
   /// Type of data which is referenced
   IndexedType m_baseType;
+  /// True if this is an rvalue-reference, false for lvalue-references
+  bool m_isRValue : 1;
 };
 
 KDEVPLATFORMLANGUAGE_EXPORT DECLARE_LIST_MEMBER_HASH(FunctionTypeData, m_arguments, IndexedType)

commit ce1f1845582634d21208465289ccad4f9ec63a61

Add missing comparison of m_isRValue to ReferenceType::equals()
2a76d4ff5a32357e8b304bc80ad097c0a83ffaba introduced a new data member
ReferenceTypeData::m_isRValue but failed to compare it in
ReferenceType::equals().

The wrong implementation of ReferenceType::equals() can trigger the
following assertion failure in ItemRepository::index():
    found item in unexpected bucket, ensure your ItemRequest::equals
    method is correct. Note: For custom AbstractType's e.g. ensure you
    have a proper equals() override
Once the assertion failure occurs, it recurs each time KDevelop is
started, until the cache is cleared.

The assertion failure is triggered in case of a hash collision in
Bucket::findIndex() when ItemRepository::index() attempts to insert a
ReferenceType object A into a bucket that contains another ReferenceType
object B such that A and B differ only in the value of m_isRValue.
More specifically, if the remainders of the integer division by
ObjectMapSize of A.hash() and B.hash() happen to be equal (despite
different return values of ReferenceType::hash(), which does take
m_isRValue into account), the two ReferenceType objects are compared via
equals(), which wrongly returns true.

Fixes #81

kdevplatform/language/duchain/types/referencetype.cpp
@@ -40,7 +40,7 @@ bool ReferenceType::equals(const AbstractType* _rhs) const
     Q_ASSERT(dynamic_cast<const ReferenceType*>(_rhs));
     const auto* rhs = static_cast<const ReferenceType*>(_rhs);
 
-    return d_func()->m_baseType == rhs->d_func()->m_baseType;
+    return d_func()->m_baseType == rhs->d_func()->m_baseType && d_func()->m_isRValue == rhs->d_func()->m_isRValue;
 }
 
 ReferenceType::ReferenceType()

kdevplatform/language/duchain/tests/test_duchain.cpp
@@ -26,6 +26,7 @@
 #include <language/duchain/duchainregister.h>
 #include <language/duchain/problem.h>
 #include <language/duchain/parsingenvironment.h>
+#include <language/duchain/types/referencetype.h>
 
 #include <language/codegen/coderepresentation.h>
 
@@ -954,6 +955,31 @@ void TestDUChain::testTypePtr()
 
     auto abstractT3 = AbstractType::Ptr(integralT);
     QCOMPARE(abstractT2.get(), integralT.get());
+}
+
+void TestDUChain::testReferenceType()
+{
+    IntegralType::Ptr integralT(new IntegralType(IntegralType::TypeDouble));
+
+    ReferenceType ref;
+    ref.setIsRValue(false);
+    ref.setBaseType(integralT);
+    QCOMPARE(ref.toString(), "double&");
+
+    ReferenceType rValueRef;
+    rValueRef.setIsRValue(true);
+    rValueRef.setBaseType(integralT);
+    QCOMPARE(rValueRef.toString(), "double&&");
+
+    QCOMPARE_NE(rValueRef.hash(), ref.hash());
+    QVERIFY(!rValueRef.equals(&ref));
+    QVERIFY(!ref.equals(&rValueRef));
+
+    // now toggle the flag to false and make sure the equalities succeed
+    rValueRef.setIsRValue(false);
+    QCOMPARE_EQ(rValueRef.hash(), ref.hash());
+    QVERIFY(rValueRef.equals(&ref));
+    QVERIFY(ref.equals(&rValueRef));
 }
 
 #if 0

kdevplatform/language/duchain/tests/test_duchain.h
@@ -34,6 +34,7 @@ private Q_SLOTS:
     void testProblemSerialization();
     void testIdentifiers();
     void testTypePtr();
+    void testReferenceType();
     ///NOTE: these are not "automated"!
 //     void testImportCache();
 